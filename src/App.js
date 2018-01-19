0a7c668dc6dd44028041ee0864fcfe8b4ab447bd
Callil Capuozzo <callil.capuozzo@gmail.com>
2018-01-19 12:55:52 -0500

    Token name from state

--- src/App.js
+++ src/App.js
@@ -319,15 +319,15 @@ class App extends Component {
         <section className="rate border pa2">
           <span className="rate-info">
             <p>Rate</p>
-            <p>{this.state.tokenBuyRate.toFixed(3)} UNI/ETH</p>
+            <p>{this.state.tokenBuyRate.toFixed(3)} {this.state.outputToken.value + "/" + this.state.inputToken.value}</p>
           </span>
           <span className="rate-info">
             <p>Cost</p>
-            <p>{this.state.tokenCost.toFixed(5)} ETH</p>
+            <p>{this.state.tokenCost.toFixed(5)} {this.state.inputToken.value}</p>
           </span>
           <span className="rate-info">
             <p>Fee</p>
-            <p>{this.state.tokenFee.toFixed(5)} ETH</p>
+            <p>{this.state.tokenFee.toFixed(5)} {this.state.inputToken.value}</p>
           </span>
         </section>
         {this.state.interaction === 'input' ?
