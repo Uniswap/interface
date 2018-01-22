f570c4686a6b1079d35aa0e90015e623f0c5d69c
Callil Capuozzo <callil.capuozzo@gmail.com>
2018-01-22 14:05:22 -0500

    Add UX features
    Detect all metamask states
    UX for approve transactions
    UX for getting more ETH from faucet
    UX for submitted transactions
    Toggle about

--- src/components/HelperMessages.js
+++ src/components/HelperMessages.js
@@ -4,22 +4,25 @@ function HelperMessages(props) {
   let message = ''
   switch (props.interaction) {
     case 'connected':
-      message = "Nice! You're connected. Enter an input value below to get started."
+      message = <p>{"Nice! You're connected. Enter an input value below to get started."}</p>
+      break;
+    case 'locked':
+      message = <p>{"Your metamask is locked! Please unlock to continue."}</p>
       break;
     case 'input':
-      message = "You're swapping " + props.inputToken.value + " for " + props.outputToken.value + ". Want to know more about how the prices are determined?"
+      message = <p>{"You're swapping " + props.inputToken.value + " for " + props.outputToken.value + ". Want to know more about how the prices are determined?"}</p>
       // message = "Oops, looks like this address doesn't have a enough " + props.inputToken.value + " to make this trade. Add more funds to make this swap."
       break;
     case 'error1':
-      message = "You can't swap a token for itself! 😂" 
+      message = <p>{"You can't swap a token for itself! 😂"}</p>
       // message = "Oops, looks like this address doesn't have a enough " + props.inputToken.value + " to make this trade. Add more funds to make this swap."
       break;
     default:
-      message = "Hi there! This site helps you swap ERC20 tokens. Looks like you aren't connected. Need help?"
+      message = <p>{"Hi there! This site helps you swap ERC20 tokens. Looks like you aren't connected. Need help?"}</p>
   }
   return (
     <section className="info border pa2">
-      <p>{message}</p>
+      {message}
       <p>↓</p>
     </section>
   )
