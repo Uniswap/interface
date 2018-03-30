4a3bd37da8a7fbee39097e01174ea4615c210e4d
Uciel <uciel777@Uciels-MBP.home>
2018-03-29 20:00:16 -0400

    render primitive line graph

--- src/components/Candlesticks.js
+++ src/components/Candlesticks.js
@@ -10,7 +10,6 @@ class Candlesticks extends Component {
     }
     this.visualizeData.bind(this)
   }
-
   // note, this url is being used for development
   // the actual url will be wherever the API is hosted 
   componentDidMount() {
--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -161,7 +161,7 @@ class Exchange extends Component {
         <div className="value border pa2">
           <input type="number" readOnly={true} value={(this.props.exchange.outputValue/10**18).toFixed(6)} placeholder="0"/>
           <SelectToken token={this.props.exchange.outputToken} onSelectToken={this.onSelectToken} type="output"/>
-           <p className="dropdown">{'<'}</p>
+          <p className="dropdown">{'<'}</p>
         </div>
       </section>
     )
