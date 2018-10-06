7a30c1c82715f0246c1d5b4bacdab9c040754fc4
Chi Kei Chan <chikeichan@gmail.com>
2018-10-06 16:55:56 -0700

    Add NavigationTabs

--- src/pages/Swap/index.js
+++ src/pages/Swap/index.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Header from '../../components/Header';
+import NavigationTabs from '../../components/NavigationTabs';
 
 import "./swap.scss";
 
 class Swap extends Component {
+  static propTypes = {
+    // Injected by React Router Dom
+      push: PropTypes.func.isRequired,
+      pathname: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <div className="swap">
         <Header />
         <div className="swap__content">
+          <NavigationTabs />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect()(Swap);
+export default withRouter(
+  connect(
+    (state, ownProps) => ({
+      push: ownProps.history.push,
+      pathname: ownProps.location.pathname,
+    }),
+  )(Swap)
+);
