f97155d6b349de727b04e92adb8915c76bfa6df3
Chi Kei Chan <chikeichan@gmail.com>
2018-10-06 14:24:17 -0700

    Add Sass and create pages directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './pages/App';
 
 import { Provider } from 'react-redux';
 import store from './store';
