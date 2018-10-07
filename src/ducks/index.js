72a9b7ebe17e9893f0be96e9b302bf5c0ae85f41
Chi Kei Chan <chikeichan@gmail.com>
2018-10-07 04:31:17 -0700

    Connect to Web3 and get current wallet address

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 // import global from './global-reducer';
-import web3Store from './web3';
+import web3 from './web3';
 import exchangeContracts from './exchange-contract';
 import tokenContracts from './token-contract';
 import exchange from './exchange';
 
 export default combineReducers({
-  web3Store,
+  web3,
   exchangeContracts,
   tokenContracts,
   exchange
