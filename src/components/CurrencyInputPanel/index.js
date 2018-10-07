4bf0776e8d756b07575f983cace84d8a1f2ac870
Chi Kei Chan <chikeichan@gmail.com>
2018-10-06 19:08:49 -0700

    Add Swap screen

--- src/components/CurrencyInputPanel/index.js
+++ src/components/CurrencyInputPanel/index.js
@@ -5,17 +5,28 @@ import PropTypes from 'prop-types';
 import './currency-panel.scss';
 
 class CurrencyInputPanel extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    extraText: PropTypes.string,
+  };
+
   render() {
+    const {
+      title,
+      description,
+      extraText,
+    } = this.props;
+
     return (
       <div className="currency-input-panel">
-        <div></div>
         <div className="currency-input-panel__container">
           <div className="currency-input-panel__label-row">
             <div className="currency-input-panel__label-container">
-              <span className="currency-input-panel__label">Input</span>
-              <span className="currency-input-panel__label-description">(estimated)</span>
+              <span className="currency-input-panel__label">{title}</span>
+              <span className="currency-input-panel__label-description">{description}</span>
             </div>
-            <span className="currency-input-panel__extra-text">Balance: 0.03141</span>
+            <span className="currency-input-panel__extra-text">{extraText}</span>
           </div>
           <div className="currency-input-panel__input-row">
             <input type="number" className="currency-input-panel__input" placeholder="0.0" />
@@ -25,7 +36,6 @@ class CurrencyInputPanel extends Component {
             </button>
           </div>
         </div>
-        <div></div>
       </div>
     )
   }
--- src/pages/Swap/index.js
+++ src/pages/Swap/index.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import Header from '../../components/Header';
 import NavigationTabs from '../../components/NavigationTabs';
 import CurrencyInputPanel from '../../components/CurrencyInputPanel';
+import OversizedPanel from '../../components/OversizedPanel';
+import ArrowDown from '../../assets/images/arrow-down-blue.svg';
 
 import "./swap.scss";
 
@@ -21,8 +23,32 @@ class Swap extends Component {
         <Header />
         <NavigationTabs className="swap__navigation" />
         <div className="swap__content">
-          <CurrencyInputPanel />
+          <CurrencyInputPanel
+            title="Input"
+            extraText="Balance: 0.03141"
+          />
+          <OversizedPanel>
+            <div className="swap__down-arrow-background">
+              <img className="swap__down-arrow" src={ArrowDown} />
+            </div>
+          </OversizedPanel>
+          <CurrencyInputPanel
+            title="Output"
+            description="(estimated)"
+            extraText="Balance: 0.0"
+          />
+          <OversizedPanel hideBottom>
+            <div className="swap__exchange-rate-wrapper">
+              <span className="swap__exchange-rate">Exchange Rate</span>
+              <span>1 ETH = 1283.878 BAT</span>
+            </div>
+          </OversizedPanel>
+          <div className="swap__summary-wrapper">
+            <div>You are selling <span className="swap__highlight-text">0.01 ETH</span></div>
+            <div>You will receive between <span className="swap__highlight-text">12.80</span> and <span className="swap__highlight-text">12.83 BAT</span></div>
+          </div>
         </div>
+        <button className="swap__cta-btn">Swap</button>
       </div>
     );
   }
