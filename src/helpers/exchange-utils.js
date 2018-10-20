ade904d22e93bf05a79c613f6adb4ff30ffe36f6
Kenny Tran <kenny8tran@gmail.com>
2018-10-19 22:53:14 -0700

    Approve / unlock functionality (#66)
    * Initial approve / unlock functionality
    
    * Refactor out getdecimal/getbalance. Add in approval limit
    
    * Change validateBalance to shouldValidateBalance
    
    * Add deepEqual and shouldComponentUpdate
    
    * Use web3 toHex util
    
    * Add pending icon and use

--- src/helpers/exchange-utils.js
+++ src/helpers/exchange-utils.js
@@ -1,5 +1,6 @@
 import {BigNumber as BN} from "bignumber.js";
 import promisify from "./web3-promisfy";
+import { getDecimals, getBalance } from './contract-utils';
 
 export const calculateExchangeRateFromInput = async opts => {
   const { inputCurrency, outputCurrency } = opts;
@@ -570,54 +571,3 @@ const ERC20_TO_ERC20 = {
     );
   },
 };
-
-function getDecimals({ address, drizzleCtx, contractStore }) {
-  return new Promise(async (resolve, reject) => {
-    if (address === 'ETH') {
-      resolve('18');
-      return;
-    }
-
-    const decimalsKey = drizzleCtx.contracts[address].methods.decimals.cacheCall();
-    const decimals = contractStore[address].decimals[decimalsKey];
-    resolve(decimals && decimals.value);
-  });
-}
-
-const BALANCE_KEY = {};
-
-function getBalance({ currency, address, drizzleCtx, contractStore }) {
-  return new Promise(async (resolve, reject) => {
-    if (currency === 'ETH') {
-      drizzleCtx.web3.eth.getBalance(address, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    } else {
-      const token = drizzleCtx.contracts[currency];
-      if (!token) {
-        return;
-      }
-
-      let balanceKey = BALANCE_KEY[address];
-
-      if (!balanceKey) {
-        balanceKey = token.methods.balanceOf.cacheCall(address);
-        BALANCE_KEY[address] = balanceKey;
-      }
-
-      const tokenStore = contractStore[currency];
-
-      if (!tokenStore) {
-        reject(new Error(`Cannot find ${currency} in contract store`));
-        return;
-      }
-
-      let balance = tokenStore.balanceOf[balanceKey];
-      resolve(balance && balance.value);
-    }
-  });
-}
