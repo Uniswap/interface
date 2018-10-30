6b6146b4784ce4b8d6b19aece32926bd9298d573
Hayden Adams <haydenzadams@gmail.com>
2018-10-30 05:18:03 +0100

    update addresses and various text changes

--- src/pages/Pool/AddLiquidity.js
+++ src/pages/Pool/AddLiquidity.js
@@ -547,10 +547,10 @@ class AddLiquidity extends Component {
             ? (
               <div className="pool__new-exchange-warning">
                 <div className="pool__new-exchange-warning-text">
-                  You are the first person to add liquidity🚰!
+                  🚰 You are the first person to add liquidity to this token!
                 </div>
                 <div className="pool__new-exchange-warning-text">
-                  {`A new exchange rate will be set based on your deposits. Please make sure that your ETH and ${label} deposits have the same fiat value.`}
+                  {`The initial exchange rate will be set based on your deposits. Please make sure that your ETH and ${label} deposits have the same fiat value.`}
                 </div>
               </div>
             )
