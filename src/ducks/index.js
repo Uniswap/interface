14f5bde3cdd59aba8a949432a70fe3ab5a3eaaef
Chi Kei Chan <chikeichan@gmail.com>
2018-11-01 16:10:20 +0100

    Restyle beta message

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import addresses from './addresses';
 import send from './send';
+import app from './app';
 import web3connect from './web3connect';
 
 export default combineReducers({
+  app,
   addresses,
   send,
   web3connect,
