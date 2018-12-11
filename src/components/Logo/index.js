8af58a74e078b258828afcff42b77a96e06ee8a5
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-12-11 10:30:05 +1000

    Update to cmt

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,7 +4,9 @@ import "./logo.scss";
 export default function Logo(props) {
   return (
     <div className="logo">
-      <span role="img" aria-label="logo">🦄</span>
+    <span role="img" aria-label="logo">
+     <img src="cmt.png" alt="CMT header logo"></img>
+     </span>
     </div>
   );
 }
