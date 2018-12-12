ddfe41aae86efad6a64c7d42afe34ba4c88890eb
Timothy McCallum <tpmccallum@users.noreply.github.com>
2018-12-12 13:53:30 +1000

    Removing duplicate line as per PR146
    I noticed a duplicate line of code, I removed it and created a PR which was accepted by the Uniswap frontend devs so I went ahead and made the appropriate changes here (on this fork) also.
    https://github.com/Uniswap/uniswap-frontend/pull/146

--- src/ducks/addresses.js
+++ src/ducks/addresses.js
@@ -151,10 +151,6 @@ export const addExchange = ({label, exchangeAddress, tokenAddress}) => (dispatch
     return;
   }
 
-  if (tokenAddresses.addresses.filter(([ symbol ]) => symbol === label).length) {
-    return;
-  }
-
   if (exchangeAddresses.fromToken[tokenAddresses]) {
     return;
   }
