55a9e0c8b3b1cdc9f0c868ca57c84c2591884471
kennethashley <ken.u.diggitt@gmail.com>
2019-01-18 19:36:37 -0500

    Add fragment

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
