0a4ff6431dab82ee3f06898df5c82f58a22e3267
kennethashley <ken.u.diggitt@gmail.com>
2019-01-26 18:34:56 -0500

    Add github repos

--- src/pages/App.js
+++ src/pages/App.js
@@ -106,8 +106,13 @@ class App extends Component {
             onCancel={this.handleCancel}
           >
             <p>
-              The Vexchange contracts and front-end are open source works that are licensed under GNU. This software is provided without any guarantees or liability, the code and licenses can be reviewed <a href="https://github.com/Monti/vexchange" traget="_blank">here</a>. The Vexchange site is simply an interface to an exchange running on the VeChain blockchain. We do not endorse any of the tokens and are not licensed to give investment advice. You acknowledge that you use this software at your own risk, both in terms of security and financial loss.
+              The Vexchange contracts and front-end are open source works that are licensed under GNU. This software is provided without any guarantees or liability, the code and licenses can be reviewed <a href="https://github.com/Monti/vexchange" target="_blank">here</a>. The Vexchange site is simply an interface to an exchange running on the VeChain blockchain. We do not endorse any of the tokens and are not licensed to give investment advice. You acknowledge that you use this software at your own risk, both in terms of security and financial loss.
             </p>
+            <div>
+              <a href="https://github.com/Monti/vexchange" target="_blank">Vexchange Github Repo</a>
+              <br />
+              <a href="https://github.com/Uniswap/uniswap-frontend" target="_blank">Uniswap Github Repo</a>
+            </div>
           </Modal>
         </div>
       </div>
--- src/pages/Tos/index.js
+++ src/pages/Tos/index.js
@@ -10,6 +10,11 @@ class Tos extends Component {
           <p>
             The Vexchange contracts and front-end are open source works that are licensed under GNU. This software is provided without any guarantees or liability, the code and licenses can be reviewed <a href="https://github.com/Monti/vexchange" traget="_blank">here</a>. The Vexchange site is simply an interface to an exchange running on the VeChain blockchain. We do not endorse any of the tokens and are not licensed to give investment advice. You acknowledge that you use this software at your own risk, both in terms of security and financial loss.
           </p>
+          <div>
+            <a href="https://github.com/Monti/vexchange" target="_blank">Vexchange Github Repo</a>
+            <br />
+            <a href="https://github.com/Uniswap/uniswap-frontend" target="_blank">Uniswap Github Repo</a>
+          </div>
         </div>
         <Link to="/">
           <button className="tos__cta-btn">Go back</button>
