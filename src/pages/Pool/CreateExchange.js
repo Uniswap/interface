4075e041978e65ccbc40d8d49b4bbcfae8568186
Noah Zinsmeister <noahwz@gmail.com>
2019-05-31 13:12:18 -0400

    fix ens name bug

--- src/pages/Pool/CreateExchange.js
+++ src/pages/Pool/CreateExchange.js
@@ -65,6 +65,8 @@ function CreateExchange({ history, location }) {
   })
   const [tokenAddressError, setTokenAddressError] = useState()
 
+  console.log(tokenAddress)
+
   const { name, symbol, decimals, exchangeAddress } = useTokenDetails(tokenAddress.address)
   const addTransaction = useTransactionAdder()
 
