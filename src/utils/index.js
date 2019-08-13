677537ca316b338d35babb27f431d9d59d1e6f00
Ian Lapham <ianlapham@gmail.com>
2019-08-12 20:37:32 -0400

    Updates to Token Modal  (#399)

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,7 @@ import EXCHANGE_ABI from '../constants/abis/exchange'
 import ERC20_ABI from '../constants/abis/erc20'
 import ERC20_BYTES32_ABI from '../constants/abis/erc20_bytes32'
 import { FACTORY_ADDRESSES } from '../constants'
+import { formatFixed } from '@uniswap/sdk'
 
 import UncheckedJsonRpcSigner from './signer'
 
@@ -178,8 +179,25 @@ export async function getEtherBalance(address, library) {
   if (!isAddress(address)) {
     throw Error(`Invalid 'address' parameter '${address}'`)
   }
-
   return library.getBalance(address)
+}
+
+export function formatEthBalance(balance) {
+  return amountFormatter(balance, 18, 6)
+}
+
+export function formatTokenBalance(balance, decimal) {
+  return !!(balance && Number.isInteger(decimal)) ? amountFormatter(balance, decimal, Math.min(4, decimal)) : 0
+}
+
+export function formatToUsd(price) {
+  const format = { decimalSeparator: '.', groupSeparator: ',', groupSize: 3 }
+  const usdPrice = formatFixed(price, {
+    decimalPlaces: 2,
+    dropTrailingZeros: false,
+    format
+  })
+  return usdPrice
 }
 
 // get the token balance of an address
