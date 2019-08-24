d18ee78ee7af51b72848912e2b6bace5115051a0
Ian Lapham <ianlapham@gmail.com>
2019-08-24 02:50:43 -0400

    Fix firefox bugs (#427)

--- src/components/CurrencyInputPanel/index.js
+++ src/components/CurrencyInputPanel/index.js
@@ -29,18 +29,18 @@ const GAS_MARGIN = ethers.utils.bigNumberify(1000)
 
 const SubCurrencySelect = styled.button`
   ${({ theme }) => theme.flexRowNoWrap}
-  background: ${({ theme }) => theme.zumthorBlue};
-
-  border: 1px solid ${({ theme }) => theme.royalBlue};
-  color: ${({ theme }) => theme.royalBlue};
+  padding: 4px 50px 4px 15px;
+  margin-right: -40px;
   line-height: 0;
   height: 2rem;
-  padding: 10px 50px 10px 15px;
-  margin-right: -40px;
+  align-items: center;
   border-radius: 2.5rem;
   outline: none;
   cursor: pointer;
   user-select: none;
+  background: ${({ theme }) => theme.zumthorBlue};
+  border: 1px solid ${({ theme }) => theme.royalBlue};
+  color: ${({ theme }) => theme.royalBlue};
 `
 
 const InputRow = styled.div`
