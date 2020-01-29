c48a7f26cccbf149397916466afea9e7480cc9c3
Noah Zinsmeister <noahwz@gmail.com>
2020-01-29 18:48:33 -0500

    improve network env stuff (#621)
    * improve network env setup

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -7,35 +7,43 @@ import { NetworkConnector } from './Network'
 import { FortmaticConnector } from './Fortmatic'
 
 const POLLING_INTERVAL = 10000
+const NETWORK_URL =
+  process.env.REACT_APP_IS_PRODUCTION_DEPLOY === 'true'
+    ? process.env.REACT_APP_NETWORK_URL_PROD
+    : process.env.REACT_APP_NETWORK_URL
 
 export const network = new NetworkConnector({
-  urls: { 1: process.env.REACT_APP_NETWORK_URL },
+  urls: { [Number(process.env.REACT_APP_CHAIN_ID)]: NETWORK_URL },
   pollingInterval: POLLING_INTERVAL * 3
 })
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1]
+  supportedChainIds: [Number(process.env.REACT_APP_CHAIN_ID)]
 })
 
+// mainnet only
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: process.env.REACT_APP_NETWORK_URL },
+  rpc: { 1: NETWORK_URL },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: false,
   pollingInterval: POLLING_INTERVAL
 })
 
+// mainnet only
 export const fortmatic = new FortmaticConnector({
   apiKey: process.env.REACT_APP_FORTMATIC_KEY,
   chainId: 1
 })
 
+// mainnet only
 export const portis = new PortisConnector({
   dAppId: process.env.REACT_APP_PORTIS_ID,
   networks: [1]
 })
 
+// mainnet only
 export const walletlink = new WalletLinkConnector({
-  url: process.env.REACT_APP_NETWORK_URL,
+  url: NETWORK_URL,
   appName: 'Uniswap',
   appLogoUrl:
     'https://mpng.pngfly.com/20181202/bex/kisspng-emoji-domain-unicorn-pin-badges-sticker-unicorn-tumblr-emoji-unicorn-iphoneemoji-5c046729264a77.5671679315437924251569.jpg'
