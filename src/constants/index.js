25b68747d4ffa3defc2f607e6d029fb550245c60
Noah Zinsmeister <noahwz@gmail.com>
2020-01-29 18:48:33 -0500

    improve network env stuff (#621)
    * improve network env setup

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ export const SUPPORTED_THEMES = {
   LIGHT: 'LIGHT'
 }
 
-export const SUPPORTED_WALLETS = {
+const MAINNET_WALLETS = {
   INJECTED: {
     connector: injected,
     name: 'Injected',
@@ -29,60 +29,69 @@ export const SUPPORTED_WALLETS = {
     description: 'Easy-to-use browser extension.',
     href: null,
     color: '#E8831D'
-  },
-  WALLET_CONNECT: {
-    connector: walletconnect,
-    name: 'WalletConnect',
-    iconName: 'walletConnectIcon.svg',
-    description: 'Connect to Trust Wallet, Rainbow Wallet and more...',
-    href: null,
-    color: '#4196FC'
-  },
-  WALLET_LINK: {
-    connector: walletlink,
-    name: 'Coinbase Wallet',
-    iconName: 'coinbaseWalletIcon.svg',
-    description: 'Use Coinbase Wallet app on mobile device',
-    href: null,
-    color: '#315CF5'
-  },
-  COINBASE_LINK: {
-    name: 'Open in Coinbase Wallet',
-    iconName: 'coinbaseWalletIcon.svg',
-    description: 'Open in Coinbase Wallet app.',
-    href: 'https://go.cb-w.com/mtUDhEZPy1',
-    color: '#315CF5',
-    mobile: true,
-    mobileOnly: true
-  },
-  TRUST_WALLET_LINK: {
-    name: 'Open in Trust Wallet',
-    iconName: 'trustWallet.png',
-    description: 'iOS and Android app.',
-    href: 'https://link.trustwallet.com/open_url?coin_id=60&url=https://uniswap.exchange/swap',
-    color: '#1C74CC',
-    mobile: true,
-    mobileOnly: true
-  },
-  FORTMATIC: {
-    connector: fortmatic,
-    name: 'Fortmatic',
-    iconName: 'fortmaticIcon.png',
-    description: 'Login using Fortmatic hosted wallet',
-    href: null,
-    color: '#6748FF',
-    mobile: true
-  },
-  Portis: {
-    connector: portis,
-    name: 'Portis',
-    iconName: 'portisIcon.png',
-    description: 'Login using Portis hosted wallet',
-    href: null,
-    color: '#4A6C9B',
-    mobile: true
   }
 }
+
+export const SUPPORTED_WALLETS =
+  process.env.REACT_APP_CHAIN_ID !== '1'
+    ? MAINNET_WALLETS
+    : {
+        ...MAINNET_WALLETS,
+        ...{
+          WALLET_CONNECT: {
+            connector: walletconnect,
+            name: 'WalletConnect',
+            iconName: 'walletConnectIcon.svg',
+            description: 'Connect to Trust Wallet, Rainbow Wallet and more...',
+            href: null,
+            color: '#4196FC'
+          },
+          WALLET_LINK: {
+            connector: walletlink,
+            name: 'Coinbase Wallet',
+            iconName: 'coinbaseWalletIcon.svg',
+            description: 'Use Coinbase Wallet app on mobile device',
+            href: null,
+            color: '#315CF5'
+          },
+          COINBASE_LINK: {
+            name: 'Open in Coinbase Wallet',
+            iconName: 'coinbaseWalletIcon.svg',
+            description: 'Open in Coinbase Wallet app.',
+            href: 'https://go.cb-w.com/mtUDhEZPy1',
+            color: '#315CF5',
+            mobile: true,
+            mobileOnly: true
+          },
+          TRUST_WALLET_LINK: {
+            name: 'Open in Trust Wallet',
+            iconName: 'trustWallet.png',
+            description: 'iOS and Android app.',
+            href: 'https://link.trustwallet.com/open_url?coin_id=60&url=https://uniswap.exchange/swap',
+            color: '#1C74CC',
+            mobile: true,
+            mobileOnly: true
+          },
+          FORTMATIC: {
+            connector: fortmatic,
+            name: 'Fortmatic',
+            iconName: 'fortmaticIcon.png',
+            description: 'Login using Fortmatic hosted wallet',
+            href: null,
+            color: '#6748FF',
+            mobile: true
+          },
+          Portis: {
+            connector: portis,
+            name: 'Portis',
+            iconName: 'portisIcon.png',
+            description: 'Login using Portis hosted wallet',
+            href: null,
+            color: '#4A6C9B',
+            mobile: true
+          }
+        }
+      }
 
 // list of tokens that lock fund on adding liquidity - used to disable button
 export const brokenTokens = [
