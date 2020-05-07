e08b87a667ab543600fe15f3bd1f4f0b38942209
Moody Salem <moodysalem@users.noreply.github.com>
2020-05-07 14:36:26 -0400

    remove unused functions, typescript (#725)
    * TypeScript in the utils directory, README fixes
    
    * Remove unused methods
    
    * Added unnecessary space
    
    * Some env variable cleanup
    
    * Bug in change
    
    * Another file to ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import ReactGA from 'react-ga'
 import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core'
 import { ethers } from 'ethers'
+import { Web3Provider } from 'ethers/providers'
 
 import { NetworkContextName } from './constants'
 import { isMobile } from 'react-device-detect'
@@ -19,19 +20,19 @@ import './i18n'
 
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
-function getLibrary(provider) {
+function getLibrary(provider): Web3Provider {
   const library = new ethers.providers.Web3Provider(provider)
   library.pollingInterval = 10000
   return library
 }
 
 if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize('UA-128182339-1')
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID)
   ReactGA.set({
-    customBrowserType: !isMobile ? 'desktop' : window.web3 || window.ethereum ? 'mobileWeb3' : 'mobileRegular'
+    customBrowserType: !isMobile ? 'desktop' : 'web3' in window || 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
   })
 } else {
-  ReactGA.initialize('test', { testMode: true })
+  ReactGA.initialize('test', { testMode: true, debug: true })
 }
 
 ReactGA.pageview(window.location.pathname + window.location.search)
@@ -57,9 +58,9 @@ function ContextProviders({ children }) {
 function Updaters() {
   return (
     <>
-      <ApplicationContextUpdater />
-      <TransactionContextUpdater />
-      <BalancesContextUpdater />
+      <ApplicationContextUpdater/>
+      <TransactionContextUpdater/>
+      <BalancesContextUpdater/>
     </>
   )
 }
@@ -68,11 +69,11 @@ ReactDOM.render(
   <Web3ReactProvider getLibrary={getLibrary}>
     <Web3ProviderNetwork getLibrary={getLibrary}>
       <ContextProviders>
-        <Updaters />
+        <Updaters/>
         <ThemeProvider>
           <>
-            <GlobalStyle />
-            <App />
+            <GlobalStyle/>
+            <App/>
           </>
         </ThemeProvider>
       </ContextProviders>
