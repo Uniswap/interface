2bbf420a7decdd1d3b742a25988d8c45ebe5d793
Noah Zinsmeister <noahwz@gmail.com>
2020-05-11 15:31:25 -0400

    add calculateSlippageAmount safety check and tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -107,6 +107,9 @@ export function calculateGasMargin(value: BigNumber): BigNumber {
 }
 
 export function calculateSlippageAmount(value: TokenAmount, slippage: number): [JSBI, JSBI] {
+  if (slippage < 0 || slippage > 10000) {
+    throw Error(`Unexpected slippage value: ${slippage}`)
+  }
   return [
     JSBI.divide(JSBI.multiply(value.raw, JSBI.BigInt(10000 - slippage)), JSBI.BigInt(10000)),
     JSBI.divide(JSBI.multiply(value.raw, JSBI.BigInt(10000 + slippage)), JSBI.BigInt(10000))
