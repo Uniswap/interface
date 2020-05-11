c028279b3934298645c0f5d3cc1722bde95822a8
Moody Salem <moody.salem@gmail.com>
2020-05-10 20:02:15 -0400

    Clean up method call

--- src/state/application/reducer.ts
+++ src/state/application/reducer.ts
@@ -17,37 +17,37 @@ interface ApplicationState {
   userAdvanced: boolean
 }
 
-export default createReducer(
-  {
-    blockNumber: {},
-    popupList: [],
-    walletModalOpen: false,
-    userAdvanced: false
-  } as ApplicationState,
-  builder =>
-    builder
-      .addCase(updateBlockNumber, (state, action) => {
-        const { networkId, blockNumber } = action.payload
-        state.blockNumber[networkId] = blockNumber
+const initialState: ApplicationState = {
+  blockNumber: {},
+  popupList: [],
+  walletModalOpen: false,
+  userAdvanced: false
+}
+
+export default createReducer(initialState, builder =>
+  builder
+    .addCase(updateBlockNumber, (state, action) => {
+      const { networkId, blockNumber } = action.payload
+      state.blockNumber[networkId] = blockNumber
+    })
+    .addCase(toggleUserAdvanced, state => {
+      state.userAdvanced = !state.userAdvanced
+    })
+    .addCase(toggleWalletModal, state => {
+      state.walletModalOpen = !state.walletModalOpen
+    })
+    .addCase(addPopup, (state, { payload: { content } }) => {
+      state.popupList.push({
+        key: nanoid(),
+        show: true,
+        content
       })
-      .addCase(toggleUserAdvanced, state => {
-        state.userAdvanced = !state.userAdvanced
+    })
+    .addCase(removePopup, (state, { payload: { key } }) => {
+      state.popupList.forEach(p => {
+        if (p.key === key) {
+          p.show = false
+        }
       })
-      .addCase(toggleWalletModal, state => {
-        state.walletModalOpen = !state.walletModalOpen
-      })
-      .addCase(addPopup, (state, { payload: { content } }) => {
-        state.popupList.push({
-          key: nanoid(),
-          show: true,
-          content
-        })
-      })
-      .addCase(removePopup, (state, { payload: { key } }) => {
-        state.popupList.forEach(p => {
-          if (p.key === key) {
-            p.show = false
-          }
-        })
-      })
+    })
 )
