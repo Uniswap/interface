741c116e651756059a3791458672cc841f3c1803
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-05-12 10:20:12 -0400

    Design tweaks

--- src/components/ExchangePage/index.tsx
+++ src/components/ExchangePage/index.tsx
@@ -1211,14 +1211,13 @@ function ExchangePage({ sendingInput = false, history, params }: ExchangePagePro
           </ButtonError>
         )}
         {v1TradeLinkIfBetter && (
-          <YellowCard style={{ marginTop: '12px', padding: '.75rem .5rem' }}>
+          <YellowCard style={{ marginTop: '12px', padding: '8px 4px' }}>
             <AutoColumn gap="sm" justify="center" style={{ alignItems: 'center', textAlign: 'center' }}>
-              <Text lineHeight="145.23%;" fontSize={16} fontWeight={400} color={theme.text1}>
-                There is a better price for this trade on <br />
+              <Text lineHeight="145.23%;" fontSize={14} fontWeight={400} color={theme.text1}>
+                There is a better price for this trade on
                 <Link href={v1TradeLinkIfBetter}>
-                  <b>Uniswap V1 ↗</b>
+                  <b> Uniswap V1 ↗</b>
                 </Link>
-                .
               </Text>
             </AutoColumn>
           </YellowCard>
