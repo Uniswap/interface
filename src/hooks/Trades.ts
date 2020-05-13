0733cdd63c3e1a06e4741b7d0d7c73f6389278bb
Moody Salem <moody.salem@gmail.com>
2020-05-13 19:57:12 -0400

    Fix trade routing to not throw

--- src/hooks/Trades.ts
+++ src/hooks/Trades.ts
@@ -1,47 +1,54 @@
 import { useMemo } from 'react'
-import { WETH, Token, TokenAmount, Trade, ChainId } from '@uniswap/sdk'
+import { WETH, Token, TokenAmount, Trade, ChainId, Pair } from '@uniswap/sdk'
 import { useWeb3React } from './index'
 import { usePair } from '../data/Reserves'
+
+const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin')
+const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD//C')
+function useAllCommonPairs(tokenA?: Token, tokenB?: Token): Pair[] {
+  const { chainId } = useWeb3React()
+
+  // check for direct pair between tokens
+  const pairBetween = usePair(tokenA, tokenB)
+
+  // get token<->WETH pairs
+  const aToETH = usePair(tokenA, WETH[chainId])
+  const bToETH = usePair(tokenB, WETH[chainId])
+
+  // get token<->DAI pairs
+  const aToDAI = usePair(tokenA, chainId === ChainId.MAINNET ? DAI : null)
+  const bToDAI = usePair(tokenB, chainId === ChainId.MAINNET ? DAI : null)
+
+  // get token<->USDC pairs
+  const aToUSDC = usePair(tokenA, chainId === ChainId.MAINNET ? USDC : null)
+  const bToUSDC = usePair(tokenB, chainId === ChainId.MAINNET ? USDC : null)
+
+  return useMemo(() => [pairBetween, aToETH, bToETH, aToDAI, bToDAI, aToUSDC, bToUSDC].filter(p => !!p), [
+    pairBetween,
+    aToETH,
+    bToETH,
+    aToDAI,
+    bToDAI,
+    aToUSDC,
+    bToUSDC
+  ])
+}
 
 /**
  * Returns the best trade for the exact amount of tokens in to the given token out
  */
-const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin')
-const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD//C')
 export function useTradeExactIn(amountIn?: TokenAmount, tokenOut?: Token): Trade | null {
   const inputToken = amountIn?.token
   const outputToken = tokenOut
 
-  const { chainId } = useWeb3React()
-
-  // check for direct pair between tokens
-  const pairBetween = usePair(inputToken, outputToken)
-
-  // get token<->WETH pairs
-  const aToETH = usePair(inputToken, WETH[chainId])
-  const bToETH = usePair(outputToken, WETH[chainId])
-
-  // get token<->DAI pairs
-  const aToDAI = usePair(inputToken, chainId === ChainId.MAINNET ? DAI : null)
-  const bToDAI = usePair(outputToken, chainId === ChainId.MAINNET ? DAI : null)
-
-  // get token<->USDC pairs
-  const aToUSDC = usePair(inputToken, chainId === ChainId.MAINNET ? USDC : null)
-  const bToUSDC = usePair(outputToken, chainId === ChainId.MAINNET ? USDC : null)
+  const allowedPairs = useAllCommonPairs(inputToken, outputToken)
 
   return useMemo(() => {
-    const allPairs = [pairBetween, aToETH, bToETH, aToDAI, bToDAI, aToUSDC, bToUSDC].filter(p => !!p)
-
-    if (amountIn && tokenOut && allPairs.length > 0) {
-      try {
-        // TODO(moodysalem): remove when the insufficient reserves/input errors do not throw exceptions
-        return Trade.bestTradeExactIn(allPairs, amountIn, tokenOut)[0] ?? null
-      } catch (error) {
-        return null
-      }
+    if (amountIn && tokenOut && allowedPairs.length > 0) {
+      return Trade.bestTradeExactIn(allowedPairs, amountIn, tokenOut)[0] ?? null
     }
     return null
-  }, [pairBetween, aToETH, bToETH, aToDAI, bToDAI, aToUSDC, bToUSDC, amountIn, tokenOut])
+  }, [allowedPairs, amountIn, tokenOut])
 }
 
 /**
@@ -51,34 +58,12 @@ export function useTradeExactOut(tokenIn?: Token, amountOut?: TokenAmount): Trad
   const inputToken = tokenIn
   const outputToken = amountOut?.token
 
-  const { chainId } = useWeb3React()
-
-  // check for direct pair between tokens
-  const pairBetween = usePair(amountOut?.token, tokenIn)
-
-  // get token<->WETH pairs
-  const aToETH = usePair(inputToken, WETH[chainId])
-  const bToETH = usePair(outputToken, WETH[chainId])
-
-  // get token<->DAI pairs
-  const aToDAI = usePair(inputToken, chainId === ChainId.MAINNET ? DAI : null)
-  const bToDAI = usePair(outputToken, chainId === ChainId.MAINNET ? DAI : null)
-
-  // get token<->USDC pairs
-  const aToUSDC = usePair(inputToken, chainId === ChainId.MAINNET ? USDC : null)
-  const bToUSDC = usePair(outputToken, chainId === ChainId.MAINNET ? USDC : null)
+  const allowedPairs = useAllCommonPairs(inputToken, outputToken)
 
   return useMemo(() => {
-    const allPairs = [pairBetween, aToETH, bToETH, aToDAI, bToDAI, aToUSDC, bToUSDC].filter(p => !!p)
-
-    if (tokenIn && amountOut && allPairs.length > 0) {
-      try {
-        // TODO(moodysalem): remove when the insufficient reserves/input errors do not throw exceptions
-        return Trade.bestTradeExactOut(allPairs, tokenIn, amountOut)[0] ?? null
-      } catch (error) {
-        return null
-      }
+    if (tokenIn && amountOut && allowedPairs.length > 0) {
+      return Trade.bestTradeExactOut(allowedPairs, tokenIn, amountOut)[0] ?? null
     }
     return null
-  }, [pairBetween, aToETH, bToETH, aToDAI, bToDAI, aToUSDC, bToUSDC, tokenIn, amountOut])
+  }, [allowedPairs, tokenIn, amountOut])
 }
