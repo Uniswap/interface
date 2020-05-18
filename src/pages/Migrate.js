87118c54216d91c934ecc94e64a479cd421b1287
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-05-18 09:40:00 -0400

    Fix links

--- src/pages/Migrate.js
+++ src/pages/Migrate.js
@@ -199,7 +199,7 @@ function Migrate() {
           <TextBlock padding={'1rem 0 0 0'} style={{ lineHeight: '140%' }}>
             For each pool, approve the migration helper and click migrate liquidity. Your liquidity will be withdrawn
             from Uniswap V1 and deposited into Uniswap V2. Once you've completed the migration you can view your
-            liquidity on the new <Link href="v2.uniswap.exchange">Uniswap V2 interface</Link>.
+            liquidity on the new <Link href="https://uniswap.exchange">Uniswap V2 interface</Link>.
           </TextBlock>
           <TextBlock padding={'1rem 0 0 0'} style={{ lineHeight: '140%' }}>
             If your liquidity does not appear below automatically, you may need to find it by pasting the token address
