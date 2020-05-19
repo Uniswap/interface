b2d22f5f22882121de0d38d0565b87502b76c0ca
Noah Zinsmeister <noahwz@gmail.com>
2020-05-19 12:40:54 -0400

    default timeout from 15 -> 20 minutes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -106,8 +106,8 @@ export const NetworkContextName = 'NETWORK'
 
 // default allowed slippage, in bips
 export const INITIAL_ALLOWED_SLIPPAGE = 50
-// 15 minutes, denominated in seconds
-export const DEFAULT_DEADLINE_FROM_NOW = 60 * 15
+// 20 minutes, denominated in seconds
+export const DEFAULT_DEADLINE_FROM_NOW = 60 * 20
 
 // one basis point
 export const ONE_BIPS = new Percent(JSBI.BigInt(1), JSBI.BigInt(10000))
