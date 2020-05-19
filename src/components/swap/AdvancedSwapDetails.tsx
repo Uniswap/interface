9e2c579eb3ac0249826b3e67f759beb90abc364e
Noah Zinsmeister <noahwz@gmail.com>
2020-05-19 19:42:14 -0400

    add border, only show if >2 tokens in the path

--- src/components/swap/AdvancedSwapDetails.tsx
+++ src/components/swap/AdvancedSwapDetails.tsx
@@ -91,27 +91,32 @@ export function AdvancedSwapDetails({ trade, onDismiss, ...slippageTabProps }: A
       </RowFixed>
       <SlippageTabs {...slippageTabProps} />
 
-      <AutoColumn style={{ padding: '0 20px' }}>
-        <RowFixed>
-          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-            Route
-          </TYPE.black>
-          <QuestionHelper text="Routing through these tokens resulted in the best price for your trade." />
-        </RowFixed>
-        <Flex width="100%" justifyContent="center" flexWrap="wrap">
-          {trade?.route?.path.map((token, i) => (
-            <Flex mr="1rem" my="0.5rem" alignItems="center" key={token.address}>
-              <TokenLogo address={token.address} size="1.5rem" />
-              <TYPE.black fontSize={14} color={theme.text1} ml="0.5rem">
-                {token.symbol}
-              </TYPE.black>
-              {i !== trade?.route?.path?.length - 1 && (
-                <ChevronRight color={theme.text2} style={{ marginLeft: '1rem' }} />
-              )}
+      {trade.route.path.length > 2 && (
+        <AutoColumn style={{ padding: '0 20px' }}>
+          <RowFixed>
+            <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
+              Route
+            </TYPE.black>
+            <QuestionHelper text="Routing through these tokens resulted in the best price for your trade." />
+          </RowFixed>
+          <Flex my="0.5rem" width="100%" justifyContent="center" flexWrap="wrap">
+            <Flex px="1rem" py="0.5rem" style={{ border: `1px solid ${theme.bg3}`, borderRadius: '1rem' }}>
+              {trade?.route?.path.map((token, i) => {
+                const lastItem = i === trade.route.path.length - 1
+                return (
+                  <Flex mr={lastItem ? 0 : '1rem'} my="0.5rem" alignItems="center" key={token.address}>
+                    <TokenLogo address={token.address} size="1.5rem" />
+                    <TYPE.black fontSize={14} color={theme.text1} ml="0.5rem">
+                      {token.symbol}
+                    </TYPE.black>
+                    {!lastItem && <ChevronRight color={theme.text2} style={{ marginLeft: '1rem' }} />}
+                  </Flex>
+                )
+              })}
             </Flex>
-          ))}
-        </Flex>
-      </AutoColumn>
+          </Flex>
+        </AutoColumn>
+      )}
     </AutoColumn>
   )
 }
