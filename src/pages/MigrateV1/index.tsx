ce819aa2d6aa6e07c1ca116312657d742d9a8186
Moody Salem <moody.salem@gmail.com>
2020-05-31 15:58:25 -0400

    links and page

--- src/pages/MigrateV1/index.tsx
+++ src/pages/MigrateV1/index.tsx
@@ -1,40 +1,54 @@
-import { JSBI, Token } from '@uniswap/sdk'
+import { JSBI, Token, TokenAmount } from '@uniswap/sdk'
 import React, { useMemo } from 'react'
 import { RouteComponentProps } from 'react-router'
-import { useAllV1ExchangeAddresses } from '../../data/V1'
+import { useTotalSupply } from '../../data/TotalSupply'
+import { useAllTokenV1ExchangeAddresses } from '../../data/V1'
 import { useActiveWeb3React } from '../../hooks'
 import { useTokenBalances } from '../../state/wallet/hooks'
+import { TYPE } from '../../theme'
 
-const PLACEHOLDER_ACCOUNT = (
-  <div>
-    <h1>You must connect a wallet to use this tool.</h1>
-  </div>
-)
+function V1PairMigration({ liquidityTokenAmount }: { liquidityTokenAmount: TokenAmount }) {
+  const totalSupply = useTotalSupply(liquidityTokenAmount.token)
+
+  return (
+    <div>
+      Exchange: {liquidityTokenAmount.token.address}; totalSupply: {totalSupply?.toSignificant(6)}; your amount:
+      {liquidityTokenAmount.toSignificant(6)}
+    </div>
+  )
+}
 
 /**
  * Page component for migrating liquidity from V1
  */
 export default function MigrateV1({}: RouteComponentProps) {
   const { account, chainId } = useActiveWeb3React()
-  const v1ExchangeAddresses = useAllV1ExchangeAddresses()
+  const userTokenV1ExchangeAddresses = useAllTokenV1ExchangeAddresses()
 
   const v1ExchangeTokens: Token[] = useMemo(() => {
-    return v1ExchangeAddresses.map(exchangeAddress => new Token(chainId, exchangeAddress, 18))
-  }, [chainId, v1ExchangeAddresses])
+    return userTokenV1ExchangeAddresses.map(exchangeAddress => new Token(chainId, exchangeAddress, 18))
+  }, [chainId, userTokenV1ExchangeAddresses])
 
   const tokenBalances = useTokenBalances(account, v1ExchangeTokens)
 
-  const unmigratedExchangeAddresses = useMemo(
+  const unmigratedLiquidity = useMemo(
     () =>
-      Object.keys(tokenBalances).filter(tokenAddress =>
-        tokenBalances[tokenAddress] ? JSBI.greaterThan(tokenBalances[tokenAddress]?.raw, JSBI.BigInt(0)) : false
-      ),
+      Object.keys(tokenBalances)
+        .filter(tokenAddress =>
+          tokenBalances[tokenAddress] ? JSBI.greaterThan(tokenBalances[tokenAddress]?.raw, JSBI.BigInt(0)) : false
+        )
+        .map(ta => tokenBalances[ta]),
     [tokenBalances]
   )
 
-  if (!account) {
-    return PLACEHOLDER_ACCOUNT
-  }
-
-  return <div>{unmigratedExchangeAddresses?.join('\n')}</div>
+  return (
+    <div>
+      <div style={{ maxWidth: 600, padding: 15 }}>
+        <TYPE.largeHeader>Uniswap V2 Migration</TYPE.largeHeader>
+      </div>
+      {unmigratedLiquidity?.map(amount => (
+        <V1PairMigration key={amount.token.address} liquidityTokenAmount={amount} />
+      ))}
+    </div>
+  )
 }
