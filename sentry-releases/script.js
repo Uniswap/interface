317c2d8088f838899ae697200e124fc258b3ed4b
Corey Caplan <coreycaplan3@gmail.com>
2020-05-31 20:44:05 -0400

    Fixed up script further

--- sentry-releases/script.js
+++ sentry-releases/script.js
@@ -10,6 +10,21 @@ async function main() {
   const version = `v1.${releases.length}`
   releases.push(version)
 
+  await new Promise((success, failure) => {
+    const command1 = `sentry-cli releases --project dmm-dao-web-app new ${version}`
+    const command2 = `sentry-cli releases --project dmm-dao-web-app files ${version} upload-sourcemaps ./build`
+    const command3 = `sentry-cli releases --project dmm-dao-web-app finalize ${version}`
+    const commands = `${command1}; ${command2}; ${command3}`
+    exec(commands, (error, stdout) => {
+      console.log('sentry-cli output: ', stdout);
+      if (!!error) {
+        failure(error.message)
+      } else {
+        success()
+      }
+    })
+  });
+
   console.log('Tagging most-recent commit to be version ', version);
   await new Promise((success, failure) => {
     exec(`git tag -a ${version} -m "Releasing production version ${version}"`, (error, stdout) => {
@@ -22,13 +37,22 @@ async function main() {
     })
   });
 
+  console.log('Pushing tags to repository');
   await new Promise((success, failure) => {
-    const command1 = `sentry-cli releases --project dmm-dao-web-app new ${version}`
-    const command2 = `sentry-cli releases --project dmm-dao-web-app files ${version} upload-sourcemaps ./build`
-    const command3 = `sentry-cli releases --project dmm-dao-web-app finalize ${version}`
-    const commands = `${command1}; ${command2}; ${command3}`
-    exec(commands, (error, stdout) => {
-      console.log('sentry-cli output: ', stdout);
+    exec('URL=`git remote get-url origin`; git push --tags URL', (error, stdout) => {
+      console.log('git push tag output: ', stdout);
+      if (!!error) {
+        failure(error.message)
+      } else {
+        success()
+      }
+    })
+  });
+
+  console.log('Deploying to firebase');
+  await new Promise((success, failure) => {
+    exec('firebase deploy', (error, stdout) => {
+      console.log('firebase deploy output: ', stdout);
       if (!!error) {
         failure(error.message)
       } else {
