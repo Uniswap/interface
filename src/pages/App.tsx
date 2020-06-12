1227e2e9c667ce2855c5671f25badd12412b5445
Moody Salem <moody.salem@gmail.com>
2020-06-12 13:42:35 -0400

    fix(background image): cuts off at the bottom when scrolling

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -49,25 +49,6 @@ const BodyWrapper = styled.div`
   z-index: 1;
 `
 
-const BackgroundGradient = styled.div`
-  width: 100%;
-  height: 170vh;
-  background: ${({ theme }) => `radial-gradient(50% 50% at 50% 50%, ${theme.primary1} 0%, ${theme.bg1} 100%)`};
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  opacity: 0.1;
-  z-index: -1;
-
-  transform: translateY(-70vh);
-
-  @media (max-width: 960px) {
-    height: 300px;
-    width: 100%;
-    transform: translateY(-150px);
-  }
-`
-
 const Marginer = styled.div`
   margin-top: 5rem;
 `
@@ -109,7 +90,6 @@ export default function App() {
             <Marginer />
             <Footer />
           </BodyWrapper>
-          <BackgroundGradient />
         </AppWrapper>
       </Router>
     </Suspense>
