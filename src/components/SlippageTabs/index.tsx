889e990bc8010f76870175e97b99a88979a93a62
ianlapham <ianlapham@gmail.com>
2020-06-12 15:18:07 -0400

    update language and bg

--- src/components/SlippageTabs/index.tsx
+++ src/components/SlippageTabs/index.tsx
@@ -146,7 +146,7 @@ export default function SlippageTabs({ rawSlippage, setRawSlippage, deadline, se
       <AutoColumn gap="sm">
         <RowFixed>
           <TYPE.black fontWeight={400} fontSize={14} color={theme.text2}>
-            Set slippage tolerance
+            Slippage tolerance
           </TYPE.black>
           <QuestionHelper text="Your transaction will revert if the price changes unfavorably by more than this percentage." />
         </RowFixed>
