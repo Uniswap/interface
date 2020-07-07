3eac261d42a5cc1d2c9f1386459d621bbe66d162
Antonio Savage <afmsavage@gmail.com>
2020-07-06 21:56:38 -0500

    Updated dropdown menu index.tsx
    The discord invite link in the dropdown menu on uniswap was dead.  I replaced it with the working on off of the docs site.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -121,7 +121,7 @@ export default function Menu() {
             <Code size={14} />
             Code
           </MenuItem>
-          <MenuItem id="link" href="https://discord.gg/vXCdddD">
+          <MenuItem id="link" href="https://discord.gg/EwFs3Pp">
             <MessageCircle size={14} />
             Discord
           </MenuItem>
