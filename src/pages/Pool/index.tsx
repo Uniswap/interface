624744086b021babf5d41cfc42ce941b27b2f857
Moody Salem <moody.salem@gmail.com>
2020-07-09 11:57:08 -0400

    deleting some code first

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useContext, useCallback } from 'react'
-import styled, { ThemeContext } from 'styled-components'
+import React, { useContext } from 'react'
+import { ThemeContext } from 'styled-components'
 import { JSBI } from '@uniswap/sdk'
 import { RouteComponentProps } from 'react-router-dom'
 import { SwapPoolTabs } from '../../components/NavigationTabs'
 
 import Question from '../../components/QuestionHelper'
-import PairSearchModal from '../../components/SearchModal/PairSearchModal'
 import PositionCard from '../../components/PositionCard'
 import { useUserHasLiquidityInAllTokens } from '../../data/V1'
 import { useTokenBalancesWithLoadingIndicator } from '../../state/wallet/hooks'
@@ -14,7 +13,7 @@ import { Text } from 'rebass'
 import { LightCard } from '../../components/Card'
 import { RowBetween } from '../../components/Row'
 import { ButtonPrimary, ButtonSecondary } from '../../components/Button'
-import { AutoColumn, ColumnCenter } from '../../components/Column'
+import { AutoColumn } from '../../components/Column'
 
 import { useActiveWeb3React } from '../../hooks'
 import { usePairs } from '../../data/Reserves'
@@ -22,21 +21,9 @@ import { useAllDummyPairs } from '../../state/user/hooks'
 import AppBody from '../AppBody'
 import { Dots } from '../../components/swap/styleds'
 
-const Positions = styled.div`
-  position: relative;
-  width: 100%;
-`
-
-const FixedBottom = styled.div`
-  position: absolute;
-  bottom: -80px;
-  width: 100%;
-`
-
 export default function Pool({ history }: RouteComponentProps) {
   const theme = useContext(ThemeContext)
   const { account } = useActiveWeb3React()
-  const [showPoolSearch, setShowPoolSearch] = useState(false)
 
   // fetch the user's balances of all tracked V2 LP tokens
   const V2DummyPairs = useAllDummyPairs()
@@ -65,28 +52,18 @@ export default function Pool({ history }: RouteComponentProps) {
 
   const hasV1Liquidity = useUserHasLiquidityInAllTokens()
 
-  const handleSearchDismiss = useCallback(() => {
-    setShowPoolSearch(false)
-  }, [setShowPoolSearch])
-
   return (
-    <AppBody>
-      <SwapPoolTabs active={'pool'} />
-      <AutoColumn gap="lg" justify="center">
-        <ButtonPrimary
-          id="join-pool-button"
-          padding="16px"
-          onClick={() => {
-            setShowPoolSearch(true)
-          }}
-        >
-          <Text fontWeight={500} fontSize={20}>
-            Join {allV2PairsWithLiquidity?.length > 0 ? 'another' : 'a'} pool
-          </Text>
-        </ButtonPrimary>
+    <>
+      <AppBody>
+        <SwapPoolTabs active={'pool'} />
+        <AutoColumn gap="lg" justify="center">
+          <ButtonPrimary id="join-pool-button" style={{ padding: 16 }}>
+            <Text fontWeight={500} fontSize={20}>
+              Add Liquidity
+            </Text>
+          </ButtonPrimary>
 
-        <Positions>
-          <AutoColumn gap="12px">
+          <AutoColumn gap="12px" style={{ width: '100%' }}>
             <RowBetween padding={'0 8px'}>
               <Text color={theme.text1} fontWeight={500}>
                 Your Liquidity
@@ -125,16 +102,19 @@ export default function Pool({ history }: RouteComponentProps) {
               </Text>
             </div>
           </AutoColumn>
-          <FixedBottom>
-            <ColumnCenter>
-              <ButtonSecondary width="136px" padding="8px" borderRadius="10px" onClick={() => history.push('/create')}>
-                + Create Pool
-              </ButtonSecondary>
-            </ColumnCenter>
-          </FixedBottom>
-        </Positions>
-        <PairSearchModal isOpen={showPoolSearch} onDismiss={handleSearchDismiss} />
-      </AutoColumn>
-    </AppBody>
+        </AutoColumn>
+      </AppBody>
+
+      <div style={{ display: 'flex', alignItems: 'center', marginTop: '1.5rem' }}>
+        <ButtonSecondary
+          style={{ width: 'initial' }}
+          padding="8px"
+          borderRadius="10px"
+          onClick={() => history.push('/migrate/v1')}
+        >
+          Migrate V1 Liquidity
+        </ButtonSecondary>
+      </div>
+    </>
   )
 }
