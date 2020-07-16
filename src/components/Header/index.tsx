bb14fbccbb9c03f88aca67478c4105c89d214e77
pokim <1325147857@qq.com>
2020-07-16 19:00:23 +0800

    disable VersionSwitch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,7 +19,7 @@ import Menu from '../Menu'
 
 import Row, { RowBetween } from '../Row'
 import Web3Status from '../Web3Status'
-import VersionSwitch from './VersionSwitch'
+// import VersionSwitch from './VersionSwitch'
 
 const HeaderFrame = styled.div`
   display: flex;
@@ -168,7 +168,7 @@ export default function Header() {
             </AccountElement>
           </HeaderElement>
           <HeaderElementWrap>
-            <VersionSwitch />
+            {/*<VersionSwitch />*/}
             <Settings />
             <Menu />
           </HeaderElementWrap>
