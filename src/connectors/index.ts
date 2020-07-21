32d300009e85200b37d970bcbf322a5e30db6ec6
Moody Salem <moody.salem@gmail.com>
2020-07-21 15:57:09 -0500

    just bump the polling interval, aiming to have same # of blockNumber requests as calls or less

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -6,7 +6,7 @@ import { PortisConnector } from '@web3-react/portis-connector'
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
 
-const POLLING_INTERVAL = 10000
+const POLLING_INTERVAL = 12500
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
