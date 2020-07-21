ca3811774935c929ba21dfd682b89839331db0eb
Moody Salem <moody.salem@gmail.com>
2020-07-21 11:07:10 -0500

    chore(infura): rotate keys (complete)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -73,9 +73,9 @@ Cypress.Commands.overwrite('visit', (original, url, options) => {
     ...options,
     onBeforeLoad(win) {
       options && options.onBeforeLoad && options.onBeforeLoad(win)
-      const provider = new JsonRpcProvider('https://rinkeby.infura.io/v3/acb7e55995d04c49bfb52b7141599467', 4)
+      const provider = new JsonRpcProvider('https://rinkeby.infura.io/v3/4bf032f2d38a4ed6bb975b80d6340847', 4)
       const signer = new Wallet(PRIVATE_KEY_TEST_NEVER_USE, provider)
       win.ethereum = new CustomizedBridge(signer, provider)
-    },
+    }
   })
 })
