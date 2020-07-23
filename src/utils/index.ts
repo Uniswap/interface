498d110eabd54d8a01aa512647877e187aa3aa98
Moody Salem <moody.salem@gmail.com>
2020-07-23 12:34:19 -0500

    fix the custom added/default tokens

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,6 +6,7 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { abi as IUniswapV2Router02ABI } from '@uniswap/v2-periphery/build/IUniswapV2Router02.json'
 import { ROUTER_ADDRESS } from '../constants'
 import { ChainId, JSBI, Percent, Token, CurrencyAmount, Currency, ETHER } from '@uniswap/sdk'
+import { TokenAddressMap } from '../state/lists/hooks'
 
 // returns the checksummed address if the address is valid, otherwise returns false
 export function isAddress(value: any): string | false {
@@ -98,12 +99,7 @@ export function escapeRegExp(string: string): string {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
 }
 
-export function isDefaultToken(currency?: Currency): boolean {
+export function isDefaultToken(defaultTokens: TokenAddressMap, currency?: Currency): boolean {
   if (currency === ETHER) return true
-  return true
-}
-
-export function isCustomAddedToken(allTokens: { [address: string]: Token }, currency?: Currency): boolean {
-  // const isDefault = isDefaultToken(currency)
-  return false
+  return Boolean(currency instanceof Token && defaultTokens[currency.chainId]?.[currency.address])
 }
