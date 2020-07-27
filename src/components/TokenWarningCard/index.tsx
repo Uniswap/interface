1fdfe8c18630fad3f16ad5599f3ded682f9582c7
ianlapham <ianlapham@gmail.com>
2020-07-27 14:59:29 -0400

    remove useless styling

--- src/components/TokenWarningCard/index.tsx
+++ src/components/TokenWarningCard/index.tsx
@@ -127,9 +127,7 @@ export function TokenWarningCards({
             width={'140px'}
             padding="0.5rem 1rem"
             style={{
-              borderRadius: '10px',
-              boxShadow:
-                '0px 0px 1px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04), 0px 24px 32px rgba(0, 0, 0, 0.04);'
+              borderRadius: '10px'
             }}
             onClick={() => {
               dismissToken0 && dismissToken0()
