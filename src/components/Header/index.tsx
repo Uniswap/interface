89e0b16d720e49646a7a25ecb9adfaac360f269e
artall64@gmail.com <artall64@gmail.com>
2020-08-04 13:40:46 +0300

    Droped migration Button and Routing, Commened out V2/V1 switch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -168,7 +168,7 @@ export default function Header() {
             </AccountElement>
           </HeaderElement>
           <HeaderElementWrap>
-            <VersionSwitch />
+            {/*<VersionSwitch />*/}
             <Settings />
             <Menu />
           </HeaderElementWrap>
