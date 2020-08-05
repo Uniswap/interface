d0d904112afe12d085d1cab73626772a08459f45
Kirill <krboktv@gmail.com>
2020-08-05 23:00:54 +0300

    remove add a send

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -308,11 +308,6 @@ export default function Swap() {
                       color={currencies[Field.INPUT] && currencies[Field.OUTPUT] ? theme.primary1 : theme.text2}
                     />
                   </ArrowWrapper>
-                  {recipient === null && !showWrap ? (
-                    <LinkStyledButton id="add-recipient-button" onClick={() => onChangeRecipient('')}>
-                      + Add a send (optional)
-                    </LinkStyledButton>
-                  ) : null}
                 </AutoRow>
               </AutoColumn>
             </CursorPointer>
