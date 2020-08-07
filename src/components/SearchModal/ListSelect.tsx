02a6bbcd22c3a49fd0214c026a4ba73951614946
Moody Salem <moody.salem@gmail.com>
2020-08-06 20:25:34 -0500

    add/remove/enter key

--- src/components/SearchModal/ListSelect.tsx
+++ src/components/SearchModal/ListSelect.tsx
@@ -1,12 +1,16 @@
-import React, { useCallback, useContext, useState } from 'react'
+import React, { useCallback, useContext, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Text } from 'rebass'
 import { ThemeContext } from 'styled-components'
+import { DEFAULT_TOKEN_LIST_URL } from '../../constants'
 import { AppDispatch, AppState } from '../../state'
-import { addList } from '../../state/lists/actions'
-import { CloseIcon, LinkStyledButton } from '../../theme'
+import { addList, removeList, selectList } from '../../state/lists/actions'
+import { useSelectedListUrl } from '../../state/lists/hooks'
+import { CloseIcon, LinkStyledButton, TYPE } from '../../theme'
+import { getTokenList } from '../../utils/getTokenList'
 import listVersionLabel from '../../utils/listVersionLabel'
-import { ButtonPrimary } from '../Button'
+import uriToHttp from '../../utils/uriToHttp'
+import { ButtonPrimary, ButtonSecondary } from '../Button'
 import Card from '../Card'
 import Column from '../Column'
 import QuestionHelper from '../QuestionHelper'
@@ -16,16 +20,41 @@ import { PaddedColumn, SearchInput, Separator } from './styleds'
 export function ListSelect({ onDismiss, onBack }: { onDismiss: () => void; onBack: () => void }) {
   const theme = useContext(ThemeContext)
   const [listUrlInput, setListUrlInput] = useState<string>('')
+  const [addError, setAddError] = useState<string | null>(null)
   const handleInput = useCallback(e => {
     setListUrlInput(e.target.value)
   }, [])
   const dispatch = useDispatch<AppDispatch>()
 
   const handleAddList = useCallback(() => {
-    dispatch(addList(listUrlInput))
+    setAddError(null)
+    getTokenList(listUrlInput)
+      .then(() => {
+        dispatch(addList(listUrlInput))
+      })
+      .then(() => {
+        setListUrlInput('')
+      })
+      .catch(error => {
+        setAddError(error.message)
+      })
   }, [dispatch, listUrlInput])
 
+  const selectedListUrl = useSelectedListUrl()
   const lists = useSelector<AppState, AppState['lists']['byUrl']>(state => state.lists.byUrl)
+
+  const validUrl: boolean = useMemo(() => {
+    return uriToHttp(listUrlInput).length > 0
+  }, [listUrlInput])
+
+  const handleEnterKey = useCallback(
+    e => {
+      if (e.key === 'Enter') {
+        handleAddList()
+      }
+    },
+    [handleAddList]
+  )
 
   return (
     <Column style={{ width: '100%', flex: '1 1' }}>
@@ -44,12 +73,13 @@ export function ListSelect({ onDismiss, onBack }: { onDismiss: () => void; onBac
             placeholder="ipfs:// or https://"
             value={listUrlInput}
             onChange={handleInput}
-            onKeyDown={handleAddList}
+            onKeyDown={handleEnterKey}
           />
-          <ButtonPrimary style={{ maxWidth: '4rem', marginLeft: '1rem' }} onClick={handleAddList}>
+          <ButtonPrimary style={{ maxWidth: '4rem', marginLeft: '1rem' }} onClick={handleAddList} disabled={!validUrl}>
             Add
           </ButtonPrimary>
         </Row>
+        {addError ? <TYPE.error error>{addError}</TYPE.error> : null}
       </PaddedColumn>
 
       <Separator />
@@ -58,10 +88,30 @@ export function ListSelect({ onDismiss, onBack }: { onDismiss: () => void; onBac
         {Object.keys(lists).map(listUrl => {
           const { current: list } = lists[listUrl]
 
+          const isSelected = listUrl === selectedListUrl
+
           return (
             <div key={listUrl}>
-              <div title={listUrl}>{list?.name}</div>
+              <div title={listUrl}>{list?.name ?? listUrl}</div>
               <div>{list?.version && listVersionLabel(list?.version)}</div>
+              <Row>
+                <ButtonSecondary
+                  onClick={() => {
+                    !isSelected && dispatch(selectList(listUrl))
+                  }}
+                  disabled={isSelected}
+                >
+                  {isSelected ? 'Selected' : 'Select'}
+                </ButtonSecondary>
+                <ButtonSecondary
+                  onClick={() => {
+                    dispatch(removeList(listUrl))
+                  }}
+                  disabled={listUrl === DEFAULT_TOKEN_LIST_URL}
+                >
+                  Remove
+                </ButtonSecondary>
+              </Row>
             </div>
           )
         })}
--- src/state/lists/actions.ts
+++ src/state/lists/actions.ts
@@ -1,40 +1,6 @@
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit'
 import { TokenList, Version } from '@uniswap/token-lists'
-import schema from '@uniswap/token-lists/src/tokenlist.schema.json'
-import Ajv from 'ajv'
-import uriToHttp from '../../utils/uriToHttp'
-
-const tokenListValidator = new Ajv({ allErrors: true }).compile(schema)
-
-/**
- * Contains the logic for resolving a URL to a valid token list
- * @param listUrl list url
- */
-async function getTokenList(listUrl: string): Promise<TokenList> {
-  const urls = uriToHttp(listUrl)
-  for (const url of urls) {
-    let response
-    try {
-      response = await fetch(url)
-      if (!response.ok) continue
-    } catch (error) {
-      console.error(`failed to fetch list ${listUrl} at uri ${url}`)
-      continue
-    }
-
-    const json = await response.json()
-    if (!tokenListValidator(json)) {
-      throw new Error(
-        tokenListValidator.errors?.reduce<string>((memo, error) => {
-          const add = `${error.dataPath} ${error.message ?? ''}`
-          return memo.length > 0 ? `${memo}; ${add}` : `${add}`
-        }, '') ?? 'Token list failed validation'
-      )
-    }
-    return json
-  }
-  throw new Error('Unrecognized list URL protocol.')
-}
+import { getTokenList } from '../../utils/getTokenList'
 
 const fetchCache: { [url: string]: Promise<TokenList> } = {}
 export const fetchTokenList = createAsyncThunk<TokenList, string>(
