45b27d2f501db41db542185c675162ebdbc8154b
Ashu <orangehat.rb@gmail.com>
2020-08-08 00:23:32 +0800

    Fix lint warning

--- src/hooks/useWrapCallback.ts
+++ src/hooks/useWrapCallback.ts
@@ -73,5 +73,5 @@ export default function useWrapCallback(
     } else {
       return NOT_APPLICABLE
     }
-  }, [wethContract, chainId, inputCurrency, outputCurrency, inputAmount, balance, addTransaction])
+  }, [wethContract, chainId, inputCurrency, outputCurrency, inputAmount, balance, addTransaction, t])
 }
