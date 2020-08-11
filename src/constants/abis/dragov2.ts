e0e4e9c7fa7d81b90769d8422c23fc5080ef2500
gab <gabriele.rigo@gmail.com>
2020-08-12 00:04:14 +0200

    export drago interface

--- src/constants/abis/dragov2.ts
+++ src/constants/abis/dragov2.ts
@@ -4,4 +4,4 @@ import Dragov2_ABI from './erc20.json'
 const Dragov2_INTERFACE = new Interface(Dragov2_ABI)
 
 export default Dragov2_INTERFACE
-export { Dragov2_ABI }
+export { Dragov2_ABI, Dragov2_INTERFACE }
