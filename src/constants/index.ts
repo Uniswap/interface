08f06ac6f96336941035e81da03cd6670e3d12b9
gab <gabriele.rigo@gmail.com>
2020-08-12 14:51:55 +0200

    fix default token list import

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -165,4 +165,4 @@ export const BETTER_TRADE_LINK_THRESHOLD = new Percent(JSBI.BigInt(75), JSBI.Big
 
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://unpkg.com/@uniswap/default-token-list@latest/uniswap-default.tokenlist.json'
+  'https://unpkg.com/@uniswap/default-token-list@latest/build/uniswap-default.tokenlist.json'
