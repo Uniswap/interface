02df121cebb84efeb708cad112f39e18800c611b
Brandon Curtis <brandon.curtis@gmail.com>
2020-08-25 10:41:19 -0700

    remove sUSD from routing shortlist

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -36,7 +36,7 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT, COMP, MKR, SUSD]
+  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT, COMP, MKR]
 }
 
 /**
