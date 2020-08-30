c9ffd9699e32e0978dc4ad33a13e616e7493e570
Cristopher Ortega <ortega.cpp@gmail.com>
2020-08-29 23:08:46 -0400

    updates for xdai

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -89,7 +89,7 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
+  '0x5e6d7FFf1b7BfB31006fE8039FFb0f28d73A4BF9', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
   '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // v2 router 02
 ]
