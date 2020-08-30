c05486d0f44c3e7a10c9e94ffa8bafe576952ed0
Cristopher Ortega <ortega.cpp@gmail.com>
2020-08-30 01:17:33 -0400

    display dai on header instead of eth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -162,7 +162,7 @@ export default function Header() {
             <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
               {account && userEthBalance ? (
                 <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
-                  {userEthBalance?.toSignificant(4)} ETH
+                  {userEthBalance?.toSignificant(4)} {chainId === 100 ? 'DAI' : 'ETH'}
                 </BalanceText>
               ) : null}
               <Web3Status />
