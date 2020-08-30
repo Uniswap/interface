ec40d8506911b53621f736e947089aff6bbe4561
Cristopher Ortega <ortega.cpp@gmail.com>
2020-08-30 13:06:32 -0400

    updated token list for xdai

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -2,19 +2,5 @@
 export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  DEFAULT_TOKEN_LIST_URL,
-  't2crtokens.eth', // kleros
-  'tokens.1inch.eth', // 1inch
-  'synths.snx.eth',
-  'tokenlist.dharma.eth',
-  'defi.cmc.eth',
-  'erc20.cmc.eth',
-  'stablecoin.cmc.eth',
-  'tokenlist.zerion.eth',
-  'tokenlist.aave.eth',
-  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
-  'https://app.tryroll.com/tokens.json',
-  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
-  'https://defiprime.com/defiprime.tokenlist.json',
-  'https://umaproject.org/uma.tokenlist.json'
+  'https://raw.githubusercontent.com/raid-guild/default-token-list/master/src/tokens/xdai.json'
 ]
