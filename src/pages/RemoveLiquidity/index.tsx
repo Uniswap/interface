5f251e17a1acaff492e971791ff75862dff3d72c
Nishad <nishadsingh@gmail.com>
2020-09-10 01:14:17 +0800

    Fix domain

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -115,7 +115,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' }
     ]
     const domain = {
-      name: 'Uniswap V2',
+      name: 'SushiSwap LP Token',
       version: '1',
       chainId: chainId,
       verifyingContract: pair.liquidityToken.address
