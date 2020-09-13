d15f04b27702b3b3187f79399acb6ec0d724f0a1
jiro-ono <jirosushi@protonmail.com>
2020-09-12 20:18:08 -0400

    Updated lists

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
-// the Uniswap Default token list lives here
+// the SushiSwap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL = window.location.origin + '/tokens.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
@@ -12,9 +12,9 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   // 'stablecoin.cmc.eth',
   // 'tokenlist.zerion.eth',
   // 'tokenlist.aave.eth',
-  // 'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
+  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
   // 'https://app.tryroll.com/tokens.json',
   // 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
-  'https://defiprime.com/defiprime.tokenlist.json',
+  // 'https://defiprime.com/defiprime.tokenlist.json',
   // 'https://umaproject.org/uma.tokenlist.json'
 ]
