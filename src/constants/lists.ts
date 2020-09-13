8d46bcf4648c898329f3e7396827c5e8606e2cd3
jiro-ono <jirosushi@protonmail.com>
2020-09-13 00:04:39 -0400

    Update default list to use github hosted list, makes it easier to update in future

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the SushiSwap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = window.location.origin + '/tokens.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/sushiswapclassic/token-list/master/sushiswap.tokenlist.json';
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
@@ -14,7 +14,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   // 'tokenlist.aave.eth',
   'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
   // 'https://app.tryroll.com/tokens.json',
-  // 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
   // 'https://defiprime.com/defiprime.tokenlist.json',
   // 'https://umaproject.org/uma.tokenlist.json'
 ]
