f79df41a5cf8cd283559f35826a97d3b3fe58d75
Mulenga Bowa <mulengabowa53@gmail.com>
2020-09-21 16:09:24 +0200

    added new fuse-default-token package

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -8,7 +8,7 @@ import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
 const getTokenLogoURL = (address: string) =>
-  `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+  `https://raw.githubusercontent.com/mul53/default-token-list/master/assets/${address}/logo.png`
 
 const StyledEthereumLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
