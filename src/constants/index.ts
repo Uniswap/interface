e5bc3aa51ab68f0292e0e6fb09105176f212b7e1
slinky57 <slinky57@protonmail.com>
2020-10-08 15:04:40 -0600

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -50,7 +50,7 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT, COMP, MKR]
+  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT, COMP, MKR, YFI, yYFI]
 }
 
 /**
