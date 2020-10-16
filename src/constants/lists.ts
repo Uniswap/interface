72ab18eb3ca739664a727281c30dfecdbecf24d6
Leo Vigna <leo.vigna@gmail.com>
2020-10-16 21:22:40 +0200

    Add custom list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,8 +1,9 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/leovigna/default-token-list/master/build/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
+  'tokens.uniswap.eth',
   't2crtokens.eth', // kleros
   'tokens.1inch.eth', // 1inch
   'synths.snx.eth',
