2ec830c2497a21094ebdd70a080363c2c917c6ea
QIUCHENGHUI <243852877@qq.com>
2020-10-20 18:04:13 +0800

    Update deerfi log

--- src/components/StableHeader/index.tsx
+++ src/components/StableHeader/index.tsx
@@ -7,8 +7,8 @@ import { useTranslation } from 'react-i18next'
 
 import styled from 'styled-components'
 
-import Logo from '../../assets/svg/logo.svg'
-import LogoDark from '../../assets/svg/logo_white.svg'
+import Logo from '../../assets/svg/logo.png'
+import LogoDark from '../../assets/svg/logo_white.png'
 import { useActiveWeb3React } from '../../hooks'
 import { useDarkModeManager } from '../../state/user/hooks'
 import { useETHBalances } from '../../state/wallet/hooks'
