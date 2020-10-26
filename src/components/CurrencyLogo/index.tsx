ac52ecc6ef2ee346b9e507ce1757aa6f06db2012
jenya <hypnagonia@gmail.com>
2020-10-26 18:10:45 +0300

    display swoop missing currency logos #8

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -1,53 +1,56 @@
-import { Currency, HARMONY, Token } from '@swoop-exchange/sdk'
-import React, { useMemo } from 'react'
-import styled from 'styled-components'
+import {Currency, HARMONY, Token} from '@swoop-exchange/sdk';
+import React, {useMemo} from 'react';
+import styled from 'styled-components';
 
-import HarmonyLogo from '../../assets/images/harmony.png'
-import useHttpLocations from '../../hooks/useHttpLocations'
-import { WrappedTokenInfo } from '../../state/lists/hooks'
-import Logo from '../Logo'
+import HarmonyLogo from '../../assets/images/harmony.png';
+import useHttpLocations from '../../hooks/useHttpLocations';
+import {WrappedTokenInfo} from '../../state/lists/hooks';
+import Logo from '../Logo';
 
 const getTokenLogoURL = (address: string) =>
-  `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+  `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`;
 
-const StyledHarmonyLogo = styled.img<{ size: string }>`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
+const StyledHarmonyLogo = styled.img<{size: string}>`
+  width: ${({size}) => size};
+  height: ${({size}) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
   border-radius: 24px;
-`
+`;
 
-const StyledLogo = styled(Logo)<{ size: string }>`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
-`
+const StyledLogo = styled(Logo)<{size: string}>`
+  width: ${({size}) => size};
+  height: ${({size}) => size};
+`;
+
+const addSwoopLogoUrl = currency => `https://swoop-exchange.s3-us-west-1.amazonaws.com/tokens/${currency.symbol}.png`;
 
 export default function CurrencyLogo({
-  currency,
-  size = '24px',
-  style
-}: {
+                                       currency,
+                                       size = '24px',
+                                       style,
+                                     }: {
   currency?: Currency
   size?: string
   style?: React.CSSProperties
 }) {
-  const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined)
+  const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined);
   const srcs: string[] = useMemo(() => {
-    if (currency === HARMONY || currency?.symbol === 'WONE') return []
+    if (currency === HARMONY || currency?.symbol === 'WONE') return [];
 
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
-        return [...uriLocations, getTokenLogoURL(currency.address)]
+        return [...uriLocations, getTokenLogoURL(currency.address)];
       }
 
-      return [getTokenLogoURL(currency.address)]
+      return [addSwoopLogoUrl(currency), getTokenLogoURL(currency.address)];
     }
-    return []
-  }, [currency, uriLocations])
+    return [];
+  }, [currency, uriLocations]);
 
   if (currency === HARMONY || currency?.symbol === 'WONE') {
-    return <StyledHarmonyLogo src={HarmonyLogo} size={size} style={style} />
+    return <StyledHarmonyLogo src={HarmonyLogo} size={size} style={style} />;
   }
 
-  return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />
+  //console.log('logo',currency?.symbol, {srcs})
+  return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />;
 }
