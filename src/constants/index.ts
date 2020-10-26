5c44cd1247b7e3ceb7b8ac410028b7b3531bf641
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-10-26 17:15:17 +0100

    New deployment

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,9 +5,9 @@ import { injected, oneWallet, mathWallet } from '../connectors'
 
 const { ChainID } = require("@harmony-js/utils");
 
-// Testnet address: 0x5264120DDF12EA1266A992E555B480F54Fa66232 - deployed 2020-10-25 13:05 UTC
+// Testnet address: 0x14AD662D9426322c51afa1025EE4F73aa01Cc04D - deployed 2020-10-26 16:10 UTC
 // Mainnet address: 0x9F6B533Aa9E13aAA316f13e95e631B9c71cB66cc - deployed 2020-10-22 18:32 UTC
-export const ROUTER_ADDRESS = '0x5264120DDF12EA1266A992E555B480F54Fa66232'
+export const ROUTER_ADDRESS = '0x14AD662D9426322c51afa1025EE4F73aa01Cc04D'
 
 // a list of tokens by chain
 type ChainTokenList = {
