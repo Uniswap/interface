4ae7cb21401a97f001a8054d8edb3286c72775a6
Milan Rasovic <ezo@Milans-MacBook-Pro.local>
2020-12-09 21:38:44 +0100

    popup final fix

--- src/components/Popups/index.tsx
+++ src/components/Popups/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useActivePopups } from '../../state/application/hooks'
+import { AutoColumn } from '../Column'
 import PopupItem from './PopupItem'
+
+import { useURLWarningVisible } from '../../state/user/hooks'
 
 const MobilePopupWrapper = styled.div<{ height: string | number }>`
   position: relative;
@@ -9,7 +12,11 @@ const MobilePopupWrapper = styled.div<{ height: string | number }>`
   height: ${({ height }) => height};
   margin: ${({ height }) => (height ? '0 auto;' : 0)};
   margin-bottom: ${({ height }) => (height ? '20px' : 0)}};
-  display: block;
+
+  display: none;
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    display: block;
+  `};
 `
 
 const MobilePopupInner = styled.div`
@@ -24,12 +31,32 @@ const MobilePopupInner = styled.div`
   }
 `
 
+const FixedPopupColumn = styled(AutoColumn)<{ extraPadding: boolean }>`
+  position: fixed;
+  top: ${({ extraPadding }) => (extraPadding ? '108px' : '88px')};
+  right: 1rem;
+  max-width: 355px !important;
+  width: 100%;
+  z-index: 3;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    display: none;
+  `};
+`
+
 export default function Popups() {
   // get all popups
   const activePopups = useActivePopups()
 
+  const urlWarningActive = useURLWarningVisible()
+
   return (
     <>
+      <FixedPopupColumn gap="20px" extraPadding={urlWarningActive}>
+        {activePopups.map(item => (
+          <PopupItem key={item.key} content={item.content} popKey={item.key} removeAfterMs={item.removeAfterMs} />
+        ))}
+      </FixedPopupColumn>
       <MobilePopupWrapper height={activePopups?.length > 0 ? 'fit-content' : 0}>
         <MobilePopupInner>
           {activePopups // reverse so new items up front
