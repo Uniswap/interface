7f2d85ea7f65a1ba13751fb5590725591a937f52
Ian Lapham <ian@uniswap.org>
2021-01-20 23:22:32 -0700

    Improvement(token import): update import flow and style tweaks (#1273)
    * update import flow and style tweaks
    
    * update tests
    
    * Update tsconfig.json

--- src/components/SearchModal/ImportToken.tsx
+++ src/components/SearchModal/ImportToken.tsx
@@ -39,13 +39,13 @@ const AddressText = styled(TYPE.blue)`
 `
 
 interface ImportProps {
-  token: Token
-  onBack: () => void
-  onDismiss: () => void
-  handleCurrencySelect: (currency: Currency) => void
+  tokens: Token[]
+  onBack?: () => void
+  onDismiss?: () => void
+  handleCurrencySelect?: (currency: Currency) => void
 }
 
-export function ImportToken({ token, onBack, onDismiss, handleCurrencySelect }: ImportProps) {
+export function ImportToken({ tokens, onBack, onDismiss, handleCurrencySelect }: ImportProps) {
   const theme = useTheme()
 
   const { chainId } = useActiveWeb3React()
@@ -57,86 +57,103 @@ export function ImportToken({ token, onBack, onDismiss, handleCurrencySelect }: 
   // use for showing import source on inactive tokens
   const inactiveTokenList = useCombinedInactiveList()
 
-  const list = chainId && inactiveTokenList?.[chainId]?.[token.address]?.list
+  // higher warning severity if either is not on a list
+  const fromLists =
+    (chainId && inactiveTokenList?.[chainId]?.[tokens[0]?.address]?.list) ||
+    (chainId && inactiveTokenList?.[chainId]?.[tokens[1]?.address]?.list)
 
   return (
     <Wrapper>
       <PaddedColumn gap="14px" style={{ width: '100%', flex: '1 1' }}>
         <RowBetween>
-          <ArrowLeft style={{ cursor: 'pointer' }} onClick={onBack} />
-          <TYPE.mediumHeader>Import Token</TYPE.mediumHeader>
-          <CloseIcon onClick={onDismiss} />
+          {onBack ? <ArrowLeft style={{ cursor: 'pointer' }} onClick={onBack} /> : <div></div>}
+          <TYPE.mediumHeader>Import {tokens.length > 1 ? 'Tokens' : 'Token'}</TYPE.mediumHeader>
+          {onDismiss ? <CloseIcon onClick={onDismiss} /> : <div></div>}
         </RowBetween>
       </PaddedColumn>
       <SectionBreak />
       <PaddedColumn gap="md">
-        <Card backgroundColor={theme.bg2}>
-          <AutoColumn gap="10px">
-            <AutoRow align="center">
-              <CurrencyLogo currency={token} size={'24px'} />
-              <TYPE.body ml="8px" mr="8px" fontWeight={500}>
-                {token.symbol}
-              </TYPE.body>
-              <TYPE.darkGray fontWeight={300}>{token.name}</TYPE.darkGray>
-            </AutoRow>
-            {chainId && (
-              <ExternalLink href={getEtherscanLink(chainId, token.address, 'address')}>
-                <AddressText>{token.address}</AddressText>
-              </ExternalLink>
-            )}
-            {list !== undefined ? (
-              <RowFixed>
-                {list.logoURI && <ListLogo logoURI={list.logoURI} size="12px" />}
-                <TYPE.small ml="6px" color={theme.text3}>
-                  via {list.name}
-                </TYPE.small>
-              </RowFixed>
-            ) : (
-              <WarningWrapper borderRadius="4px" padding="4px" highWarning={true}>
-                <RowFixed>
-                  <AlertTriangle stroke={theme.red1} size="10px" />
-                  <TYPE.body color={theme.red1} ml="4px" fontSize="10px" fontWeight={500}>
-                    Unkown Source
+        {tokens.map(token => {
+          const list = chainId && inactiveTokenList?.[chainId]?.[token.address]?.list
+          return (
+            <Card backgroundColor={theme.bg2} key={'import' + token.address} className=".token-warning-container">
+              <AutoColumn gap="10px">
+                <AutoRow align="center">
+                  <CurrencyLogo currency={token} size={'24px'} />
+                  <TYPE.body ml="8px" mr="8px" fontWeight={500}>
+                    {token.symbol}
                   </TYPE.body>
-                </RowFixed>
-              </WarningWrapper>
-            )}
-          </AutoColumn>
-        </Card>
-        <Card style={{ backgroundColor: list ? transparentize(0.8, theme.yellow2) : transparentize(0.8, theme.red1) }}>
+                  <TYPE.darkGray fontWeight={300}>{token.name}</TYPE.darkGray>
+                </AutoRow>
+                {chainId && (
+                  <ExternalLink href={getEtherscanLink(chainId, token.address, 'address')}>
+                    <AddressText>{token.address}</AddressText>
+                  </ExternalLink>
+                )}
+                {list !== undefined ? (
+                  <RowFixed>
+                    {list.logoURI && <ListLogo logoURI={list.logoURI} size="12px" />}
+                    <TYPE.small ml="6px" color={theme.text3}>
+                      via {list.name}
+                    </TYPE.small>
+                  </RowFixed>
+                ) : (
+                  <WarningWrapper borderRadius="4px" padding="4px" highWarning={true}>
+                    <RowFixed>
+                      <AlertTriangle stroke={theme.red1} size="10px" />
+                      <TYPE.body color={theme.red1} ml="4px" fontSize="10px" fontWeight={500}>
+                        Unkown Source
+                      </TYPE.body>
+                    </RowFixed>
+                  </WarningWrapper>
+                )}
+              </AutoColumn>
+            </Card>
+          )
+        })}
+
+        <Card
+          style={{ backgroundColor: fromLists ? transparentize(0.8, theme.yellow2) : transparentize(0.8, theme.red1) }}
+        >
           <AutoColumn justify="center" style={{ textAlign: 'center', gap: '16px', marginBottom: '12px' }}>
-            <AlertTriangle stroke={list ? theme.yellow2 : theme.red1} size={32} />
-            <TYPE.body fontWeight={600} fontSize={20} color={list ? theme.yellow2 : theme.red1}>
+            <AlertTriangle stroke={fromLists ? theme.yellow2 : theme.red1} size={32} />
+            <TYPE.body fontWeight={600} fontSize={20} color={fromLists ? theme.yellow2 : theme.red1}>
               Trade at your own risk!
             </TYPE.body>
           </AutoColumn>
 
           <AutoColumn style={{ textAlign: 'center', gap: '16px', marginBottom: '12px' }}>
-            <TYPE.body fontWeight={400} color={list ? theme.yellow2 : theme.red1}>
+            <TYPE.body fontWeight={400} color={fromLists ? theme.yellow2 : theme.red1}>
               Anyone can create a token, including creating fake versions of existing tokens that claim to represent
               projects.
             </TYPE.body>
-            <TYPE.body fontWeight={600} color={list ? theme.yellow2 : theme.red1}>
+            <TYPE.body fontWeight={600} color={fromLists ? theme.yellow2 : theme.red1}>
               If you purchase this token, you may not be able to sell it back.
             </TYPE.body>
           </AutoColumn>
           <AutoRow justify="center" style={{ cursor: 'pointer' }} onClick={() => setConfirmed(!confirmed)}>
-            <Checkbox name="confirmed" type="checkbox" checked={confirmed} onChange={() => setConfirmed(!confirmed)} />
-            <TYPE.body ml="10px" fontSize="16px" color={list ? theme.yellow2 : theme.red1} fontWeight={500}>
+            <Checkbox
+              className=".understand-checkbox"
+              name="confirmed"
+              type="checkbox"
+              checked={confirmed}
+              onChange={() => setConfirmed(!confirmed)}
+            />
+            <TYPE.body ml="10px" fontSize="16px" color={fromLists ? theme.yellow2 : theme.red1} fontWeight={500}>
               I understand
             </TYPE.body>
           </AutoRow>
         </Card>
-
         <ButtonPrimary
           disabled={!confirmed}
           altDisabledStyle={true}
           borderRadius="20px"
           padding="10px 1rem"
           onClick={() => {
-            addToken(token)
-            handleCurrencySelect(token)
+            tokens.map(token => addToken(token))
+            handleCurrencySelect && handleCurrencySelect(tokens[0])
           }}
+          className=".token-dismiss-button"
         >
           Import
         </ButtonPrimary>
