3d26f39783b42d822f5c25dacb510f29006f1425
Brandon Curtis <brandon.curtis@gmail.com>
2021-02-05 04:17:35 -0800

    remove COMP routing; add WBTC, UST

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -87,7 +87,7 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY,
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT, COMP, MKR]
+  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], DAI, USDC, USDT, WBTC, UST, FARM]
 }
 
 /**
