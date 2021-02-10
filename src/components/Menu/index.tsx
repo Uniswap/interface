0b7846ee1de65160f34c5fddc0f2674b6adbb7fe
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-02-10 10:56:42 -0500

    fix(discord): correct links (#1315)

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -117,7 +117,7 @@ export default function Menu() {
             <Code size={14} />
             Code
           </MenuItem>
-          <MenuItem id="link" href="https://discord.gg/EwFs3Pp">
+          <MenuItem id="link" href="https://discord.gg/FCfyBSbCU5">
             <MessageCircle size={14} />
             Discord
           </MenuItem>
