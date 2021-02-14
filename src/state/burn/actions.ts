505309f93b9bb400b3972c3bd4e3b2b968465be6
Lint Action <lint-action@samuelmeuli.com>
2021-02-14 08:11:05 +0000

    Fix code style issues with ESLint

--- src/state/burn/actions.ts
+++ src/state/burn/actions.ts
@@ -4,7 +4,7 @@ export enum Field {
   LIQUIDITY_PERCENT = 'LIQUIDITY_PERCENT',
   LIQUIDITY = 'LIQUIDITY',
   CURRENCY_A = 'CURRENCY_A',
-  CURRENCY_B = 'CURRENCY_B',
+  CURRENCY_B = 'CURRENCY_B'
 }
 
 export const typeInput = createAction<{ field: Field; typedValue: string }>('burn/typeInputBurn')
