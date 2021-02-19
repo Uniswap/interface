386032fb57213c9eddaa739731b1f15b83915306
Federico Luzzi <fedeluzzi00@gmail.com>
2021-02-19 18:09:54 +0100

    feat: review pair view skeleton

--- src/components/Pool/PairView/Loading/index.tsx
+++ src/components/Pool/PairView/Loading/index.tsx
@@ -8,9 +8,9 @@ export default function Loading() {
   return (
     <ContentLoader backgroundColor={theme.bg3} foregroundColor={theme.bg2} viewBox="0 0 500px 500px" height="100%">
       {/* header */}
-      <circle r="13px" cx="13px" cy="13px" />
-      <circle r="13px" cx="26px" cy="13px" />
-      <rect x="48" y="6" rx="2" ry="2" width="60" height="16" />
+      <circle r="10px" cx="10px" cy="10px" />
+      <circle r="10px" cx="23px" cy="10px" />
+      <rect x="44" y="0" rx="2" ry="2" width="60" height="20" />
 
       {/* first data row */}
       <rect x="0" y="44" rx="2" ry="2" width="60" height="14" />
@@ -21,13 +21,14 @@ export default function Loading() {
       <rect x="100%" y="71" rx="2" ry="2" width="60" height="14" style={{ transform: 'translateX(-60px)' }} />
 
       {/* third data row */}
-      <rect x="0" y="1312" rx="2" ry="2" width="60" height="14" />
-      <rect x="100%" y="106" rx="8" ry="8" width="80" height="27" style={{ transform: 'translateX(-80px)' }} />
-      <rect x="100%" y="106" rx="8" ry="8" width="40" height="27" style={{ transform: 'translateX(-130px)' }} />
+      <rect x="0" y="98" rx="2" ry="2" width="60" height="14" />
+      <rect x="100%" y="98" rx="2" ry="2" width="60" height="14" style={{ transform: 'translateX(-60px)' }} />
 
       {/* first collapsible */}
-      <rect x="0" y="152" rx="8" ry="8" width="100%" height="68" />
-      <rect x="0" y="228" rx="8" ry="8" width="100%" height="68" />
+      <rect x="0" y="140" rx="8" ry="8" width="100%" height="295px" />
+
+      {/* governance button */}
+      <rect x="0" y="462" rx="8" ry="8" width="100%" height="37px" />
     </ContentLoader>
   )
 }
