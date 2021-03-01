596de1ecc85e93a3073e89cb515753df22678266
tien7668 <tien7668@gmail.com>
2021-03-01 17:23:02 +0700

    ignore lib/sdk

--- src/libs/sdk/jest.config.js
+++
@@ -1,6 +0,0 @@
-// jest.config.js
-// Sync object
-module.exports = {
-  verbose: true,
-  testTimeout: 30000
-}
--- src/libs/sdk/src/index.ts
+++
@@ -1,9 +0,0 @@
-import JSBI from 'jsbi'
-export { JSBI }
-
-export { ChainId, TradeType, Rounding, MINIMUM_LIQUIDITY } from './constants'
-export type {BigintIsh} from './constants'
-export * from './errors'
-export * from './entities'
-export * from './router'
-export * from './fetcher'
