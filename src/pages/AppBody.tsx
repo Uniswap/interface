b2d02de63c4af5f5e146da7e74ef908a68bd0fda
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-02 17:21:50 +0100

    feat: review swap box style

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import styled from 'styled-components'
-import border8pxRadius from '../assets/images/border-8px-radius.png'
+import React, { useContext } from 'react'
+import styled, { ThemeContext } from 'styled-components'
+import { DarkCard } from '../components/Card'
 
-export const BodyWrapper = styled.div<{ tradeDetailsOpen?: boolean }>`
+export const BodyWrapper = styled(DarkCard)<{ tradeDetailsOpen?: boolean }>`
   position: relative;
   max-width: 420px;
   width: 100%;
-  background: ${({ theme }) => theme.bg1};
-  border: 8px solid transparent;
   border-radius: 8px;
-  border-image: url(${border8pxRadius}) 8;
-  padding: 12px;
+  padding: 16px;
   transition: box-shadow 0.3s ease;
-  box-shadow: ${({ tradeDetailsOpen }) =>
-    tradeDetailsOpen ? '0px 40px 42px -24px rgba(0, 0, 0, 0.22);' : '0px 40px 36px -24px rgba(0, 0, 0, 0.32)'};
+  box-shadow: 0px 6px 14px 0px #000000 10%;
+  ::before {
+    background: ${props => props.theme.dark1};
+  }
 `
 
 interface AppBodyProps {
@@ -25,5 +24,10 @@ interface AppBodyProps {
  * The styled container element that wraps the content of most pages and the tabs.
  */
 export default function AppBody({ children, tradeDetailsOpen }: AppBodyProps) {
-  return <BodyWrapper tradeDetailsOpen={tradeDetailsOpen}>{children}</BodyWrapper>
+  const theme = useContext(ThemeContext)
+  return (
+    <BodyWrapper backgroundColor={theme.bg1} tradeDetailsOpen={tradeDetailsOpen}>
+      {children}
+    </BodyWrapper>
+  )
 }
