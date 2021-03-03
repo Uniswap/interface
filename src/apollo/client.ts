a0377683d522d366a95358a20dc3a528101ce861
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-03 16:06:48 +0100

    feat: fix pagination and support reloading on network switch

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -3,23 +3,13 @@ import { ChainId } from 'dxswap-sdk'
 
 export const defaultSubgraphClient = new ApolloClient({
   uri: 'https://api.thegraph.com/subgraphs/name/luzzif/swapr-mainnet-alpha',
-  cache: new InMemoryCache(),
-  defaultOptions: {
-    query: {
-      fetchPolicy: 'network-only'
-    }
-  }
+  cache: new InMemoryCache()
 })
 
 export const subgraphClients: { [chainId in ChainId]?: ApolloClient<NormalizedCacheObject> | undefined } = {
   [ChainId.MAINNET]: defaultSubgraphClient,
   [ChainId.RINKEBY]: new ApolloClient({
     uri: 'https://api.thegraph.com/subgraphs/name/luzzif/swapr_rinkeby',
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      query: {
-        fetchPolicy: 'network-only'
-      }
-    }
+    cache: new InMemoryCache()
   })
 }
--- src/components/Pool/AggregatedPairsList/index.tsx
+++ src/components/Pool/AggregatedPairsList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import AggregatedPairs from './AggregatedPairs'
 import { Box, Flex, Text } from 'rebass'
 import Pagination from '../../Pagination'
@@ -8,8 +8,6 @@ import { useAggregatedByToken0ExistingPairsWithRemainingRewardsAndMaximumApy } f
 import Empty from '../Empty'
 import MyPairs from './MyPairs'
 import styled from 'styled-components'
-import { Pair, Token, TokenAmount } from 'dxswap-sdk'
-import BigNumber from 'bignumber.js'
 
 const ListLayout = styled.div`
   display: grid;
@@ -26,15 +24,6 @@ export default function AggregatedPairsList() {
   const [page, setPage] = useState(1)
   const [filter, setFilter] = useState(PairsFilterType.ALL)
   const { loading, aggregatedData } = useAggregatedByToken0ExistingPairsWithRemainingRewardsAndMaximumApy(filter)
-  const [paginatedItems, setPaginatedItems] = useState<
-    {
-      token0: Token
-      lpTokensBalance: TokenAmount
-      pairs: Pair[]
-      remainingRewardsUSD: BigNumber
-      maximumApy: BigNumber
-    }[]
-  >([])
 
   const userLpPairs = useMemo(
     () =>
@@ -44,24 +33,12 @@ export default function AggregatedPairsList() {
     [aggregatedData]
   )
 
-  // populate the first paginated page after loading. The first page contains
-  // only 11 items because the my pools card is always there
-  useEffect(() => {
-    if (!loading && aggregatedData.length > 0 && paginatedItems.length === 0) {
-      setPaginatedItems(aggregatedData.slice(0, ITEMS_PER_PAGE - 1))
-    }
-  }, [aggregatedData, loading, paginatedItems.length])
-
-  const handlePageChange = useCallback(
-    page => {
-      setPage(page)
-      const zeroIndexPage = page - 1
-      const normalizedItemsPerPage = zeroIndexPage < 2 ? ITEMS_PER_PAGE - 1 : ITEMS_PER_PAGE
-      const offset = zeroIndexPage * normalizedItemsPerPage
-      setPaginatedItems(aggregatedData.slice(offset, offset + normalizedItemsPerPage))
-    },
-    [aggregatedData]
-  )
+  const { pageOffset, pageItemsAmount } = useMemo(() => {
+    const zeroIndexPage = page - 1
+    const normalizedItemsPerPage = zeroIndexPage < 2 ? ITEMS_PER_PAGE - 1 : ITEMS_PER_PAGE
+    const pageOffset = zeroIndexPage * normalizedItemsPerPage
+    return { pageOffset, pageItemsAmount: normalizedItemsPerPage }
+  }, [page])
 
   return (
     <Flex flexDirection="column">
@@ -71,10 +48,10 @@ export default function AggregatedPairsList() {
       <Box mb="8px" height="460px">
         {loading ? (
           <LoadingList />
-        ) : paginatedItems.length > 0 ? (
+        ) : aggregatedData.length > 0 ? (
           <ListLayout>
             {page === 1 && <MyPairs pairs={userLpPairs} />}
-            {paginatedItems.map(aggregation => (
+            {aggregatedData.slice(pageOffset, pageOffset + pageItemsAmount).map(aggregation => (
               <AggregatedPairs
                 key={aggregation.token0.address}
                 token={aggregation.token0}
@@ -99,7 +76,7 @@ export default function AggregatedPairsList() {
             /* +1 because we account for the my pools card */
             totalItems={aggregatedData.length + 1}
             itemsPerPage={ITEMS_PER_PAGE}
-            onPageChange={handlePageChange}
+            onPageChange={setPage}
           />
         </Box>
       </Flex>
