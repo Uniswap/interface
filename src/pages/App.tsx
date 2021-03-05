ea8dcad2cbfeefdb1666b8e629de3524d3acee41
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-05 16:41:06 +0100

    feat: better react skeleton integration

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,8 @@
 import { ApolloProvider } from '@apollo/client'
 import { ChainId } from 'dxswap-sdk'
-import React, { Suspense } from 'react'
+import React, { Suspense, useContext } from 'react'
 import { Route, Switch, HashRouter } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { ThemeContext } from 'styled-components'
 import { defaultSubgraphClient, subgraphClients } from '../apollo/client'
 import Header from '../components/Header'
 import Popups from '../components/Popups'
@@ -20,6 +20,7 @@ import { RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
 import Pair from './Pools/Pair'
 import CreateLiquidityMining from './LiquidityMining/Create'
 import { useActiveWeb3React } from '../hooks'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -62,44 +63,47 @@ const Marginer = styled.div`
 
 export default function App() {
   const { chainId } = useActiveWeb3React()
+  const theme = useContext(ThemeContext)
 
   return (
     <Suspense fallback={null}>
-      <ApolloProvider client={subgraphClients[chainId as ChainId] || defaultSubgraphClient}>
-        <HashRouter>
-          <Route component={DarkModeQueryParamReader} />
-          <AppWrapper>
-            <HeaderWrapper>
-              <Header />
-            </HeaderWrapper>
-            <BodyWrapper>
-              <Popups />
-              <Web3ReactManager>
-                <Switch>
-                  <Route exact strict path="/swap" component={Swap} />
-                  <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
-                  <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
-                  <Route exact strict path="/find" component={PoolFinder} />
-                  <Route exact strict path="/pools" component={Pools} />
-                  <Route exact strict path="/pools/:currencyIdA" component={PairsByToken0} />
-                  <Route exact strict path="/pools/:currencyIdA/:currencyIdB" component={Pair} />
-                  <Route exact strict path="/create" component={AddLiquidity} />
-                  <Route exact path="/add" component={AddLiquidity} />
-                  {/* <Route exact strict path="/governance" component={GovPages} /> */}
-                  {/* <Route exact strict path="/governance/:asset/pairs" component={GovPages} /> */}
-                  <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-                  <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-                  <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
-                  <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-                  <Route exact strict path="/liquidity-mining/create" component={CreateLiquidityMining} />
-                  <Route component={RedirectPathToSwapOnly} />
-                </Switch>
-              </Web3ReactManager>
-              <Marginer />
-            </BodyWrapper>
-          </AppWrapper>
-        </HashRouter>
-      </ApolloProvider>
+      <SkeletonTheme color={theme.bg3} highlightColor={theme.bg2}>
+        <ApolloProvider client={subgraphClients[chainId as ChainId] || defaultSubgraphClient}>
+          <HashRouter>
+            <Route component={DarkModeQueryParamReader} />
+            <AppWrapper>
+              <HeaderWrapper>
+                <Header />
+              </HeaderWrapper>
+              <BodyWrapper>
+                <Popups />
+                <Web3ReactManager>
+                  <Switch>
+                    <Route exact strict path="/swap" component={Swap} />
+                    <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
+                    <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
+                    <Route exact strict path="/find" component={PoolFinder} />
+                    <Route exact strict path="/pools" component={Pools} />
+                    <Route exact strict path="/pools/:currencyIdA" component={PairsByToken0} />
+                    <Route exact strict path="/pools/:currencyIdA/:currencyIdB" component={Pair} />
+                    <Route exact strict path="/create" component={AddLiquidity} />
+                    <Route exact path="/add" component={AddLiquidity} />
+                    {/* <Route exact strict path="/governance" component={GovPages} /> */}
+                    {/* <Route exact strict path="/governance/:asset/pairs" component={GovPages} /> */}
+                    <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
+                    <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
+                    <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
+                    <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
+                    <Route exact strict path="/liquidity-mining/create" component={CreateLiquidityMining} />
+                    <Route component={RedirectPathToSwapOnly} />
+                  </Switch>
+                </Web3ReactManager>
+                <Marginer />
+              </BodyWrapper>
+            </AppWrapper>
+          </HashRouter>
+        </ApolloProvider>
+      </SkeletonTheme>
     </Suspense>
   )
 }
