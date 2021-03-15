2b76eddb23a042927e2ea85afe5ed10b3fa9fafd
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-15 21:50:43 +0100

    feat: add data in liquidity mining modal

--- src/components/Pool/PairView/LiquidityMiningCampaignModal/Information/index.tsx
+++ src/components/Pool/PairView/LiquidityMiningCampaignModal/Information/index.tsx
@@ -1,9 +1,14 @@
 import { LiquidityMiningCampaign } from 'dxswap-sdk'
 import { DateTime } from 'luxon'
 import React from 'react'
+import Skeleton from 'react-loading-skeleton'
 import { Box, Flex } from 'rebass'
 import styled from 'styled-components'
+import { useNativeCurrencyUSDPrice } from '../../../../../hooks/useNativeCurrencyUSDPrice'
+import { getRemainingRewardsUSD } from '../../../../../utils/liquidityMining'
 import Countdown from '../../../../Countdown'
+import CurrencyLogo from '../../../../CurrencyLogo'
+import { RowBetween } from '../../../../Row'
 import ApyBadge from '../../../ApyBadge'
 import DataRow from '../DataRow'
 
@@ -19,11 +24,32 @@ interface LiquidityMiningInformationProps {
 
 export default function LiquidityMiningInformation({ campaign }: LiquidityMiningInformationProps) {
   const { currentlyActive, locked, startsAt, endsAt, apy } = campaign
+  const { loading: loadingNativeCurrencyUSDPrice, nativeCurrencyUSDPrice } = useNativeCurrencyUSDPrice()
+
   return (
     <Flex justifyContent="stretch" width="100%">
       <Flex flexDirection="column" flex="1">
         <DataRow title="APY" value={<ApyBadge apy={apy} />} />
         {currentlyActive && <DataRow title="Time left" value={<Countdown to={parseInt(endsAt.toString())} />} />}
+        <DataRow
+          title="Rewards left"
+          value={campaign.remainingRewards.map(remainingReward => (
+            <RowBetween key={remainingReward.token.address}>
+              {remainingReward.toSignificant(3)}
+              <CurrencyLogo size="14px" currency={remainingReward.token} />
+            </RowBetween>
+          ))}
+        />
+        <DataRow
+          title="Rewards left (USD)"
+          value={
+            loadingNativeCurrencyUSDPrice ? (
+              <Skeleton width="60px" />
+            ) : (
+              `$${getRemainingRewardsUSD(campaign, nativeCurrencyUSDPrice).toFixed(2)}`
+            )
+          }
+        />
       </Flex>
       <Box mx="18px">
         <Divider />
@@ -38,6 +64,7 @@ export default function LiquidityMiningInformation({ campaign }: LiquidityMining
           value={DateTime.fromSeconds(parseInt(endsAt.toString())).toFormat('dd-MM-yyyy hh:mm')}
         />
         <DataRow title="Timelock" value={locked ? 'ON' : 'OFF'} />
+        <DataRow title="Max pool size" value="UNLIMITED" />
       </Flex>
     </Flex>
   )
