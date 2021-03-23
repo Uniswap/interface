0c02b43f69258e64f9dee6558f831f04c635fc2a
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-23 11:31:53 +0100

    feat: improve pagination style

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -316,22 +316,28 @@ body {
   outline: 0;
   cursor: pointer;
   user-select: none;
-  border: solid 1px ${props => props.theme.bg4};
+  border: solid 1px ${props => props.theme.bg3};
   transition: border 0.3s ease, color 0.3s ease;
   font-size: 14px;
   border-radius: 4px;
   text-align: center;
   line-height: 20px;
-  color: ${props => props.theme.text4};
+  color: ${props => props.theme.text5};
 }
 
-.swapr-pagination li.rc-pagination-disabled {
-  border: solid 1px ${props => props.theme.bg3};
-  color: ${props => props.theme.text5};
+.swapr-pagination li.rc-pagination-item-active {
+  border: solid 1px ${props => props.theme.bg4};
 }
 
 .swapr-pagination li.rc-pagination-prev,
 .swapr-pagination li.rc-pagination-next {
+  color: ${props => props.theme.white};
   padding-top: 2px;
 }
+
+.swapr-pagination li.rc-pagination-disabled {
+  border: solid 1px ${props => props.theme.bg3};
+  color: ${props => props.theme.bg3};
+}
+
 `
