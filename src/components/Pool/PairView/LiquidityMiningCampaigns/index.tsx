428bb52b3d2ac2055726e443f63035c5cc85d1e2
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-25 15:30:39 +0100

    feat: implement my positions tab

--- src/components/Pool/PairView/LiquidityMiningCampaigns/index.tsx
+++ src/components/Pool/PairView/LiquidityMiningCampaigns/index.tsx
@@ -1,10 +1,12 @@
 import { Pair } from 'dxswap-sdk'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useActiveWeb3React } from '../../../../hooks'
 import { TYPE } from '../../../../theme'
 import { AutoColumn } from '../../../Column'
 import TabBar from '../../../TabBar'
 import List from './List'
 import ActiveTab from './Tabs/Active'
+import ConnectedAccountTab from './Tabs/ConnectedAccount'
 import ExpiredTab from './Tabs/Expired'
 import UpcomingTab from './Tabs/Upcoming'
 
@@ -12,26 +14,38 @@ interface LiquidityMiningCampaignsListProps {
   pair?: Pair
 }
 
-const TAB_TITLES = ['Active', 'Upcoming', 'Expired (30 days)']
+const INITIAL_TAB_TITLES = ['Active', 'Upcoming', 'Expired (30 days)']
 
 export default function LiquidityMiningCampaigns({ pair }: LiquidityMiningCampaignsListProps) {
+  const { account } = useActiveWeb3React()
+
+  const [tabTitles, setTabTitles] = useState(INITIAL_TAB_TITLES)
   const [activeTab, setActiveTab] = useState(0)
 
-  const handleTabChange = useCallback(newActiveTab => {
-    setActiveTab(newActiveTab)
-  }, [])
+  const handleTabChange = useCallback(
+    newActiveTab => {
+      setActiveTab(account ? newActiveTab : newActiveTab + 1)
+    },
+    [account]
+  )
+
+  useEffect(() => {
+    if (account) setTabTitles(['My positions', ...INITIAL_TAB_TITLES])
+    else setTabTitles(INITIAL_TAB_TITLES)
+  }, [account])
 
   return (
     <AutoColumn gap="16px">
       <TYPE.mediumHeader fontSize="18px" color="white">
         Reward pools
       </TYPE.mediumHeader>
-      <TabBar titles={TAB_TITLES} active={activeTab} onChange={handleTabChange} />
+      <TabBar titles={tabTitles} active={activeTab} onChange={handleTabChange} />
       {pair ? (
         <>
-          {activeTab === 0 && <ActiveTab pair={pair} />}
-          {activeTab === 1 && <UpcomingTab pair={pair} />}
-          {activeTab === 2 && <ExpiredTab pair={pair} />}
+          {activeTab === 0 && <ConnectedAccountTab pair={pair} />}
+          {activeTab === 1 && <ActiveTab pair={pair} />}
+          {activeTab === 2 && <UpcomingTab pair={pair} />}
+          {activeTab === 3 && <ExpiredTab pair={pair} />}
         </>
       ) : (
         <List loading />
