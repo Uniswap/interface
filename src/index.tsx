8d90bb7a3990b8a7f6394969cf89432c7831fec7
Moody Salem <moody.salem@gmail.com>
2021-03-26 16:32:13 -0500

    fix(google analytics): don't set user cookies

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,12 @@ if (!!window.ethereum) {
 
 const GOOGLE_ANALYTICS_ID: string | undefined = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
 if (typeof GOOGLE_ANALYTICS_ID === 'string') {
-  ReactGA.initialize(GOOGLE_ANALYTICS_ID)
+  ReactGA.initialize(GOOGLE_ANALYTICS_ID, {
+    gaOptions: {
+      storage: 'none',
+      storeGac: false
+    }
+  })
   ReactGA.set({
     customBrowserType: !isMobile ? 'desktop' : 'web3' in window || 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
   })
