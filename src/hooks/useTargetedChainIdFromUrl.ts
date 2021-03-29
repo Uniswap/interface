0571d9c7160242f2f2e8cd2c584660378b9684e9
Federico Luzzi <fedeluzzi00@gmail.com>
2021-03-29 14:12:35 +0200

    feat: dynamically change network-only connector chain id if specified in url

---
+++ src/hooks/useTargetedChainIdFromUrl.ts
@@ -0,0 +1,11 @@
+import { ChainId } from 'dxswap-sdk'
+import useParsedQueryString from './useParsedQueryString'
+
+export function useTargetedChainIdFromUrl(): ChainId | undefined {
+  const { chainId: targetedChainId } = useParsedQueryString()
+  if (typeof targetedChainId === 'string') {
+    const parsedTargetedChainId = parseInt(targetedChainId)
+    return Object.values(ChainId).indexOf(parsedTargetedChainId) >= 0 ? parsedTargetedChainId : undefined
+  }
+  return undefined
+}
