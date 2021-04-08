d08cae175a80394c99971708de25c94cae92affd
Noah Zinsmeister <noahwz@gmail.com>
2021-04-08 11:27:19 -0400

    update to latest deploy + multicall 2

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -27,7 +27,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 1337],
+  supportedChainIds: [1, 3, 4, 5, 42],
 })
 
 // mainnet only
--- src/constants/v3/index.ts
+++ src/constants/v3/index.ts
@@ -1,46 +1,41 @@
 import { ChainId } from '@uniswap/sdk-core'
 
-export const FACTORY_ADDRESSES: { [chainId in ChainId | 1337]: string } = {
+export const FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '',
   [ChainId.ROPSTEN]: '',
   [ChainId.RINKEBY]: '',
-  [ChainId.GÖRLI]: '',
+  [ChainId.GÖRLI]: '0x2e52A6ed50aeD4396BdC5889ACB9D04F6804D150',
   [ChainId.KOVAN]: '',
-  [1337]: '0x49A6d0854B0fF95065f0dA247b8a2d440D92D2c7',
 }
 
-export const TICK_LENS_ADDRESSES: { [chainId in ChainId | 1337]: string } = {
+export const TICK_LENS_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '',
   [ChainId.ROPSTEN]: '',
   [ChainId.RINKEBY]: '',
-  [ChainId.GÖRLI]: '',
+  [ChainId.GÖRLI]: '0x637df729C49f229B328445AB64573B726F6814dC',
   [ChainId.KOVAN]: '',
-  [1337]: '0x99Cb00560BFca3E5B7Cda8e820dDFF1ED403966E',
 }
 
-export const NONFUNGIBLE_POSITION_MANAGER_ADDRESSES: { [chainId in ChainId | 1337]: string } = {
+export const NONFUNGIBLE_POSITION_MANAGER_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '',
   [ChainId.ROPSTEN]: '',
   [ChainId.RINKEBY]: '',
-  [ChainId.GÖRLI]: '',
+  [ChainId.GÖRLI]: '0x514A3562Bbf313B262A4d2377B559CDe75FAC3C9',
   [ChainId.KOVAN]: '',
-  [1337]: '0x273Edaa13C845F605b5886Dd66C89AB497A6B17b',
 }
 
-export const NONFUNGIBLE_TOKEN_POSITION_DESCRIPTOR_ADDRESSES: { [chainId in ChainId | 1337]: string } = {
+export const NONFUNGIBLE_TOKEN_POSITION_DESCRIPTOR_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '',
   [ChainId.ROPSTEN]: '',
   [ChainId.RINKEBY]: '',
-  [ChainId.GÖRLI]: '',
+  [ChainId.GÖRLI]: '0x2a3DB14c12674986f6F52deaD1d5ee0919715E8A',
   [ChainId.KOVAN]: '',
-  [1337]: '0x3d137e860008BaF6d1c063158e5ec0baBbcFefF8',
 }
 
-export const SWAP_ROUTER_ADDRESSES: { [chainId in ChainId | 1337]: string } = {
+export const SWAP_ROUTER_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '',
   [ChainId.ROPSTEN]: '',
   [ChainId.RINKEBY]: '',
-  [ChainId.GÖRLI]: '',
+  [ChainId.GÖRLI]: '0x86e14633F8ab81FD49aEF388172A43A7C8731664',
   [ChainId.KOVAN]: '',
-  [1337]: '0x80AacDBEe92DC1c2Fbaa261Fb369696AF1AD9f98',
 }
