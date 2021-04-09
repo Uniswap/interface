9dc8941cc525c218673e39037765bf1c1406bda1
Federico Luzzi <fedeluzzi00@gmail.com>
2021-04-09 11:53:42 +0200

    chore: copywrite fix in comment

--- src/hooks/usePage.ts
+++ src/hooks/usePage.ts
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 
 /**
  * This hook returns the correct page (defined as a slice of a passed in array) given a dataset, the items per page
- * that are to be displayed, the wanted page number (NON zero based) and the number of
+ * that are to be displayed, the wanted page number (NON zero indexed) and the number of
  * items which are always present in the first page (specific example: my pools card in aggregated view, since it is
  * always there in the first page)
  **/
