c2577abbab02e86f84470db21ab9c89660894688
Moody Salem <moody.salem@gmail.com>
2021-04-19 12:55:54 -0500

    more flexible detection of network

--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -1,12 +1,15 @@
 import { Web3Provider } from '@ethersproject/providers'
-import { MiniRpcProvider } from '../connectors/NetworkConnector'
 
 export default function getLibrary(provider: any): Web3Provider {
-  // ethers tries to detect the network which fails and is unnecessary with our mini rpc provider if we do not pass the correct network id
-  if (provider instanceof MiniRpcProvider) {
-    return new Web3Provider(provider as any, provider.chainId)
-  }
-  const library = new Web3Provider(provider, 'any')
+  // latest ethers version tries to detect the network which fails
+  const library = new Web3Provider(
+    provider,
+    typeof provider.chainId === 'number'
+      ? provider.chainId
+      : typeof provider.chainId === 'string'
+      ? parseInt(provider.chainId)
+      : 'any'
+  )
   library.pollingInterval = 15000
   return library
 }
