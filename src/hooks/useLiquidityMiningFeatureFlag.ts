1b0c9c92d2f65f0512d162816376fdd7a014f731
Federico Luzzi <fedeluzzi00@gmail.com>
2021-04-23 20:42:52 +0200

    feat: put back xdai as a network with enabled farming

--- src/hooks/useLiquidityMiningFeatureFlag.ts
+++ src/hooks/useLiquidityMiningFeatureFlag.ts
@@ -3,5 +3,5 @@ import { useActiveWeb3React } from '.'
 
 export function useLiquidityMiningFeatureFlag(): boolean {
   const { chainId } = useActiveWeb3React()
-  return chainId === ChainId.RINKEBY
+  return chainId === ChainId.RINKEBY || chainId === ChainId.XDAI
 }
