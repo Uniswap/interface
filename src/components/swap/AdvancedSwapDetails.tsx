1d3b0beff8492c140a8ab7a1387c7d583f148879
Moody Salem <moody.salem@gmail.com>
2021-04-28 12:55:28 -0500

    drop the show details stuff

--- src/components/swap/AdvancedSwapDetails.tsx
+++ src/components/swap/AdvancedSwapDetails.tsx
@@ -81,7 +81,7 @@ function TradeSummary({ trade }: { trade: V2Trade | V3Trade }) {
             <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
               Liquidity Provider Fee
             </TYPE.black>
-            <QuestionHelper text="A portion of each trade (0.30%) goes to liquidity providers as a protocol incentive." />
+            <QuestionHelper text="A portion of each trade goes to liquidity providers as a protocol incentive." />
           </RowFixed>
           <TYPE.black fontSize={14} color={theme.text1}>
             {realizedLPFee ? `${realizedLPFee.toSignificant(4)} ${trade.inputAmount.currency.symbol}` : '-'}
--- src/components/swap/TradePrice.tsx
+++ src/components/swap/TradePrice.tsx
@@ -1,59 +1,44 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Price } from '@uniswap/sdk-core'
 import { useContext } from 'react'
 import { Text } from 'rebass'
 
-import { ThemeContext } from 'styled-components'
+import styled, { ThemeContext } from 'styled-components'
 import { StyledBalanceMaxMini } from './styleds'
 import Switch from '../../assets/svg/switch.svg'
-import { ButtonEmpty } from '../Button'
 
 interface TradePriceProps {
   price?: Price
   showInverted: boolean
   setShowInverted: (showInverted: boolean) => void
-  showDetails: boolean
-  setShowDetails: (showInverted: boolean) => void
 }
 
-export default function TradePrice({
-  price,
-  showInverted,
-  setShowInverted,
-  showDetails,
-  setShowDetails,
-}: TradePriceProps) {
+const StyledPriceContainer = styled.div`
+  justify-content: flex-end;
+  align-items: center;
+  display: flex;
+  width: 100%;
+`
+
+export default function TradePrice({ price, showInverted, setShowInverted }: TradePriceProps) {
   const theme = useContext(ThemeContext)
 
   const formattedPrice = showInverted ? price?.toSignificant(6) : price?.invert()?.toSignificant(6)
 
   const label = showInverted ? `${price?.quoteCurrency?.symbol}` : `${price?.baseCurrency?.symbol} `
   const labelInverted = showInverted ? `${price?.baseCurrency?.symbol} ` : `${price?.quoteCurrency?.symbol}`
+  const flipPrice = useCallback(() => setShowInverted(!showInverted), [setShowInverted, showInverted])
 
   return (
-    <div
-      style={{
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        display: 'flex',
-        width: '100%',
-      }}
-      onClick={() => setShowInverted(!showInverted)}
-    >
-      <ButtonEmpty style={{ padding: '0.25rem', width: 'fit-content' }} onClick={() => setShowDetails(!showDetails)}>
-        <Text fontWeight={500} fontSize={14} color={theme.text2} style={{ marginRight: '.25rem' }}>
-          Show Details
-        </Text>
-      </ButtonEmpty>
-
+    <StyledPriceContainer>
       <div style={{ alignItems: 'center', display: 'flex', width: 'fit-content' }}>
         <Text fontWeight={500} fontSize={14} color={theme.text2}>
           {'1 ' + labelInverted + ' = ' + formattedPrice ?? '-'} {label}
         </Text>
-        <StyledBalanceMaxMini style={{ marginLeft: '0.5rem' }} onClick={() => setShowInverted(!showInverted)}>
+        <StyledBalanceMaxMini style={{ marginLeft: '0.5rem' }} onClick={flipPrice}>
           <img width={'16px'} src={Switch} alt="logo" />
         </StyledBalanceMaxMini>
       </div>
-    </div>
+    </StyledPriceContainer>
   )
 }
