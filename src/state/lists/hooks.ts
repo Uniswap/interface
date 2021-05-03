ab390bc1febe27d4219307e5fef113ad392ab451
Ian Lapham <ian@uniswap.org>
2021-05-03 13:34:01 -0400

    fix for error token map parsings (#1384)

--- src/state/lists/hooks.ts
+++ src/state/lists/hooks.ts
@@ -61,7 +61,10 @@ export function listToTokenMap(list: TokenList): TokenAddressMap {
           })
           ?.filter((x): x is TagInfo => Boolean(x)) ?? []
       const token = new WrappedTokenInfo(tokenInfo, tags)
-      if (tokenMap[token.chainId][token.address] !== undefined) throw Error('Duplicate tokens.')
+      if (tokenMap[token.chainId][token.address] !== undefined) {
+        console.error(new Error(`Duplicate token! ${token.address}`))
+        return tokenMap
+      }
       return {
         ...tokenMap,
         [token.chainId]: {
