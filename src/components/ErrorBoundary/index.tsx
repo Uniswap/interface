2595db2b5bf4830adb54d01c9af49c4f41e69ba4
Jochen Boesmans <jochen.boesmans@gmail.com>
2021-05-07 22:47:38 +0200

    Slightly reduce vertical padding on code block.

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -25,7 +25,7 @@ const CodeBlockWrapper = styled.div`
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
   border-radius: 24px;
-  padding: 24px;
+  padding: 18px 24px;
   color: ${({ theme }) => theme.text1};
 `
 
