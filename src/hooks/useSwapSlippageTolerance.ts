3f298b291bb907a26465ccf00fc19012ae6c7591
Moody Salem <moody.salem@gmail.com>
2021-05-07 13:38:51 -0400

    fix the display bug

--- src/hooks/useSwapSlippageTolerance.ts
+++ src/hooks/useSwapSlippageTolerance.ts
@@ -5,7 +5,7 @@ import { useMemo } from 'react'
 import { ZERO_PERCENT } from '../constants'
 import { useUserSlippageToleranceWithDefault } from '../state/user/hooks'
 import { computePriceImpactWithMaximumSlippage } from '../utils/computePriceImpactWithMaximumSlippage'
-import { computeRealizedLPFee } from 'utils/prices'
+import { computeRealizedLPFeePercent } from 'utils/prices'
 
 const ONE_BIP = new Percent(1, 10_000) // .01%
 const ONE_TENTHS_PERCENT = new Percent(10, 10_000) // .1%
@@ -21,10 +21,12 @@ export default function useSwapSlippageTolerance(trade: V2Trade | V3Trade | unde
     let executionPriceImpact = computePriceImpactWithMaximumSlippage(trade, ZERO_PERCENT)
 
     // compute lp fee
-    const realizedLPFee = computeRealizedLPFee(trade)
+    const realizedLPFeePercent = computeRealizedLPFeePercent(trade)
 
     // subtract the lp fee from the price impact, with a ceiling of 1 bip
-    executionPriceImpact = executionPriceImpact.subtract(realizedLPFee)
+    executionPriceImpact = executionPriceImpact.subtract(realizedLPFeePercent)
+    // workaround for the display bug where subtract returns a fraction but we need a percent
+    executionPriceImpact = new Percent(executionPriceImpact.numerator, executionPriceImpact.denominator)
     if (executionPriceImpact.lessThan(ONE_BIP)) executionPriceImpact = ONE_BIP
 
     // floor of 1%
--- src/utils/prices.ts
+++ src/utils/prices.ts
@@ -14,38 +14,41 @@ const ONE_HUNDRED_PERCENT = new Percent(JSBI.BigInt(10000), JSBI.BigInt(10000))
 const INPUT_FRACTION_AFTER_FEE = ONE_HUNDRED_PERCENT.subtract(THIRTY_BIPS_FEE)
 
 // computes realized lp fee as a percent
-export function computeRealizedLPFee(trade: V2Trade | V3Trade): Percent {
+export function computeRealizedLPFeePercent(trade: V2Trade | V3Trade): Percent {
+  let percent: Percent
   if (trade instanceof V2Trade) {
     // for each hop in our trade, take away the x*y=k price impact from 0.3% fees
     // e.g. for 3 tokens/2 hops: 1 - ((1 - .03) * (1-.03))
-    return ONE_HUNDRED_PERCENT.subtract(
+    percent = ONE_HUNDRED_PERCENT.subtract(
       trade.route.pairs.reduce<Fraction>(
         (currentFee: Fraction): Fraction => currentFee.multiply(INPUT_FRACTION_AFTER_FEE),
         ONE_HUNDRED_PERCENT
       )
     )
+  } else {
+    percent = ONE_HUNDRED_PERCENT.subtract(
+      trade.route.pools.reduce<Fraction>(
+        (currentFee: Fraction, pool): Fraction =>
+          currentFee.multiply(ONE_HUNDRED_PERCENT.subtract(new Fraction(pool.fee, 1_000_000))),
+        ONE_HUNDRED_PERCENT
+      )
+    )
   }
 
-  return ONE_HUNDRED_PERCENT.subtract(
-    trade.route.pools.reduce<Fraction>(
-      (currentFee: Fraction, pool): Fraction =>
-        currentFee.multiply(ONE_HUNDRED_PERCENT.subtract(new Fraction(pool.fee, 1_000_000))),
-      ONE_HUNDRED_PERCENT
-    )
-  )
+  return new Percent(percent.numerator, percent.denominator)
 }
 
 // computes price breakdown for the trade
 export function computeRealizedLPFeeAmount(trade?: V2Trade | V3Trade | null): CurrencyAmount | undefined {
   if (trade instanceof V2Trade) {
-    const realizedLPFee = computeRealizedLPFee(trade)
+    const realizedLPFee = computeRealizedLPFeePercent(trade)
 
     // the amount of the input that accrues to LPs
     return trade.inputAmount instanceof TokenAmount
       ? new TokenAmount(trade.inputAmount.token, realizedLPFee.multiply(trade.inputAmount.raw).quotient)
       : CurrencyAmount.ether(realizedLPFee.multiply(trade.inputAmount.raw).quotient)
   } else if (trade instanceof V3Trade) {
-    const realizedLPFee = computeRealizedLPFee(trade)
+    const realizedLPFee = computeRealizedLPFeePercent(trade)
 
     return trade.inputAmount instanceof TokenAmount
       ? new TokenAmount(trade.inputAmount.token, realizedLPFee.multiply(trade.inputAmount.raw).quotient)
