58a508c9d6ecbb367f4b64af79615cf6ffd0c215
Noah Zinsmeister <noahwz@gmail.com>
2021-05-11 13:53:15 -0400

    .25% -> .30% slippage for v3 swaps

--- src/hooks/useSwapSlippageTolerance.ts
+++ src/hooks/useSwapSlippageTolerance.ts
@@ -5,7 +5,7 @@ import { useMemo } from 'react'
 import { useUserSlippageToleranceWithDefault } from '../state/user/hooks'
 
 const V2_SWAP_DEFAULT_SLIPPAGE = new Percent(45, 10_000) // .45%
-const V3_SWAP_DEFAULT_SLIPPAGE = new Percent(25, 10_000) // .25%
+const V3_SWAP_DEFAULT_SLIPPAGE = new Percent(30, 10_000) // .30%
 const ONE_TENTHS_PERCENT = new Percent(10, 10_000) // .10%
 
 export default function useSwapSlippageTolerance(trade: V2Trade | V3Trade | undefined): Percent {
