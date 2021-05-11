df90aa41ce19825832b1a281d1195f79fce11659
Justin Domingue <domingue.justin@gmail.com>
2021-05-11 18:44:49 -0400

    fix lint error

--- src/pages/Pool/PositionPage.tsx
+++ src/pages/Pool/PositionPage.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import React, { SyntheticEvent, useCallback, useMemo, useRef, useState } from 'react'
 import { NonfungiblePositionManager, Pool, Position } from '@uniswap/v3-sdk'
 
 import { PoolState, usePool } from 'hooks/usePools'
