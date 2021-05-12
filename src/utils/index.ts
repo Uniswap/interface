6baaa4c51c5618a467d07fe912ec728085d5c9db
Noah Zinsmeister <noahwz@gmail.com>
2021-05-12 12:35:46 -0400

    bump gas margin to 20%

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -58,9 +58,9 @@ export function shortenAddress(address: string, chars = 4): string {
   return `${parsed.substring(0, chars + 2)}...${parsed.substring(42 - chars)}`
 }
 
-// add 10%
+// add 20%
 export function calculateGasMargin(value: BigNumber): BigNumber {
-  return value.mul(BigNumber.from(10000).add(BigNumber.from(1000))).div(BigNumber.from(10000))
+  return value.mul(BigNumber.from(10000 + 2000)).div(BigNumber.from(10000))
 }
 
 const ONE = new Fraction(1, 1)
