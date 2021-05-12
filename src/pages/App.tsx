94542022524969e5e496e9f7dbd323d3f1a12d3c
Moody Salem <moody.salem@gmail.com>
2021-05-12 13:11:06 -0500

    clean up the error boundary

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,6 @@ import GoogleAnalyticsReporter from '../components/analytics/GoogleAnalyticsRepo
 import AddressClaimModal from '../components/claim/AddressClaimModal'
 import Header from '../components/Header'
 import Polling from '../components/Header/Polling'
-// import URLWarning from '../components/Header/URLWarning'
 import Popups from '../components/Popups'
 import Web3ReactManager from '../components/Web3ReactManager'
 import ErrorBoundary from '../components/ErrorBoundary'
