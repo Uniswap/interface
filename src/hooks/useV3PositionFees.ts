f5fc5da34123f7dd7f41f978bb8c79d193f289ca
Noah Zinsmeister <noahwz@gmail.com>
2021-05-12 12:20:20 -0400

    fix erroneous addition of tokensOwned{0,1} (#1533)
    only pass tokenId to useV3PositionFees

--- src/hooks/useV3PositionFees.ts
+++ src/hooks/useV3PositionFees.ts
@@ -1,6 +1,5 @@
 import { useSingleCallResult } from 'state/multicall/hooks'
 import { useEffect, useState } from 'react'
-import { PositionDetails } from 'types/position'
 import { useV3NFTPositionManagerContract } from './useContract'
 import { BigNumber } from '@ethersproject/bignumber'
 import { Pool } from '@uniswap/v3-sdk'
@@ -12,26 +11,24 @@ const MAX_UINT128 = BigNumber.from(2).pow(128).sub(1)
 // compute current + counterfactual fees for a v3 position
 export function useV3PositionFees(
   pool?: Pool,
-  positionDetails?: PositionDetails
+  tokenId?: BigNumber
 ): [CurrencyAmount<Token>, CurrencyAmount<Token>] | [undefined, undefined] {
   const positionManager = useV3NFTPositionManagerContract(false)
-  const owner = useSingleCallResult(positionDetails?.tokenId ? positionManager : null, 'ownerOf', [
-    positionDetails?.tokenId,
-  ]).result?.[0]
+  const owner = useSingleCallResult(tokenId ? positionManager : null, 'ownerOf', [tokenId]).result?.[0]
 
-  const tokenId = positionDetails?.tokenId?.toHexString()
+  const tokenIdHexString = tokenId?.toHexString()
   const latestBlockNumber = useBlockNumber()
 
   // TODO find a way to get this into multicall
-  // because fees don't ever go down, we don't actually need to clear this state
-  // latestBlockNumber is included to ensure data stays up-to-date fresh
+  // because these amounts don't ever go down, we don't actually need to clear this state
+  // latestBlockNumber is included to ensure data stays up-to-date every block
   const [amounts, setAmounts] = useState<[BigNumber, BigNumber]>()
   useEffect(() => {
-    if (positionManager && tokenId && owner && typeof latestBlockNumber === 'number') {
+    if (positionManager && tokenIdHexString && owner && typeof latestBlockNumber === 'number') {
       positionManager.callStatic
         .collect(
           {
-            tokenId,
+            tokenId: tokenIdHexString,
             recipient: owner, // some tokens might fail if transferred to address(0)
             amount0Max: MAX_UINT128,
             amount1Max: MAX_UINT128,
@@ -42,12 +39,12 @@ export function useV3PositionFees(
           setAmounts([results.amount0, results.amount1])
         })
     }
-  }, [positionManager, tokenId, owner, latestBlockNumber])
+  }, [positionManager, tokenIdHexString, owner, latestBlockNumber])
 
-  if (pool && positionDetails && amounts) {
+  if (pool && amounts) {
     return [
-      CurrencyAmount.fromRawAmount(pool.token0, positionDetails.tokensOwed0.add(amounts[0]).toString()),
-      CurrencyAmount.fromRawAmount(pool.token1, positionDetails.tokensOwed1.add(amounts[1]).toString()),
+      CurrencyAmount.fromRawAmount(pool.token0, amounts[0].toString()),
+      CurrencyAmount.fromRawAmount(pool.token1, amounts[1].toString()),
     ]
   } else {
     return [undefined, undefined]
