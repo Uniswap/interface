6c8770ab5872d53253ff6a6aed17afd7819cd395
Moody Salem <moody.salem@gmail.com>
2021-05-14 07:10:25 -0500

    perf: debounce localStorage save

--- src/state/index.ts
+++ src/state/index.ts
@@ -28,7 +28,7 @@ const store = configureStore({
     multicall,
     lists,
   },
-  middleware: [...getDefaultMiddleware({ thunk: false }), save({ states: PERSISTED_KEYS })],
+  middleware: [...getDefaultMiddleware({ thunk: false }), save({ states: PERSISTED_KEYS, debounce: 1000 })],
   preloadedState: load({ states: PERSISTED_KEYS }),
 })
 
