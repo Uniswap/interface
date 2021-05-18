95f668e62503ff1ea9675e39a8b3a71b402d9fe7
Justin Domingue <judo@uniswap.org>
2021-05-18 08:35:10 -0700

    set pair to invalid if both reserves are 0

--- src/hooks/useV2Pairs.ts
+++ src/hooks/useV2Pairs.ts
@@ -48,6 +48,7 @@ export function useV2Pairs(currencies: [Currency | undefined, Currency | undefin
       if (!tokenA || !tokenB || tokenA.equals(tokenB)) return [PairState.INVALID, null]
       if (!reserves) return [PairState.NOT_EXISTS, null]
       const { reserve0, reserve1 } = reserves
+      if (reserve0.isZero() && reserve1.isZero()) return [PairState.NOT_EXISTS, null]
       const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
       return [
         PairState.EXISTS,
