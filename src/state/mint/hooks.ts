6a36e844eb4166295ac0a2a7aba2b3dbd8f33b99
Justin Domingue <judo@uniswap.org>
2021-05-20 10:33:46 -0700

    revert: "fix: handle insufficient input amount error in v2 add liquidity (#1652)"
    This reverts commit 307a995a50c0ec39c9d4f43937081c67e5e33505.

--- src/state/mint/hooks.ts
+++ src/state/mint/hooks.ts
@@ -160,16 +160,10 @@ export function useDerivedMintInfo(
       wrappedCurrencyAmount(currencyBAmount, chainId),
     ]
     if (pair && totalSupply && tokenAmountA && tokenAmountB) {
-      try {
-        return pair.getLiquidityMinted(totalSupply, tokenAmountA, tokenAmountB)
-      } catch (error) {
-        if (error.InsufficientInputAmountError) {
-          return CurrencyAmount.fromRawAmount(pair.liquidityToken, ZERO)
-        }
-        return undefined
-      }
+      return pair.getLiquidityMinted(totalSupply, tokenAmountA, tokenAmountB)
+    } else {
+      return undefined
     }
-    return undefined
   }, [parsedAmounts, chainId, pair, totalSupply])
 
   const poolTokenPercentage = useMemo(() => {
@@ -201,10 +195,6 @@ export function useDerivedMintInfo(
 
   if (currencyBAmount && currencyBalances?.[Field.CURRENCY_B]?.lessThan(currencyBAmount)) {
     error = 'Insufficient ' + currencies[Field.CURRENCY_B]?.symbol + ' balance'
-  }
-
-  if (!liquidityMinted?.greaterThan(ZERO)) {
-    error = `Insufficient input amount`
   }
 
   return {
