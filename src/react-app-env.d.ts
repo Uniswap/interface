5b14255bde894185281ba8156572e820f1e08469
T2 <studioTeaTwo@gmail.com>
2021-05-21 01:32:00 +0900

    fix: replace jazzicon to @metamask/jazzicon

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react-scripts" />
 
-declare module 'jazzicon' {
+declare module '@metamask/jazzicon' {
   export default function (diameter: number, seed: number): HTMLElement
 }
 
