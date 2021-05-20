536a5d99de92a55a3a962012d64cbcf75cba98ea
Justin Domingue <judo@uniswap.org>
2021-05-20 10:11:42 -0700

    fix: fix typo is getLiquidityMinted catch (#1663)

--- src/state/mint/hooks.ts
+++ src/state/mint/hooks.ts
@@ -161,7 +161,7 @@ export function useDerivedMintInfo(
       try {
         return pair.getLiquidityMinted(totalSupply, tokenAmountA, tokenAmountB)
       } catch (error) {
-        if (error.InsufficientInputAmountError) {
+        if (error.isInsufficientInputAmountError) {
           return CurrencyAmount.fromRawAmount(pair.liquidityToken, ZERO)
         }
         return undefined
