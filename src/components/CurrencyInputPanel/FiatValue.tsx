5cebfd456b4dbf1e540c86de00ddd1b789174e45
Justin Domingue <judo@uniswap.org>
2021-05-21 09:42:26 -0700

    use empty string over space

--- src/components/CurrencyInputPanel/FiatValue.tsx
+++ src/components/CurrencyInputPanel/FiatValue.tsx
@@ -25,7 +25,7 @@ export function FiatValue({
   return (
     <TYPE.body fontSize={14} color={fiatValue ? theme.text2 : theme.text4}>
       {fiatValue ? '~$' : ''}
-      <HoverInlineText text={fiatValue ? fiatValue?.toSignificant(6, { groupSeparator: ',' }) : ' '} />{' '}
+      <HoverInlineText text={fiatValue ? fiatValue?.toSignificant(6, { groupSeparator: ',' }) : ''} />{' '}
       {priceImpact ? (
         <span style={{ color: priceImpactColor }}> ({priceImpact.multiply(-1).toSignificant(3)}%)</span>
       ) : null}
