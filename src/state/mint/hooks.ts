bfef2296077300d41e9c71b8a13318700aa46867
Moody Salem <moody.salem@gmail.com>
2021-05-21 09:39:07 -0500

    remove the unwrapped token from mint hook

--- src/state/mint/hooks.ts
+++ src/state/mint/hooks.ts
@@ -1,6 +1,5 @@
 import { useCallback, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { unwrappedToken } from '../../utils/unwrappedToken'
 import { AppDispatch, AppState } from '../index'
 import { Field, typeInput } from './actions'
 import { Pair } from '@uniswap/v2-sdk'
