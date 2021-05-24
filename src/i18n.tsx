929fc3fa8b8f192bc5d86e4f55fed92e8c73d325
Justin Domingue <judo@uniswap.org>
2021-05-23 18:17:18 -0700

    detect locale

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
+import { detect, fromUrl, fromNavigator } from '@lingui/detect-locale'
 import { ReactNode, useEffect } from 'react'
 
 export const locales = ['en']
 export const defaultLocale = 'en'
 
 locales.map((locale) => i18n.loadLocaleData(locale, { plurals: () => null }))
+
+const detectedLocale = detect(
+  fromUrl('lang'), // helps development
+  fromNavigator(),
+  defaultLocale
+)
 
 export async function dynamicActivate(locale: string) {
   const { messages } = await import(`@lingui/loader!./locales/${locale}/messages.po`)
@@ -16,7 +23,7 @@ export async function dynamicActivate(locale: string) {
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
-    dynamicActivate(defaultLocale)
+    dynamicActivate(detectedLocale || defaultLocale)
   })
 
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>
