11fdb8aa91fb254bfd1f0ff73b7ce7f0e4723c5e
Justin Domingue <judo@uniswap.org>
2021-05-26 08:31:29 -0700

    clean up

--- lingui.config.js
+++ lingui.config.js
@@ -18,4 +18,3 @@ module.exports = {
   runtimeConfigModule: ['@lingui/core', 'i18n'],
   sourceLocale: 'en',
 }
-//locales: ['en', 'de', 'es-AR', 'es-US', 'it-IT', 'iw', 'ro', 'ru', 'vi', 'zh-CN', 'zh-TW'],
