089b42e2943504da829505b0cbba44986e703144
Justin Domingue <judo@uniswap.org>
2021-05-26 18:54:51 -0700

    load locale data dynamically

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -18,6 +18,7 @@ const getDetectedLocale = () => {
 
 export async function dynamicActivate(locale: string) {
   const { messages } = await import(`@lingui/loader!./locales/${locale}/messages.po`)
+  i18n.loadLocaleData(locale, { plurals: () => null })
   i18n.load(locale, messages)
   i18n.activate(locale)
 }
