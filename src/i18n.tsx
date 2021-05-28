53ea358625257ff7a0d9a9820a23b8546bf1328f
Justin Domingue <judo@uniswap.org>
2021-05-27 19:31:40 -0700

    added navigator locale fallback to generic dialect

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useLocale } from './hooks'
 import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
 import { detect, fromNavigator } from '@lingui/detect-locale'
 import { ReactNode } from 'react'
+import { useDispatch } from 'react-redux'
+import useParsedQueryString from 'hooks/useParsedQueryString'
+import { useEffect } from '@storybook/addons'
+import { AppDispatch } from 'state'
+
+type IE11NavigatorLanguage = {
+  userLanguage?: string
+}
 
 export const supportedLocales = [
   'en',
@@ -20,11 +29,25 @@ export const supportedLocales = [
 ]
 export const defaultFallback = () => 'en'
 
-export const isSupportedLocale = (locale: string): boolean => supportedLocales.includes(locale)
+const isSupportedLocale = (locale: string): boolean => supportedLocales.includes(locale)
 
-export const getDetectedLocale = (): string => {
-  const detected = detect(fromNavigator())
-  return detected && isSupportedLocale(detected) ? detected : defaultFallback()
+const getDetectedLocale = (
+  navigator: Partial<Navigator & IE11NavigatorLanguage> = window.navigator
+): string | undefined => {
+  const language = navigator.language ?? navigator.language
+  if (!language) return undefined
+
+  if (isSupportedLocale(language)) return navigator.language
+
+  // fallack to generic dialect, if supported
+  const [subtag] = language.split('-')
+  if (isSupportedLocale(subtag)) return subtag
+
+  return undefined
+}
+
+const getActiveLocale = (cached: string | undefined, qs: string | undefined): string => {
+  return qs ?? cached ?? getDetectedLocale() ?? defaultFallback()
 }
 
 export async function dynamicActivate(locale: string) {
@@ -39,5 +62,18 @@ export async function dynamicActivate(locale: string) {
 }
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
+  const dispatch = useDispatch<AppDispatch>()
+  const parsed = useParsedQueryString()
+  const userLocale = useLocale()
+
+  useEffect(() => {
+    const parsedLocale = typeof parsed.lng === 'string' && isSupportedLocale(parsed.lng) ? parsed.lng : undefined
+
+    const activeLocale = getActiveLocale(userLocale, parsedLocale)
+    if (activeLocale) {
+      dynamicActivate(activeLocale)
+    }
+  }, [dispatch, userLocale, parsed])
+
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>
 }
--- src/state/user/updater.tsx
+++ src/state/user/updater.tsx
@@ -1,16 +1,10 @@
-import useParsedQueryString from 'hooks/useParsedQueryString'
-import { dynamicActivate, getDetectedLocale, isSupportedLocale } from 'i18n'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../index'
 import { updateMatchesDarkMode } from './actions'
-import { useLocale } from './hooks'
 
 export default function Updater(): null {
   const dispatch = useDispatch<AppDispatch>()
-  const parsed = useParsedQueryString()
-
-  const locale = useLocale()
 
   // keep dark mode in sync with the system
   useEffect(() => {
@@ -35,18 +29,6 @@ export default function Updater(): null {
       }
     }
   }, [dispatch])
-
-  useEffect(() => {
-    if (typeof parsed.lng === 'string' && isSupportedLocale(parsed.lng)) {
-      // always respect lgn query param
-      dynamicActivate(parsed.lng)
-    } else if (locale) {
-      dynamicActivate(locale)
-    } else {
-      const detectedLocale = getDetectedLocale()
-      if (detectedLocale) dynamicActivate(detectedLocale)
-    }
-  }, [dispatch, locale, parsed])
 
   return null
 }
