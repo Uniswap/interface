d097e37a12f54c1324331312867d564b815db3f1
Justin Domingue <judo@uniswap.org>
2021-05-28 09:03:53 -0700

    feat(i18n): save and load locale from redux store (#1723)
    * wrap sock around function component to get re-renders
    
    * removed sourcelocale since we are using custom ids
    
    * load locale data dynamically
    
    * undo dev change
    
    * ran yarn i18n:extract
    
    * store locale in redux
    
    * added support for redux
    
    * restored header
    
    * refactor
    
    * addressed pr feedback
    
    * specify type
    
    * added navigator locale fallback to generic dialect
    
    * make locale array const and typed
    
    * addressed pr feedback
    
    * fixed various
    
    * addressed pr feedback
    
    * make supported locales constant uppercase
    
    * add back toUpperCase removed during refactoring
    
    * removed lingui/detect-locale
    
    * run yarn

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,34 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { i18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
-import { detect, fromUrl } from '@lingui/detect-locale'
-import { ReactNode, useEffect } from 'react'
+import { ReactNode } from 'react'
+import useParsedQueryString from 'hooks/useParsedQueryString'
+import { useLocale } from 'state/user/hooks'
+import { SupportedLocale, SUPPORTED_LOCALES, defaultLocale } from './constants/locales'
 
-export const locales = ['en', 'pseudo-en', 'de', 'es-AR', 'es-US', 'it-IT', 'iw', 'ro', 'ru', 'vi', 'zh-CN', 'zh-TW']
-export const defaultLocale = 'en'
-
-const getDetectedLocale = () => {
-  const detected =
-    detect(
-      fromUrl('lang'), // helps local development
-      defaultLocale
-    ) ?? defaultLocale
-  return locales.includes(detected) ? detected : defaultLocale
+function parseLocale(maybeSupportedLocale: string): SupportedLocale | undefined {
+  return SUPPORTED_LOCALES.find((locale) => locale === maybeSupportedLocale)
 }
 
-export async function dynamicActivate(locale: string) {
-  const { messages } = await import(`@lingui/loader!./locales/${locale}.po`)
-  i18n.loadLocaleData(locale, { plurals: () => null })
-  i18n.load(locale, messages)
-  i18n.activate(locale)
+function navigatorLocale(): SupportedLocale | undefined {
+  if (!navigator.language) return undefined
+
+  const [language, region] = navigator.language.split('-')
+
+  if (region) {
+    return parseLocale(`${language}-${region.toUpperCase()}`) ?? parseLocale(language)
+  }
+
+  return parseLocale(language)
+}
+
+export async function dynamicActivate(locale: SupportedLocale) {
+  try {
+    const { messages } = await import(`@lingui/loader!./locales/${locale}.po`)
+    i18n.loadLocaleData(locale, { plurals: () => null })
+    i18n.load(locale, messages)
+    i18n.activate(locale)
+  } catch (error) {
+    console.error(`Failed to load locale data for ${locale}`, error)
+  }
 }
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
+  const parsed = useParsedQueryString()
+  const userLocale = useLocale()
+
   useEffect(() => {
-    dynamicActivate(getDetectedLocale()).catch((error) => {
-      console.error('Failed to load locale data', error)
-    })
-  }, [])
+    const urlLocale = () => (typeof parsed.lng === 'string' && parseLocale(parsed.lng)) || undefined
+
+    dynamicActivate(userLocale ?? urlLocale() ?? navigatorLocale() ?? defaultLocale)
+  }, [userLocale, parsed])
 
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>
 }
