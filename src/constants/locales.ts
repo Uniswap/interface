0dafd3e7ba9010c22152aefc2d8663f2b1f0dee8
Moody Salem <moody.salem@gmail.com>
2021-05-28 23:25:32 -0500

    chore: update to the top 30 locales

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -1,8 +1,40 @@
-export const SUPPORTED_LOCALES = ['en-US'] as const
+export const SUPPORTED_LOCALES = [
+  'af-ZA',
+  'ar-SA',
+  'ca-ES',
+  'cs-CZ',
+  'da-DK',
+  'de-DE',
+  'el-GR',
+  'en-US',
+  'es-ES',
+  'fi-FI',
+  'fr-FR',
+  'he-IL',
+  'hu-HU',
+  'it-IT',
+  'ja-JP',
+  'ko-KR',
+  'nl-NL',
+  'no-NO',
+  'pl-PL',
+  'pt-BR',
+  'pt-PT',
+  'ro-RO',
+  'ru-RU',
+  'sr-SP',
+  'sv-SE',
+  'tr-TR',
+  'uk-UA',
+  'vi-VN',
+  'zh-CN',
+  'zh-TW',
+] as const
 export type SupportedLocale = typeof SUPPORTED_LOCALES[number]
 
 export const DEFAULT_LOCALE: SupportedLocale = 'en-US'
 
-export const LOCALE_LABEL: { [locale in SupportedLocale]: string } = {
+// todo: fill this back out
+export const LOCALE_LABEL: { [locale in SupportedLocale]?: string } = {
   'en-US': 'English',
 }
