f7896db5aa1462ba66b7bc46508d9446f7d7b391
Moody Salem <moody.salem@gmail.com>
2021-05-31 22:08:38 -0500

    fix linting error, fix header responsiveness in non-english languages, try to fix the background color issue on safari

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components/macro'
 import GoogleAnalyticsReporter from '../components/analytics/GoogleAnalyticsReporter'
