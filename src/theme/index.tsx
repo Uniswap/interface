eb2dc1b769e9fd5fcb0baea2cf8c9d783ec3ba9b
Moody Salem <moody.salem@gmail.com>
2021-05-31 20:55:52 -0500

    move the radial gradient into the index.css file

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -188,25 +188,6 @@ export const TYPE = {
   },
 }
 
-export const ThemedBackground = styled.div<{ backgroundColor?: string | undefined }>`
-  position: fixed;
-  top: 0;
-  left: calc(-100vw / 2);
-  right: 0;
-  pointer-events: none;
-  /* max-width: 100vw !important; */
-  width: 200vw;
-  height: 200vh;
-  mix-blend-mode: color;
-  background: ${({ backgroundColor }) =>
-    `radial-gradient(50% 50% at 50% 50%, ${
-      backgroundColor ? backgroundColor : '#fc077d10'
-    } 0%, rgba(255, 255, 255, 0) 100%)`};
-  transform: translateY(-100vh);
-  will-change: background;
-  transition: background 450ms ease;
-`
-
 export const ThemedGlobalStyle = createGlobalStyle`
 html {
   color: ${({ theme }) => theme.text1};
