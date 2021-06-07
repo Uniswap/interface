130322ed31a7ee6a63479210553a83f4193f4463
Moody Salem <moody.salem@gmail.com>
2021-06-07 17:47:18 -0500

    use the cloudflare eth gateway for mainnet

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -22,7 +22,7 @@ if (typeof INFURA_KEY === 'undefined') {
 const NETWORK_URLS: {
   [chainId in SupportedChainId]: string
 } = {
-  [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+  [SupportedChainId.MAINNET]: `https://cloudflare-eth.com`,
   [SupportedChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
   [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
