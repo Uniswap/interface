42874765ca137c85f82f324d66a3b7c6436bd973
Justin Domingue <judo@uniswap.org>
2021-06-24 13:33:11 -0700

    fix: typos in modal css (#1911)
    * allow modal scrolling on dekstop
    
    * removed quote and fixed typo

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -30,7 +30,7 @@ const StyledDialogContent = styled(({ minHeight, maxHeight, mobile, isOpen, ...r
 )).attrs({
   'aria-label': 'dialog',
 })`
-  overflow-y: 'scrol';
+  overflow-y: scroll;
 
   &[data-reach-dialog-content] {
     margin: 0 0 2rem 0;
@@ -39,7 +39,7 @@ const StyledDialogContent = styled(({ minHeight, maxHeight, mobile, isOpen, ...r
     box-shadow: 0 4px 8px 0 ${({ theme }) => transparentize(0.95, theme.shadow1)};
     padding: 0px;
     width: 50vw;
-    overflow-y: 'scroll';
+    overflow-y: scroll;
     overflow-x: hidden;
 
     align-self: ${({ mobile }) => (mobile ? 'flex-end' : 'center')};
