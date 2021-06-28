f3537b5b9f6b71ad1bf167dc6a1b0fb0923ad6f3
Justin Domingue <judo@uniswap.org>
2021-06-28 09:38:23 -0700

    remove comment

--- src/constants/ticks.ts
+++ src/constants/ticks.ts
@@ -14,26 +14,3 @@ export interface TickProcessed {
   liquidityNet: JSBI
   price0: string
 }
-
-/* Three options
-
-1/  (current)
--process ticks as {tick, liquidity}[]
--further extract { [tick]: liquidity }
--while not at bounds
-  --how to define bounds?
-
-2/ sorting
--process ticks as {tick, liquidity}[] SORTED
-  -no need to extract { [tick]: liquidity }
-- from 0, both sides, while there's initialized ticks
-  -bounds are easy to calculate
-  -sorting is o(nlogn)
-
-3/ straight to map
--process ticks as { [tick]: liquidity }
--while not at bounds
-  -how to define bounds?
-
-
-*/
