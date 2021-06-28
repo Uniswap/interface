3e759ae402cb84ae14ecffb7672598460feb1433
Hoang Trinh <hoang.trinhj@gmail.com>
2021-06-28 15:11:16 +0700

    https://kybernetwork.atlassian.net/browse/DMM-247 - Fix Billion Symbol

--- src/utils/formatBalance.ts
+++ src/utils/formatBalance.ts
@@ -21,7 +21,7 @@ export const formatBigLiquidity = (num: string, decimals: number, usd = true): s
     { value: 1, symbol: '' },
     { value: 1e3, symbol: 'k' },
     { value: 1e6, symbol: 'M' },
-    { value: 1e9, symbol: 'G' },
+    { value: 1e9, symbol: 'B' },
     { value: 1e12, symbol: 'T' },
     { value: 1e15, symbol: 'P' },
     { value: 1e18, symbol: 'E' }
