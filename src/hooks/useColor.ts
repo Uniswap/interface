50c7d361645bffe39412db4b0eaaea2bea5dbcaf
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-06-28 16:35:31 -0400

    replace trustwallet assets repo w/ our fork (#1922)

--- src/hooks/useColor.ts
+++ src/hooks/useColor.ts
@@ -10,7 +10,7 @@ async function getColorFromToken(token: Token): Promise<string | null> {
     return Promise.resolve('#FAAB14')
   }
 
-  const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${token.address}/logo.png`
+  const path = `https://raw.githubusercontent.com/uniswap/assets/master/blockchains/ethereum/assets/${token.address}/logo.png`
 
   return Vibrant.from(path)
     .getPalette()
