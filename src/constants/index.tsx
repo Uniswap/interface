086f666f943449f2d5f5792e453cd9ee7c8fe328
Federico Luzzi <fedeluzzi00@gmail.com>
2021-06-30 14:52:12 +0200

    feat: new default token list

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -205,7 +205,7 @@ export const BLOCKED_PRICE_IMPACT_NON_EXPERT: Percent = new Percent(JSBI.BigInt(
 // used to ensure the user doesn't send so much ETH so they end up with <.01
 export const MIN_ETH: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16)) // .01 ETH
 
-export const DEFAULT_TOKEN_LIST = 'ipfs://QmRYaeveehcmBJA7WN4X1A5UNz6NQHJCCuWGLrLdYbQqH4'
+export const DEFAULT_TOKEN_LIST = 'ipfs://QmeCv4FhDPXRCcG1odeaFyEqrfKe8DGjaprT8BeTurYY51'
 
 export const ZERO_USD = CurrencyAmount.usd('0')
 
