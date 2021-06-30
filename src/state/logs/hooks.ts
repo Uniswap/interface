6b8334f87d0ac3ebfbaf1d5f7e972bda3dfb76b6
Moody Salem <moody.salem@gmail.com>
2021-06-30 18:54:28 -0500

    remove the unnecessary retry code

--- src/state/logs/hooks.ts
+++ src/state/logs/hooks.ts
@@ -45,15 +45,12 @@ export function useLogs(
     // we are already synced, skip
     if (state && state.chainId === chainId && state.blockNumber >= blockNumber) return
 
-    // TODO: we could try to fetch only as of the last block we've seen, however there aren't good ways to force a fetch as of a block
-    const { promise, cancel } = retry(() => library.getLogs({ ...filter, fromBlock: 0, toBlock: blockNumber }), {
-      n: 5,
-      maxWait: 1000,
-      minWait: 2500,
-    })
+    let stale = false
 
-    promise
+    library
+      .getLogs({ ...filter, fromBlock: 0, toBlock: blockNumber })
       .then((logs) => {
+        if (stale) return
         setState((prev) => {
           if (prev && prev.chainId === chainId && prev.blockNumber > blockNumber) return prev
 
@@ -65,13 +62,15 @@ export function useLogs(
         })
       })
       .catch((error) => {
-        if (error.isCancelledError) return
+        if (stale) return
 
         console.error('Failed to fetch logs', error)
         setState(undefined)
       })
 
-    return cancel
+    return () => {
+      stale = true
+    }
   }, [library, chainId, filter, blockNumber, state])
 
   return useMemo(() => {
