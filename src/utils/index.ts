f89cdd52b776fd963caf13151068dcb580190d71
alexcherman <sashacherman@gmail.com>
2021-07-01 15:58:53 +0200

    Show specific amount of characters for desktop and mobile

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -69,12 +69,12 @@ export function getExplorerLink(
 }
 
 // shorten the checksummed version of the input address to have 0x + 4 characters at start and end
-export function shortenAddress(address: string, chars = 4): string {
+export function shortenAddress(address: string, charsBefore = 4, charsAfter = 4): string {
   const parsed = isAddress(address)
   if (!parsed) {
     throw Error(`Invalid 'address' parameter '${address}'.`)
   }
-  return `${parsed.substring(0, chars + 2)}...${parsed.substring(42 - chars)}`
+  return `${parsed.substring(0, charsBefore + 2)}...${parsed.substring(42 - charsAfter)}`
 }
 
 // add 10%
