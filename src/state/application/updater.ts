5298a5ce298c126e9d26b02d02ee7203847df1ed
Justin Domingue <judo@uniswap.org>
2021-07-01 09:50:03 -0700

    feat: handle chain id in subgraph api (#1938)
    * build dynamic subgraph url based on chain id
    
    * reset api state (query cache) on chain id change
    
    * removed dependency on rtk-query/graphql
    
    * add error message

--- src/state/application/updater.ts
+++ src/state/application/updater.ts
@@ -1,9 +1,21 @@
 import { useCallback, useEffect, useState } from 'react'
-import { useAppDispatch } from 'state/hooks'
+import { api } from 'state/data/slice'
+import { useAppDispatch, useAppSelector } from 'state/hooks'
+import { supportedChainId } from 'utils/supportedChainId'
 import useDebounce from '../../hooks/useDebounce'
 import useIsWindowVisible from '../../hooks/useIsWindowVisible'
 import { useActiveWeb3React } from '../../hooks/web3'
-import { updateBlockNumber } from './actions'
+import { updateBlockNumber, updateChainId } from './actions'
+
+function useQueryCacheInvalidator() {
+  const chainId = useAppSelector((state) => state.application.chainId)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    console.log('chanid changed')
+    dispatch(api.util.resetApiState())
+  }, [chainId, dispatch])
+}
 
 export default function Updater(): null {
   const { library, chainId } = useActiveWeb3React()
@@ -15,6 +27,8 @@ export default function Updater(): null {
     chainId,
     blockNumber: null,
   })
+
+  useQueryCacheInvalidator()
 
   const blockNumberCallback = useCallback(
     (blockNumber: number) => {
@@ -52,6 +66,12 @@ export default function Updater(): null {
     if (!debouncedState.chainId || !debouncedState.blockNumber || !windowVisible) return
     dispatch(updateBlockNumber({ chainId: debouncedState.chainId, blockNumber: debouncedState.blockNumber }))
   }, [windowVisible, dispatch, debouncedState.blockNumber, debouncedState.chainId])
+
+  useEffect(() => {
+    dispatch(
+      updateChainId({ chainId: debouncedState.chainId ? supportedChainId(debouncedState.chainId) ?? null : null })
+    )
+  }, [dispatch, debouncedState.chainId])
 
   return null
 }
--- src/state/data/slice.ts
+++ src/state/data/slice.ts
@@ -1,17 +1,57 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
-import { gql, GraphQLClient } from 'graphql-request'
+import { ClientError, gql, GraphQLClient } from 'graphql-request'
 import { FeeAmount } from '@uniswap/v3-sdk'
 import { reduce } from 'lodash'
-import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
 
 import { FeeTierDistribution, PoolTVL } from './types'
+import { SupportedChainId } from 'constants/chains'
+import { AppState } from 'state'
+import { BaseQueryApi, BaseQueryFn } from '@reduxjs/toolkit/dist/query/baseQueryTypes'
+import { DocumentNode } from 'graphql'
 
 export const UNISWAP_V3_GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
+
+export const graphqlRequestBaseQuery = (): BaseQueryFn<
+  { document: string | DocumentNode; variables?: any },
+  unknown,
+  Pick<ClientError, 'name' | 'message' | 'stack'>,
+  Partial<Pick<ClientError, 'request' | 'response'>>
+> => {
+  return async ({ document, variables }, { getState }: BaseQueryApi) => {
+    try {
+      const chainId = (getState() as AppState).application.chainId
+
+      let client: GraphQLClient | null = null
+
+      switch (chainId) {
+        case SupportedChainId.MAINNET:
+          client = new GraphQLClient(UNISWAP_V3_GRAPH_URL)
+          break
+        default:
+          return {
+            error: {
+              name: 'UnsupportedChainId',
+              message: `Subgraph queries again ChainId ${chainId} are not supported.`,
+              stack: '',
+            },
+          }
+      }
+
+      return { data: await client.request(document, variables), meta: {} }
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const { name, message, stack, request, response } = error
+        return { error: { name, message, stack }, meta: { request, response } }
+      }
+      throw error
+    }
+  }
+}
 
 export const client = new GraphQLClient(UNISWAP_V3_GRAPH_URL)
 export const api = createApi({
   reducerPath: 'dataApi',
-  baseQuery: graphqlRequestBaseQuery({ client }),
+  baseQuery: graphqlRequestBaseQuery(),
   endpoints: (builder) => ({
     getFeeTierDistribution: builder.query<FeeTierDistribution, { token0: string; token1: string }>({
       query: ({ token0, token1 }) => ({
