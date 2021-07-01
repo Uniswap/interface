074e9cf838099567def196993b0557f82103f968
Justin Domingue <judo@uniswap.org>
2021-07-01 15:41:18 -0700

    adjust fee selector

--- src/pages/AddLiquidity/index.tsx
+++ src/pages/AddLiquidity/index.tsx
@@ -527,6 +527,8 @@ export default function AddLiquidity({
                     disabled={!currencyB || !currencyA}
                     feeAmount={feeAmount}
                     handleFeePoolSelect={handleFeePoolSelect}
+                    token0={currencyA?.wrapped}
+                    token1={currencyB?.wrapped}
                   />
                 )}
 
