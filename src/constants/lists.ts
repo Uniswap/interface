7a9debe47393e1d3b2334bc29ccea5fceb151856
ianlapham <ianlapham@gmail.com>
2021-07-02 13:16:55 -0400

    update defaults

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,10 +3,8 @@
 const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
 const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
 const AAVE_LIST = 'tokenlist.aave.eth'
-const SYNTHETIX_LIST = 'synths.snx.eth'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
 const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
-const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
 const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const CMC_ALL_LIST = 'defi.cmc.eth'
@@ -21,11 +19,9 @@ export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   COMPOUND_LIST,
   AAVE_LIST,
-  SYNTHETIX_LIST,
   UMA_LIST,
   WRAPPED_LIST,
   SET_LIST,
-  OPYN_LIST,
   ROLL_LIST,
   COINGECKO_LIST,
   CMC_ALL_LIST,
