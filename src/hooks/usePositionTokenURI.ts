c52cd2c38ee0155bacc97cd91fc56e1dff32a07e
Moody Salem <moody.salem@gmail.com>
2021-07-06 15:59:23 -0500

    fix(position image): increase the gas required constraint for the token URI

--- src/hooks/usePositionTokenURI.ts
+++ src/hooks/usePositionTokenURI.ts
@@ -33,7 +33,7 @@ export function usePositionTokenURI(tokenId: TokenId | undefined): UsePositionTo
     () => [tokenId instanceof BigNumber ? tokenId.toHexString() : tokenId?.toString(16)],
     [tokenId]
   )
-  const { result, error, loading, valid } = useSingleCallResult(contract, 'tokenURI', inputs, NEVER_RELOAD, 1_600_000)
+  const { result, error, loading, valid } = useSingleCallResult(contract, 'tokenURI', inputs, NEVER_RELOAD, 3_000_000)
 
   return useMemo(() => {
     if (error || !valid || !tokenId) {
