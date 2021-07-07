969b002afeadbbe985bd0ecc09dd4710d24e6724
tien7668 <tien7668@gmail.com>
2021-07-07 11:52:14 +0700

    update node

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -183,7 +183,7 @@ export default function WalletModal({
       activate(connector, undefined, true).catch(error => {
         if (error instanceof UnsupportedChainIdError) {
           ;(connector as any).close()
-          activate(connector) // a little janky...can't use setError because the connector isn't set
+          // activate(connector)
         } else {
           setPendingError(true)
         }
