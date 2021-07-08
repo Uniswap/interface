4fea334b68e72a182a920abbd7406e41a9cc879c
Noah Zinsmeister <noahwz@gmail.com>
2021-07-08 12:39:52 -0400

    fix for calculateGasMargin on optimism

--- src/utils/calculateGasMargin.ts
+++ src/utils/calculateGasMargin.ts
@@ -3,7 +3,7 @@ import { SupportedChainId } from 'constants/chains'
 
 // add 20% (except on optimism)
 export function calculateGasMargin(chainId: number, value: BigNumber): BigNumber {
-  return chainId === SupportedChainId.OPTIMISTIC_KOVAN
+  return chainId === SupportedChainId.OPTIMISM || chainId === SupportedChainId.OPTIMISTIC_KOVAN
     ? value
     : value.mul(BigNumber.from(10000 + 2000)).div(BigNumber.from(10000))
 }
