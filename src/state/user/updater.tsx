a22d5b8b2a4141629e89b9b7cd4572001404989f
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-07-08 19:36:33 -0400

    switch deadline implementation

--- src/state/user/updater.tsx
+++ src/state/user/updater.tsx
@@ -1,24 +1,9 @@
-import { L2_CHAIN_IDS } from 'constants/chains'
-import { DEFAULT_DEADLINE_FROM_NOW, L2_DEADLINE_FROM_NOW } from 'constants/misc'
-import { useActiveWeb3React } from 'hooks/web3'
 import { useEffect } from 'react'
 import { useAppDispatch } from 'state/hooks'
-
 import { updateMatchesDarkMode } from './actions'
-import { useUserTransactionTTL } from './hooks'
 
 export default function Updater(): null {
   const dispatch = useAppDispatch()
-  const { chainId } = useActiveWeb3React()
-  const [, setTTL] = useUserTransactionTTL()
-
-  // keep deadline appropriate to chainId
-  useEffect(() => {
-    if (!chainId) {
-      return
-    }
-    setTTL(L2_CHAIN_IDS.includes(chainId) ? L2_DEADLINE_FROM_NOW : DEFAULT_DEADLINE_FROM_NOW)
-  }, [chainId, setTTL])
 
   // keep dark mode in sync with the system
   useEffect(() => {
