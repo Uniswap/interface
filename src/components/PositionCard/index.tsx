2554a7a9744cb7656ca224eedf076ce3a583c4fd
gab <gabriele.rigo@gmail.com>
2021-07-09 07:41:20 +0200

    fix link in position card
    - link to drago address on info site

--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -319,7 +319,7 @@ export default function FullPositionCard({ pair, border, stakedBalance }: Positi
             <ButtonSecondary padding="8px" borderRadius="8px">
               <ExternalLink
                 style={{ width: '100%', textAlign: 'center' }}
-                href={`https://v2.info.uniswap.org/account/${account}`}
+                href={`https://v2.info.uniswap.org/account/${recipient}`}
               >
                 <Trans>
                   View accrued fees and analytics<span style={{ fontSize: '11px' }}>↗</span>
