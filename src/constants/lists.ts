ce1df1201fd3274e8119d6f4584f4867f7cf9d63
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-07-10 21:41:36 -0400

    add optimism token list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -28,6 +28,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   CMC_ALL_LIST,
   CMC_STABLECOIN,
   KLEROS_LIST,
+  OPTIMISM_LIST,
   GEMINI_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
