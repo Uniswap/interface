54a5aa6604e6e5faad87297774dda32177e88be5
alexcherman <sashacherman@gmail.com>
2021-07-12 15:02:55 +0200

    Change desktop layout

--- src/components/Pool/LiquidityMiningCampaignView/Information/index.tsx
+++ src/components/Pool/LiquidityMiningCampaignView/Information/index.tsx
@@ -15,6 +15,18 @@ import Row, { RowFixed } from '../../../Row'
 import DataDisplayer from '../DataDisplayer'
 import TokenAmountDisplayer from '../TokenAmountDisplayer'
 
+const View = styled(Flex)`
+  flex-wrap: wrap;
+
+  & > *:nth-child(odd) {
+    width: 40%;
+  }
+  & > *:nth-child(even) {
+    width: 60%;
+  }
+`;
+
+
 const StyledLock = styled(Lock)`
   color: ${props => props.theme.red1};
 `
@@ -77,211 +89,207 @@ function Information({
   }, [endsAt])
 
   return (
-    <Flex justifyContent="space-between">
-      <Flex flexDirection="column">
-        <Box mb="18px">
+    <View justifyContent="space-between">
+      <Box mb="24px">
+        <DataDisplayer
+          title="REWARD PROGRAM"
+          data={
+            <Flex alignItems="center">
+              <Box mr="8px">
+                <DoubleCurrencyLogo
+                  loading={!targetedPair}
+                  size={26}
+                  currency0={targetedPair?.token0}
+                  currency1={targetedPair?.token1}
+                />
+              </Box>
+              <Box>
+                <Text fontSize="18px" fontWeight="600" lineHeight="20px">
+                  {!targetedPair ? (
+                    <Skeleton width="60px" height="18px" />
+                  ) : (
+                    `${targetedPair.token0.symbol}/${targetedPair.token1.symbol}`
+                  )}
+                </Text>
+              </Box>
+            </Flex>
+          }
+        />
+      </Box>
+      <Flex mb="24px" alignItems="center" justifyContent="flex-end">
+        <Box mr="24px">
           <DataDisplayer
-            title="REWARD PROGRAM"
+            alignTitleRight
+            title="TVL"
             data={
-              <Flex alignItems="center">
-                <Box mr="8px">
-                  <DoubleCurrencyLogo
-                    loading={!targetedPair}
-                    size={26}
-                    currency0={targetedPair?.token0}
-                    currency1={targetedPair?.token1}
-                  />
-                </Box>
-                <Box>
-                  <Text fontSize="18px" fontWeight="600" lineHeight="20px">
-                    {!targetedPair ? (
-                      <Skeleton width="60px" height="18px" />
-                    ) : (
-                      `${targetedPair.token0.symbol}/${targetedPair.token1.symbol}`
-                    )}
-                  </Text>
-                </Box>
-              </Flex>
+              !staked || loadingNativeCurrencyUSDPrice || !nativeCurrencyUSDPrice ? (
+                <Skeleton width="60px" height="14px" />
+              ) : (
+                `$${staked.nativeCurrencyAmount.multiply(nativeCurrencyUSDPrice).toFixed(2)}`
+              )
             }
           />
         </Box>
-        <Box mb="18px">
-          <DataDisplayer
-            title="MAX POOL SIZE"
-            data={
-              stakingCap ? (
-                stakingCap.equalTo('0') ? (
-                  'INFINITE'
-                ) : (
-                  stakingCap.toFixed(4)
-                )
+        <Flex mr="24px" flexDirection="column" alignItems="flex-end">
+          <Box mb="4px">
+            {!endsAt ? (
+              <Skeleton width="40px" height="14px" />
+            ) : (
+              <BadgeRoot expired={expired} upcoming={upcoming}>
+                <BadgeText expired={expired} upcoming={upcoming}>
+                  {expired ? 'EXPIRED' : upcoming ? 'UPCOMING' : 'ACTIVE'}
+                </BadgeText>
+              </BadgeRoot>
+            )}
+          </Box>
+          <Box>
+            <TYPE.small fontWeight="500" fontSize="14px">
+              {!endsAt || !startsAt ? (
+                <Skeleton width="136px" height="14px" />
               ) : (
-                <Skeleton width="60px" height="14px" />
+                <TYPE.body fontSize="14px" fontWeight="500" lineHeight="14px" color="text3">
+                  <Countdown to={upcoming ? startsAt : endsAt} />
+                </TYPE.body>
+              )}
+            </TYPE.small>
+          </Box>
+        </Flex>
+        <Box>
+          <DataDisplayer
+            alignTitleRight
+            title="APY"
+            data={!apy ? <Skeleton width="80px" height="22px" /> : `${apy.toFixed(2)}%`}
+            dataTextSize={22}
+            fontWeight={600}
+            color="white"
+          />
+        </Box>
+      </Flex>
+      <Box mb="24px">
+        <DataDisplayer
+          title="MAX POOL SIZE"
+          data={
+            stakingCap ? (
+              stakingCap.equalTo('0') ? (
+                'INFINITE'
+              ) : (
+                stakingCap.toFixed(4)
+              )
+            ) : (
+              <Skeleton width="60px" height="14px" />
+            )
+          }
+        />
+      </Box>
+      <Flex mb="24px" alignItems="flex-start" justifyContent="flex-end">
+        <Box mr="24px">
+          <DataDisplayer
+            alignTitleRight
+            title="REWARDS"
+            data={
+              !rewards ? (
+                <Row alignItems="center" justifyContent="flex-end" mb="4px">
+                  <Skeleton width="24px" height="14px" />
+                  <CurrencyLogo loading marginLeft={4} marginRight={4} size="14px" />
+                </Row>
+              ) : (
+                rewards.map(reward => (
+                  <TokenAmountDisplayer
+                    key={reward.token.address}
+                    amount={reward}
+                    fontSize="16px"
+                    alignRight
+                    showUSDValue={showUSDValue}
+                  />
+                ))
               )
             }
           />
         </Box>
         <Box>
           <DataDisplayer
-            title="POOL TYPE"
+            alignTitleRight
+            title="REMAINING"
             data={
-              <RowFixed>
-                {locked !== undefined ? (
-                  locked ? (
-                    <StyledLock size="14px" />
-                  ) : (
-                    <StyledUnlock size="14px" />
-                  )
-                ) : (
-                  <Skeleton width="14px" height="14px" />
-                )}
-                <div style={{ width: 4 }} />
-                {locked !== undefined ? (
-                  locked ? (
-                    'LOCKED STAKING'
-                  ) : (
-                    'UNLOCKED STAKING'
-                  )
-                ) : (
-                  <Skeleton width="60px" height="14px" />
-                )}
-              </RowFixed>
+              !remainingRewards ? (
+                <Row alignItems="center" justifyContent="flex-end" mb="4px">
+                  <Skeleton width="24px" height="14px" />
+                  <CurrencyLogo loading marginLeft={4} marginRight={4} size="14px" />
+                </Row>
+              ) : (
+                remainingRewards.map(remainingReward => (
+                  <TokenAmountDisplayer
+                    key={remainingReward.token.address}
+                    amount={remainingReward}
+                    fontSize="16px"
+                    alignRight
+                    showUSDValue={showUSDValue}
+                  />
+                ))
+              )
             }
           />
         </Box>
       </Flex>
-      <Flex flexDirection="column" alignItems="flex-end">
-        <Flex mb="18px">
-          <Box mr="24px">
-            <DataDisplayer
-              alignTitleRight
-              title="TVL"
-              data={
-                !staked || loadingNativeCurrencyUSDPrice || !nativeCurrencyUSDPrice ? (
-                  <Skeleton width="60px" height="14px" />
+      <Box>
+        <DataDisplayer
+          title="POOL TYPE"
+          data={
+            <RowFixed>
+              {locked !== undefined ? (
+                locked ? (
+                  <StyledLock size="14px" />
                 ) : (
-                  `$${staked.nativeCurrencyAmount.multiply(nativeCurrencyUSDPrice).toFixed(2)}`
+                  <StyledUnlock size="14px" />
                 )
-              }
-            />
-          </Box>
-          <Flex mr="24px" width="136px" flexDirection="column" alignItems="flex-end">
-            <Box mb="4px">
-              {!endsAt ? (
-                <Skeleton width="40px" height="14px" />
               ) : (
-                <BadgeRoot expired={expired} upcoming={upcoming}>
-                  <BadgeText expired={expired} upcoming={upcoming}>
-                    {expired ? 'EXPIRED' : upcoming ? 'UPCOMING' : 'ACTIVE'}
-                  </BadgeText>
-                </BadgeRoot>
+                <Skeleton width="14px" height="14px" />
               )}
-            </Box>
-            <Box>
-              <TYPE.small fontWeight="500" fontSize="14px">
-                {!endsAt || !startsAt ? (
-                  <Skeleton width="136px" height="14px" />
+              <div style={{ width: 4 }} />
+              {locked !== undefined ? (
+                locked ? (
+                  'LOCKED STAKING'
                 ) : (
-                  <TYPE.body fontSize="14px" fontWeight="500" lineHeight="14px" color="text3">
-                    <Countdown to={upcoming ? startsAt : endsAt} />
-                  </TYPE.body>
-                )}
-              </TYPE.small>
-            </Box>
-          </Flex>
-          <Box>
-            <DataDisplayer
-              alignTitleRight
-              title="APY"
-              data={!apy ? <Skeleton width="80px" height="22px" /> : `${apy.toFixed(2)}%`}
-              dataTextSize={22}
-              fontWeight={600}
-              color="white"
-            />
-          </Box>
-        </Flex>
-        <Flex mb="18px">
-          <Box mr="24px">
-            <DataDisplayer
-              alignTitleRight
-              title="REWARDS"
-              data={
-                !rewards ? (
-                  <Row alignItems="center" justifyContent="flex-end" mb="4px">
-                    <Skeleton width="24px" height="14px" />
-                    <CurrencyLogo loading marginLeft={4} marginRight={4} size="14px" />
-                  </Row>
-                ) : (
-                  rewards.map(reward => (
-                    <TokenAmountDisplayer
-                      key={reward.token.address}
-                      amount={reward}
-                      fontSize="16px"
-                      alignRight
-                      showUSDValue={showUSDValue}
-                    />
-                  ))
+                  'UNLOCKED STAKING'
                 )
-              }
-            />
-          </Box>
-          <Box>
-            <DataDisplayer
-              alignTitleRight
-              title="REMAINING"
-              data={
-                !remainingRewards ? (
-                  <Row alignItems="center" justifyContent="flex-end" mb="4px">
-                    <Skeleton width="24px" height="14px" />
-                    <CurrencyLogo loading marginLeft={4} marginRight={4} size="14px" />
-                  </Row>
-                ) : (
-                  remainingRewards.map(remainingReward => (
-                    <TokenAmountDisplayer
-                      key={remainingReward.token.address}
-                      amount={remainingReward}
-                      fontSize="16px"
-                      alignRight
-                      showUSDValue={showUSDValue}
-                    />
-                  ))
-                )
-              }
-            />
-          </Box>
-        </Flex>
-        <Flex>
-          <Box mr="24px">
-            <DataDisplayer
-              alignTitleRight
-              title="START"
-              data={
-                !startsAt ? (
-                  <Skeleton width="80px" height="10.5px" />
-                ) : (
-                  DateTime.fromSeconds(parseInt(startsAt.toString())).toFormat('dd-MM-yyyy hh:mm')
-                )
-              }
-              dataTextSize={10.5}
-            />
-          </Box>
-          <Box>
-            <DataDisplayer
-              alignTitleRight
-              title="END"
-              data={
-                !endsAt ? (
-                  <Skeleton width="80px" height="10.5px" />
-                ) : (
-                  DateTime.fromSeconds(parseInt(endsAt.toString())).toFormat('dd-MM-yyyy hh:mm')
-                )
-              }
-              dataTextSize={10.5}
-            />
-          </Box>
-        </Flex>
+              ) : (
+                <Skeleton width="60px" height="14px" />
+              )}
+            </RowFixed>
+          }
+        />
+      </Box>
+      <Flex mb="24px" alignItems="flex-start" justifyContent="flex-end">
+        <Box mr="24px">
+          <DataDisplayer
+            alignTitleRight
+            title="START"
+            data={
+              !startsAt ? (
+                <Skeleton width="80px" height="10.5px" />
+              ) : (
+                DateTime.fromSeconds(parseInt(startsAt.toString())).toFormat('dd-MM-yyyy hh:mm')
+              )
+            }
+            dataTextSize={10.5}
+          />
+        </Box>
+        <Box>
+          <DataDisplayer
+            alignTitleRight
+            title="END"
+            data={
+              !endsAt ? (
+                <Skeleton width="80px" height="10.5px" />
+              ) : (
+                DateTime.fromSeconds(parseInt(endsAt.toString())).toFormat('dd-MM-yyyy hh:mm')
+              )
+            }
+            dataTextSize={10.5}
+          />
+        </Box>
       </Flex>
-    </Flex>
+    </View>
   )
 }
 
