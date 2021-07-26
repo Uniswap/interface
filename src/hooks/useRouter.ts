b4a4321bd75bbc0545d1551632da2b9d963a8520
Justin Domingue <judo@uniswap.org>
2021-07-26 13:46:14 -0700

    integrate with v3-sdk 3.3.1

--- src/hooks/useRouter.ts
+++ src/hooks/useRouter.ts
@@ -1,14 +1,16 @@
 import { skipToken } from '@reduxjs/toolkit/query/react'
 import { Currency, CurrencyAmount, TradeType } from '@uniswap/sdk-core'
+import { Trade } from '@uniswap/v3-sdk'
 import ms from 'ms.macro'
-import { MultiRouteTrade, useGetQuoteQuery } from 'state/routing/slice'
+import { useMemo } from 'react'
+import { useGetQuoteQuery } from 'state/routing/slice'
 import { V3TradeState } from './useBestV3Trade'
 import { useActiveWeb3React } from './web3'
 
 export function useRouterTradeExactIn(amountIn?: CurrencyAmount<Currency>, currencyOut?: Currency) {
   const { account } = useActiveWeb3React()
 
-  const { isLoading, isError, data } = useGetQuoteQuery(
+  const { isLoading, isFetching, isError, data } = useGetQuoteQuery(
     amountIn && currencyOut && account
       ? {
           tokenInAddress: amountIn.currency.wrapped.address,
@@ -24,33 +26,46 @@ export function useRouterTradeExactIn(amountIn?: CurrencyAmount<Currency>, curre
 
   // todo(judo): validate block number for freshness
 
-  // augment routing api response with additional data
-  const multiRouteTrade =
-    currencyOut && data
-      ? ({
-          ...data,
-          tradeType: TradeType.EXACT_INPUT,
-          inputAmount: amountIn,
-          outputAmount: CurrencyAmount.fromRawAmount(currencyOut, data.quote),
-        } as MultiRouteTrade<Currency, Currency, TradeType.EXACT_INPUT>)
-      : null
+  return useMemo(() => {
+    if (!amountIn || !currencyOut || isError) {
+      return {
+        state: V3TradeState.INVALID,
+        trade: null,
+      }
+    }
 
-  return {
-    state: isLoading
-      ? V3TradeState.LOADING
-      : isError
-      ? V3TradeState.NO_ROUTE_FOUND
-      : !data
-      ? V3TradeState.NO_ROUTE_FOUND
-      : V3TradeState.VALID,
-    trade: multiRouteTrade,
-  }
+    if (isLoading) {
+      return {
+        state: V3TradeState.LOADING,
+        trade: null,
+      }
+    }
+
+    const amountOut = currencyOut && data ? CurrencyAmount.fromRawAmount(currencyOut, data.quote) : undefined
+
+    if (!amountOut) {
+      return {
+        state: V3TradeState.NO_ROUTE_FOUND,
+        trade: null,
+      }
+    }
+
+    const trade = Trade.createUncheckedTradeWithMultipleRoutes<Currency, Currency, TradeType.EXACT_INPUT>({
+      routes: [],
+      tradeType: TradeType.EXACT_INPUT,
+    })
+
+    return {
+      state: isFetching ? V3TradeState.SYNCING : V3TradeState.VALID,
+      trade: trade,
+    }
+  }, [amountIn, currencyOut, isError, isLoading, data, isFetching])
 }
 
 export function useRouterTradeExactOut(currencyIn?: Currency, amountOut?: CurrencyAmount<Currency>) {
   const { account } = useActiveWeb3React()
 
-  const { isLoading, isError, data } = useGetQuoteQuery(
+  const { isLoading, isFetching, isError, data } = useGetQuoteQuery(
     amountOut && currencyIn && account
       ? {
           tokenInAddress: currencyIn.wrapped.address,
@@ -66,25 +81,36 @@ export function useRouterTradeExactOut(currencyIn?: Currency, amountOut?: Curren
 
   // todo(judo): validate block number for freshness
 
-  // augment routing api response with additional data
-  const multiRouteTrade =
-    currencyIn && data
-      ? ({
-          ...data,
-          tradeType: TradeType.EXACT_OUTPUT,
-          inputAmount: CurrencyAmount.fromRawAmount(currencyIn, data.quote),
-          outputAmount: amountOut,
-        } as MultiRouteTrade<Currency, Currency, TradeType.EXACT_OUTPUT>)
-      : null
+  return useMemo(() => {
+    if (!amountOut || !currencyIn || isError) {
+      return {
+        state: V3TradeState.INVALID,
+        trade: null,
+      }
+    }
 
-  return {
-    state: isLoading
-      ? V3TradeState.LOADING
-      : isError
-      ? V3TradeState.NO_ROUTE_FOUND
-      : !data
-      ? V3TradeState.NO_ROUTE_FOUND
-      : V3TradeState.VALID,
-    trade: multiRouteTrade,
-  }
+    if (isLoading) {
+      return {
+        state: V3TradeState.LOADING,
+        trade: null,
+      }
+    }
+
+    if (!currencyIn) {
+      return {
+        state: V3TradeState.NO_ROUTE_FOUND,
+        trade: null,
+      }
+    }
+
+    const trade = Trade.createUncheckedTradeWithMultipleRoutes<Currency, Currency, TradeType.EXACT_OUTPUT>({
+      routes: [],
+      tradeType: TradeType.EXACT_OUTPUT,
+    })
+
+    return {
+      state: isFetching ? V3TradeState.SYNCING : V3TradeState.VALID,
+      trade: trade,
+    }
+  }, [amountOut, currencyIn, isError, isLoading, isFetching])
 }
