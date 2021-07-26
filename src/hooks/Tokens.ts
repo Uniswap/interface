c5d2bc03ddc6710750c4f1e8f27c4dc8273366b4
Moody Salem <moody.salem@gmail.com>
2021-07-26 10:58:31 -0500

    revert changes to Tokens.ts

--- src/hooks/Tokens.ts
+++ src/hooks/Tokens.ts
@@ -179,10 +179,8 @@ export function useCurrency(currencyId: string | undefined): Currency | null | u
   const { chainId } = useActiveWeb3React()
   const isETH = currencyId?.toUpperCase() === 'ETH'
   const token = useToken(isETH ? undefined : currencyId)
-  if (!chainId) return undefined
-
-  const ether = ExtendedEther.onChain(chainId)
-  const weth = WETH9_EXTENDED[chainId]
+  const extendedEther = useMemo(() => (chainId ? ExtendedEther.onChain(chainId) : undefined), [chainId])
+  const weth = chainId ? WETH9_EXTENDED[chainId] : undefined
   if (weth?.address?.toLowerCase() === currencyId?.toLowerCase()) return weth
-  return isETH ? ether : token
+  return isETH ? extendedEther : token
 }
