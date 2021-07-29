926bcd00885edd986a326c7795ad730f8eee4e79
Federico Luzzi <fedeluzzi00@gmail.com>
2021-07-29 13:08:55 +0200

    v1.0.0-beta.3

--- src/state/application/reducer.ts
+++ src/state/application/reducer.ts
@@ -27,6 +27,7 @@ const initialState: ApplicationState = {
 export default createReducer(initialState, builder =>
   builder
     .addCase(updateBlockNumber, (state, action) => {
+      console.log('new block')
       const { chainId, blockNumber } = action.payload
       if (typeof state.blockNumber[chainId] !== 'number') {
         state.blockNumber[chainId] = blockNumber
