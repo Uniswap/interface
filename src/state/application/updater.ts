beb1bf3bdceb9ce5b33ce6789553b51dbfb6c20e
Lint Action <lint-action@samuelmeuli.com>
2021-07-29 17:26:43 +0000

    Fix code style issues with ESLint

--- src/state/application/updater.ts
+++ src/state/application/updater.ts
@@ -9,12 +9,11 @@ import { updateBlockNumber, updateChainId } from './actions'
 
 function useQueryCacheInvalidator() {
   const dispatch = useAppDispatch()
-  
+
   // subscribe to `chainId` changes in the redux store rather than Web3
   // this will ensure that when `invalidateTags` is called, the latest
   // `chainId` is available in redux to build the subgraph url
   const chainId = useAppSelector((state) => state.application.chainId)
-
 
   useEffect(() => {
     dispatch(api.util.invalidateTags([CHAIN_TAG]))
