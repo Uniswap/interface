aacda948f103a6a70c3795e36077a4af35f4dd52
Justin Domingue <judo@uniswap.org>
2021-07-29 14:58:01 -0700

    fixed tests

--- src/components/RoutingDiagram/RoutingDiagram.test.tsx
+++ src/components/RoutingDiagram/RoutingDiagram.test.tsx
@@ -5,7 +5,7 @@ import { FeeAmount } from '@uniswap/v3-sdk'
 import { render } from 'test-utils'
 import useHttpLocations from 'hooks/useHttpLocations'
 
-const percent = (strings: TemplateStringsArray) => new Percent(parseInt(strings[0], 1000))
+const percent = (strings: TemplateStringsArray) => new Percent(parseInt(strings[0]), 1000)
 
 const USDC = new Token(1, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC')
 const DAI = new Token(1, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 6, 'DAI')
