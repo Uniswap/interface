bf01b0d342dc2490cc3b478f5e9afe4464a7d60a
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-08-02 10:50:46 -0400

    fix: optimism link and some copy changes (#2126)
    * fix: optimism link
    
    * fix: (un)planned downtime copy changes

--- src/components/NetworkAlert/styles.ts
+++ src/components/NetworkAlert/styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components/macro'
 import { ExternalLink } from 'theme'
 
 export const ReadMoreLink = styled(ExternalLink)`
-  color: white;
+  color: ${({ theme }) => theme.text1};
   text-decoration: underline;
 `
