fa12fc3ca77312af6532657cc53ac7269e8ce08e
Federico Luzzi <fedeluzzi00@gmail.com>
2021-08-15 19:10:11 +0200

    feat: make has claimed hook use multicall to support ui automatic update

--- src/hooks/swpr/useHasClaimed.ts
+++ src/hooks/swpr/useHasClaimed.ts
@@ -1,27 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useMemo } from 'react'
+import { useSingleCallResult } from '../../state/multicall/hooks'
 import { useSWPRClaimerContract } from '../useContract'
 
 export default function useHasClaimed(account: string | null | undefined): { loading: boolean; claimed: boolean } {
   const swprClaimerContract = useSWPRClaimerContract()
-  const [claimed, setClaimed] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const result = useSingleCallResult(swprClaimerContract, 'claimed', account ? [account] : [])
 
-  useEffect(() => {
-    if (!account || !swprClaimerContract) {
-      setClaimed(true)
-      setLoading(false)
-      return
-    }
-    setLoading(true)
-    try {
-      swprClaimerContract
-        .claimed(account)
-        .then(setClaimed)
-        .catch(console.error)
-    } finally {
-      setLoading(false)
-    }
-  }, [account, swprClaimerContract])
-
-  return { loading, claimed }
+  return useMemo(() => {
+    if (!account || !swprClaimerContract) return { loading: false, claimed: false }
+    if (result.loading || result.error) return { loading: true, claimed: false }
+    return { loading: false, claimed: result.result?.[0] }
+  }, [account, result, swprClaimerContract])
 }
