b536cf162bfcac77a968527f279ad97b7c12205f
Zach Pomerantz <zzmp@uniswap.org>
2021-08-20 12:21:05 -0400

    chore: limit service-worker caching

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,10 +2,8 @@
 /* eslint-disable no-restricted-globals */
 
 import { clientsClaim } from 'workbox-core'
-import { ExpirationPlugin } from 'workbox-expiration'
 import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
-import { StaleWhileRevalidate } from 'workbox-strategies'
 
 declare const self: ServiceWorkerGlobalScope
 
@@ -28,11 +26,17 @@ const manifest = self.__WB_MANIFEST.filter((entry) => {
 })
 precacheAndRoute(manifest)
 
-// Set up App Shell-style routing, so that all navigation requests
-// are fulfilled immediately with your index.html shell. See
+// Set up App Shell-style routing, so that navigation requests are fulfilled
+// immediately with a local index.html shell. See
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$')
 registerRoute(({ request, url }: { request: Request; url: URL }) => {
+  // If this isn't app.uniswap.org, skip. IPFS gateways may not have domain
+  // separation, so they cannot use App Shell-style routing.
+  if (url.hostname !== 'app.uniswap.org') {
+    return false
+  }
+
   // If this isn't a navigation, skip.
   if (request.mode !== 'navigate') {
     return false
@@ -45,21 +49,3 @@ registerRoute(({ request, url }: { request: Request; url: URL }) => {
 
   return true
 }, createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'))
-
-// Cache any public/ images (eg logos).
-registerRoute(
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
-  new StaleWhileRevalidate({
-    cacheName: 'logos',
-    plugins: [new ExpirationPlugin({ maxEntries: 5 })],
-  })
-)
-
-// Cache any token logos.
-registerRoute(
-  ({ url }) => url.pathname.endsWith('.png') || url.pathname.endsWith('.svg'),
-  new StaleWhileRevalidate({
-    cacheName: 'tokens',
-    plugins: [new ExpirationPlugin({ maxEntries: 50 })],
-  })
-)
