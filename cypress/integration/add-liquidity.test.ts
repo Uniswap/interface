e83b355a57fdb63fffec86ebae04e880c6d2d324
Zach Pomerantz <zzmp@uniswap.org>
2021-08-30 19:10:20 -0700

    fix: differentiate liquidity fixtures

--- cypress/integration/add-liquidity.test.ts
+++ cypress/integration/add-liquidity.test.ts
@@ -25,27 +25,35 @@ describe('Add Liquidity', () => {
   })
 
   describe('with subgraph', () => {
-    it('loads fee tier distribution', () => {
-      cy.intercept('POST', '/subgraphs/name/uniswap/uniswap-v3', { fixture: 'subgraph/feeTierDistribution.json' }).as(
-        'getFeeTierDistributionQuery'
-      )
+    const TEST_HEADER = 'x-test'
+    const FEE_TIER_DISTRIBUTION = 'feeTierDistribution'
+    const ALL_V3_TICKS = 'allV3Ticks'
 
+    beforeEach(() => {
+      cy.intercept('POST', '/subgraphs/name/uniswap/uniswap-v3', (req) => {
+        if (/feeTierDistribution/.test(req.body.query)) {
+          req.alias = 'queryFeeTierDistribution'
+          req.reply({fixture: 'subgraph/feeTierDistribution.json'})
+        } else if (/allV3Ticks/.test(req.body.query)) {
+          req.alias = 'queryAllV3Ticks'
+          req.reply({fixture: 'subgraph/allV3Ticks.json'})
+        }
+      })
+    })
+
+    it('loads fee tier distribution', () => {
       cy.visit('/add/0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85/0xc778417E063141139Fce010982780140Aa0cD5Ab')
 
-      cy.wait('@getFeeTierDistributionQuery')
+      cy.wait('@queryFeeTierDistribution')
 
       cy.get('[data-test-id=selected-fee-label]').should('contain.text', '0.3% fee tier')
       cy.get('[data-test-id=selected-fee-percentage]').should('contain.text', '70%')
     })
 
     it('loads ticks data', () => {
-      cy.intercept('POST', '/subgraphs/name/uniswap/uniswap-v3', { fixture: 'subgraph/v3Ticks.json' }).as(
-        'getV3TicksQuery'
-      )
-
       cy.visit('/add/0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85/0xc778417E063141139Fce010982780140Aa0cD5Ab/3000')
 
-      cy.wait('@getV3TicksQuery')
+      cy.wait('@queryAllV3Ticks')
 
       // TODO: test ticks data (requires stubbing infura pool.tickCurrent)
       cy.get('[data-test-id=liquidity-chart-loader]').should('exist')
