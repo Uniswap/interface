d030c778b36ea166cf03049b47c97a8356924d37
Justin Domingue <judo@uniswap.org>
2021-09-01 10:27:52 -0400

    optimize tradeBetter hook

--- src/hooks/useIsV2orV3TradeBetter.ts
+++ src/hooks/useIsV2orV3TradeBetter.ts
@@ -1,3 +1,4 @@
+import { useEffect, useMemo, useState } from 'react'
 import { useActiveWeb3React } from 'hooks/web3'
 import { Router, Trade as V2Trade } from '@uniswap/v2-sdk'
 import { FeeAmount, Pool, Trade as V3Trade } from '@uniswap/v3-sdk'
@@ -33,6 +34,8 @@ export async function useCompareGasAdjustedV2TradeandV3Trade(
   const v2TradeApprovalGasEstimate = BigNumber.from(10000) // TODO
   const v3TradeApprovalGasEstimate = BigNumber.from(10000) // TODO
 
+  const [v2SwapGasEstimate, setV2SwapGasEstimate] = useState<BigNumber | undefined>()
+
   const tradeType = v2Trade.tradeType
   const quoteToken = tradeType == TradeType.EXACT_INPUT ? v2Trade.outputAmount.currency : v2Trade.inputAmount.currency
   const weth = chainId ? WETH9_EXTENDED[chainId] : undefined
@@ -43,85 +46,104 @@ export async function useCompareGasAdjustedV2TradeandV3Trade(
     [quoteToken, weth, FeeAmount.LOW],
   ])
 
-  if (!v2Trade || !deadline || !library || !chainId || !routerContract || !weth) {
-    return undefined
-  }
+  useEffect(() => {
+    if (!library || !routerContract) return
 
-  const sampleSlippage = new Percent(5, 100)
-  const sampleRecipient = '0x0'
-  const sampleDeadline = deadline.toNumber()
+    const sampleSlippage = new Percent(5, 100)
+    const sampleRecipient = '0x0'
+    const sampleDeadline = deadline?.toNumber() ?? 60
 
-  const { methodName, args, value } = Router.swapCallParameters(v2Trade, {
-    feeOnTransfer: false,
-    allowedSlippage: sampleSlippage,
-    recipient: sampleRecipient,
-    deadline: sampleDeadline,
-  })
+    const { methodName, args, value } = Router.swapCallParameters(v2Trade, {
+      feeOnTransfer: false,
+      allowedSlippage: sampleSlippage,
+      recipient: sampleRecipient,
+      deadline: sampleDeadline,
+    })
 
-  const address = routerContract.address
-  const calldata = routerContract.interface.encodeFunctionData(methodName, args)
+    const address = routerContract.address
+    const calldata = routerContract.interface.encodeFunctionData(methodName, args)
 
-  const tx =
-    !value || isZero(value)
-      ? { /* from: account,  */ to: address, data: calldata }
-      : {
-          /* from: account, */
-          to: address,
-          data: calldata,
-          value,
-        }
+    const tx =
+      !value || isZero(value)
+        ? { /* from: account,  */ to: address, data: calldata }
+        : {
+            /* from: account, */
+            to: address,
+            data: calldata,
+            value,
+          }
 
-  const v2SwapGasEstimate = await library.estimateGas(tx)
-  const v2GasEstimate = v2SwapGasEstimate.add(v2TradeApprovalGasEstimate)
-  console.log(
-    { swapGasEstimate: v2SwapGasEstimate.toString(), approvalGasEstimate: v2TradeApprovalGasEstimate.toString() },
-    'Got gas estimates for V2 trade'
-  )
+    library.estimateGas(tx).then((gasEstimate) => setV2SwapGasEstimate(gasEstimate))
+  }, [deadline, library, routerContract, v2Trade])
 
-  let v2TradeAndApprovalGasCostInQuoteToken
-  let v3ApprovalGasCostInQuoteToken
-  if (weth && (quoteToken.equals(weth) || quoteToken.isNative)) {
-    v2TradeAndApprovalGasCostInQuoteToken = CurrencyAmount.fromRawAmount(
-      quoteToken,
-      v2GasEstimate.mul(gasPrice).toString()
-    )
-    v3ApprovalGasCostInQuoteToken = CurrencyAmount.fromRawAmount(
-      quoteToken,
-      v3TradeApprovalGasEstimate.mul(gasPrice).toString()
-    )
-  } else {
-    let ethPool: Pool | null = null
-    for (const [, pool] of pools) {
-      if (!ethPool || (pool && JSBI.greaterThan(pool?.liquidity, ethPool.liquidity))) {
-        ethPool = pool
-      }
+  return useMemo(() => {
+    if (!v2Trade || !weth || !v2SwapGasEstimate) {
+      return undefined
     }
 
-    if (!ethPool) {
-      console.debug(
-        `Unable to find ${quoteToken.symbol}/WETH pool to compute gas adjusted amount for V2. Assuming V3 trade is better.`
+    const v2GasEstimate = v2SwapGasEstimate.add(v2TradeApprovalGasEstimate)
+    console.log(
+      { swapGasEstimate: v2SwapGasEstimate.toString(), approvalGasEstimate: v2TradeApprovalGasEstimate.toString() },
+      'Got gas estimates for V2 trade'
+    )
+
+    let v2TradeAndApprovalGasCostInQuoteToken
+    let v3ApprovalGasCostInQuoteToken
+    if (weth && (quoteToken.equals(weth) || quoteToken.isNative)) {
+      v2TradeAndApprovalGasCostInQuoteToken = CurrencyAmount.fromRawAmount(
+        quoteToken,
+        v2GasEstimate.mul(gasPrice).toString()
       )
-      return true
+      v3ApprovalGasCostInQuoteToken = CurrencyAmount.fromRawAmount(
+        quoteToken,
+        v3TradeApprovalGasEstimate.mul(gasPrice).toString()
+      )
+    } else {
+      let ethPool: Pool | null = null
+      for (const [, pool] of pools) {
+        if (!ethPool || (pool && JSBI.greaterThan(pool?.liquidity, ethPool.liquidity))) {
+          ethPool = pool
+        }
+      }
+
+      if (!ethPool) {
+        console.debug(
+          `Unable to find ${quoteToken.symbol}/WETH pool to compute gas adjusted amount for V2. Assuming V3 trade is better.`
+        )
+        return true
+      }
+
+      const ethToken0 = ethPool.token0.address == weth?.address
+      const ethTokenPrice = ethToken0 ? ethPool.token0Price : ethPool.token1Price
+      const v2TradeGasCostInEth = CurrencyAmount.fromRawAmount(weth, v2SwapGasEstimate.mul(gasPrice).toString())
+      const v3TradeApprovalCostInEth = CurrencyAmount.fromRawAmount(
+        weth,
+        v3TradeApprovalGasEstimate.mul(gasPrice).toString()
+      )
+      v2TradeAndApprovalGasCostInQuoteToken = ethTokenPrice.quote(v2TradeGasCostInEth)
+      v3ApprovalGasCostInQuoteToken = ethTokenPrice.quote(v3TradeApprovalCostInEth)
     }
 
-    const ethToken0 = ethPool.token0.address == weth?.address
-    const ethTokenPrice = ethToken0 ? ethPool.token0Price : ethPool.token1Price
-    const v2TradeGasCostInEth = CurrencyAmount.fromRawAmount(weth, v2SwapGasEstimate.mul(gasPrice).toString())
-    const v3TradeApprovalCostInEth = CurrencyAmount.fromRawAmount(
-      weth,
-      v3TradeApprovalGasEstimate.mul(gasPrice).toString()
-    )
-    v2TradeAndApprovalGasCostInQuoteToken = ethTokenPrice.quote(v2TradeGasCostInEth)
-    v3ApprovalGasCostInQuoteToken = ethTokenPrice.quote(v3TradeApprovalCostInEth)
-  }
-
-  if (tradeType == TradeType.EXACT_INPUT) {
-    const v3QuoteGasAdjusted = v3TradeGasAdjusted.outputAmount.subtract(v3ApprovalGasCostInQuoteToken)
-    const v2QuoteGasAdjusted = v2Trade.outputAmount.subtract(v2TradeAndApprovalGasCostInQuoteToken)
-    return v3QuoteGasAdjusted.greaterThan(v2QuoteGasAdjusted)
-  } else {
-    const v3QuoteGasAdjusted = v3TradeGasAdjusted.inputAmount.add(v3ApprovalGasCostInQuoteToken)
-    const v2QuoteGasAdjusted = v2Trade.inputAmount.add(v2TradeAndApprovalGasCostInQuoteToken)
-    return v2QuoteGasAdjusted.greaterThan(v3QuoteGasAdjusted)
-  }
+    if (tradeType == TradeType.EXACT_INPUT) {
+      const v3QuoteGasAdjusted = v3TradeGasAdjusted.outputAmount.subtract(v3ApprovalGasCostInQuoteToken)
+      const v2QuoteGasAdjusted = v2Trade.outputAmount.subtract(v2TradeAndApprovalGasCostInQuoteToken)
+      return v3QuoteGasAdjusted.greaterThan(v2QuoteGasAdjusted)
+    } else {
+      const v3QuoteGasAdjusted = v3TradeGasAdjusted.inputAmount.add(v3ApprovalGasCostInQuoteToken)
+      const v2QuoteGasAdjusted = v2Trade.inputAmount.add(v2TradeAndApprovalGasCostInQuoteToken)
+      return v2QuoteGasAdjusted.greaterThan(v3QuoteGasAdjusted)
+    }
+  }, [
+    gasPrice,
+    pools,
+    quoteToken,
+    tradeType,
+    v2SwapGasEstimate,
+    v2Trade,
+    v2TradeApprovalGasEstimate,
+    v3TradeApprovalGasEstimate,
+    v3TradeGasAdjusted.inputAmount,
+    v3TradeGasAdjusted.outputAmount,
+    weth,
+  ])
 }
