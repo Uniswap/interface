43eea2d9ebc1d81732b94e8af526b96b995a997d
Justin Domingue <judo@uniswap.org>
2021-09-02 11:01:07 -0400

    add loadingbar

---
+++ src/components/Loader/LoadingBar.tsx
@@ -0,0 +1,21 @@
+import styled, { keyframes } from 'styled-components/macro'
+
+const pulse = keyframes`
+  0% {
+    opacity: 0.7;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.7;
+  }
+`
+
+export const LoadingBar = styled.div<{ width: number; height: number }>`
+  animation: ${pulse} 1s ease-in-out infinite alternate;
+  background-color: ${({ theme }) => theme.bg3};
+  border-radius: 0.125rem;
+  height: ${({ height }) => `${height}px`};
+  width: ${({ width }) => `${width}px`};
+`
