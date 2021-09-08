89250f7bae90f1eb441d00f8df4d729dc6ebac59
Federico Luzzi <fedeluzzi00@gmail.com>
2021-09-08 21:22:38 +0200

    fix: change clients from arb1 to arb rinkeby

--- src/hooks/swpr/useHasClaimed.ts
+++ src/hooks/swpr/useHasClaimed.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { ARBITRUM_ONE_PROVIDER } from '../../constants'
+import { ARBITRUM_RINKEBY_PROVIDER } from '../../constants'
 import { useClaimTxConfirmed } from '../../state/claim/hooks'
 import { useSWPRClaimerContract } from '../useContract'
 
@@ -11,9 +11,9 @@ export default function useHasClaimed(account: string | null | undefined): { loa
   const claimTxConfirmed = useClaimTxConfirmed()
 
   useEffect(() => {
-    ARBITRUM_ONE_PROVIDER.on('block', setLatestBlockNumber)
+    ARBITRUM_RINKEBY_PROVIDER.on('block', setLatestBlockNumber) // TODO: change to Arb1
     return () => {
-      ARBITRUM_ONE_PROVIDER.removeListener('block', setLatestBlockNumber)
+      ARBITRUM_RINKEBY_PROVIDER.removeListener('block', setLatestBlockNumber) // TODO: change to Arb1
     }
   }, [])
 
