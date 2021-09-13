21c612ea3ea30170b15226b6e45a44ac6f16e922
Josh Jones <joshjones@wearealexander.com>
2021-09-13 10:11:25 -0500

    add new bg and make it default.

--- src/pages/ThemedBg/ThemedBg.tsx
+++ src/pages/ThemedBg/ThemedBg.tsx
@@ -7,7 +7,7 @@ import { Image, User } from 'react-feather'
 import { TYPE } from 'theme'
 import { CardSection } from 'components/earn/styled'
 import { OutlineCard } from 'components/Card'
-export const THEME_BG_KEY = 'themedBG'
+export const THEME_BG_KEY = 'themed_BG'
 
 export const ThemedBg = ({ theme, setTheme }: { theme: any; setTheme: (theme: string) => void }) => {
   console.log(theme, setTheme)
@@ -16,7 +16,7 @@ export const ThemedBg = ({ theme, setTheme }: { theme: any; setTheme: (theme: st
       <OutlineCard>
       <span style={{textAlign: 'center', fontSize:22, color: "#fff"}}>Choose a custom background</span>
       <ul style={{background:'transparent', padding: 20, cursor: 'pointer'}}>
-        {['./trump.mp4', './trumpspace.mp4', './video.mp4'].map((key) => (
+        {['./squeeze.mp4', './trumpspace.mp4', './video.mp4'].map((key) => (
           <li onClick={(e) => setTheme(key)} style={{ listStyle: 'none', padding: 15, margin: 10 }} key={key}>
             <video style={{ maxWidth: 400, border: theme === key ? '1px solid green' : '1px solid #eee' }}>
               <source src={key} type="video/mp4" />
