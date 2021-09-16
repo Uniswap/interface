1314477a884e1df07b43ebd1420b969ba5591693
Justin Domingue <judo@uniswap.org>
2021-09-16 14:16:19 -0400

    add static route svg (#2373)

--- src/components/swap/RouterLabel.tsx
+++ src/components/swap/RouterLabel.tsx
@@ -3,17 +3,25 @@ import { useRoutingAPIEnabled } from 'state/user/hooks'
 import styled from 'styled-components/macro'
 import { TYPE } from 'theme'
 import { ReactComponent as AutoRouterIcon } from '../../assets/svg/auto_router.svg'
+import { ReactComponent as StaticRouterIcon } from '../../assets/svg/static_route.svg'
 
 const StyledAutoRouterIcon = styled(AutoRouterIcon)`
   height: 16px;
   width: 16px;
-  stroke: ${({ theme }) => theme.blue1};
+
+  :hover {
+    filter: brightness(1.3);
+  }
 `
 
-const DisabledAutoRouterIcon = styled(StyledAutoRouterIcon)`
-  stroke: ${({ theme }) => theme.text3};
+const StyledStaticRouterIcon = styled(StaticRouterIcon)`
+  height: 16px;
+  width: 16px;
+
+  fill: ${({ theme }) => theme.text3};
+
   :hover {
-    stroke: ${({ theme }) => theme.text1};
+    filter: brightness(1.3);
   }
 `
 
@@ -33,7 +41,7 @@ const StyledAutoRouterLabel = styled(TYPE.black)`
 export function AutoRouterLogo() {
   const routingAPIEnabled = useRoutingAPIEnabled()
 
-  return routingAPIEnabled ? <StyledAutoRouterIcon /> : <DisabledAutoRouterIcon />
+  return routingAPIEnabled ? <StyledAutoRouterIcon /> : <StyledStaticRouterIcon />
 }
 
 export function AutoRouterLabel() {
