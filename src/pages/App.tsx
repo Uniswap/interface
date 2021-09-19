732e9cc35066ad8ee40e291db99572f83816fbfe
chenxsan <chenxsan@gmail.com>
2021-09-19 20:47:35 +0800

    lazy load routes to reduce initial bundle size

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import ApeModeQueryParamReader from 'hooks/useApeModeQueryParamReader'
 import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components/macro'
@@ -11,24 +12,37 @@ import Web3ReactManager from '../components/Web3ReactManager'
 import { ApplicationModal } from '../state/application/actions'
 import { useModalOpen, useToggleModal } from '../state/application/hooks'
 import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
-import AddLiquidity from './AddLiquidity'
-import { RedirectDuplicateTokenIds } from './AddLiquidity/redirects'
-import { RedirectDuplicateTokenIdsV2 } from './AddLiquidityV2/redirects'
-import CreateProposal from './CreateProposal'
-import Earn from './Earn'
-import Manage from './Earn/Manage'
-import MigrateV2 from './MigrateV2'
-import MigrateV2Pair from './MigrateV2/MigrateV2Pair'
-import Pool from './Pool'
-import { PositionPage } from './Pool/PositionPage'
-import PoolV2 from './Pool/v2'
-import PoolFinder from './PoolFinder'
-import RemoveLiquidity from './RemoveLiquidity'
-import RemoveLiquidityV3 from './RemoveLiquidity/V3'
 import Swap from './Swap'
-import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
-import Vote from './Vote'
-import VotePage from './Vote/VotePage'
+
+const { lazy, Suspense } = React
+
+const AddLiquidity = lazy(() => import('./AddLiquidity'))
+const CreateProposal = lazy(() => import('./CreateProposal'))
+const Earn = lazy(() => import('./Earn'))
+const Manage = lazy(() => import('./Earn/Manage'))
+const MigrateV2 = lazy(() => import('./MigrateV2'))
+const MigrateV2Pair = lazy(() => import('./MigrateV2/MigrateV2Pair'))
+const OpenClaimAddressModalAndRedirectToSwap = lazy(() =>
+  import('./Swap/redirects').then((module) => ({ default: module.OpenClaimAddressModalAndRedirectToSwap }))
+)
+const RedirectToSwap = lazy(() => import('./Swap/redirects').then((module) => ({ default: module.RedirectToSwap })))
+const Pool = lazy(() => import('./Pool'))
+const PoolFinder = lazy(() => import('./PoolFinder'))
+const PoolV2 = lazy(() => import('./Pool/v2'))
+const PositionPage = lazy(() => import('./Pool/PositionPage').then((module) => ({ default: module.PositionPage })))
+const RedirectDuplicateTokenIds = lazy(() =>
+  import('./AddLiquidity/redirects').then((module) => ({ default: module.RedirectDuplicateTokenIds }))
+)
+const RedirectDuplicateTokenIdsV2 = lazy(() =>
+  import('./AddLiquidityV2/redirects').then((module) => ({ default: module.RedirectDuplicateTokenIdsV2 }))
+)
+const RedirectPathToSwapOnly = lazy(() =>
+  import('./Swap/redirects').then((module) => ({ default: module.RedirectPathToSwapOnly }))
+)
+const RemoveLiquidity = lazy(() => import('./RemoveLiquidity'))
+const RemoveLiquidityV3 = lazy(() => import('./RemoveLiquidity/V3'))
+const Vote = lazy(() => import('./Vote'))
+const VotePage = lazy(() => import('./Vote/VotePage'))
 
 const AppWrapper = styled.div`
   display: flex;
@@ -84,46 +98,51 @@ export default function App() {
             <Popups />
             <Polling />
             <TopLevelModals />
-            <Switch>
-              <Route exact strict path="/vote" component={Vote} />
-              <Route exact strict path="/vote/:governorIndex/:id" component={VotePage} />
-              <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
-              <Route exact strict path="/uni" component={Earn} />
-              <Route exact strict path="/uni/:currencyIdA/:currencyIdB" component={Manage} />
+            <Suspense fallback={<div>loading...</div>}>
+              <Switch>
+                <Route exact strict path="/vote" component={Vote} />
+                <Route exact strict path="/vote/:governorIndex/:id" component={VotePage} />
+                <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
+                <Route exact strict path="/uni" component={Earn} />
+                <Route exact strict path="/uni/:currencyIdA/:currencyIdB" component={Manage} />
 
-              <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
-              <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
-              <Route exact strict path="/swap" component={Swap} />
+                <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
+                <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
+                <Route exact strict path="/swap" component={Swap} />
+                <Route exact strict path="/pool/v2/find" component={PoolFinder} />
+                <Route exact strict path="/pool/v2" component={PoolV2} />
+                <Route exact strict path="/pool" component={Pool} />
+                <Route exact strict path="/pool/:tokenId" component={PositionPage} />
+                <Route
+                  exact
+                  strict
+                  path="/add/v2/:currencyIdA?/:currencyIdB?"
+                  component={RedirectDuplicateTokenIdsV2}
+                />
+                <Route
+                  exact
+                  strict
+                  path="/add/:currencyIdA?/:currencyIdB?/:feeAmount?"
+                  component={RedirectDuplicateTokenIds}
+                />
 
-              <Route exact strict path="/pool/v2/find" component={PoolFinder} />
-              <Route exact strict path="/pool/v2" component={PoolV2} />
-              <Route exact strict path="/pool" component={Pool} />
-              <Route exact strict path="/pool/:tokenId" component={PositionPage} />
+                <Route
+                  exact
+                  strict
+                  path="/increase/:currencyIdA?/:currencyIdB?/:feeAmount?/:tokenId?"
+                  component={AddLiquidity}
+                />
 
-              <Route exact strict path="/add/v2/:currencyIdA?/:currencyIdB?" component={RedirectDuplicateTokenIdsV2} />
-              <Route
-                exact
-                strict
-                path="/add/:currencyIdA?/:currencyIdB?/:feeAmount?"
-                component={RedirectDuplicateTokenIds}
-              />
+                <Route exact strict path="/remove/v2/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
+                <Route exact strict path="/remove/:tokenId" component={RemoveLiquidityV3} />
 
-              <Route
-                exact
-                strict
-                path="/increase/:currencyIdA?/:currencyIdB?/:feeAmount?/:tokenId?"
-                component={AddLiquidity}
-              />
+                <Route exact strict path="/migrate/v2" component={MigrateV2} />
+                <Route exact strict path="/migrate/v2/:address" component={MigrateV2Pair} />
 
-              <Route exact strict path="/remove/v2/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-              <Route exact strict path="/remove/:tokenId" component={RemoveLiquidityV3} />
-
-              <Route exact strict path="/migrate/v2" component={MigrateV2} />
-              <Route exact strict path="/migrate/v2/:address" component={MigrateV2Pair} />
-
-              <Route exact strict path="/create-proposal" component={CreateProposal} />
-              <Route component={RedirectPathToSwapOnly} />
-            </Switch>
+                <Route exact strict path="/create-proposal" component={CreateProposal} />
+                <Route component={RedirectPathToSwapOnly} />
+              </Switch>
+            </Suspense>
             <Marginer />
           </BodyWrapper>
         </AppWrapper>
