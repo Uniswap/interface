0de133a771b5403c3bbf269ace858a167e20b261
Federico Luzzi <fedeluzzi00@gmail.com>
2021-09-21 14:44:14 +0200

    feat: always show tooltip

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -24,17 +24,13 @@ export function CustomTooltip({ content, ...rest }: PopoverProps) {
   return <Popover offsetY={3} placement={'bottom'} content={content} {...rest} />
 }
 
-interface MouseoverTooltipProps extends TooltipProps {
-  disabled?: boolean
-}
-
-export function MouseoverTooltip({ children, content, disabled, ...rest }: Omit<MouseoverTooltipProps, 'show'>) {
+export function MouseoverTooltip({ children, content, ...rest }: Omit<TooltipProps, 'show'>) {
   const [show, setShow] = useState(false)
   const open = useCallback(() => setShow(true), [setShow])
   const close = useCallback(() => setShow(false), [setShow])
   return (
     <CustomTooltip content={content} {...rest} show={show}>
-      <CursorPointerDiv onMouseEnter={disabled ? undefined : open} onMouseLeave={disabled ? undefined : close}>
+      <CursorPointerDiv onMouseEnter={open} onMouseLeave={close}>
         {children}
       </CursorPointerDiv>
     </CustomTooltip>
