95bf310c5e8ce9eef78f4bd1646d94f1cad2fe36
Zach Pomerantz <zzmp@uniswap.org>
2021-09-22 16:17:05 -0700

    perf: rm duplicated ua-parser
    react-device-detect was only used to detect mobile devices, which duplicates ua-parser. react-device-detect wraps ua-parser.min.js, so ua-parser was being imported twice.
    Re-using ua-parser to detect mobile devices (see src/utils/userAgent.ts) reduces the uncompressed vendors bundle by 16 KB and allows us to remove react-device-detect from from devDependencies.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,10 +1,11 @@
 import { DialogContent, DialogOverlay } from '@reach/dialog'
 import { transparentize } from 'polished'
 import React from 'react'
-import { isMobile } from 'react-device-detect'
 import { animated, useSpring, useTransition } from 'react-spring'
 import { useGesture } from 'react-use-gesture'
 import styled, { css } from 'styled-components/macro'
+
+import { isMobile } from '../../utils/userAgent'
 
 const AnimatedDialogOverlay = animated(DialogOverlay)
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
