472124b832bc2d4463277a1de6c1a9e492a536ca
Justin Domingue <judo@uniswap.org>
2021-09-22 12:05:05 -0400

    adjust comment

--- src/components/LiquidityChartRangeInput/Brush.tsx
+++ src/components/LiquidityChartRangeInput/Brush.tsx
@@ -48,7 +48,7 @@ const BRUSH_EXTENT_MARGIN_PX = 2
  * same pixel coordinate as elements in `b`
  */
 const compare = (a: [number, number], b: [number, number], xScale: ScaleLinear<number, number>): boolean => {
-  // normalize pixels to 4 decimals
+  // normalize pixels to 1 decimals
   const aNorm = a.map((x) => xScale(x).toFixed(1))
   const bNorm = b.map((x) => xScale(x).toFixed(1))
   return aNorm.every((v, i) => v === bNorm[i])
