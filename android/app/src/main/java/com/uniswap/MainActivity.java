5c8665b55db01169eb765326e98315ee2b2b9cb9
J M Rossy <jm.rossy@gmail.com>
2021-09-24 13:35:42 -0400

    Add comment about React-Nav in MainActivity.java

--- android/app/src/main/java/com/uniswap/MainActivity.java
+++ android/app/src/main/java/com/uniswap/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ReactActivity {
     return "Uniswap";
   }
 
+  // Required for react-navigation to work on Android
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
