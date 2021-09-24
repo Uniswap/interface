2482a10ca87bfe61a2ba2023f847cdf2f909f273
Zach Pomerantz <zzmp@uniswap.org>
2021-09-24 08:13:40 -0700

    perf: rm duplicated ua-parser (#2427)

--- src/utils/getUserAgent.ts
+++
@@ -1,6 +0,0 @@
-import { UAParser } from 'ua-parser-js'
-
-export function getUserAgent(): UAParser.IResult {
-  const parser = new UAParser(window.navigator.userAgent)
-  return parser.getResult()
-}
