288e38376cd363689865f1be4a0474510f773ad1
Oskar Karcz <oskar.karcz@rumblefish.pl>
2021-09-27 11:31:38 +0200

    add withdrawEth

--- src/hooks/useArbBridge.ts
+++ src/hooks/useArbBridge.ts
@@ -94,40 +94,47 @@ export const useArbBridge = () => {
     [account, bridge, dispatch, l1ChainId, l2ChainId]
   )
 
+
+
+
+
+
+
+
+
+
 const withdrawEth = useCallback(
   async (value: string) => {
-    if (!bridge || !chainId || !account) return
-    const { partnerChainId } = NETWORK_DETAIL[chainId]
-
-    if (!partnerChainId) return
+    if (!account || !bridge || !l1ChainId || !l2ChainId) return
     const weiValue = utils.parseEther(value)
 
     try {
       // L2
-      const withdrawTx = await bridge.withdrawETH(weiValue)
-      console.log('Call withdrawETH', withdrawTx)
-      console.log('Withdraw hash', withdrawTx.hash)
+      const txn = await bridge.withdrawETH(weiValue)
+      
+      console.log('Call withdrawETH', txn)
+      console.log('Withdraw hash', txn.hash)
+      
       dispatch(
         addBridgeTxn({
           assetName: 'ETH',
           assetType: BridgeAssetType.ETH,
           type: 'withdraw',
           value,
-          txHash: withdrawTx.hash,
-          status: 'l1-pending',
-          from: chainId,
-          to: partnerChainId,
+          txHash: txn.hash,
+          chainId: l2ChainId,
           sender: account
         })
       )
 
-      const withdrawReceipt = await withdrawTx.wait()
+      const withdrawReceipt = await txn.wait()
       console.log('Get withdraw receipt', withdrawReceipt)
+
+
       dispatch(
         updateBridgeTxnReceipt({
-          chainId,
-          txHash: withdrawTx.hash,
-          layer: 2,
+          chainId: l2ChainId,
+          txHash: txn.hash,
           receipt: withdrawReceipt
         })
       )
@@ -135,9 +142,9 @@ const withdrawEth = useCallback(
       //withdrawal event
       const l2ToL2EventData = await bridge.getWithdrawalsInL2Transaction(withdrawReceipt)
       console.log("Call getWithdrawalsInL2Transaction and get event data")
-      // if (l2ToL2EventData.length === 0)
-      // throw new Error(`Txn ${withdrawTx} did not initiate an outgoing messages`)
       console.log('Event data length:',l2ToL2EventData.length)
+      
+      
       if (l2ToL2EventData.length === 1) {
         const { batchNumber, indexInBatch } = l2ToL2EventData[0]
 
@@ -150,51 +157,14 @@ const withdrawEth = useCallback(
           `Waiting for message to be confirmed: Batchnumber: ${batchNumber}, IndexInBatch ${indexInBatch}`
         )
         
-        while (outgoingMessageState !== OutgoingMessageState.CONFIRMED) {
-          await wait(1000 * 5)
-          outgoingMessageState = await bridge.getOutGoingMessageState(
-            batchNumber,
-            indexInBatch
-          )
-          console.log('Updated outgoing message state', outgoingMessageState)
-
-          switch (outgoingMessageState) {
-            case OutgoingMessageState.NOT_FOUND: {
-              console.log('Message not found; something strange and bad happened')
-              break
-            }
-            case OutgoingMessageState.EXECUTED: {
-              console.log(`Message already executed! Nothing else to do here`)
-              break
-            }
-            case OutgoingMessageState.UNCONFIRMED: {
-              console.log(`Message not yet confirmed; we'll wait a bit and try again`)
-              break
-            }
-      
-            default:
-              break
-          }
-        }
-        console.log('Outgoing message state after waiting for confirmation:', outgoingMessageState)
         
-        /**
-         * Now that its confirmed, we can retrieve the Merkle proof data from the chain, and execute our message in its outbox entry.
-         * triggerL2ToL1Transaction handles these steps
-         */
-        const res = await bridge.triggerL2ToL1Transaction(batchNumber, indexInBatch)
-        console.log('Transaction triggered. Waiting.')
-        const rec = await res.wait()
-      
-        console.log('Done! Your transaction is executed')
-        console.log(rec)  
       }
       return withdrawReceipt
     } catch (err) {
       throw err
     }
   },
-  [account, bridge, chainId, dispatch]
+  [account, bridge, dispatch, l1ChainId, l2ChainId]
 )
 
 
