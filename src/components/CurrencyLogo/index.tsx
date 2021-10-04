31ca2c31588aa1d51169ab87cfeaaa531f993374
Nguyen Van Viet <vietnv1304@gmail.com>
2021-10-04 23:09:20 +0700

    fix: add background for transparent logo (#526)

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -24,6 +24,7 @@ const StyledLogo = styled(Logo)<{ size: string }>`
   height: ${({ size }) => size};
   border-radius: ${({ size }) => size};
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
+  background: #fff;
 `
 
 const logo: { readonly [chainId in ChainId]?: string } = {
