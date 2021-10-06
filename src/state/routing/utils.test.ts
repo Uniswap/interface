f1efc2b9355c6aa665ac8e4b8ce12c9037d5cbda
Lint Action <lint-action@samuelmeuli.com>
2021-10-06 16:33:21 +0000

    Fix code style issues with ESLint

--- src/state/routing/utils.test.ts
+++ src/state/routing/utils.test.ts
@@ -168,7 +168,7 @@ describe('#useRoute', () => {
         route: [
           [
             {
-            type: 'v3-pool',
+              type: 'v3-pool',
               address: '0x1f8F72aA9304c8B593d555F12eF6589cC3A579A2',
               amountIn: (1e18).toString(),
               amountOut: amount`5`,
@@ -198,7 +198,7 @@ describe('#useRoute', () => {
         route: [
           [
             {
-            type: 'v3-pool',
+              type: 'v3-pool',
               address: '0x1f8F72aA9304c8B593d555F12eF6589cC3A579A2',
               amountIn: amount`5`,
               amountOut: (1e18).toString(),
