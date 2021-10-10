e2768d5bc8180ceceb1dcbf71a774d012c3a409c
Hoang Trinh <hoang.trinhj@gmail.com>
2021-10-10 18:11:09 +0700

    style: Modal Is Overlapped By Header

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -11,7 +11,7 @@ const AnimatedDialogOverlay = animated(DialogOverlay)
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const StyledDialogOverlay = styled(AnimatedDialogOverlay)`
   &[data-reach-dialog-overlay] {
-    z-index: 2;
+    z-index: 3;
     background-color: transparent;
     overflow: hidden;
 
