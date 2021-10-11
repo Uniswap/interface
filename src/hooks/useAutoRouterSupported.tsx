780d2d25a5c24f063aae653bfb9db6d6796c500d
Justin Domingue <judo@uniswap.org>
2021-10-11 13:58:03 -0400

    remove useAutoRouter

--- src/hooks/useAutoRouterSupported.tsx
+++
@@ -1,8 +0,0 @@
-import { SupportedChainId } from 'constants/chains'
-
-import { useActiveWeb3React } from './web3'
-
-export default function useAutoRouterSupported(): boolean {
-  const { chainId } = useActiveWeb3React()
-  return chainId === SupportedChainId.MAINNET
-}
