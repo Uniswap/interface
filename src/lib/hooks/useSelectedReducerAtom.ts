999d3f3230df836d02a9cc723ecf43b95cd53838
Zach Pomerantz <zzmp@uniswap.org>
2021-10-13 14:00:10 -0700

    fix: improve selector typing

--- src/lib/hooks/useSelectedReducerAtom.ts
+++ src/lib/hooks/useSelectedReducerAtom.ts
@@ -3,19 +3,17 @@ import { WritableAtom } from 'jotai'
 import { selectAtom, useAtomValue, useUpdateAtom } from 'jotai/utils'
 import { AnyAction } from 'redux'
 
-export default function useSelectedReducerAtom<State, Value, Payload>(
+export default function useSelectedReducerAtom<
+  State,
+  Value,
+  Payload,
+  Args extends any[] = Payload extends any[] ? Payload : Payload extends void ? [] : [Payload]
+>(
   atom: WritableAtom<State, AnyAction>,
   selector: (state: State) => Value,
   action: PayloadActionCreator<Payload>
-): [
-  Value,
-  Payload extends any[]
-    ? (...updates: Payload) => void
-    : Payload extends void
-    ? () => void
-    : (...updates: [Payload]) => void
-] {
+): [Value, (...updates: Args) => void] {
   const value = useAtomValue(selectAtom(atom, selector))
   const dispatch = useUpdateAtom(atom)
-  return [value, (...args: any[]) => dispatch(action(args))]
+  return [value, (...args) => dispatch(action(args))]
 }
