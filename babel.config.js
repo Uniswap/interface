23c5d1709c7c2a5c632af5f9f0e1a939543651e0
Connor McEwen <connor.mcewen@gmail.com>
2021-10-14 17:33:28 -0400

    feat: Fastlane infrastructure (#35)
    * Fastlane scaffolding
    
    * remove no-audit

--- babel.config.js
+++ babel.config.js
@@ -15,5 +15,7 @@ module.exports = {
         globals: ['__scanOCR'],
       },
     ],
+    // TypeScript compiles this, but in production builds, metro doesn't use tsc
+    ['@babel/plugin-proposal-logical-assignment-operators'],
   ],
 }
