33542ead03e5aac448891e870e4ec27b9f2f199f
Zach Pomerantz <zzmp@uniswap.org>
2021-10-17 09:34:55 -0700

    refactor: modularize Tooltip

--- src/lib/components/Swap/Settings/Label.tsx
+++ src/lib/components/Swap/Settings/Label.tsx
@@ -1,11 +1,8 @@
 import styled, { Theme } from 'lib/theme'
-import { styledIcon } from 'lib/theme/components'
 import TYPE from 'lib/theme/type'
-import { useState } from 'react'
-import { HelpCircle } from 'react-feather'
 
-import Popover from '../../Popover'
 import Row from '../../Row'
+import Tooltip from '../../Tooltip'
 
 export const Value = styled.div<{ selected?: boolean; theme: Theme }>`
   border: 1px solid ${({ selected, theme }) => (selected ? theme.active : theme.outline)};
@@ -14,13 +11,6 @@ export const Value = styled.div<{ selected?: boolean; theme: Theme }>`
   grid-gap: 0.25em;
   padding: 0.5em;
   cursor: pointer;
-`
-
-const StyledHelpCircle = styled(styledIcon(HelpCircle))`
-  :hover {
-    cursor: help;
-    opacity: 0.7;
-  }
 `
 
 interface LabelProps {
@@ -34,17 +24,10 @@ const LabelRow = styled(Row)`
 `
 
 export default function Label({ name, tooltip }: LabelProps) {
-  const [show, setShow] = useState(false)
   return (
     <LabelRow gap="0.5em">
       <TYPE.subhead2>{name}</TYPE.subhead2>
-      {tooltip && (
-        <TYPE.body2>
-          <Popover content={<TYPE.body2>{tooltip}</TYPE.body2>} show={show} placement="top">
-            <StyledHelpCircle onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)} />
-          </Popover>
-        </TYPE.body2>
-      )}
+      {tooltip && <Tooltip>{tooltip}</Tooltip>}
     </LabelRow>
   )
 }
