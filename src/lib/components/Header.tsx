cdb9b77cbd3ab3b6f569715fbfe02c7a9e397d02
Zach Pomerantz <zzmp@uniswap.org>
2021-10-18 15:42:47 -0700

    fix: eslint

--- src/lib/components/Header.tsx
+++ src/lib/components/Header.tsx
@@ -8,8 +8,8 @@ import Row from './Row'
 const HeaderRow = styled(Row)<{ divider?: boolean; theme: Theme }>`
   border-bottom: 1px solid ${({ divider, theme }) => (divider ? theme.outline : 'transparent')};
   margin: 0 1em;
-  padding-top: 1em;
   padding-bottom: ${({ divider }) => (divider ? '1em' : undefined)};
+  padding-top: 1em;
 `
 
 const StyledLogo = styled(Logo)`
