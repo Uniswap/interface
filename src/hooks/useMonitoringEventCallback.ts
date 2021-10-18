333ef20773ed4ff28c017d2ade2b038f1eb50480
Justin Domingue <judo@uniswap.org>
2021-10-18 13:37:22 -0400

    added hook

---
+++ src/hooks/useMonitoringEventCallback.ts
@@ -0,0 +1,23 @@
+import { useCallback } from 'react'
+import ReactGA from 'react-ga'
+
+import { useActiveWeb3React } from './web3'
+
+type MonitoringEvent = 'wallet connected' | 'swap' | 'add liquidity/v3' | 'add liquidity/v2'
+
+export function useMonitoringEventCallback() {
+  const { account, chainId } = useActiveWeb3React()
+
+  const event = useCallback(
+    (action: MonitoringEvent) => {
+      ReactGA.event({
+        category: 'Monitoring',
+        action,
+        label: [`walletAddress: ${account}`, `chainId: ${chainId}`, `timestamp: ${Date.now()}`].join(';'),
+      })
+    },
+    [account, chainId]
+  )
+
+  return event
+}
