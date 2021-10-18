817e5a11224c201c9a80c51e9f496bc8c4b9cc77
Zach Pomerantz <zzmp@uniswap.org>
2021-10-18 15:28:48 -0700

    fix: header divider

--- src/lib/components/Header.tsx
+++ src/lib/components/Header.tsx
@@ -8,7 +8,8 @@ import Row from './Row'
 const HeaderRow = styled(Row)<{ divider?: boolean; theme: Theme }>`
   border-bottom: 1px solid ${({ divider, theme }) => (divider ? theme.outline : 'transparent')};
   margin: 0 1em;
-  padding: 1em 0;
+  padding-top: 1em;
+  padding-bottom: ${({ divider }) => (divider ? '1em' : undefined)};
 `
 
 const StyledLogo = styled(Logo)`
