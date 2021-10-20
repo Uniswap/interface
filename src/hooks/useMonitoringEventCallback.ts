a87a876f0b182d5f48a7d28d957a69c43061e154
Justin Domingue <judo@uniswap.org>
2021-10-20 16:42:02 -0400

    use hostname

--- src/hooks/useMonitoringEventCallback.ts
+++ src/hooks/useMonitoringEventCallback.ts
@@ -35,7 +35,7 @@ export function useMonitoringEventCallback() {
         return
       }
       try {
-        push(ref(db, location.origin), {
+        push(ref(db, location.hostname), {
           chainId,
           signedTransactionHash: hash ?? 'n/a',
           timestamp: Date.now(),
