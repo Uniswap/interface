78f7b0bc04bb6123d7f1efc6d4a127c86d5c5c39
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-10-20 10:34:34 -0500

    ensure chainIds match before fetching pool data

--- src/hooks/usePools.ts
+++ src/hooks/usePools.ts
@@ -31,6 +31,7 @@ export function usePools(
       const tokenA = currencyA?.wrapped
       const tokenB = currencyB?.wrapped
       if (!tokenA || !tokenB || tokenA.equals(tokenB)) return null
+      if (tokenA.chainId !== tokenB.chainId) return null
       const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA]
       return [token0, token1, feeAmount]
     })
