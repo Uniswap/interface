a8f40265e4736ae1e5607004d367c16fb01c0825
Zach Pomerantz <zzmp@uniswap.org>
2021-10-19 20:00:43 -0700

    Revert "feat: add @davatar/react for robust avatar support (#2321)"
    This reverts commit cc8c5712eca69201a0ac2a3175a8509f1d26a484.

--- src/components/Identicon/index.tsx
+++ src/components/Identicon/index.tsx
@@ -1,4 +1,5 @@
-import Davatar from '@davatar/react'
+import Jazzicon from '@metamask/jazzicon'
+import { useEffect, useRef } from 'react'
 import styled from 'styled-components/macro'
 
 import { useActiveWeb3React } from '../../hooks/web3'
@@ -11,12 +12,17 @@ const StyledIdenticonContainer = styled.div`
 `
 
 export default function Identicon() {
-  const { account, library } = useActiveWeb3React()
+  const ref = useRef<HTMLDivElement>()
+
+  const { account } = useActiveWeb3React()
+
+  useEffect(() => {
+    if (account && ref.current) {
+      ref.current.innerHTML = ''
+      ref.current.appendChild(Jazzicon(16, parseInt(account.slice(2, 10), 16)))
+    }
+  }, [account])
 
   // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/30451
-  return (
-    <StyledIdenticonContainer>
-      {account && library?.provider && <Davatar address={account} size={16} provider={library.provider} />}
-    </StyledIdenticonContainer>
-  )
+  return <StyledIdenticonContainer ref={ref as any} />
 }
