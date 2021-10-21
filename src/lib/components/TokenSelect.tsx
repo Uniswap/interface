c99172a254f38b329fc79c24f5dcf13411dde6b6
Zach Pomerantz <zzmp@uniswap.org>
2021-10-20 22:59:11 -0700

    feat: define TokenSelect

--- src/lib/components/TokenSelect.tsx
+++ src/lib/components/TokenSelect.tsx
@@ -1,3 +1,14 @@
-export default function TokenSelect() {
+interface Token {
+  address: string
+  symbol: string
+  logoUri?: string
+}
+
+interface TokenSelectProps {
+  value?: Token
+  onChange: (token: Token) => void
+}
+
+export default function TokenSelect({}: TokenSelectProps) {
   return null
 }
