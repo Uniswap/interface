efd33470b70c29ec6c1589a218ad904b75508f31
Zach Pomerantz <zzmp@uniswap.org>
2021-10-22 15:29:12 -0700

    feat: token select

---
+++ src/lib/components/TokenSelect.fixture.tsx
@@ -0,0 +1,11 @@
+import { Modal } from './Dialog'
+import { TokenSelectDialog } from './TokenSelect'
+import Widget from './Widget'
+
+export default (
+  <Widget>
+    <Modal color="module">
+      <TokenSelectDialog onChange={() => void 0} onClose={() => void 0} />
+    </Modal>
+  </Widget>
+)
