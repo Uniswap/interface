6e775a64e230ebc34f7219468eb73770fb1a891d
Zach Pomerantz <zzmp@uniswap.org>
2021-10-25 11:44:44 -0700

    fix: use vibrant

--- src/lib/components/Swap/SwapOutput.tsx
+++ src/lib/components/Swap/SwapOutput.tsx
@@ -1,13 +1,11 @@
-import ColorThief from 'colorthief'
 import styled, { DynamicProvider as DynamicThemeProvider, useTheme } from 'lib/theme'
 import TYPE from 'lib/theme/type'
+import Vibrant from 'node-vibrant/lib/bundle'
 import { ReactNode, useEffect, useRef, useState } from 'react'
 
 import Column from '../Column'
 import Row from '../Row'
 import TokenInput, { TokenInputProps } from './TokenInput'
-
-const thief = new ColorThief()
 
 const OutputColumn = styled(Column)`
   background-color: ${({ theme }) => theme.module};
@@ -27,22 +25,12 @@ export default function SwapOutput({ children, ...props }: { children: ReactNode
       const node: HTMLImageElement | null = ref.current.querySelector(`img[src="${token.logoURI}"]`)
       if (node) {
         const src = node.src + '?_' // forces a different browser-cache for crossorigin requests
-        const img = node.cloneNode() as HTMLImageElement
-        img.src = src
-        img.crossOrigin = 'anonymous'
-        img.addEventListener('load', onLoad)
-        return () => {
-          img.removeEventListener('load', onLoad)
-        }
+        Vibrant.from(src)
+          .getPalette()
+          .then((palette) => palette && palette.Vibrant?.hex)
+          .then(setColor)
+          .catch(() => void 0)
       }
-    }
-    return () => void 0
-
-    function onLoad(e: Event) {
-      try {
-        const [r, g, b] = thief.getColor(e.target)
-        setColor(`#${r.toString(16)}${g.toString(16)}${b.toString(16)}`)
-      } catch {}
     }
   }, [ref, token, module])
   return (
