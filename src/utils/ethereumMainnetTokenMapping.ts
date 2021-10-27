4954016cd5de9ec55d2fc26580baa0039dbe440c
Hoang Trinh <hoang.trinhj@gmail.com>
2021-10-27 20:36:36 +0700

    fix: EVRY Token Logo (#623)

--- src/utils/ethereumMainnetTokenMapping.ts
+++ src/utils/ethereumMainnetTokenMapping.ts
@@ -7,6 +7,9 @@ export const getEthereumMainnetTokenLogoURL = (address: string) => {
   if (address?.toLowerCase() === ZERO_ADDRESS) {
     //native token
     address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+  } else if (address?.toLowerCase() === '0xd7DCd9B99787C619b4D57979521258D1A7267ad7'.toLowerCase()) {
+    //EVRY
+    uri = 'https://s2.coinmarketcap.com/static/img/coins/64x64/11458.png'
   }
 
   if (!uri) {
