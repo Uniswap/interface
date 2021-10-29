2f60532b7b891c8a41f0a85febd9f5028a7f2b0a
Justin Domingue <judo@uniswap.org>
2021-10-29 09:37:58 -0400

    chore: simplify Screen component with createBox (#79)
    * chore: simplify Screen component with createBox
    
    * rename to box

--- src/components/layout/Screen.tsx
+++ src/components/layout/Screen.tsx
@@ -1,18 +1,14 @@
-import { useTheme } from '@shopify/restyle'
-import React, { PropsWithChildren, useMemo } from 'react'
+import { createBox } from '@shopify/restyle'
+import React, { PropsWithChildren } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Theme } from 'src/styles/theme'
 
+const SafeAreaBox = createBox<Theme>(SafeAreaView)
+
 export function Screen(props: PropsWithChildren<any>) {
-  const theme = useTheme<Theme>()
-
-  const container = useMemo(
-    () => ({
-      flex: 1,
-      backgroundColor: theme.colors.mainBackground,
-    }),
-    [theme]
+  return (
+    <SafeAreaBox flex={1} bg="mainBackground">
+      {props.children}
+    </SafeAreaBox>
   )
-
-  return <SafeAreaView style={container}>{props.children}</SafeAreaView>
 }
