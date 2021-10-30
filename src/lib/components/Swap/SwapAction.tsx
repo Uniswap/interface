8a5fea905a5f1f7762c47ff1a4f1f7041800eebf
Zach Pomerantz <zzmp@uniswap.org>
2021-10-29 17:16:46 -0700

    refactor: action button

--- src/lib/components/Swap/SwapAction.tsx
+++ src/lib/components/Swap/SwapAction.tsx
@@ -1,83 +1,9 @@
 import assert from 'assert'
 import { useAtomValue } from 'jotai/utils'
-import styled, { Color, icon, Theme } from 'lib/theme'
-import TYPE from 'lib/theme/type'
-import { ReactNode, useMemo } from 'react'
-import { AlertTriangle } from 'react-feather'
+import { useMemo } from 'react'
 
-import Button from '../Button'
-import Row from '../Row'
+import Action, { Approval, Disabled, Loading } from '../Action'
 import { inputAtom, State, swapAtom } from './state'
-
-export const ActionButton = styled(Button)<{ color?: Color; theme: Theme }>`
-  background-color: ${({ color, theme }) => color && theme[color]};
-  border-radius: ${({ theme }) => theme.borderRadius * 0.5}em;
-  height: 3.5em;
-  outline: ${({ color, theme }) => (color ? undefined : `1px solid ${theme.outline}`)};
-
-  :disabled {
-    opacity: 1;
-  }
-`
-
-const ApprovalRow = styled(Row)`
-  border-radius: ${({ theme }) => theme.borderRadius * 0.5}em;
-  height: 3.5em;
-  outline: 1px solid ${({ theme }) => theme.outline};
-  padding: 0.5em;
-`
-
-const ApproveButton = styled(Button)`
-  background-color: ${({ theme }) => theme.interactive};
-  border-radius: ${({ theme }) => theme.borderRadius * 0.5}em;
-  color: ${({ theme }) => theme.contrast};
-  height: 100%;
-  padding: 0 1em;
-`
-
-function Disabled({ children }: { children: ReactNode }) {
-  return (
-    <ActionButton disabled>
-      <TYPE.buttonLarge>{children}</TYPE.buttonLarge>
-    </ActionButton>
-  )
-}
-
-function Loading() {
-  return (
-    <ActionButton disabled>
-      <TYPE.buttonLarge>Loading…</TYPE.buttonLarge>
-    </ActionButton>
-  )
-}
-
-interface ActionProps {
-  onClick: () => void
-  children: ReactNode
-}
-
-const AlertIcon = icon(AlertTriangle, { color: 'primary' })
-
-function Approval({ onClick, children }: ActionProps) {
-  return (
-    <ApprovalRow>
-      <Row gap={0.5}>
-        <AlertIcon />
-        <TYPE.subhead2>{children}</TYPE.subhead2>
-      </Row>
-      <ApproveButton onClick={onClick}>Approve</ApproveButton>
-    </ApprovalRow>
-  )
-  return null
-}
-
-function Action({ onClick, children }: ActionProps) {
-  return (
-    <ActionButton color="interactive" onClick={onClick}>
-      <TYPE.buttonLarge color="contrast">{children}</TYPE.buttonLarge>
-    </ActionButton>
-  )
-}
 
 function Empty() {
   return <Disabled>Enter amount</Disabled>
