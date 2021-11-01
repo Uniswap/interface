edb3ce421cdac1a16db9bc11761b03eda94ac99f
Zach Pomerantz <zzmp@uniswap.org>
2021-11-01 10:00:21 -0700

    fix: token color transition

--- src/lib/components/Swap/SwapOutput.tsx
+++ src/lib/components/Swap/SwapOutput.tsx
@@ -1,7 +1,7 @@
 import { useAtomValue } from 'jotai/utils'
 import { useUpdateAtom } from 'jotai/utils'
 import useColor, { prefetchColor } from 'lib/hooks/useColor'
-import styled, { DynamicProvider as DynamicThemeProvider, icon, Theme } from 'lib/theme'
+import styled, { Color, DynamicProvider as DynamicThemeProvider, icon, Theme } from 'lib/theme'
 import TYPE from 'lib/theme/type'
 import { Token } from 'lib/types'
 import { pickAtom } from 'lib/utils/atoms'
@@ -15,7 +15,7 @@ import TokenInput from './TokenInput'
 
 const BookIcon = icon(Book)
 
-const OutputColumn = styled(Column)<{ color?: string; token?: Token; theme: Theme }>`
+const OutputColumn = styled(Column)<{ hasColor: boolean | null; theme: Theme }>`
   background-color: ${({ theme }) => theme.module};
   border-radius: ${({ theme }) => theme.borderRadius - 0.25}em;
   padding: 0.75em;
@@ -23,10 +23,10 @@ const OutputColumn = styled(Column)<{ color?: string; token?: Token; theme: Them
 
   // Set transitions to reduce color flashes when switching color/token.
   // When color loads, transition the background so that it transitions from the empty or last state, but not _to_ the empty state.
-  transition: ${({ color }) => (color ? 'background-color 0.3s ease-out' : undefined)};
+  transition: ${({ hasColor }) => (hasColor ? 'background-color 0.3s ease-out' : undefined)};
   * {
     // When color is loading, delay the color/stroke so that it seems to transition from the last state.
-    transition: ${({ color, token }) => (!color && token ? 'color 0.3s ease-in, stroke 0.3s ease-in' : undefined)};
+    transition: ${({ hasColor }) => (hasColor === null ? 'color 0.3s ease-in, stroke 0.3s ease-in' : undefined)};
   }
 `
 
@@ -39,6 +39,13 @@ export default function SwapOutput({ children }: { children: ReactNode }) {
 
   const color = useColor(output.token)
   prefetchColor(useAtomValue(pickAtom(inputAtom, 'token'))) // extract eagerly in case of reversal
+
+  const hasColor = useMemo(() => {
+    if (color) {
+      return true
+    }
+    return output.token ? null : false
+  }, [color, output.token])
 
   const change = useMemo(() => {
     if (swap.output && swap.input) {
@@ -56,7 +63,7 @@ export default function SwapOutput({ children }: { children: ReactNode }) {
 
   return (
     <DynamicThemeProvider color={color}>
-      <OutputColumn token={output.token} gap={0.75}>
+      <OutputColumn hasColor={hasColor} gap={0.75}>
         <Row>
           <TYPE.subhead3>For</TYPE.subhead3>
         </Row>
