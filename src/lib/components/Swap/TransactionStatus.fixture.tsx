832e0224628dbc9fc1ed5a92e1876a6634ddc243
Zach Pomerantz <zzmp@uniswap.org>
2021-11-02 22:36:50 -0700

    test: tx error

--- src/lib/components/Swap/TransactionStatus.fixture.tsx
+++ src/lib/components/Swap/TransactionStatus.fixture.tsx
@@ -37,7 +37,7 @@ function Fixture() {
         setTransaction((tx) => {
           assert(tx)
           tx.status = new Error(
-            'Swap failed: Unknown error: "Error: This is an especially long string to demonstrate overflow behavior. It must be at least a few lines. One is too few. Two is too few. Three may work!"'
+            'Swap failed: Unknown error: "Error: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar, risus eu pretium condimentum, tellus dui fermentum turpis, id gravida metus justo ac lorem. Etiam vitae dapibus eros, nec elementum ipsum. Duis condimentum, felis vel tempor ultricies, eros diam tempus odio, at tempor urna odio id massa. Aliquam laoreet turpis justo, auctor accumsan est pellentesque at. Integer et dolor feugiat, sodales tortor non, cursus augue. Phasellus id suscipit justo, in ultricies tortor. Aenean libero nibh, egestas sit amet vehicula sit amet, tempor ac ligula. Cras at tempor lectus. Mauris sollicitudin est velit, nec consectetur lorem dapibus ut. Praesent magna ex, faucibus ac fermentum malesuada, molestie at ex. Phasellus bibendum lorem nec dolor dignissim eleifend. Nam dignissim varius velit, at volutpat justo pretium id."'
           )
           tx.elapsedMs = Date.now() - tx.timestamp
         })
