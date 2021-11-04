46044074607b151ea442fa58af5f63188348405b
Brian Li <brnli7@gmail.com>
2021-11-04 16:41:09 -0400

    Add TFBX dual farm (#373)
    * Add TFBX dual farm
    
    * Update TFBX dual farm address

--- src/state/stake/farms.ts
+++ src/state/stake/farms.ts
@@ -56,6 +56,14 @@ export const multiRewardPools: MultiRewardPool[] = [
     numRewards: 2,
     active: true,
   },
+  // TFBX-UBE
+  {
+    address: '0x501ba7c59BA8afC1427F75D310A862BA0D2adcD2',
+    underlyingPool: '0x3DAc201Ec1b3a037bC9124906A2ae0A6a09ACC1d',
+    basePool: '0x3DAc201Ec1b3a037bC9124906A2ae0A6a09ACC1d',
+    numRewards: 2,
+    active: true,
+  },
 
   // ** D4P ** //
   // UBE-CELO
