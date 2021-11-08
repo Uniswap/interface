47278c50b7475e7214030c543009b54822cce7b3
Spencer Yen <spencersyen@gmail.com>
2021-11-08 10:51:04 -0500

    Dismiss keyboard on return key for MnemonicInput (#102)

--- src/components/input/MnemonicInput.tsx
+++ src/components/input/MnemonicInput.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-import { TextStyle } from 'react-native'
+import { Keyboard, TextStyle } from 'react-native'
 import { TextInput, TextInputProps } from 'src/components/input/TextInput'
 
 export function MnemonicInput(props: TextInputProps) {
-  return <TextInput multiline={true} numberOfLines={5} pt="md" style={defaultStyle} {...props} />
+  return (
+    <TextInput
+      multiline={true}
+      numberOfLines={5}
+      pt="md"
+      returnKeyType="done"
+      onSubmitEditing={() => {
+        Keyboard.dismiss()
+      }}
+      style={defaultStyle}
+      {...props}
+    />
+  )
 }
 
 const defaultStyle: TextStyle = {
