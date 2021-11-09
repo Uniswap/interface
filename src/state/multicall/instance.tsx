956cc66e46a40dcb25075b026ad27ff4a27a20bd
J M Rossy <jm.rossy@gmail.com>
2021-11-09 16:28:48 +0100

    Replace multicall implementation with usage of multicall lib

---
+++ src/state/multicall/instance.tsx
@@ -0,0 +1,4 @@
+import { createMulticall } from 'multicall-query' // TODO switch out for '@uniswap/redux-multicall' when published
+
+// Create a multicall instance with default settings
+export const multicall = createMulticall()
