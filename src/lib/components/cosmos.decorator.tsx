4442525b4950d258a60d29e31644493d7035b30d
Zach Pomerantz <zzmp@uniswap.org>
2021-11-10 15:12:29 -0800

    feat: cosmos locale selector

--- src/lib/components/cosmos.decorator.tsx
+++ src/lib/components/cosmos.decorator.tsx
@@ -1,12 +1,13 @@
+import { DEFAULT_LOCALE, SUPPORTED_LOCALES } from 'constants/locales'
 import { getDefaultTheme } from 'lib/theme'
 import { ReactNode } from 'react'
-import { useValue } from 'react-cosmos/fixture'
+import { useSelect, useValue } from 'react-cosmos/fixture'
 
 import Widget from './Widget'
 
 export default function WidgetDecorator({ children }: { children: ReactNode }) {
   const [theme] = useValue('theme', { defaultValue: getDefaultTheme() })
-  const [locale] = useValue('locale', { defaultValue: 'pseudo' })
+  const [locale] = useSelect('locale', { defaultValue: DEFAULT_LOCALE, options: ['pseudo', ...SUPPORTED_LOCALES] })
   return (
     <Widget theme={theme} locale={locale}>
       {children}
