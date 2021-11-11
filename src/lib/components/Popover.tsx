225b3df7d37f56ee4405566f37ca701ee4763406
Zach Pomerantz <zzmp@uniswap.org>
2021-11-11 14:29:23 -0800

    fix: tooltip color

--- src/lib/components/Popover.tsx
+++ src/lib/components/Popover.tsx
@@ -15,6 +15,7 @@ const PopoverContainer = styled.div<{ show: boolean }>`
   background-color: ${({ theme }) => theme.dialog};
   border: 1px solid ${({ theme }) => theme.outline};
   border-radius: 0.5em;
+  color: ${({ theme }) => theme.primary};
   opacity: ${(props) => (props.show ? 1 : 0)};
   padding: 8px;
   transition: visibility 0.15s linear, opacity 0.15s linear;
