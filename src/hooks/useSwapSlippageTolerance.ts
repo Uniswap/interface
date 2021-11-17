74a5560393d124111722a3033712ecf56504317b
Moody Salem <moody.salem@gmail.com>
2021-11-17 13:53:29 -0500

    comments

--- src/hooks/useSwapSlippageTolerance.ts
+++ src/hooks/useSwapSlippageTolerance.ts
@@ -15,11 +15,16 @@ const V2_SWAP_DEFAULT_SLIPPAGE = new Percent(50, 10_000) // .50%
 const V3_SWAP_DEFAULT_SLIPPAGE = new Percent(50, 10_000) // .50%
 const ONE_TENTHS_PERCENT = new Percent(10, 10_000) // .10%
 
+/**
+ * Return a guess of the gas cost used in computing slippage tolerance for a given trade
+ * @param trade the trade for which to _guess_ the amount of gas it would cost to execute
+ */
 function guestimateGas(
   trade: V2Trade<Currency, Currency, TradeType> | V3Trade<Currency, Currency, TradeType> | undefined
 ): number | undefined {
   if (!trade) return undefined
   if (trade instanceof V2Trade) return 90_000 + trade.route.pairs.length * 20_000
+  // does not consider the number of ticks crossed
   return 100_000 + trade.swaps.reduce((memo, swap) => swap.route.pools.length + memo, 0) * 30_000
 }
 
