79d73c15c34aa1ed93618277314c7a20aa85e135
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2021-11-17 23:13:36 -0600

    useAtomValue

--- src/lib/cosmos.decorator.tsx
+++ src/lib/cosmos.decorator.tsx
@@ -1,6 +1,7 @@
 import { Provider } from '@ethersproject/abstract-provider'
 import { DEFAULT_LOCALE, SUPPORTED_LOCALES } from 'constants/locales'
-import { atom, useAtom } from 'jotai'
+import { atom } from 'jotai'
+import { useAtomValue } from 'jotai/utils'
 import { getDefaultTheme } from 'lib/theme'
 import { ReactNode } from 'react'
 import { useSelect, useValue } from 'react-cosmos/fixture'
@@ -13,7 +14,7 @@ export const providerAtom = atom<Provider | undefined>(undefined)
 export default function WidgetDecorator({ children }: { children: ReactNode }) {
   const [theme] = useValue('theme', { defaultValue: getDefaultTheme() })
   const [locale] = useSelect('locale', { defaultValue: DEFAULT_LOCALE, options: ['pseudo', ...SUPPORTED_LOCALES] })
-  const [provider] = useAtom(providerAtom)
+  const provider = useAtomValue(providerAtom)
   return (
     <>
       <Connectors />
