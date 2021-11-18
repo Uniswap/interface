0f205ebcf0a67de183c0ac50e3f68d34c19c2984
Moody Salem <moody.salem@gmail.com>
2021-11-18 12:34:01 -0500

    address review feedback

--- src/hooks/useSwapSlippageTolerance.ts
+++ src/hooks/useSwapSlippageTolerance.ts
@@ -19,13 +19,15 @@ const ONE_TENTHS_PERCENT = new Percent(10, 10_000) // .10%
  * Return a guess of the gas cost used in computing slippage tolerance for a given trade
  * @param trade the trade for which to _guess_ the amount of gas it would cost to execute
  */
-function guestimateGas(
+function guesstimateGas(
   trade: V2Trade<Currency, Currency, TradeType> | V3Trade<Currency, Currency, TradeType> | undefined
 ): number | undefined {
-  if (!trade) return undefined
-  if (trade instanceof V2Trade) return 90_000 + trade.route.pairs.length * 20_000
-  // does not consider the number of ticks crossed
-  return 100_000 + trade.swaps.reduce((memo, swap) => swap.route.pools.length + memo, 0) * 30_000
+  if (trade instanceof V2Trade) {
+    return 90_000 + trade.route.pairs.length * 30_000
+  } else if (trade instanceof V3Trade) {
+    return 100_000 + trade.swaps.reduce((memo, swap) => swap.route.pools.length + memo, 0) * 30_000
+  }
+  return undefined
 }
 
 const MIN_AUTO_SLIPPAGE_TOLERANCE = new Percent(5, 1000) // 0.5%
@@ -39,7 +41,7 @@ export default function useSwapSlippageTolerance(
   const outputDollarValue = useUSDCValue(trade?.outputAmount)
   const ethGasPrice = useGasPrice()
 
-  const gasEstimate = guestimateGas(trade)
+  const gasEstimate = guesstimateGas(trade)
   const ether = useCurrency('ETH')
   const etherPrice = useUSDCPrice(ether ?? undefined)
 
@@ -52,6 +54,8 @@ export default function useSwapSlippageTolerance(
       ether && ethGasCost && etherPrice ? etherPrice.quote(CurrencyAmount.fromRawAmount(ether, ethGasCost)) : undefined
 
     if (outputDollarValue && dollarGasCost) {
+      // the rationale is that a user will not want their trade to fail for a loss due to slippage that is less than
+      // the cost of the gas of the failed transaction
       const fraction = dollarGasCost.asFraction.divide(outputDollarValue.asFraction)
       const result = new Percent(fraction.numerator, fraction.denominator)
       if (result.greaterThan(MAX_AUTO_SLIPPAGE_TOLERANCE)) return MAX_AUTO_SLIPPAGE_TOLERANCE
