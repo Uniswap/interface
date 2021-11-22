5fc4a259669dc166ce60e35b8492028f9a82437b
Zach Pomerantz <zzmp@uniswap.org>
2021-11-22 13:32:56 -0800

    fix: export Memo

--- src/types/react.d.ts
+++ src/types/react.d.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-types */
-declare global {
-  interface Memoized<T extends object> extends T {
-    __memoized: string
-  }
-  type Memo<T> = T extends object ? Memoized<T> : T
+interface IMemo<T extends object> extends T {
+  __memo: string
+}
+export type Memo<T> = T extends object ? IMemo<T> : T
 
+declare global {
   namespace React {
     function useMemo<T>(factory: () => T, deps: DependencyList | undefined): Memo<T>
   }
