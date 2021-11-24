e7a9379e93d4c666235cb8b732a811d02a342d04
Josh <jonesjosh41@gmail.com>
2021-11-24 11:18:30 -0600

    Bug fixes and such

--- src/utils/switchToNetwork.ts
+++ src/utils/switchToNetwork.ts
@@ -1,6 +1,7 @@
 import { SupportedChainId } from 'constants/chains'
 import { BigNumber, utils } from 'ethers'
 import { Web3Provider } from '@ethersproject/providers'
+import Swal from 'sweetalert2';
 
 interface SwitchNetworkArguments {
   library: Web3Provider
@@ -10,12 +11,37 @@ interface SwitchNetworkArguments {
 // provider.request returns Promise<any>, but wallet_switchEthereumChain must return null or throw
 // see https://github.com/rekmarks/EIPs/blob/3326-create/EIPS/eip-3326.md for more info on wallet_switchEthereumChain
 export async function switchToNetwork({ library, chainId }: SwitchNetworkArguments): Promise<null | void> {
-  if (!library?.provider?.request) {
-    return
+  const switchFn = async () => {
+    if (!library?.provider?.request) {
+      return
+    }
+    localStorage.removeItem('kibaBalance');
+    const formattedChainId = utils.hexStripZeros(BigNumber.from(chainId).toHexString())
+    return library?.provider.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: formattedChainId }],
+    })
   }
-  const formattedChainId = utils.hexStripZeros(BigNumber.from(chainId).toHexString())
-  return library?.provider.request({
-    method: 'wallet_switchEthereumChain',
-    params: [{ chainId: formattedChainId }],
-  })
+
+  const trackedValue = localStorage.getItem('kibaBalance');
+  if (trackedValue) {
+    const trackedValueParsed = parseFloat(trackedValue);
+    if (trackedValueParsed > 0) {
+      const { isConfirmed } = await Swal.fire({
+        title: "Are you sure?",
+        text: "Switching networks will stop your current gains tracking session.",
+        confirmButtonText: "Okay",
+        cancelButtonText: "Cancel",
+        confirmButtonColor: '#991816',
+        cancelButtonColor: '#444',
+        showCancelButton: true,
+        icon: "question",
+      })
+
+      if (isConfirmed) await switchFn()
+    } 
+    else await switchFn()
+  } 
+  else await switchFn()
+
 }
