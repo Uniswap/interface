34e2948177db9013f587f88b522782c9ab23f9df
Justin Domingue <judo@uniswap.org>
2021-11-24 10:28:59 -0500

    update useSwapSlippageTolerance

--- src/hooks/useSwapSlippageTolerance.ts
+++ src/hooks/useSwapSlippageTolerance.ts
@@ -1,6 +1,6 @@
+import { Trade } from '@uniswap/router-sdk'
 import { Currency, CurrencyAmount, Percent, TradeType } from '@uniswap/sdk-core'
 import { Trade as V2Trade } from '@uniswap/v2-sdk'
-import { Trade as V3Trade } from '@uniswap/v3-sdk'
 import { L2_CHAIN_IDS } from 'constants/chains'
 import JSBI from 'jsbi'
 import { useMemo } from 'react'
@@ -18,23 +18,20 @@ const ONE_TENTHS_PERCENT = new Percent(10, 10_000) // .10%
  * Return a guess of the gas cost used in computing slippage tolerance for a given trade
  * @param trade the trade for which to _guess_ the amount of gas it would cost to execute
  */
-function guesstimateGas(
-  trade: V2Trade<Currency, Currency, TradeType> | V3Trade<Currency, Currency, TradeType> | undefined
-): number | undefined {
+function guesstimateGas(trade: Trade<Currency, Currency, TradeType> | undefined): number | undefined {
   if (trade instanceof V2Trade) {
     return 90_000 + trade.route.pairs.length * 30_000
-  } else if (trade instanceof V3Trade) {
+  } else if (!!trade) {
     return 100_000 + trade.swaps.reduce((memo, swap) => swap.route.pools.length + memo, 0) * 30_000
   }
   return undefined
 }
 
+const V2_SWAP_DEFAULT_SLIPPAGE = new Percent(50, 10_000) // .50%
 const MIN_AUTO_SLIPPAGE_TOLERANCE = new Percent(5, 1000) // 0.5%
 const MAX_AUTO_SLIPPAGE_TOLERANCE = new Percent(25, 100) // 25%
 
-export default function useSwapSlippageTolerance(
-  trade: V2Trade<Currency, Currency, TradeType> | V3Trade<Currency, Currency, TradeType> | undefined
-): Percent {
+export default function useSwapSlippageTolerance(trade: Trade<Currency, Currency, TradeType> | undefined): Percent {
   const { chainId } = useActiveWeb3React()
   const onL2 = chainId && L2_CHAIN_IDS.includes(chainId)
   const outputDollarValue = useUSDCValue(trade?.outputAmount)
