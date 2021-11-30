5cead7bbbfff3e6030e1c0b83ef1a1a83919fcfd
Lint Action <lint-action@samuelmeuli.com>
2021-11-30 15:26:21 +0000

    Fix code style issues with ESLint

--- src/constants/governance.ts
+++ src/constants/governance.ts
@@ -19,5 +19,5 @@ export const DEFAULT_AVERAGE_BLOCK_TIME_IN_SECS = 13
 
 // Block time here is slightly higher (~1s) than average in order to avoid ongoing proposals past the displayed time
 export const AVERAGE_BLOCK_TIME_IN_SECS: { [chainId: number]: number } = {
-  [1]: DEFAULT_AVERAGE_BLOCK_TIME_IN_SECS,
+  1: DEFAULT_AVERAGE_BLOCK_TIME_IN_SECS,
 }
