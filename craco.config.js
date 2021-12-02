e8754323cabfcc686190c8699e2d5910f919abc3
Ian Macalinao <ian@ubeswap.org>
2021-12-02 14:22:19 -0800

    Add deploys to GitHub Pages

--- craco.config.js
+++ craco.config.js
@@ -12,22 +12,28 @@ module.exports = {
               authToken: process.env.SENTRY_AUTH_TOKEN,
               org: process.env.SENTRY_ORG,
               project: process.env.SENTRY_PROJECT,
-              release: `${
-                process.env.VERCEL_GIT_COMMIT_REF ? process.env.VERCEL_GIT_COMMIT_REF.replace(/\//g, '--') : 'unknown'
-              }-${process.env.VERCEL_GIT_COMMIT_SHA ? process.env.VERCEL_GIT_COMMIT_SHA : 'unknown'}`,
+              release:
+                process.env.REACT_APP_SENTRY_RELEASE ??
+                `${
+                  process.env.VERCEL_GIT_COMMIT_REF ? process.env.VERCEL_GIT_COMMIT_REF.replace(/\//g, '--') : 'unknown'
+                }-${process.env.VERCEL_GIT_COMMIT_SHA ? process.env.VERCEL_GIT_COMMIT_SHA : 'unknown'}`,
 
               // webpack specific configuration
               include: './build/',
               ignore: ['node_modules'],
               setCommits: {
-                repo: `${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}`,
-                commit: process.env.VERCEL_GIT_COMMIT_SHA,
+                repo: process.env.GITHUB_REPO,
+                commit: process.env.GIT_COMMIT_SHA ?? process.env.VERCEL_GIT_COMMIT_SHA,
               },
-              deploy: {
-                env: process.env.VERCEL_ENV,
-                url: `https://${process.env.VERCEL_URL}`,
-                started: now,
-              },
+              ...(process.env.VERCEL_URL
+                ? {
+                    deploy: {
+                      env: process.env.REACT_APP_SENTRY_ENVIRONMENT ?? process.env.VERCEL_ENV,
+                      url: `https://${process.env.VERCEL_URL}`,
+                      started: now,
+                    },
+                  }
+                : {}),
             }),
           ]
         : [],
