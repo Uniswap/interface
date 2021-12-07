8e6ab49477bc8d44512b06b3e6f9f23a2ce261b3
Zach Pomerantz <zzmp@uniswap.org>
2021-12-06 16:10:09 -0800

    chore: rm wallet (for now)

--- src/lib/components/Swap/index.tsx
+++ src/lib/components/Swap/index.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 
 import Header from '../Header'
 import { BoundaryProvider } from '../Popover'
-import Wallet from '../Wallet'
 import Input from './Input'
 import Output from './Output'
 import ReverseButton from './ReverseButton'
@@ -16,7 +15,6 @@ export default function Swap() {
   return (
     <>
       <Header logo title={<Trans>Swap</Trans>}>
-        <Wallet />
         <Settings />
       </Header>
       <div ref={setBoundary}>
