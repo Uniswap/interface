ee607413dbfd441f37de66d2aa4fc4fb4092e43a
Zach Pomerantz <zzmp@uniswap.org>
2021-12-09 14:16:59 -0800

    fix: memoize current block timestamp

--- src/hooks/useCurrentBlockTimestamp.ts
+++ src/hooks/useCurrentBlockTimestamp.ts
@@ -1,10 +1,19 @@
 import { BigNumber } from '@ethersproject/bignumber'
+import { useEffect, useState } from 'react'
 
 import { useSingleCallResult } from '../state/multicall/hooks'
 import { useMulticall2Contract } from './useContract'
 
 // gets the current timestamp from the blockchain
 export default function useCurrentBlockTimestamp(): BigNumber | undefined {
+  const [lastBlockTimestamp, setLastBlockTimestamp] = useState<BigNumber | undefined>(undefined)
   const multicall = useMulticall2Contract()
-  return useSingleCallResult(multicall, 'getCurrentBlockTimestamp')?.result?.[0]
+  // TODO:zzmp
+  const currentBlockTimestamp = useSingleCallResult(multicall, 'getCurrentBlockTimestamp')?.result?.[0]
+  useEffect(() => {
+    if (!lastBlockTimestamp?.eq(currentBlockTimestamp)) {
+      setLastBlockTimestamp(currentBlockTimestamp)
+    }
+  }, [currentBlockTimestamp, lastBlockTimestamp])
+  return lastBlockTimestamp
 }
