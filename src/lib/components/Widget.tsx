af984be9f9e865566a1b6e33a6880b45017dca5c
Zach Pomerantz <zzmp@uniswap.org>
2021-12-10 15:16:06 -0800

    fix: widget height

--- src/lib/components/Widget.tsx
+++ src/lib/components/Widget.tsx
@@ -21,7 +21,7 @@ const WidgetWrapper = styled.div<{ width?: number | string }>`
   display: flex;
   flex-direction: column;
   font-size: 16px;
-  min-height: 340px;
+  height: 340px;
   min-width: 300px;
   padding: 0.25em;
   position: relative;
