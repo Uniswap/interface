2ef596641ca4001dbd47d022bb8ad0513b344475
Zach Pomerantz <zzmp@uniswap.org>
2021-12-13 11:26:13 -0800

    fix: nits

--- src/hooks/useCurrentBlockTimestamp.ts
+++ src/hooks/useCurrentBlockTimestamp.ts
@@ -8,7 +8,6 @@ import { useMulticall2Contract } from './useContract'
 export default function useCurrentBlockTimestamp(): BigNumber | undefined {
   const [lastBlockTimestamp, setLastBlockTimestamp] = useState<BigNumber | undefined>(undefined)
   const multicall = useMulticall2Contract()
-  // TODO:zzmp
   const currentBlockTimestamp = useSingleCallResult(multicall, 'getCurrentBlockTimestamp')?.result?.[0]
   useEffect(() => {
     if (!lastBlockTimestamp?.eq(currentBlockTimestamp)) {
--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 
 /**
  * Debounces updates to a value.
- * Non-primitivevs *must* wrap the value in useMemo, or the value will be updated due to referential inequality.
+ * Non-primitives *must* wrap the value in useMemo, or the value will be updated due to referential inequality.
  */
 // modified from https://usehooks.com/useDebounce/
 export default function useDebounce<T>(value: T, delay: number): T {
