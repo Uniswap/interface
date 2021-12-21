5b138f8ea4917102da176526d011473cc3c295de
pro100skm <pro100skm@yandex.ru>
2021-12-22 01:29:28 +0300

    feat: changed logo

--- src/state/user/actions.ts
+++ src/state/user/actions.ts
@@ -15,12 +15,7 @@ export interface SerializedPair {
 }
 
 export const updateMatchesDarkMode = createAction<{ matchesDarkMode: boolean }>('user/updateMatchesDarkMode')
-export const updateArbitrumAlphaAcknowledged = createAction<{ arbitrumAlphaAcknowledged: boolean }>(
-  'user/updateArbitrumAlphaAcknowledged'
-)
-export const updateOptimismAlphaAcknowledged = createAction<{ optimismAlphaAcknowledged: boolean }>(
-  'user/updateOptimismAlphaAcknowledged'
-)
+
 export const updateUserDarkMode = createAction<{ userDarkMode: boolean }>('user/updateUserDarkMode')
 export const updateUserExpertMode = createAction<{ userExpertMode: boolean }>('user/updateUserExpertMode')
 export const updateUserLocale = createAction<{ userLocale: SupportedLocale }>('user/updateUserLocale')
