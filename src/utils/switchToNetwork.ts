13c42a384b5099dac7d963bf90c18f065db7ce50
Moody Salem <moody.salem@gmail.com>
2021-12-23 10:18:58 -0500

    fix(network switch): do not show failed to switch networks for successful network switches

--- src/utils/switchToNetwork.ts
+++ src/utils/switchToNetwork.ts
@@ -37,15 +37,16 @@ export async function switchToNetwork({ library, chainId }: SwitchNetworkArgumen
           },
         ],
       })
-      const { chainId: chainIdAfterSwitch } = await library.getNetwork()
-      if (chainIdAfterSwitch !== chainId) {
-        // metamask (only known implementer) automatically switches after a network is added
-        // the second call is done here because that behavior is not a part of the spec and cannot be relied upon in the future
-        // metamask's behavior when switching to the current network is just to return null (a no-op)
+      // metamask (only known implementer) automatically switches after a network is added
+      // the second call is done here because that behavior is not a part of the spec and cannot be relied upon in the future
+      // metamask's behavior when switching to the current network is just to return null (a no-op)
+      try {
         await library.provider.request({
           method: 'wallet_switchEthereumChain',
           params: [{ chainId: formattedChainId }],
         })
+      } catch (error) {
+        console.debug('Added network but could not switch chains', error)
       }
     } else {
       throw error
