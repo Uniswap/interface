30e8c62ff919292b8665f222092218d835a89bea
crypzoh <crypzoh@protonmail.com>
2021-12-29 19:01:42 +0100

    Vote: Remove NavbarLink in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -294,11 +294,6 @@ export default function Header() {
         >
           <Trans>Pool</Trans>
         </StyledNavLink>
-        {(!chainId || chainId === SupportedChainId.MAINNET) && (
-          <StyledNavLink id={`vote-nav-link`} to={'/vote'}>
-            <Trans>Vote</Trans>
-          </StyledNavLink>
-        )}
         <StyledExternalLink id={`charts-nav-link`} href={infoLink}>
           <Trans>Charts</Trans>
           <sup>↗</sup>
