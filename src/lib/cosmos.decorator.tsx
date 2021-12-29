820220c85b99a678accd9054235e3ab4332d8a1a
Zach Pomerantz <zzmp@uniswap.org>
2021-12-29 10:12:13 -0800

    chore: mv web3 state to cosmos selectors

--- src/lib/cosmos.decorator.tsx
+++ src/lib/cosmos.decorator.tsx
@@ -1,16 +1,7 @@
 import { JSXElementConstructor, ReactElement } from 'react'
-import { createGlobalStyle } from 'styled-components'
 
 import Row from './components/Row'
-import Connectors from './cosmos/components/Connectors'
 import Widget from './cosmos/components/Widget'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-  }
-`
 
 export default function WidgetDecorator({
   children,
@@ -18,12 +9,8 @@ export default function WidgetDecorator({
   children: ReactElement<any, string | JSXElementConstructor<any>>
 }) {
   return (
-    <>
-      <GlobalStyle />
-      <Connectors />
-      <Row justify="center">
-        <Widget>{children}</Widget>
-      </Row>
-    </>
+    <Row justify="center">
+      <Widget>{children}</Widget>
+    </Row>
   )
 }
--- src/lib/cosmos/connectors/network.ts
+++ src/lib/cosmos/connectors/network.ts
@@ -1,6 +1,4 @@
 import { SupportedChainId } from 'constants/chains'
-import { initializeConnector } from 'widgets-web3-react/core'
-import { Network } from 'widgets-web3-react/network'
 
 const ALCHEMY_KEY = '-mzwnEVG3Ssm75WVbmsEpYiekfTF3W1z'
 const alchemyUrl = (network: string) => `https://${network}.alchemyapi.io/v2/${ALCHEMY_KEY}`
@@ -14,8 +12,3 @@ export const URLS = {
   [SupportedChainId.OPTIMISM]: [alchemyUrl('optimism-mainnet')],
   [SupportedChainId.ARBITRUM_ONE]: [alchemyUrl('arbitrum-mainnet')],
 }
-
-export const [network, hooks] = initializeConnector<Network>(
-  (actions) => new Network(actions, URLS),
-  Object.keys(URLS).map((chainId) => parseInt(chainId, 10))
-)
