995a92419ea821f343fe98aa99ad3d142a7e967e
nguyenhuudungz <34548156+nguyenhuudungz@users.noreply.github.com>
2021-12-29 10:23:22 +0700

    fix: swap notification message (#764)

--- src/state/transactions/updater.tsx
+++ src/state/transactions/updater.tsx
@@ -91,7 +91,7 @@ export default function Updater(): null {
       const inputAmount = getFullDisplayBalance(BigNumber.from(decodedValues[4].toString()), inputDecimals, 3)
       const outputAmount = getFullDisplayBalance(BigNumber.from(decodedValues[5].toString()), outputDecimals, 3)
 
-      const base = `Swap ${inputAmount} ${inputSymbol} for ${outputAmount} ${outputSymbol}`
+      const base = `${inputAmount} ${inputSymbol} for ${outputAmount} ${outputSymbol}`
 
       return `${base} ${withRecipient ?? ''}`
     },
