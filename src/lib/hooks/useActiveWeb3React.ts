f145a56f4ae86e337a42be7da7e74d8c02562b2f
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-01-05 20:48:33 -0600

    fix: web3-react-fixes (#3066) (#3075)
    * add empty connector
    
    add url connector
    
    * naming
    
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>

--- src/lib/hooks/useActiveWeb3React.ts
+++ src/lib/hooks/useActiveWeb3React.ts
@@ -1,11 +1,11 @@
 import { useAtomValue } from 'jotai/utils'
-import { injectedAtom, networkAtom, Web3ReactState } from 'lib/state/web3'
+import { injectedAtom, urlAtom, Web3ReactState } from 'lib/state/web3'
 import { Web3ReactHooks } from 'widgets-web3-react/core'
 
 export function useActiveWeb3ReactState(): Web3ReactState {
   const injected = useAtomValue(injectedAtom)
-  const network = useAtomValue(networkAtom)
-  return injected[1].useIsActive() ? injected : network
+  const url = useAtomValue(urlAtom)
+  return injected[1].useIsActive() ? injected : url
 }
 
 export function useActiveWeb3ReactHooks(): Web3ReactHooks {
--- src/lib/state/web3.ts
+++ src/lib/state/web3.ts
@@ -2,23 +2,15 @@ import { createMulticall } from '@uniswap/redux-multicall'
 import { atomWithStore } from 'jotai/redux'
 import { atomWithDefault } from 'jotai/utils'
 import { createStore } from 'redux'
-import { Web3ReactHooks } from 'widgets-web3-react/core'
-import { initializeConnector } from 'widgets-web3-react/core'
+import { initializeConnector, Web3ReactHooks } from 'widgets-web3-react/core'
+import { EMPTY } from 'widgets-web3-react/empty'
 import { Connector } from 'widgets-web3-react/types'
 
-// TODO(zzmp): EmptyConnector singleton should come from 'widgets-web3-react/empty'
-const EMPTY_CONNECTOR = initializeConnector<Connector>(
-  (actions) =>
-    new (class EmptyConnector extends Connector {
-      activate() {
-        void 0
-      }
-    })(actions)
-)
+const EMPTY_CONNECTOR = initializeConnector(() => EMPTY)
 
 export type Web3ReactState = [Connector, Web3ReactHooks]
 
-export const networkAtom = atomWithDefault<Web3ReactState>(() => EMPTY_CONNECTOR)
+export const urlAtom = atomWithDefault<Web3ReactState>(() => EMPTY_CONNECTOR)
 export const injectedAtom = atomWithDefault<Web3ReactState>(() => EMPTY_CONNECTOR)
 
 export const multicall = createMulticall()
