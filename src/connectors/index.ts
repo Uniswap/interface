5722902f96873be47769c101c373f0d6a600f15f
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-01-11 14:15:09 -0600

    add polygon to supported walletlink chainIds (#3096)

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -48,10 +48,9 @@ export const portis = new PortisConnector({
   networks: [1],
 })
 
-// mainnet only
 export const walletlink = new WalletLinkConnector({
   url: INFURA_NETWORK_URLS[SupportedChainId.MAINNET],
   appName: 'Uniswap',
   appLogoUrl: UNISWAP_LOGO_URL,
-  supportedChainIds: [SupportedChainId.MAINNET],
+  supportedChainIds: [SupportedChainId.MAINNET, SupportedChainId.POLYGON],
 })
