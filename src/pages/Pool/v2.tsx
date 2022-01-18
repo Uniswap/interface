0dee617533c17e18dccefa31d2af6da2a3cb865e
Noah <noah.genesis@protonmail.com>
2022-01-18 12:06:35 -0300

    Removes v2 from routes

--- src/pages/Pool/v2.tsx
+++ src/pages/Pool/v2.tsx
@@ -193,15 +193,12 @@ export default function Pool() {
                   </ThemedText.MediumHeader>
                 </HideSmall>
                 <ButtonRow>
-                  <ResponsiveButtonSecondary as={Link} padding="6px 8px" to="/add/v2/ETH">
-                    <Trans>Create a pair</Trans>
-                  </ResponsiveButtonSecondary>
-                  <ResponsiveButtonPrimary id="find-pool-button" as={Link} to="/pool/v2/find" padding="6px 8px">
+                  <ResponsiveButtonSecondary id="find-pool-button" as={Link} to="/pool/find" padding="6px 8px">
                     <Text fontWeight={500} fontSize={16}>
                       <Trans>Import Pool</Trans>
                     </Text>
-                  </ResponsiveButtonPrimary>
-                  <ResponsiveButtonPrimary id="join-pool-button" as={Link} to="/add/v2/ETH" padding="6px 8px">
+                  </ResponsiveButtonSecondary>
+                  <ResponsiveButtonPrimary id="join-pool-button" as={Link} to="/add/ETH" padding="6px 8px">
                     <Text fontWeight={500} fontSize={16}>
                       <Trans>Add V2 Liquidity</Trans>
                     </Text>
