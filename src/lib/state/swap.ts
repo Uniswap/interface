f9e974f94703449cae1173045dc9983a656383bd
ianlapham <ianlapham@gmail.com>
2022-01-19 17:37:14 -0500

    refactor: update swap state to match biz logic

--- src/lib/state/swap.ts
+++ src/lib/state/swap.ts
@@ -1,89 +1,42 @@
-import { Currency } from '@uniswap/sdk-core'
-import { SupportedChainId } from 'constants/chains'
-import { nativeOnChain } from 'constants/tokens'
-import { atom, WritableAtom } from 'jotai'
+import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
 import { atomWithImmer } from 'jotai/immer'
-import { useUpdateAtom } from 'jotai/utils'
 import { pickAtom } from 'lib/state/atoms'
-import { useMemo } from 'react'
 
 export enum Field {
-  INPUT = 'input',
-  OUTPUT = 'output',
+  INPUT = 'INPUT',
+  OUTPUT = 'OUTPUT',
 }
 
-export interface Input {
-  value?: number
-  token?: Currency
-  usdc?: number
-}
-
-export interface State {
-  activeInput: Field
-  [Field.INPUT]: Input & { approved?: boolean }
-  [Field.OUTPUT]: Input
-  swap?: {
-    lpFee: number
-    priceImpact: number
-    slippageTolerance: number
-    integratorFee?: number
-    maximumSent?: number
-    minimumReceived?: number
+export interface Swap {
+  independentField: Field
+  readonly amount: string
+  readonly [Field.INPUT]: {
+    readonly currencyId?: string
   }
+  readonly [Field.OUTPUT]: {
+    readonly currencyId?: string
+  }
+  integratorFee?: number
 }
 
-export const stateAtom = atomWithImmer<State>({
-  activeInput: Field.INPUT,
-  input: { token: nativeOnChain(SupportedChainId.MAINNET) },
-  output: {},
+export const swapAtom = atomWithImmer<Swap>({
+  independentField: Field.INPUT,
+  amount: '',
+  [Field.INPUT]: { currencyId: 'ETH' },
+  [Field.OUTPUT]: { currencyId: undefined },
 })
 
-export const swapAtom = pickAtom(stateAtom, 'swap')
+export const independentFieldAtom = pickAtom(swapAtom, 'independentField')
+export const integratorFeeAtom = pickAtom(swapAtom, 'integratorFee')
+export const amountAtom = pickAtom(swapAtom, 'amount')
 
-export const inputAtom = atom(
-  (get) => get(stateAtom).input,
-  (get, set, update: Input & { approved?: boolean }) => {
-    set(stateAtom, (state) => {
-      state.activeInput = Field.INPUT
-      state.input = update
-      state.swap = undefined
-    })
-  }
-)
-
-export const outputAtom = atom(
-  (get) => get(stateAtom).output,
-  (get, set, update: Input) => {
-    set(stateAtom, (state) => {
-      state.activeInput = Field.OUTPUT
-      state.output = update
-      state.swap = undefined
-    })
-  }
-)
-
-export function useUpdateInputValue(inputAtom: WritableAtom<Input, Input>) {
-  return useUpdateAtom(
-    useMemo(
-      () => atom(null, (get, set, value: Input['value']) => set(inputAtom, { token: get(inputAtom).token, value })),
-      [inputAtom]
-    )
-  )
-}
-
-export function useUpdateInputToken(inputAtom: WritableAtom<Input, Input>) {
-  return useUpdateAtom(
-    useMemo(() => atom(null, (get, set, token: Input['token']) => set(inputAtom, { token })), [inputAtom])
-  )
-}
-
-export interface Transaction {
-  input: Required<Pick<Input, 'token' | 'value'>>
-  output: Required<Pick<Input, 'token' | 'value'>>
+export interface SwapTransaction {
+  input: CurrencyAmount<Currency>
+  output: CurrencyAmount<Currency>
   receipt: string
   timestamp: number
   elapsedMs?: number
   status?: true | Error
 }
 
-export const transactionAtom = atomWithImmer<Transaction | null>(null)
+export const transactionAtom = atomWithImmer<SwapTransaction | null>(null)
