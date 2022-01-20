1018e5ffca340ffad8c3b4f46632819cfc0223f9
Zach Pomerantz <zzmp@uniswap.org>
2022-01-20 08:46:18 -0800

    fix: gate old web3 on widget env

--- src/hooks/useActiveWeb3React.ts
+++ src/hooks/useActiveWeb3React.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { Web3Provider } from '@ethersproject/providers'
 import { useWeb3React } from '@web3-react/core'
 import { default as useWidgetsWeb3React } from 'lib/hooks/useActiveWeb3React'
@@ -5,15 +6,15 @@ import { default as useWidgetsWeb3React } from 'lib/hooks/useActiveWeb3React'
 import { NetworkContextName } from '../constants/misc'
 
 export default function useActiveWeb3React() {
-  const widgetsContext = useWidgetsWeb3React()
+  if (process.env.REACT_APP_IS_WIDGET) {
+    return useWidgetsWeb3React()
+  }
+
   const interfaceContext = useWeb3React<Web3Provider>()
   const interfaceNetworkContext = useWeb3React<Web3Provider>(
     process.env.REACT_APP_IS_WIDGET ? undefined : NetworkContextName
   )
 
-  if (process.env.REACT_APP_IS_WIDGET) {
-    return widgetsContext
-  }
   if (interfaceContext.active) {
     return interfaceContext
   }
