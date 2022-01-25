e769b86d2e3029710fbaa078a7b58da04d4f122a
Zach Pomerantz <zzmp@uniswap.org>
2022-01-25 15:23:15 -0800

    fix: comment usage of trade for optimized trade

--- src/lib/components/Swap/SwapButton.tsx
+++ src/lib/components/Swap/SwapButton.tsx
@@ -29,8 +29,10 @@ export default function SwapButton({ disabled }: SwapButtonProps) {
   }, [trade])
 
   // TODO(zzmp): Return an optimized trade directly from useSwapInfo.
-  const optimizedTrade = useSwapApprovalOptimizedTrade(trade.trade, allowedSlippage, useIsPendingApproval)
-  const [approval, getApproval] = useSwapApproval(optimizedTrade || trade.trade, allowedSlippage, useIsPendingApproval)
+  const optimizedTrade =
+    // Use trade.trade if there is no swap optimized trade. This occurs if approvals are still pending.
+    useSwapApprovalOptimizedTrade(trade.trade, allowedSlippage, useIsPendingApproval) || trade.trade
+  const [approval, getApproval] = useSwapApproval(optimizedTrade, allowedSlippage, useIsPendingApproval)
 
   const addTransaction = useAddTransaction()
   const addApprovalTransaction = useCallback(() => {
