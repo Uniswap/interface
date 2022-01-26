ce96873a720d27e6152b54b54ce943c19a8624a6
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-01-26 13:14:18 -0500

    feat(widgets): use default input/output (#3161)
    * feat: use default input/output on chain switch
    
    * feat(widgets): ErrorGenerator -> PropValidator
    
    * default prop validation
    
    * useDefaults hook
    
    * pr feedback
    
    * fix cosmos
    
    * drop token map changes
    
    * add default inputs to cosmos fixture
    
    * set up different validation layers for widget and swap
    
    * split widget/swap prop types
    
    * cleanup
    
    * pr feedback
    
    * clear defaults when they're no longer valid on the current chain
    
    * remove state checks on validators
    
    * stop using address in cosmos fixture
    
    * pr feedback
    
    * useMemo on useSwapDefaults args
    
    * tell the user what they gave to error'd props
    
    Co-authored-by: Zach Pomerantz <zzmp@uniswap.org>

--- src/lib/components/Swap/index.tsx
+++ src/lib/components/Swap/index.tsx
@@ -1,12 +1,10 @@
 import { Trans } from '@lingui/macro'
 import { TokenInfo } from '@uniswap/token-lists'
-import { nativeOnChain } from 'constants/tokens'
-import { useSwapAmount, useSwapCurrency } from 'lib/hooks/swap'
+import useSwapDefaults from 'lib/hooks/swap/useSwapDefaults'
 import { SwapInfoUpdater } from 'lib/hooks/swap/useSwapInfo'
 import useActiveWeb3React from 'lib/hooks/useActiveWeb3React'
-import useTokenList, { DEFAULT_TOKEN_LIST } from 'lib/hooks/useTokenList'
-import { Field } from 'lib/state/swap'
-import { useLayoutEffect, useMemo, useState } from 'react'
+import useTokenList from 'lib/hooks/useTokenList'
+import { useState } from 'react'
 
 import Header from '../Header'
 import { BoundaryProvider } from '../Popover'
@@ -16,60 +14,29 @@ import Output from './Output'
 import ReverseButton from './ReverseButton'
 import Settings from './Settings'
 import SwapButton from './SwapButton'
+import SwapPropValidator from './SwapPropValidator'
 import Toolbar from './Toolbar'
 
-interface DefaultTokenAmount {
-  address?: string | { [chainId: number]: string }
-  amount?: number
-}
-
-interface SwapDefaults {
-  tokenList: string | TokenInfo[]
-  input: DefaultTokenAmount
-  output: DefaultTokenAmount
-}
-
-const DEFAULT_INPUT: DefaultTokenAmount = { address: 'ETH' }
-const DEFAULT_OUTPUT: DefaultTokenAmount = {}
-
-function useSwapDefaults(defaults: Partial<SwapDefaults> = {}): SwapDefaults {
-  const tokenList = defaults.tokenList || DEFAULT_TOKEN_LIST
-  const input: DefaultTokenAmount = defaults.input || DEFAULT_INPUT
-  const output: DefaultTokenAmount = defaults.output || DEFAULT_OUTPUT
-  input.amount = input.amount || 0
-  output.amount = output.amount || 0
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useMemo(() => ({ tokenList, input, output }), [])
-}
-
+export type DefaultAddress = string | { [chainId: number]: string | 'NATIVE' } | 'NATIVE'
 export interface SwapProps {
+  tokenList?: string | TokenInfo[]
+  defaultInputAddress?: DefaultAddress
+  defaultInputAmount?: string
+  defaultOutputAddress?: DefaultAddress
+  defaultOutputAmount?: string
   convenienceFee?: number
-  convenienceFeeRecipient?: string // TODO: improve typing to require recipient when fee is set
-  defaults?: Partial<SwapDefaults>
+  convenienceFeeRecipient?: string | { [chainId: number]: string }
 }
 
-export default function Swap({ defaults }: SwapProps) {
-  const { tokenList } = useSwapDefaults(defaults)
-  useTokenList(tokenList)
+export default function Swap(props: SwapProps) {
+  useTokenList(props.tokenList)
+  useSwapDefaults(props)
 
+  const { active, account } = useActiveWeb3React()
   const [boundary, setBoundary] = useState<HTMLDivElement | null>(null)
-  const { chainId, active, account } = useActiveWeb3React()
-
-  // Switch to on-chain currencies if/when chain changes to prevent chain mismatched currencies.
-  const [, updateSwapInputCurrency] = useSwapCurrency(Field.INPUT)
-  const [, updateSwapOutputCurrency] = useSwapCurrency(Field.OUTPUT)
-  const [, updateSwapInputAmount] = useSwapAmount(Field.INPUT)
-  useLayoutEffect(() => {
-    if (chainId) {
-      updateSwapInputCurrency(nativeOnChain(chainId))
-      updateSwapOutputCurrency()
-      updateSwapInputAmount('')
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chainId])
 
   return (
-    <>
+    <SwapPropValidator {...props}>
       <SwapInfoUpdater />
       <Header logo title={<Trans>Swap</Trans>}>
         {active && <Wallet disabled={!account} />}
@@ -85,6 +52,6 @@ export default function Swap({ defaults }: SwapProps) {
           </Output>
         </BoundaryProvider>
       </div>
-    </>
+    </SwapPropValidator>
   )
 }
---
+++ src/lib/hooks/swap/useSwapDefaults.ts
@@ -0,0 +1,70 @@
+import { Currency } from '@uniswap/sdk-core'
+import { nativeOnChain } from 'constants/tokens'
+import { useUpdateAtom } from 'jotai/utils'
+import { DefaultAddress } from 'lib/components/Swap'
+import useActiveWeb3React from 'lib/hooks/useActiveWeb3React'
+import { useToken } from 'lib/hooks/useCurrency'
+import { Field, Swap, swapAtom } from 'lib/state/swap'
+import { useCallback, useLayoutEffect, useState } from 'react'
+
+function useDefaultToken(
+  defaultAddress: DefaultAddress | undefined,
+  chainId: number | undefined
+): Currency | null | undefined {
+  let address = undefined
+  if (typeof defaultAddress === 'string') {
+    address = defaultAddress
+  } else if (typeof defaultAddress === 'object' && chainId) {
+    address = defaultAddress[chainId]
+  }
+  const token = useToken(address)
+  if (chainId && address === 'NATIVE') {
+    return nativeOnChain(chainId)
+  }
+  return token
+}
+
+interface UseSwapDefaultsArgs {
+  defaultInputAddress?: DefaultAddress
+  defaultInputAmount?: string
+  defaultOutputAddress?: DefaultAddress
+  defaultOutputAmount?: string
+}
+
+export default function useSwapDefaults({
+  defaultInputAddress,
+  defaultInputAmount,
+  defaultOutputAddress,
+  defaultOutputAmount,
+}: UseSwapDefaultsArgs) {
+  const updateSwap = useUpdateAtom(swapAtom)
+  const { chainId } = useActiveWeb3React()
+  const defaultInputToken = useDefaultToken(defaultInputAddress, chainId)
+  const defaultOutputToken = useDefaultToken(defaultOutputAddress, chainId)
+
+  const setToDefaults = useCallback(() => {
+    const defaultSwapState: Swap = {
+      amount: '',
+      [Field.INPUT]: defaultInputToken || undefined,
+      [Field.OUTPUT]: defaultOutputToken || undefined,
+      independentField: Field.INPUT,
+    }
+    if (defaultInputAmount && defaultInputToken) {
+      defaultSwapState.amount = defaultInputAmount
+    } else if (defaultOutputAmount && defaultOutputToken) {
+      defaultSwapState.independentField = Field.OUTPUT
+      defaultSwapState.amount = defaultOutputAmount
+    }
+    updateSwap((swap) => ({ ...swap, ...defaultSwapState }))
+  }, [defaultInputToken, defaultOutputToken, defaultInputAmount, defaultOutputAmount, updateSwap])
+
+  const [previousChainId, setPreviousChainId] = useState(chainId)
+  useLayoutEffect(() => {
+    setPreviousChainId(chainId)
+  }, [chainId])
+  useLayoutEffect(() => {
+    if (chainId && chainId !== previousChainId) {
+      setToDefaults()
+    }
+  }, [chainId, previousChainId, setToDefaults])
+}
