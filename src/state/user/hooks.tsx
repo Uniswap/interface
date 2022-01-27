6f5f8da13e534d261471def9ddf0f9c3de0c9a3e
crypzoh <crypzoh@protonmail.com>
2022-01-27 00:07:09 +0000

    Rename the LP Position Token to DIFF

--- src/state/user/hooks.tsx
+++ src/state/user/hooks.tsx
@@ -228,7 +228,7 @@ export function useURLWarningToggle(): () => void {
  * @param tokenB the other token
  */
 export function toV2LiquidityToken([tokenA, tokenB]: [Token, Token]): Token {
-  return new Token(tokenA.chainId, Pair.getAddress(tokenA, tokenB), 18, 'UNI-V2', 'Uniswap V2')
+  return new Token(tokenA.chainId, Pair.getAddress(tokenA, tokenB), 18, 'DIFF', 'Diffusion LP')
 }
 
 /**
