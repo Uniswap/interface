3ac84594d6852cac019239264e63009a3df34cdb
Zach Pomerantz <zzmp@uniswap.org>
2022-01-26 16:16:04 -0800

    feat: expand Row grow

--- src/lib/components/Row.tsx
+++ src/lib/components/Row.tsx
@@ -8,7 +8,7 @@ const Row = styled.div<{
   pad?: number
   gap?: number
   flex?: true
-  grow?: true
+  grow?: true | 'first' | 'last'
   children?: ReactNode
   theme: Theme
 }>`
@@ -19,7 +19,12 @@ const Row = styled.div<{
   flex-grow: ${({ grow }) => grow && 1};
   gap: ${({ gap }) => gap && `${gap}em`};
   grid-auto-flow: column;
-  grid-template-columns: ${({ grow, children }) => (grow ? `repeat(${Children.count(children)}, 1fr)` : '')};
+  grid-template-columns: ${({ grow, children }) => {
+    if (grow === 'first') return '1fr'
+    if (grow === 'last') return `repeat(${Children.count(children) - 1}, auto) 1fr`
+    if (grow) return `repeat(${Children.count(children)}, 1fr)`
+    return undefined
+  }};
   justify-content: ${({ justify }) => justify ?? 'space-between'};
   padding: ${({ pad }) => pad && `0 ${pad}em`};
 `
