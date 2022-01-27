f9bbd93ddd03e20261517f13632a0ef6436b0f69
crypzoh <crypzoh@protonmail.com>
2022-01-27 22:45:19 +0000

    Fix Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 const { dirname, join, parse, resolve } = require('path')
 const { existsSync } = require('fs')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
@@ -12,5 +13,14 @@ module.exports = {
       shouldExtractLiteralValuesFromEnum: true,
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ]
+    return config
   },
 }
