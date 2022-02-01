436ae612977b21367571c5dc9674aa7bc4e02fd2
Zach Pomerantz <zzmp@uniswap.org>
2022-02-01 14:11:18 -0800

    fix: better max slippage popovers

--- src/lib/components/Swap/Settings/MaxSlippageSelect.tsx
+++ src/lib/components/Swap/Settings/MaxSlippageSelect.tsx
@@ -2,11 +2,11 @@ import { Trans } from '@lingui/macro'
 import { Percent } from '@uniswap/sdk-core'
 import { useAtom } from 'jotai'
 import Popover from 'lib/components/Popover'
-import Tooltip from 'lib/components/Tooltip'
-import { AlertTriangle, Check, LargeIcon, XOctagon } from 'lib/icons'
+import { TooltipHandlers, useTooltip } from 'lib/components/Tooltip'
+import { AlertTriangle, Check, Icon, LargeIcon, XOctagon } from 'lib/icons'
 import { MAX_VALID_SLIPPAGE, maxSlippageAtom, MIN_HIGH_SLIPPAGE } from 'lib/state/settings'
-import styled, { ThemedText } from 'lib/theme'
-import { PropsWithChildren, ReactNode, useCallback, useMemo, useRef, useState } from 'react'
+import styled, { Color, ThemedText } from 'lib/theme'
+import { memo, PropsWithChildren, ReactNode, useCallback, useEffect, useRef, useState } from 'react'
 
 import { BaseButton, TextButton } from '../../Button'
 import Column from '../../Column'
@@ -22,11 +22,6 @@ const invalidSlippage = <Trans>Please enter a valid slippage %</Trans>
 
 const placeholder = '0.10'
 
-enum Warning {
-  HIGH_SLIPPAGE,
-  INVALID_SLIPPAGE,
-}
-
 const Button = styled(TextButton)<{ selected: boolean }>`
   ${({ selected }) => optionCss(selected)}
 `
@@ -37,16 +32,23 @@ const Custom = styled(BaseButton)<{ selected: boolean }>`
   padding: calc(0.75em - 3px) 0.625em;
 `
 
-interface OptionProps {
+interface OptionProps extends Partial<TooltipHandlers> {
   wrapper: typeof Button | typeof Custom
   selected: boolean
   onSelect: () => void
   icon?: ReactNode
 }
 
-function Option({ wrapper: Wrapper, children, selected, onSelect, icon }: PropsWithChildren<OptionProps>) {
+function Option({
+  wrapper: Wrapper,
+  children,
+  selected,
+  onSelect,
+  icon,
+  ...tooltipHandlers
+}: PropsWithChildren<OptionProps>) {
   return (
-    <Wrapper selected={selected} onClick={onSelect}>
+    <Wrapper selected={selected} onClick={onSelect} {...tooltipHandlers}>
       <Row gap={0.5}>
         {children}
         {icon ? icon : <LargeIcon icon={selected ? Check : undefined} size={1.25} />}
@@ -55,70 +57,80 @@ function Option({ wrapper: Wrapper, children, selected, onSelect, icon }: PropsW
   )
 }
 
+enum WarningState {
+  NONE,
+  HIGH_SLIPPAGE,
+  INVALID_SLIPPAGE,
+}
+
+const Warning = memo(function Warning({ state, showTooltip }: { state: WarningState; showTooltip: boolean }) {
+  let icon: Icon
+  let color: Color
+  let content: ReactNode
+  let show = showTooltip
+  switch (state) {
+    case WarningState.INVALID_SLIPPAGE:
+      icon = XOctagon
+      color = 'error'
+      content = invalidSlippage
+      show = true
+      break
+    case WarningState.HIGH_SLIPPAGE:
+      icon = AlertTriangle
+      color = 'warning'
+      content = highSlippage
+      break
+    case WarningState.NONE:
+      return null
+  }
+  return (
+    <Popover
+      key={state}
+      content={<ThemedText.Caption>{content}</ThemedText.Caption>}
+      show={show}
+      placement="top"
+      offset={16}
+      contained
+    >
+      <LargeIcon icon={icon} color={color} size={1.25} />
+    </Popover>
+  )
+})
+
 export default function MaxSlippageSelect() {
   const [maxSlippage, setMaxSlippage] = useAtom(maxSlippageAtom)
 
   const [custom, setCustom] = useState('')
-  const [warning, setWarning] = useState<Warning | undefined>()
   const input = useRef<HTMLInputElement>(null)
   const focus = useCallback(() => input.current?.focus(), [input])
 
-  const warningPopover = useMemo(() => {
-    switch (warning) {
-      case Warning.HIGH_SLIPPAGE:
-        return (
-          <Tooltip
-            placement="top"
-            icon={LargeIcon}
-            iconProps={{ icon: AlertTriangle, color: 'warning', size: 1.25 }}
-            offset={16}
-            contained
-          >
-            <ThemedText.Caption>{highSlippage}</ThemedText.Caption>
-          </Tooltip>
-        )
-      case Warning.INVALID_SLIPPAGE:
-        return (
-          <Popover
-            content={<ThemedText.Caption>{invalidSlippage}</ThemedText.Caption>}
-            show={true}
-            placement="top"
-            offset={16}
-            contained
-          >
-            <LargeIcon icon={XOctagon} color="error" size={1.25} />
-          </Popover>
-        )
-        return
-      default:
-        return
-    }
-  }, [warning])
+  const [warning, setWarning] = useState(WarningState.NONE)
+  const [showTooltip, setShowTooltip, tooltipProps] = useTooltip()
+  useEffect(() => setShowTooltip(true), [warning, setShowTooltip]) // enables the tooltip if a warning is set
 
-  const onInputChange = useCallback(
-    (custom: string) => {
-      setCustom(custom)
-      const numerator = Math.floor(+custom * 100)
-      if (numerator) {
-        const percent = new Percent(numerator, 10_000)
-        if (percent.greaterThan(MAX_VALID_SLIPPAGE)) {
-          setWarning(Warning.INVALID_SLIPPAGE)
-          setMaxSlippage('auto')
-          return
-        } else if (percent.greaterThan(MIN_HIGH_SLIPPAGE)) {
-          setWarning(Warning.HIGH_SLIPPAGE)
-        }
+  const processInput = useCallback(() => {
+    const numerator = Math.floor(+custom * 100)
+    if (numerator) {
+      const percent = new Percent(numerator, 10_000)
+      if (percent.greaterThan(MAX_VALID_SLIPPAGE)) {
+        setWarning(WarningState.INVALID_SLIPPAGE)
+        setMaxSlippage('auto')
+      } else if (percent.greaterThan(MIN_HIGH_SLIPPAGE)) {
+        setWarning(WarningState.HIGH_SLIPPAGE)
         setMaxSlippage(percent)
       } else {
-        setMaxSlippage('auto')
+        setWarning(WarningState.NONE)
+        setMaxSlippage(percent)
       }
-    },
-    [setMaxSlippage]
-  )
+    } else {
+      setMaxSlippage('auto')
+    }
+  }, [custom, setMaxSlippage])
+  useEffect(processInput, [processInput])
   const onInputSelect = useCallback(() => {
     focus()
-    onInputChange(custom)
-  }, [custom, focus, onInputChange])
+    processInput()
+  }, [focus, processInput])
 
   return (
     <Column gap={0.75}>
@@ -129,12 +141,18 @@ export default function MaxSlippageSelect() {
             <Trans>Auto</Trans>
           </ThemedText.ButtonMedium>
         </Option>
-        <Option wrapper={Custom} selected={maxSlippage !== 'auto'} onSelect={onInputSelect} icon={warningPopover}>
+        <Option
+          wrapper={Custom}
+          selected={maxSlippage !== 'auto'}
+          onSelect={onInputSelect}
+          icon={<Warning state={warning} showTooltip={showTooltip} />}
+          {...tooltipProps}
+        >
           <Row>
             <DecimalInput
               size={Math.max(custom.length, 3)}
               value={custom}
-              onChange={onInputChange}
+              onChange={setCustom}
               placeholder={placeholder}
               ref={input}
             />
