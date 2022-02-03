43931dd689ae8a2855be77b0ee0640bfe9e2958e
Zach Pomerantz <zzmp@uniswap.org>
2022-02-03 11:30:50 -0800

    feat: chain-specific ttls (#3228)

--- src/lib/state/settings.ts
+++ src/lib/state/settings.ts
@@ -6,13 +6,9 @@ import { pickAtom, setTogglable } from './atoms'
 export const MAX_VALID_SLIPPAGE = new Percent(1, 2)
 export const MIN_HIGH_SLIPPAGE = new Percent(1, 100)
 
-// transaction deadline in minutes, needs to be adjusted to seconds before use
-// @TODO(ianlapham): update this to be stored as seconds
-export const TRANSACTION_TTL_DEFAULT = 30
-
 interface Settings {
   maxSlippage: Percent | 'auto' // auto will cause slippage to resort to default calculation
-  transactionTtl: number | undefined
+  transactionTtl: number | undefined // transaction ttl in minutes
   integratorFee: number | undefined
   mockTogglable: boolean
   clientSideRouter: boolean // whether to use the client-side router or query the remote API
