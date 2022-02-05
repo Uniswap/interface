1db8c567038e93f98d1dd2575c0af6800f9e0df3
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-02-04 23:53:30 -0600

    test formatLocaleNumber

--- src/lib/utils/formatLocaleNumber.ts
+++ src/lib/utils/formatLocaleNumber.ts
@@ -1,10 +1,12 @@
 import { BigintIsh } from '@uniswap/sdk-core'
 import { SupportedLocale } from 'constants/locales'
 
-export default function formatLocaleNumber(
-  n: BigintIsh,
-  locale: SupportedLocale | null | undefined,
-  options: Intl.NumberFormatOptions | undefined
-): string {
-  return n.toLocaleString(locale ? [locale, 'en-US'] : 'en-US', options)
+interface FormatLocaleNumberArgs {
+  number: BigintIsh
+  locale: SupportedLocale | null | undefined
+  options?: Intl.NumberFormatOptions
+}
+
+export default function formatLocaleNumber({ number, locale, options }: FormatLocaleNumberArgs): string {
+  return number.toLocaleString(locale ? [locale, 'en-US'] : 'en-US', options)
 }
