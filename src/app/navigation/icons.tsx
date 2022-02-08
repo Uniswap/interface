2b9ad3c15290717c889f23836b4a89923a82288c
Tina <59578595+tinaszheng@users.noreply.github.com>
2022-02-08 11:05:55 -0800

    feat: navbar dot under focused tab (#442)
    * box, header, and graph loading states
    
    * add back hermes lol
    
    * adjust heights
    
    * run pod install
    
    * use Box instead of View
    
    * most const out of component
    
    * add maskview to jest ignore
    
    * add expo-linear-gradient to ignore
    
    * add story
    
    * add to storybook
    
    * add dot to focused tab
    
    * use EmptyCircleg

--- src/app/navigation/icons.tsx
+++ src/app/navigation/icons.tsx
@@ -1,10 +1,12 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs'
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
 import { useAppTheme } from 'src/app/hooks'
 import SearchIcon from 'src/assets/icons/search.svg'
 import SwapIcon from 'src/assets/icons/swap.svg'
 import WalletIcon from 'src/assets/icons/wallet.svg'
 import { IconButton } from 'src/components/buttons/IconButton'
+import { EmptyCircle } from 'src/components/icons/EmptyCircle'
+import { Flex } from 'src/components/layout'
 import { ElementName } from 'src/features/telemetry/constants'
 
 export function SwapButton(props: BottomTabBarButtonProps) {
@@ -26,19 +28,32 @@ interface NavIconProps {
 
 export function WalletTabIcon({ focused }: NavIconProps) {
   const primaryColor = useTabNavColors()
-  return <WalletIcon fill={primaryColor} height={30} opacity={focused ? 1 : 0.3} width={30} />
+  return (
+    <WithDot focused={focused}>
+      <WalletIcon fill={primaryColor} opacity={focused ? 1 : 0.3} />
+    </WithDot>
+  )
 }
 
 export function ExploreTabIcon({ focused }: NavIconProps) {
   const primaryColor = useTabNavColors()
   return (
-    <SearchIcon
-      height={30}
-      opacity={focused ? 1 : 0.3}
-      stroke={primaryColor}
-      strokeWidth={2.2}
-      width={30}
-    />
+    <WithDot focused={focused}>
+      <SearchIcon opacity={focused ? 1 : 0.3} stroke={primaryColor} strokeWidth={2.2} />
+    </WithDot>
+  )
+}
+
+function WithDot({ children, focused }: PropsWithChildren<NavIconProps>) {
+  return (
+    <Flex alignItems="center" gap="xs">
+      {/* this is kind of a hack but im rendering a dot above and below so that
+        the main icon stays centered within the navbar even when there is a dot below it
+      */}
+      <EmptyCircle backgroundColor="none" borderWidth={0} size={4} />
+      {children}
+      <EmptyCircle backgroundColor={focused ? 'primary1' : 'none'} borderWidth={0} size={4} />
+    </Flex>
   )
 }
 
