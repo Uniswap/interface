e65a80205a9408ae0cf145f3bd19d79681f8f4f2
Adam Azad <adam@adamazad.com>
2022-02-08 20:56:06 +0100

    feat(gas-info): refactor

--- src/hooks/useGasInfo.ts
+++ src/hooks/useGasInfo.ts
@@ -3,20 +3,26 @@ import { useActiveWeb3React } from '.'
 import { ChainId } from '@swapr/sdk'
 import { INFURA_PROJECT_ID } from '../connectors'
 
-const gasInfoChainUrls: {
-  [chainId: number]: { url: string; body?: any; objectDestrucutre?: any; deepDestrucuture?: string }
-} = {
+interface ChainGasInfo {
+  [chainId: number]: {
+    url: string
+    requestConfig?: RequestInit
+    keys?: string[]
+  }
+}
+
+const gasInfoChainUrls: ChainGasInfo = {
   [ChainId.MAINNET]: {
     url: 'http://ethgas.watch/api/gas',
-    objectDestrucutre: ['normal', 'fast', 'slow']
+    keys: ['normal', 'fast', 'slow']
   },
   [ChainId.XDAI]: {
     url: 'https://blockscout.com/xdai/mainnet/api/v1/gas-price-oracle',
-    objectDestrucutre: ['average', 'fast', 'slow']
+    keys: ['average', 'fast', 'slow']
   },
   [ChainId.ARBITRUM_ONE]: {
     url: `https://arbitrum-mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
-    body: {
+    requestConfig: {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -30,56 +36,78 @@ const gasInfoChainUrls: {
     }
   }
 }
-interface GasTypes {
+
+interface Gas {
   fast: number
   normal: number
   slow: number
 }
-export function useGasInfo(): { loading: boolean; gas: GasTypes } {
+
+const defaultGasState: Gas = {
+  fast: 0,
+  normal: 0,
+  slow: 0
+}
+
+/**
+ * Fetches and return gas price information for the current active chain
+ * @returns Gas prices
+ */
+export function useGasInfo(): { loading: boolean; gas: Gas } {
   const { chainId } = useActiveWeb3React()
   const [loading, setLoading] = useState<boolean>(true)
-  const [gas, setGas] = useState<GasTypes>({ normal: 0, fast: 0, slow: 0 })
+  const [gas, setGas] = useState<Gas>(defaultGasState)
 
   useEffect(() => {
     if (!chainId || !gasInfoChainUrls[chainId]) {
-      setLoading(true)
-      setGas({ normal: 0, fast: 0, slow: 0 })
-    } else {
-      fetch(gasInfoChainUrls[chainId].url, gasInfoChainUrls[chainId].body)
-        .then(res => res.json())
-        .then(data => {
-          let average
-          let fast = 0
-          let slow = 0
-          if (chainId === ChainId.MAINNET || chainId === ChainId.XDAI) {
-            const averageData = data[gasInfoChainUrls[chainId].objectDestrucutre[0]]
-            const fastData = data[gasInfoChainUrls[chainId].objectDestrucutre[1]]
-            const slowData = data[gasInfoChainUrls[chainId].objectDestrucutre[2]]
-            if (chainId === ChainId.MAINNET) {
-              average = averageData.gwei
-              fast = fastData.gwei
-              slow = slowData.gwei
-            } else {
-              average = averageData
-              fast = fastData
-              slow = slowData
-            }
-          } else {
-            average = (parseInt(data.result, 16) / 1e9).toFixed(3)
-          }
-
-          setLoading(false)
-          setGas({ normal: average, fast: fast, slow: slow })
-        })
-        .catch(e => {
-          console.error('gasInfo error: ', e)
-          setLoading(true)
-          setGas({ normal: 0, fast: 0, slow: 0 })
-        })
-        .finally(() => {
-          setLoading(false)
-        })
+      setLoading(false)
+      setGas(defaultGasState)
+      return
     }
+
+    // Fetch gas price data
+    const chainGasInfo = gasInfoChainUrls[chainId]
+
+    fetch(chainGasInfo.url, chainGasInfo.requestConfig)
+      .then(res => res.json())
+      .then(data => {
+        // Default gas prices
+        let { normal, slow, fast } = defaultGasState
+
+        // Mainnet and xDAI uses external API
+        if (chainId === ChainId.MAINNET || chainId === ChainId.XDAI) {
+          const keys = chainGasInfo.keys!
+          // Pick the keys
+          const gasNormalData = data[keys[0]]
+          const gasFastData = data[keys[1]]
+          const gasSlowData = data[keys[2]]
+          // ethgas.watch returns both USD and Gwei units
+          if (chainId === ChainId.MAINNET) {
+            normal = gasNormalData.gwei
+            fast = gasFastData.gwei
+            slow = gasSlowData.gwei
+          } else {
+            normal = gasNormalData
+            fast = gasFastData
+            slow = gasSlowData
+          }
+        } else {
+          // On Arbitrum (and other L2's), parse Gwei to decimal and round the number
+          // There is no fast nor slow gas prices
+          normal = Math.round(parseInt(data.result, 16) / 1e9)
+        }
+        // Update state
+        setGas({ normal, fast, slow })
+      })
+      .catch(e => {
+        console.error('useGasInfo error: ', e)
+        setGas(defaultGasState)
+      })
+      .finally(() => setLoading(false))
   }, [chainId])
-  return { loading, gas }
+
+  return {
+    loading,
+    gas
+  }
 }
