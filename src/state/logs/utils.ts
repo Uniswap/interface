6da6498197aa7373ee92104e4c6e6557beb776fa
Josh <jonesjosh41@gmail.com>
2022-02-08 09:57:30 -0600

    latest updates

--- src/state/logs/utils.ts
+++ src/state/logs/utils.ts
@@ -1,18 +1,20 @@
-import gql from 'graphql-tag'
 import { ApolloClient, useQuery } from '@apollo/client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import moment from 'moment'
+import { INFO_CLIENT, fetchBscTokenData, getDeltaTimestamps, useBlocksFromTimestamps, useBnbPrices, useBscTokenDataHook } from './bscUtils'
 import React, { useCallback } from 'react'
-import useInterval from 'hooks/useInterval'
-import _, { isEqual } from 'lodash'
-import { fetchBscTokenData, getDeltaTimestamps, INFO_CLIENT, useBlocksFromTimestamps, useBnbPrices, useBscTokenDataHook } from './bscUtils'
-import { useWeb3React } from '@web3-react/core'
-import { useActiveWeb3React } from 'hooks/web3'
-import { useKiba } from 'pages/Vote/VotePage'
 import { Token, WETH9 } from '@uniswap/sdk-core'
-import { useTokenBalance } from 'state/wallet/hooks'
-import { binanceTokens } from 'utils/binance.tokens'
+import _, { isEqual } from 'lodash'
+
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import { V2_ROUTER_ADDRESS } from 'constants/addresses'
+import { binanceTokens } from 'utils/binance.tokens'
+import gql from 'graphql-tag'
+import moment from 'moment'
+import { useActiveWeb3React } from 'hooks/web3'
+import useInterval from 'hooks/useInterval'
+import { useKiba } from 'pages/Vote/VotePage'
+import { useTokenBalance } from 'state/wallet/hooks'
+import { useWeb3React } from '@web3-react/core'
+
 export interface EventFilter {
   address?: string
   topics?: Array<string | Array<string> | null>
@@ -137,9 +139,25 @@ export const TOKEN_DATA = (tokenAddress: string, block: any, isBnb?: boolean) =>
       }
       pairs0: pairs(where: {token0: "${tokenAddress}"}, first: 2, orderBy: reserveUSD, orderDirection: desc){
         id
+        token0 {
+          id
+          symbol
+        }
+        token1 {
+          id
+          symbol
+        }
       }
       pairs1: pairs(where: {token1: "${tokenAddress}"}, first: 2, orderBy: reserveUSD, orderDirection: desc){
         id
+        token0 {
+          id
+          symbol
+        }
+        token1 {
+          id
+          symbol
+        }
       }
     }
   `
