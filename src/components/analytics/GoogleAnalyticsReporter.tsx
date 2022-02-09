5469c42887f3a99dfea702d5689a6f7315677db9
Tina Zheng <tina.s.zheng+github@gmail.com>
2022-02-09 11:04:35 -0800

    store client id in localstorage

--- src/components/analytics/GoogleAnalyticsReporter.tsx
+++ src/components/analytics/GoogleAnalyticsReporter.tsx
@@ -1,8 +1,10 @@
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { useEffect } from 'react'
-import ReactGA from 'react-ga'
+import ReactGA, { ga } from 'react-ga'
 import { RouteComponentProps } from 'react-router-dom'
 import { getCLS, getFCP, getFID, getLCP, Metric } from 'web-vitals'
+
+import { GOOGLE_ANALYTICS_CLIENT_ID_STORAGE_KEY } from '.'
 
 function reportWebVitals({ name, delta, id }: Metric) {
   ReactGA.timing({
@@ -23,6 +25,7 @@ export default function GoogleAnalyticsReporter({ location: { pathname, search }
   }, [])
 
   const { chainId } = useActiveWeb3React()
+
   useEffect(() => {
     // cd1 - custom dimension 1 - chainId
     ReactGA.set({ cd1: chainId ?? 0 })
@@ -31,5 +34,15 @@ export default function GoogleAnalyticsReporter({ location: { pathname, search }
   useEffect(() => {
     ReactGA.pageview(`${pathname}${search}`)
   }, [pathname, search])
+
+  useEffect(() => {
+    // typed as 'any' in react-ga -.-
+    ga((tracker: any) => {
+      if (!tracker) return
+
+      const clientId = tracker.get('clientId')
+      window.localStorage.setItem(GOOGLE_ANALYTICS_CLIENT_ID_STORAGE_KEY, clientId)
+    })
+  }, [])
   return null
 }
