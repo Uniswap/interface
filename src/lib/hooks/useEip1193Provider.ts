9cc4d8b29c57a5937b51fe9b98e5b40e3968fdc7
Zach Pomerantz <zzmp@uniswap.org>
2022-02-17 11:47:14 -0800

    fix: return eip1193bridge chainId as hexa

--- src/lib/hooks/useEip1193Provider.ts
+++ src/lib/hooks/useEip1193Provider.ts
@@ -1,13 +1,27 @@
 import { Provider as EthersProvider } from '@ethersproject/abstract-provider'
 import { Signer as EthersSigner } from '@ethersproject/abstract-signer'
 import { VoidSigner } from '@ethersproject/abstract-signer'
-import { Eip1193Bridge } from '@ethersproject/experimental'
+import { Eip1193Bridge as ExperimentalEip1193Bridge } from '@ethersproject/experimental'
 import { JsonRpcProvider } from '@ethersproject/providers'
 import { Provider as Eip1193Provider } from '@web3-react/types'
 import { ZERO_ADDRESS } from 'constants/misc'
 import { useMemo } from 'react'
 
 const voidSigner = new VoidSigner(ZERO_ADDRESS)
+
+class Eip1193Bridge extends ExperimentalEip1193Bridge {
+  async send(method: string, params?: Array<any>): Promise<any> {
+    switch (method) {
+      case 'eth_chainId': {
+        // TODO(https://github.com/ethers-io/ethers.js/pull/2711): Returns eth_chainId as a hexadecimal.
+        const result = await this.provider.getNetwork()
+        return '0x' + result.chainId.toString(16)
+      }
+      default:
+        return super.send(method, params)
+    }
+  }
+}
 
 export default function useEip1193Provider(
   provider?: Eip1193Provider | EthersProvider | JsonRpcProvider | { provider: EthersProvider; signer: EthersSigner }
