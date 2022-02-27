5d99e97d8eeafc65553ad60a40c1cc8017188e4e
crypzoh <crypzoh@protonmail.com>
2022-02-27 17:16:33 +0000

    Add mainnet rpc

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -6,6 +6,6 @@ export enum ChainId {
 export const NETWORK_URLS: {
   [chainId in ChainId]: string
 } = {
-  [ChainId.MAINNET]: `https://ethereum.rpc.evmos.org`,
+  [ChainId.MAINNET]: `https://eth.bd.evmos.org:8545`,
   [ChainId.TESTNET]: `https://evmos-archive-testnet.api.bdnodes.net:8545`,
 }
