59c6ab16dd1fd4dd310f2f7179fde52c9c4906ad
Zach Pomerantz <zzmp@uniswap.org>
2022-03-01 11:32:55 -0800

    fix: exports (#3379)
    * fix: cjs in cra
    
    * chore: wrap cjs for mjs
    
    * chore: export cjs and esm
    
    * chore: export chunks
    
    * fix: correctly export locales
    
    * chore: note exports compat
    
    * fix: use cjs for common deps
    
    * chore: bump to v0.0.20-beta
    
    * chore: rm outdated comment

--- rollup.config.ts
+++ rollup.config.ts
@@ -17,6 +17,8 @@ import { RollupWarning } from 'rollup'
 import copy from 'rollup-plugin-copy'
 import del from 'rollup-plugin-delete'
 import dts from 'rollup-plugin-dts'
+// @ts-ignore // missing types
+import multi from 'rollup-plugin-multi-input'
 import externals from 'rollup-plugin-node-externals'
 import sass from 'rollup-plugin-scss'
 import { CompilerOptions } from 'typescript'
@@ -24,6 +26,8 @@ import { CompilerOptions } from 'typescript'
 const REPLACEMENTS = {
   'process.env.REACT_APP_IS_WIDGET': true,
   'process.env.REACT_APP_LOCALES': '"./locales"',
+  // esm requires fully-specified paths:
+  'react/jsx-runtime': 'react/jsx-runtime.js',
 }
 
 const EXTENSIONS = ['.js', '.jsx', '.ts', '.tsx']
@@ -45,7 +49,6 @@ const aliases = Object.entries({ ...paths }).flatMap(([find, replacements]) => {
 
 const plugins = [
   // Dependency resolution
-  externals({ exclude: ['constants'], deps: true, peerDeps: true }), // marks builtins, dependencies, and peerDependencies external
   resolve({ extensions: EXTENSIONS }), // resolves third-party modules within node_modules/
   alias({ entries: aliases }), // resolves paths aliased through the tsconfig (babel does not use tsconfig path resolution)
 
@@ -56,32 +59,69 @@ const plugins = [
 
 const check = {
   input: 'src/lib/index.tsx',
-  output: { file: 'dist/widgets.tsc' },
+  output: { file: 'dist/widgets.tsc', inlineDynamicImports: true },
   external: isAsset,
-  plugins: [...plugins, typescript({ tsconfig: TS_CONFIG })],
+  plugins: [
+    externals({ exclude: ['constants'], deps: true, peerDeps: true }), // marks builtins, dependencies, and peerDependencies external
+    ...plugins,
+    typescript({ tsconfig: TS_CONFIG }),
+  ],
   onwarn: squelchTranspilationWarnings, // this pipeline is only for typechecking and generating definitions
 }
 
 const type = {
   input: 'dist/dts/lib/index.d.ts',
-  output: { file: 'dist/widgets.d.ts' },
+  output: { file: 'dist/index.d.ts' },
   external: isAsset,
   plugins: [
+    externals({ exclude: ['constants'], deps: true, peerDeps: true }),
     dts({ compilerOptions: { baseUrl: 'dist/dts' } }),
     process.env.ROLLUP_WATCH ? undefined : del({ hook: 'buildEnd', targets: ['dist/widgets.tsc', 'dist/dts'] }),
   ],
 }
 
+/**
+ * This exports scheme works for nextjs and for CRA5.
+ *
+ * It will also work for CRA4 if you use direct imports:
+ *   instead of `import { SwapWidget } from '@uniswap/widgets'`,
+ *              `import { SwapWidget } from '@uniswap/widgets/dist/index.js'`.
+ * I do not know why CRA4 does not seem to use exports for resolution.
+ *
+ * Note that chunks are enabled. This is so the tokenlist spec can be loaded async,
+ * to improve first load time (due to ajv). Locales are also in separate chunks.
+ *
+ * Lastly, note that JSON and lingui are bundled into the library, as neither are fully
+ * supported/compatible with ES Modules. Both _could_ be bundled only with esm, but this
+ * yields a less complex pipeline.
+ */
+
 const transpile = {
   input: 'src/lib/index.tsx',
   output: [
     {
-      file: 'dist/widgets.cjs',
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: false,
+    },
+    {
+      dir: 'dist/cjs',
+      entryFileNames: '[name].cjs',
+      chunkFileNames: '[name]-[hash].cjs',
       format: 'cjs',
-      sourcemap: true,
+      sourcemap: false,
     },
   ],
   plugins: [
+    externals({
+      exclude: [
+        'constants',
+        /@lingui\/(core|react)/, // @lingui incorrectly exports esm, so it must be bundled in
+        /\.json$/, // esm does not support JSON loading, so it must be bundled in
+      ],
+      deps: true,
+      peerDeps: true,
+    }),
     ...plugins,
 
     // Source code transformation
@@ -99,16 +139,30 @@ const transpile = {
         '@babel/plugin-transform-runtime', // embeds the babel runtime for library distribution
       ],
     }),
-
-    copy({
-      copyOnce: true,
-      targets: [{ src: 'src/locales/*.js', dest: 'dist/locales' }],
-    }),
   ],
   onwarn: squelchTypeWarnings, // this pipeline is only for transpilation
 }
 
-const config = [check, type, transpile]
+const locales = {
+  input: 'src/locales/*.js',
+  output: [
+    {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: false,
+    },
+  ],
+  plugins: [
+    copy({
+      copyOnce: true,
+      targets: [{ src: 'src/locales/*.js', dest: 'dist/cjs/locales', rename: (name) => `${name}.cjs` }],
+    }),
+    commonjs(),
+    multi(),
+  ],
+}
+
+const config = [check, type, transpile, locales]
 export default config
 
 function squelchTranspilationWarnings(warning: RollupWarning, warn: (warning: RollupWarning) => void) {
