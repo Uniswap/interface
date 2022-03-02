18ba8fca158e8966b6de49da415deb03d6e1a2e1
Zach Pomerantz <zzmp@uniswap.org>
2022-03-02 13:52:55 -0800

    feat: export used types

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,10 +1,17 @@
 import Swap, { SwapProps } from './components/Swap'
 import Widget, { WidgetProps } from './components/Widget'
+
+export type { ErrorHandler } from './components/Error/ErrorBoundary'
+export type { DefaultAddress } from './components/Swap'
+export type { Theme } from './theme'
 export { darkTheme, lightTheme } from './theme'
-export type { Theme } from './theme/theme'
+export type { Provider as EthersProvider } from '@ethersproject/abstract-provider'
+export type { TokenInfo } from '@uniswap/token-lists'
+export type { Provider as Eip1193Provider } from '@web3-react/types'
+export type { SupportedLocale } from 'constants/locales'
 export { SUPPORTED_LOCALES } from 'constants/locales'
 
-type SwapWidgetProps = SwapProps & WidgetProps
+export type SwapWidgetProps = SwapProps & WidgetProps
 
 export function SwapWidget(props: SwapWidgetProps) {
   return (
