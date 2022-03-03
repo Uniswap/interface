21473165c757777ae0fe33f20131c2caa50e02c4
AdamGenesis <adam@usegenesis.com>
2022-03-02 22:21:04 -0300

    Updated color scheme

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -66,30 +66,30 @@ function colors(darkMode: boolean): Colors {
     text5: darkMode ? '#2C2F36' : '#EDEEF2',
 
     // backgrounds / greys
-    bg0: darkMode ? '#191B1F' : '#FFF',
-    bg1: darkMode ? '#212429' : '#F7F8FA',
-    bg2: darkMode ? '#2C2F36' : '#EDEEF2',
-    bg3: darkMode ? '#40444F' : '#CED0D9',
-    bg4: darkMode ? '#565A69' : '#888D9B',
-    bg5: darkMode ? '#6C7284' : '#888D9B',
-    bg6: darkMode ? '#1A2028' : '#6C7284',
+    bg0: darkMode ? '#150C1D' : '#FFF',
+    bg1: darkMode ? '#1E1228' : '#F7F8FA',
+    bg2: darkMode ? '#1F1935' : '#EDEEF2',
+    bg3: darkMode ? '#322D4A' : '#CED0D9',
+    bg4: darkMode ? '#443D62' : '#888D9B',
+    bg5: darkMode ? '#564D78' : '#888D9B',
+    bg6: darkMode ? '#7E7BAF' : '#6C7284',
 
     //specialty colors
     modalBG: darkMode ? 'rgba(0,0,0,.425)' : 'rgba(0,0,0,0.3)',
     advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#2172E5' : '#E8006F',
-    primary2: darkMode ? '#3680E7' : '#FF8CC3',
-    primary3: darkMode ? '#4D8FEA' : '#FF99C9',
-    primary4: darkMode ? '#376bad70' : '#F6DDE8',
-    primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+    primary1: darkMode ? '#9146FF' : '#E8006F',
+    primary2: darkMode ? '#A76BFF' : '#FF8CC3',
+    primary3: darkMode ? '#BD90FF' : '#FF99C9',
+    primary4: darkMode ? '#9D9DCB70' : '#F6DDE8',
+    primary5: darkMode ? '#CACAEE70' : '#FDEAF1',
 
     // color text
     primaryText1: darkMode ? '#5090ea' : '#D50066',
 
     // secondary colors
-    secondary1: darkMode ? '#2172E5' : '#E8006F',
+    secondary1: darkMode ? '#9146FF' : '#E8006F',
     secondary2: darkMode ? '#17000b26' : '#F6DDE8',
     secondary3: darkMode ? '#17000b26' : '#FDEAF1',
 
@@ -101,15 +101,14 @@ function colors(darkMode: boolean): Colors {
     yellow1: '#E3A507',
     yellow2: '#FF8F00',
     yellow3: '#F3B71E',
-    blue1: darkMode ? '#2172E5' : '#0068FC',
-    blue2: darkMode ? '#5199FF' : '#0068FC',
+    blue1: darkMode ? '#9146FF' : '#0068FC',
+    blue2: darkMode ? '#A76BFF' : '#0068FC',
     error: darkMode ? '#FD4040' : '#DF1F38',
     success: darkMode ? '#27AE60' : '#007D35',
     warning: '#FF8F00',
 
     // dont wanna forget these blue yet
-    blue4: darkMode ? '#153d6f70' : '#C4D9F8',
-    // blue5: darkMode ? '#153d6f70' : '#EBF4FF',
+    blue4: darkMode ? '#CACAEE70' : '#C4D9F8',
   }
 }
 
