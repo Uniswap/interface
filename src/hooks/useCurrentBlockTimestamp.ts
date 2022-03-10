bcf01c2dc089460a63dd0eff226aa3d27dcdf844
Zach Pomerantz <zzmp@uniswap.org>
2022-03-10 09:50:27 -0800

    fix: memo-ize onchain results

--- src/hooks/useCurrentBlockTimestamp.ts
+++ src/hooks/useCurrentBlockTimestamp.ts
@@ -1,20 +1,15 @@
 import { BigNumber } from '@ethersproject/bignumber'
 import { useSingleCallResult } from 'lib/hooks/multicall'
-import { useEffect, useState } from 'react'
+import { useMemo } from 'react'
 
 import { useInterfaceMulticall } from './useContract'
 
 // gets the current timestamp from the blockchain
 export default function useCurrentBlockTimestamp(): BigNumber | undefined {
-  const [lastBlock, setLastBlock] = useState<BigNumber | undefined>()
   const multicall = useInterfaceMulticall()
-  const block: BigNumber | undefined = useSingleCallResult(multicall, 'getCurrentBlockTimestamp')?.result?.[0]
-  useEffect(() => {
-    // If block xor lastBlock are undefined, or if block has progressed, then update lastBlock.
-    // This prevents updates when the block doesn't change, because the returned BigNumber will still be referentially unique.
-    if (Boolean(block) !== Boolean(lastBlock) || (block && lastBlock && !block.eq(lastBlock))) {
-      setLastBlock(block)
-    }
-  }, [block, lastBlock])
-  return lastBlock
+  const resultStr: string | undefined = useSingleCallResult(
+    multicall,
+    'getCurrentBlockTimestamp'
+  )?.result?.[0]?.toString()
+  return useMemo(() => (typeof resultStr === 'string' ? BigNumber.from(resultStr) : undefined), [resultStr])
 }
