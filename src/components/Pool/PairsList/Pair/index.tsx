b5e322e685099f5fcbb6314c1db6000596ea2a73
Paolo Guerra <pg.paologuerra@gmail.com>
2022-03-15 20:39:21 +0100

    fix: adjust the badges width

--- src/components/Pool/PairsList/Pair/index.tsx
+++ src/components/Pool/PairsList/Pair/index.tsx
@@ -34,7 +34,8 @@ const SizedCard = styled(DarkCard)`
 
 const FarmingBadge = styled.div<{ isGreyed?: boolean }>`
   height: 16px;
-  border: ${props => (props.isGreyed ? `solid 1.5px transparent` : `solid 1.5px ${props.theme.green2}`)};
+  border: solid 1px;
+  border-color: ${props => (props.isGreyed ? `transparent` : `${props.theme.green2}`)};
   div {
     color: ${props => (props.isGreyed ? props.theme.purple2 : props.theme.green2)};
   }
@@ -44,9 +45,10 @@ const FarmingBadge = styled.div<{ isGreyed?: boolean }>`
   justify-content: center;
   align-items: center;
   border-radius: 4px;
-  padding: 0 4px;
+  padding: 0 2px;
   background-color: ${props => props.isGreyed && props.theme.bg3};
   opacity: ${props => props.isGreyed && '0.5'};
+  gap: 4px;
   svg {
     > path {
       fill: ${props => (props.isGreyed ? props.theme.purple2 : props.theme.green2)};
@@ -60,7 +62,6 @@ const FarmingBadge = styled.div<{ isGreyed?: boolean }>`
 
 const BadgeText = styled.div`
   font-weight: 700;
-  margin-left: 3px;
   font-size: 9px;
   line-height: 9px;
   letter-spacing: 0.02em;
