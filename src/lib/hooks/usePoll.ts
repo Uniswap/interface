7fcc5c9bc8c24af7387c67e118d2fdc0b206da37
Zach Pomerantz <zzmp@uniswap.org>
2022-03-16 13:42:14 -0700

    chore: comments

--- src/lib/hooks/usePoll.ts
+++ src/lib/hooks/usePoll.ts
@@ -4,7 +4,7 @@ import { useEffect, useMemo, useState } from 'react'
 const DEFAULT_POLLING_INTERVAL = ms`15s`
 const DEFAULT_KEEP_UNUSED_DATA_FOR = ms`10s`
 
-export default function usePoll<A, T>(
+export default function usePoll<T>(
   fetch: () => Promise<T>,
   key = '',
   pollingInterval = DEFAULT_POLLING_INTERVAL,
@@ -18,11 +18,13 @@ export default function usePoll<A, T>(
 
     const entry = cache.get(key)
     if (entry && entry.ttl + keepUnusedDataFor > Date.now()) {
-      timeout = setTimeout(poll, Math.max(0, entry.ttl - Date.now()))
+      // If there is a fresh entry, return it and queue the next poll.
       setData({ key, result: entry.result })
+      timeout = setTimeout(poll, Math.max(0, entry.ttl - Date.now()))
     } else {
-      setData({ key })
+      // Otherwise, set a new entry (to avoid duplicate polling) and trigger a poll immediately.
       cache.set(key, { ttl: Date.now() + pollingInterval })
+      setData({ key })
       poll()
     }
 
@@ -33,12 +35,23 @@ export default function usePoll<A, T>(
     async function poll(ttl = Date.now() + pollingInterval) {
       timeout = setTimeout(poll, pollingInterval)
       const result = await fetch()
+      // Always set the result in the cache, but only set it as data if the key is still being queried.
       cache.set(key, { ttl, result })
       setData((data) => {
         return data.key === key ? { key, result } : data
       })
     }
   }, [cache, fetch, keepUnusedDataFor, key, pollingInterval])
+
+  // Cleanup stale entries.
+  useEffect(() => {
+    const now = Date.now()
+    cache.forEach(({ ttl }, key) => {
+      if (ttl + keepUnusedDataFor > now) {
+        cache.delete(key)
+      }
+    })
+  }, [cache, keepUnusedDataFor, key])
 
   return data.result
 }
