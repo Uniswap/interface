b6a0e0f4908e0d411362dd59e9178945e728092a
Zach Pomerantz <zzmp@uniswap.org>
2022-03-15 17:59:05 -0700

    fix: keep block state per cache

--- src/lib/hooks/useBlockCache.ts
+++ src/lib/hooks/useBlockCache.ts
@@ -1,3 +1,4 @@
+import useCurrentBlockTimestamp from 'hooks/useCurrentBlockTimestamp'
 import useBlockNumber from 'lib/hooks/useBlockNumber'
 import { useCallback, useEffect, useState } from 'react'
 
@@ -6,8 +7,21 @@ export const ShouldUpdate = Symbol()
 // Denotes a cache entry that is pending data, so that other nodes do not re-fetch the same data.
 const Pending = Symbol()
 
-// The current block being cached by subscribers to useBlockCache.
-let cacheBlock: number | undefined = undefined
+// The current block being cached by subscribers to useBlockCache, for a specific cache.
+const blockCache =
+  typeof WeakMap !== 'undefined'
+    ? new WeakMap<
+        Map<string, unknown>,
+        {
+          block: number
+          timestamp: number
+        }
+      >()
+    : null
+
+// The TTL between blocks, in seconds.
+// This is necessary for L2, where the cache would otherwise clear too frequently to be usable.
+const BLOCK_CACHE_TTL = 30
 
 /**
  * Configures leader/followers for a block-scoped cache (ie a cache that clears every block).
@@ -45,39 +59,44 @@ export default function useBlockCache<V>(
   cache?: Map<string, V | typeof Pending>,
   key?: string
 ): [V | undefined | typeof ShouldUpdate, (update: V) => void] {
-  const block = useBlockNumber()
-  const [updatingBlock, setUpdatingBlock] = useState<number | false>(false)
+  const block = useBlockNumber() ?? 0
+  const timestamp = useCurrentBlockTimestamp()?.toNumber() ?? 0
+  const [cachingBlock, setCachingBlock] = useState<number | false>(false)
 
   useEffect(() => {
-    // Clears the cache with every new block.
-    if (block !== cacheBlock) {
-      cacheBlock = block
-      cache?.clear()
-    }
-    // At this point, block is synced with cacheBlock.
+    const { block: cacheBlock = 0, timestamp: cacheTimestamp = 0 } = (cache && blockCache?.get(cache)) ?? {}
 
-    // Claims a key to update. cache.has(key) tracks whether this key is already owned.
-    if (block && key && cache && !cache.has(key)) {
+    // Clears the cache if there is a new block at least 15s fresher for this cache
+    // (uses Math.abs to compute the difference in case we are on a different chain).
+    if (cache && block !== cacheBlock && Math.abs(timestamp - cacheTimestamp) > BLOCK_CACHE_TTL) {
+      blockCache?.set(cache, { block, timestamp })
+      cache?.clear()
+      return
+    }
+
+    // Claims a key to update. cache.has(key) tracks whether this key is already owned for this block.
+    if (cacheBlock && key && cache && !cache.has(key)) {
       cache?.set(key, Pending)
-      setUpdatingBlock(block)
+      setCachingBlock(cacheBlock)
     }
 
     // Unclaims a key (when switching keys or unmounting) if it has not yet been written this block.
     return () => {
-      if (updatingBlock === cacheBlock && key && !cache?.get(key)) {
+      if (cachingBlock === cacheBlock && key && cache?.get(key) === Pending) {
         cache?.delete(key)
-        setUpdatingBlock(false)
+        setCachingBlock(false)
       }
     }
-  }, [block, cache, key, updatingBlock])
+  }, [block, cache, key, cachingBlock, timestamp])
 
-  // Grab the value outside of a hook to force a re-render when it is updated.
   const cacheValue = key ? cache?.get(key) : undefined
   const isPending = cacheValue === Pending
   const value = isPending ? undefined : cacheValue
 
   // Only return ShouldUpdate if a this hook has claimed the key this block, and it is still pending.
-  const shouldUpdate = updatingBlock === cacheBlock && isPending
+  // NB: If WeakMap is not supported, this becomes a nop by setting shouldUpdate = true.
+  const { block: cacheBlock = 0 } = (cache && blockCache?.get(cache)) ?? {}
+  const shouldUpdate = !blockCache || (cachingBlock === cacheBlock && isPending)
 
   const setValue = useCallback(
     (update: V) => {
@@ -90,5 +109,6 @@ export default function useBlockCache<V>(
     },
     [cache, key, shouldUpdate]
   )
+
   return [shouldUpdate ? ShouldUpdate : value, setValue]
 }
