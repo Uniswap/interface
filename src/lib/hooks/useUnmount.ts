53450008d7d1d3e4dee7c05173bfeb2478d246e9
Zach Pomerantz <zzmp@uniswap.org>
2022-03-19 23:07:03 -0700

    fix: useUnmount portability

--- src/lib/hooks/useUnmount.ts
+++ src/lib/hooks/useUnmount.ts
@@ -3,8 +3,10 @@ import { RefObject, useEffect } from 'react'
 export const UNMOUNTING = 'unmounting'
 
 /**
- * Delays a node's unmounting so that an animation may be applied.
- * An animation *must* be applied, or the node will not unmount.
+ * Delays a node's unmounting until any animations on that node are finished, so that an unmounting
+ * animation may be applied. If there is no animation, this is a no-op.
+ *
+ * CSS should target the UNMOUNTING class to determine when to apply an unmounting animation.
  */
 export default function useUnmount(node: RefObject<HTMLElement>) {
   useEffect(() => {
@@ -15,7 +17,11 @@ export default function useUnmount(node: RefObject<HTMLElement>) {
       parent.removeChild = function <T extends Node>(child: T) {
         if ((child as Node) === current) {
           current.classList.add(UNMOUNTING)
-          current.onanimationend = () => {
+          if (current.getAnimations().length) {
+            current.onanimationend = () => {
+              removeChild.call(parent, child)
+            }
+          } else {
             removeChild.call(parent, child)
           }
           return child
