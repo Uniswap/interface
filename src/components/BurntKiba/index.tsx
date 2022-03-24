658b9425f22a191d34ee4d4fe949f2e3e7c77c66
Josh <jonesjosh41@gmail.com>
2022-03-24 13:51:12 -0500

    update chart linkks

--- src/components/BurntKiba/index.tsx
+++ src/components/BurntKiba/index.tsx
@@ -1,18 +1,26 @@
 import Badge, { BadgeVariant } from 'components/Badge'
-import { CardBGImage, CardSection } from 'components/earn/styled'
+import { CardBGImage, CardNoise, CardSection } from 'components/earn/styled'
 import { LinkStyledButton, StyledInternalLink } from 'theme'
 import { useKiba, useKibaBalanceUSD } from 'pages/Vote/VotePage'
 
+import { AutoColumn } from 'components/Column'
+import Confetti from 'components/Confetti'
 import { DarkCard } from 'components/Card'
 import React from 'react'
 import { SupportedChainId } from 'constants/chains'
 import { Token } from '@uniswap/sdk-core'
+import { Wrapper } from 'pages/Pool/styleds'
 import {Zap} from 'react-feather'
+import styled from 'styled-components/macro'
 import { useCurrency } from 'hooks/Tokens'
 import { useCurrencyBalance } from 'state/wallet/hooks'
 import { useUSDCValue } from 'hooks/useUSDCPrice'
 import { useWeb3React } from '@web3-react/core'
 
+const ContentWrapper = styled(AutoColumn)`
+  width: 100%;
+  padding: 1rem;
+`
 function abbreviateNumber(value: any) {
     return Intl.NumberFormat('en-US', {
         notation: "compact",
@@ -39,17 +47,25 @@ export const BurntKiba = ({showDetails}:{showDetails?:boolean}) => {
     return (
         deadWalletKibaBalance ? 
         showDetails ? 
+        <ContentWrapper gap="lg">
+            <Confetti start={true} variant={'bottom'} />
+      
+            <CardBGImage desaturate />
+            <CardNoise />
             <DarkCard>
                 <h1>Burnt Kiba {isBinance ? "(BSC)" : "(ETH)"}</h1>
-                <CardBGImage desaturate>
-
-                </CardBGImage>
+                
                 <p style={{color: '#F76C1D'}}>{abbreviateNumber(+deadWalletKibaBalance.toFixed(2))}   <small>({(Number(+deadWalletKibaBalance.toFixed(18)).toLocaleString())}) Tokens</small></p>
                {burntValue && isBinance === false && <p style={{color: '#F76C1D'}}><Badge>Burnt Value ${Number(burntValue?.toFixed(2)).toLocaleString()} USD</Badge></p>}
                {bscBurntValue && isBinance === true && <p style={{color: '#F76C1D'}}><Badge>Burnt Value ${bscBurntValue} USD</Badge></p>}
-            </DarkCard> :
+            <small style={{marginTop: 5}}>
+                Value includes price impact
+            </small>
+            </DarkCard> 
+              </ContentWrapper>   :
             <StyledInternalLink to="/dashboard">
                 <Badge   style={{color: '#fff', background: 'url(https://bestanimations.com/media/flames/1396965048fire-flames-sparks-billowing-animated-gif-image.gif)', backgroundPosition: 'center center',backgroundSize: 'contain'}} variant={BadgeVariant.DEFAULT}> {abbreviateNumber(+deadWalletKibaBalance.toFixed(2))}</Badge>
             </StyledInternalLink> : null
+         
     )
 }
