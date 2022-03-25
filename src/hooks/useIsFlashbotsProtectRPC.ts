0b700f467ae3121d00e15a28fa6ad74508f63e9c
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2022-03-25 14:01:10 -0700

    utilize useContract hook and chain ID flag in useIsFlashbotsProtectRPC hook

--- src/hooks/useIsFlashbotsProtectRPC.ts
+++ src/hooks/useIsFlashbotsProtectRPC.ts
@@ -1,13 +1,23 @@
-import { Contract } from '@ethersproject/contracts'
+import { SupportedChainId } from 'constants/chains'
 import { useEffect, useState } from 'react'
 
 import { useBlockNumber } from '../state/application/hooks'
+import { useContract } from './useContract'
 import { useActiveWeb3React } from './web3'
 
 export default function useIsFlashbotsProtectRPC(): [boolean, () => Promise<boolean>] {
   const [isFlashRPC, setIsFlashRPC] = useState<boolean>(false)
   const { library, chainId } = useActiveWeb3React()
   const blockNumber = useBlockNumber()
+  const flashbotsContract = useContract('0xf1a54b0759b58661cea17cff19dd37940a9b5f1a', [
+    {
+      inputs: [],
+      name: 'isFlashRPC',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ])
 
   useEffect(() => {
     detectFlashRPC()
@@ -16,27 +26,17 @@ export default function useIsFlashbotsProtectRPC(): [boolean, () => Promise<bool
 
   async function detectFlashRPC(): Promise<boolean> {
     console.log('detect start')
-    if (!library || !chainId || chainId !== 1) {
+    if (!library || !chainId || chainId !== SupportedChainId.MAINNET) {
       setIsFlashRPC(false)
       return false
     }
-    const flasbotsRPCContract = new Contract(
-      '0xf1a54b0759b58661cea17cff19dd37940a9b5f1a',
-      [
-        {
-          inputs: [],
-          name: 'isFlashRPC',
-          outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
-          stateMutability: 'view',
-          type: 'function',
-        },
-      ],
-      library
-    )
-    const flashbotsRPCDetection: boolean = await flasbotsRPCContract.isFlashRPC()
-    console.log('Detect Flashbots RPC: ', flashbotsRPCDetection)
-    setIsFlashRPC(flashbotsRPCDetection)
-    return flashbotsRPCDetection
+    if (flashbotsContract) {
+      const flashbotsRPCDetection: boolean = await flashbotsContract.isFlashRPC()
+      console.log('Detect Flashbots RPC: ', flashbotsRPCDetection)
+      setIsFlashRPC(flashbotsRPCDetection)
+      return flashbotsRPCDetection
+    }
+    return false
   }
   return [isFlashRPC, detectFlashRPC]
 }
