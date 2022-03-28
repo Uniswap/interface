0fb5b7cbde921416c6f05678c41a3e7007e49314
MilanVojnovic95 <mv.coversproduction@gmail.com>
2022-03-28 11:15:59 +0200

    Rewards Page | Smoke Test WIP

--- tests/cypress/integration/smoke/RewardsPageTest.ts
+++ tests/cypress/integration/smoke/RewardsPageTest.ts
@@ -1,9 +1,15 @@
+import { RewardsPage } from '../../../pages/RewardsPage'
+
 describe('Rewards Page test', () => {
     before(() => {
       cy.visit('/rewards?chainId=1')
     })
     it('Should check all buttons and items on Rewards Page', () => {
-      
+        RewardsPage.campaignsAndExpiredCampaigns_Buttons().should('be.visible')
+        RewardsPage.creeateCapmaign_Button().should('be.visible')
+        RewardsPage.searchAPair_ModalWindow().should('be.visible')
+        RewardsPage.searchAPair_Field().should('be.visible')
+        RewardsPage.myPairs_ToggleSwitch().should('be.visible')
     })
     
   })
