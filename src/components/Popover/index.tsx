b0778f007d9ad102ccb9e049bbbb5e69462e2d68
nutz <nutz>
2022-04-05 17:13:15 -0500

    UI Changes

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -11,11 +11,11 @@ const PopoverContainer = styled.div<{ show: boolean }>`
   visibility: ${(props) => (props.show ? 'visible' : 'hidden')};
   opacity: ${(props) => (props.show ? 1 : 0)};
   transition: visibility 150ms linear, opacity 150ms linear;
-  background: ${({ theme }) => theme.bg2};
-  border: 1px solid ${({ theme }) => theme.bg3};
+  background: ${({ theme }) => theme.bg0};
+  border: 1px solid ${({ theme }) => theme.bg2};
   box-shadow: 0 4px 8px 0 ${({ theme }) => transparentize(0.9, theme.shadow1)};
   color: ${({ theme }) => theme.text2};
-  border-radius: 8px;
+  border-radius: 12px;
 `
 
 const ReferenceElement = styled.div`
@@ -34,9 +34,8 @@ const Arrow = styled.div`
     z-index: 9998;
 
     content: '';
-    border: 1px solid ${({ theme }) => theme.bg3};
     transform: rotate(45deg);
-    background: ${({ theme }) => theme.bg2};
+    background: 'transparent';
   }
 
   &.arrow-top {
