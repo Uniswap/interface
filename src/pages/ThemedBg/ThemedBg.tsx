7462105022677e390700f6067adb8d0231fc3adc
nutz <nutz>
2022-04-05 17:03:40 -0500

    Add the ability to change your background on trumpswap.

---
+++ src/pages/ThemedBg/ThemedBg.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { GreyCard } from 'components/Card'
+import { AutoColumn } from 'components/Column'
+import Row from 'components/Row'
+import { Image, User } from 'react-feather'
+import { TYPE } from 'theme'
+import { CardSection } from 'components/earn/styled'
+import { OutlineCard } from 'components/Card'
+export const THEME_BG_KEY = 'themedBG'
+
+export const ThemedBg = ({ theme, setTheme }: { theme: any; setTheme: (theme: string) => void }) => {
+  console.log(theme, setTheme)
+  return (
+    <GreyCard style={{ maxWidth: 600 }}>
+      <OutlineCard>
+      <span style={{textAlign: 'center', fontSize:22, color: "#fff"}}>Choose a custom background</span>
+      <ul style={{background:'transparent', padding: 20, cursor: 'pointer'}}>
+        {['./trump.mp4', './trumpspace.mp4', './video.mp4'].map((key) => (
+          <li onClick={(e) => setTheme(key)} style={{ listStyle: 'none', padding: 15, margin: 10 }} key={key}>
+            <video style={{ maxWidth: 400, border: theme === key ? '1px solid green' : '1px solid #eee' }}>
+              <source src={key} type="video/mp4" />
+            </video>
+          </li>
+        ))}
+      </ul>
+      </OutlineCard>
+    </GreyCard>
+  )
+}
