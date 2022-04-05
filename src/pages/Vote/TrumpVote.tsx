7e1c116b6e85a0fea62431c2a23789585c3d2ff2
nutz <nutz>
2022-04-05 17:03:42 -0500

    update proposal stuff

--- src/pages/Vote/TrumpVote.tsx
+++ src/pages/Vote/TrumpVote.tsx
@@ -3,6 +3,7 @@ import { BlueCard, OutlineCard, DarkCard } from 'components/Card';
 import Tooltip from 'components/Tooltip';
 import React from 'react';
 import { ChevronLeft, Info, ThumbsDown, ThumbsUp } from 'react-feather';
+import { useIsDarkMode } from 'state/user/hooks';
 import styled from 'styled-components/macro';
 import { TYPE } from 'theme';
 import { IconWrapper } from 'theme/components';
@@ -32,7 +33,7 @@ export const TrumpVote = ( ) => {
     font-size:12px;
     width: 100%;
     `
-
+    const isDark = useIsDarkMode()
     const [show, setShow] = React.useState(false)
     const message = "Proposals are created by members of the BabyTrump community who own atleast 200 Trumpgold. A proposal is then voted on by members of the community (Anyone who owns any of our tokens)."
 
@@ -53,7 +54,7 @@ export const TrumpVote = ( ) => {
                        <ul style={{padding:9}}>
                        {proposals.map(proposal => (
                             <li key={proposal._id} style={{display:'flex'}}>
-                                <StyledInternalLink style={{ fontSize: 16, color:"#fff"}} to={`/proposal/details/${proposal._id}`}>
+                                <StyledInternalLink style={{ fontSize: 16, color: isDark? "#fff" : "#222"}} to={`/proposal/details/${proposal._id}`}>
                                     {'"'}{proposal.title}{'"'} created by: {proposal.proposedBy}
                                     </StyledInternalLink>
                                 <div style={{display:'inline-flex', marginLeft: 10}}>
