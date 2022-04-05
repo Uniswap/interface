4cfd1b78e62dd9b094b5b8be654c5d18e5614078
nutz <nutz>
2022-04-05 17:13:15 -0500

    UI Changes

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -14,7 +14,9 @@ export enum BadgeVariant {
   POSITIVE_OUTLINE = 'POSITIVE_OUTLINE',
   NEGATIVE_OUTLINE = 'NEGATIVE_OUTLINE',
   RED_WHITE = 'RED_WHITE',
-  GREY = 'GREY'
+  GREY = 'GREY',
+  HOLLOW = 'HOLLOW',
+  BLUE = 'BLUE'
 }
 
 interface BadgeProps {
@@ -38,6 +40,10 @@ function pickBackgroundColor(variant: BadgeVariant | undefined, theme: DefaultTh
       return 'transparent'
     case BadgeVariant.GREY:
       return '#000';
+    case BadgeVariant.HOLLOW:
+      return 'transparent'
+    case BadgeVariant.BLUE:
+      return '#4F4F62'
     default:
       return theme.bg2
   }
@@ -49,9 +55,13 @@ function pickBorder(variant: BadgeVariant | undefined, theme: DefaultTheme): str
       return `1px solid ${theme.primary1}`
     case BadgeVariant.POSITIVE_OUTLINE:
       return `1px solid ${theme.success}`
-      case BadgeVariant.RED_WHITE:
-      case BadgeVariant.NEGATIVE_OUTLINE:
+    case BadgeVariant.RED_WHITE:
+    case BadgeVariant.NEGATIVE_OUTLINE:
         return `1px solid ${theme.error}`
+    case BadgeVariant.HOLLOW:
+      return `1px solid ${theme.white}`
+    case BadgeVariant.BLUE:
+      return `2px solid #4F4F62`
     default:
       return 'unset'
   }
@@ -74,8 +84,22 @@ function pickFontColor(variant: BadgeVariant | undefined, theme: DefaultTheme): 
     case BadgeVariant.RED_WHITE:
     case BadgeVariant.GREY:
       return '#FFF';
-    default:
+    case BadgeVariant.HOLLOW:
+      return '#FFF';
+      case BadgeVariant.BLUE:
+        return '#FFF';
+      default:
       return readableColor(theme.bg2)
+  }
+}
+
+function pickFontWeight(variant: BadgeVariant | undefined, theme: DefaultTheme): string {
+  switch (variant) {
+      case BadgeVariant.BLUE:
+        return '600';
+    
+      default:
+      return '500'
   }
 }
 
@@ -88,7 +112,8 @@ const Badge = styled.div<PropsWithChildren<BadgeProps>>`
   display: inline-flex;
   padding: 4px 6px;
   justify-content: center;
-  font-weight: 500;
+  font-weight: ${({ theme, variant }) => pickFontWeight(variant, theme)};
+  font-family: 'Archivo Narrow';
 `
 
 export default Badge
