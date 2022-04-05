c222e2e6eba3881ed26a88d2aaf80aa349d942b6
nutz <nutz>
2022-04-05 17:03:01 -0500

    update route to gains and update gains page with newer grafficsetc

--- src/components/NavigationTabs/index.tsx
+++ src/components/NavigationTabs/index.tsx
@@ -161,7 +161,7 @@ export function CreateProposalTabs() {
   return (
     <Tabs>
       <Row style={{ padding: '1rem 1rem 0 1rem' }}>
-        <HistoryLink to="/vote">
+        <HistoryLink to="/gains">
           <StyledArrowLeft />
         </HistoryLink>
         <ActiveText style={{ marginLeft: 'auto', marginRight: 'auto' }}>Create Proposal</ActiveText>
