201bd2308a3caf648368b3945d5b73d8cb46c816
Bruno Crosier <bruno.crosier@gmail.com>
2022-04-13 21:10:19 +0100

    fix: :bug: add crossplatform `prei18n-extract` script

---
+++ prei18n-extract.js
@@ -0,0 +1,8 @@
+import { exec } from 'child_process'
+const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE)
+
+if (isWindows) {
+  exec(`type nul > src/locales/en-US.po`)
+} else {
+  exec(`touch src/locales/en-US.po`)
+}
