fa62711e07dca23fadcd96a3629df2d1d6cbe05e
crypzoh <crypzoh@protonmail.com>
2022-05-01 10:17:38 +0200

    Keep it sorted by list order

--- src/components/SearchModal/sorting.ts
+++ src/components/SearchModal/sorting.ts
@@ -1,5 +1,6 @@
 import { Token, CurrencyAmount, Currency } from '@uniswap/sdk-core'
 import { useMemo } from 'react'
+import { WrappedTokenInfo } from 'state/lists/wrappedTokenInfo'
 import { useAllTokenBalances } from '../../state/wallet/hooks'
 
 // compare two token amounts with highest one coming first
@@ -17,7 +18,7 @@ function balanceComparator(balanceA?: CurrencyAmount<Currency>, balanceB?: Curre
 function getTokenComparator(balances: {
   [tokenAddress: string]: CurrencyAmount<Currency> | undefined
 }): (tokenA: Token, tokenB: Token) => number {
-  return function sortTokens(tokenA: Token, tokenB: Token): number {
+  return function sortTokens(tokenA: WrappedTokenInfo | Token, tokenB: WrappedTokenInfo | Token): number {
     // -1 = a is first
     // 1 = b is first
 
@@ -28,6 +29,12 @@ function getTokenComparator(balances: {
     const balanceComp = balanceComparator(balanceA, balanceB)
     if (balanceComp !== 0) return balanceComp
 
+    if ((tokenA as WrappedTokenInfo).list.name === (tokenB as WrappedTokenInfo).list.name) {
+      const tokenAPosition = getPositionOnList(tokenA as WrappedTokenInfo)
+      const tokenBPosition = getPositionOnList(tokenB as WrappedTokenInfo)
+      return tokenAPosition < tokenBPosition ? -1 : 1
+    }
+
     if (tokenA.symbol && tokenB.symbol) {
       // sort by symbol
       return tokenA.symbol.toLowerCase() < tokenB.symbol.toLowerCase() ? -1 : 1
@@ -35,6 +42,10 @@ function getTokenComparator(balances: {
       return tokenA.symbol ? -1 : tokenB.symbol ? -1 : 0
     }
   }
+}
+
+function getPositionOnList(t: WrappedTokenInfo) {
+  return t.list.tokens.map((t) => t.address).indexOf(t.address)
 }
 
 export function useTokenComparator(inverted: boolean): (tokenA: Token, tokenB: Token) => number {
