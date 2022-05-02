1cbc7fc67e41e4340263affb544ce164d83cc815
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2022-05-02 12:10:52 -0400

    add gnosis

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,6 +1,6 @@
-// import { SafeAppConnector } from '@gnosis.pm/safe-apps-web3-react'
 import { CoinbaseWallet } from '@web3-react/coinbase-wallet'
 import { initializeConnector, Web3ReactHooks } from '@web3-react/core'
+import { GnosisSafe } from '@web3-react/gnosis-safe'
 import { MetaMask } from '@web3-react/metamask'
 import { Network } from '@web3-react/network'
 import { WalletConnect } from '@web3-react/walletconnect'
@@ -8,9 +8,6 @@ import { ALL_SUPPORTED_CHAIN_IDS, SupportedChainId } from 'constants/chains'
 import { INFURA_NETWORK_URLS } from 'constants/infura'
 
 import UNISWAP_LOGO_URL from '../assets/svg/logo.svg'
-// import { FortmaticConnector } from './Fortmatic'
-
-// const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 
 export const [network, networkHooks] = initializeConnector<Network>(
   (actions) => new Network(actions, INFURA_NETWORK_URLS, true, 1),
@@ -22,13 +19,7 @@ export const [injected, injectedHooks] = initializeConnector<MetaMask>(
   ALL_SUPPORTED_CHAIN_IDS
 )
 
-// export const gnosisSafe = new SafeAppConnector()
-
-// // mainnet only
-// export const fortmatic = new FortmaticConnector({
-//   apiKey: FORMATIC_KEY ?? '',
-//   chainId: 1,
-// })
+export const [gnosisSafe, gnosisSafeHooks] = initializeConnector<GnosisSafe>((actions) => new GnosisSafe(actions, true))
 
 export const [walletConnect, walletConnectHooks] = initializeConnector<WalletConnect>(
   (actions) =>
@@ -57,7 +48,8 @@ export const [coinbaseWallet, coinbaseWalletHooks] = initializeConnector<Coinbas
   ALL_SUPPORTED_CHAIN_IDS
 )
 
-export const connectors: [MetaMask | WalletConnect | CoinbaseWallet | Network, Web3ReactHooks][] = [
+export const connectors: [GnosisSafe | MetaMask | WalletConnect | CoinbaseWallet | Network, Web3ReactHooks][] = [
+  [gnosisSafe, gnosisSafeHooks],
   [injected, injectedHooks],
   [walletConnect, walletConnectHooks],
   [coinbaseWallet, coinbaseWalletHooks],
