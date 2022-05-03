8599f97939f5c8ed22335ebf31b791046ead04d8
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2022-05-03 16:45:44 -0400

    useWeb3ReactListener

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import 'polyfills'
 import 'components/analytics'
 
 import { Web3ReactProvider } from '@web3-react/core'
+import { getConnectorForWallet } from 'constants/wallet'
+import useWeb3ReactListener from 'hooks/useWeb3ReactListener'
 import { BlockNumberProvider } from 'lib/hooks/useBlockNumber'
 import { MulticallUpdater } from 'lib/state/multicall'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
+import { useAppSelector } from 'state/hooks'
 
 import Blocklist from './components/Blocklist'
 import { connectors } from './connectors'
@@ -43,20 +46,29 @@ function Updaters() {
   )
 }
 
+const Wrapper2 = () => {
+  useWeb3ReactListener()
+  return (
+    <Blocklist>
+      <BlockNumberProvider>
+        <Updaters />
+        <ThemeProvider>
+          <ThemedGlobalStyle />
+          <App />
+        </ThemeProvider>
+      </BlockNumberProvider>
+    </Blocklist>
+  )
+}
+
 const Wrapper = () => {
+  const walletOverride = useAppSelector((state) => state.user.walletOverride)
+  const connectorOverride = walletOverride ? getConnectorForWallet(walletOverride) : undefined
   return (
     <HashRouter>
       <LanguageProvider>
-        <Web3ReactProvider connectors={connectors}>
-          <Blocklist>
-            <BlockNumberProvider>
-              <Updaters />
-              <ThemeProvider>
-                <ThemedGlobalStyle />
-                <App />
-              </ThemeProvider>
-            </BlockNumberProvider>
-          </Blocklist>
+        <Web3ReactProvider connectors={connectors} connectorOverride={connectorOverride}>
+          <Wrapper2 />
         </Web3ReactProvider>
       </LanguageProvider>
     </HashRouter>
