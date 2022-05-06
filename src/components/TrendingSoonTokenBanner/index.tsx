0895a56f2599f493c5d36ca677afd458d83c96be
nguyenhuudungz <34548156+nguyenhuudungz@users.noreply.github.com>
2022-05-06 14:19:23 +0700

    Change network correctly in top trending tokens widget (#947)
    Change network correctly in the top trending tokens widget

--- src/components/TrendingSoonTokenBanner/index.tsx
+++ src/components/TrendingSoonTokenBanner/index.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, memo, useMemo } from 'react'
+import React, { CSSProperties, useMemo } from 'react'
 import { Currency, Token } from '@dynamic-amm/sdk'
 import { useActiveWeb3React } from 'hooks'
 import { wrappedCurrency } from 'utils/wrappedCurrency'
@@ -12,27 +12,26 @@ import useTheme from 'hooks/useTheme'
 import { rgba } from 'polished'
 import useMixpanel, { MIXPANEL_TYPE, nativeNameFromETH } from 'hooks/useMixpanel'
 import { Flex } from 'rebass'
+import { Field } from '../../state/swap/actions'
 
 const TrendingSoonTokenBanner = ({
-  currency0,
-  currency1,
+  currencies,
   style,
 }: {
-  currency0?: Currency
-  currency1?: Currency
+  currencies: { [field in Field]?: Currency }
   style?: CSSProperties
 }) => {
   const { chainId } = useActiveWeb3React()
   const theme = useTheme()
   const { mixpanelHandler } = useMixpanel()
 
-  const token0 = wrappedCurrency(currency0, chainId)
-  const token1 = wrappedCurrency(currency1, chainId)
+  const token0 = useMemo(() => wrappedCurrency(currencies[Field.INPUT], chainId), [chainId, currencies])
+  const token1 = useMemo(() => wrappedCurrency(currencies[Field.OUTPUT], chainId), [chainId, currencies])
   const trendingToken0Id = useGetTrendingSoonTokenId(token0)
   const trendingToken1Id = useGetTrendingSoonTokenId(token1)
   const trendingSoonCurrency = useMemo(
-    () => (trendingToken0Id ? currency0 : trendingToken1Id ? currency1 : undefined),
-    [currency0, currency1, trendingToken0Id, trendingToken1Id],
+    () => (trendingToken0Id ? currencies[Field.INPUT] : trendingToken1Id ? currencies[Field.OUTPUT] : undefined),
+    [currencies, trendingToken0Id, trendingToken1Id],
   )
 
   if (trendingSoonCurrency === undefined) return null
@@ -104,4 +103,4 @@ const BannerText = styled.div`
   //}
 `
 
-export default memo(TrendingSoonTokenBanner)
+export default TrendingSoonTokenBanner
