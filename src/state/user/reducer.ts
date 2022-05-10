83af532a4a57ac6b612d92add5ebd2d535673064
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2022-05-10 17:18:33 -0400

    add comment for wallet override

--- src/state/user/reducer.ts
+++ src/state/user/reducer.ts
@@ -52,8 +52,12 @@ export interface UserState {
 
   showDonationLink: boolean
 
-  walletOverride: Wallet | undefined
+  // We want the user to be able to define which wallet they want to use, even if there are multiple connected wallets via web3-react.
+  // If a user had previously connected a wallet but didn't have a wallet override set (because they connected prior to this field being added),
+  // we want to handle that case by backfilling them manually. Once we backfill, we set the backfilled field to `true`.
+  // After some period of time, our active users will have this property set so we can likely remove the backfilling logic.
   walletOverrideBackfilled: boolean
+  walletOverride: Wallet | undefined
 }
 
 function pairKey(token0Address: string, token1Address: string) {
