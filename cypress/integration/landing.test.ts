b15449e65cf00ee13ee294dbae7daf95f461698f
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2022-05-11 12:56:21 -0400

    add back account details test

--- cypress/integration/landing.test.ts
+++ cypress/integration/landing.test.ts
@@ -19,4 +19,9 @@ describe('Landing Page', () => {
   it('is connected', () => {
     cy.get('#web3-status-connected').contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
   })
+
+  it('displays account details', () => {
+    cy.get('#web3-status-connected').click()
+    cy.get('#web3-account-identifier-row').contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
+  })
 })
