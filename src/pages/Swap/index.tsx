f3e152c95f9d26df3a162a70374e72efa62dba62
Christine Legge <christine.legge@uniswap.org>
2022-05-31 16:38:29 -0400

    testing

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -229,6 +229,7 @@ export default function Swap({ history }: RouteComponentProps) {
       try {
         await gatherPermitSignature()
       } catch (error) {
+        console.log('FAILED!!!!!! WTF', error)
         // try to approve if gatherPermitSignature failed for any reason other than the user rejecting it
         if (error?.code !== 4001) {
           await approveCallback()
