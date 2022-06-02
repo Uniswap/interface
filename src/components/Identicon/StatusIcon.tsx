87a9f02a9879958b2d4b2dc7b5c3c71fe83adced
Vignesh Mohankumar <me@vig.xyz>
2022-06-02 16:25:51 -0400

    fortmatic icon

--- src/components/Identicon/StatusIcon.tsx
+++ src/components/Identicon/StatusIcon.tsx
@@ -1,8 +1,9 @@
 import { Connector } from '@web3-react/types'
 
 import CoinbaseWalletIcon from '../../assets/images/coinbaseWalletIcon.svg'
+import FortmaticIcon from '../../assets/images/fortmaticIcon.png'
 import WalletConnectIcon from '../../assets/images/walletConnectIcon.svg'
-import { coinbaseWallet, injected, walletConnect } from '../../connectors'
+import { coinbaseWallet, fortmatic, injected, walletConnect } from '../../connectors'
 import Identicon from '../Identicon'
 
 export default function StatusIcon({ connector }: { connector: Connector }) {
@@ -10,9 +11,11 @@ export default function StatusIcon({ connector }: { connector: Connector }) {
     case injected:
       return <Identicon />
     case walletConnect:
-      return <img src={WalletConnectIcon} alt={'WalletConnect'} />
+      return <img src={WalletConnectIcon} alt="WalletConnect" />
     case coinbaseWallet:
-      return <img src={CoinbaseWalletIcon} alt={'Coinbase Wallet'} />
+      return <img src={CoinbaseWalletIcon} alt="Coinbase Wallet" />
+    case fortmatic:
+      return <img src={FortmaticIcon} alt="Fortmatic" />
     default:
       return null
   }
