11658bb80fd45ef298bc77f16df6a293f245a660
Tina <59578595+tinaszheng@users.noreply.github.com>
2022-06-02 14:39:46 -0700

    feat: WC disabled camera state (#677)
    * disabled camera state
    
    * change text color
    
    * revert connected sites text color
    
    * -.-

--- src/components/WalletConnect/ScanSheet/QRCodeScanner.tsx
+++ src/components/WalletConnect/ScanSheet/QRCodeScanner.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { LayoutRectangle, StyleSheet } from 'react-native'
+import { Alert, LayoutRectangle, StyleSheet } from 'react-native'
 import { FadeIn, FadeOut, runOnJS } from 'react-native-reanimated'
 import {
   Camera,
@@ -12,10 +12,8 @@ import { useAppTheme } from 'src/app/hooks'
 import CameraScan from 'src/assets/icons/camera-scan.svg'
 import WalletConnectLogo from 'src/assets/icons/walletconnect.svg'
 import { Button } from 'src/components/buttons/Button'
-import { PrimaryButton } from 'src/components/buttons/PrimaryButton'
 import { AnimatedFlex, Flex } from 'src/components/layout'
 import { Text } from 'src/components/Text'
-import { ElementName } from 'src/features/telemetry/constants'
 import { opacify } from 'src/utils/colors'
 import { openSettings } from 'src/utils/linking'
 import { Barcode, BarcodeFormat, scanBarcodes } from 'vision-camera-code-scanner'
@@ -65,6 +63,21 @@ export function QRCodeScanner({
   }, [])
 
   useEffect(() => {
+    if (permission === 'denied') {
+      Alert.alert(
+        t('Camera is disabled'),
+        t('To scan a code, allow Camera access in system settings'),
+        [
+          { text: t('Go to settings'), onPress: openSettings },
+          {
+            text: t('Not now'),
+          },
+        ]
+      )
+    }
+  }, [permission, t])
+
+  useEffect(() => {
     if (!data) return
 
     // qr code scanning returns string here
@@ -77,73 +90,61 @@ export function QRCodeScanner({
     runOnJS(setBarcodes)(detectedBarcodes)
   }, [])
 
-  if (permission === 'authorized' && backCamera) {
-    return (
-      <AnimatedFlex grow borderRadius="md" entering={FadeIn} exiting={FadeOut} overflow="hidden">
+  return (
+    <AnimatedFlex
+      grow
+      borderRadius="md"
+      entering={FadeIn}
+      exiting={FadeOut}
+      overflow="hidden"
+      onLayout={(event: any) => setLayout(event.nativeEvent.layout)}>
+      {backCamera && (
         <Camera
           isActive
           device={backCamera}
           frameProcessor={frameProcessor}
           style={StyleSheet.absoluteFill}
-          onLayout={(event) => setLayout(event.nativeEvent.layout)}
         />
-        {layout && (
-          <Flex centered gap="xxl" style={StyleSheet.absoluteFill}>
-            <Flex centered gap="xs">
-              <Text color="white" variant="largeLabel">
-                {t('Scan a QR code')}
+      )}
+      {layout && (
+        <Flex centered gap="xxl" style={StyleSheet.absoluteFill}>
+          <Flex centered gap="xs">
+            <Text color="neutralTextPrimary" variant="largeLabel">
+              {t('Scan a QR code')}
+            </Text>
+            <Flex centered row gap="sm">
+              {<WalletConnectLogo height={16} width={16} />}
+              <Text color="neutralTextPrimary" variant="body2">
+                {t('Connect to an app with WalletConnect')}
               </Text>
-              <Flex centered row gap="sm">
-                {<WalletConnectLogo height={16} width={16} />}
-                <Text color="white" variant="body2">
-                  {t('Connect to an app with WalletConnect')}
+            </Flex>
+          </Flex>
+          <CameraScan
+            color={theme.colors.white}
+            height={layout.width * SCAN_ICON_WIDTH_RATIO}
+            strokeWidth={5}
+            width={layout.width * SCAN_ICON_WIDTH_RATIO}
+          />
+          {numConnections && (
+            <Button onPress={onPressConnections}>
+              <Flex
+                row
+                alignItems="center"
+                borderRadius="full"
+                px="lg"
+                py="sm"
+                style={{ backgroundColor: opacify(40, theme.colors.black) }}>
+                <WalletConnectLogo height={30} width={30} />
+                <Text color="white" variant="mediumLabel">
+                  {numConnections === 1
+                    ? t('1 site connected')
+                    : t('{{numConnections}} sites connected', { numConnections })}
                 </Text>
               </Flex>
-            </Flex>
-            <CameraScan
-              color={theme.colors.white}
-              height={layout.width * SCAN_ICON_WIDTH_RATIO}
-              strokeWidth={5}
-              width={layout.width * SCAN_ICON_WIDTH_RATIO}
-            />
-            {numConnections && (
-              <Button onPress={onPressConnections}>
-                <Flex
-                  row
-                  alignItems="center"
-                  borderRadius="full"
-                  px="lg"
-                  py="sm"
-                  style={{ backgroundColor: opacify(40, theme.colors.black) }}>
-                  <WalletConnectLogo height={30} width={30} />
-                  <Text color="white" variant="mediumLabel">
-                    {numConnections === 1
-                      ? t('1 site connected')
-                      : t('{{numConnections}} sites connected', { numConnections })}
-                  </Text>
-                </Flex>
-              </Button>
-            )}
-          </Flex>
-        )}
-      </AnimatedFlex>
-    )
-  }
-
-  return (
-    <Flex centered grow backgroundColor="deprecated_background1" gap="md">
-      {permission === 'denied' ||
-        (permission === 'restricted' && (
-          <>
-            <Text variant="mediumLabel">📸</Text>
-            <Text variant="mediumLabel">{t('Please enable your camera.')}</Text>
-            <PrimaryButton
-              label="Open settings"
-              name={ElementName.OpenSettingsButton}
-              onPress={openSettings}
-            />
-          </>
-        ))}
-    </Flex>
+            </Button>
+          )}
+        </Flex>
+      )}
+    </AnimatedFlex>
   )
 }
