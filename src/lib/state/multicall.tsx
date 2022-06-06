d2a1c5075307bf2e32ba55a1d70368ec9641c89e
samisbakedham <samsafahi@gmail.com>
2022-06-05 22:55:46 -0700

    up

--- src/lib/state/multicall.tsx
+++ src/lib/state/multicall.tsx
@@ -1,4 +1,4 @@
-import { createMulticall } from '@uniswap/redux-multicall'
+import { createMulticall } from '@cndllabs/redux-multicall'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { useInterfaceMulticall } from 'hooks/useContract'
 import useBlockNumber from 'lib/hooks/useBlockNumber'
