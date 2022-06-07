c861f530f54f6eca7566289461a7ca8fa603bfdb
Lynn Yu <lynn.yu@uniswap.org>
2022-06-07 14:34:01 -0400

    fix: add missing GoogleAnalyticsProvider import

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-restricted-imports
 import { t, Trans } from '@lingui/macro'
 import { Percent } from '@uniswap/sdk-core'
+import GoogleAnalyticsProvider from 'components/analytics/GoogleAnalyticsProvider'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { AUTO_ROUTER_SUPPORTED_CHAINS } from 'lib/hooks/routing/clientSideSmartOrderRouter'
 import { useContext, useRef, useState } from 'react'
