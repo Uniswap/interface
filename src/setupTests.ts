e0f44280d6bdaf37e38a2b22ed8d3f2d582fa347
Zach Pomerantz <zzmp@uniswap.org>
2022-06-08 16:15:49 -0400

    fix: Readable for ReadableStream in jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,10 +1,10 @@
 import '@testing-library/jest-dom'
 
-import { ReadableStream } from 'stream/web'
+import { Readable } from 'stream'
 import { TextDecoder, TextEncoder } from 'util'
 
 if (typeof global.TextEncoder === 'undefined') {
-  global.ReadableStream = ReadableStream
+  global.ReadableStream = Readable as unknown as typeof globalThis.ReadableStream
   global.TextEncoder = TextEncoder
   global.TextDecoder = TextDecoder as typeof global.TextDecoder
 }
