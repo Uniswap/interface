727dd1b7074f8e3139a099cee26faa0b4d9f6efa
Zach Pomerantz <zzmp@uniswap.org>
2022-06-09 16:02:50 -0400

    fix: cy then

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -10,6 +10,8 @@ import './ethereum'
 
 // Unregister the ServiceWorker between test suites.
 // Doing this globally ensures that tests always run on the latest version of the site.
-beforeEach(async () => {
-  await window.navigator.serviceWorker.getRegistration().then((sw) => sw?.unregister())
+beforeEach(() => {
+  cy.window().then(async (window) => {
+    await window.navigator.serviceWorker.getRegistration().then((sw) => sw?.unregister())
+  })
 })
