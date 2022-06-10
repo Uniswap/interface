9f0fddfa68f0c838aee4bc95e08b42d27112536b
Zach Pomerantz <zzmp@uniswap.org>
2022-06-10 11:48:49 -0400

    style: lint

--- cypress/integration/service-worker.test.ts
+++ cypress/integration/service-worker.test.ts
@@ -1,4 +1,4 @@
-import assert = require("assert")
+import assert = require('assert')
 
 describe('ServiceWorker', () => {
   beforeEach(() => {
@@ -17,7 +17,7 @@ describe('ServiceWorker', () => {
   })
 
   it('installs a ServiceWorker', () => {
-    cy.get('#swap-page').wait('@NoServiceWorker', { timeout: 20000 })
+    cy.get('#swap-page').wait('@NoServiceWorker', { timeout: 30000 })
     precacheReady()
   })
 
@@ -28,7 +28,7 @@ describe('ServiceWorker', () => {
         .then(async ({ cache, key }) => cache.put(key, new Response()))
         .reload()
         .get('#swap-page')
-        .wait('@CacheMiss', { timeout: 20000 })
+        .wait('@CacheMiss', { timeout: 30000 })
     })
 
     it('records a cache hit from the ServiceWorker', () => {
@@ -37,39 +37,43 @@ describe('ServiceWorker', () => {
         .then(async ({ cache, key }) => {
           await cache.delete(key)
           const index = await fetch('/')
-          const text = await index.text() + '<script>document.domain="localhost"</script>'
+          const text = (await index.text()) + '<script>document.domain="localhost"</script>'
           const wrappedIndex = new Response(text, index)
           cache.put(key, wrappedIndex)
         })
         .pause()
         .reload()
         .get('#swap-page')
-        .wait('@CacheHit', { timeout: 20000 })
+        .wait('@CacheHit', { timeout: 30000 })
     })
   })
 
-  function precacheReady(): Cypress.Chainable<{ cache: Cache, key: Request }> {
+  function precacheReady(): Cypress.Chainable<{ cache: Cache; key: Request }> {
     let cache: Cache
     let key: Request
 
-    return cy.window({ timeout: 20000 }).and(async () => {
-      const sw = await window.navigator.serviceWorker.ready
-      expect(sw?.active?.state).to.equal('activated')
-    }).and(async () => {
-      // Give the SW a chance to open (and lock) the cache.
-      await new Promise((resolve) => setTimeout(resolve, 10))
+    return cy
+      .window({ timeout: 20000 })
+      .and(async () => {
+        const sw = await window.navigator.serviceWorker.ready
+        expect(sw?.active?.state).to.equal('activated')
+      })
+      .and(async () => {
+        // Give the SW a chance to open (and lock) the cache.
+        await new Promise((resolve) => setTimeout(resolve, 10))
 
-      // Cypress wraps the document to allow for cross-domain inspection,
-      // so we must replace the SW's cached index.html with a fetched wrapped index.html.
-      const cacheKey = (await window.caches.keys()).find((key) => key.includes('precache'))
-      expect(cacheKey).to.exist
-      assert(cacheKey)
-      cache = await caches.open(cacheKey)
+        // Cypress wraps the document to allow for cross-domain inspection,
+        // so we must replace the SW's cached index.html with a fetched wrapped index.html.
+        const cacheKey = (await window.caches.keys()).find((key) => key.includes('precache'))
+        expect(cacheKey).to.match(/precache/)
+        assert(cacheKey)
+        cache = await caches.open(cacheKey)
 
-      const req = (await cache.keys()).find((req) => req.url.includes('index.html'))
-      expect(req?.url).to.match(/index.html/)
-      assert(req)
-      key = req
-    }).then(() => ({ cache, key }))
+        const req = (await cache.keys()).find((req) => req.url.includes('index.html'))
+        expect(req?.url).to.match(/index.html/)
+        assert(req)
+        key = req
+      })
+      .then(() => ({ cache, key }))
   }
 })
