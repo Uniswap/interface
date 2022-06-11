6e6e668d93693f79388f40715a89eee5772db079
Tina <59578595+tinaszheng@users.noreply.github.com>
2022-06-11 10:13:50 -0700

    feat: Use Input-Mono as monospace font (#746)
    replace font

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -16,7 +16,7 @@ export const fontFamily = {
     medium: 'Inter-Medium',
     semibold: 'Inter-SemiBold',
     bold: 'Inter-Bold',
-    monospace: 'monospace',
+    monospace: 'InputMono-Regular',
   },
 }
 
