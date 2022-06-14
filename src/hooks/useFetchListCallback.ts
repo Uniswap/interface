ab489a93f46ba4a3146901f24182d6a5138e4470
Vignesh Mohankumar <me@vig.xyz>
2022-06-14 14:21:49 -0400

    mainnet provider

--- src/hooks/useFetchListCallback.ts
+++ src/hooks/useFetchListCallback.ts
@@ -1,7 +1,7 @@
-import { Web3Provider } from '@ethersproject/providers'
+import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers'
 import { nanoid } from '@reduxjs/toolkit'
 import { TokenList } from '@uniswap/token-lists'
-import { networkHooks } from 'connectors'
+import { MAINNET_PROVIDER } from 'constants/infura'
 import getTokenList from 'lib/hooks/useTokenList/fetchTokenList'
 import resolveENSContentHash from 'lib/utils/resolveENSContentHash'
 import { useCallback } from 'react'
@@ -9,24 +9,19 @@ import { useAppDispatch } from 'state/hooks'
 
 import { fetchTokenList } from '../state/lists/actions'
 
-const ensResolver = async (ensName: string, provider: Web3Provider | undefined, isActive: boolean) => {
-  if (!provider || !isActive) {
-    throw Error('network is inactive')
-  }
+const ensResolver = async (ensName: string, provider: JsonRpcProvider | Web3Provider) => {
   return resolveENSContentHash(ensName, provider)
 }
 
 export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean) => Promise<TokenList> {
   const dispatch = useAppDispatch()
-  const provider = networkHooks.useProvider()
-  const isActive = networkHooks.useIsActive()
 
   // note: prevent dispatch if using for list search or unsupported list
   return useCallback(
     async (listUrl: string, sendDispatch = true) => {
       const requestId = nanoid()
       sendDispatch && dispatch(fetchTokenList.pending({ requestId, url: listUrl }))
-      return getTokenList(listUrl, (ensName: string) => ensResolver(ensName, provider, isActive))
+      return getTokenList(listUrl, (ensName: string) => ensResolver(ensName, MAINNET_PROVIDER))
         .then((tokenList) => {
           sendDispatch && dispatch(fetchTokenList.fulfilled({ url: listUrl, tokenList, requestId }))
           return tokenList
@@ -37,6 +32,6 @@ export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean
           throw error
         })
     },
-    [dispatch, isActive, provider]
+    [dispatch]
   )
 }
