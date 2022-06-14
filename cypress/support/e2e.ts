fa14e929e51847bfd10780fb292a34e2d8e1aec2
Zach Pomerantz <zzmp@uniswap.org>
2022-06-13 20:58:38 -0400

    fix: inject ethereum

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -6,7 +6,36 @@
 // ***********************************************************
 
 // Import commands.ts using ES2015 syntax:
-import './ethereum'
+import { injected } from './ethereum'
+import assert = require('assert')
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface ApplicationWindow {
+      ethereum: typeof injected
+    }
+  }
+}
+
+// sets up the injected provider to be a mock ethereum provider with the given mnemonic/index
+// eslint-disable-next-line no-undef
+Cypress.Commands.overwrite(
+  'visit',
+  (original, url: string | Partial<Cypress.VisitOptions>, options?: Partial<Cypress.VisitOptions>) => {
+    assert(typeof url === 'string')
+    return original({
+      ...options,
+      url: (url.startsWith('/') && url.length > 2 && !url.startsWith('/#') ? `/#${url}` : url) + '?chain=rinkeby',
+      async onBeforeLoad(win) {
+        options?.onBeforeLoad?.(win)
+        win.localStorage.clear()
+        win.navigator.serviceWorker.getRegistration().then((sw) => sw?.unregister())
+        win.ethereum = injected
+      },
+    })
+  }
+)
 
 beforeEach(() => {
   // Infura security policies are based on Origin headers.
