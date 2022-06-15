c7e0596ec577f604adcfe6720f1be7f3b59a8044
Vignesh Mohankumar <me@vig.xyz>
2022-06-15 15:11:10 -0400

    rm ensResolver

--- src/hooks/useFetchListCallback.ts
+++ src/hooks/useFetchListCallback.ts
@@ -1,4 +1,3 @@
-import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers'
 import { nanoid } from '@reduxjs/toolkit'
 import { TokenList } from '@uniswap/token-lists'
 import { MAINNET_PROVIDER } from 'constants/infura'
@@ -9,10 +8,6 @@ import { useAppDispatch } from 'state/hooks'
 
 import { fetchTokenList } from '../state/lists/actions'
 
-const ensResolver = async (ensName: string, provider: JsonRpcProvider | Web3Provider) => {
-  return resolveENSContentHash(ensName, provider)
-}
-
 export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean) => Promise<TokenList> {
   const dispatch = useAppDispatch()
 
@@ -21,7 +16,7 @@ export function useFetchListCallback(): (listUrl: string, sendDispatch?: boolean
     async (listUrl: string, sendDispatch = true) => {
       const requestId = nanoid()
       sendDispatch && dispatch(fetchTokenList.pending({ requestId, url: listUrl }))
-      return getTokenList(listUrl, (ensName: string) => ensResolver(ensName, MAINNET_PROVIDER))
+      return getTokenList(listUrl, (ensName: string) => resolveENSContentHash(ensName, MAINNET_PROVIDER))
         .then((tokenList) => {
           sendDispatch && dispatch(fetchTokenList.fulfilled({ url: listUrl, tokenList, requestId }))
           return tokenList
