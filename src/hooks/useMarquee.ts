7372c97e4cc489a2144b87c00194e5d764f409b0
nguyenhuudungz <34548156+nguyenhuudungz@users.noreply.github.com>
2022-06-16 14:53:51 +0700

    Fix marquee (#1029)

--- src/hooks/useMarquee.ts
+++ src/hooks/useMarquee.ts
@@ -8,16 +8,24 @@ export default function useMarquee(...args: any): React.RefObject<HTMLDivElement
   useDeepCompareEffect(() => {
     let itv: NodeJS.Timeout | undefined
     if (marqueeContainerRef && marqueeContainerRef.current && !endScroll) {
+      let lastScrollLeft = marqueeContainerRef.current.scrollLeft
       itv = setInterval(() => {
         if (
           marqueeContainerRef.current &&
           marqueeContainerRef.current.scrollLeft + marqueeContainerRef.current.clientWidth <
             marqueeContainerRef.current.scrollWidth - 1
         ) {
+          const acceleration = Math.abs(lastScrollLeft - marqueeContainerRef.current.scrollLeft)
+          if (acceleration >= 30) {
+            setEndScroll(true)
+            return
+          }
+
           if (!endScroll) {
             marqueeContainerRef.current.scrollTo({
               left: marqueeContainerRef.current.scrollLeft + 1,
             })
+            lastScrollLeft = marqueeContainerRef.current.scrollLeft
           }
         } else {
           setEndScroll(true)
