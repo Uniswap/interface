12cdf09c05612ef6c44366b7328c10c05e5709ce
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-06-16 13:46:01 -0400

    Added ETH->wETH testing
    Added a Cypress test to check wrapped value swap has no price impact

--- cypress/e2e/swap.test.ts
+++ cypress/e2e/swap.test.ts
@@ -48,6 +48,17 @@ describe('Swap', () => {
     cy.get('#add-recipient-button').should('not.exist')
   })
 
+  it('ETH to wETH is same value (wrapped swaps have no price impact)', () => {
+    cy.get('#swap-currency-output .open-currency-select-button').click()
+    cy.get('.token-item-0xc778417E063141139Fce010982780140Aa0cD5Ab').should('be.visible')
+    cy.get('.token-item-0xc778417E063141139Fce010982780140Aa0cD5Ab').click({ force: true })
+    cy.get('#swap-currency-input .token-amount-input').should('be.visible')
+    cy.get('#swap-currency-input .token-amount-input').type('0.01', { force: true, delay: 100 })
+    console.log(cy.get('#swap-currency-input .token-amount-input'))
+    console.log(cy.get('#swap-currency-output .token-amount-input'))
+    cy.get('#swap-currency-output .token-amount-input').should('have.value', '0.01')
+  })
+
   describe('expert mode', () => {
     beforeEach(() => {
       cy.window().then((win) => {
