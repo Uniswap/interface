be3f2d8535da0c3a158e24662fb7acb24ddc2777
Nguyen Van Viet <vietnv1304@gmail.com>
2022-06-17 14:19:45 +0700

    fix: infinite call due to transaction deadline (#1035)

--- src/hooks/useTransactionDeadline.ts
+++ src/hooks/useTransactionDeadline.ts
@@ -1,15 +1,15 @@
-import { BigNumber } from 'ethers'
 import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { AppState } from '../state'
+import useCurrentBlockTimestamp from './useCurrentBlockTimestamp'
+import { BigNumber } from 'ethers'
 
 // combines the block timestamp with the user setting to give the deadline that should be used for any submitted transaction
 export default function useTransactionDeadline(): BigNumber | undefined {
   const ttl = useSelector<AppState, number>(state => state.user.userDeadline)
-
-  const currentTimeStamp = BigNumber.from(Math.floor(+new Date() / 1000))
+  const blockTimestamp = useCurrentBlockTimestamp()
   return useMemo(() => {
-    if (currentTimeStamp && ttl) return currentTimeStamp.add(ttl)
+    if (blockTimestamp && ttl) return blockTimestamp.add(ttl)
     return undefined
-  }, [currentTimeStamp, ttl])
+  }, [blockTimestamp, ttl])
 }
