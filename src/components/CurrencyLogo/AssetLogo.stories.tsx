04691297d3d0c167daf78468c6e526f3e10a44a2
Justin Domingue <judo@uniswap.org>
2022-06-17 10:25:40 -0400

    chore: add fallback logo component for assets (#736)

---
+++ src/components/CurrencyLogo/AssetLogo.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
+import { Box } from '../layout'
+import { AssetLogoWithFallback } from './AssetLogo'
+
+export default {
+  title: 'Logos/AssetLogo',
+  component: AssetLogoWithFallback,
+  decorators: [
+    (Story) => (
+      <Box bg="deprecated_gray50" height={300} width={300}>
+        <Story />
+      </Box>
+    ),
+  ],
+} as ComponentMeta<typeof AssetLogoWithFallback>
+
+const Template: ComponentStory<typeof AssetLogoWithFallback> = (args) => (
+  <AssetLogoWithFallback {...args} />
+)
+
+export const Primary = Template.bind({})
+Primary.args = {
+  url: 'https://token-icons.s3.amazonaws.com/eth.png',
+  name: 'Maker',
+}
+
+export const Fallback = Template.bind({})
+Fallback.args = {
+  url: undefined,
+  name: 'Maker',
+}
---
+++ src/components/CurrencyLogo/AssetLogo.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Image, ImageStyle } from 'react-native'
+import { Flex } from 'src/components/layout/Flex'
+import { Text } from 'src/components/Text'
+
+const tokenLogoStyle: ImageStyle = { width: 35, height: 35, borderRadius: 35 / 2 }
+
+interface AssetLogoProps {
+  url?: string
+  // will fallback to name if no url
+  name: string
+}
+
+export function AssetLogoWithFallback({ name, url }: AssetLogoProps) {
+  return url ? (
+    <Image source={{ uri: url }} style={tokenLogoStyle} />
+  ) : (
+    <Flex
+      centered
+      backgroundColor="neutralContainer"
+      borderRadius="full"
+      height={tokenLogoStyle.height}
+      width={tokenLogoStyle.width}>
+      <Text color="neutralTextTertiary" variant="subHead2">
+        {name[0]}
+      </Text>
+    </Flex>
+  )
+}
