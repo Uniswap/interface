c4c64f821dafbf2557b3634b9d2301e62c94ff37
Spencer Yen <spencersyen@gmail.com>
2022-06-22 14:36:35 -0400

    chore: Add basic documentation to RNEthersRS key management functions (#806)
    * Add basic documentation to RNEthersRS key management functions
    
    * Fix param docs for generateAndStorePrivateKey
    
    * Update ios/RNEthersRS.swift
    
    Co-authored-by: Justin Domingue <judo@uniswap.org>
    
    Co-authored-by: Justin Domingue <judo@uniswap.org>

--- ios/RNEthersRS.swift
+++ ios/RNEthersRS.swift
@@ -3,7 +3,13 @@
 //  Uniswap
 //
 //  Created by Connor McEwen on 10/28/21.
-//
+
+/**
+ Provides the generation, storage, and signing logic for mnemonics and private keys so that they never passed to JS.
+ 
+ Mnemonics and private keys are stored and accessed in the native iOS secure keychain key-value store via associated keys formed from concatenating a constant prefix with the associated public address.
+ 
+ Uses KeychainSwift as a wrapper utility to interface with the native iOS secure keychain. */
 
 import Foundation
 
@@ -25,6 +31,11 @@ class RNEthersRS: NSObject {
     return false
   }
 
+  /**
+   Fetches all mnemonic IDs, which are used as keys to access the actual mnemonics in the native keychain secure key-value store.
+  
+   - returns: array of mnemonic IDs
+  */
   @objc(getMnemonicIds:reject:)
   func getMnemonicIds(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
     let mnemonicIds = keychain.allKeys.filter { key in
@@ -35,6 +46,12 @@ class RNEthersRS: NSObject {
     resolve(mnemonicIds)
   }
 
+  /**
+   Derives private key from mnemonic with derivation index 0 and retrieves associated public address. Stores imported mnemonic in native keychain with the mnemonic ID key as the public address.
+   
+   - parameter mnemonic: The mnemonic phrase to import
+   - returns: public address from the mnemonic's first derived private key
+   */
   @objc(importMnemonic:resolve:reject:)
   func importMnemonic(
     mnemonic: String, resolve: RCTPromiseResolveBlock,
@@ -54,6 +71,11 @@ class RNEthersRS: NSObject {
     return
   }
 
+  /**
+   Generates a new mnemonic and retrieves associated public address. Stores new mnemonic in native keychain with the mnemonic ID key as the public address.
+
+   - returns: public address from the mnemonic's first derived private key
+   */
   @objc(generateAndStoreMnemonic:reject:)
   func generateAndStoreMnemonic(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
     let mnemonic_ptr = generate_mnemonic()
@@ -64,6 +86,11 @@ class RNEthersRS: NSObject {
     resolve(res)
   }
 
+  /**
+   Stores mnemonic phrase in Native Keychain under the address
+
+   - returns: public address if successfully stored in native keychain
+   */
   func storeNewMnemonic(mnemonic: String, address: String) -> String? {
     let newMnemonicKey = keychainKeyForMnemonicId(mnemonicId: address);
     let checkStored = retrieveMnemonic(mnemonicId: newMnemonicKey)
@@ -84,6 +111,11 @@ class RNEthersRS: NSObject {
     return keychain.get(keychainKeyForMnemonicId(mnemonicId: mnemonicId))
   }
 
+  /**
+   Fetches all public addresses from private keys stored under `privateKeyPrefix` in native keychain. Used from React Native to verify the native keychain has the private key for an account that is attempting create a NativeSigner that calls native signing methods
+
+   - returns: public addresses for all stored private keys
+   */
   @objc(getAddressesForStoredPrivateKeys:reject:)
   func getAddressesForStoredPrivateKeys(
     resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock
@@ -101,6 +133,13 @@ class RNEthersRS: NSObject {
     keychain.set(privateKey, forKey: newKey)
   }
 
+  /**
+   Derives private key and public address from mnemonic associated with `mnemonicId` for given `derivationIndex`. Stores the private key in native keychain with key.
+
+   - parameter mnemonicId: key string associated with mnemonic to generate private key for (currently convention is to use public address associated with mnemonic)
+   - parameter derivationIndex: number used to specify a which derivation index to use for deriving a private key from the mnemonic
+   - returns: public address associated with private key generated from the mnemonic at given derivation index
+   */
   @objc(generateAndStorePrivateKey:derivationIndex:resolve:reject:)
   func generateAndStorePrivateKey(
     mnemonicId: String, derivationIndex: Int, resolve: RCTPromiseResolveBlock,
