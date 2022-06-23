9b059b2ef6faa7d166f7f04e101b42c2ef47b04f
Vignesh Mohankumar <me@vig.xyz>
2022-06-23 15:25:26 -0400

    forceActivate

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -1,17 +1,14 @@
 import { Web3ReactProvider } from '@web3-react/core'
 import { Connector } from '@web3-react/types'
-import { BACKFILLABLE_WALLETS, getConnectorForWallet, gnosisSafe, network, useConnectors } from 'connectors'
+import { BACKFILLABLE_WALLETS, getConnectorForWallet, gnosisSafe, injected, network, useConnectors } from 'connectors'
 import { ReactNode, useEffect } from 'react'
 import { useAppSelector } from 'state/hooks'
 
 import { isMobile } from '../../utils/userAgent'
 
-const connect = async (connector: Connector) => {
-  const isMetaMask = !!window.ethereum?.isMetaMask
+const connect = async (connector: Connector, forceActivate?: boolean) => {
   try {
-    if (isMobile && isMetaMask) {
-      await connector.activate()
-    } else if (connector.connectEagerly) {
+    if (!forceActivate && connector.connectEagerly) {
       await connector.connectEagerly()
     } else {
       await connector.activate()
@@ -27,14 +24,18 @@ export default function Web3Provider({ children }: { children: ReactNode }) {
 
   const connectors = useConnectors(selectedWallet)
 
+  const isMetaMask = !!window.ethereum?.isMetaMask
+
   useEffect(() => {
     connect(gnosisSafe)
     connect(network)
 
-    if (selectedWallet) {
+    if (isMobile && isMetaMask) {
+      connect(injected, true)
+    } else if (selectedWallet) {
       connect(getConnectorForWallet(selectedWallet))
     } else if (!selectedWalletBackfilled) {
-      BACKFILLABLE_WALLETS.map(getConnectorForWallet).forEach(connect)
+      BACKFILLABLE_WALLETS.map(getConnectorForWallet).forEach((connector) => connect(connector))
     }
     // The dependency list is empty so this is only run once on mount
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
