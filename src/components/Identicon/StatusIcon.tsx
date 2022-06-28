25fe79318cea9e0b2603f0f19a48b9b8c04f4892
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2022-06-28 14:18:10 +0200

    fix: display Tally Ho and MetaMask status icons

--- src/components/Identicon/StatusIcon.tsx
+++ src/components/Identicon/StatusIcon.tsx
@@ -2,6 +2,8 @@ import { Connector } from '@web3-react/types'
 
 import CoinbaseWalletIcon from '../../assets/images/coinbaseWalletIcon.svg'
 import FortmaticIcon from '../../assets/images/fortmaticIcon.png'
+import MetamaskIcon from '../../assets/images/metamask.png'
+import TallyIcon from '../../assets/images/tally.png'
 import WalletConnectIcon from '../../assets/images/walletConnectIcon.svg'
 import { coinbaseWallet, fortmatic, injected, metaMask, tally, walletConnect } from '../../connectors'
 import Identicon from '../Identicon'
@@ -9,9 +11,11 @@ import Identicon from '../Identicon'
 export default function StatusIcon({ connector }: { connector: Connector }) {
   switch (connector) {
     case injected:
-    case tally:
-    case metaMask:
       return <Identicon />
+    case metaMask:
+      return <img src={MetamaskIcon} alt="MetaMask" />
+    case tally:
+      return <img src={TallyIcon} alt="Tally Ho" />
     case walletConnect:
       return <img src={WalletConnectIcon} alt="WalletConnect" />
     case coinbaseWallet:
