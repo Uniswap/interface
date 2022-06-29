e66eeabc41e4c01217d7e7343b8d660af8e05b33
Vignesh Mohankumar <me@vig.xyz>
2022-06-29 14:22:37 -0400

    mobile tests

--- src/components/WalletModal/index.test.tsx
+++ src/components/WalletModal/index.test.tsx
@@ -1,10 +1,7 @@
-import { ReactChildren } from 'react'
 import { ApplicationModal } from 'state/application/reducer'
 
 import { render, screen } from '../../test-utils'
 import WalletModal from './index'
-
-jest.mock('@lingui/macro', () => ({ Trans: ({ children }: { children: ReactChildren }) => children }))
 
 jest.mock('.../../state/application/hooks', () => {
   return {
@@ -35,14 +32,40 @@ test('Loads Wallet Modal on desktop', async () => {
   expect(screen.getByText('Fortmatic')).toBeInTheDocument()
 })
 
-// test('Loads Wallet Modal on mobile', async () => {
-//   render(<WalletModal pendingTransactions={[]} confirmedTransactions={[]} />)
-// })
+test('Loads Wallet Modal on desktop with MetaMask installed', async () => {
+  window.ethereum = { isMetaMask: true }
 
-// test('Loads Wallet Modal on MetaMask browser', async () => {
-//   render(<WalletModal pendingTransactions={[]} confirmedTransactions={[]} />)
-// })
+  render(<WalletModal pendingTransactions={[]} confirmedTransactions={[]} />)
+  expect(screen.getByText('MetaMask')).toBeInTheDocument()
+  expect(screen.getByText('Coinbase Wallet')).toBeInTheDocument()
+  expect(screen.getByText('WalletConnect')).toBeInTheDocument()
+  expect(screen.getByText('Fortmatic')).toBeInTheDocument()
+})
 
-// test('Loads Wallet Modal on Coinbase Wallet browser', async () => {
-//   render(<WalletModal pendingTransactions={[]} confirmedTransactions={[]} />)
-// })
+test('Loads Wallet Modal on MetaMask browser', async () => {
+  jest.doMock('../../utils/userAgent', () => {
+    return {
+      isMobile() {
+        return true
+      },
+    }
+  })
+  window.ethereum = { isMetaMask: true }
+
+  render(<WalletModal pendingTransactions={[]} confirmedTransactions={[]} />)
+  expect(screen.getByText('MetaMask')).toBeInTheDocument()
+})
+
+test('Loads Wallet Modal on Coinbase Wallet browser', async () => {
+  jest.doMock('../../utils/userAgent', () => {
+    return {
+      isMobile() {
+        return true
+      },
+    }
+  })
+  window.ethereum = { isCoinbaseWallet: true }
+
+  render(<WalletModal pendingTransactions={[]} confirmedTransactions={[]} />)
+  expect(screen.getByText('Coinbase Wallet')).toBeInTheDocument()
+})
