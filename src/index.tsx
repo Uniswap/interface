cd0bafb8e40575c2d5e262a4407cc523856f5d88
Lynn Yu <lynn.yu@uniswap.org>
2022-06-29 16:20:35 -0400

    rebase

--- src/index.tsx
+++ src/index.tsx
@@ -43,29 +43,29 @@ function Updaters() {
 }
 
 const container = document.getElementById('root')
-const root = createRoot(container!)
 
-root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <LanguageProvider>
-          <Web3Provider>
-            <Blocklist>
-              <BlockNumberProvider>
-                <Updaters />
-                <ThemeProvider>
-                  <ThemedGlobalStyle />
-                  <App />
-                </ThemeProvider>
-              </BlockNumberProvider>
-            </Blocklist>
-          </Web3Provider>
-        </LanguageProvider>
-      </HashRouter>
-    </Provider>
-  </StrictMode>
-)
+if (container)
+  createRoot(container).render(
+    <StrictMode>
+      <Provider store={store}>
+        <HashRouter>
+          <LanguageProvider>
+            <Web3Provider>
+              <Blocklist>
+                <BlockNumberProvider>
+                  <Updaters />
+                  <ThemeProvider>
+                    <ThemedGlobalStyle />
+                    <App />
+                  </ThemeProvider>
+                </BlockNumberProvider>
+              </Blocklist>
+            </Web3Provider>
+          </LanguageProvider>
+        </HashRouter>
+      </Provider>
+    </StrictMode>
+  )
 
 if (process.env.REACT_APP_SERVICE_WORKER !== 'false') {
   serviceWorkerRegistration.register()
