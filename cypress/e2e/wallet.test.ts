1e1c483ef90596b41f649443be1b191414ab3876
Vignesh Mohankumar <me@vig.xyz>
2022-06-29 15:16:45 -0400

    findByTestId
    test -> it
    
    add types
    
    temp
    
    rm exclude
    
    get -> find

--- cypress/e2e/wallet.test.ts
+++ cypress/e2e/wallet.test.ts
@@ -6,25 +6,25 @@ describe('Wallet', () => {
   })
 
   it('displays account details', () => {
-    cy.get('[data-testid=web3-status-connected]').contains(TEST_ADDRESS_NEVER_USE_SHORTENED).click()
+    cy.findByTestId('web3-status-connected').contains(TEST_ADDRESS_NEVER_USE_SHORTENED).click()
   })
 
   it('displays account view in wallet modal', () => {
-    cy.get('[data-testid=web3-account-identifier-row]').contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
+    cy.findByTestId('web3-account-identifier-row').contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
   })
 
   it('changes back to the options grid', () => {
-    cy.get('[data-testid=wallet-change]').click()
-    cy.get('[data-testid=option-grid]').should('exist')
+    cy.findByTestId('wallet-change').click()
+    cy.findByTestId('option-grid').should('exist')
   })
 
   it('selects injected wallet option', () => {
     cy.contains('Injected').click()
-    cy.get('[data-testid=web3-account-identifier-row]').contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
+    cy.findByTestId('web3-account-identifier-row').contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
   })
 
   it('shows connect buttons after disconnect', () => {
-    cy.get('[data-testid=wallet-disconnect]').click()
-    cy.get('[data-testid=option-grid]').should('exist')
+    cy.findByTestId('data-testid=wallet-disconnect').click()
+    cy.findByTestId('data-testid=option-grid').should('exist')
   })
 })
