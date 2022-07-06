a9596bb533ff779d47aa3e9a9579d2eb51b193c0
Christine Legge <christine.legge@uniswap.org>
2022-07-06 11:22:30 -0400

    fix: watched wallet card UI fixes (#877)
    * add bottom margin to wallets you're watching and add onPress handler
    
    * change pressable -> button

--- src/components/explore/WatchedWalletsCard.tsx
+++ src/components/explore/WatchedWalletsCard.tsx
@@ -1,21 +1,14 @@
-import { default as React, useMemo } from 'react'
+import { default as React, useCallback, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { FlatList, ListRenderItemInfo } from 'react-native'
 import { useAppSelector } from 'src/app/hooks'
 import { useExploreStackNavigation } from 'src/app/navigation/types'
 import { AddressDisplay } from 'src/components/AddressDisplay'
+import { Button } from 'src/components/buttons/Button'
 import { Box } from 'src/components/layout'
 import { BaseCard } from 'src/components/layout/BaseCard'
 import { selectWatchedAddressSet } from 'src/features/favorites/selectors'
 import { Screens } from 'src/screens/Screens'
-
-function renderItem({ item: address }: ListRenderItemInfo<string>) {
-  return (
-    <Box mx="sm">
-      <AddressDisplay address={address} direction="column" variant="smallLabel" />
-    </Box>
-  )
-}
 
 /** Renders the favorite tokens card on the Explore page */
 export function WatchedWalletsCard({ onSearchWallets }: { onSearchWallets: () => void }) {
@@ -23,6 +16,22 @@ export function WatchedWalletsCard({ onSearchWallets }: { onSearchWallets: () =>
   const navigation = useExploreStackNavigation()
   const watchedWalletsSet = useAppSelector(selectWatchedAddressSet)
   const watchedWalletsList = useMemo(() => Array.from(watchedWalletsSet), [watchedWalletsSet])
+
+  const renderItem = useCallback(
+    ({ item: address }: ListRenderItemInfo<string>) => {
+      return (
+        <Button
+          onPress={() => {
+            navigation.navigate(Screens.User, { address })
+          }}>
+          <Box mx="sm">
+            <AddressDisplay address={address} direction="column" variant="smallLabel" />
+          </Box>
+        </Button>
+      )
+    },
+    [navigation]
+  )
 
   const hasWatchedWallets = watchedWalletsList.length > 0
 
@@ -41,7 +50,7 @@ export function WatchedWalletsCard({ onSearchWallets }: { onSearchWallets: () =>
         }
       />
       {hasWatchedWallets ? (
-        <Box mt="md" mx="xxs">
+        <Box mx="xxs" my="md">
           <FlatList
             horizontal
             data={watchedWalletsList}
