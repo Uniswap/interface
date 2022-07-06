e4fe993b2a6cfea014ee0116cc1b93d466cc1128
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-06 13:23:01 -0400

    token null fix

--- src/components/Explore/TokenDetail.tsx
+++ src/components/Explore/TokenDetail.tsx
@@ -2,11 +2,11 @@ import { useToken } from 'hooks/Tokens'
 
 export default function TokenDetail({ address }: { address: string }) {
   const token = useToken(address)
-  const tokenName = token?.name
-  const tokenSymbol = token?.symbol
   if (!token) {
     return <div>no token</div>
   }
+  const tokenName = token.name
+  const tokenSymbol = token.symbol
 
   return (
     <div>
