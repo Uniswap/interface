2ee70b9582861d728fa5d239b8afbc9dd9d298c6
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-07 10:43:32 -0400

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -220,8 +220,7 @@ const StyledNavLink = styled(NavLink).attrs({
 
 const StyledExternalLink = styled(ExternalLink).attrs({
   activeClassName,
-  // eslint-disable-next-line prettier/prettier
-}) <{ isActive?: boolean }>`
+})<{ isActive?: boolean }>`
   ${({ theme }) => theme.flexRowNoWrap}
   align-items: left;
   border-radius: 3rem;
