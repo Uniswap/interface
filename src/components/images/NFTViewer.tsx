646d53c96d78571b55b8c2ea742d4ed896659d04
Justin Domingue <judo@uniswap.org>
2022-07-08 10:37:23 -0400

    chore: support null nft assets (#897)

--- src/components/images/NFTViewer.tsx
+++ src/components/images/NFTViewer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ImageUri } from 'src/components/images/ImageUri'
 import { WebSvgUri } from 'src/components/images/WebSvgUri'
+import { Box } from 'src/components/layout'
 import { uriToHttp } from 'src/utils/uriToHttp'
 
 type Props = {
@@ -16,9 +17,17 @@ type Props = {
 export function NFTViewer({ autoplay = false, maxHeight, uri }: Props) {
   const imageHttpUri = uriToHttp(uri)[0]
 
+  if (!uri) {
+    // Sometimes Opensea does not return any asset, show placeholder
+    // TODO: replace with actual placeholder when design creates it
+    return <Box aspectRatio={1} bg="backgroundAction" width="100%" />
+  }
+
   if (imageHttpUri?.endsWith('.svg')) {
     return <WebSvgUri autoplay={autoplay} maxHeight={maxHeight} uri={imageHttpUri} />
-  } else {
-    return <ImageUri maxHeight={maxHeight} uri={imageHttpUri} />
   }
+
+  // TODO:  handle more asset types (video, audio, etc.)
+
+  return <ImageUri maxHeight={maxHeight} uri={imageHttpUri} />
 }
