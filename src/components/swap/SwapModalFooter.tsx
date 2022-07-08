5ee155d260b3567b258a78be43c2c85e1f623af9
Lynn Yu <lynn.yu@uniswap.org>
2022-07-08 00:05:21 -0400

    add element name constant

--- src/components/swap/SwapModalFooter.tsx
+++ src/components/swap/SwapModalFooter.tsx
@@ -1,7 +1,7 @@
 import { Trans } from '@lingui/macro'
 import { Trade } from '@uniswap/router-sdk'
 import { Currency, TradeType } from '@uniswap/sdk-core'
-import { EventName } from 'components/AmplitudeAnalytics/constants'
+import { ElementName, EventName } from 'components/AmplitudeAnalytics/constants'
 import { ActionProps } from 'components/AmplitudeAnalytics/constants'
 import { TraceEvent } from 'components/AmplitudeAnalytics/TraceEvent'
 import { ReactNode } from 'react'
@@ -30,7 +30,7 @@ export default function SwapModalFooter({
       <AutoRow>
         <TraceEvent
           actionProps={ButtonActionProps}
-          elementName={'something'}
+          elementName={ElementName.CONFIRM_SWAP_BUTTON}
           eventName={EventName.SWAP_SUBMITTED}
           eventProperties={eventProperties}
         >
@@ -38,7 +38,8 @@ export default function SwapModalFooter({
             onClick={onConfirm}
             disabled={disabledConfirm}
             style={{ margin: '10px 0 0 0' }}
-            id="confirm-swap-or-send"
+            className={ElementName.CONFIRM_SWAP_BUTTON}
+            id={ElementName.CONFIRM_SWAP_BUTTON}
           >
             <Text fontSize={20} fontWeight={500}>
               <Trans>Confirm Swap</Trans>
