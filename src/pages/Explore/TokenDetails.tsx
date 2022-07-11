ecfebe07d676002c5086f232f5a695864b79f7c5
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-11 11:44:11 -0400

    add fake widget

--- src/pages/Explore/TokenDetails.tsx
+++ src/pages/Explore/TokenDetails.tsx
@@ -3,9 +3,22 @@ import LoadedTokenDetail, { LoadingTokenDetail } from 'components/Explore/TokenD
 import { useState } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import styled from 'styled-components/macro'
+
 const TokenDetailsLayout = styled.div`
   display: flex;
   gap: 80px;
+`
+const RightPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+const Widget = styled.div`
+  height: 354px;
+  width: 284px;
+  background-color: ${({ theme }) => theme.bg2};
+  border-radius: 12px;
+  border: 1px solid rgba(153, 161, 189, 0.24);
 `
 export function TokenDetails({
   match: {
@@ -17,13 +30,14 @@ export function TokenDetails({
     setLoading(false)
   }, 1000)
 
-  const tokenDetailState = loading ? (
-    <LoadingTokenDetail />
-  ) : (
-    <>
-      <LoadedTokenDetail address={tokenAddress} />
-      <BalanceSummary address={tokenAddress} />
-    </>
+  const tokenDetailState = loading ? <LoadingTokenDetail /> : <LoadedTokenDetail address={tokenAddress} />
+  return (
+    <TokenDetailsLayout>
+      {tokenDetailState}
+      <RightPanel>
+        <Widget />
+        {loading ? <></> : <BalanceSummary address={tokenAddress} />}
+      </RightPanel>
+    </TokenDetailsLayout>
   )
-  return <TokenDetailsLayout>{tokenDetailState}</TokenDetailsLayout>
 }
