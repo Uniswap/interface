9315e48c0ccdc374c3e1adc2ae745d98a57a2852
Zach Pomerantz <zzmp@uniswap.org>
2022-07-11 19:20:13 +0200

    build: wait 1m between release tests

--- cypress/release.ts
+++ cypress/release.ts
@@ -1,8 +1,20 @@
-describe('Release', () => {
-  it('loads swap page', () => {
-    cy.visit('/', {
-      retryOnStatusCodeFailure: true,
-      retryOnNetworkFailure: true,
-    }).get('#swap-page')
-  })
-})
+const ONE_MINUTE = 60_000
+
+describe(
+  'Release',
+  {
+    pageLoadTimeout: ONE_MINUTE,
+    retries: 30,
+  },
+  () => {
+    it('loads swap page', () => {
+      // We *must* wait in order to space out the retry attempts.
+      cy.wait(ONE_MINUTE)
+        .visit('/', {
+          retryOnStatusCodeFailure: true,
+          retryOnNetworkFailure: true,
+        })
+        .get('#swap-page')
+    })
+  }
+)
