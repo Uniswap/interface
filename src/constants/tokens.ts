1f805ba8337cf5a8bafa9a360c8106f6e6088ce5
sawa <jesse.sawa@gmail.com>
2022-07-12 05:36:17 -0400

    fix: celo token casting

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -290,14 +290,14 @@ export const CUSD_CELO = new Token(
   SupportedChainId.CELO,
   '0x765DE816845861e75A25fCA122bb6898B8B1282a',
   18,
-  'CUSD',
+  'cUSD',
   'Celo Dollar'
 )
 export const CEUR_CELO = new Token(
   SupportedChainId.CELO,
   '0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73',
   18,
-  'CEUR',
+  'cEUR',
   'Celo Euro Stablecoin'
 )
 export const PORTAL_ETH_CELO = new Token(
