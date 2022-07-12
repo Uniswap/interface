a5bb529f71067e65d331b292315728deaadd2028
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-12 13:38:07 -0400

    fix prettier

--- src/components/Explore/state.ts
+++ src/components/Explore/state.ts
@@ -1,4 +1,4 @@
-import { atomWithStorage } from "jotai/utils"
+import { atomWithStorage } from 'jotai/utils'
 
 export const favoritesAtom = atomWithStorage<string[]>('favorites', [])
 export const showFavoritesAtom = atomWithStorage<boolean>('showFavorites', false)
