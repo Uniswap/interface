30292b8ac62aed15d22135d700da7965786b2c7b
Zach Pomerantz <zzmp@uniswap.org>
2022-07-12 11:14:09 +0200

    build: wait an hour for IPFS release

--- cypress.release.config.ts
+++ cypress.release.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   projectId: 'yp82ef',
+  video: false,
   e2e: {
     specPattern: 'cypress/release.ts',
   },
--- cypress/release.ts
+++ cypress/release.ts
@@ -4,17 +4,12 @@ describe(
   'Release',
   {
     pageLoadTimeout: ONE_MINUTE,
-    retries: 30,
+    retries: 60,
   },
   () => {
     it('loads swap page', () => {
       // We *must* wait in order to space out the retry attempts.
-      cy.wait(ONE_MINUTE)
-        .visit('/', {
-          retryOnStatusCodeFailure: true,
-          retryOnNetworkFailure: true,
-        })
-        .get('#swap-page')
+      cy.wait(ONE_MINUTE).visit('/').get('#swap-page')
     })
   }
 )
