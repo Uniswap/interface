d34d5cf98dffd2ffdcdae329f34bfba23a139df9
Vignesh Mohankumar <me@vig.xyz>
2022-07-12 00:03:47 -0400

    fix connectors changing

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -1,11 +1,20 @@
-import { Web3ReactProvider } from '@web3-react/core'
-import useConnectors from 'hooks/useConnectors'
+import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core'
+import { Connector } from '@web3-react/types'
+import { Connection } from 'connection'
+import { getConnectionName } from 'connection/utils'
+import useOrderedConnections from 'hooks/useConnectors'
 import useEagerlyConnect from 'hooks/useEagerlyConnect'
 import { ReactNode } from 'react'
 
 export default function Web3Provider({ children }: { children: ReactNode }) {
   useEagerlyConnect()
-  const connectors = useConnectors()
+  const connections = useOrderedConnections()
+  const connectors: [Connector, Web3ReactHooks][] = connections.map(({ hooks, connector }) => [connector, hooks])
+  const key = connections.map(({ type }: Connection) => getConnectionName(type)).join('-')
 
-  return <Web3ReactProvider connectors={connectors}>{children}</Web3ReactProvider>
+  return (
+    <Web3ReactProvider connectors={connectors} key={key}>
+      {children}
+    </Web3ReactProvider>
+  )
 }
