7d4c0555b9a407861caead1283569d2b20c7a038
sawa <jesse.sawa@gmail.com>
2022-07-12 07:04:39 -0400

    fix: celo celo explorer it

--- src/utils/getExplorerLink.test.ts
+++ src/utils/getExplorerLink.test.ts
@@ -20,7 +20,7 @@ describe('#getExplorerLink', () => {
     expect(getExplorerLink(137, 'abc', ExplorerDataType.ADDRESS)).toEqual('https://polygonscan.com/address/abc')
   })
   it('celo', () => {
-    expect(getExplorerLink(42220, 'abc', ExplorerDataType.ADDRESS)).toEqual('https://explorer.celo.org/address/abc')
+    expect(getExplorerLink(42220, 'abc', ExplorerDataType.ADDRESS)).toEqual('https://celoscan.io/address/abc')
   })
   it('ropsten', () => {
     expect(getExplorerLink(3, 'abc', ExplorerDataType.ADDRESS)).toEqual('https://ropsten.etherscan.io/address/abc')
