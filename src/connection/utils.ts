386816c4790f134f9e639d1dd62fc92106314a9b
Vignesh Mohankumar <me@vig.xyz>
2022-07-12 11:55:51 -0400

    add connection name

--- src/connection/utils.ts
+++ src/connection/utils.ts
@@ -42,3 +42,20 @@ export function getConnection(c: Connector | ConnectionType) {
     }
   }
 }
+
+export function getConnectionName(connectionType: ConnectionType, isMetaMask?: boolean) {
+  switch (connectionType) {
+    case ConnectionType.INJECTED:
+      return isMetaMask ? 'MetaMask' : 'Injected'
+    case ConnectionType.COINBASE_WALLET:
+      return 'Coinbase Wallet'
+    case ConnectionType.WALLET_CONNECT:
+      return 'WalletConnect'
+    case ConnectionType.FORTMATIC:
+      return 'Fortmatic'
+    case ConnectionType.NETWORK:
+      return 'Network'
+    case ConnectionType.GNOSIS_SAFE:
+      return 'Gnosis Safe'
+  }
+}
