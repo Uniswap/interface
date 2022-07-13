d12ec2efb7f42f2b6bc701aae9a22eea15896e85
Vignesh Mohankumar <me@vig.xyz>
2022-07-13 17:14:25 -0400

    icons

--- src/components/Explore/SearchBar.tsx
+++ src/components/Explore/SearchBar.tsx
@@ -1,6 +1,8 @@
-import useTheme from 'hooks/useTheme'
 import { useRef, useState } from 'react'
 import styled from 'styled-components/macro'
+
+import searchIcon from './search.svg'
+import xIcon from './x.svg'
 
 export const SMALL_MEDIA_BREAKPOINT = '580px'
 
@@ -12,7 +14,7 @@ const SearchBarWrapper = styled.div`
 
 const SearchInput = styled.input<{ expanded: boolean }>`
   background: no-repeat scroll 7px 7px;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%2399A1BD' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-search'%3E%3Ccircle cx='11' cy='11' r='8'%3E%3C/circle%3E%3Cline x1='21' y1='21' x2='16.65' y2='16.65'%3E%3C/line%3E%3C/svg%3E");
+  background-image: url(${searchIcon});
   background-size: 20px 20px;
   background-position: 12px center;
   background-color: ${({ theme }) => theme.bg0};
@@ -50,20 +52,13 @@ const SearchInput = styled.input<{ expanded: boolean }>`
     appearance: none;
     height: 24px;
     width: 24px;
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%2399A1BD' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-x'%3E%3Cline x1='18' y1='6' x2='6' y2='18'%3E%3C/line%3E%3Cline x1='6' y1='6' x2='18' y2='18'%3E%3C/line%3E%3C/svg%3E");
+    background-image: url(${xIcon});
     margin-right: 10px;
     background-size: 24px 24px;
   }
 `
-const IconContainer = styled.span`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  left: 12px;
-`
 
 export default function SearchBar() {
-  const theme = useTheme()
   const [isExpanded, setExpanded] = useState(false)
   const [searchContent, setSearchContent] = useState('')
   const searchRef = useRef<HTMLInputElement>(null)
