c2b2e872b0b81973e6b8d65c65d139aaddbac364
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-14 13:36:03 -0400

    fix

--- src/components/Explore/state.ts
+++ src/components/Explore/state.ts
@@ -1,9 +1,10 @@
-import { useAtom } from 'jotai'
+import { atom, useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 import { useCallback } from 'react'
 
 export const favoritesAtom = atomWithStorage<string[]>('favorites', [])
 export const showFavoritesAtom = atomWithStorage<boolean>('showFavorites', false)
+export const filterStringAtom = atom<string>('')
 
 export function useToggleFavorite(tokenAddress: string) {
   const [favoriteTokens, updateFavoriteTokens] = useAtom(favoritesAtom)
