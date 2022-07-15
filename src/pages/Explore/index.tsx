d1ec4878b76b965352419faedfd79c5e63f0168b
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-15 14:14:23 -0400

    responsive

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,4 +1,3 @@
-import { MAX_WIDTH_MEDIA_BREAKPOINT } from 'components/Explore/constants'
 import FavoriteButton from 'components/Explore/FavoriteButton'
 import SearchBar from 'components/Explore/SearchBar'
 import TimeSelector from 'components/Explore/TimeSelector'
@@ -8,9 +7,10 @@ import styled from 'styled-components/macro'
 const ExploreContainer = styled.div`
   width: 100%;
   min-width: 390px;
+  padding: 0px 12px;
 `
 const TokenTableContainer = styled.div`
-  padding: 16px 12px;
+  padding: 16px 0px;
 `
 const FiltersContainer = styled.div`
   display: flex;
@@ -20,10 +20,6 @@ const FiltersContainer = styled.div`
   grid-template-columns: 9fr 1.5fr 1fr;
   margin-left: auto;
   margin-right: auto;
-
-  @media only screen and (max-width: ${MAX_WIDTH_MEDIA_BREAKPOINT}) {
-    padding: 0px 12px;
-  }
 `
 const Explore = () => {
   return (
