81013de4c31275139a26e6d24d682e2543067516
chenxsan <chenxsan@gmail.com>
2022-07-16 14:32:23 +0800

    fix: update e2e test

--- cypress/e2e/pool.test.ts
+++ cypress/e2e/pool.test.ts
@@ -5,10 +5,4 @@ describe('Pool', () => {
     cy.get('#join-pool-button').click()
     cy.url().should('contain', '/add/ETH')
   })
-
-  it('should return to /pool', () => {
-    cy.visit('/pool/1')
-    cy.get('[data-cy="visit-pool"]').click()
-    cy.get('#join-pool-button').should('exist')
-  })
 })
