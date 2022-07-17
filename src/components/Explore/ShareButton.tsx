071961076560c57b69533b04ba352fd7eb7e018d
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-17 17:21:54 -0400

    success state

--- src/components/Explore/ShareButton.tsx
+++ src/components/Explore/ShareButton.tsx
@@ -1,7 +1,7 @@
 import { useOnClickOutside } from 'hooks/useOnClickOutside'
 import { darken } from 'polished'
-import { useRef } from 'react'
-import { Link, Share, Twitter } from 'react-feather'
+import { ReactNode, useRef, useState } from 'react'
+import { Check, Link, Share, Twitter } from 'react-feather'
 import { useModalIsOpen, useToggleModal } from 'state/application/hooks'
 import { ApplicationModal } from 'state/application/reducer'
 import styled, { useTheme } from 'styled-components/macro'
@@ -45,34 +45,92 @@ const ShareAction = styled.div`
     background-color: ${({ theme }) => darken(0.08, theme.bg1)};
   }
 `
+const LinkCopied = styled.div<{ show: boolean }>`
+  display: ${({ show }) => (show ? 'flex' : 'none')};
+  width: 328px;
+  height: 72px;
+  color: ${({ theme }) => theme.text1};
+  background-color: ${({ theme }) => theme.bg0};
+  justify-content: flex-start;
+  align-items: center;
+  padding: 24px 16px;
+  position: absolute;
+  right: 32px;
+  bottom: 32px;
+  font-size: 14px;
+  gap: 8px;
+  border: 1px solid rgba(153, 161, 189, 0.08);
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.9), 0px 8px 12px rgba(13, 14, 14, 0.8);
+  border-radius: 20px;
 
-export default function ShareButton() {
+  -moz-animation: cssAnimation 0s ease-in 3s forwards;
+  -webkit-animation: cssAnimation 0s ease-in 3s forwards;
+  -o-animation: cssAnimation 0s ease-in 3s forwards;
+  animation: cssAnimation 0s ease-in 3s forwards;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+
+  @keyframes cssAnimation {
+    to {
+      width: 0;
+      height: 0;
+      overflow: hidden;
+      display: none;
+    }
+  }
+  @-webkit-keyframes cssAnimation {
+    to {
+      width: 0;
+      height: 0;
+      visibility: hidden;
+      display: none;
+    }
+  }
+`
+
+export default function ShareButton({ tokenName, tokenSymbol }: { tokenName: ReactNode; tokenSymbol: ReactNode }) {
   const theme = useTheme()
   const node = useRef<HTMLDivElement | null>(null)
   const open = useModalIsOpen(ApplicationModal.SHARE)
   const toggleShare = useToggleModal(ApplicationModal.SHARE)
   useOnClickOutside(node, open ? toggleShare : undefined)
+  const [showCopied, setShowCopied] = useState(false)
+
   const shareTweet = () => {
     toggleShare()
-    window.open(`https://twitter.com/intent/tweet?text=app.uniswap.org`)
+    window.open(
+      `https://twitter.com/intent/tweet?text=Check%20out%20${tokenName}%20(${tokenSymbol})%20https://app.uniswap.org/#/tokens/${tokenSymbol}%20via%20@uniswap`
+    )
+  }
+  const copyLink = () => {
+    navigator.clipboard.writeText(window.location.href)
+    setShowCopied(true)
+    toggleShare()
+    setTimeout(() => setShowCopied(false), 3000)
   }
 
   return (
-    <ShareButtonDisplay ref={node}>
-      <Share size={18} onClick={toggleShare} aria-label={`ShareOptions`} />
-      {open && (
-        <ShareActions>
-          <ShareAction onClick={() => navigator.clipboard.writeText(window.location.href)}>
-            <Link color={theme.text2} />
-            Copy link
-          </ShareAction>
+    <>
+      <ShareButtonDisplay ref={node}>
+        <Share size={18} onClick={toggleShare} aria-label={`ShareOptions`} />
+        {open && (
+          <ShareActions>
+            <ShareAction onClick={copyLink}>
+              <Link color={theme.text2} />
+              Copy link
+            </ShareAction>
 
-          <ShareAction onClick={shareTweet}>
-            <Twitter color={theme.text2} />
-            Share to Twitter
-          </ShareAction>
-        </ShareActions>
-      )}
-    </ShareButtonDisplay>
+            <ShareAction onClick={shareTweet}>
+              <Twitter color={theme.text2} />
+              Share to Twitter
+            </ShareAction>
+          </ShareActions>
+        )}
+      </ShareButtonDisplay>
+      <LinkCopied show={showCopied}>
+        <Check color={theme.green1} />
+        Link Copied
+      </LinkCopied>
+    </>
   )
 }
