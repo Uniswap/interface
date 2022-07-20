1c2a6d472bb13e654bb03c999db0fa87b89651e9
sawa <jesse.sawa@gmail.com>
2022-07-20 12:21:53 -0400

    fix: add celo gas override to circumvent 'out of gas' error from multicall

--- src/hooks/useClientSideV3Trade.ts
+++ src/hooks/useClientSideV3Trade.ts
@@ -14,6 +14,8 @@ import { useQuoter } from './useContract'
 const QUOTE_GAS_OVERRIDES: { [chainId: number]: number } = {
   [SupportedChainId.ARBITRUM_ONE]: 25_000_000,
   [SupportedChainId.ARBITRUM_RINKEBY]: 25_000_000,
+  [SupportedChainId.CELO]: 50_000_000,
+  [SupportedChainId.CELO_ALFAJORES]: 50_000_000,
 }
 
 const DEFAULT_GAS_QUOTE = 2_000_000
