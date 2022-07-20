ab0e89ff4e8d1ec9b1c102e434d2f5833f2ff6ab
Vignesh Mohankumar <me@vig.xyz>
2022-07-20 15:01:15 -0400

    comment

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom' // jest custom assertions
-import 'jest-styled-components'
+import 'jest-styled-components' // adds style diffs to snapshot tests
 
 import { Readable } from 'stream'
 import { TextDecoder, TextEncoder } from 'util'
