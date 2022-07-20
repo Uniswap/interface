17c7a9ee9de1cff94365216694fc6fd146e48921
Rachel-Eichenberger <60412342+Rachel-Eichenberger@users.noreply.github.com>
2022-07-20 08:59:44 -0700

    fix: increase Polygon gas limit (#3882)
    * Update graph link
    
    * Add Gas over ride temp for Polygon
    
    * removal of personal tweaks
    
    * Update index.tsx
    
    * reset to original file
    
    * missing EOL
    
    * Update useClientSideV3Trade.ts
    
    * remove space

--- src/hooks/useClientSideV3Trade.ts
+++ src/hooks/useClientSideV3Trade.ts
@@ -14,6 +14,8 @@ import { useQuoter } from './useContract'
 const QUOTE_GAS_OVERRIDES: { [chainId: number]: number } = {
   [SupportedChainId.ARBITRUM_ONE]: 25_000_000,
   [SupportedChainId.ARBITRUM_RINKEBY]: 25_000_000,
+  [SupportedChainId.POLYGON]: 40_000_000,
+  [SupportedChainId.POLYGON_MUMBAI]: 40_000_000,
 }
 
 const DEFAULT_GAS_QUOTE = 2_000_000
