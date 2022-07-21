f01e9dea01e7d32f5e4380be5cf11abcf0b1d90d
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-21 17:04:56 -0400

    feat: different table states (#4166)
    * error state
    
    * center
    
    * filter at table level (error)
    
    * Update settings.json
    
    * filter at table
    
    * add hook
    
    * fix hook, add no tokens state
    
    * favorite in hook
    
    * add favorites part 2
    
    * fix import
    
    * revert toptokens data type
    
    * componenet

--- src/components/Explore/TokenTable.tsx
+++ src/components/Explore/TokenTable.tsx
@@ -1,9 +1,12 @@
+import { useAllTokens } from 'hooks/Tokens'
 import useTopTokens, { TimePeriod } from 'hooks/useTopTokens'
-import { useAtom } from 'jotai'
+import { useAtomValue } from 'jotai/utils'
+import { ReactNode, useMemo } from 'react'
+import { AlertTriangle } from 'react-feather'
 import styled from 'styled-components/macro'
 
 import { MAX_WIDTH_MEDIA_BREAKPOINT } from './constants'
-import { favoritesAtom, showFavoritesAtom } from './state'
+import { favoritesAtom, filterStringAtom, showFavoritesAtom } from './state'
 import LoadedRow, { HeaderRow, LoadingRow } from './TokenRow'
 
 const GridContainer = styled.div`
@@ -22,12 +25,15 @@ const GridContainer = styled.div`
 `
 const NoTokenDisplay = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
   height: 60px;
   color: ${({ theme }) => theme.textSecondary};
   font-size: 16px;
+  font-weight: 500;
   align-items: center;
   padding: 0px 28px;
+  gap: 8px;
 `
 const TokenRowsContainer = styled.div`
   padding: 4px 0px;
@@ -38,11 +44,47 @@ const LOADING_ROWS = Array(10)
     return <LoadingRow key={`${index}`} />
   })
 
+function useFilteredTokens(topTokenAddresses: string[]) {
+  const filterString = useAtomValue(filterStringAtom)
+  const favoriteTokens = useAtomValue(favoritesAtom)
+  const showFavorites = useAtomValue(showFavoritesAtom)
+  const shownTokens = showFavorites ? favoriteTokens : topTokenAddresses
+  const allTokens = useAllTokens()
+
+  return useMemo(() => {
+    const filteredTokens = shownTokens.filter((tokenAddress) => {
+      const token = allTokens[tokenAddress]
+      const tokenName = token?.name ?? ''
+      const tokenSymbol = token?.symbol ?? ''
+
+      if (!filterString) {
+        return true
+      }
+      const lowercaseFilterString = filterString.toLowerCase()
+      const addressIncludesFilterString = tokenAddress.toLowerCase().includes(lowercaseFilterString)
+      const nameIncludesFilterString = tokenName.toLowerCase().includes(lowercaseFilterString)
+      const symbolIncludesFilterString = tokenSymbol.toLowerCase().includes(lowercaseFilterString)
+      return nameIncludesFilterString || symbolIncludesFilterString || addressIncludesFilterString
+    })
+    return filteredTokens
+  }, [allTokens, shownTokens, filterString])
+}
+
+function NoTokensState({ message, timePeriod }: { message: ReactNode; timePeriod: TimePeriod }) {
+  return (
+    <GridContainer>
+      <HeaderRow timeframe={timePeriod} />
+      <NoTokenDisplay>{message}</NoTokenDisplay>
+    </GridContainer>
+  )
+}
+
 export default function TokenTable() {
   const { data, error, loading } = useTopTokens()
-  const [favoriteTokens] = useAtom(favoritesAtom)
-  const [showFavorites] = useAtom(showFavoritesAtom)
+  const showFavorites = useAtomValue(showFavoritesAtom)
   const timePeriod = TimePeriod.day
+  const topTokenAddresses = data ? Object.keys(data) : []
+  const filteredTokens = useFilteredTokens(topTokenAddresses)
 
   /* loading and error state */
   if (loading) {
@@ -53,21 +95,28 @@ export default function TokenTable() {
       </GridContainer>
     )
   } else if (error || data === null) {
-    return <GridContainer>Error Loading Top Token Data</GridContainer>
-  }
-  /* if no favorites tokens */
-  if (showFavorites && favoriteTokens.length === 0) {
     return (
-      <GridContainer>
-        <HeaderRow timeframe={timePeriod} />
-        <NoTokenDisplay>No Favorited Tokens</NoTokenDisplay>
-      </GridContainer>
+      <NoTokensState
+        message={
+          <>
+            <AlertTriangle size={16} />
+            An error occured loading tokens. Please try again.
+          </>
+        }
+        timePeriod={timePeriod}
+      />
     )
   }
 
-  const topTokenAddresses = Object.keys(data)
-  const showTokens = showFavorites ? favoriteTokens : topTokenAddresses
-  const tokenRows = showTokens.map((tokenAddress, index) => {
+  if (showFavorites && filteredTokens.length === 0) {
+    return <NoTokensState message={'You have no favorited tokens'} timePeriod={timePeriod} />
+  }
+
+  if (!showFavorites && filteredTokens.length === 0) {
+    return <NoTokensState message={'No tokens found'} timePeriod={timePeriod} />
+  }
+
+  const tokenRows = filteredTokens.map((tokenAddress, index) => {
     return (
       <LoadedRow
         key={tokenAddress}
