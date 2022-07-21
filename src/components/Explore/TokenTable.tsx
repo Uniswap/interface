40fd1d5b83b0a88dd7bd71b37e6105dcf8f8d98c
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-07-21 12:38:33 -0400

    error state

--- src/components/Explore/TokenTable.tsx
+++ src/components/Explore/TokenTable.tsx
@@ -22,10 +22,12 @@ const GridContainer = styled.div`
 `
 const NoTokenDisplay = styled.div`
   display: flex;
+  justify-content: center;
   width: 100%;
   height: 60px;
   color: ${({ theme }) => theme.textSecondary};
   font-size: 16px;
+  font-weight: 500;
   align-items: center;
   padding: 0px 28px;
 `
@@ -53,14 +55,19 @@ export default function TokenTable() {
       </GridContainer>
     )
   } else if (error || data === null) {
-    return <GridContainer>Error Loading Top Token Data</GridContainer>
+    return (
+      <GridContainer>
+        <HeaderRow timeframe={timePeriod} />
+        <NoTokenDisplay>An error occured loading tokens. Please try again.</NoTokenDisplay>
+      </GridContainer>
+    )
   }
   /* if no favorites tokens */
   if (showFavorites && favoriteTokens.length === 0) {
     return (
       <GridContainer>
         <HeaderRow timeframe={timePeriod} />
-        <NoTokenDisplay>No Favorited Tokens</NoTokenDisplay>
+        <NoTokenDisplay>You have no favorited tokens</NoTokenDisplay>
       </GridContainer>
     )
   }
