c548d9aa5456fa4b7313d85511ee059192012a91
Jorge <dalmendray@gmail.com>
2022-07-24 10:22:23 -0400

    perf: remove wdyr library from bundle (#1072)
    Co-authored-by: Jorge Dalmendray <dalmendray@jorgeuni.localdomain>

--- wdyr.ts
+++ wdyr.ts
@@ -1,8 +1,10 @@
 /// <reference types="@welldone-software/why-did-you-render" />
-import whyDidYouRender from '@welldone-software/why-did-you-render'
 import React from 'react'
 
 if (process.env.NODE_ENV === 'development') {
+  // The library should NEVER be used in production because it slows down React
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const whyDidYouRender = require('@welldone-software/why-did-you-render')
   // Default to not tracking all components, add whyDidYouRender = true to component functions you want to track
   whyDidYouRender(React, {
     trackAllPureComponents: false,
