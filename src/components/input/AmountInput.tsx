c2a83537ae089ecddc88e0547f677e3a759bc1c5
Tina <59578595+tinaszheng@users.noreply.github.com>
2022-07-24 16:22:12 -0400

    fix: Manually set placeholder text color for $ input (#1080)
    use textTertiary for only $ input

--- src/components/input/AmountInput.tsx
+++ src/components/input/AmountInput.tsx
@@ -22,6 +22,7 @@ export function AmountInput({ onChangeText, value, showCurrencySign, ...rest }: 
 
   return (
     <TextInput
+      color={!value ? 'textTertiary' : 'textPrimary'}
       keyboardType="numeric"
       value={formattedValue}
       onChangeText={handleChange}
