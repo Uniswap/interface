880472e343886bd76d9f6755c03258a81f555925
Eric Zhong <eric.zhong@uniswap.org>
2022-07-24 21:24:10 -0400

    lint autofix

--- src/state/routing/utils.ts
+++ src/state/routing/utils.ts
@@ -1,7 +1,7 @@
+import { MixedRouteSDK } from '@uniswap/router-sdk'
 import { Currency, CurrencyAmount, Token, TradeType } from '@uniswap/sdk-core'
 import { Pair, Route as V2Route } from '@uniswap/v2-sdk'
 import { FeeAmount, Pool, Route as V3Route } from '@uniswap/v3-sdk'
-import { MixedRouteSDK } from '@uniswap/router-sdk'
 
 import { nativeOnChain } from '../../constants/tokens'
 import { GetQuoteResult, InterfaceTrade, V2PoolInRoute, V3PoolInRoute } from './types'
