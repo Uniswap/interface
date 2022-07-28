4779c1074d72ab9df09d5a5fd6d890613aff9ad9
Tarik Bellamine <40568900+tarikbellamine@users.noreply.github.com>
2022-07-28 17:07:28 +0200

    fix: storybook test for notifications (#1150)
    * fix storybook
    
    * notification rendering but body text is black instead of white for some reason
    
    * remove unused var

--- src/features/notifications/NotificationToast.stories.tsx
+++ src/features/notifications/NotificationToast.stories.tsx
@@ -1,39 +1,76 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { rootReducer } from 'src/app/rootReducer'
 import { Box } from 'src/components/layout'
 import { ChainId } from 'src/constants/chains'
-import { DefaultNotification, WCNotification } from 'src/features/notifications/Notifications'
-import { NotificationToast } from 'src/features/notifications/NotificationToast'
+import { NotificationToastRouter } from 'src/features/notifications/NotificationToastWrapper'
 import { AppNotificationType } from 'src/features/notifications/types'
 import { WalletConnectEvent } from 'src/features/walletConnect/saga'
+import { DynamicThemeProvider } from 'src/styles/DynamicThemeProvider'
+
+const mockAddress = '0x000000'
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: { notificationQueue: [{ address: mockAddress }] },
+  reducers: {},
+})
+
+const walletSlice = createSlice({
+  name: 'wallet',
+  initialState: {
+    activeAccountAddress: mockAddress,
+    accounts: { [mockAddress]: {} },
+  },
+  reducers: {},
+})
+
+const store = configureStore<typeof rootReducer>({
+  reducer: {
+    notifications: notificationsSlice.reducer,
+    wallet: walletSlice.reducer,
+  },
+})
 
 export default {
   title: 'WIP/Notifications/NotificationToasts',
-  component: WCNotification,
+  component: NotificationToastRouter,
   decorators: [
     (Story) => (
-      <Box bg="backgroundBackdrop" p="md" width={300}>
-        <Story />
-      </Box>
+      <Provider store={store}>
+        <DynamicThemeProvider>
+          <Box width={300}>
+            <Story />
+          </Box>
+        </DynamicThemeProvider>
+      </Provider>
     ),
   ],
-} as ComponentMeta<typeof WCNotification>
+} as ComponentMeta<typeof NotificationToastRouter>
 
-export const WallectConnectNotification: ComponentStory<typeof WCNotification> = () => {
-  return (
-    <WCNotification
-      notification={{
-        type: AppNotificationType.WalletConnect,
-        address: '0x000000',
-        event: WalletConnectEvent.Connected,
-        dappName: 'Uniswap',
-        imageUrl: 'https://app.uniswap.org/images/192x192_App_Icon.png',
-        chainId: ChainId.Mainnet,
-      }}
-    />
-  )
+const Template: ComponentStory<typeof NotificationToastRouter> = (args) => (
+  <NotificationToastRouter {...args} />
+)
+
+export const WallectConnectNotification = Template.bind({})
+WallectConnectNotification.args = {
+  notification: {
+    type: AppNotificationType.WalletConnect,
+    address: '0x000000',
+    event: WalletConnectEvent.Connected,
+    dappName: 'Uniswap',
+    imageUrl: 'https://app.uniswap.org/images/192x192_App_Icon.png',
+    chainId: ChainId.Mainnet,
+  },
 }
 
-export const BaseAppNotification: ComponentStory<typeof DefaultNotification> = () => {
-  return <NotificationToast title="This is a text notification" />
+export const DefaultNotification = Template.bind({})
+DefaultNotification.args = {
+  notification: {
+    type: AppNotificationType.Default,
+    address: '0x000000',
+    title: 'Hello world!',
+  },
 }
