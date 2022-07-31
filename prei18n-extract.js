eb90dfaee32d91eb52f2b89ab2277c230b857609
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-31 18:53:40 +0800

    Delete prei18n-extract.js

--- prei18n-extract.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const exec = require('child_process').exec
-const isWindows = process.platform === 'win32' || /^(msys|cygwin)$/.test(process.env.OSTYPE)
-
-if (isWindows) {
-  exec(`type nul > src/locales/en-US.po`)
-} else {
-  exec(`touch src/locales/en-US.po`)
-}
