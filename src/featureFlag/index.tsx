b5b9203741173c2db8a2a4cd05e7914d12e9ef15
Vignesh Mohankumar <me@vig.xyz>
2022-08-01 17:43:15 -0400

    show more logic

--- src/featureFlag/index.tsx
+++ src/featureFlag/index.tsx
@@ -2,22 +2,42 @@ import { createContext, ReactNode, useContext } from 'react'
 
 interface FeatureFlagContextType {
   isLoaded: boolean
+  flags: Record<string, string>
 }
 
-const DEFAULT_STATE = { isLoaded: false }
-const FeatureFlagContext = createContext<FeatureFlagContextType>(DEFAULT_STATE)
+const FeatureFlagContext = createContext<FeatureFlagContextType | undefined>(undefined)
+
+function useFeatureFlagContext() {
+  const context = useContext(FeatureFlagContext)
+  if (!context) throw Error('Feature flag hooks can only be used by children of FeatureFlagProvider.')
+  return context
+}
 
 export function FeatureFlagProvider({ children }: { children: ReactNode }) {
-  return <FeatureFlagContext.Provider value={{ isLoaded: false }}>{children}</FeatureFlagContext.Provider>
+  const value = {
+    isLoaded: true,
+    flags: {
+      phase0: 'control',
+    },
+  }
+  return <FeatureFlagContext.Provider value={value}>{children}</FeatureFlagContext.Provider>
 }
 
-enum Phase0Flag {
+enum Phase0Variant {
   Control = 'Control',
   Enabled = 'Enabled',
 }
 
-export function usePhase0Flag(): Phase0Flag {
-  const context = useContext(FeatureFlagContext)
-  if (!context) throw Error('Feature flag hooks can only be used by children of FeatureFlagProvider.')
-  return Phase0Flag.Control
+export function useFeatureFlagsLoaded(): boolean {
+  return useFeatureFlagContext().isLoaded
+}
+
+export function usePhase0Flag(): Phase0Variant {
+  const phase0Variant = useFeatureFlagContext().flags['phase0']
+  switch (phase0Variant) {
+    case 'enabled':
+      return Phase0Variant.Enabled
+    default:
+      return Phase0Variant.Control
+  }
 }
