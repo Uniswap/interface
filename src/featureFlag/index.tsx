cc11f71a0e211118634066ccff474a049337ae40
Vignesh Mohankumar <me@vig.xyz>
2022-08-02 11:23:51 -0400

    add control specifically

--- src/featureFlag/index.tsx
+++ src/featureFlag/index.tsx
@@ -44,6 +44,7 @@ export function usePhase0Flag(): Phase0Variant {
   switch (useFeatureFlagsContext().flags['phase0']) {
     case 'enabled':
       return Phase0Variant.Enabled
+    case 'control':
     default:
       return Phase0Variant.Control
   }
@@ -58,6 +59,7 @@ export function usePhase1Flag(): Phase1Variant {
   switch (useFeatureFlagsContext().flags['phase1']) {
     case 'enabled':
       return Phase1Variant.Enabled
+    case 'control':
     default:
       return Phase1Variant.Control
   }
