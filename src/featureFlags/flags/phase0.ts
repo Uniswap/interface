148d1c1c0df97fc9e54ba31deb1e12c1294d16c2
Vignesh Mohankumar <me@vig.xyz>
2022-08-04 14:31:54 -0400

    switch

--- src/featureFlags/flags/phase0.ts
+++ src/featureFlags/flags/phase0.ts
@@ -1,4 +1,4 @@
-import { useBaseFlag, BaseVariant } from '../index'
+import { BaseVariant, useBaseFlag } from '../index'
 
 export function usePhase0Flag(): BaseVariant {
   return useBaseFlag('phase0')
