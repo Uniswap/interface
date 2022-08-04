956fb9faeaef281ff6502379cdf5ddd100398f76
Spencer Yen <spencersyen@gmail.com>
2022-08-04 15:16:51 -0700

    fix(MOB-1595): Display cloud backups in sorted date order (#1258)

--- src/screens/Import/RestoreCloudBackupScreen.tsx
+++ src/screens/Import/RestoreCloudBackupScreen.tsx
@@ -2,6 +2,7 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Alert } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import { useAppDispatch, useAppTheme } from 'src/app/hooks'
 import { OnboardingStackParamList } from 'src/app/navigation/types'
 import { Button } from 'src/components/buttons/Button'
@@ -29,8 +30,9 @@ type Props = NativeStackScreenProps<OnboardingStackParamList, OnboardingScreens.
 export function RestoreCloudBackupScreen({ navigation, route: { params } }: Props) {
   const { t } = useTranslation()
   const theme = useAppTheme()
-  const backups = useCloudBackups()
   const dispatch = useAppDispatch()
+  const backups = useCloudBackups()
+  const sortedBackups = backups.slice().sort((a, b) => a.createdAt - b.createdAt)
 
   const onPressRestoreBackup = async (backup: ICloudMnemonicBackup) => {
     // Clear any existing pending accounts
@@ -72,44 +74,46 @@ export function RestoreCloudBackupScreen({ navigation, route: { params } }: Prop
     <OnboardingScreen
       subtitle={t("Please select which backup you'd like to recover")}
       title={t('We found multiple recovery phrase backups')}>
-      <Flex gap="xs">
-        {backups.map((backup, index) => {
-          const { mnemonicId, createdAt } = backup
-          return (
-            <Button
-              key={backup.mnemonicId}
-              backgroundColor="backgroundContainer"
-              borderColor="backgroundAction"
-              borderRadius="lg"
-              borderWidth={1}
-              p="md"
-              onPress={() => onPressRestoreBackup(backup)}>
-              <Flex row alignItems="center" justifyContent="space-between">
-                <Flex centered row gap="sm">
-                  <Unicon address={mnemonicId} size={32} />
-                  <Flex gap="none">
-                    <Text numberOfLines={1} variant="body">
-                      {t('Backup {{backupIndex}}', { backupIndex: index + 1 })}
-                    </Text>
-                    <Text color="textSecondary" variant="caption">
-                      {shortenAddress(mnemonicId)}
-                    </Text>
+      <ScrollView>
+        <Flex gap="xs">
+          {sortedBackups.map((backup, index) => {
+            const { mnemonicId, createdAt } = backup
+            return (
+              <Button
+                key={backup.mnemonicId}
+                backgroundColor="backgroundContainer"
+                borderColor="backgroundAction"
+                borderRadius="lg"
+                borderWidth={1}
+                p="md"
+                onPress={() => onPressRestoreBackup(backup)}>
+                <Flex row alignItems="center" justifyContent="space-between">
+                  <Flex centered row gap="sm">
+                    <Unicon address={mnemonicId} size={32} />
+                    <Flex gap="none">
+                      <Text numberOfLines={1} variant="body">
+                        {t('Backup {{backupIndex}}', { backupIndex: index + 1 })}
+                      </Text>
+                      <Text color="textSecondary" variant="caption">
+                        {shortenAddress(mnemonicId)}
+                      </Text>
+                    </Flex>
+                  </Flex>
+                  <Flex row gap="sm">
+                    <Flex alignItems="flex-end" gap="xxs">
+                      <Text color="textSecondary" variant="caption">
+                        {t('Backed up on:')}
+                      </Text>
+                      <Text variant="caption">{formatDate(new Date(createdAt * 1000))}</Text>
+                    </Flex>
+                    <Chevron color={theme.colors.textPrimary} direction="e" />
                   </Flex>
                 </Flex>
-                <Flex row gap="sm">
-                  <Flex alignItems="flex-end" gap="xxs">
-                    <Text color="textSecondary" variant="caption">
-                      {t('Backed up on:')}
-                    </Text>
-                    <Text variant="caption">{formatDate(new Date(createdAt * 1000))}</Text>
-                  </Flex>
-                  <Chevron color={theme.colors.textPrimary} direction="e" />
-                </Flex>
-              </Flex>
-            </Button>
-          )
-        })}
-      </Flex>
+              </Button>
+            )
+          })}
+        </Flex>
+      </ScrollView>
     </OnboardingScreen>
   )
 }
