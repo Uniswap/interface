7da9de7e81f5e1f0389970bb59bb698362d49748
Andrew <andrewtingnyc@gmail.com>
2022-08-04 16:19:41 -0400

    feat: add UI for token details stats (#1222)
    * working graphql query
    
    * wip
    
    * rm comments
    
    * working token details stats ui
    
    * add read more
    
    * regen graphql
    
    * check for unsupported chain
    
    * update query to get 52W high low
    
    * implement 52W low high
    
    * data api needs null for native tokens
    
    * wip
    
    * add name
    
    * rename helper fn

--- src/components/TokenDetails/TokenDetailsStats.tsx
+++ src/components/TokenDetails/TokenDetailsStats.tsx
@@ -1,10 +1,17 @@
 import { Currency } from '@uniswap/sdk-core'
 import { graphql } from 'babel-plugin-relay/macro'
 import React, { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useLazyLoadQuery } from 'react-relay'
+import { LinkButton } from 'src/components/buttons/LinkButton'
+import { Flex } from 'src/components/layout'
 import { Loading } from 'src/components/loading'
+import { Text } from 'src/components/Text'
+import { LongText } from 'src/components/text/LongText'
 import { TokenDetailsStatsQuery } from 'src/components/TokenDetails/__generated__/TokenDetailsStatsQuery.graphql'
 import { toGraphQLChain } from 'src/utils/chainId'
+import { formatPrice, formatUSDPrice } from 'src/utils/format'
+import { ExplorerDataType, getExplorerLink, getTwitterLink } from 'src/utils/linking'
 
 const tokenDetailsStatsQuery = graphql`
   query TokenDetailsStatsQuery($contract: ContractInput) {
@@ -49,16 +56,111 @@ const tokenDetailsStatsQuery = graphql`
   }
 `
 
+function formatTokenPrice(price: Nullable<number>) {
+  if (!price) return '-'
+  return price < 1 ? formatPrice(price) : formatUSDPrice(price)
+}
+
 function TokenDetailsStatsInner({ currency }: { currency: Currency }) {
+  const { t } = useTranslation()
+
+  const graphQLChain = toGraphQLChain(currency.chainId)
   const data = useLazyLoadQuery<TokenDetailsStatsQuery>(tokenDetailsStatsQuery, {
     contract: {
-      address: currency.wrapped.address,
-      chain: toGraphQLChain(currency.chainId) ?? 'ETHEREUM',
+      address: currency.isNative ? null : currency.wrapped.address,
+      chain: graphQLChain ?? 'ETHEREUM',
     },
   })
-  data // to prevent lint error, UI PR is coming
 
-  return null
+  if (
+    !graphQLChain ||
+    !data.tokenProjects ||
+    data.tokenProjects.length < 1 ||
+    !data.tokenProjects[0]
+  )
+    return null
+
+  const tokenProject = data.tokenProjects[0]
+  const marketData = tokenProject.markets ? tokenProject.markets[0] : null
+
+  const explorerLink = getExplorerLink(
+    currency.chainId,
+    currency.wrapped.address,
+    ExplorerDataType.ADDRESS
+  )
+
+  return (
+    <Flex gap="lg" p="md">
+      <Flex row justifyContent="space-between">
+        <Flex flex={1} gap="lg">
+          <Flex gap="xs">
+            <Text color="textSecondary" variant="subheadSmall">
+              {t('Market cap')}
+            </Text>
+            <Text variant="headlineMedium">{formatPrice(marketData?.marketCap?.value)}</Text>
+          </Flex>
+          <Flex gap="xs">
+            <Text color="textSecondary" variant="subheadSmall">
+              {t('52W low')}
+            </Text>
+            <Text variant="headlineMedium">{formatTokenPrice(marketData?.priceLow52W?.value)}</Text>
+          </Flex>
+        </Flex>
+        <Flex flex={1} gap="lg">
+          <Flex gap="xs">
+            <Text color="textSecondary" variant="subheadSmall">
+              {t('24h volume')}
+            </Text>
+            <Text variant="headlineMedium">{formatPrice(marketData?.volume24h?.value)}</Text>
+          </Flex>
+          <Flex gap="xs">
+            <Text color="textSecondary" variant="subheadSmall">
+              {t('52W high')}
+            </Text>
+            <Text variant="headlineMedium">
+              {formatTokenPrice(marketData?.priceHigh52W?.value)}
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex gap="md">
+        <Text color="textSecondary" variant="headlineSmall">
+          {t('About {{ token }}', { token: tokenProject.name ?? currency.name })}
+        </Text>
+        {tokenProject.description && (
+          <LongText
+            initialDisplayedLines={5}
+            text={tokenProject.description.trim()}
+            variant="bodySmall"
+          />
+        )}
+        <Flex row>
+          {tokenProject.homepageUrl && (
+            <LinkButton
+              color="accentAction"
+              label={t('Website')}
+              textVariant="caption"
+              url={tokenProject.homepageUrl}
+            />
+          )}
+          {tokenProject.twitterName && (
+            <LinkButton
+              color="accentAction"
+              label={t('Twitter')}
+              textVariant="caption"
+              url={getTwitterLink(tokenProject.twitterName)}
+            />
+          )}
+          <LinkButton
+            color="accentAction"
+            label={t('Etherscan')}
+            textVariant="caption"
+            url={explorerLink}
+          />
+        </Flex>
+      </Flex>
+    </Flex>
+  )
 }
 
 export function TokenDetailsStats({ currency }: { currency: Currency }) {
