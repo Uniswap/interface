99bc734353655cbc4a1e4b177952d5e4557b003f
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2022-08-04 09:39:51 -0400

    chore: Revert "chore: add craco and vanilla extract libraries (#4100)" (#4269)
    Revert "chore: add craco and vanilla extract libraries (#4100)"
    
    This reverts commit fa284d85f1c3cc9f9d276d2e0207ebc1fc5de656.

--- src/css/cssStringFromTheme.ts
+++
@@ -1,8 +0,0 @@
-import { cssObjectFromTheme } from './cssObjectFromTheme'
-import { Theme } from './sprinkles.css'
-
-export function cssStringFromTheme(theme: Theme | (() => Theme), options: { extends?: Theme | (() => Theme) } = {}) {
-  return Object.entries(cssObjectFromTheme(theme, options))
-    .map(([key, value]) => `${key}:${value};`)
-    .join('')
-}
