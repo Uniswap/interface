46f362e4879a13904405fb5f7e271ca41e8b258e
Charles Bachmeier <charlie@genie.xyz>
2022-08-05 10:57:48 -0700

    remove outdated comment

--- src/nft/css/cssObjectFromTheme.ts
+++ src/nft/css/cssObjectFromTheme.ts
@@ -9,10 +9,6 @@ export function cssObjectFromTheme(
   { extends: baseTheme }: { extends?: Theme | (() => Theme) } = {}
 ) {
   const resolvedThemeVars = {
-    // We use an object spread here to ensure it's a plain object since vanilla-extract's
-    // var objects have a custom 'toString' method that returns a CSS string, but we don't
-    // want to leak this to our consumers since they're unaware we're using vanilla-extract.
-    // Instead, we want them to handle this explicitly via our 'cssStringFromTheme' function.
     ...assignInlineVars(themeVars, resolveTheme(theme)),
   }
 
