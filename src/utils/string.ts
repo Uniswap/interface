d3f70337baa8595c69276203c2aa7e43a82e5943
nguyenhuudungz <34548156+nguyenhuudungz@users.noreply.github.com>
2022-08-08 16:10:20 +0700

    fix: slug function (#1205)

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -9,8 +9,9 @@ import { NETWORKS_INFO } from 'constants/networks'
  */
 export function convertToSlug(text: string) {
   return text
+    .trim()
     .toLowerCase()
-    .replace(/ /g, '-')
+    .replace(/ +/g, '-')
     .replace(/[^\w-.]+/g, '')
 }
 
