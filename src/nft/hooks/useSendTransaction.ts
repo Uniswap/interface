bf000bd714d1ba5ab4a36b82f459e77abf5a328d
Charles Bachmeier <charlie@genie.xyz>
2022-08-08 09:08:13 -0700

    remove getEtherscan helper fn

--- src/nft/hooks/useSendTransaction.ts
+++ src/nft/hooks/useSendTransaction.ts
@@ -12,10 +12,6 @@ import CryptoPunksMarket from '../abis/CryptoPunksMarket.json'
 import { GenieAsset, RouteResponse, RoutingItem, TxResponse, TxStateType, UpdatedGenieAsset } from '../types'
 import { combineBuyItemsWithTxRoute } from '../utils/txRoute/combineItemsWithTxRoute'
 
-export const getEtherscanLink = (txHash: string) => {
-  return `https://etherscan.io/tx/${txHash}`
-}
-
 // Shortens a given txHash. With standard charsToShorten var of 4, a hash will become 0x1234...1234
 export const shortenTxHash = (txHash: string, charsToShorten = 4, addCharsToBack = 0): string => {
   return `${txHash.substring(0, charsToShorten + 2)}...${txHash.substring(
