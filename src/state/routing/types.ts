1d21eebcd3fc47b4ec9787d612bd16a12ec520ca
lynn <41491154+lynnshaoyu@users.noreply.github.com>
2022-08-09 10:57:35 -0400

    feat: Web 714 additional swap submitted event properties (block number, route properties) (#4305)
    * init commit
    
    * remove absolute value in date calc
    
    * all the events are now logged properly plus changed native token address to NATIVE
    
    * add documentation line
    
    * remove unnecessary prop
    
    * init
    
    * add approve token event
    
    * fix build
    
    * add route event properties
    
    * fix build
    
    * respond to vm comments
    
    * respond to vm comments
    
    * temp
    
    * init
    
    * add blockNumber
    
    * remove console logs

--- src/state/routing/types.ts
+++ src/state/routing/types.ts
@@ -74,12 +74,15 @@ export class InterfaceTrade<
   TTradeType extends TradeType
 > extends Trade<TInput, TOutput, TTradeType> {
   gasUseEstimateUSD: CurrencyAmount<Token> | null | undefined
+  blockNumber: string | null | undefined
 
   constructor({
     gasUseEstimateUSD,
+    blockNumber,
     ...routes
   }: {
     gasUseEstimateUSD?: CurrencyAmount<Token> | undefined | null
+    blockNumber?: string | null | undefined
     v2Routes: {
       routev2: V2Route<TInput, TOutput>
       inputAmount: CurrencyAmount<TInput>
@@ -93,6 +96,7 @@ export class InterfaceTrade<
     tradeType: TTradeType
   }) {
     super(routes)
+    this.blockNumber = blockNumber
     this.gasUseEstimateUSD = gasUseEstimateUSD
   }
 }
