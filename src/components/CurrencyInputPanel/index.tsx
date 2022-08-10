1246831b4e8f844f3b85faa3fdf18ef628b40400
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2022-08-10 14:27:53 -0400

    Update index.tsx

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -186,7 +186,7 @@ const StyledDropDown = styled(DropDown)<{ selected: boolean; phase0Flag: boolean
 const StyledTokenName = styled.span<{ active?: boolean; phase0Flag: boolean }>`
   ${({ active }) => (active ? '  margin: 0 0.25rem 0 0.25rem;' : '  margin: 0 0.25rem 0 0.25rem;')}
   font-size:  ${({ active }) => (active ? '18px' : '18px')};
-  font-weight: ${({ phase0Flag }) => phase0Flag && '600'};
+  font-weight: ${({ phase0Flag }) => (phase0Flag ? '600' : '500')};
 `
 
 const StyledBalanceMax = styled.button<{ disabled?: boolean; phase0Flag: boolean }>`
