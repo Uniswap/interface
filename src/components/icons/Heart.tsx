a26c8039d9bd4f47c6bf2417d0648df5c4a69250
chelsy <101524262+chelsywu@users.noreply.github.com>
2022-08-12 16:10:52 -0400

    fix: Changed active heart icon fill to pink with no stroke (#1351)
    Changed active heart icon fill to pink with no stroke

--- src/components/icons/Heart.tsx
+++ src/components/icons/Heart.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 import { useAppTheme } from 'src/app/hooks'
 import HeartIcon from 'src/assets/icons/heart.svg'
+import { colors } from 'src/styles/color'
 
 type Props = {
   active: boolean
@@ -13,8 +14,10 @@ function _Heart({ active, size }: Props) {
   return (
     <HeartIcon
       color={theme.colors.textPrimary}
-      fill={active ? theme.colors.accentAction : theme.colors.backgroundSurface}
+      fill={active ? colors.pink400 : theme.colors.none}
       height={size}
+      stroke={active ? colors.pink400 : theme.colors.textPrimary}
+      strokeWidth={2}
       width={size}
     />
   )
