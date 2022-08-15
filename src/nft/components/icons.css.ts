30b4d4d2e27f815f046c37a46c0072b0d909a200
Charles Bachmeier <charlie@genie.xyz>
2022-08-15 15:33:10 -0700

    rotate 360

--- src/nft/components/icons.css.ts
+++ src/nft/components/icons.css.ts
@@ -5,7 +5,7 @@ const rotate = keyframes({
     transform: 'rotate(0deg)',
   },
   to: {
-    transform: 'rotate(359deg)',
+    transform: 'rotate(360deg)',
   },
 })
 export const clockSpinArrows = style({
