8b9017f8ed206d369db41a1d6d6830b60aaacabc
Vignesh Mohankumar <me@vig.xyz>
2022-08-15 16:10:53 -0400

    use hooks for chainId

--- src/components/NavBar/ChainSwitcher.tsx
+++ src/components/NavBar/ChainSwitcher.tsx
@@ -1,24 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useWeb3React } from '@web3-react/core'
-import { getConnection } from 'connection/utils'
 import { getChainInfo } from 'constants/chainInfo'
-import { CHAIN_IDS_TO_NAMES, SupportedChainId } from 'constants/chains'
+import { SupportedChainId } from 'constants/chains'
 import { useOnClickOutside } from 'hooks/useOnClickOutside'
-import useParsedQueryString from 'hooks/useParsedQueryString'
-import usePrevious from 'hooks/usePrevious'
+import useSelectChain from 'hooks/useSelectChain'
+import useSyncChainQuery from 'hooks/useSyncChainQuery'
 import { Box } from 'nft/components/Box'
 import { Column, Row } from 'nft/components/Flex'
 import { NewChevronDownIcon, NewChevronUpIcon } from 'nft/components/icons'
 import { CheckMarkIcon } from 'nft/components/icons'
 import { subhead } from 'nft/css/common.css'
-import { ParsedQs } from 'qs'
-import { ReactNode, useCallback, useEffect, useReducer, useRef } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { addPopup } from 'state/application/reducer'
-import { updateConnectionError } from 'state/connection/reducer'
-import { useAppDispatch } from 'state/hooks'
-import { replaceURLParam } from 'utils/routes'
-import { isChainAllowed, switchChain } from 'utils/switchChain'
+import { ReactNode, useReducer, useRef } from 'react'
+import { isChainAllowed } from 'utils/switchChain'
 
 import * as styles from './ChainSwitcher.css'
 import { NavDropdown } from './NavDropdown'
@@ -55,24 +48,6 @@ const ChainRow = ({
 
 const ChainDetails = ({ children }: { children: ReactNode }) => <Row>{children}</Row>
 
-const getParsedChainId = (parsedQs?: ParsedQs) => {
-  const chain = parsedQs?.chain
-  if (!chain || typeof chain !== 'string') return { urlChain: undefined, urlChainId: undefined }
-
-  return { urlChain: chain.toLowerCase(), urlChainId: getChainIdFromName(chain) }
-}
-
-const getChainIdFromName = (name: string) => {
-  const entry = Object.entries(CHAIN_IDS_TO_NAMES).find(([_, n]) => n === name)
-  const chainId = entry?.[0]
-  return chainId ? parseInt(chainId) : undefined
-}
-
-const getChainNameFromId = (id: string | number) => {
-  // casting here may not be right but fine to return undefined if it's not a supported chain ID
-  return CHAIN_IDS_TO_NAMES[id as SupportedChainId] || ''
-}
-
 const NETWORK_SELECTOR_CHAINS = [
   SupportedChainId.MAINNET,
   SupportedChainId.POLYGON,
@@ -85,67 +60,19 @@ interface ChainSwitcherProps {
 }
 
 export const ChainSwitcher = ({ isMobile }: ChainSwitcherProps) => {
-  const dispatch = useAppDispatch()
-  const { chainId, provider, connector } = useWeb3React()
-  const previousChainId = usePrevious(chainId)
+  const { chainId, connector } = useWeb3React()
   const [isOpen, toggleOpen] = useReducer((s) => !s, false)
-  const parsedQs = useParsedQueryString()
-  const { urlChain, urlChainId } = getParsedChainId(parsedQs)
-  const previousUrlChainId = usePrevious(urlChainId)
 
   const ref = useRef<HTMLDivElement>(null)
   useOnClickOutside(ref, isOpen ? toggleOpen : undefined)
 
-  const navigate = useNavigate()
-  const location = useLocation()
   const info = chainId ? getChainInfo(chainId) : undefined
 
-  const onSelectChain = useCallback(
-    async (targetChain: number, skipToggle?: boolean) => {
-      if (!connector) return
+  const selectChain = useSelectChain()
+  useSyncChainQuery()
 
-      const connectionType = getConnection(connector).type
-
-      try {
-        dispatch(updateConnectionError({ connectionType, error: undefined }))
-        await switchChain(connector, targetChain)
-      } catch (error) {
-        console.error('Failed to switch networks', error)
-        dispatch(updateConnectionError({ connectionType, error: error.message }))
-        dispatch(addPopup({ content: { failedSwitchNetwork: targetChain }, key: `failed-network-switch` }))
-      }
-
-      if (!skipToggle) {
-        toggleOpen()
-      }
-    },
-    [connector, toggleOpen, dispatch]
-  )
-
-  useEffect(() => {
-    if (!chainId || !previousChainId) return
-
-    // when network change originates from wallet or dropdown selector, just update URL
-    if (chainId !== previousChainId && chainId !== urlChainId) {
-      navigate({ search: replaceURLParam(location.search, 'chain', getChainNameFromId(chainId)) }, { replace: true })
-      // otherwise assume network change originates from URL
-    } else if (urlChainId && urlChainId !== previousUrlChainId && urlChainId !== chainId) {
-      onSelectChain(urlChainId, true).catch(() => {
-        // we want app network <-> chainId param to be in sync, so if user changes the network by changing the URL
-        // but the request fails, revert the URL back to current chainId
-        navigate({ search: replaceURLParam(location.search, 'chain', getChainNameFromId(chainId)) }, { replace: true })
-      })
-    }
-  }, [chainId, urlChainId, previousChainId, previousUrlChainId, onSelectChain, location])
-
-  // set chain parameter on initial load if not there
-  useEffect(() => {
-    if (chainId && !urlChainId) {
-      navigate({ search: replaceURLParam(location.search, 'chain', getChainNameFromId(chainId)) }, { replace: true })
-    }
-  }, [chainId, location, urlChainId, urlChain])
-
-  if (!chainId || !info || !provider) {
+  // TODO: need to handle unsupported chain
+  if (!chainId || !info) {
     return null
   }
 
@@ -167,7 +94,14 @@ export const ChainSwitcher = ({ isMobile }: ChainSwitcherProps) => {
           <Column gap="4">
             {NETWORK_SELECTOR_CHAINS.map((chainId: SupportedChainId) =>
               isChainAllowed(connector, chainId) ? (
-                <ChainRow onSelectChain={onSelectChain} targetChain={chainId} key={chainId} />
+                <ChainRow
+                  onSelectChain={async (targetChainId: SupportedChainId) => {
+                    await selectChain(targetChainId)
+                    toggleOpen()
+                  }}
+                  targetChain={chainId}
+                  key={chainId}
+                />
               ) : null
             )}
           </Column>
