f349ecdd3cd899f841849a161a4023ef4abb9951
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2022-08-15 18:39:16 -0400

    chore: remove selectedWalletBackfilled (#4283)
    * remove selectedWalletBackfilled
    
    * FIX

--- src/state/user/reducer.ts
+++ src/state/user/reducer.ts
@@ -9,11 +9,6 @@ import { SerializedPair, SerializedToken } from './types'
 const currentTimestamp = () => new Date().getTime()
 
 export interface UserState {
-  // We want the user to be able to define which wallet they want to use, even if there are multiple connected wallets via web3-react.
-  // If a user had previously connected a wallet but didn't have a wallet override set (because they connected prior to this field being added),
-  // we want to handle that case by backfilling them manually. Once we backfill, we set the backfilled field to `true`.
-  // After some period of time, our active users will have this property set so we can likely remove the backfilling logic.
-  selectedWalletBackfilled: boolean
   selectedWallet?: ConnectionType
 
   // the timestamp of the last updateVersion action
@@ -66,7 +61,6 @@ function pairKey(token0Address: string, token1Address: string) {
 
 export const initialState: UserState = {
   selectedWallet: undefined,
-  selectedWalletBackfilled: false,
   matchesDarkMode: false,
   userDarkMode: null,
   userExpertMode: false,
@@ -90,7 +84,6 @@ const userSlice = createSlice({
   reducers: {
     updateSelectedWallet(state, { payload: { wallet } }) {
       state.selectedWallet = wallet
-      state.selectedWalletBackfilled = true
     },
     updateUserDarkMode(state, action) {
       state.userDarkMode = action.payload.userDarkMode
