9ca17a51c9b6024856ca6d4d3b4a25d24a53b60a
Alex Ball <alexball@UNISWAP-MAC-038.fios-router.home>
2022-08-18 14:13:05 -0400

    header fix

--- src/components/WalletDropdown/SlideOutMenu.tsx
+++ src/components/WalletDropdown/SlideOutMenu.tsx
@@ -19,12 +19,11 @@ const Menu = styled.div`
   overflow-y: scroll;
 `
 
-const Header = styled.span`
+const Header = styled.div`
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-  height: 200px;
 `
 
 const ClearAll = styled.div`
