a1ced5803ead81d839bde30fc6d4fe627cc907d6
Lynn Yu <lynn.yu@uniswap.org>
2022-08-18 18:07:43 -0400

    fixes for kaylee comments

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -83,11 +83,7 @@ const HeaderRow = styled.div<{ redesignFlag?: boolean }>`
 
 const ContentWrapper = styled.div<{ redesignFlag?: boolean }>`
   background-color: ${({ theme, redesignFlag }) => (redesignFlag ? theme.backgroundSurface : theme.deprecated_bg0)};
-<<<<<<< HEAD
-  border: ${({ theme, redesignFlag }) => (redesignFlag ? `1px solid ${theme.backgroundOutline}` : '')};
-=======
   border: ${({ theme, redesignFlag }) => redesignFlag && `1px solid ${theme.backgroundOutline}`};
->>>>>>> main
   padding: 0 1rem 1rem 1rem;
   border-bottom-left-radius: 20px;
   border-bottom-right-radius: 20px;
