5768996e2cad25fe0019143c55174f124597f0e8
Justin Domingue <judo@uniswap.org>
2022-08-22 13:58:26 -0400

    feat: vertical cursor line (#1435)

--- src/components/PriceChart/Cursor.tsx
+++ src/components/PriceChart/Cursor.tsx
@@ -6,6 +6,7 @@ import {
   useAnimatedGestureHandler,
   useAnimatedStyle,
   withSpring,
+  withTiming,
 } from 'react-native-reanimated'
 import { getYForX } from 'react-native-redash'
 import { AnimatedBox, Box } from 'src/components/layout/Box'
@@ -14,8 +15,11 @@ import {
   AnimatedTranslation,
   GraphMetadatas,
 } from 'src/components/PriceChart/types'
+import { HEIGHT } from 'src/components/PriceChart/utils'
 
-const CURSOR_SIZE = 50
+const CURSOR_INNER_SIZE = 12
+const CURSOR_SIZE = CURSOR_INNER_SIZE + 6
+const LINE_WIDTH = 1
 
 interface CursorProps {
   graphs: GraphMetadatas
@@ -38,6 +42,10 @@ export const Cursor = ({ graphs, index, isActive, translation }: CursorProps) =>
     },
   })
 
+  const containerStyles = useAnimatedStyle(() => ({
+    opacity: withTiming(isActive.value ? 1 : 0),
+  }))
+
   // cursor position and scale animation
   const cursorAnimatedStyles = useAnimatedStyle(() => {
     const translateX = translation.x.value - CURSOR_SIZE / 2
@@ -47,27 +55,53 @@ export const Cursor = ({ graphs, index, isActive, translation }: CursorProps) =>
     }
   })
 
+  const verticalLineAnimatedStyles = useAnimatedStyle(() => {
+    const translateX = translation.x.value - LINE_WIDTH
+    return {
+      transform: [{ translateX }, { scale: withTiming(isActive.value ? 1 : 0) }],
+    }
+  })
+
   return (
     <Box flex={1} style={StyleSheet.absoluteFill}>
       <PanGestureHandler onGestureEvent={onGestureEvent}>
-        <AnimatedBox style={StyleSheet.absoluteFill}>
+        <AnimatedBox style={[StyleSheet.absoluteFill, containerStyles]}>
+          {/* Vertical line */}
+          <AnimatedBox
+            borderColor="backgroundOutline"
+            borderRadius="xs"
+            borderStyle="dashed"
+            borderWidth={1}
+            height={HEIGHT}
+            style={[verticalLineAnimatedStyles, StyleSheet.absoluteFill]}
+            width={LINE_WIDTH}
+          />
+
+          {/* Circular cursor */}
           <AnimatedBox
             alignItems="center"
-            borderRadius="full"
             height={CURSOR_SIZE}
             justifyContent="center"
-            style={[styles.cursor, cursorAnimatedStyles]}
+            position="relative"
+            style={cursorAnimatedStyles}
             width={CURSOR_SIZE}>
-            <Box bg="accentAction" borderRadius="full" height={15} width={15} />
+            {/* Translucent cursor background */}
+            <Box
+              bg="backgroundOutline"
+              borderRadius="full"
+              height={CURSOR_SIZE}
+              style={StyleSheet.absoluteFillObject}
+              width={CURSOR_SIZE}
+            />
+            <Box
+              bg="textSecondary"
+              borderRadius="full"
+              height={CURSOR_INNER_SIZE}
+              width={CURSOR_INNER_SIZE}
+            />
           </AnimatedBox>
         </AnimatedBox>
       </PanGestureHandler>
     </Box>
   )
 }
-
-const styles = StyleSheet.create({
-  cursor: {
-    backgroundColor: 'rgba(243, 71, 191, 0.171)',
-  },
-})
