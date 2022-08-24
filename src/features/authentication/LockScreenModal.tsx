030a09465894ab4e8c27e7f0670d0ae7286d78c3
Thomas Thachil <thomasthachil@users.noreply.github.com>
2022-08-24 11:53:26 -0400

    fix: Adding changes for lock screen fallback (#1481)
    Adding fallback for lock screen to retrigger FaceID

--- src/features/authentication/LockScreenModal.tsx
+++ src/features/authentication/LockScreenModal.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Image } from 'react-native'
 import { UNISWAP_SPLASH_LOGO } from 'src/assets'
+import { Button } from 'src/components/buttons/Button'
 import { Box } from 'src/components/layout/Box'
 import { Modal } from 'src/components/modals/Modal'
 import { useLockScreenContext } from 'src/features/authentication/lockScreenContext'
+import { useBiometricPrompt } from 'src/features/biometrics/hooks'
 import { dimensions } from 'src/styles/sizing'
 
 export function LockScreenModal() {
-  const { isLockScreenVisible, animationType } = useLockScreenContext()
+  const { isLockScreenVisible, animationType, setIsLockScreenVisible } = useLockScreenContext()
+  const { trigger } = useBiometricPrompt(() => setIsLockScreenVisible(false))
+
   return (
     <Modal
       animationType={animationType}
@@ -19,17 +23,19 @@ export function LockScreenModal() {
       showCloseButton={false}
       transparent={false}
       visible={isLockScreenVisible}>
-      <Box
-        alignItems="center"
-        backgroundColor="backgroundBackdrop"
-        pointerEvents="none"
-        style={{
-          width: dimensions.fullWidth,
-          height: dimensions.fullHeight,
-          paddingTop: dimensions.fullHeight / 7, // just a placeholder, the faceid thing blocks the logo in the middle
-        }}>
-        <Image source={UNISWAP_SPLASH_LOGO} />
-      </Box>
+      <Button onPress={trigger}>
+        <Box
+          alignItems="center"
+          backgroundColor="backgroundBackdrop"
+          pointerEvents="none"
+          style={{
+            width: dimensions.fullWidth,
+            height: dimensions.fullHeight,
+            paddingTop: dimensions.fullHeight / 7, // just a placeholder, the faceid thing blocks the logo in the middle
+          }}>
+          <Image source={UNISWAP_SPLASH_LOGO} />
+        </Box>
+      </Button>
     </Modal>
   )
 }
