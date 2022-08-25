64bc9b69c2e3bdfab6da291dd9d46323441f2328
Charlie <charlie@uniswap.org>
2022-08-25 16:10:13 -0700

    delete old comments

--- src/components/NavBar/NavDropdown.tsx
+++ src/components/NavBar/NavDropdown.tsx
@@ -1,37 +1,8 @@
 import { Box, BoxProps } from 'nft/components/Box'
 import { useIsMobile } from 'nft/hooks'
-// import { ReactNode } from 'react'
 import { ForwardedRef, forwardRef } from 'react'
 
 import * as styles from './NavDropdown.css'
-
-// interface NavDropdownProps {
-//   top?: number
-//   right?: number
-//   leftAligned?: boolean
-//   paddingBottom?: number
-//   paddingTop?: number
-//   children: ReactNode
-// }
-
-// export const NavDropdown = ({ top, leftAligned, paddingBottom, paddingTop, children }: NavDropdownProps) => {
-//   const isMobile = useIsMobile()
-//   return (
-//     <Box
-//       style={{
-//         top: isMobile ? 'unset' : `${top}px`,
-//         left: isMobile ? 0 : leftAligned ? '0px' : 'auto',
-//         right: isMobile ? 0 : leftAligned ? 'auto' : '0px',
-//         paddingBottom: paddingBottom ?? '24px',
-//         paddingTop: paddingTop ?? '24px',
-
-//       }}
-//       className={styles.NavDropdown}
-//     >
-//       {children}
-//     </Box>
-//   )
-// }
 
 export const NavDropdown = forwardRef((props: BoxProps, ref: ForwardedRef<HTMLElement>) => {
   const isMobile = useIsMobile()
