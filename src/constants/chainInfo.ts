007fcfba5a4567c65c023c2d36502e3f7050baa4
hyemmie <lhm0710@naver.com>
2022-08-30 12:51:33 +0900

    add private network info

--- src/constants/chainInfo.ts
+++ src/constants/chainInfo.ts
@@ -1,4 +1,5 @@
 import ethereumLogoUrl from 'assets/images/ethereum-logo.png'
+import radiusLogo from 'assets/images/radius_sun.png'
 import arbitrumLogoUrl from 'assets/svg/arbitrum_logo.svg'
 import optimismLogoUrl from 'assets/svg/optimistic_ethereum.svg'
 import polygonMaticLogo from 'assets/svg/polygon-matic-logo.svg'
@@ -166,5 +167,16 @@ export const CHAIN_INFO: ChainInfoMap = {
     label: 'Polygon Mumbai',
     logoUrl: polygonMaticLogo,
     nativeCurrency: { name: 'Polygon Mumbai Matic', symbol: 'mMATIC', decimals: 18 },
+  },
+  [SupportedChainId.TEX_PRIVATE]: {
+    networkType: NetworkType.L1,
+    blockWaitMsBeforeWarning: ms`10m`,
+    bridge: 'https://wallet.polygon.technology/bridge',
+    docs: 'https://polygon.io/',
+    explorer: 'https://mumbai.polygonscan.com/',
+    infoLink: 'https://info.uniswap.org/#/polygon/',
+    label: 'TEX Private',
+    logoUrl: radiusLogo,
+    nativeCurrency: { name: 'TEX Private Ether', symbol: 'TEX', decimals: 18 },
   },
 }
