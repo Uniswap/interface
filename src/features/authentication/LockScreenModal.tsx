f28bf0f2187c25eab056068338e3669c3f87e72e
Jorge <dalmendray@gmail.com>
2022-08-30 19:35:18 +0200

    fix: putting change back after mistakenly merging to main: feat: use uniswap logo in lock screen (#1547)
    fix change
    
    Co-authored-by: Jorge Dalmendray <dalmendray@jorgeuni.local>

--- src/features/authentication/LockScreenModal.tsx
+++ src/features/authentication/LockScreenModal.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Image } from 'react-native'
-import { UNISWAP_SPLASH_LOGO } from 'src/assets'
+import { UNISWAP_LOGO } from 'src/assets'
 import { Button } from 'src/components/buttons/Button'
-import { Box } from 'src/components/layout/Box'
+import { Flex } from 'src/components/layout'
 import { Modal } from 'src/components/modals/Modal'
 import { useLockScreenContext } from 'src/features/authentication/lockScreenContext'
 import { useBiometricPrompt } from 'src/features/biometrics/hooks'
@@ -11,7 +11,6 @@ import { dimensions } from 'src/styles/sizing'
 export function LockScreenModal() {
   const { isLockScreenVisible, animationType, setIsLockScreenVisible } = useLockScreenContext()
   const { trigger } = useBiometricPrompt(() => setIsLockScreenVisible(false))
-
   return (
     <Modal
       animationType={animationType}
@@ -24,17 +23,17 @@ export function LockScreenModal() {
       transparent={false}
       visible={isLockScreenVisible}>
       <Button onPress={trigger}>
-        <Box
+        <Flex
+          centered
           alignItems="center"
           backgroundColor="backgroundBackdrop"
           pointerEvents="none"
           style={{
             width: dimensions.fullWidth,
             height: dimensions.fullHeight,
-            paddingTop: dimensions.fullHeight / 7, // just a placeholder, the faceid thing blocks the logo in the middle
           }}>
-          <Image source={UNISWAP_SPLASH_LOGO} />
-        </Box>
+          <Image source={UNISWAP_LOGO} />
+        </Flex>
       </Button>
     </Modal>
   )
