421d78e2aee3b630fe64de5e23e90b560a41f950
Frank Wei <frank.wei@davionlabs.com>
2022-08-31 14:56:17 +0800

    style: redesign farming card

--- packages/ui/src/pages/Farm/PoolCard.tsx
+++ packages/ui/src/pages/Farm/PoolCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import styled from 'styled-components'
 import { TYPE, StyledInternalLink } from '../../theme'
 // import { ETHER, JSBI, TokenAmount } from '@teleswap/sdk'
@@ -13,22 +13,27 @@ import { useColor } from '../../hooks/useColor'
 import { AutoColumn } from 'components/Column'
 import { ButtonPrimary } from 'components/Button'
 // import DoubleCurrencyLogo from 'components/DoubleLogo'
-import { CardBGImage, CardNoise, Break } from 'components/earn/styled'
-import { RowBetween } from 'components/Row'
+// import { Break } from 'components/earn/styled'
+// import { RowBetween } from 'components/Row'
+import StakingModal from 'components/masterchef/StakingModal'
+import UnstakingModal from 'components/masterchef/UnstakingModal'
 // import { Chef } from 'constants/farm/chef.enum'
 import { CHAINID_TO_FARMING_CONFIG } from 'constants/farming.config'
 import { useActiveWeb3React } from 'hooks'
+import { useChefPositions } from 'hooks/farm/useChefPositions'
+import { useChefContract } from 'hooks/farm/useChefContract'
+import { CurrencyAmount, Token } from '@teleswap/sdk'
+import { BigNumber } from 'ethers'
+import { UNI, ZERO_ADDRESS } from 'constants/index'
+import { Chef } from 'constants/farm/chef.enum'
+import useMasterChef from 'hooks/farm/useMasterChef'
 // import { Token } from '@teleswap/sdk'
 // import { useMasterChefPoolInfo } from 'hooks/farm/useMasterChefPoolInfo'
 
 const StatContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  flex-direction: column;
   gap: 12px;
-  margin-bottom: 1rem;
-  margin-right: 1rem;
-  margin-left: 1rem;
   ${({ theme }) => theme.mediaWidth.upToSmall`
   display: none;
 `};
@@ -40,20 +45,11 @@ const Wrapper = styled(AutoColumn)<{ showBackground: boolean; bgColor: any }>`
   overflow: hidden;
   position: relative;
   opacity: ${({ showBackground }) => (showBackground ? '1' : '1')};
-  background: ${({ theme, bgColor, showBackground }) =>
-    `radial-gradient(91.85% 100% at 1.84% 0%, ${bgColor} 0%, ${showBackground ? theme.black : theme.bg5} 100%) `};
   color: ${({ theme, showBackground }) => (showBackground ? theme.white : theme.text1)} !important;
-
-  ${({ showBackground }) =>
-    showBackground &&
-    `  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
-    0px 24px 32px rgba(0, 0, 0, 0.01);`}
 `
 
 const TopSection = styled.div`
-  display: grid;
-  grid-template-columns: 48px 1fr 120px;
-  grid-gap: 0px;
+  display: flex;
   align-items: center;
   padding: 1rem;
   z-index: 1;
@@ -62,22 +58,28 @@ const TopSection = styled.div`
   `};
 `
 
-const BottomSection = styled.div<{ showBackground: boolean }>`
-  padding: 12px 16px;
-  opacity: ${({ showBackground }) => (showBackground ? '1' : '0.4')};
-  border-radius: 0 0 12px 12px;
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  justify-content: space-between;
-  z-index: 1;
+// const BottomSection = styled.div<{ showBackground: boolean }>`
+//   padding: 12px 16px;
+//   opacity: ${({ showBackground }) => (showBackground ? '1' : '0.4')};
+//   border-radius: 0 0 12px 12px;
+//   display: flex;
+//   flex-direction: row;
+//   align-items: baseline;
+//   justify-content: space-between;
+//   z-index: 1;
+// `
+
+const StakingColumn = styled.div`
+  max-width: 288px;
+  width: 100%;
 `
 
 export default function PoolCard({ pid }: { pid: number }) {
   const { chainId } = useActiveWeb3React()
   const farmingConfig = CHAINID_TO_FARMING_CONFIG[chainId || 420]
-  // const mchefContract = useChefContract(farmingConfig?.chefType || Chef.MINICHEF)
-  // const positions = useChefPositions(mchefContract, undefined, chainId)
+  const mchefContract = useChefContract(farmingConfig?.chefType || Chef.MINICHEF)
+  const masterChef = useMasterChef(Chef.MINICHEF)
+  const positions = useChefPositions(mchefContract, undefined, chainId)
   // const poolInfos = useMasterChefPoolInfo(farmingConfig?.chefType || Chef.MINICHEF)
   // const token0 = stakingInfo.tokens[0]
   // const token1 = stakingInfo.tokens[1]
@@ -90,6 +92,8 @@ export default function PoolCard({ pid }: { pid: number }) {
   // // get the color of the token
   // const token = currency0 === ETHER ? token1 : token0
   // const WETH = currency0 === ETHER ? token0 : token1
+  const [showStakingModal, setShowStakingModal] = useState(false)
+  const [showUnstakingModal, setShowUnstakingModal] = useState(false)
   const backgroundColor = useColor()
 
   // const totalSupplyOfStakingToken = useTotalSupply(stakingInfo.stakedAmount.token)
@@ -117,71 +121,125 @@ export default function PoolCard({ pid }: { pid: number }) {
   //   valueOfTotalStakedAmountInWETH && USDPrice?.quote(valueOfTotalStakedAmountInWETH)
 
   const isStaking = true
+  const rewardToken = UNI[chainId || 420]
+
+  const parsedStakedAmount = useMemo(() => {
+    try {
+      if (positions && positions[pid] && positions[pid].amount) {
+        const bi = (positions[pid].amount as BigNumber).toBigInt()
+        return CurrencyAmount.fromRawAmount(new Token(chainId || 420, ZERO_ADDRESS, 18), bi)?.toSignificant(6)
+      }
+    } catch (error) {
+      console.error('parsedStakedAmount::error', error)
+    }
+    return '--.--'
+  }, [chainId, positions, pid])
+
+  const parsedPendingSushiAmount = useMemo(() => {
+    try {
+      if (positions && positions[pid] && positions[pid].pendingSushi) {
+        const bi = (positions[pid].pendingSushi as BigNumber).toBigInt()
+        console.debug('parsedPendingSushiAmount::bi', bi)
+        return CurrencyAmount.fromRawAmount(rewardToken, bi).toFixed(6)
+      }
+    } catch (error) {
+      console.error('parsedPendingSushiAmount::error', error)
+    }
+    return '--.--'
+  }, [rewardToken, positions, pid])
 
   return (
     <Wrapper showBackground={isStaking} bgColor={backgroundColor}>
-      <CardBGImage desaturate />
-      <CardNoise />
-
       <TopSection>
         {/* <DoubleCurrencyLogo currency0={currency0} currency1={currency1} size={24} /> */}
-        <TYPE.white fontWeight={600} fontSize={24} style={{ marginLeft: '8px' }}>
+        <TYPE.white fontWeight={600} fontSize={18} style={{ marginLeft: '8px' }}>
           {farmingConfig?.pools[pid].stakingAsset.name}
         </TYPE.white>
 
-        <StyledInternalLink to={`/farm/${pid}`} style={{ width: '100%' }}>
-          <ButtonPrimary padding="8px" borderRadius="8px">
-            {isStaking ? 'Manage' : 'Deposit'}
-          </ButtonPrimary>
-        </StyledInternalLink>
+        {farmingConfig?.pools[pid].stakingAsset.isLpToken && (
+          <TYPE.green01 marginLeft={32} fontSize={14}>
+            Get {farmingConfig?.pools[pid].stakingAsset.name}
+          </TYPE.green01>
+        )}
       </TopSection>
 
       <StatContainer>
-        <RowBetween>
-          <TYPE.white>Total staked value</TYPE.white>
-          <TYPE.white>
-            {/* {valueOfTotalStakedAmountInUSDC
-              ? `$${valueOfTotalStakedAmountInUSDC.toFixed(0, { groupSeparator: ',' })}`
-              : `${valueOfTotalStakedAmountInWETH?.toSignificant(4, { groupSeparator: ',' }) ?? '-'} ETH`} */}
-            114514.1919 USD
+        <StakingColumn>
+          <TYPE.gray fontSize={12}>Staked LP</TYPE.gray>
+          <TYPE.white fontSize={16}>{parsedStakedAmount}</TYPE.white>
+          <ButtonPrimary
+            height={28}
+            width="auto"
+            fontSize={12}
+            padding="5px 12px"
+            onClick={() => setShowStakingModal(true)}
+          >
+            Stake
+          </ButtonPrimary>
+          <ButtonPrimary
+            height={28}
+            width="auto"
+            fontSize={12}
+            padding="5px 12px"
+            onClick={() => setShowUnstakingModal(true)}
+          >
+            Withdraw
+          </ButtonPrimary>
+        </StakingColumn>
+        <StakingColumn>
+          <TYPE.gray fontSize={12}>Earned Rewards</TYPE.gray>
+          <TYPE.white fontSize={16}>
+            {parsedPendingSushiAmount} {rewardToken.symbol}
           </TYPE.white>
-        </RowBetween>
-        <RowBetween>
-          <TYPE.white> APR </TYPE.white>
-          <TYPE.white>
-            {/* {stakingInfo
-              ? stakingInfo.active
-                ? `${stakingInfo.totalRewardRate
-                    ?.multiply(BIG_INT_SECONDS_IN_WEEK)
-                    ?.toFixed(0, { groupSeparator: ',' })} UNI / week`
-                : '0 UNI / week'
-              : '-'} */}
-            19.19810%
-          </TYPE.white>
-        </RowBetween>
+          <ButtonPrimary
+            height={28}
+            width="auto"
+            fontSize={12}
+            padding="5px 12px"
+            onClick={() => masterChef.harvest(pid)}
+          >
+            Claim
+          </ButtonPrimary>
+        </StakingColumn>
+        <StakingColumn>
+          <TYPE.gray fontSize={12}>APR</TYPE.gray>
+          <TYPE.white fontSize={16}>19.810%</TYPE.white>
+        </StakingColumn>
+        <StakingColumn>
+          <TYPE.gray fontSize={12}>Liquidity</TYPE.gray>
+          <TYPE.white fontSize={16}>$1145141919.810</TYPE.white>
+        </StakingColumn>
       </StatContainer>
 
-      {isStaking && (
+      {/* {isStaking && (
         <>
-          <Break />
           <BottomSection showBackground={true}>
             <TYPE.black color={'white'} fontWeight={500}>
               <span>You earned</span>
             </TYPE.black>
 
             <TYPE.black style={{ textAlign: 'right' }} color={'white'} fontWeight={500}>
-              {/* {stakingInfo
+              {stakingInfo
                 ? stakingInfo.active
                   ? `${stakingInfo.rewardRate
                       ?.multiply(BIG_INT_SECONDS_IN_WEEK)
                       ?.toSignificant(4, { groupSeparator: ',' })} UNI / week`
                   : '0 UNI / week'
-                : '-'} */}
+                : '-'}
               810.1919 UNI
             </TYPE.black>
           </BottomSection>
         </>
-      )}
+      )} */}
+      <>
+        <StakingModal isOpen={showStakingModal} pid={pid} onDismiss={() => setShowStakingModal(false)} />
+        <UnstakingModal isOpen={showUnstakingModal} pid={pid} onDismiss={() => setShowUnstakingModal(false)} />
+        {/* <ClaimRewardModal
+          isOpen={showClaimRewardModal}
+          pid={pid}
+          onDismiss={() => setShowClaimRewardModal(false)}
+        /> */}
+      </>
     </Wrapper>
   )
 }
