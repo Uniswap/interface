8cf328c0db4eb97c027821bae2db7ff1d2c68429
Jorge <dalmendray@gmail.com>
2022-09-01 23:32:27 +0200

    feat: add relay-devtools to flipper (#1595)
    * feat: add relay-devtools to flipper
    
    * remove flipper
    
    * comment
    
    Co-authored-by: Jorge Dalmendray <dalmendray@jorgeuni.local>

--- src/data/relay.ts
+++ src/data/relay.ts
@@ -23,6 +23,13 @@ async function fetchRelay(params: RequestParameters, variables: Variables) {
   return await response.json()
 }
 
+if (__DEV__) {
+  // We need to add the plugin in this file. See discussion here:
+  // https://github.com/th3rdwave/flipper-plugin-relay-devtools/issues/10#issuecomment-1135440690
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('react-native-flipper-relay-devtools').addPlugin()
+}
+
 export default new Environment({
   network: Network.create(fetchRelay),
   store: new Store(new RecordSource()),
