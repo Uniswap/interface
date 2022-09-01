a8e6b1f0b976a9f0a457aedec2cd3ceb9ef7439e
Ian Lapham <ianlapham@gmail.com>
2022-09-01 16:47:51 -0400

    feat: Add ensavatars to search (#1592)
    * add avatars to search
    
    * refactor to use address display
    
    * remove change

--- src/components/explore/search/SearchWalletItem.tsx
+++ src/components/explore/search/SearchWalletItem.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { useAppDispatch, useAppTheme } from 'src/app/hooks'
 import { useExploreStackNavigation } from 'src/app/navigation/types'
 import ProfileIcon from 'src/assets/icons/profile.svg'
+import { AddressDisplay } from 'src/components/AddressDisplay'
 import { Button } from 'src/components/buttons/Button'
 import { Flex } from 'src/components/layout/Flex'
-import { Text } from 'src/components/Text'
-import { Unicon } from 'src/components/unicons/Unicon'
 import { addToSearchHistory, WalletSearchResult } from 'src/features/explore/searchHistorySlice'
 import { ElementName } from 'src/features/telemetry/constants'
 import { Screens } from 'src/screens/Screens'
-import { shortenAddress } from 'src/utils/addresses'
 
 type SearchWalletItemProps = {
   wallet: WalletSearchResult
@@ -20,7 +18,7 @@ export function SearchWalletItem({ wallet }: SearchWalletItemProps) {
   const dispatch = useAppDispatch()
   const navigation = useExploreStackNavigation()
 
-  const { address, ensName } = wallet
+  const { address } = wallet
 
   const onPress = () => {
     dispatch(addToSearchHistory({ searchResult: wallet }))
@@ -30,17 +28,13 @@ export function SearchWalletItem({ wallet }: SearchWalletItemProps) {
   return (
     <Button name={ElementName.SearchWalletItem} testID={`wallet-item-${address}`} onPress={onPress}>
       <Flex row alignItems="center" gap="sm" justifyContent="space-between" px="xs" py="sm">
-        <Flex centered row gap="xs">
-          <Unicon address={address} size={32} />
-          <Flex gap="none">
-            <Text variant="subhead">{ensName}</Text>
-            <Flex height={20}>
-              <Text color="textSecondary" variant="caption">
-                {shortenAddress(address)}
-              </Text>
-            </Flex>
-          </Flex>
-        </Flex>
+        <AddressDisplay
+          showAddressAsSubtitle
+          address={address}
+          size={32}
+          variant="subhead"
+          verticalGap="none"
+        />
         <ProfileIcon color={theme.colors.textSecondary} height={24} width={24} />
       </Flex>
     </Button>
