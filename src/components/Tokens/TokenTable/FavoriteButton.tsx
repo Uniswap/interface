0db8010663634af860c1228ee3051c01f00c6315
Alex Ball <alexball@UNISWAP-MAC-038.local>
2022-09-06 16:41:45 -0400

    fixing to match design

--- src/components/Tokens/TokenTable/FavoriteButton.tsx
+++ src/components/Tokens/TokenTable/FavoriteButton.tsx
@@ -15,15 +15,16 @@ const FavoriteButtonContent = styled.div`
 const StyledFavoriteButton = styled.button<{ active: boolean }>`
   padding: 0px 16px;
   border-radius: 12px;
-  background-color: ${({ theme, active }) => (active ? theme.accentActiveSoft : theme.backgroundInteractive)};
-  border: none;
-  color: ${({ theme, active }) => (active ? theme.accentActive : theme.textPrimary)};
+  background-color: ${({ theme, active }) => (active ? theme.accentActionSoft : theme.backgroundInteractive)};
+  border: ${({ active, theme }) => (active ? `1px solid ${theme.accentAction}` : 'none')};
+  color: ${({ theme, active }) => (active ? theme.accentAction : theme.textPrimary)};
   font-size: 16px;
   font-weight: 600;
   cursor: pointer;
 
   :hover {
     background-color: ${({ theme, active }) => !active && theme.backgroundModule};
+    opacity: ${({ active }) => (active ? '60%' : '100%')};
   }
 `
 const FavoriteText = styled.span`
@@ -38,11 +39,7 @@ export default function FavoriteButton() {
   return (
     <StyledFavoriteButton onClick={() => setShowFavorites(!showFavorites)} active={showFavorites}>
       <FavoriteButtonContent>
-        <Heart
-          size={17}
-          color={showFavorites ? theme.accentActive : theme.textPrimary}
-          fill={showFavorites ? theme.accentActive : 'transparent'}
-        />
+        <Heart size={17} color={showFavorites ? theme.accentAction : theme.textPrimary} />
         <FavoriteText>
           <Trans>Favorites</Trans>
         </FavoriteText>
