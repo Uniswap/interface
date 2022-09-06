27acf7d62e3f490ab0f0cc06e347ada60f956e28
hyemmie <lhm0710@naver.com>
2022-09-06 17:17:31 +0900

    edit test token logouri

--- src/state/lists/reducer.ts
+++ src/state/lists/reducer.ts
@@ -99,7 +99,8 @@ export default createReducer(initialState, (builder) =>
                   name: 'Gold',
                   symbol: 'GLD',
                   decimals: 18,
-                  logoURI: 'https://static.optimism.io/data/wstETH/logo.svg',
+                  logoURI:
+                    'https://raw.githubusercontent.com/radiusxyz/tex-interface/main/src/assets/images/radius.jpg',
                   extensions: {
                     optimismBridgeAddress: '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1',
                   },
@@ -110,7 +111,8 @@ export default createReducer(initialState, (builder) =>
                   name: 'Silver',
                   symbol: 'SLVR',
                   decimals: 18,
-                  logoURI: 'https://static.optimism.io/data/wstETH/logo.svg',
+                  logoURI:
+                    'https://raw.githubusercontent.com/radiusxyz/tex-interface/main/src/assets/images/radius.jpg',
                   extensions: {
                     optimismBridgeAddress: '0xE9e84b42E53A8784dF45c761aCE4d1cf4f948Eb3',
                   },
@@ -156,7 +158,7 @@ export default createReducer(initialState, (builder) =>
                 name: 'Gold',
                 symbol: 'GLD',
                 decimals: 18,
-                logoURI: 'https://static.optimism.io/data/wstETH/logo.svg',
+                logoURI: 'https://raw.githubusercontent.com/radiusxyz/tex-interface/main/src/assets/images/radius.jpg',
                 extensions: {
                   optimismBridgeAddress: '0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1',
                 },
@@ -167,7 +169,7 @@ export default createReducer(initialState, (builder) =>
                 name: 'Silver',
                 symbol: 'SLVR',
                 decimals: 18,
-                logoURI: 'https://static.optimism.io/data/wstETH/logo.svg',
+                logoURI: 'https://raw.githubusercontent.com/radiusxyz/tex-interface/main/src/assets/images/radius.jpg',
                 extensions: {
                   optimismBridgeAddress: '0xE9e84b42E53A8784dF45c761aCE4d1cf4f948Eb3',
                 },
