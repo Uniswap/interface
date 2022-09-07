74a21a8589e97614356654f73f6f8f9a489a8d27
Alex Ball <alexball@UNISWAP-MAC-038.local>
2022-09-07 14:35:23 -0400

    current progress towards merge

---
+++ src/nft/components/details/Traits.css.ts
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css'
+import { sprinkles } from '../../css/sprinkles.css'
+
+export const grid = style([
+  sprinkles({ gap: '16', display: 'grid' }),
+  {
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    '@media': {
+      '(max-width: 1512px)': {
+        gridTemplateColumns: 'repeat(3, 1fr)',
+      },
+      '(max-width: 515px)': {
+        gridTemplateColumns: 'repeat(2, 1fr)',
+      },
+    },
+  },
+])
