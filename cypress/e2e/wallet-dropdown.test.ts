520be4023d4f7a6bdf039c0069b00588fddefb76
Alex Ball <alexball@UNISWAP-MAC-038.local>
2022-09-08 14:17:48 -0400

    testing disconnected state

--- cypress/e2e/wallet-dropdown.test.ts
+++ cypress/e2e/wallet-dropdown.test.ts
@@ -6,39 +6,10 @@ describe('Wallet Dropdown', () => {
     cy.visit('/', { featureFlag: FeatureFlag.navBar })
   })
 
-  it('should change the theme', () => {
-    cy.get(getTestSelector('web3-status-connected')).click()
-    cy.get(getTestSelector('wallet-select-theme')).click()
-    cy.get(getTestSelector('wallet-select-theme')).contains('Light theme').should('exist')
-  })
-
-  it('should select a language', () => {
-    cy.get(getTestSelector('wallet-select-language')).click()
-    cy.get(getTestSelector('wallet-language-item')).contains('Afrikaans').click({ force: true })
-    cy.get(getTestSelector('wallet-header')).should('contain', 'Taal')
-    cy.get(getTestSelector('wallet-language-item')).contains('English').click({ force: true })
-    cy.get(getTestSelector('wallet-header')).should('contain', 'Language')
-    cy.get(getTestSelector('wallet-back')).click()
-  })
-
-  // loggedout
-  // it('should open the wallet connect modal from the drop down', () => {
-  //   cy.get(getTestSelector('wallet-disconnect')).click()
-  //   cy.get(getTestSelector('wallet-connect-wallet')).click()
-  //   cy.get(getTestSelector('wallet-modal')).should('exist')
-  //   cy.get(getTestSelector('wallet-modal-close')).click()
-  // })
-
-  // it('should open the wallet connect modal from the navbar', () => {
-  //   cy.get(getTestSelector('navbar-connect-wallet')).click()
-  //   cy.get(getTestSelector('wallet-modal')).should('exist')
-  //   cy.get(getTestSelector('wallet-modal-close')).click()
-  // })
-
   // it('should change the theme', () => {
-  //   cy.get(getTestSelector('navbar-wallet-dropdown')).click()
+  //   cy.get(getTestSelector('web3-status-connected')).click()
   //   cy.get(getTestSelector('wallet-select-theme')).click()
-  //   cy.get(getTestSelector('wallet-select-theme')).contains('Dark theme').should('exist')
+  //   cy.get(getTestSelector('wallet-select-theme')).contains('Light theme').should('exist')
   // })
 
   // it('should select a language', () => {
@@ -49,4 +20,35 @@ describe('Wallet Dropdown', () => {
   //   cy.get(getTestSelector('wallet-header')).should('contain', 'Language')
   //   cy.get(getTestSelector('wallet-back')).click()
   // })
+
+  // loggedout
+  it('should open the wallet connect modal from the drop down', () => {
+    cy.get(getTestSelector('web3-status-connected')).click()
+    cy.get(getTestSelector('wallet-disconnect')).click()
+    cy.get(getTestSelector('wallet-connect-wallet')).click()
+    cy.get(getTestSelector('wallet-modal')).should('exist')
+    cy.get(getTestSelector('wallet-modal-close')).click()
+  })
+
+  it('should open the wallet connect modal from the navbar', () => {
+    cy.get(getTestSelector('navbar-connect-wallet')).click()
+    cy.get(getTestSelector('wallet-modal')).should('exist')
+    cy.get(getTestSelector('wallet-modal-close')).click()
+  })
+
+  // it('should change the theme', () => {
+  //   cy.get(getTestSelector('navbar-wallet-dropdown')).click()
+  //   cy.get(getTestSelector('wallet-select-theme')).click()
+  //   cy.get(getTestSelector('wallet-select-theme')).contains('Dark theme').should('exist')
+  // })
+
+  it('should select a language', () => {
+    cy.get(getTestSelector('navbar-wallet-dropdown')).click()
+    cy.get(getTestSelector('wallet-select-language')).click()
+    cy.get(getTestSelector('wallet-language-item')).contains('Afrikaans').click({ force: true })
+    cy.get(getTestSelector('wallet-header')).should('contain', 'Taal')
+    cy.get(getTestSelector('wallet-language-item')).contains('English').click({ force: true })
+    cy.get(getTestSelector('wallet-header')).should('contain', 'Language')
+    cy.get(getTestSelector('wallet-back')).click()
+  })
 })
