e6edbe3f26b4ff73731bd1fa66a9b95299849c3e
Alex Ball <alexball@UNISWAP-MAC-038.local>
2022-09-08 16:30:32 -0400

    trying different order

--- cypress/e2e/wallet-dropdown.test.ts
+++ cypress/e2e/wallet-dropdown.test.ts
@@ -21,33 +21,25 @@ describe('Wallet Dropdown', () => {
     cy.get(getTestSelector('wallet-back')).click()
   })
 
-  // loggedout
-  it('should open the wallet connect modal from the drop down', () => {
+  it('should change the theme', () => {
     cy.get(getTestSelector('wallet-disconnect')).click()
-    cy.get(getTestSelector('wallet-connect-wallet')).click()
-    cy.get(getTestSelector('wallet-modal')).should('exist')
-    cy.get(getTestSelector('wallet-modal-close')).click()
+    cy.get(getTestSelector('wallet-select-theme')).click()
+    cy.get(getTestSelector('wallet-select-theme')).contains('Dark theme').should('exist')
   })
-
-  it('should open the wallet connect modal from the navbar', () => {
-    cy.get(getTestSelector('navbar-connect-wallet')).click()
-    cy.get(getTestSelector('wallet-modal')).should('exist')
-    cy.get(getTestSelector('wallet-modal-close')).click()
-  })
-
-  // it('should change the theme', () => {
-  //   cy.get(getTestSelector('navbar-wallet-dropdown')).click()
-  //   cy.get(getTestSelector('wallet-select-theme')).click()
-  //   cy.get(getTestSelector('wallet-select-theme')).contains('Dark theme').should('exist')
-  // })
 
   it('should select a language', () => {
-    cy.get(getTestSelector('navbar-wallet-dropdown')).click()
     cy.get(getTestSelector('wallet-select-language')).click()
     cy.get(getTestSelector('wallet-language-item')).contains('Afrikaans').click({ force: true })
     cy.get(getTestSelector('wallet-header')).should('contain', 'Taal')
     cy.get(getTestSelector('wallet-language-item')).contains('English').click({ force: true })
     cy.get(getTestSelector('wallet-header')).should('contain', 'Language')
     cy.get(getTestSelector('wallet-back')).click()
+  })
+
+  // loggedout
+  it('should open the wallet connect modal from the drop down', () => {
+    cy.get(getTestSelector('wallet-connect-wallet')).click()
+    cy.get(getTestSelector('wallet-modal')).should('exist')
+    cy.get(getTestSelector('wallet-modal-close')).click()
   })
 })
