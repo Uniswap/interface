7bddf4576e9a1d9ab6214a86e60ed57271f32a8f
Alex Ball <alexball@UNISWAP-MAC-038.local>
2022-09-08 16:34:37 -0400

    removing unused util code

--- cypress/utils/index.ts
+++ cypress/utils/index.ts
@@ -1,10 +1,1 @@
-import { FeatureFlag } from '../../src/featureFlags/flags/featureFlags'
-
 export const getTestSelector = (selectorId: string) => `[data-testid=${selectorId}]`
-
-export const selectFeatureFlag = (featureFlags: Array<FeatureFlag>) => {
-  cy.get(getTestSelector('menu')).click()
-  cy.get(getTestSelector('feature-flags')).click()
-  featureFlags.forEach((featureFlag) => cy.get(`#${featureFlag}`).select('enabled'))
-  cy.reload()
-}
