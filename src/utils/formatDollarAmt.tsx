c7e8d78214a56eacf7fb189b155ec7a566e973c5
Krasimir Raykov <raykov@weichain.tech>
2022-09-09 16:35:26 +0300

    fix: remove $ when formatting amounts

--- src/utils/formatDollarAmt.tsx
+++ src/utils/formatDollarAmt.tsx
@@ -26,7 +26,7 @@ export const formatAmount = (num: number | undefined, digits = 2) => {
   if (num === 0) return '0'
   if (!num) return '-'
   if (num < 0.001) {
-    return '$<0.001'
+    return '<0.001'
   }
   return numbro(num).format({
     average: true,
