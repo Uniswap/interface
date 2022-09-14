451d4017752735016ad5e2834919bb477dd3ac84
Alex Ball <alexball@UNISWAP-MAC-038.local>
2022-09-14 13:14:05 -0400

    adding in loading styling

---
+++ src/nft/components/collection/CollectionAssetLoading.css.ts
@@ -0,0 +1,20 @@
+import { style } from '@vanilla-extract/css'
+import { themeVars } from '../../css/sprinkles.css'
+import { loadingAsset } from 'nft/css/loading.css'
+
+export const collectionAssetLoading = style([
+  {
+    backgroundColor: themeVars.colors.lightGray,
+    borderRadius: 12,
+    paddingBottom: 12,
+  },
+])
+
+export const collectionAssetsImageLoading = style([
+  loadingAsset,
+  {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+  },
+])
--- src/nft/css/loading.css.ts
+++ src/nft/css/loading.css.ts
@@ -1,5 +1,6 @@
 import { keyframes, style } from '@vanilla-extract/css'
 import { darken } from 'polished'
+import { sprinkles } from './sprinkles.css'
 
 export const loadingAnimation = keyframes({
   '0%': {
@@ -17,5 +18,17 @@ export const loadingBlock = style([
     background: `linear-gradient(to left, #7C85A24D 25%, ${darken(0.8, '#7C85A24D')} 50%, #7C85A24D 75%)`,
     backgroundSize: '400%',
     willChange: 'background-position',
+  },
+])
+
+export const loadingAsset = style([
+  loadingBlock,
+  sprinkles({
+    borderRadius: '12',
+    cursor: 'default',
+    color: 'transparent',
+  }),
+  {
+    userSelect: 'none',
   },
 ])
