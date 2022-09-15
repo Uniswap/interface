5a765b72351b16659a8b95bd96bbb985be2440df
Alex Ball <alex.ball@uniswap.org>
2022-09-15 16:28:19 -0400

    slight refractor

--- src/nft/hooks/useTimeout.ts
+++ src/nft/hooks/useTimeout.ts
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react'
 
+const MINUTE = 1000 * 60
+const HOUR = MINUTE * 60
+const DAY = 24 * HOUR
+
 const getReturnValues = (countDown: number): [number, number, number, number] => {
   // calculate time left
-  const days = Math.floor(countDown / (1000 * 60 * 60 * 24))
-  const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.floor((countDown % (1000 * 60)) / 1000)
+  const days = Math.floor(countDown / DAY)
+  const hours = Math.floor((countDown % DAY) / HOUR)
+  const minutes = Math.floor((countDown % HOUR) / MINUTE)
+  const seconds = Math.floor((countDown % MINUTE) / 1000)
 
   return [days, hours, minutes, seconds]
 }
