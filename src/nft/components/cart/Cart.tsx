44366dc1bb5b78589044bd921476101e197bfeb3
Charles Bachmeier <charlie@genie.xyz>
2022-09-16 05:55:03 -0700

    Add shopping bag to top lovel modal wrapper

--- src/nft/components/cart/Cart.tsx
+++ src/nft/components/cart/Cart.tsx
@@ -1,10 +1,11 @@
+import Loader from 'components/Loader'
 import { Box } from 'nft/components/Box'
 import { Column } from 'nft/components/Flex'
 import { CloseDropDownIcon } from 'nft/components/icons'
 import { bodySmall, buttonMedium, headlineSmall } from 'nft/css/common.css'
 import { useBag, useIsMobile, useSellAsset, useSellPageState } from 'nft/hooks'
 import { SellPageStateType } from 'nft/types'
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import { useLocation } from 'react-router-dom'
 
 import * as styles from './Cart.css'
@@ -13,61 +14,76 @@ import { orderButton } from './CartBalance.css'
 const CartSellAssetRow = lazy(() => import('./CartSellAssetRow'))
 
 const Cart = () => {
-  const location = useLocation()
-  const isSell = location.pathname === '/nfts/sell'
+  const { pathname } = useLocation()
+  const isSell = pathname === '/nfts/sell'
+  const isNFTPage = pathname.startsWith('/nfts')
   const sellAssets = useSellAsset((state) => state.sellAssets)
   const setSellPageState = useSellPageState((state) => state.setSellPageState)
   const sellPageState = useSellPageState((state) => state.state)
   const toggleCart = useBag((state) => state.toggleBag)
   const isMobile = useIsMobile()
+  const bagExpanded = useBag((s) => s.bagExpanded)
 
   return (
-    <Column
-      marginTop={{ md: '4', sm: '0' }}
-      marginRight={{ md: '20', sm: '0' }}
-      className={styles.cartContainer}
-      width={{ md: '288', sm: 'full' }}
+    <Box
+      zIndex={{ sm: '3', md: '2' }}
+      width={{ sm: 'full', md: 'auto' }}
       height={{ sm: 'full', md: 'auto' }}
-      backgroundColor="white"
-      marginLeft="0"
-      justifyContent="flex-start"
+      position="fixed"
+      left={{ sm: '0', md: 'unset' }}
+      right={{ sm: 'unset', md: '0' }}
+      top={{ sm: '0', md: 'unset' }}
+      display={bagExpanded && isNFTPage ? 'flex' : 'none'}
     >
-      {isSell && sellPageState === SellPageStateType.LISTING ? null : (
-        <>
-          <BagHeader isSell={isSell} bagQuantity={isSell ? sellAssets.length : 0} />
-          {isSell ? (
-            <Column
-              overflowX="hidden"
-              overflowY="auto"
-              position="relative"
-              paddingTop="6"
-              paddingBottom="6"
-              height="full"
-              className={styles.cartAssets}
-            >
-              {sellAssets.length
-                ? sellAssets.map((asset, index) => <CartSellAssetRow asset={asset} key={index} />)
-                : null}
-            </Column>
-          ) : null}
-          {isSell ? (
-            <Box padding="12">
-              <Box
-                as="button"
-                className={`${buttonMedium} ${orderButton}`}
-                disabled={sellAssets.length === 0}
-                onClick={() => {
-                  isMobile && toggleCart()
-                  setSellPageState(SellPageStateType.LISTING)
-                }}
-              >
-                Continue
-              </Box>
-            </Box>
-          ) : null}
-        </>
-      )}
-    </Column>
+      <Suspense fallback={<Loader />}>
+        <Column
+          marginTop={{ sm: '0', md: '4' }}
+          marginRight={{ sm: '0', md: '20' }}
+          className={styles.cartContainer}
+          width={{ sm: 'full', md: '288' }}
+          height={{ sm: 'full', md: 'auto' }}
+          backgroundColor="white"
+          marginLeft="0"
+          justifyContent="flex-start"
+        >
+          {isSell && sellPageState === SellPageStateType.LISTING ? null : (
+            <>
+              <BagHeader isSell={isSell} bagQuantity={isSell ? sellAssets.length : 0} />
+              {isSell ? (
+                <Column
+                  overflowX="hidden"
+                  overflowY="auto"
+                  position="relative"
+                  paddingTop="6"
+                  paddingBottom="6"
+                  height="full"
+                  className={styles.cartAssets}
+                >
+                  {sellAssets.length
+                    ? sellAssets.map((asset, index) => <CartSellAssetRow asset={asset} key={index} />)
+                    : null}
+                </Column>
+              ) : null}
+              {isSell ? (
+                <Box padding="12">
+                  <Box
+                    as="button"
+                    className={`${buttonMedium} ${orderButton}`}
+                    disabled={sellAssets.length === 0}
+                    onClick={() => {
+                      isMobile && toggleCart()
+                      setSellPageState(SellPageStateType.LISTING)
+                    }}
+                  >
+                    Continue
+                  </Box>
+                </Box>
+              ) : null}
+            </>
+          )}
+        </Column>
+      </Suspense>
+    </Box>
   )
 }
 
