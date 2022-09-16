8c947a0e0d0316b52829eb021262e289fef8f055
Charles Bachmeier <charles@bachmeier.io>
2022-09-16 11:31:25 -0700

    feat: Add Select Page Modal (#4643)
    * add select nfts shopping bag modal
    
    * Add shopping bag to top lovel modal wrapper
    
    * addressing comments
    
    * rename to listingTag
    
    Co-authored-by: Charles Bachmeier <charlie@genie.xyz>

--- src/components/TopLevelModals/index.tsx
+++ src/components/TopLevelModals/index.tsx
@@ -2,8 +2,11 @@ import { useWeb3React } from '@web3-react/core'
 import AddressClaimModal from 'components/claim/AddressClaimModal'
 import ConnectedAccountBlocked from 'components/ConnectedAccountBlocked'
 import useAccountRiskCheck from 'hooks/useAccountRiskCheck'
+import { lazy } from 'react'
 import { useModalIsOpen, useToggleModal } from 'state/application/hooks'
 import { ApplicationModal } from 'state/application/reducer'
+
+const Cart = lazy(() => import('nft/components/sell/modal/ListingTag'))
 
 export default function TopLevelModals() {
   const addressClaimOpen = useModalIsOpen(ApplicationModal.ADDRESS_CLAIM)
@@ -18,6 +21,7 @@ export default function TopLevelModals() {
     <>
       <AddressClaimModal isOpen={addressClaimOpen} onDismiss={addressClaimToggle} />
       <ConnectedAccountBlocked account={account} isOpen={open} />
+      <Cart />
     </>
   )
 }
