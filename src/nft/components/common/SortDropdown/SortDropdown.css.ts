49c5cbbf3b620b66cf2cb142f4afb7f01ba89ee6
Charles Bachmeier <charles@bachmeier.io>
2022-09-16 10:55:26 -0700

    feat: Add sell page filters sidebar (#4630)
    * working sell filters
    
    * split filters into own file
    
    * include new file
    
    * fix eslint warnings
    
    * update filter button param and fix rerender bug
    
    * de morgon's law
    
    * usecallback
    
    * move max_padding
    
    * extend htmlinputelement for checkbox
    
    * styles cleanup
    
    * simplify checkbox sprinkles
    
    * add null check to collectionfilteritem
    
    * remove x axis scrollbar on collections
    
    * update fitlerbutton logic
    
    * scrollbar width
    
    Co-authored-by: Charles Bachmeier <charlie@genie.xyz>

---
+++ src/nft/components/common/SortDropdown/SortDropdown.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css'
+
+export const activeDropdown = style({
+  borderBottom: 'none',
+})
+
+export const activeDropDownItems = style({
+  borderTop: 'none',
+})
