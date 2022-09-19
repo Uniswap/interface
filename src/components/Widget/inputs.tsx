2917cbba99a6bede4ea9fbaedc93f2dd653b7196
Vignesh Mohankumar <me@vig.xyz>
2022-09-19 14:00:58 -0400

    fix: reset Widget amount on URL change

--- src/components/Widget/inputs.tsx
+++ src/components/Widget/inputs.tsx
@@ -22,6 +22,7 @@ export function useSyncWidgetInputs(defaultToken?: Currency) {
     setTokens({
       [Field.OUTPUT]: defaultToken,
     })
+    setAmount('')
   }, [defaultToken])
 
   const onSwitchTokens = useCallback(() => {
