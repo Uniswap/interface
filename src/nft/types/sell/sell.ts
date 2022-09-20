ee97d8d902457c6749b343c4b0c0a8d2f9a7f652
Charles Bachmeier <charles@bachmeier.io>
2022-09-20 12:45:22 -0500

    feat: add listing modal (#4663)
    * add listing modal
    
    * add new files
    
    * remove useeffect
    
    * re-add useeffect and includes array
    
    * position relative
    
    * add listing datatype
    
    * use pluralize
    
    * readable const
    
    * clsx
    
    * parseFloat 0 default
    
    * don't use any
    
    * cant use months for ms
    
    Co-authored-by: Charles Bachmeier <charlie@genie.xyz>

--- src/nft/types/sell/sell.ts
+++ src/nft/types/sell/sell.ts
@@ -28,6 +28,12 @@ export interface SellOrder {
   tokenReserves?: number
 }
 
+export interface Listing {
+  price?: string
+  marketplace: ListingMarket
+  overrideFloorPrice?: boolean
+}
+
 export interface WalletAsset {
   id?: string
   image_url: string
@@ -60,11 +66,7 @@ export interface WalletAsset {
   // Used for creating new listings
   expirationTime?: number
   marketAgnosticPrice?: string
-  newListings?: {
-    price?: string
-    marketplace: ListingMarket
-    overrideFloorPrice?: boolean
-  }[]
+  newListings?: Listing[]
   marketplaces?: ListingMarket[]
   listingWarnings?: ListingWarning[]
 }
@@ -88,19 +90,19 @@ export enum ListingStatus {
   SIGNING = 'Signing',
 }
 
-export interface ListingRow {
+export interface AssetRow {
   images: string[]
   name: string
   status: ListingStatus
   callback?: () => Promise<void>
 }
 
-export interface AssetRow extends ListingRow {
+export interface ListingRow extends AssetRow {
   asset: WalletAsset
   marketplace: ListingMarket
 }
 
-export interface CollectionRow extends ListingRow {
+export interface CollectionRow extends AssetRow {
   collectionAddress: string
   marketplace: ListingMarket
 }
