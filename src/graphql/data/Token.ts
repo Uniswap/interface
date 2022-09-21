7f2cc9a3e6ee1bb61d4d3ff55ad509b56ec85928
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-09-21 15:35:17 -0500

    feat: add tvl to token details page (#4692)
    * add tvl to token details page
    
    * remove mcap, add explanation of market entities

--- src/graphql/data/Token.ts
+++ src/graphql/data/Token.ts
@@ -25,7 +25,14 @@ const tokenPricesFragment = graphql`
     }
   }
 `
-
+/*
+The difference between Token and TokenProject:
+  Token: an on-chain entity referring to a contract (e.g. uni token on ethereum 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984)
+  TokenProject: an off-chain, aggregated entity that consists of a token and its bridged tokens (e.g. uni token on all chains)
+  TokenMarket and TokenProjectMarket then are market data entities for the above.
+    TokenMarket is per-chain market data for contracts pulled from the graph.
+    TokenProjectMarket is aggregated market data (aggregated over multiple dexes and centralized exchanges) that we get from coingecko.
+*/
 const tokenQuery = graphql`
   query TokenQuery($contract: ContractInput!, $duration: HistoryDuration!, $skip: Boolean = false) {
     tokenProjects(contracts: [$contract]) @skip(if: $skip) {
@@ -37,6 +44,12 @@ const tokenQuery = graphql`
         chain
         address
         symbol
+        market {
+          totalValueLocked {
+            value
+            currency
+          }
+        }
       }
       prices: markets(currencies: [USD]) {
         ...TokenPrices
