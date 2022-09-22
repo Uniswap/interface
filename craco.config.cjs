74762f9219934ee5e583d47b00b1f5d5cd367b37
Vignesh Mohankumar <me@vig.xyz>
2022-09-22 14:51:13 -0400

    feat: add git commit hash on build

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,13 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
+
+const commitHash = require('child_process').execSync('git rev-parse --short HEAD')
 
 module.exports = {
   babel: {
     plugins: ['@vanilla-extract/babel-plugin'],
   },
   webpack: {
-    plugins: [new VanillaExtractPlugin()],
+    plugins: [
+      new VanillaExtractPlugin(),
+      new webpack.DefinePlugin({
+        GIT_COMMIT_HASH: JSON.stringify(commitHash),
+      }),
+    ],
     configure: (webpackConfig) => {
       const instanceOfMiniCssExtractPlugin = webpackConfig.plugins.find(
         (plugin) => plugin instanceof MiniCssExtractPlugin
