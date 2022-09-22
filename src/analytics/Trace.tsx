5ec797917641223a3530a22f050863d324942591
Vignesh Mohankumar <me@vig.xyz>
2022-09-22 15:08:24 -0400

    log

--- src/analytics/Trace.tsx
+++ src/analytics/Trace.tsx
@@ -45,7 +45,13 @@ export const Trace = memo(
     useEffect(() => {
       if (shouldLogImpression) {
         const origin = window.location.origin
-        sendAnalyticsEvent(name ?? EventName.PAGE_VIEWED, { ...combinedProps, ...properties, origin })
+        const commitHash = process.env.REACT_APP_GIT_COMMIT_HASH
+        sendAnalyticsEvent(name ?? EventName.PAGE_VIEWED, {
+          ...combinedProps,
+          ...properties,
+          origin,
+          git_commit_hash: commitHash,
+        })
       }
       // Impressions should only be logged on mount.
       // eslint-disable-next-line react-hooks/exhaustive-deps
