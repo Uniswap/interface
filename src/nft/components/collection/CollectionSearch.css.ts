0dec2f86b8ae84d10683f1f67f5ccdc19f32ad81
Alex Ball <alex.ball@uniswap.org>
2022-09-23 11:28:56 -0400

    added search to loading state

--- src/nft/components/collection/CollectionSearch.css.ts
+++ src/nft/components/collection/CollectionSearch.css.ts
@@ -1,3 +1,10 @@
 import { style } from '@vanilla-extract/css'
 import { sprinkles, themeVars, vars } from 'nft/css/sprinkles.css'
 import { loadingAsset } from 'nft/css/loading.css'
+
+export const filterButtonLoading = style([
+  loadingAsset,
+  sprinkles({
+    border: 'none',
+  }),
+])
--- src/nft/components/collection/CollectionSearch.tsx
+++ src/nft/components/collection/CollectionSearch.tsx
@@ -2,8 +2,9 @@ import { Box } from 'nft/components/Box'
 import { useIsLoading } from 'nft/hooks'
 import { useCollectionFilters } from 'nft/hooks/useCollectionFilters'
 import { FormEvent } from 'react'
+import clsx from 'clsx'
 
-import * as styles from 'nft/components/collection/CollectionSearch'
+import * as styles from 'nft/components/collection/CollectionSearch.css'
 
 export const CollectionSearch = () => {
   const setSearchByNameText = useCollectionFilters((state) => state.setSearch)
@@ -23,7 +24,8 @@ export const CollectionSearch = () => {
       height="44"
       color={{ placeholder: 'darkGray', default: 'blackBlue' }}
       value={searchByNameText}
-      placeholder={'Search by name'}
+      placeholder={isLoading ? '' : 'Search by name'}
+      className={clsx(isLoading && styles.filterButtonLoading)}
       onChange={(e: FormEvent<HTMLInputElement>) => {
         setSearchByNameText(e.currentTarget.value)
       }}
