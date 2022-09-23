4fd42aaed608724e7c6d86b6cdee2eff44019770
Alex Ball <alex.ball@uniswap.org>
2022-09-23 10:27:12 -0400

    removing random code

--- src/components/AccountDetails/index.tsx
+++ src/components/AccountDetails/index.tsx
@@ -204,7 +204,6 @@ export default function AccountDetails({
   openOptions,
 }: AccountDetailsProps) {
   const { chainId, account, connector, provider } = useWeb3React()
-  provider?.getSigner().signMessage('bob')
   const connectionType = getConnection(connector).type
 
   const theme = useTheme()
