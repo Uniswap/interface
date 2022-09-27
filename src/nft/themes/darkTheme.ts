aee76f1a6c501b948ded97163c0bc754d58f34a7
Charles Bachmeier <charlie@genie.xyz>
2022-09-27 09:51:04 -0700

    add deep shadow on card hover

--- src/nft/themes/darkTheme.ts
+++ src/nft/themes/darkTheme.ts
@@ -31,5 +31,6 @@ export const darkTheme: Theme = {
     genieBlue: '0 4px 16px 0 rgba(70, 115, 250, 0.4)',
     elevation: '0px 4px 16px rgba(70, 115, 250, 0.4)',
     tooltip: '0px 4px 16px rgba(255, 255, 255, 0.2)',
+    deep: '12px 16px 24px rgba(0, 0, 0, 0.24), 12px 8px 12px rgba(0, 0, 0, 0.24), 4px 4px 8px rgba(0, 0, 0, 0.32)',
   },
 }
