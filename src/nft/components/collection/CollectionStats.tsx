17fa102a66a2778b7e5870923ee5370fb454b4ff
Jack Short <john.short.tj@gmail.com>
2022-09-30 13:54:44 -0400

    style: filling background color for collection header

--- src/nft/components/collection/CollectionStats.tsx
+++ src/nft/components/collection/CollectionStats.tsx
@@ -311,6 +311,7 @@ export const CollectionStats = ({ stats, isMobile }: { stats: GenieCollection; i
       )}
       <Box
         as={isCollectionStatsLoading ? 'div' : 'img'}
+        background="backgroundSurface"
         borderRadius="round"
         position="absolute"
         className={isCollectionStatsLoading ? styles.collectionImageIsLoading : styles.collectionImage}
