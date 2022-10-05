2c5a950e1fd3fb13092d59a69351bda4d7805cc9
Justin Domingue <judo@uniswap.org>
2022-10-05 16:11:22 -0400

    chore: remove unnecessary token list from downloaded lists (#1864)

--- src/constants/tokenLists/tokenLists.ts
+++ src/constants/tokenLists/tokenLists.ts
@@ -1,22 +1,13 @@
-// Copied from https://github.com/Uniswap/interface/blob/main/src/constants/lists.ts
+// Originally copied from https://github.com/Uniswap/interface/blob/main/src/constants/lists.ts
 
-export const UNI_LIST = 'https://tokens.uniswap.org'
-const AAVE_LIST = 'tokenlist.aave.eth'
+const ARBITRUM_LIST = 'https://bridge.arbitrum.io/token-list-42161.json'
 const BA_LIST =
   'https://raw.githubusercontent.com/The-Blockchain-Association/sec-notice-list/master/ba-sec-list.json'
-const CMC_ALL_LIST = 'defi.cmc.eth'
-const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
-const COMPOUND_LIST =
-  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
-const ARBITRUM_LIST = 'https://bridge.arbitrum.io/token-list-42161.json'
-const KLEROS_LIST = 't2crtokens.eth'
 const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json'
-const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
-const SET_LIST =
-  'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
+export const UNI_LIST = 'https://tokens.uniswap.org'
 
 export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
 
@@ -24,15 +15,8 @@ export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST]
 // lower index == higher priority for token import
 const DEFAULT_LIST_OF_LISTS_TO_DISPLAY: string[] = [
   UNI_LIST,
-  COMPOUND_LIST,
-  AAVE_LIST,
-  CMC_ALL_LIST,
-  CMC_STABLECOIN,
   WRAPPED_LIST,
-  SET_LIST,
-  ROLL_LIST,
   COINGECKO_LIST,
-  KLEROS_LIST,
   ARBITRUM_LIST,
   OPTIMISM_LIST,
   GEMINI_LIST,
