e45b100e09757067f3453c8782ff67aeb5cd687b
Callil Capuozzo <callil.capuozzo@gmail.com>
2022-10-05 12:53:10 -0400

    Adjust to match designs

--- src/components/TokenSafety/TokenSafetyIcon.tsx
+++ src/components/TokenSafety/TokenSafetyIcon.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as Verified } from 'assets/svg/verified.svg'
 import { Warning } from 'constants/tokenSafety'
 import styled from 'styled-components/macro'
+import { colors } from 'theme/colors'
 
 const VerifiedContainer = styled.div`
   margin-left: 4px;
@@ -11,7 +12,7 @@ const VerifiedContainer = styled.div`
 export const VerifiedIcon = styled(Verified)<{ size?: string }>`
   width: ${({ size }) => size ?? '1em'};
   height: ${({ size }) => size ?? '1em'};
-  color: ${({ theme }) => theme.accentAction};
+  color: ${colors.magentaVibrant};
 `
 
 export default function TokenSafetyIcon({ warning }: { warning: Warning | null }) {
