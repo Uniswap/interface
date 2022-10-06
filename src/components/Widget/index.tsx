843afa93c33e1e75834473fc366d5ceca38911d2
Zach Pomerantz <zzmp@uniswap.org>
2022-10-06 17:50:33 -0500

    fix: do not load token list (#4820)
    * build: upgrade widgets
    
    * fix: use empty token list
    
    * build: trigger checks

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import { Currency, OnReviewSwapClick, SwapWidget, SwapWidgetSkeleton } from '@uniswap/widgets'
+import { Currency, EMPTY_TOKEN_LIST, OnReviewSwapClick, SwapWidget, SwapWidgetSkeleton } from '@uniswap/widgets'
 import { useWeb3React } from '@web3-react/core'
 import { useActiveLocale } from 'hooks/useActiveLocale'
 import { useIsDarkMode } from 'state/user/hooks'
@@ -39,6 +39,7 @@ export default function Widget({ defaultToken, onReviewSwapClick }: WidgetProps)
         onReviewSwapClick={onReviewSwapClick}
         // defaultChainId is excluded - it is always inferred from the passed provider
         provider={provider}
+        tokenList={EMPTY_TOKEN_LIST} // prevents loading the default token list, as we use our own token selector UI
         {...inputs}
         {...settings}
         {...transactions}
