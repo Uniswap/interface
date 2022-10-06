e0a90df801910c18082e2cebde13613fc9fc25c2
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-10-06 17:16:54 -0500

    fix(token-details): nits from design re: headers, spacing, layout

--- src/components/Tokens/TokenDetails/StatsSection.tsx
+++ src/components/Tokens/TokenDetails/StatsSection.tsx
@@ -1,6 +1,7 @@
 import { Trans } from '@lingui/macro'
 import { ReactNode } from 'react'
 import styled from 'styled-components/macro'
+import { ThemedText } from 'theme'
 import { textFadeIn } from 'theme/animations'
 import { formatDollarAmount } from 'utils/formatDollarAmt'
 
@@ -17,12 +18,14 @@ export const StatWrapper = styled.div`
 export const TokenStatsSection = styled.div`
   display: flex;
   flex-wrap: wrap;
-  ${textFadeIn}
 `
 export const StatPair = styled.div`
   display: flex;
   flex: 1;
   flex-wrap: wrap;
+`
+const Header = styled(ThemedText.MediumHeader)`
+  font-size: 28px !important;
 `
 const StatPrice = styled.span`
   font-size: 28px;
@@ -30,6 +33,10 @@ const StatPrice = styled.span`
 `
 const NoData = styled.div`
   color: ${({ theme }) => theme.textTertiary};
+`
+const Wrapper = styled.div`
+  gap: 16px;
+  ${textFadeIn}
 `
 
 type NumericStat = number | undefined | null
@@ -53,16 +60,21 @@ export default function StatsSection(props: StatsSectionProps) {
   const { priceLow52W, priceHigh52W, TVL, volume24H } = props
   if (TVL || volume24H || priceLow52W || priceHigh52W) {
     return (
-      <TokenStatsSection>
-        <StatPair>
-          <Stat value={TVL} title={<Trans>Total Value Locked</Trans>} />
-          <Stat value={volume24H} title={<Trans>24H volume</Trans>} />
-        </StatPair>
-        <StatPair>
-          <Stat value={priceLow52W} title={<Trans>52W low</Trans>} />
-          <Stat value={priceHigh52W} title={<Trans>52W high</Trans>} />
-        </StatPair>
-      </TokenStatsSection>
+      <Wrapper>
+        <Header>
+          <Trans>Stats</Trans>
+        </Header>
+        <TokenStatsSection>
+          <StatPair>
+            <Stat value={TVL} title={<Trans>Total Value Locked</Trans>} />
+            <Stat value={volume24H} title={<Trans>24H volume</Trans>} />
+          </StatPair>
+          <StatPair>
+            <Stat value={priceLow52W} title={<Trans>52W low</Trans>} />
+            <Stat value={priceHigh52W} title={<Trans>52W high</Trans>} />
+          </StatPair>
+        </TokenStatsSection>
+      </Wrapper>
     )
   } else {
     return <NoData>No stats available</NoData>
