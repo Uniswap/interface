6fc9b85b2bd8491d3ce03d5e7f550d7900246de3
Vignesh Mohankumar <me@vig.xyz>
2022-10-07 12:28:05 -0400

    use the data cy

--- cypress/e2e/link.test.ts
+++ cypress/e2e/link.test.ts
@@ -3,6 +3,6 @@ describe('Link', () => {
   it('should update route', () => {
     cy.visit('/')
     cy.contains('Pool').click()
-    cy.contains('Your active V3 liquidity positions will appear here.').should('exist')
+    cy.get('[data-cy="join-pool-button"]').should('exist')
   })
 })
