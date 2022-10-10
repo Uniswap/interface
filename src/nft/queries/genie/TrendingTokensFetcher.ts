370a24931674860df67f4a30be8613dffb5fd6a7
Zach Pomerantz <zzmp@uniswap.org>
2022-10-10 16:19:51 -0700

    fix: map wrapped to native for trending tokens

--- src/nft/queries/genie/TrendingTokensFetcher.ts
+++ src/nft/queries/genie/TrendingTokensFetcher.ts
@@ -1,6 +1,18 @@
+import { nativeOnChain, WRAPPED_NATIVE_CURRENCY } from 'constants/tokens'
+
 import { FungibleToken } from '../../types'
 
+function replaceWrappedWithNative(token: FungibleToken) {
+  const address = token.address.toLowerCase()
+  const nativeAddress = WRAPPED_NATIVE_CURRENCY[token.chainId]?.address.toLowerCase()
+  if (address !== nativeAddress) return token
+
+  const nativeToken = nativeOnChain(token.chainId)
+  return { ...token, ...nativeToken }
+}
+
 export const fetchTrendingTokens = async (numTokens?: number): Promise<FungibleToken[]> => {
+  // TODO: WETH->ETH
   const url = `${process.env.REACT_APP_TEMP_API_URL}/tokens/trending${numTokens ? `?numTokens=${numTokens}` : ''}`
 
   const r = await fetch(url, {
@@ -10,7 +22,6 @@ export const fetchTrendingTokens = async (numTokens?: number): Promise<FungibleT
     },
   })
 
-  const data = await r.json()
-
-  return data.data
+  const { data } = (await r.json()) as { data: FungibleToken[] }
+  return data.map(replaceWrappedWithNative)
 }
