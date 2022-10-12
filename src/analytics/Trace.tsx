ef3d664363b5032eea48ebe90b208cb67189197d
Zach Pomerantz <zzmp@uniswap.org>
2022-10-11 18:23:37 -0700

    feat: widget tracing

--- src/analytics/Trace.tsx
+++ src/analytics/Trace.tsx
@@ -20,6 +20,10 @@ export interface ITraceContext {
 
 export const TraceContext = createContext<ITraceContext>({})
 
+export function useTrace(): ITraceContext {
+  return useContext(TraceContext)
+}
+
 type TraceProps = {
   shouldLogImpression?: boolean // whether to log impression on mount
   name?: EventName
@@ -41,7 +45,7 @@ export const Trace = memo(
     element,
     properties,
   }: PropsWithChildren<TraceProps>) => {
-    const parentTrace = useContext(TraceContext)
+    const parentTrace = useTrace()
 
     const combinedProps = useMemo(
       () => ({
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -6,11 +6,12 @@ import { Currency, EMPTY_TOKEN_LIST, OnReviewSwapClick, SwapWidget, SwapWidgetSk
 import { useWeb3React } from '@web3-react/core'
 import { sendAnalyticsEvent } from 'analytics'
 import { EventName, SectionName } from 'analytics/constants'
+import { Trace, useTrace } from 'analytics/Trace'
 import { getTokenAddress } from 'analytics/utils'
 import { networkConnection } from 'connection'
 import { RPC_PROVIDERS } from 'constants/providers'
 import { useActiveLocale } from 'hooks/useActiveLocale'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { useIsDarkMode } from 'state/user/hooks'
 import { DARK_THEME, LIGHT_THEME } from 'theme/widget'
 
@@ -36,6 +37,9 @@ export default function Widget({ defaultToken, onReviewSwap }: WidgetProps) {
   const { settings } = useSyncWidgetSettings()
   const { transactions } = useSyncWidgetTransactions()
 
+  const parentTrace = useTrace()
+  const trace = useMemo(() => ({ ...parentTrace, section: SectionName.WIDGET }), [parentTrace])
+
   const onReviewSwapClick = useCallback(() => {
     // TODO(lynnshaoyu): Swap Confirm Modal Opened
     return onReviewSwap?.()
@@ -48,30 +52,32 @@ export default function Widget({ defaultToken, onReviewSwap }: WidgetProps) {
       chain_id: input.chainId,
       token_symbol: input.symbol,
       token_address: getTokenAddress(input),
-      section: SectionName.WIDGET,
+      ...trace,
     }
     sendAnalyticsEvent(EventName.APPROVE_TOKEN_TXN_SUBMITTED, eventProperties)
-  }, [inputs.value.INPUT])
+  }, [inputs.value.INPUT, trace])
 
   return (
     <>
-      <SwapWidget
-        disableBranding
-        hideConnectionUI
-        jsonRpcUrlMap={RPC_PROVIDERS}
-        routerUrl={WIDGET_ROUTER_URL}
-        width={WIDGET_WIDTH}
-        locale={locale}
-        theme={theme}
-        onReviewSwapClick={onReviewSwapClick}
-        // defaultChainId is excluded - it is always inferred from the passed provider
-        provider={connector === networkConnection.connector ? null : provider} // use jsonRpcUrlMap for network providers
-        tokenList={EMPTY_TOKEN_LIST} // prevents loading the default token list, as we use our own token selector UI
-        onSwapApprove={onSwapApprove}
-        {...inputs}
-        {...settings}
-        {...transactions}
-      />
+      <Trace section={SectionName.WIDGET}>
+        <SwapWidget
+          disableBranding
+          hideConnectionUI
+          jsonRpcUrlMap={RPC_PROVIDERS}
+          routerUrl={WIDGET_ROUTER_URL}
+          width={WIDGET_WIDTH}
+          locale={locale}
+          theme={theme}
+          onReviewSwapClick={onReviewSwapClick}
+          // defaultChainId is excluded - it is always inferred from the passed provider
+          provider={connector === networkConnection.connector ? null : provider} // use jsonRpcUrlMap for network providers
+          tokenList={EMPTY_TOKEN_LIST} // prevents loading the default token list, as we use our own token selector UI
+          onSwapApprove={onSwapApprove}
+          {...inputs}
+          {...settings}
+          {...transactions}
+        />
+      </Trace>
       {tokenSelector}
     </>
   )
