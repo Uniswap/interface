f2506f7ad03b5775d01d1111c4b287946320327d
Zach Pomerantz <zzmp@uniswap.org>
2022-10-11 23:53:51 -0700

    fix: only fetch token details decimals

--- src/pages/TokenDetails/index.tsx
+++ src/pages/TokenDetails/index.tsx
@@ -1,3 +1,4 @@
+import { Token } from '@uniswap/sdk-core'
 import { useWeb3React } from '@web3-react/core'
 import { filterTimeAtom } from 'components/Tokens/state'
 import { AboutSection } from 'components/Tokens/TokenDetails/About'
@@ -10,7 +11,7 @@ import StatsSection from 'components/Tokens/TokenDetails/StatsSection'
 import TokenSafetyMessage from 'components/TokenSafety/TokenSafetyMessage'
 import TokenSafetyModal from 'components/TokenSafety/TokenSafetyModal'
 import Widget, { WIDGET_WIDTH } from 'components/Widget'
-import { isCelo, NATIVE_CHAIN_ID, nativeOnChain } from 'constants/tokens'
+import { NATIVE_CHAIN_ID, nativeOnChain } from 'constants/tokens'
 import { checkWarning } from 'constants/tokenSafety'
 import { Chain } from 'graphql/data/__generated__/TokenQuery.graphql'
 import { useTokenQuery } from 'graphql/data/Token'
@@ -18,12 +19,13 @@ import { CHAIN_NAME_TO_CHAIN_ID, validateUrlChainParam } from 'graphql/data/util
 import { useIsUserAddedTokenOnChain } from 'hooks/Tokens'
 import { useOnGlobalChainSwitch } from 'hooks/useGlobalChainSwitch'
 import { useAtomValue } from 'jotai/utils'
-import { useTokenFromNetwork } from 'lib/hooks/useCurrency'
+import { useDecimals } from 'lib/hooks/useCurrency'
 import useCurrencyBalance, { useTokenBalance } from 'lib/hooks/useCurrencyBalance'
 import { useCallback, useMemo, useState } from 'react'
 import { ArrowLeft } from 'react-feather'
 import { useNavigate, useParams } from 'react-router-dom'
 import styled from 'styled-components/macro'
+import { isAddress } from 'utils'
 
 const Hr = styled.hr`
   background-color: ${({ theme }) => theme.textSecondary};
@@ -66,23 +68,38 @@ export const RightPanel = styled.div`
 `
 
 export default function TokenDetails() {
-  const { tokenAddress: tokenAddressParam, chainName } = useParams<{ tokenAddress?: string; chainName?: string }>()
+  const { tokenAddress, chainName } = useParams<{ tokenAddress?: string; chainName?: string }>()
   const { account } = useWeb3React()
   const currentChainName = validateUrlChainParam(chainName)
   const pageChainId = CHAIN_NAME_TO_CHAIN_ID[currentChainName]
   const nativeCurrency = nativeOnChain(pageChainId)
   const timePeriod = useAtomValue(filterTimeAtom)
-  const isNative = tokenAddressParam === NATIVE_CHAIN_ID
-  const tokenQueryAddress = isNative ? nativeCurrency.wrapped.address : tokenAddressParam
-  const [tokenQueryData, prices] = useTokenQuery(tokenQueryAddress ?? '', currentChainName, timePeriod)
+  const isNative = tokenAddress === NATIVE_CHAIN_ID
+  const [tokenQueryData, prices] = useTokenQuery(
+    isNative ? nativeCurrency.wrapped.address : tokenAddress ?? '',
+    currentChainName,
+    timePeriod
+  )
 
-  const pageToken = useTokenFromNetwork(tokenAddressParam, CHAIN_NAME_TO_CHAIN_ID[currentChainName])
+  const decimals = useDecimals(tokenAddress, pageChainId)
+  const token = useMemo(() => {
+    const address = isAddress(tokenAddress)
+    if (!address || !tokenQueryData || !decimals) return
+
+    return new Token(
+      pageChainId,
+      address,
+      decimals,
+      tokenQueryData.symbol ?? undefined,
+      tokenQueryData.name ?? undefined
+    )
+  }, [decimals, pageChainId, tokenAddress, tokenQueryData])
 
   const nativeCurrencyBalance = useCurrencyBalance(account, nativeCurrency)
 
-  const tokenBalance = useTokenBalance(account, isNative ? nativeCurrency.wrapped : pageToken ?? undefined)
+  const tokenBalance = useTokenBalance(account, isNative ? nativeCurrency.wrapped : token ?? undefined)
 
-  const tokenWarning = tokenAddressParam ? checkWarning(tokenAddressParam) : null
+  const tokenWarning = tokenAddress ? checkWarning(tokenAddress) : null
   const isBlockedToken = tokenWarning?.canProceed === false
 
   const navigate = useNavigate()
@@ -105,7 +122,7 @@ export default function TokenDetails() {
 
   const [continueSwap, setContinueSwap] = useState<{ resolve: (value: boolean | PromiseLike<boolean>) => void }>()
 
-  const shouldShowSpeedbump = !useIsUserAddedTokenOnChain(tokenAddressParam, pageChainId) && tokenWarning !== null
+  const shouldShowSpeedbump = !useIsUserAddedTokenOnChain(tokenAddress, pageChainId) && tokenWarning !== null
   // Show token safety modal if Swap-reviewing a warning token, at all times if the current token is blocked
   const onReviewSwap = useCallback(
     () => new Promise<boolean>((resolve) => (shouldShowSpeedbump ? setContinueSwap({ resolve }) : resolve(true))),
@@ -119,17 +136,6 @@ export default function TokenDetails() {
     },
     [continueSwap, setContinueSwap]
   )
-
-  const widgetToken = useMemo(() => {
-    if (pageToken) {
-      return pageToken
-    }
-    if (nativeCurrency) {
-      if (isCelo(pageChainId)) return undefined
-      return nativeCurrency
-    }
-    return undefined
-  }, [nativeCurrency, pageChainId, pageToken])
 
   return (
     <TokenDetailsLayout>
@@ -160,7 +166,7 @@ export default function TokenDetails() {
             <AddressSection address={tokenQueryData.address ?? ''} />
           </LeftPanel>
           <RightPanel>
-            <Widget defaultToken={widgetToken} onReviewSwapClick={onReviewSwap} />
+            <Widget defaultToken={token ?? nativeCurrency} onReviewSwapClick={onReviewSwap} />
             {tokenWarning && <TokenSafetyMessage tokenAddress={tokenQueryData.address ?? ''} warning={tokenWarning} />}
             <BalanceSummary
               tokenAmount={tokenBalance}
