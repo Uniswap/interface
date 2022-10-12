6dd213e0c1283ffd82024d9026ab81dc77adeaf5
Charles Bachmeier <charlie@genie.xyz>
2022-10-12 15:13:50 -0700

    add styles for listed cards

--- src/nft/components/profile/view/WalletAssetDisplay.tsx
+++ src/nft/components/profile/view/WalletAssetDisplay.tsx
@@ -1,10 +1,10 @@
 import { Box } from 'nft/components/Box'
-import { Column } from 'nft/components/Flex'
+import { Column, Row } from 'nft/components/Flex'
 import { VerifiedIcon } from 'nft/components/icons'
-import { bodySmall, buttonTextSmall, subheadSmall } from 'nft/css/common.css'
+import { bodySmall, buttonTextSmall, subhead } from 'nft/css/common.css'
 import { useBag, useIsMobile, useSellAsset } from 'nft/hooks'
 import { DetailsOrigin, WalletAsset } from 'nft/types'
-import { getAssetHref } from 'nft/utils'
+import { formatEth, getAssetHref } from 'nft/utils'
 import { useMemo, useReducer } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -41,11 +41,19 @@ export const WalletAssetDisplay = ({ asset, isSellMode }: { asset: WalletAsset; 
       toggleCart()
   }
 
+  const uniqueSellOrdersMarketplaces = useMemo(
+    () => [...new Set(asset.sellOrders.map((order) => order.marketplace))],
+    [asset.sellOrders]
+  )
+
   return (
     <Link to={getAssetHref(asset, DetailsOrigin.PROFILE)} style={{ textDecoration: 'none' }}>
       <Column
-        // color={'textPrimary'}
-        // className={subheadSmall}
+        borderBottomLeftRadius="20"
+        borderBottomRightRadius="20"
+        paddingBottom="20"
+        transition="250"
+        backgroundColor={boxHovered ? 'backgroundOutline' : 'backgroundSurface'}
         onMouseEnter={toggleBoxHovered}
         onMouseLeave={toggleBoxHovered}
       >
@@ -58,62 +66,47 @@ export const WalletAssetDisplay = ({ asset, isSellMode }: { asset: WalletAsset; 
           src={asset.image_url ?? '/nft/svgs/image-placeholder.svg'}
           style={{ aspectRatio: '1' }}
         />
-        <Column
-          position="relative"
-          borderBottomLeftRadius="20"
-          borderBottomRightRadius="20"
-          transition="250"
-          backgroundColor={boxHovered ? 'backgroundOutline' : 'backgroundSurface'}
-          paddingTop="12"
-          paddingBottom="20"
-          paddingX="12"
-        >
-          <Box
-            className={subheadSmall}
-            color="textPrimary"
-            overflow="hidden"
-            textOverflow="ellipsis"
-            whiteSpace="nowrap"
-          >
-            {asset.name ? asset.name : `#${asset.tokenId}`}
-          </Box>
-          <Box
-            className={bodySmall}
-            color="textSecondary"
-            marginTop="4"
-            overflow="hidden"
-            textOverflow="ellipsis"
-            whiteSpace="nowrap"
-          >
-            {asset.collection?.name}
-            {asset.collectionIsVerified ? <VerifiedIcon className={styles.verifiedBadge} /> : null}
-          </Box>
-          {/* <Box as="span" fontSize="12" lineHeight="16" color="textSecondary" marginTop="8">
-            Last:&nbsp;
-            {asset.lastPrice ? (
-              <>
-                {formatEth(asset.lastPrice)}
-                &nbsp;ETH
-              </>
-            ) : (
-              <Box as="span" marginLeft="6">
-                &mdash;
+        <Column paddingTop="12" paddingX="12">
+          <Row>
+            <Column flex="2" gap="4" whiteSpace="nowrap" style={{ maxWidth: '67%' }}>
+              <Box className={subhead} color="textPrimary" overflow="hidden" textOverflow="ellipsis">
+                {asset.name ? asset.name : `#${asset.tokenId}`}
               </Box>
-            )}
-          </Box>
-          <Box as="span" fontSize="12" lineHeight="16" color="textSecondary" marginTop="4">
-            Floor:&nbsp;
-            {asset.floorPrice ? (
-              <>
-                {formatEth(asset.floorPrice)}
-                &nbsp;ETH
-              </>
-            ) : (
-              <Box as="span" marginLeft="8">
-                &mdash;
+              <Box className={bodySmall} color="textSecondary" overflow="hidden" textOverflow="ellipsis">
+                {asset.collection?.name}
+                {asset.collectionIsVerified ? <VerifiedIcon className={styles.verifiedBadge} /> : null}
               </Box>
+            </Column>
+            {asset.sellOrders.length > 0 && (
+              <Column gap="6" flex="1" justifyContent="flex-end" whiteSpace="nowrap" style={{ maxWidth: '33%' }}>
+                <>
+                  <Row className={subhead} color="textPrimary">
+                    <Box width="full" textAlign="right" overflow="hidden" textOverflow="ellipsis">
+                      {formatEth(asset.floor_sell_order_price)}
+                    </Box>
+                    &nbsp;ETH
+                  </Row>
+                  <Row justifyContent="flex-end">
+                    {uniqueSellOrdersMarketplaces.map((market, index) => {
+                      return (
+                        <Box
+                          as="img"
+                          key={index}
+                          alt={market}
+                          width="16"
+                          height="16"
+                          borderRadius="4"
+                          marginLeft="4"
+                          objectFit="cover"
+                          src={`/nft/svgs/marketplaces/${market}.svg`}
+                        />
+                      )
+                    })}
+                  </Row>
+                </>
+              </Column>
             )}
-          </Box> */}
+          </Row>
           {isSellMode && (
             <Box
               marginTop="12"
