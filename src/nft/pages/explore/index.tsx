9370383f64c1e5bf4965486e9a38bd04c31a1bdd
Greg Bugyis <gbugyis@users.noreply.github.com>
2022-10-13 17:35:56 +0300

    feat: Remove value prop from NFT Explore (#4914)

--- src/nft/pages/explore/index.tsx
+++ src/nft/pages/explore/index.tsx
@@ -1,12 +1,10 @@
 import Banner from 'nft/components/explore/Banner'
 import TrendingCollections from 'nft/components/explore/TrendingCollections'
-import ValueProp from 'nft/components/explore/ValueProp'
 
 const NftExplore = () => {
   return (
     <>
       <Banner />
-      <ValueProp />
       <TrendingCollections />
     </>
   )
