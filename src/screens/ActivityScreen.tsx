5cc7140811875d8642b45ff791eb96fb8eee8777
Ian Lapham <ianlapham@gmail.com>
2022-10-17 12:55:13 -0400

    fix: remove scan on activity  (#1933)
    * remove scan
    
    * remove util on activity screen
    
    * use header component
    
    * remove memo

--- src/screens/ActivityScreen.tsx
+++ src/screens/ActivityScreen.tsx
@@ -1,25 +1,18 @@
 import { graphql } from 'babel-plugin-relay/macro'
-import { selectionAsync } from 'expo-haptics'
 import React, { Suspense, useCallback, useEffect, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { PreloadedQuery, usePreloadedQuery } from 'react-relay'
-import { useAppDispatch, useAppTheme } from 'src/app/hooks'
+import { useAppDispatch } from 'src/app/hooks'
 import { AppStackScreenProp, useAppStackNavigation } from 'src/app/navigation/types'
-import ScanQRIcon from 'src/assets/icons/scan-qr.svg'
 import { AddressDisplay } from 'src/components/AddressDisplay'
-import { BackButton } from 'src/components/buttons/BackButton'
-import { Button } from 'src/components/buttons/Button'
-import { PrimaryButton } from 'src/components/buttons/PrimaryButton'
 import { Flex } from 'src/components/layout'
+import { BackHeader } from 'src/components/layout/BackHeader'
 import { HeaderScrollScreen } from 'src/components/layout/screens/HeaderScrollScreen'
 import { Loading } from 'src/components/loading'
-import { ScannerModalState } from 'src/components/QRCodeScanner/constants'
 import { Text } from 'src/components/Text'
 import TransactionList from 'src/components/TransactionList/TransactionList'
 import SessionsButton from 'src/components/WalletConnect/SessionsButton'
-import { openModal } from 'src/features/modals/modalSlice'
 import { clearNotificationCount } from 'src/features/notifications/notificationSlice'
-import { ElementName, ModalName } from 'src/features/telemetry/constants'
 import { parseDataResponseToTransactionDetails } from 'src/features/transactions/history/utils'
 import { useMergeLocalAndRemoteTransactions } from 'src/features/transactions/hooks'
 import { AccountType } from 'src/features/wallet/accounts/types'
@@ -123,7 +116,6 @@ export function Activity({
 }: {
   preloadedQuery: PreloadedQuery<ActivityScreenQuery>
 }) {
-  const theme = useAppTheme()
   const dispatch = useAppDispatch()
   const navigation = useAppStackNavigation()
   const { t } = useTranslation()
@@ -144,16 +136,6 @@ export function Activity({
 
   const { sessions } = useWalletConnect(address)
 
-  const onPressScan = useCallback(() => {
-    selectionAsync()
-    dispatch(
-      openModal({
-        name: ModalName.WalletConnectScan,
-        initialState: ScannerModalState.WalletQr,
-      })
-    )
-  }, [dispatch])
-
   const onPressSessions = useCallback(() => {
     if (address) {
       navigation.navigate(Screens.SettingsWalletManageConnection, { address })
@@ -164,22 +146,13 @@ export function Activity({
     dispatch(clearNotificationCount({ address }))
   }, [dispatch, address])
 
-  const ContentHeader = useMemo(
-    () => (
-      <Flex row alignItems="center" justifyContent="space-between" p="md">
-        <BackButton size={theme.iconSizes.lg} />
-        <Text variant="mediumLabel">{t('Activity')}</Text>
-        <Button name={ElementName.WalletConnectScan} onPress={onPressScan}>
-          <ScanQRIcon color={theme.colors.textSecondary} height={24} width={24} />
-        </Button>
-      </Flex>
-    ),
-    [onPressScan, t, theme.colors.textSecondary, theme.iconSizes.lg]
-  )
-
   return (
     <HeaderScrollScreen
-      contentHeader={ContentHeader}
+      contentHeader={
+        <BackHeader p="md">
+          <Text variant="mediumLabel">{t('Activity')}</Text>
+        </BackHeader>
+      }
       fixedHeader={
         <Flex centered>
           <AddressDisplay address={address} variant="subhead" />
@@ -195,24 +168,12 @@ export function Activity({
         <TransactionList
           emptyStateContent={
             <Flex centered gap="xxl" mt="xl" mx="xl">
-              <Flex centered gap="xs">
-                <Text variant="subhead">{t('No activity yet')}</Text>
-                <Text color="textSecondary" variant="bodySmall">
-                  {t(
-                    'When you make transactions or interact with sites, details of your activity will appear here.'
-                  )}
-                </Text>
-              </Flex>
-              <Flex row>
-                {/* TODO: Add a buy button when fiat OR is implemented */}
-                <PrimaryButton
-                  borderRadius="md"
-                  icon={<ScanQRIcon color={theme.colors.textPrimary} height={20} width={20} />}
-                  label={t('Receive')}
-                  variant="transparent"
-                  onPress={onPressScan}
-                />
-              </Flex>
+              <Text variant="subhead">{t('No activity yet')}</Text>
+              <Text color="textSecondary" variant="bodySmall">
+                {t(
+                  'When you make transactions or interact with sites, details of your activity will appear here.'
+                )}
+              </Text>
             </Flex>
           }
           readonly={readonly}
