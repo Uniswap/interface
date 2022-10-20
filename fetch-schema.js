003035dd5f827d30aab6fa85e833025e47e1c484
Charles Bachmeier <charlie@genie.xyz>
2022-10-20 15:58:41 -0700

    fetch schema from main endpoint

--- fetch-schema.js
+++ fetch-schema.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-require('dotenv').config({ path: '.env.local' })
+require('dotenv').config({ path: '.env.production' })
 const { exec } = require('child_process')
 const dataConfig = require('./relay.config')
 const thegraphConfig = require('./relay_thegraph.config')
@@ -8,7 +8,7 @@ const thegraphConfig = require('./relay_thegraph.config')
 const THEGRAPH_API_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
 exec(`get-graphql-schema ${THEGRAPH_API_URL} > ${thegraphConfig.schema}`)
 
-console.log(process.env.REACT_APP_NFT_AWS_API_ENDPOINT)
+console.log(process.env.REACT_APP_AWS_API_ENDPOINT)
 exec(
-  `get-graphql-schema --h Origin=https://app.uniswap.org --h x-api-key=${process.env.REACT_APP_NFT_AWS_X_API_KEY} ${process.env.REACT_APP_NFT_AWS_API_ENDPOINT} > ${dataConfig.schema}`
+  `get-graphql-schema --h Origin=https://app.uniswap.org ${process.env.REACT_APP_AWS_API_ENDPOINT} > ${dataConfig.schema}`
 )
