a961d5b2d1068956d376233dd0789a2c449717d1
Nam Nguyen <nam.nguyen@kyber.network>
2022-10-20 15:57:35 +0700

    feat: RUM sample rate 10 (#1421)
    * feat: sample rate 10 RUM
    
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>
    
    * update version
    
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>
    
    Signed-off-by: Nam Nguyen <nam.nguyen@kyber.network>

--- src/index.tsx
+++ src/index.tsx
@@ -31,23 +31,24 @@ import UserUpdater from './state/user/updater'
 import ThemeProvider, { FixedGlobalStyle, ThemedGlobalStyle } from './theme'
 import getLibrary from './utils/getLibrary'
 
-datadogRum.init({
-  applicationId: '5bd0c243-6141-4bab-be21-5dac9b9efa9f',
-  clientToken: 'pub9163f29b2cdb31314b89ae232af37d5a',
-  site: 'datadoghq.eu',
-  service: 'kyberswap-interface',
+if (process.env.REACT_APP_TAG) {
+  datadogRum.init({
+    applicationId: '5bd0c243-6141-4bab-be21-5dac9b9efa9f',
+    clientToken: 'pub9163f29b2cdb31314b89ae232af37d5a',
+    site: 'datadoghq.eu',
+    service: 'kyberswap-interface',
 
-  // Specify a version number to identify the deployed version of your application in Datadog
-  // version: '1.0.0',
-  sampleRate: 100,
-  sessionReplaySampleRate: 20,
-  trackInteractions: true,
-  trackResources: true,
-  trackLongTasks: true,
-  defaultPrivacyLevel: 'mask-user-input',
-})
+    version: process.env.REACT_APP_TAG,
+    sampleRate: 10,
+    sessionReplaySampleRate: 100,
+    trackInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+  })
 
-datadogRum.startSessionReplayRecording()
+  datadogRum.startSessionReplayRecording()
+}
 
 AOS.init()
 
