f296a0c507d72200d401d3d4102e28dc3968caf1
cartcrom <cartergcromer@gmail.com>
2022-10-27 14:57:43 -0400

    cleaned up unecessary code

--- src/graphql/data/TokenPrice.ts
+++ src/graphql/data/TokenPrice.ts
@@ -20,14 +20,13 @@ export const tokenPriceQuery = graphql`
     }
   }
 `
-// export type PricePoint = { timestamp: number; value: number }
 export type PricePoint = NonNullable<
   NonNullable<NonNullable<NonNullable<TokenPriceQuery['response']['tokens']>[number]>['market']['priceHistory']>[number]
 >
 export type PriceDurations = Partial<Record<TimePeriod, PricePoint[]>>
 
 export function isPricePoint(p: { timestamp: number; value: number } | null): p is PricePoint {
-  return Boolean(p)
+  return p !== null
 }
 
 export function useLoadTokenPriceQuery(address: string, chain: Chain, timePeriod: TimePeriod) {
@@ -46,13 +45,11 @@ export function usePreloadedTokenPriceQuery(queryReference: PreloadedQuery<Token
   const queryData = usePreloadedQuery<TokenPriceQuery>(tokenPriceQuery, queryReference)
 
   const priceHistory = useMemo(() => {
-    let priceHistory = queryData.tokens?.[0]?.market?.priceHistory?.filter(isPricePoint)
+    const priceHistory = queryData.tokens?.[0]?.market?.priceHistory?.filter(isPricePoint)
     const currentPrice = queryData.tokens?.[0]?.market?.price?.value
 
     // Append the current price to the end of the priceHistory array
     if (currentPrice !== undefined && priceHistory && priceHistory.length > 0) {
-      // If we can append the current price, make pricehistory array not readonly
-      priceHistory = Array.from(priceHistory)
       const timestamp = Date.now() / 1000
       priceHistory.push({ timestamp, value: currentPrice })
     }
@@ -61,44 +58,3 @@ export function usePreloadedTokenPriceQuery(queryReference: PreloadedQuery<Token
 
   return priceHistory
 }
-
-// export function useTokenPriceQuery(address: string, chain: Chain): PriceDurations | undefined {
-//   const contract = useMemo(() => ({ address: address.toLowerCase(), chain }), [address, chain])
-//   const [prices, setPrices] = useState<PriceDurations>()
-
-//   useEffect(() => {
-//     const subscription = fetchQuery<TokenPriceQuery>(environment, tokenPriceQuery, { contract }).subscribe({
-//       next: (response: TokenPriceQuery['response']) => {
-//         const priceData = response.tokens?.[0]?.market
-//         const prices = {
-//           [TimePeriod.HOUR]: priceData?.priceHistory1H?.filter(isPricePoint),
-//           [TimePeriod.DAY]: priceData?.priceHistory1D?.filter(isPricePoint),
-//           [TimePeriod.WEEK]: priceData?.priceHistory1W?.filter(isPricePoint),
-//           [TimePeriod.MONTH]: priceData?.priceHistory1M?.filter(isPricePoint),
-//           [TimePeriod.YEAR]: priceData?.priceHistory1Y?.filter(isPricePoint),
-//         }
-
-//         // Ensure the latest price available is available for every TimePeriod.
-//         const latests = Object.values(prices)
-//           .map((prices) => prices?.slice(-1)?.[0] ?? null)
-//           .filter(isPricePoint)
-//         if (latests.length) {
-//           const latest = latests.reduce((latest, pricePoint) =>
-//             latest.timestamp > pricePoint.timestamp ? latest : pricePoint
-//           )
-//           Object.values(prices)
-//             .filter((prices) => prices && prices.slice(-1)[0] !== latest)
-//             .forEach((prices) => prices?.push(latest))
-//         }
-
-//         setPrices(prices)
-//       },
-//     })
-//     return () => {
-//       setPrices(undefined)
-//       subscription.unsubscribe()
-//     }
-//   }, [contract])
-
-//   return prices
-// }
