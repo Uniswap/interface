06da3453b9b47eb9450230c668c3e816a542f897
Charles Bachmeier <charlie@genie.xyz>
2022-10-26 23:16:33 -0700

    add back in suspense wrapper

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -224,7 +224,14 @@ export default function App() {
                       <Route path="/profile" element={<Profile />} />
                       <Route path="/nfts" element={<NftExplore />} />
                       <Route path="/nfts/asset/:contractAddress/:tokenId" element={<Asset />} />
-                      <Route path="/nfts/collection/:contractAddress" element={<CollectionPageLoading />} />
+                      <Route
+                        path="/nfts/collection/:contractAddress"
+                        element={
+                          <Suspense fallback={<CollectionPageLoading />}>
+                            <Collection />
+                          </Suspense>
+                        }
+                      />
                       <Route
                         path="/nfts/collection/:contractAddress/activity"
                         element={
