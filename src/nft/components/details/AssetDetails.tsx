3b5b25977e3b776a52988b1306481dac9d9f9270
Alex Ball <alex.ball@uniswap.org>
2022-10-28 13:50:21 -0500

    minor changes

--- src/nft/components/details/AssetDetails.tsx
+++ src/nft/components/details/AssetDetails.tsx
@@ -386,8 +386,6 @@ export const AssetDetails = ({ asset, collection }: AssetDetailsProps) => {
 
   const rarity = asset.rarity ? asset.rarity?.providers[0].rank : undefined
 
-  console.log(asset)
-
   return (
     <Column>
       <MediaContainer>
