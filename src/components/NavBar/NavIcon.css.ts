a1007c43e55f4b51919e8fc88c9c204934cdba29
Lynn Yu <lynn.yu@uniswap.org>
2022-10-31 17:04:56 -0400

    init

--- src/components/NavBar/NavIcon.css.ts
+++ src/components/NavBar/NavIcon.css.ts
@@ -11,7 +11,6 @@ export const navIcon = style([
     justifyContent: 'center',
     textAlign: 'center',
     cursor: 'pointer',
-    padding: '10',
     borderRadius: '8',
     transition: '250',
   }),
