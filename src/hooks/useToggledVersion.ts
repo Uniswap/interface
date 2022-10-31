b79634ba7e706457f4aab58ef6ec522cb1d6190d
nutz <nutz>
2022-10-31 15:16:14 -0500

    arbitrum

--- src/hooks/useToggledVersion.ts
+++ src/hooks/useToggledVersion.ts
@@ -1,5 +1,6 @@
+import {SupportedChainId} from '../constants/chains'
+import { useActiveWeb3React } from './web3'
 import useParsedQueryString from './useParsedQueryString'
-
 export enum Version {
   v2 = 'V2',
   v3 = 'V3',
@@ -8,9 +9,14 @@ export enum Version {
 export const DEFAULT_VERSION: Version = Version.v2
 
 export default function useToggledVersion(): Version {
+  const { chainId } = useActiveWeb3React()
   const { use } = useParsedQueryString()
   if (typeof use !== 'string') {
-    return DEFAULT_VERSION
+    return chainId && chainId === 1 ? 
+      DEFAULT_VERSION 
+      : chainId === SupportedChainId.ARBITRUM_ONE 
+      ? Version.v3 
+      : DEFAULT_VERSION
   }
   switch (use.toLowerCase()) {
     case 'v2':
