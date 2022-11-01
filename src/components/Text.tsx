75064dd6219d26bf19301a2a25c627f372eb764d
Daniel James <danielcolinjames@gmail.com>
2022-11-01 13:35:03 -0400

    chore: Make reusable Text loader (#2162)
    * Add loader prop to Text component
    
    * Update comment
    
    * Address feedback
    
    * Use Box instead of Flex
    
    * Fix Text types

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,8 @@
 import { createText, useResponsiveProp } from '@shopify/restyle'
-import React, { ComponentProps } from 'react'
+import React, { ComponentProps, PropsWithChildren } from 'react'
 import Animated from 'react-native-reanimated'
+import { Box } from 'src/components/layout'
+import { HiddenFromScreenReaders } from 'src/components/text/HiddenFromScreenReaders'
 import { textVariants } from 'src/styles/font'
 import { Theme } from 'src/styles/theme'
 
@@ -8,7 +10,8 @@ type TextProps = ComponentProps<typeof ThemedText> & {
   maxFontSizeMultiplier?: number
   animated?: boolean
   noTextScaling?: boolean
-}
+  loaderOnly?: boolean
+} & Pick<ComponentProps<typeof Box>, 'height' | 'width'>
 
 const NO_TEXT_SCALING_MULTIPLIER = 1
 
@@ -17,14 +20,51 @@ const NO_TEXT_SCALING_MULTIPLIER = 1
 const ThemedText = createText<Theme>()
 const ThemedAnimatedText = createText<Theme>(Animated.Text)
 
+const TextLoaderWrapper = ({
+  children,
+  height = '90%',
+  width = 'auto',
+}: PropsWithChildren<TextProps>) => {
+  return (
+    <Box alignItems="center" flexDirection="row">
+      <Box
+        bg="background3"
+        borderRadius="xs"
+        // 90% height (or anything less than 100%) plus the wrapping flex row allow us to get the loader to take up exactly the same amount of space as the text, without looking visually weird when the final text layout doesn't have any gap between two vertically stacked text boxes, e.g. header and body (otherwise the loaders would just be two boxes with no gap between them, OR the loader would take up more space than the final layout if we add a gap)
+        height={height}
+        width={width}>
+        <HiddenFromScreenReaders>{children}</HiddenFromScreenReaders>
+      </Box>
+    </Box>
+  )
+}
+
 // Wrap themed restyle text component with RN Text component to add support for maxFontSizeMultiplier prop so app is still usable with large text
-export const Text = ({ animated, maxFontSizeMultiplier, noTextScaling, ...rest }: TextProps) => {
+export const Text = ({
+  animated,
+  loaderOnly,
+  maxFontSizeMultiplier,
+  noTextScaling,
+  height,
+  width,
+  ...rest
+}: TextProps) => {
   const variant = useResponsiveProp(rest.variant ?? 'bodySmall') as keyof typeof textVariants
   const multiplier = noTextScaling
     ? NO_TEXT_SCALING_MULTIPLIER
     : maxFontSizeMultiplier ?? textVariants[variant].maxFontSizeMultiplier
+
   if (animated) {
     return <ThemedAnimatedText maxFontSizeMultiplier={multiplier} {...rest} />
   }
+
+  if (loaderOnly) {
+    return (
+      <TextLoaderWrapper height={height} width={width}>
+        <ThemedText color="none" maxFontSizeMultiplier={multiplier} opacity={0} {...rest} />
+      </TextLoaderWrapper>
+    )
+  }
+
   return <ThemedText maxFontSizeMultiplier={multiplier} {...rest} />
 }
--- src/components/loading/WalletLoader.tsx
+++ src/components/loading/WalletLoader.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Box, Flex } from 'src/components/layout'
 import { Text } from 'src/components/Text'
-import { HiddenFromScreenReaders } from 'src/components/text/HiddenFromScreenReaders'
 import { ADDRESS_WRAPPER_HEIGHT } from 'src/features/import/WalletPreviewCard'
 
 interface Props {
@@ -24,25 +23,12 @@ export function WalletLoader({ opacity }: Props) {
       <Flex row alignItems="center" gap="sm" height={ADDRESS_WRAPPER_HEIGHT}>
         <Box bg="background3" borderRadius="full" height={32} width={32} />
         <Flex alignItems="flex-start" gap="none" width="100%">
-          <Flex row alignItems="center" width="100%">
-            {/* 90% height and the wrapping flex row allow us to get the box to take up exactly the same amount of space as the text, without looking visually weird when the final text layout doesn't have any gap between the header and body text (otherwise it would just be two boxes with no gap between them, OR it would take up more space than the final layout if we add a gap) */}
-            <Box bg="background3" borderRadius="xs" height="90%" width="35%">
-              <HiddenFromScreenReaders>
-                <Text color="none" opacity={0} variant="bodyLarge">
-                  Wallet Nickname
-                </Text>
-              </HiddenFromScreenReaders>
-            </Box>
-          </Flex>
-          <Flex row alignItems="center" width="100%">
-            <Box bg="background3" borderRadius="xs" height="90%" width="25%">
-              <HiddenFromScreenReaders>
-                <Text color="none" opacity={0} variant="subheadSmall">
-                  Wallet Address
-                </Text>
-              </HiddenFromScreenReaders>
-            </Box>
-          </Flex>
+          <Text loaderOnly variant="bodyLarge">
+            Wallet Nickname
+          </Text>
+          <Text loaderOnly variant="subheadSmall">
+            Wallet Address
+          </Text>
         </Flex>
       </Flex>
     </Flex>
