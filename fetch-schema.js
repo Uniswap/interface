9de0ec7c8e2a2e25d6a54012f3b82be253b613aa
Charles Bachmeier <charlie@genie.xyz>
2022-11-01 15:00:10 -0700

    add comment

--- fetch-schema.js
+++ fetch-schema.js
@@ -8,7 +8,7 @@ const thegraphConfig = require('./relay_thegraph.config')
 const THEGRAPH_API_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
 exec(`get-graphql-schema ${THEGRAPH_API_URL} > ${thegraphConfig.schema}`)
 
-console.log(process.env.REACT_APP_NFT_AWS_API_ENDPOINT)
+console.log(process.env.REACT_APP_NFT_AWS_API_ENDPOINT) // TODO: undo when prod endpoint is updated
 exec(
   `get-graphql-schema --h Origin=https://app.uniswap.org --h x-api-key=${process.env.REACT_APP_NFT_AWS_X_API_KEY} ${process.env.REACT_APP_NFT_AWS_API_ENDPOINT} > ${dataConfig.schema}`
 )
