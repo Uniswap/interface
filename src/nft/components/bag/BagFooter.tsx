689caa31dcb950039e9218cbcab8f7959b619564
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-11-02 16:51:39 -0500

    fix: remove border-top-x-radius

--- src/nft/components/bag/BagFooter.tsx
+++ src/nft/components/bag/BagFooter.tsx
@@ -16,7 +16,7 @@ import { ThemedText } from 'theme'
 
 import * as styles from './BagFooter.css'
 
-const Footer = styled.div<{ $showWarning: boolean }>`
+const Footer = styled.div`
   border-top: 1px solid ${({ theme }) => theme.backgroundOutline};
   color: ${({ theme }) => theme.textPrimary};
   display: flex;
@@ -24,8 +24,6 @@ const Footer = styled.div<{ $showWarning: boolean }>`
   padding: 12px 16px;
   border-bottom-left-radius: 12px;
   border-bottom-right-radius: 12px;
-  border-top-left-radius: ${({ $showWarning }) => ($showWarning ? '0' : '12')}px;
-  border-top-right-radius: ${({ $showWarning }) => ($showWarning ? '0' : '12')}px;
 `
 
 const WarningIcon = styled(AlertTriangle)`
@@ -80,7 +78,7 @@ export const BagFooter = ({
 
   return (
     <Column className={styles.footerContainer}>
-      <Footer $showWarning={showWarning}>
+      <Footer>
         <Column gap="4" paddingTop="8" paddingBottom="20">
           <Row justifyContent="space-between">
             <Box>
