b7297552efc7ca8470042733ab172afba3c65517
Alex Ball <alex.ball@uniswap.org>
2022-11-03 14:10:38 -0600

    reverting change

--- src/nft/utils/putCommas.ts
+++ src/nft/utils/putCommas.ts
@@ -1,7 +1,7 @@
 export const putCommas = (value: number) => {
   try {
     if (!value) return value
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') ?? ''
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   } catch (err) {
     return value
   }
