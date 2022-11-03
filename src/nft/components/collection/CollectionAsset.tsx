bbb048ecaa045159ce4585a7c5ef2554f24420aa
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-11-03 10:20:13 -0500

    feat(bag): add check if the user has manually closed the bag, and don't reopen on asset add until bag is cleared

--- src/nft/components/collection/CollectionAsset.tsx
+++ src/nft/components/collection/CollectionAsset.tsx
@@ -5,7 +5,7 @@ import { useTrace } from 'analytics/Trace'
 import { useBag } from 'nft/hooks'
 import { GenieAsset, Markets, UniformHeight } from 'nft/types'
 import { formatWeiToDecimal, isAudio, isVideo, rarityProviderLogo } from 'nft/utils'
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 
 import * as Card from './Card'
 
@@ -34,6 +34,7 @@ export const CollectionAsset = ({
   setCurrentTokenPlayingMedia,
   rarityVerified,
 }: CollectionAssetProps) => {
+  const bagManuallyClosed = useBag((state) => state.bagManuallyClosed)
   const addAssetsToBag = useBag((state) => state.addAssetsToBag)
   const removeAssetsFromBag = useBag((state) => state.removeAssetsFromBag)
   const itemsInBag = useBag((state) => state.itemsInBag)
@@ -76,25 +77,29 @@ export const CollectionAsset = ({
     }
   }, [asset])
 
-  const eventProperties = {
-    collection_address: asset.address,
-    token_id: asset.tokenId,
-    token_type: asset.tokenType,
-    ...trace,
-  }
+  const handleAddAssetToBag = useCallback(() => {
+    addAssetsToBag([asset])
+    if (!bagExpanded && !isMobile && !bagManuallyClosed) {
+      toggleBag()
+    }
+    sendAnalyticsEvent(EventName.NFT_BUY_ADDED, {
+      collection_address: asset.address,
+      token_id: asset.tokenId,
+      token_type: asset.tokenType,
+      ...trace,
+    })
+  }, [addAssetsToBag, asset, bagExpanded, bagManuallyClosed, isMobile, toggleBag, trace])
+
+  const handleRemoveAssetFromBag = useCallback(() => {
+    removeAssetsFromBag([asset])
+  }, [asset, removeAssetsFromBag])
 
   return (
     <Card.Container
       asset={asset}
       selected={isSelected}
-      addAssetToBag={() => {
-        addAssetsToBag([asset])
-        !bagExpanded && !isMobile && toggleBag()
-        sendAnalyticsEvent(EventName.NFT_BUY_ADDED, { ...eventProperties })
-      }}
-      removeAssetFromBag={() => {
-        removeAssetsFromBag([asset])
-      }}
+      addAssetToBag={handleAddAssetToBag}
+      removeAssetFromBag={handleRemoveAssetFromBag}
     >
       <Card.ImageContainer>
         {asset.tokenType === 'ERC1155' && quantity > 0 && <Card.Erc1155Controls quantity={quantity.toString()} />}
