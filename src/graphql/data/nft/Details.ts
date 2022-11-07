b2481d6ba8914e00856a6f58695496176f8bc860
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-11-07 10:22:03 -0600

    fix(asset-details): if an asset has no listings, don't try to render them (#5088)
    * fix(asset-details): if an asset has no listings, don't try to render them
    
    * add todo

--- src/graphql/data/nft/Details.ts
+++ src/graphql/data/nft/Details.ts
@@ -96,7 +96,8 @@ export function useDetailsQuery(address: string, tokenId: string): [GenieAsset, 
 
   const asset = queryData.nftAssets?.edges[0]?.node
   const collection = asset?.collection
-  const ethPrice = parseEther(asset?.listings?.edges[0].node.price.value?.toString() ?? '0').toString()
+  const listing = asset?.listings?.edges[0]?.node
+  const ethPrice = parseEther(listing?.price.value?.toString() ?? '0').toString()
 
   return [
     {
@@ -107,7 +108,8 @@ export function useDetailsQuery(address: string, tokenId: string): [GenieAsset, 
       collectionSymbol: asset?.collection?.image?.url,
       imageUrl: asset?.image?.url,
       animationUrl: asset?.animationUrl ?? undefined,
-      marketplace: asset?.listings?.edges[0]?.node.marketplace.toLowerCase() as any,
+      // todo: fix the back/frontend discrepency here and drop the any
+      marketplace: listing?.marketplace.toLowerCase() as any,
       name: asset?.name ?? undefined,
       priceInfo: {
         ETHPrice: ethPrice,
