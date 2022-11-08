7c380b557ee6abcf6e72ddfa2b0eb67f718bc862
Alex Ball <alex.ball@uniswap.org>
2022-11-07 20:56:03 -0500

    removing uneeded code

--- src/nft/utils/scrollToTop.ts
+++ src/nft/utils/scrollToTop.ts
@@ -1,7 +1,4 @@
-const DESKTOP_OFFSET = 540
-
 export const scrollToTop = () => {
-  // window.scrollTo({ top: DESKTOP_OFFSET })
   window.document.getElementById('nft-anchor')?.scrollIntoView({
     behavior: 'smooth',
   })
