2cd1c231ab730507739e9a39908f237d27020ac1
Christine Legge <christine.legge@uniswap.org>
2022-11-09 14:04:42 -0500

    chore: remove unused API keys (#2321)

--- src/config.ts
+++ src/config.ts
@@ -5,12 +5,10 @@ import {
   AMPLITUDE_API_URL,
   AMPLITUDE_EXPERIMENTS_DEPLOYMENT_KEY,
   COINGECKO_API_URL,
-  COVALENT_API_KEY,
   DEBUG,
   INFURA_PROJECT_ID,
   LOG_BUFFER_SIZE,
   ONESIGNAL_APP_ID,
-  OPENSEA_API_KEY,
   SENTRY_DSN,
   SHAKE_CLIENT_ID,
   SHAKE_CLIENT_SECRET,
@@ -31,7 +29,6 @@ export interface Config {
   amplitudeExperimentsDeploymentKey: string
   amplitudeApiUrl: string
   coingeckoApiUrl: string
-  covalentApiKey: string
   debug: boolean
   uniswapApiBaseUrl: string
   uniswapApiKey: string
@@ -39,7 +36,6 @@ export interface Config {
   infuraProjectId: string
   logBufferSize: number
   onesignalAppId: string
-  openseaApiKey: string
   sentryDsn: string
   shakeClientId: string
   shakeClientSecret: string
@@ -54,7 +50,6 @@ const _config: Config = {
   amplitudeExperimentsDeploymentKey:
     process.env.AMPLITUDE_EXPERIMENTS_DEPLOYMENT_KEY || AMPLITUDE_EXPERIMENTS_DEPLOYMENT_KEY,
   coingeckoApiUrl: COINGECKO_API_URL,
-  covalentApiKey: process.env.COVALENT_API_KEY || COVALENT_API_KEY,
   debug: parseBoolean(DEBUG),
   uniswapApiBaseUrl: process.env.UNISWAP_API_BASE_URL || UNISWAP_API_BASE_URL,
   uniswapApiKey: process.env.UNISWAP_API_KEY || UNISWAP_API_KEY,
@@ -62,7 +57,6 @@ const _config: Config = {
   infuraProjectId: process.env.INFURA_PROJECT_ID || INFURA_PROJECT_ID,
   logBufferSize: parseInt(LOG_BUFFER_SIZE, 10),
   onesignalAppId: process.env.ONESIGNAL_APP_ID || ONESIGNAL_APP_ID,
-  openseaApiKey: process.env.OPENSEA_API_KEY || OPENSEA_API_KEY,
   sentryDsn: process.env.SENTRY_DSN || SENTRY_DSN,
   shakeClientId: process.env.SHAKE_CLIENT_ID || SHAKE_CLIENT_ID,
   shakeClientSecret: process.env.SHAKE_CLIENT_SECRET || SHAKE_CLIENT_SECRET,
--- src/env.d.ts
+++ src/env.d.ts
@@ -7,14 +7,12 @@ declare module 'react-native-dotenv' {
   export const AMPLITUDE_API_URL: string
   export const AMPLITUDE_EXPERIMENTS_DEPLOYMENT_KEY: string
   export const COINGECKO_API_URL: string
-  export const COVALENT_API_KEY: string
   export const DEBUG: string
   export const UNISWAP_API_BASE_URL: string
   export const UNISWAP_API_KEY: string
   export const UNISWAP_APP_URL: string
   export const INFURA_PROJECT_ID: string
   export const LOG_BUFFER_SIZE: string
-  export const OPENSEA_API_KEY: string
   export const SENTRY_DSN: string
   export const SHAKE_CLIENT_ID: string
   export const SHAKE_CLIENT_SECRET: string
