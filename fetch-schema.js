f52eef8a2bf5f81ac665ab3f726f8537ba8e26de
cartcrom <cartergcromer@gmail.com>
2022-11-09 17:04:42 -0500

    fixed input too long error failing in cypress/vercel

--- fetch-schema.js
+++ fetch-schema.js
@@ -6,15 +6,17 @@ const thegraphConfig = require('./relay_thegraph.config')
 /* eslint-enable */
 
 function fetchSchema(url, outputFile) {
-  exec(`get-graphql-schema --h Origin=https://app.uniswap.org ${url}`, (error, stdout, stderr) => {
-    if (error || stderr) {
-      console.log(`Failed to fetch schema from ${url}`)
-    } else if (stdout) {
-      exec(`echo '${stdout}' > ${outputFile}`)
+  exec(
+    `get-graphql-schema --h Origin=https://app.uniswap.org ${url} | tee ${outputFile}.temp`,
+    (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(`Failed to fetch schema from ${url}`)
+      } else if (stdout) {
+        exec(`mv ${outputFile}.temp ${outputFile}`)
+      }
     }
-  })
+  )
 }
 
 fetchSchema(process.env.THE_GRAPH_SCHEMA_ENDPOINT, thegraphConfig.schema)
-
 fetchSchema(process.env.REACT_APP_AWS_API_ENDPOINT, dataConfig.schema)
