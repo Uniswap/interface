297137cb168e8fbc83935cfd0db5656e57ca9c67
Vignesh Mohankumar <me@vig.xyz>
2022-11-09 11:44:16 -0500

    refactor: useIsNftPage hooks

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,6 +3,7 @@ import { useWeb3React } from '@web3-react/core'
 import Web3Status from 'components/Web3Status'
 import { NftVariant, useNftFlag } from 'featureFlags/flags/nft'
 import { chainIdToBackendName } from 'graphql/data/util'
+import { useIsNftPage } from 'hooks/useIsNftPage'
 import { Box } from 'nft/components/Box'
 import { Row } from 'nft/components/Flex'
 import { UniIcon } from 'nft/components/icons'
@@ -48,6 +49,8 @@ const PageTabs = () => {
     pathname.startsWith('/increase') ||
     pathname.startsWith('/find')
 
+  const isNftPage = useIsNftPage()
+
   return (
     <>
       <MenuItem href="/swap" isActive={pathname.startsWith('/swap')}>
@@ -57,7 +60,7 @@ const PageTabs = () => {
         <Trans>Tokens</Trans>
       </MenuItem>
       {nftFlag === NftVariant.Enabled && (
-        <MenuItem href="/nfts" isActive={pathname.startsWith('/nfts')}>
+        <MenuItem href="/nfts" isActive={isNftPage}>
           <Trans>NFTs</Trans>
         </MenuItem>
       )}
@@ -69,8 +72,7 @@ const PageTabs = () => {
 }
 
 const Navbar = () => {
-  const { pathname } = useLocation()
-  const isNftPage = pathname.startsWith('/nfts')
+  const isNftPage = useIsNftPage()
 
   return (
     <>
