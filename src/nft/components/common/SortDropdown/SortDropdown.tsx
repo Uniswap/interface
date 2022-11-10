d7bcdb67b12aa41f4b118c39a8445310f95507f0
Alex Ball <alex.ball@uniswap.org>
2022-11-10 17:10:55 -0500

    addressing another small listing change

--- src/nft/components/common/SortDropdown/SortDropdown.tsx
+++ src/nft/components/common/SortDropdown/SortDropdown.tsx
@@ -136,7 +136,6 @@ export const SortDropdown = ({
         zIndex="3"
         width={inFilters ? 'auto' : 'inherit'}
         right={inFilters ? '16' : 'auto'}
-        paddingBottom="8"
         fontSize="14"
         background="backgroundInteractive"
         borderStyle="solid"
@@ -217,11 +216,6 @@ const DropDownItem = ({
       onClick={onClick}
       cursor="pointer"
     >
-      {option.icon && (
-        <Box width="20" height="20">
-          {option.icon}
-        </Box>
-      )}
       <Box marginLeft="8" className={buttonTextMedium}>
         {option.displayText}
       </Box>
