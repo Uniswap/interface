267bccf679897f9b5f3479d531ffe79b9a1d8a0b
Vignesh Mohankumar <me@vig.xyz>
2022-11-10 09:22:09 -0500

    use debounced

--- src/components/NavBar/SearchBar.tsx
+++ src/components/NavBar/SearchBar.tsx
@@ -58,7 +58,7 @@ export const SearchBar = () => {
       refetchOnWindowFocus: false,
       refetchOnMount: false,
       refetchOnReconnect: false,
-      enabled: !!searchValue,
+      enabled: !!debouncedSearchValue,
     }
   )
 
