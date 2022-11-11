34b96e87eee03f00139f33aceadef4d85c814096
Charlie <charles@bachmeier.io>
2022-11-11 14:45:31 -0500

    fixed scroll on mobile

--- src/nft/pages/collection/index.tsx
+++ src/nft/pages/collection/index.tsx
@@ -169,7 +169,7 @@ const Collection = () => {
 
                 {/* @ts-ignore: https://github.com/microsoft/TypeScript/issues/34933 */}
                 <AnimatedBox
-                  position={isMobile && isFiltersExpanded ? 'sticky' : 'static'}
+                  position={isMobile && isFiltersExpanded ? 'fixed' : 'static'}
                   style={{
                     transform: gridX.to((x) => `translate(${x as number}px)`),
                     width: gridWidthOffset.to((x) => `calc(100% - ${x as number}px)`),
