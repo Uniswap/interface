a35f58412edbac4e7d0ac8ffbf1191f93bb46d9f
Justin Domingue <judo@uniswap.org>
2022-11-10 22:11:21 -0500

    fix(MOB-2999): Show strong warning when users completely disable FaceId in the settings (#2363)

--- src/screens/SettingsBiometricAuthScreen.tsx
+++ src/screens/SettingsBiometricAuthScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Alert, ListRenderItemInfo } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
@@ -9,6 +9,7 @@ import { BackHeader } from 'src/components/layout/BackHeader'
 import { Box } from 'src/components/layout/Box'
 import { Flex } from 'src/components/layout/Flex'
 import { Screen } from 'src/components/layout/Screen'
+import { BiometricAuthWarningModal } from 'src/components/Settings/BiometricAuthWarningModal'
 import { Text } from 'src/components/Text'
 import {
   useBiometricAppSettings,
@@ -33,6 +34,12 @@ interface BiometricAuthSetting {
 export function SettingsBiometricAuthScreen() {
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
+
+  const [showUnsafeWarningModal, setShowUnsafeWarningModal] = useState(false)
+  const [unsafeWarningModalType, setUnsafeWarningModalType] = useState<BiometricSettingType | null>(
+    null
+  )
+  const onCloseModal = useCallback(() => setShowUnsafeWarningModal(false), [])
 
   const osBiometricAuthEnabled = useOsBiometricAuthEnabled()
   const { touchId } = useDeviceSupportsBiometricAuth()
@@ -69,11 +76,16 @@ export function SettingsBiometricAuthScreen() {
 
     return [
       {
-        onValueChange: (newValue) => {
+        onValueChange: (newRequiredForAppAccessValue) => {
+          if (!newRequiredForAppAccessValue && !requiredForTransactions) {
+            setShowUnsafeWarningModal(true)
+            setUnsafeWarningModalType(BiometricSettingType.RequiredForAppAccess)
+            return
+          }
           osBiometricAuthEnabled
             ? trigger({
                 biometricAppSettingType: BiometricSettingType.RequiredForAppAccess,
-                newValue,
+                newValue: newRequiredForAppAccessValue,
               })
             : handleFaceIdTurnedOff()
         },
@@ -82,11 +94,16 @@ export function SettingsBiometricAuthScreen() {
         subText: t('Require {{authenticationTypeName}} ID to open app', { authenticationTypeName }),
       },
       {
-        onValueChange: (newValue) => {
+        onValueChange: (newRequiredForTransactionsValue) => {
+          if (!newRequiredForTransactionsValue && !requiredForAppAccess) {
+            setShowUnsafeWarningModal(true)
+            setUnsafeWarningModalType(BiometricSettingType.RequiredForTransactions)
+            return
+          }
           osBiometricAuthEnabled
             ? trigger({
                 biometricAppSettingType: BiometricSettingType.RequiredForTransactions,
-                newValue,
+                newValue: newRequiredForTransactionsValue,
               })
             : handleFaceIdTurnedOff()
         },
@@ -129,20 +146,39 @@ export function SettingsBiometricAuthScreen() {
   }
 
   return (
-    <Screen>
-      <BackHeader alignment="left" mx="md" pt="md">
-        <Text variant="buttonLabelLarge">
-          {t('{{authenticationTypeName}} ID', { authenticationTypeName })}
-        </Text>
-      </BackHeader>
-      <Box p="lg">
-        <FlatList
-          ItemSeparatorComponent={() => <Box bg="backgroundOutline" height={1} my="md" />}
-          data={options}
-          renderItem={renderItem}
-          scrollEnabled={false}
+    <>
+      {showUnsafeWarningModal && (
+        <BiometricAuthWarningModal
+          isTouchIdDevice={touchId}
+          onClose={onCloseModal}
+          onConfirm={() => {
+            trigger({
+              biometricAppSettingType: unsafeWarningModalType,
+              // flip the bit
+              newValue: !(unsafeWarningModalType === BiometricSettingType.RequiredForAppAccess
+                ? requiredForAppAccess
+                : requiredForTransactions),
+            })
+            setShowUnsafeWarningModal(false)
+            setUnsafeWarningModalType(null)
+          }}
         />
-      </Box>
-    </Screen>
+      )}
+      <Screen>
+        <BackHeader alignment="left" mx="md" pt="md">
+          <Text variant="buttonLabelLarge">
+            {t('{{authenticationTypeName}} ID', { authenticationTypeName })}
+          </Text>
+        </BackHeader>
+        <Box p="lg">
+          <FlatList
+            ItemSeparatorComponent={() => <Box bg="backgroundOutline" height={1} my="md" />}
+            data={options}
+            renderItem={renderItem}
+            scrollEnabled={false}
+          />
+        </Box>
+      </Screen>
+    </>
   )
 }
