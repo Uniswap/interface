420e0541a6daf1e4c8652710f87ad9055691f1fb
Christine Legge <christine.legge@uniswap.org>
2022-11-10 20:19:35 -0500

    fix: Revert "fix: long wallet name overlapping other elements on Dashboard and Sidebar" (#2364)
    Revert "fix: long wallet name overlapping other elements on Dashboard and Sidebar (#2342)"
    
    This reverts commit 0030eb57e6c1ef6f1ca8f303aeb999be98b16dc4.

--- src/components/accounts/AccountCardItem.tsx
+++ src/components/accounts/AccountCardItem.tsx
@@ -43,19 +43,11 @@ export function AccountCardItem({ account, isViewOnly, isActive, onPress, onPres
 
   return (
     <TouchableArea hapticFeedback pb="sm" pt="xs" px="lg" onPress={() => onPress?.(address)}>
-      <Flex
-        row
-        alignItems="center"
-        justifyContent="space-between"
-        testID={`account_item/${address.toLowerCase()}`}>
-        <Flex row shrink>
-          <NotificationBadge showIndicator={hasNotifications}>{icon}</NotificationBadge>
-          <Flex fill gap="none">
-            <Text numberOfLines={1} variant="bodyLarge">
-              {displayName?.name}
-            </Text>
-            <PortfolioBalance color="textSecondary" owner={address} variant="bodySmall" />
-          </Flex>
+      <Flex row alignItems="center" testID={`account_item/${address.toLowerCase()}`}>
+        <NotificationBadge showIndicator={hasNotifications}>{icon}</NotificationBadge>
+        <Flex grow gap="none">
+          <Text variant="bodyLarge">{displayName?.name}</Text>
+          <PortfolioBalance color="textSecondary" owner={address} variant="bodySmall" />
         </Flex>
         <Flex row alignItems="center" gap="xs">
           {isActive && (
