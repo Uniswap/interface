fd0fc652e7f948a1d111a2af5adc0771059564c6
Callil Capuozzo <callil.capuozzo@gmail.com>
2022-11-14 18:24:02 -0500

    Update CollectionNfts.css.ts

--- src/nft/components/collection/CollectionNfts.css.ts
+++ src/nft/components/collection/CollectionNfts.css.ts
@@ -5,19 +5,48 @@ import { sprinkles } from '../../css/sprinkles.css'
 export const assetList = style([
   sprinkles({
     display: 'grid',
-    gap: { sm: '8', md: '12', lg: '20' },
+    gap: { sm: '8', md: '12', lg: '16', xxxl: '20' },
   }),
   {
     paddingLeft: 14,
     paddingRight: 14,
     gridTemplateColumns: 'repeat(auto-fill, minmax(160px, 1fr) )',
     '@media': {
-      'screen and (min-width: 708px)': {
-        gridTemplateColumns: 'repeat(auto-fill, minmax(240px, 1fr) )',
+      'screen and (min-width: 640px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(33% - 12px) )',
       },
-      'screen and (min-width: 1185px)': {
-        gridTemplateColumns: 'repeat(auto-fill, minmax(1fr, 280px) )',
+      'screen and (min-width: 960px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(33% - 12px) )',
+      },
+      'screen and (min-width: 1440px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(25% - 16px) )',
+      },
+      'screen and (min-width: 1680px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(14.25% - 20px) )',
       },
     },
   },
 ])
+
+// gap: { sm: '8', md: '12', lg: '16', xxxl: '20' },
+// }),
+// {
+//   boxSizing: 'border-box',
+//   WebkitBoxSizing: 'border-box',
+//   '@media': {
+//     'screen and (min-width: 640px)': {
+//       maxWidth: 'calc(33% - 8px)',
+//     },
+//     'screen and (min-width: 960px)': {
+//       maxWidth: 'calc(33% - 12px)',
+//     },
+//     'screen and (min-width: 1200px)': {
+//       maxWidth: 'calc(25% - 16px)',
+//     },
+//     'screen and (min-width: 1440px)': {
+//       maxWidth: 'calc(25% - 16px)',
+//     },
+//     'screen and (min-width: 1680px)': {
+//       maxWidth: 'calc(14.25% - 20px)',
+//     },
+//   },
