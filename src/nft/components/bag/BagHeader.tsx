96dc7e099864f73ef8ba0f18b9d38f571448d444
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-11-14 12:34:16 -0600

    fix(bag): simplify header (#5185)
    * fix(bag): simplify header
    
    * pr feedback: adjust margins
    
    * add margin-auto instead of flex-basis for space-filling to get a more accurate click area
    
    * pr feedback from lynn
    
    * rename numberdot to counterdot

--- src/nft/components/bag/BagHeader.tsx
+++ src/nft/components/bag/BagHeader.tsx
@@ -1,11 +1,8 @@
+import { Trans } from '@lingui/macro'
 import { OpacityHoverState } from 'components/Common'
-import { Column, Row } from 'nft/components/Flex'
 import { BagCloseIcon } from 'nft/components/icons'
-import { roundAndPluralize } from 'nft/utils/roundAndPluralize'
 import styled from 'styled-components/macro'
 import { ButtonText, ThemedText } from 'theme'
-
-import * as styles from './BagHeader.css'
 
 const ClearButton = styled(ButtonText)`
   color: ${({ theme }) => theme.textSecondary};
@@ -13,29 +10,42 @@ const ClearButton = styled(ButtonText)`
   font-weight: 600;
   font-size: 14px;
   line-height: 16px;
-  transition: 150ms ease color;
+`
 
-  :hover {
-    color: ${({ theme }) => theme.accentActive};
-  }
-`
-const ControlRow = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`
 const IconWrapper = styled.button`
+  align-items: center;
   background-color: transparent;
   border-radius: 8px;
   border: none;
   color: ${({ theme }) => theme.textPrimary};
   cursor: pointer;
   display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-left: auto;
   padding: 2px;
   opacity: 1;
 
   ${OpacityHoverState}
+`
+const CounterDot = styled.div`
+  align-items: center;
+  background-color: ${({ theme }) => theme.accentAction};
+  border-radius: 100px;
+  color: ${({ theme }) => theme.accentTextLightPrimary};
+  display: flex;
+  font-size: 10px;
+  justify-content: center;
+  padding: 4px 6px;
+`
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: flex-start;
+  margin: 16px 20px;
+  text-align: center;
 `
 interface BagHeaderProps {
   numberOfAssets: number
@@ -46,19 +56,17 @@ interface BagHeaderProps {
 
 export const BagHeader = ({ numberOfAssets, closeBag, resetFlow, isProfilePage }: BagHeaderProps) => {
   return (
-    <Column gap="4" paddingX="32" marginBottom="20">
-      <Row className={styles.header}>
-        <ThemedText.HeadlineSmall>{isProfilePage ? 'Sell NFTs' : 'My bag'}</ThemedText.HeadlineSmall>
-        <IconWrapper onClick={closeBag}>
-          <BagCloseIcon />
-        </IconWrapper>
-      </Row>
+    <Wrapper>
+      <ThemedText.HeadlineSmall>{isProfilePage ? <Trans>Sell</Trans> : <Trans>Bag</Trans>}</ThemedText.HeadlineSmall>
       {numberOfAssets > 0 && (
-        <ControlRow>
-          <ThemedText.BodyPrimary>{roundAndPluralize(numberOfAssets, 'NFT')}</ThemedText.BodyPrimary>
+        <>
+          <CounterDot>{numberOfAssets}</CounterDot>
           <ClearButton onClick={resetFlow}>Clear all</ClearButton>
-        </ControlRow>
+        </>
       )}
-    </Column>
+      <IconWrapper onClick={closeBag}>
+        <BagCloseIcon />
+      </IconWrapper>
+    </Wrapper>
   )
 }
