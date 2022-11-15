c0638e9033e82cd780be02bdd7a97367bfe320ba
Jack Short <john.short.tj@gmail.com>
2022-11-15 17:26:21 -0500

    fix: adding amm pricing updates (#5225)
    * fix: adding amm pricing updates
    
    * responding to pr comments
    
    * renaming
    
    * adding filter to callback

--- src/nft/components/collection/CollectionAsset.tsx
+++ src/nft/components/collection/CollectionAsset.tsx
@@ -1,3 +1,4 @@
+import { BigNumber } from '@ethersproject/bignumber'
 import { sendAnalyticsEvent, useTrace } from '@uniswap/analytics'
 import { EventName, PageName } from '@uniswap/analytics-events'
 import { useBag } from 'nft/hooks'
@@ -56,16 +57,18 @@ export const CollectionAsset = ({
   }, [asset])
 
   const handleAddAssetToBag = useCallback(() => {
-    addAssetsToBag([asset])
-    if (!bagExpanded && !isMobile && !bagManuallyClosed) {
-      setBagExpanded({ bagExpanded: true })
+    if (BigNumber.from(asset.priceInfo?.ETHPrice ?? 0).gte(0)) {
+      addAssetsToBag([asset])
+      if (!bagExpanded && !isMobile && !bagManuallyClosed) {
+        setBagExpanded({ bagExpanded: true })
+      }
+      sendAnalyticsEvent(EventName.NFT_BUY_ADDED, {
+        collection_address: asset.address,
+        token_id: asset.tokenId,
+        token_type: asset.tokenType,
+        ...trace,
+      })
     }
-    sendAnalyticsEvent(EventName.NFT_BUY_ADDED, {
-      collection_address: asset.address,
-      token_id: asset.tokenId,
-      token_type: asset.tokenType,
-      ...trace,
-    })
   }, [addAssetsToBag, asset, bagExpanded, bagManuallyClosed, isMobile, setBagExpanded, trace])
 
   const handleRemoveAssetFromBag = useCallback(() => {
