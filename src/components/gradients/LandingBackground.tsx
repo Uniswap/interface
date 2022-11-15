1d5663ace6a5e894b7e07b96ca5201c88e08db07
Daniel James <danielcolinjames@gmail.com>
2022-11-15 13:17:43 -0500

    fix: Onboarding animation not resetting properly (#2418)
    * Fix onboarding animation not resetting properly
    
    * Revert non working fixes
    
    * Update assets and logic
    
    * Update assets
    
    * Fix onboarding animation crashes

--- src/components/gradients/LandingBackground.tsx
+++ src/components/gradients/LandingBackground.tsx
@@ -1,6 +1,11 @@
-import React, { useEffect, useRef } from 'react'
+import { useFocusEffect } from '@react-navigation/core'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useColorScheme, ViewStyle } from 'react-native'
-import Rive, { Alignment, Fit, LoopMode, RiveRef } from 'rive-react-native'
+import Rive, { Alignment, Fit, RiveRef } from 'rive-react-native'
+import { useAppStackNavigation } from 'src/app/navigation/types'
+import { useTimeout } from 'src/utils/timing'
+
+const stateMachineName = 'State Machine 1'
 
 const animationStyles: ViewStyle = {
   width: '100%',
@@ -9,15 +14,9 @@ const animationStyles: ViewStyle = {
   top: 0,
 }
 
-export const LandingBackground = () => {
+const OnboardingAnimation = () => {
   const isDarkMode = useColorScheme() === 'dark'
   const animationRef = useRef<RiveRef>(null)
-
-  useEffect(() => {
-    // reset to ensure animation plays again when changing from light to dark mode
-    animationRef.current?.reset()
-    animationRef.current?.play('Intro', LoopMode.Loop)
-  }, [isDarkMode])
 
   return (
     <Rive
@@ -25,10 +24,52 @@ export const LandingBackground = () => {
       alignment={Alignment.TopCenter}
       animationName="Intro"
       artboardName="Unified"
-      autoplay={false}
       fit={Fit.FitHeight}
       resourceName={isDarkMode ? 'OnboardingDark' : 'OnboardingLight'}
+      stateMachineName={stateMachineName}
       style={animationStyles}
     />
   )
+}
+
+export const LandingBackground = () => {
+  const navigation = useAppStackNavigation()
+  const [blurred, setBlurred] = useState(false)
+  const [hideAnimation, setHideAnimation] = useState(false)
+
+  useEffect(() => {
+    return navigation.addListener('blur', () => {
+      // set this flag on blur (when navigating to another screen)
+      setBlurred(true)
+    })
+  }, [navigation])
+
+  // callback to turn off the animation (so that we can turn it back
+  // on on focus)
+  const turnAnimationOff = useCallback(() => {
+    if (blurred) {
+      setHideAnimation(true)
+    }
+  }, [blurred])
+
+  // but make sure it's delayed a tiny bit, otherwise blur triggers
+  // immediately, so the animation would disappear before the screen
+  // transition animation happens
+  useTimeout(turnAnimationOff, 500)
+
+  // reset animation when focusing on this screen again
+  useFocusEffect(() => {
+    setBlurred(false)
+    setHideAnimation(false)
+  })
+
+  if (hideAnimation) {
+    // this is an alternative way to "reset" the animation, because
+    // something about calling Rive's ref functions like .reset() and
+    // .play() seems to cause very hard-to-debug crashes in the
+    // underlying Swift code
+    return null
+  }
+
+  return <OnboardingAnimation />
 }
