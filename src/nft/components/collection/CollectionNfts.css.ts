a6e3b9c6c1983d92695db4cbac1616712f7a9dc4
Callil Capuozzo <callil.capuozzo@gmail.com>
2022-11-15 14:06:42 -0500

    Update grid breakpoints

--- src/nft/components/collection/CollectionNfts.css.ts
+++ src/nft/components/collection/CollectionNfts.css.ts
@@ -5,25 +5,28 @@ import { sprinkles } from '../../css/sprinkles.css'
 export const assetList = style([
   sprinkles({
     display: 'grid',
-    gap: { sm: '8', md: '12', lg: '16', xxxl: '20' },
+    gap: { sm: '8', md: '8', lg: '12', xl: '16', xxl: '20', xxxl: '20' },
   }),
   {
-    gridTemplateColumns: 'repeat(auto-fill, minmax(160px, 1fr) )',
+    gridTemplateColumns: 'repeat(auto-fill, calc(100%/2 - 8px) )',
     '@media': {
-      'screen and (min-width: 640px)': {
-        gridTemplateColumns: 'repeat(auto-fill, calc(33% - 8px) )',
+      'screen and (min-width: 768px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(100%/3 - 8px) )',
       },
-      'screen and (min-width: 960px)': {
-        gridTemplateColumns: 'repeat(auto-fill, calc(33% - 8px) )',
+      'screen and (min-width: 1024px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(100%/3 - 12px) )',
       },
-      'screen and (min-width: 1440px)': {
-        gridTemplateColumns: 'repeat(auto-fill, calc(25% - 16px) )',
+      'screen and (min-width: 1280px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(100%/4 - 16px) )',
       },
-      'screen and (min-width: 1680px)': {
-        gridTemplateColumns: 'repeat(auto-fill, calc(14.2857% - 20px) )',
+      'screen and (min-width: 1536px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(100%/5 - 20px) )',
+      },
+      'screen and (min-width: 1920px)': {
+        gridTemplateColumns: 'repeat(auto-fill, calc(100%/7 - 20px) )',
       },
     },
   },
 ])
 
-export const actionBarContainer = style([{ marginLeft: '-16px', width: 'calc(100% + 16px)' }])
+export const actionBarContainer = style([{ marginLeft: '-16px', width: 'calc(100% + 32px)' }])
