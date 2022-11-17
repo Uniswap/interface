63a9bd4fbff8bc077dfdc9a1b19e0bf02c7467c8
Mike Grabowski <grabbou@gmail.com>
2022-11-17 19:59:40 +0100

    feat: improve formatting of NFT prices with shared module (#5245)
    * wip: change formatting of nft
    
    chore: use a different package
    
    use pure webpack instead
    
    * revert changes to yarn.lock
    
    * chore: update lock one more time
    
    * chore: upgrade conedison
    
    * chore: tweaks
    
    * chore: handle 0
    
    * chore: update to latest and use proper method
    
    * chore: update conditional
    
    Co-authored-by: Zach Pomerantz <zzmp@uniswap.org>

--- craco.config.cjs
+++ craco.config.cjs
@@ -21,6 +21,16 @@ module.exports = {
         (plugin) => plugin instanceof MiniCssExtractPlugin
       )
       if (instanceOfMiniCssExtractPlugin !== undefined) instanceOfMiniCssExtractPlugin.options.ignoreOrder = true
+
+      // We're currently on Webpack 4.x that doesn't support the `exports` field in package.json.
+      // See https://github.com/webpack/webpack/issues/9509.
+      //
+      // In case you need to add more modules, make sure to remap them to the correct path.
+      //
+      // Map @uniswap/conedison to its dist folder.
+      // This is required because conedison uses * to redirect all imports to its dist.
+      webpackConfig.resolve.alias['@uniswap/conedison'] = '@uniswap/conedison/dist'
+
       return webpackConfig
     },
   },
