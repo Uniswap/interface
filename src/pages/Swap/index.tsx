c1db0238445f3e3317887acd12bc9d8746f87997
Alex Ball <alex.ball@uniswap.org>
2022-11-17 17:24:12 -0500

    fixing button color

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -677,7 +677,7 @@ export default function Swap() {
                         }
                       >
                         <AutoRow justify="space-between" style={{ flexWrap: 'nowrap' }} height="20px">
-                          <ThemedText.SubHeader width="100%" textAlign="center">
+                          <ThemedText.SubHeader width="100%" textAlign="center" color="white">
                             {/* we need to shorten this string on mobile */}
                             {approvalState === ApprovalState.APPROVED ||
                             signatureState === UseERC20PermitState.SIGNED ? (
