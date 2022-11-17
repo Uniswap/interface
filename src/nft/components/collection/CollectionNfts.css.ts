aae732082fd99b35e4c6b9f77b0117bdf1ec26af
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-11-17 10:39:32 -0600

    fix: adjust gap down to 16px on large screens

--- src/nft/components/collection/CollectionNfts.css.ts
+++ src/nft/components/collection/CollectionNfts.css.ts
@@ -5,7 +5,7 @@ import { breakpoints, sprinkles } from '../../css/sprinkles.css'
 export const assetList = style([
   sprinkles({
     display: 'grid',
-    gap: { sm: '8', md: '8', lg: '12', xl: '16', xxl: '20', xxxl: '20' },
+    gap: { sm: '8', md: '8', lg: '12', xl: '16' },
   }),
   {
     //This treatment of the grid still uses minmax, but enforces an amount of grid items per breakpoint. This means that when the bag and filter panels appear, we no longer get layout thrash and have a consistent animation as the width changes. It uses calc() and subtracts the grid gap to ensure the min size will always fit without wrapping.
@@ -19,12 +19,6 @@ export const assetList = style([
       },
       [`screen and (min-width: ${breakpoints.xl}px)`]: {
         gridTemplateColumns: 'repeat(auto-fill, minmax(calc(100%/4 - 16px), 1fr) )',
-      },
-      [`screen and (min-width: ${breakpoints.xxl}px)`]: {
-        gridTemplateColumns: 'repeat(auto-fill, minmax(calc(100%/5 - 20px), 1fr) )',
-      },
-      [`screen and (min-width: ${breakpoints.xxxl}px)`]: {
-        gridTemplateColumns: 'repeat(auto-fill, minmax(calc(100%/7 - 20px), 1fr) )',
       },
     },
   },
