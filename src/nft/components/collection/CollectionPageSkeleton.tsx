5ed4abe812e4938632f4fdf1b973aae60fe1b41a
Jack Short <john.short.tj@gmail.com>
2022-11-17 16:48:17 -0500

    style: correct collection loading

--- src/nft/components/collection/CollectionPageSkeleton.tsx
+++ src/nft/components/collection/CollectionPageSkeleton.tsx
@@ -3,10 +3,20 @@ import { Column, Row } from 'nft/components/Flex'
 import { useIsMobile } from 'nft/hooks'
 import { CollectionBannerLoading } from 'nft/pages/collection'
 import { COLLECTION_BANNER_HEIGHT } from 'nft/pages/collection'
+import * as styles from 'nft/pages/collection/index.css'
+import styled from 'styled-components/macro'
 
 import { ActivitySwitcherLoading } from './ActivitySwitcher'
 import { CollectionNftsAndMenuLoading } from './CollectionNfts'
 import { CollectionStatsLoading } from './CollectionStats'
+
+const CollectionDescriptionSection = styled(Column)`
+  ${styles.ScreenBreakpointsPaddings}
+`
+
+const CollectionAssets = styled(Box)`
+  ${styles.ScreenBreakpointsPaddings}
+`
 
 export const CollectionPageSkeleton = () => {
   const isMobile = useIsMobile()
@@ -15,15 +25,15 @@ export const CollectionPageSkeleton = () => {
       <Box width="full" height={`${COLLECTION_BANNER_HEIGHT}`}>
         <CollectionBannerLoading />
       </Box>
-      <Column paddingX="32">
+      <CollectionDescriptionSection>
         <CollectionStatsLoading isMobile={isMobile} />
         <Row gap="24" marginBottom="28">
           {ActivitySwitcherLoading}
         </Row>
-      </Column>
-      <Box paddingX="48">
+      </CollectionDescriptionSection>
+      <CollectionAssets>
         <CollectionNftsAndMenuLoading />
-      </Box>
+      </CollectionAssets>
     </Column>
   )
 }
