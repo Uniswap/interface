7b6b4284acddc51efd4f63bb135bd25678bc85db
Jack Short <john.short.tj@gmail.com>
2022-11-17 17:15:29 -0500

    review comments

--- src/nft/components/collection/CollectionPageSkeleton.tsx
+++ src/nft/components/collection/CollectionPageSkeleton.tsx
@@ -1,9 +1,10 @@
+import Column from 'components/Column'
+import Row from 'components/Row'
 import { Box } from 'nft/components/Box'
-import { Column, Row } from 'nft/components/Flex'
 import { useIsMobile } from 'nft/hooks'
 import { CollectionBannerLoading } from 'nft/pages/collection'
 import { COLLECTION_BANNER_HEIGHT } from 'nft/pages/collection'
-import * as styles from 'nft/pages/collection/index.css'
+import { ScreenBreakpointsPaddings } from 'nft/pages/collection/index.css'
 import styled from 'styled-components/macro'
 
 import { ActivitySwitcherLoading } from './ActivitySwitcher'
@@ -11,29 +12,36 @@ import { CollectionNftsAndMenuLoading } from './CollectionNfts'
 import { CollectionStatsLoading } from './CollectionStats'
 
 const CollectionDescriptionSection = styled(Column)`
-  ${styles.ScreenBreakpointsPaddings}
+  ${ScreenBreakpointsPaddings}
 `
 
 const CollectionAssets = styled(Box)`
-  ${styles.ScreenBreakpointsPaddings}
+  ${ScreenBreakpointsPaddings}
+`
+
+const StyledColumn = styled(Column)`
+  width: 100%;
+`
+
+const StyledRow = styled(Row)`
+  gap: 24px;
+  margin-bottom: 28px;
 `
 
 export const CollectionPageSkeleton = () => {
   const isMobile = useIsMobile()
   return (
-    <Column width="full">
+    <StyledColumn>
       <Box width="full" height={`${COLLECTION_BANNER_HEIGHT}`}>
         <CollectionBannerLoading />
       </Box>
       <CollectionDescriptionSection>
         <CollectionStatsLoading isMobile={isMobile} />
-        <Row gap="24" marginBottom="28">
-          {ActivitySwitcherLoading}
-        </Row>
+        <StyledRow>{ActivitySwitcherLoading}</StyledRow>
       </CollectionDescriptionSection>
       <CollectionAssets>
         <CollectionNftsAndMenuLoading />
       </CollectionAssets>
-    </Column>
+    </StyledColumn>
   )
 }
