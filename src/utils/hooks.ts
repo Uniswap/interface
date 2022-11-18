81408cdf98e81dd85ebebca3f2ab5ea72d54b08d
Tina <59578595+tinaszheng@users.noreply.github.com>
2022-11-18 14:51:44 -0500

    fix: Async data fetcher should return loading immediately on new inputs (#2523)
    fix async data fetcher hook

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -18,7 +18,13 @@ export function usePrevious<T>(value: T) {
 // adapted from https://usehooks.com/useAsync/ but simplified
 // above link contains example on how to add delayed execution if ever needed
 export function useAsyncData<T>(asyncCallback: () => Promise<T> | undefined) {
-  const [data, setData] = useState<T | undefined>(undefined)
+  const [data, setData] = useState<{
+    res: T | undefined
+    input: () => Promise<T> | undefined
+  }>({
+    res: undefined,
+    input: asyncCallback,
+  })
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
@@ -27,11 +33,18 @@ export function useAsyncData<T>(asyncCallback: () => Promise<T> | undefined) {
     async function runCallback() {
       const res = await asyncCallback()
       setIsLoading(false)
-      setData(res)
+      setData({
+        res,
+        input: asyncCallback,
+      })
     }
 
     runCallback()
   }, [asyncCallback])
 
-  return useMemo(() => ({ isLoading, data }), [isLoading, data])
+  return useMemo(() => {
+    if (asyncCallback !== data.input) return { isLoading: true, data: undefined }
+
+    return { isLoading, data: data.res }
+  }, [asyncCallback, isLoading, data])
 }
