8604d70ec62508414b7e972f8b451cd74b963088
Alex Ball <alex.ball@uniswap.org>
2022-11-18 16:17:07 -0500

    revering random line delete

--- src/components/Web3Status/index.tsx
+++ src/components/Web3Status/index.tsx
@@ -15,6 +15,7 @@ import { useAppSelector } from 'state/hooks'
 import { useDerivedSwapInfo } from 'state/swap/hooks'
 import styled, { useTheme } from 'styled-components/macro'
 import { flexRowNoWrap } from 'theme/styles'
+
 import { useOnClickOutside } from '../../hooks/useOnClickOutside'
 import {
   useCloseModal,
