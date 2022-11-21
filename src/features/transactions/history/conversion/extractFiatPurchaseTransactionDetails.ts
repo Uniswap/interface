64d931e75a98aa16e308f1ca8d6dd6236cab25fb
Justin Domingue <judo@uniswap.org>
2022-11-21 12:34:14 -0500

    refactor: support arbitrary moonpay output currencies (#2550)

--- src/features/transactions/history/conversion/extractFiatPurchaseTransactionDetails.ts
+++ src/features/transactions/history/conversion/extractFiatPurchaseTransactionDetails.ts
@@ -1,5 +1,7 @@
 import { ChainId } from 'src/constants/chains'
 import { MoonpayTransactionsResponse } from 'src/features/fiatOnRamp/types'
+import { logException } from 'src/features/telemetry'
+import { LogContext } from 'src/features/telemetry/constants'
 import {
   FiatPurchaseTransactionInfo,
   TransactionDetails,
@@ -7,7 +9,7 @@ import {
   TransactionType,
 } from 'src/features/transactions/types'
 import { toSupportedChainId } from 'src/utils/chainId'
-import { buildNativeCurrencyId, currencyIdToChain } from 'src/utils/currencyId'
+import { buildCurrencyId, buildNativeCurrencyId } from 'src/utils/currencyId'
 import { logger } from 'src/utils/logger'
 
 // TODO: determine what it should actually be across chains
@@ -15,30 +17,36 @@ const MOONPAY_FIAT_ON_RAMP_ADDRESS = '0xc216ed2d6c295579718dbd4a797845cda70b3c36
 
 function parseFiatPurchaseTransaction(
   transaction: Partial<MoonpayTransactionsResponse[0]>
-): FiatPurchaseTransactionInfo | undefined {
+): FiatPurchaseTransactionInfo & { chainId: ChainId } {
   const { currency: outputCurrency, getValidQuote } = transaction
-  if (!outputCurrency) return undefined
+
+  if (!outputCurrency) {
+    throw new Error('Expected output currency to be defined.')
+  }
+
+  if (outputCurrency.type !== 'crypto') {
+    throw new Error('Expected output currency to be crypto but received ' + outputCurrency.type)
+  }
 
   const chainId = toSupportedChainId(outputCurrency.metadata.chainId ?? undefined)
-
-  if (
-    outputCurrency.type !== 'crypto' ||
-    // TODO: consider a better treatment of chain id unavailable
-    !chainId
-  ) {
-    return
+  if (!chainId) {
+    throw new Error('Unable to parse chain id' + outputCurrency.metadata.chainId)
   }
+
+  const outputCurrencyId = outputCurrency.metadata.contractAddress
+    ? buildCurrencyId(chainId, outputCurrency.metadata.contractAddress)
+    : buildNativeCurrencyId(chainId)
 
   return {
     type: TransactionType.FiatPurchase,
     // NOTE: from docs it should be `returnUrl` but in test mode this is the right one
     explorerUrl: transaction.redirectUrl,
-    // TODO(judo): support non-native currencies by checking contract address
-    outputCurrencyId: buildNativeCurrencyId(chainId),
+    outputCurrencyId,
     outputCurrencyAmountFormatted:
       getValidQuote?.quoteCurrencyAmount ?? transaction.quoteCurrencyAmount ?? 0,
     outputCurrencyAmountPrice:
       getValidQuote?.quoteCurrencyPrice ?? transaction.quoteCurrencyPrice ?? 0,
+    chainId,
   }
 }
 
@@ -68,18 +76,25 @@ export function extractFiatOnRampTransactionDetails(
 ): TransactionDetails | null {
   if (!transaction) return null
 
-  const typeInfo = parseFiatPurchaseTransaction(transaction) ?? { type: TransactionType.Unknown }
+  // given that the `transaction` object is the raw Moonpay response,
+  // we wrap the extract block in a try-catch and log to Sentry
+  try {
+    const { chainId, ...typeInfo } = parseFiatPurchaseTransaction(transaction) ?? {
+      type: TransactionType.Unknown,
+    }
 
-  return {
-    id: transaction.externalTransactionId,
-    chainId:
-      currencyIdToChain((typeInfo as FiatPurchaseTransactionInfo).outputCurrencyId) ??
-      ChainId.Mainnet,
-    hash: transaction.id,
-    addedTime: new Date(transaction.createdAt).getTime(),
-    status: moonpayStatusToTransactionInfoStatus(transaction.status),
-    from: MOONPAY_FIAT_ON_RAMP_ADDRESS,
-    typeInfo,
-    options: { request: {} },
+    return {
+      id: transaction.externalTransactionId,
+      chainId,
+      hash: transaction.id,
+      addedTime: new Date(transaction.createdAt).getTime(),
+      status: moonpayStatusToTransactionInfoStatus(transaction.status),
+      from: MOONPAY_FIAT_ON_RAMP_ADDRESS,
+      typeInfo,
+      options: { request: {} },
+    }
+  } catch (e) {
+    logException(LogContext.FiatOnRamp, `Failed to parse transaction: ${e}`)
+    return null
   }
 }
