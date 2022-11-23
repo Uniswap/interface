b7655aec3af87b4b6059e99c3e8c6134c24e413c
Thomas Thachil <thomasthachil@users.noreply.github.com>
2022-11-23 16:42:46 -0500

    chore: Update marketing + build numbers for second launch candidate (#2587)
    * chore: Update marketing + build numbers for second launch candidate
    
    * Added changes for getFullAppVersion

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -3,11 +3,15 @@ import DeviceInfo from 'react-native-device-info'
 /**
  * Returns a string with the app version and build number in the format:
  *
- * AppSemVer.BuildNumber: e.g. 1.2.3.233
+ * DEV: AppSemVer.BuildNumber: e.g. 1.2.3.233
+ * PROD: AppSemVer: eg. 1
  */
 export function getFullAppVersion() {
   const version = DeviceInfo.getVersion()
   const buildVersion = DeviceInfo.getBuildNumber()
 
-  return `${version}.${buildVersion}`
+  if (__DEV__) {
+    return `${version}.${buildVersion}`
+  }
+  return version
 }
