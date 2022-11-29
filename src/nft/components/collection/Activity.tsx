75fd7de156fc5f6d17714bb4c3806b44c986975b
Lynn Yu <lynn.yu@uniswap.org>
2022-11-29 13:39:03 -0500

    init

--- src/nft/components/collection/Activity.tsx
+++ src/nft/components/collection/Activity.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import { Box } from 'nft/components/Box'
 import { LoadingSparkle } from 'nft/components/common/Loading/LoadingSparkle'
 import { Center, Column, Row } from 'nft/components/Flex'
+import { themeVars, vars } from 'nft/css/sprinkles.css'
 import { useBag, useIsMobile } from 'nft/hooks'
 import { ActivityFetcher } from 'nft/queries/genie/ActivityFetcher'
 import { ActivityEvent, ActivityEventResponse, ActivityEventType } from 'nft/types'
@@ -9,6 +10,7 @@ import { fetchPrice } from 'nft/utils/fetchPrice'
 import { useCallback, useEffect, useMemo, useReducer, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useInfiniteQuery } from 'react-query'
+import { useIsDarkMode } from 'state/user/hooks'
 
 import * as styles from './Activity.css'
 import { AddressCell, BuyCell, EventCell, ItemCell, PriceCell } from './ActivityCells'
@@ -106,6 +108,7 @@ export const Activity = ({ contractAddress, rarityVerified, collectionName, chai
   const toggleCart = useBag((state) => state.toggleBag)
   const isMobile = useIsMobile()
   const [ethPriceInUSD, setEthPriceInUSD] = useState(0)
+  const isDarkMode = useIsDarkMode()
 
   useEffect(() => {
     fetchPrice().then((price) => {
@@ -116,17 +119,19 @@ export const Activity = ({ contractAddress, rarityVerified, collectionName, chai
   const Filter = useCallback(
     function ActivityFilter({ eventType }: { eventType: ActivityEventType }) {
       const isActive = activeFilters[eventType]
+      const activeBackgroundColor = isDarkMode ? vars.color.gray500 : vars.color.gray200
 
       return (
         <Box
-          className={clsx(styles.filter, isActive && styles.activeFilter)}
+          className={clsx(styles.filter)}
+          style={{ background: isActive ? activeBackgroundColor : themeVars.colors.backgroundInteractive }}
           onClick={() => filtersDispatch({ eventType })}
         >
           {eventType.charAt(0) + eventType.slice(1).toLowerCase() + 's'}
         </Box>
       )
     },
-    [activeFilters]
+    [activeFilters, isDarkMode]
   )
 
   return (
