0c7435652928f4014d8be9187f318af864ffece1
Lynn Yu <lynn.yu@uniswap.org>
2022-12-01 13:06:29 -0500

    simplify

--- src/nft/components/details/AssetPriceDetails.tsx
+++ src/nft/components/details/AssetPriceDetails.tsx
@@ -255,7 +255,7 @@ export const OwnerContainer = ({ asset }: { asset: GenieAsset }) => {
       )}
       {!listing ? (
         <BuyNowButton assetInBag={false} margin={true} useAccentColor={true} onClick={goToListPage}>
-          <ThemedText.SubHeader lineHeight="20px" style={{ color: 'white' }}>
+          <ThemedText.SubHeader lineHeight="20px" color="white">
             List
           </ThemedText.SubHeader>
         </BuyNowButton>
