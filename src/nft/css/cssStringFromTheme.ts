460d5426989e2da835a4b79a215736df13345700
Vignesh Mohankumar <me@vig.xyz>
2022-12-01 18:17:42 -0500

    remove some unused exports

--- src/nft/css/cssStringFromTheme.ts
+++ src/nft/css/cssStringFromTheme.ts
@@ -4,7 +4,7 @@ import { lightTheme } from 'nft/themes/lightTheme'
 import { cssObjectFromTheme } from './cssObjectFromTheme'
 import { Theme } from './sprinkles.css'
 
-export function cssStringFromTheme(theme: Theme | (() => Theme), options: { extends?: Theme | (() => Theme) } = {}) {
+function cssStringFromTheme(theme: Theme | (() => Theme), options: { extends?: Theme | (() => Theme) } = {}) {
   return Object.entries(cssObjectFromTheme(theme, options))
     .map(([key, value]) => `${key}:${value};`)
     .join('')
