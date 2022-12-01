2910881e240efd5fa546607f807b6e9266d553b1
Charles Bachmeier <charlie@genie.xyz>
2022-12-01 09:32:25 -0500

    show 1155s in collection filter

--- src/nft/queries/openSea/OSCollectionsFetcher.ts
+++ src/nft/queries/openSea/OSCollectionsFetcher.ts
@@ -13,17 +13,12 @@ export const OSCollectionsFetcher = async ({ params }: any): Promise<WalletColle
   const r = await fetch(`https://api.opensea.io/api/v1/collections?${new URLSearchParams(params).toString()}`)
   const walletCollections = await r.json()
   if (walletCollections) {
-    return walletCollections
-      .filter(
-        (collection: any) =>
-          collection.primary_asset_contracts.length && collection.primary_asset_contracts[0].schema_name === 'ERC721'
-      )
-      .map((collection: any) => ({
-        address: collection.primary_asset_contracts[0].address,
-        name: collection.name,
-        image: collection.image_url,
-        count: collection.owned_asset_count,
-      }))
+    return walletCollections.map((collection: any) => ({
+      address: collection.primary_asset_contracts[0].address,
+      name: collection.name,
+      image: collection.image_url,
+      count: collection.owned_asset_count,
+    }))
   } else {
     return []
   }
