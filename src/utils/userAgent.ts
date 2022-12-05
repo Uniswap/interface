3cb3e5a27680f1d8b169a2c6bbe55312c7098cf0
Vignesh Mohankumar <me@vig.xyz>
2022-12-05 16:16:04 -0500

    unused useragent

--- src/utils/userAgent.ts
+++ src/utils/userAgent.ts
@@ -3,6 +3,4 @@ import { UAParser } from 'ua-parser-js'
 const parser = new UAParser(window.navigator.userAgent)
 const { type } = parser.getDevice()
 
-export const userAgent = parser.getResult()
-
 export const isMobile = type === 'mobile' || type === 'tablet'
