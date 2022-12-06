86cef18b7be730aa4e3f5d60394467395e067766
Justin Domingue <judo@uniswap.org>
2022-12-06 12:21:41 -0500

    chore: jest test coverage (#2740)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   preset: 'jest-expo',
   setupFiles: ['./jest-setup.js', './node_modules/react-native-gesture-handler/jestSetup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts,tsx}',
+    '!src/test/**', // test helpers
+    '!src/stories/**',
+    '!src/abis/**', // auto-generated
+    '!**/__generated__/**', // auto-generated
+    '!**/node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      // slightly below current test coverage to prevent failing prs for now
+      // while preventing further regressions
+      lines: 21.5,
+    },
+  },
+  // consider enabling for speed
+  // changedSince: 'master',
   // https://github.com/facebook/jest/issues/2663#issuecomment-341384494
   moduleNameMapper: {
     '.+\\.(css|style|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
@@ -11,4 +29,20 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|react-native-reanimated|@react-native|@react-native-firebase|react-native-redash|react-native-webview|@gorhom|expo.*|react-native-flipper|d3-(array|color|format|interpolate|path|scale|shape|time-format|time)|internmap|react-native-qrcode-svg|react-native-modal|react-native-animatable)|react-native-masked-view|expo-linear-gradient|expo-blur|redux-persist/)',
   ],
+  // define environment variables
+  globals: {
+    AMPLITUDE_API_KEY: 123,
+    AMPLITUDE_API_TEST_KEY: 123,
+    AMPLITUDE_EXPERIMENTS_DEPLOYMENT_KEY: 123,
+    MOONPAY_API_KEY: 123,
+    MOONPAY_API_URL: 'https://api.moonpay.com',
+    MOONPAY_WIDGET_API_URL: 'https://api.moonpay.com',
+    INFURA_PROJECT_ID: 123,
+    ONESIGNAL_APP_ID: 123,
+    SENTRY_DSN: 'http://sentry.com',
+    SHAKE_CLIENT_ID: 123,
+    SHAKE_CLIENT_SECRET: 123,
+    UNISWAP_API_KEY: 123,
+    UNISWAP_API_BASE_URL: 'https://api.uniswap.org',
+  },
 }
