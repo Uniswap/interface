f068947d2cb42e222f1a6b75583b8784fc6341f3
Jack Short <john.short.tj@gmail.com>
2022-12-08 12:32:09 -0500

    updating routing code

--- src/nft/utils/txRoute/combineItemsWithTxRoute.ts
+++ src/nft/utils/txRoute/combineItemsWithTxRoute.ts
@@ -1,4 +1,14 @@
-import { BuyItem, GenieAsset, isPooledMarket, Markets, PriceInfo, RoutingItem, UpdatedGenieAsset } from 'nft/types'
+import { BigNumber } from '@ethersproject/bignumber'
+import {
+  BuyItem,
+  GenieAsset,
+  isPooledMarket,
+  Markets,
+  PriceInfo,
+  RoutingItem,
+  TokenType,
+  UpdatedGenieAsset,
+} from 'nft/types'
 import {
   calcAvgGroupPoolPrice,
   formatWeiToDecimal,
@@ -31,13 +41,43 @@ const isAveragePriceOfPooledAssets = (
   return !isPriceDiff(calcAvgGroupPoolPrice(asset, numberOfAssetsInPool), expectedPrice)
 }
 
-const isAveragedPrice = (
+const isErc1155TokenCorrectSum = (item: UpdatedGenieAsset, items: UpdatedGenieAsset[], txRoute?: RoutingItem[]) => {
+  if (!txRoute) return false
+
+  const wishItems = items.filter(
+    (wishItem) =>
+      item.address === wishItem.address && item.tokenId === wishItem.tokenId && itemHasRoute(wishItem, txRoute)
+  )
+  const wishPrice = wishItems.reduce((acc, cur) => {
+    const newPriceInfo = cur.updatedPriceInfo ?? cur.priceInfo
+    const sum = acc.add(BigNumber.from(newPriceInfo.basePrice))
+    return sum
+  }, BigNumber.from(0))
+
+  const routeItems = wishItems.map((wishItem) => getRouteForItem(wishItem, txRoute))
+  const routePrice = routeItems.reduce((acc, cur) => {
+    if (cur && 'priceInfo' in cur.assetOut) {
+      const sum = acc.add(BigNumber.from(cur.assetOut.priceInfo.basePrice))
+      return sum
+    } else {
+      return acc
+    }
+  }, BigNumber.from(0))
+
+  return wishPrice.eq(routePrice)
+}
+
+const isCorrectCalculatedPrice = (
   item: UpdatedGenieAsset,
   items: UpdatedGenieAsset[],
   route: RoutingItem,
   txRoute?: RoutingItem[]
 ): boolean => {
   if (!(route && 'priceInfo' in route.assetOut)) return false
+
+  if (item.tokenType === TokenType.ERC1155) {
+    return isErc1155TokenCorrectSum(item, items, txRoute)
+  }
 
   return (
     !!item.marketplace &&
@@ -91,7 +131,7 @@ export const combineBuyItemsWithTxRoute = (
     // if the price changed
     if (route && 'priceInfo' in route.assetOut) {
       if (isPriceDiff(newPriceInfo.basePrice, route.assetOut.priceInfo.basePrice)) {
-        if (!isAveragedPrice(item, items, route, txRoute)) {
+        if (!isCorrectCalculatedPrice(item, items, route, txRoute)) {
           return {
             ...item,
             updatedPriceInfo: route.assetOut.priceInfo,
