9452235f48b9ce1298597319a00e2499414cd10b
Charles Bachmeier <charlie@genie.xyz>
2022-12-12 12:50:28 -0700

    remove uneeded check

--- src/nft/components/details/TraitsContainer.tsx
+++ src/nft/components/details/TraitsContainer.tsx
@@ -77,7 +77,7 @@ const TraitsContainer = ({ asset }: { asset: GenieAsset }) => {
   return (
     <Grid>
       {traits?.map((trait) => {
-        return !!asset.address && <GridItem key={trait.trait_type} trait={trait} collectionAddress={asset.address} />
+        return <GridItem key={trait.trait_type} trait={trait} collectionAddress={asset.address} />
       })}
     </Grid>
   )
