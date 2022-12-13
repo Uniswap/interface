3e89da91dc19ac7aaa904b24bd16e44ea7482bef
Jack Short <john.short.tj@gmail.com>
2022-12-13 12:58:13 -0500

    adding comments

--- src/hooks/useScreenSize.ts
+++ src/hooks/useScreenSize.ts
@@ -1,3 +1,9 @@
+// HOW TO USE:
+// the hook returns a record of booleans, where the key is the name of the breakpoint
+// if the screen size is greater than or equal to the breakpoint, the value will be true
+// for example if the screen size is 1024px, the value of the 'md' key will be true and the value of the 'lg' key will be true
+// if you were to check if the screen is smaller than a breakpoint you would check if it is false
+
 import { useEffect, useState } from 'react'
 import { BREAKPOINTS } from 'theme'
 
