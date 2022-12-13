dc182cf6c1568403ace8d17d48ccd5af016ceb08
Mike <grabbou@gmail.com>
2022-12-13 15:29:50 -0500

    chore: add note

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+/**
+ * This is a workaround to allow ESLint to resolve plugins that were installed
+ * by an external config: https://github.com/eslint/eslint/issues/3458
+ */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
