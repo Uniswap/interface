85da27a1e8b9703e05798638ef5256baa41cfafd
Nguyen Van Viet <vietnv1304@gmail.com>
2022-12-14 12:20:43 +0700

    Fallback farms allchain (#1547)
    * revert all chain to thegraph
    
    * revert all chain to thegraph

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -136,7 +136,7 @@ export const TRUESIGHT_NETWORK_TO_CHAINID: NetToChain = SUPPORTED_NETWORKS.reduc
 }, {} as NetToChain) as NetToChain
 
 export const FAUCET_NETWORKS = [ChainId.BTTC]
-export const CHAINS_SUPPORT_NEW_POOL_FARM_API = [
+export const CHAINS_SUPPORT_NEW_POOL_FARM_API: ChainId[] = [
   // ChainId.OPTIMISM,
   // ChainId.AVAXMAINNET,
   // ChainId.ARBITRUM,
