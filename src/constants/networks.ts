7a462b424250a45e08e54509716fde2270a28a2e
Nguyen Van Viet <vietnv1304@gmail.com>
2022-12-14 12:12:27 +0700

    revert all chain to thegraph (#1546)

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -137,12 +137,12 @@ export const TRUESIGHT_NETWORK_TO_CHAINID: NetToChain = SUPPORTED_NETWORKS.reduc
 
 export const FAUCET_NETWORKS = [ChainId.BTTC]
 export const CHAINS_SUPPORT_NEW_POOL_FARM_API = [
-  ChainId.OPTIMISM,
-  ChainId.AVAXMAINNET,
+  // ChainId.OPTIMISM,
+  // ChainId.AVAXMAINNET,
   // ChainId.ARBITRUM,
   // ChainId.MAINNET,
-  ChainId.CRONOS,
-  ChainId.BTTC,
-  ChainId.VELAS,
-  ChainId.FANTOM,
+  // ChainId.CRONOS,
+  // ChainId.BTTC,
+  // ChainId.VELAS,
+  // ChainId.FANTOM,
 ]
