cc419b3bf9359f0b0c4eb5a0b3f9e720ac6a9964
Vignesh Mohankumar <me@vig.xyz>
2022-12-15 12:36:04 -0500

    140

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -57,7 +57,7 @@ const ContentContainer = styled.div<{ isDarkMode: boolean }>`
   position: absolute;
   bottom: 0;
   z-index: ${Z_INDEX.dropdown};
-  padding: 32px 0 120px;
+  padding: 32px 0 140px;
   transition: ${({ theme }) => `${theme.transition.duration.medium} ${theme.transition.timing.ease} opacity`};
 
   * {
