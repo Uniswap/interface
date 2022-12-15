6383e9e4bfd728cc5bc2c6ff0c3b58edfe2791bf
aballerr <alex.ball@uniswap.org>
2022-12-15 12:15:44 -0500

    fix: reverting some changes to wallet dropdown (#5694)
    reverting some changes to wallet dropdown cypress tests

--- cypress/e2e/wallet-dropdown.test.ts
+++ cypress/e2e/wallet-dropdown.test.ts
@@ -1,7 +1,7 @@
 import { getTestSelector } from '../utils'
 
-describe.skip('Wallet Dropdown', () => {
-  beforeEach(() => {
+describe('Wallet Dropdown', () => {
+  before(() => {
     cy.visit('/pool')
   })
 
@@ -12,7 +12,6 @@ describe.skip('Wallet Dropdown', () => {
   })
 
   it('should select a language', () => {
-    cy.get(getTestSelector('web3-status-connected')).click()
     cy.get(getTestSelector('wallet-select-language')).click()
     cy.get(getTestSelector('wallet-language-item')).contains('Afrikaans').click({ force: true })
     cy.get(getTestSelector('wallet-header')).should('contain', 'Taal')
@@ -22,24 +21,20 @@ describe.skip('Wallet Dropdown', () => {
   })
 
   it('should be able to view transactions', () => {
-    cy.get(getTestSelector('web3-status-connected')).click()
     cy.get(getTestSelector('wallet-transactions')).click()
     cy.get(getTestSelector('wallet-empty-transaction-text')).should('exist')
     cy.get(getTestSelector('wallet-back')).click()
   })
 
   it('should change the theme when not connected', () => {
-    cy.get(getTestSelector('web3-status-connected')).click()
     cy.get(getTestSelector('wallet-disconnect')).click()
     cy.get(getTestSelector('wallet-select-theme')).click()
-    cy.get(getTestSelector('wallet-select-theme')).contains('Light theme').should('exist')
-    cy.get(getTestSelector('wallet-select-theme')).click()
     cy.get(getTestSelector('wallet-select-theme')).contains('Dark theme').should('exist')
+    cy.get(getTestSelector('wallet-select-theme')).click()
+    cy.get(getTestSelector('wallet-select-theme')).contains('Light theme').should('exist')
   })
 
   it('should select a language when not connected', () => {
-    cy.get(getTestSelector('web3-status-connected')).click()
-    cy.get(getTestSelector('wallet-disconnect')).click()
     cy.get(getTestSelector('wallet-select-language')).click()
     cy.get(getTestSelector('wallet-language-item')).contains('Afrikaans').click({ force: true })
     cy.get(getTestSelector('wallet-header')).should('contain', 'Taal')
@@ -49,8 +44,6 @@ describe.skip('Wallet Dropdown', () => {
   })
 
   it('should open the wallet connect modal from the drop down when not connected', () => {
-    cy.get(getTestSelector('web3-status-connected')).click()
-    cy.get(getTestSelector('wallet-disconnect')).click()
     cy.get(getTestSelector('wallet-connect-wallet')).click()
     cy.get(getTestSelector('wallet-modal')).should('exist')
     cy.get(getTestSelector('wallet-modal-close')).click()
