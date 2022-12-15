bebc653f44764dd6d7bffc3268e99e939f85575d
Mike <grabbou@gmail.com>
2022-12-15 16:36:20 -0500

    chore: tweaaks

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,7 @@
 import { Trans } from '@lingui/macro'
 import { Trace } from '@uniswap/analytics'
 import { SmallButtonPrimary } from 'components/Button'
+import { useIsMobile } from 'nft/hooks'
 import { Link } from 'react-router-dom'
 import { useIsDarkMode } from 'state/user/hooks'
 import styled from 'styled-components/macro'
@@ -10,47 +11,51 @@ import darkImage from '../../assets/images/404-page-dark.png'
 import lightImage from '../../assets/images/404-page-light.png'
 
 const Image = styled.img`
-  margin: 20px;
   max-width: 510px;
   width: 100%;
   padding: 0 75px;
 `
 
-const Center = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  @media screen and (max-width: ${({ theme }) => theme.breakpoint.md}px) {
-    flex: 1;
-    justify-content: center;
-  }
 `
 
-const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const Header = styled(Container)`
+  gap: 30px;
+`
+
+const PageWrapper = styled(Container)`
   flex: 1;
-  justify-content: space-between;
+  justify-content: center;
+  gap: 50px;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
+    justify-content: space-between;
     padding-top: 65px;
   }
 `
 
 export default function NotFound() {
   const isDarkMode = useIsDarkMode()
+  const isMobile = useIsMobile()
+
+  const Title = isMobile ? ThemedText.LargeHeader : ThemedText.Hero
+  const Paragraph = isMobile ? ThemedText.HeadlineMedium : ThemedText.HeadlineLarge
+
   return (
     <PageWrapper>
       <Trace page="404-page" shouldLogImpression>
-        <Center>
-          <ThemedText.Hero>404</ThemedText.Hero>
-          <ThemedText.HeadlineLarge color="textSecondary">
-            <Trans>Page not found!</Trans>
-          </ThemedText.HeadlineLarge>
+        <Header>
+          <Container>
+            <Title>404</Title>
+            <Paragraph color="textSecondary">
+              <Trans>Page not found!</Trans>
+            </Paragraph>
+          </Container>
           <Image src={isDarkMode ? darkImage : lightImage} alt="404 page" />
-        </Center>
+        </Header>
         <SmallButtonPrimary as={Link} to="/">
           <Trans>Oops, take me back to Swap</Trans>
         </SmallButtonPrimary>
--- src/theme/components/text.tsx
+++ src/theme/components/text.tsx
@@ -31,8 +31,11 @@ export const ThemedText = {
   HeadlineSmall(props: TextProps) {
     return <TextWrapper fontWeight={600} fontSize={20} lineHeight="28px" color="textPrimary" {...props} />
   },
+  HeadlineMedium(props: TextProps) {
+    return <TextWrapper fontWeight={500} fontSize={28} color="textPrimary" {...props} />
+  },
   HeadlineLarge(props: TextProps) {
-    return <TextWrapper fontWeight={600} fontSize={36} lineHeight="44px" color="textPrimary" {...props} />
+    return <TextWrapper fontWeight={600} fontSize={36} lineHeight="36px" color="textPrimary" {...props} />
   },
   LargeHeader(props: TextProps) {
     return <TextWrapper fontWeight={400} fontSize={36} color="textPrimary" {...props} />
