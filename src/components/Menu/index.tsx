31d04b5c4114dac05c3c78a59de4af76c5da62d5
Nguyễn Hoài Danh <33005392+nguyenhoaidanh@users.noreply.github.com>
2022-12-16 09:52:05 +0700

    update: hide help on menu (#1556)
    * hide help on menu
    
    * rm unuse

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,7 +6,6 @@ import {
   BookOpen,
   Edit,
   FileText,
-  HelpCircle,
   Info,
   Menu as MenuIcon,
   MessageCircle,
@@ -320,10 +319,6 @@ export default function Menu() {
         <ExternalNavMenuItem href="https://forms.gle/gLiNsi7iUzHws2BY8">
           <Edit size={14} />
           <Trans>Business Enquiries</Trans>
-        </ExternalNavMenuItem>
-        <ExternalNavMenuItem href="https://support.kyberswap.com">
-          <HelpCircle size={16} />
-          <Trans>Help</Trans>
         </ExternalNavMenuItem>
         <ClaimRewardButton
           disabled={!account || !isEVM || !(networkInfo as EVMNetworkInfo).classic.claimReward || pendingTx}
