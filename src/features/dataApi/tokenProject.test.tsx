0c0cb2330302815f24b2735b76135afe0dfd2d0e
Justin Domingue <judo@uniswap.org>
2022-12-16 15:57:37 -0500

    test: `TokenProject` with `renderHook` (#2808)

---
+++ src/features/dataApi/tokenProject.test.tsx
@@ -0,0 +1,50 @@
+import { MockedResponse } from '@apollo/client/testing'
+import { TokenProjectsDocument, TokenProjectsQuery } from 'src/data/__generated__/types-and-hooks'
+import { useTokenProjects } from 'src/features/dataApi/tokenProjects'
+import { currencyIdToContractInput, tokenProjectToCurrencyInfos } from 'src/features/dataApi/utils'
+import { MainnetEth } from 'src/test/fixtures'
+import { TokenProjects } from 'src/test/gqlFixtures'
+import { renderHook } from 'src/test/test-utils'
+import { currencyId } from 'src/utils/currencyId'
+import { sleep } from 'src/utils/timing'
+
+const _currencyId = currencyId(MainnetEth)
+
+const mock: MockedResponse<TokenProjectsQuery> = {
+  request: {
+    query: TokenProjectsDocument,
+    variables: {
+      contracts: [currencyIdToContractInput(_currencyId)],
+    },
+  },
+  result: {
+    data: {
+      tokenProjects: TokenProjects,
+    },
+  },
+}
+
+describe(useTokenProjects, () => {
+  it('returns undefined when there is no data', async () => {
+    const emptyMock = { ...mock, result: { data: {} } }
+    const { result } = renderHook(() => useTokenProjects([_currencyId]), {
+      mocks: [emptyMock],
+    })
+
+    await sleep(100)
+
+    expect(result.current.loading).toEqual(false)
+    expect(result.current.data).toBe(undefined)
+  })
+
+  it('renders without error', async () => {
+    const { result } = renderHook(() => useTokenProjects([_currencyId]), {
+      mocks: [mock],
+    })
+
+    await sleep(100)
+
+    const data = result.current.data
+    expect(data).toEqual(tokenProjectToCurrencyInfos(TokenProjects))
+  })
+})
--- src/test/render.tsx
+++ src/test/render.tsx
@@ -4,8 +4,11 @@ import { NavigationContainer } from '@react-navigation/native'
 import type { PreloadedState } from '@reduxjs/toolkit'
 import { configureStore } from '@reduxjs/toolkit'
 import { ThemeProvider } from '@shopify/restyle'
-import type { RenderOptions } from '@testing-library/react-native'
-import { render, render as RNRender } from '@testing-library/react-native'
+import {
+  render as RNRender,
+  renderHook as RNRenderHook,
+  RenderOptions,
+} from '@testing-library/react-native'
 import React, { PropsWithChildren, ReactElement } from 'react'
 import { Provider } from 'react-redux'
 import type { RootState } from 'src/app/rootReducer'
@@ -56,7 +59,50 @@ export function renderWithProviders(
   }
 
   // Return an object with the store and all of RTL's query functions
-  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+  return { store, ...RNRender(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+
+/**
+ *
+ * @param hook Hook to render
+ * @param mocks Mocks to pass to Apollo MockedProvider
+ * @param preloadedState and store
+ * @returns `hook` wrapped with providers
+ */
+export function renderHookWithProviders(
+  // figure out a way to pass proper hook type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  hook: (...args: any) => any,
+  {
+    mocks = [],
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({ reducer: persistedReducer, preloadedState }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren<unknown>): JSX.Element {
+    // Helps expose errors in MockedProvider
+    const link = ApolloLink.from([setupErrorLink(1, 1), new MockLink(mocks)])
+
+    return (
+      <MockedProvider addTypename={false} cache={setupCache()} link={link} mocks={mocks}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </Provider>
+      </MockedProvider>
+    )
+  }
+
+  // Return an object with the store and all of RTL's query functions
+  return {
+    store,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...RNRenderHook<any, any>(hook, {
+      wrapper: Wrapper,
+      ...renderOptions,
+    }),
+  }
 }
 
 export const WithTheme = ({ component }: { component: ReactElement }) => {
