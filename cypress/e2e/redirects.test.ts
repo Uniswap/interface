4cda007737649b5fb660a0c2d5c56228c4a275f2
Alex Ball <alex.ball@uniswap.org>
2022-12-16 16:26:05 -0500

    updating description

--- cypress/e2e/redirects.test.ts
+++ cypress/e2e/redirects.test.ts
@@ -3,7 +3,7 @@ describe('Redirect', () => {
     cy.visit('/create-proposal')
     cy.url().should('match', /\/vote\/create-proposal/)
   })
-  it('should redirect to /swap when visiting nonexist url', () => {
+  it('should redirect to /not-found when visiting nonexist url', () => {
     cy.visit('/none-exist-url')
     cy.url().should('match', /\/not-found/)
   })
