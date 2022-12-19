2b8f150a5a238ef5ad53294c6f6ddeec3bc8e17c
Mike <grabbou@gmail.com>
2022-12-20 01:40:49 +0400

    tmp lets see if this works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 /* eslint-env node */
 
-require('@uniswap/eslint-config/load')
+/**
+ * This is a workaround to allow ESLint to resolve plugins that were installed
+ * by an external config: https://github.com/eslint/eslint/issues/3458
+ */
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   extends: '@uniswap/eslint-config/react',
