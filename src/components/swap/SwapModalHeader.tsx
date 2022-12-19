c098ad1ffec1e4974229f252f05c1de0a900e483
eddie <66155195+just-toby@users.noreply.github.com>
2022-12-19 09:27:43 -0500

    fix: correct font size for the trade rate to on safari (#5714)
    * fix: correct font size for the trade rate to on safari
    
    * fix: use themedText.BodySmall for this label

--- src/components/swap/SwapModalHeader.tsx
+++ src/components/swap/SwapModalHeader.tsx
@@ -75,7 +75,6 @@ export default function SwapModalHeader({
 }) {
   const theme = useTheme()
 
-  const [showInverted, setShowInverted] = useState<boolean>(false)
   const [lastExecutionPrice, setLastExecutionPrice] = useState(trade.executionPrice)
   const [priceUpdate, setPriceUpdate] = useState<number | undefined>()
 
@@ -153,7 +152,7 @@ export default function SwapModalHeader({
         </AutoColumn>
       </LightCard>
       <RowBetween style={{ marginTop: '0.25rem', padding: '0 1rem' }}>
-        <TradePrice price={trade.executionPrice} showInverted={showInverted} setShowInverted={setShowInverted} />
+        <TradePrice price={trade.executionPrice} />
       </RowBetween>
       <LightCard style={{ padding: '.75rem', marginTop: '0.5rem' }}>
         <AdvancedSwapDetails trade={trade} allowedSlippage={allowedSlippage} />
