33fd0f8453397230e084b6703cbf1750b7538f31
Mike <grabbou@gmail.com>
2022-12-20 01:25:20 +0400

    chore: update to latest preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 /* eslint-env node */
 
-/**
- * This is a workaround to allow ESLint to resolve plugins that were installed
- * by an external config: https://github.com/eslint/eslint/issues/3458
- */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@uniswap/eslint-config/load')
 
 module.exports = {
   extends: '@uniswap/eslint-config/react',
