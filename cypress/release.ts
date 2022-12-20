768f269a58cd7318c3017ba7ad3c9a76bb54fe3d
Mike <grabbou@gmail.com>
2022-12-20 22:23:10 +0400

    chore: update todo

--- cypress/release.ts
+++ cypress/release.ts
@@ -11,7 +11,7 @@ describe(
   () => {
     it('loads swap page', () => {
       // We *must* wait in order to space out the retry attempts.
-      // @todo find a better way to do this
+      // TODO: find a better way to do this
       cy.wait(ONE_MINUTE)
         .visit('/', {
           retryOnStatusCodeFailure: true,
