d1237e0d3a7afe44e0e44ea70bdd23916d74ad84
Vignesh Mohankumar <me@vig.xyz>
2022-12-20 14:15:27 -0500

    Revert "test: skip tests due to fiat on-ramp change (#5742)"
    This reverts commit 283479f76e78b99afb6ef0cce699fd16b460b9b1.

--- cypress/e2e/pool.test.ts
+++ cypress/e2e/pool.test.ts
@@ -1,7 +1,7 @@
 describe('Pool', () => {
   beforeEach(() => cy.visit('/pool'))
 
-  xit('add liquidity links to /add/ETH', () => {
+  it('add liquidity links to /add/ETH', () => {
     cy.get('#join-pool-button').click()
     cy.url().should('contain', '/add/ETH')
   })
