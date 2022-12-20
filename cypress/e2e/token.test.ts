55bba21bcb0c5252b3d76dfac2f523e082af2697
Eddie Dugan <eddie.dugan@uniswap.org>
2022-12-20 12:16:18 -0500

    fix: remove manual redirects on token page

--- cypress/e2e/token.test.ts
+++ cypress/e2e/token.test.ts
@@ -10,6 +10,27 @@ describe('Testing tokens on uniswap page', () => {
     cy.get(getTestSelectorStartsWith('token-table')).its('length').should('be.gte', 25)
   })
 
+  it('should keep the same configuration when reloaded: ETH global, OP local', () => {
+    cy.visit('/tokens/ethereum')
+    cy.get(getTestSelector('tokens-network-filter-selected')).click()
+    cy.get(getTestSelector('tokens-network-filter-option-optimism')).click()
+    cy.reload()
+    cy.get(getTestSelector('tokens-network-filter-selected')).should('contain', 'Optimism')
+  })
+
+  it('should have the correct network configuration when reloaded: OP global, Polygon local', () => {
+    cy.get(getTestSelector('chain-selector')).last().click()
+    cy.get(getTestSelector('chain-selector-option-optimism')).click()
+
+    cy.visit('/tokens/ethereum')
+    cy.get(getTestSelector('tokens-network-filter-selected')).click()
+    cy.get(getTestSelector('tokens-network-filter-option-polygon')).click()
+    cy.reload()
+    cy.get(getTestSelector('tokens-network-filter-selected')).should('contain', 'Polygon')
+    // With no wallet connected, reloading the page resets the global network.
+    cy.get(getTestSelector('chain-selector')).last().should('contain', 'Ethereum')
+  })
+
   it('should load go to ethereum token and return to token list page', () => {
     cy.visit('/tokens/ethereum')
     cy.get(getTestSelector('token-table-row-Ether')).click()
--- src/pages/Tokens/index.tsx
+++ src/pages/Tokens/index.tsx
@@ -1,7 +1,6 @@
 import { Trans } from '@lingui/macro'
 import { Trace } from '@uniswap/analytics'
 import { PageName } from '@uniswap/analytics-events'
-import { useWeb3React } from '@web3-react/core'
 import { MAX_WIDTH_MEDIA_BREAKPOINT, MEDIUM_MEDIA_BREAKPOINT } from 'components/Tokens/constants'
 import { filterStringAtom } from 'components/Tokens/state'
 import NetworkFilter from 'components/Tokens/TokenTable/NetworkFilter'
@@ -9,11 +8,9 @@ import SearchBar from 'components/Tokens/TokenTable/SearchBar'
 import TimeSelector from 'components/Tokens/TokenTable/TimeSelector'
 import TokenTable, { LoadingTokenTable } from 'components/Tokens/TokenTable/TokenTable'
 import { PAGE_SIZE } from 'graphql/data/TopTokens'
-import { chainIdToBackendName, isValidBackendChainName } from 'graphql/data/util'
-import { useOnGlobalChainSwitch } from 'hooks/useGlobalChainSwitch'
 import { useResetAtom } from 'jotai/utils'
 import { Suspense, useEffect, useState } from 'react'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import styled from 'styled-components/macro'
 import { ThemedText } from 'theme'
 
@@ -70,28 +67,12 @@ const FiltersWrapper = styled.div`
 const Tokens = () => {
   const resetFilterString = useResetAtom(filterStringAtom)
   const location = useLocation()
-  const navigate = useNavigate()
-  const { chainName: chainNameParam } = useParams<{ chainName?: string }>()
-  const { chainId: connectedChainId } = useWeb3React()
-  const connectedChainName = chainIdToBackendName(connectedChainId)
 
   const [rowCount, setRowCount] = useState(PAGE_SIZE)
 
   useEffect(() => {
     resetFilterString()
   }, [location, resetFilterString])
-
-  useEffect(() => {
-    if (!chainNameParam) {
-      navigate(`/tokens/${connectedChainName.toLowerCase()}`, { replace: true })
-    }
-  }, [chainNameParam, connectedChainName, navigate])
-
-  useOnGlobalChainSwitch((chain) => {
-    if (isValidBackendChainName(chain)) {
-      navigate(`/tokens/${chain.toLowerCase()}`, { replace: true })
-    }
-  })
 
   return (
     <Trace page={PageName.TOKENS_PAGE} shouldLogImpression>
