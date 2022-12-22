40cd73414a9b9d7de3a1ee35e349ee671c482762
Daniel Helm <dan.helm@gmail.com>
2022-12-22 14:56:24 -0600

    make eslint happy

--- src/nft/components/icons.tsx
+++ src/nft/components/icons.tsx
@@ -5,6 +5,7 @@ import { themeVars, vars } from '../css/sprinkles.css'
 
 type SVGProps = React.SVGProps<SVGSVGElement>
 
+// eslint-disable-next-line import/no-unused-modules
 export const UniIcon = (props: SVGProps) => (
   <svg {...props} fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
@@ -427,18 +428,18 @@ export const MagnifyingGlassIcon = (props: SVGProps) => (
   </svg>
 )
 
-export const NavMagnifyingGlassIcon = () => (
-  <svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path d="M21 21L16 16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-  </svg>
-)
+// export const NavMagnifyingGlassIcon = () => (
+//   <svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//     <path
+//       d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     />
+//     <path d="M21 21L16 16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+//   </svg>
+// )
 
 export const BagIcon = (props: SVGProps) => (
   <svg fill="none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...props}>
