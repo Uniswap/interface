d730a484a9290837f3c39941d83e39d815184610
Nguyễn Hoài Danh <33005392+nguyenhoaidanh@users.noreply.github.com>
2022-12-22 21:39:58 +0700

    fix: flex wrap of token search (#1578)

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -31,6 +31,7 @@ export const RowFlat = styled.div`
 `
 
 export const AutoRow = styled(Row)<{ gap?: string; justify?: string }>`
+  flex-wrap: wrap;
   margin: ${({ gap }) => gap && `-${gap}`};
   justify-content: ${({ justify }) => justify && justify};
 
