a215160cf765cefd0a23bc397195454a7cc36660
Charles Bachmeier <charlie@genie.xyz>
2022-12-27 08:50:53 -0500

    update tx complete share icon

--- src/nft/utils/asset.ts
+++ src/nft/utils/asset.ts
@@ -41,6 +41,6 @@ export const generateTweetForPurchase = (assets: UpdatedGenieAsset[], txHashUrl:
   const multipleCollections = assets.length > 0 && assets.some((asset) => asset.address !== assets[0].address)
   const tweetText = `I just purchased ${
     multipleCollections ? `${assets.length} NFTs` : `${assets.length} ${assets[0].collectionName ?? 'NFT'}`
-  } with Uniswap 🦄\n\nhttps://app.uniswap.org/#/nfts/collection/0x60bb1e2aa1c9acafb4d34f71585d7e959f387769\n${txHashUrl}`
+  } with @Uniswap 🦄\n\nhttps://app.uniswap.org/#/nfts/collection/0x60bb1e2aa1c9acafb4d34f71585d7e959f387769\n${txHashUrl}`
   return `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`
 }
