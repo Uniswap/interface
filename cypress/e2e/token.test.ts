fd4430fe69c7ba0d24a2818964dd8eeb891d76af
eddie <66155195+just-toby@users.noreply.github.com>
2023-01-03 11:57:53 -0800

    fix: include descriptions and links for native tokens (#5756)

--- cypress/e2e/token.test.ts
+++ cypress/e2e/token.test.ts
@@ -17,4 +17,18 @@ describe('Testing tokens on uniswap page', () => {
     cy.get(getTestSelector('token-details-return-button')).click()
     cy.get(getTestSelectorStartsWith('token-table')).its('length').should('be.gte', 25)
   })
+
+  it('should go to native token on ethereum and render description', () => {
+    cy.visit('/tokens/ethereum/NATIVE')
+    cy.get(getTestSelector('token-details-about-section')).should('exist')
+    cy.contains('Ethereum is a smart contract platform that enables developers').should('exist')
+    cy.contains('Etherscan').should('exist')
+  })
+
+  it('should go to native token on polygon and render description and links', () => {
+    cy.visit('/tokens/polygon/NATIVE')
+    cy.get(getTestSelector('token-details-about-section')).should('exist')
+    cy.contains('Wrapped Matic on Polygon').should('exist')
+    cy.contains('Block Explorer').should('exist')
+  })
 })
--- src/components/Tokens/TokenDetails/index.tsx
+++ src/components/Tokens/TokenDetails/index.tsx
@@ -206,18 +206,15 @@ export default function TokenDetails({
               priceHigh52W={tokenQueryData?.market?.priceHigh52W?.value}
               priceLow52W={tokenQueryData?.market?.priceLow52W?.value}
             />
-            {!token.isNative && (
-              <>
-                <Hr />
-                <AboutSection
-                  address={address}
-                  description={tokenQueryData?.project?.description}
-                  homepageUrl={tokenQueryData?.project?.homepageUrl}
-                  twitterName={tokenQueryData?.project?.twitterName}
-                />
-                <AddressSection address={address} />
-              </>
-            )}
+            <Hr />
+            <AboutSection
+              address={address}
+              chainId={pageChainId}
+              description={tokenQueryData?.project?.description}
+              homepageUrl={tokenQueryData?.project?.homepageUrl}
+              twitterName={tokenQueryData?.project?.twitterName}
+            />
+            {!token.isNative && <AddressSection address={address} />}
           </LeftPanel>
         ) : (
           <TokenDetailsSkeleton />
