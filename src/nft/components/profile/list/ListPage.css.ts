24504dc9bf2a441bf58a66b704aaf747d38435ef
Charles Bachmeier <charlie@genie.xyz>
2023-01-04 11:30:21 -0800

    remove more unused styles

--- src/nft/components/profile/list/ListPage.css.ts
+++ src/nft/components/profile/list/ListPage.css.ts
@@ -1,19 +1,4 @@
 import { style } from '@vanilla-extract/css'
-import { themeVars } from 'nft/css/sprinkles.css'
-
-export const chevronDown = style({
-  transform: 'rotate(180deg)',
-})
-
-export const dropdown = style({
-  boxShadow: `0px 4px 16px ${themeVars.colors.backgroundSurface}`,
-  marginLeft: '-12px',
-})
-
-export const removeAsset = style({
-  top: '31px',
-  left: '8px',
-})
 
 export const removeMarketplace = style({
   top: '11px',
