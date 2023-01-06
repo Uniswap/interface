53b726c59dcad77576cd24e79e101a00cb640a52
Spencer Yen <spencersyen@gmail.com>
2023-01-05 17:58:59 -0800

    chore: Add Sentry log for when notification received while app is open (#2835)

--- src/features/notifications/Onesignal.ts
+++ src/features/notifications/Onesignal.ts
@@ -1,19 +1,33 @@
 import { Linking } from 'react-native'
 import OneSignal, { NotificationReceivedEvent, OpenedEvent } from 'react-native-onesignal'
 import { config } from 'src/config'
+import { getOneSignalUserIdOrError } from 'src/features/firebase/utils'
 import { logger } from 'src/utils/logger'
 
 export const initOneSignal = () => {
   OneSignal.setLogLevel(6, 0)
   OneSignal.setAppId(config.onesignalAppId)
 
-  OneSignal.setNotificationWillShowInForegroundHandler((event: NotificationReceivedEvent) => {
+  OneSignal.setNotificationWillShowInForegroundHandler(async (event: NotificationReceivedEvent) => {
+    try {
+      // Log to Sentry when a push notification is received while app is open.
+      // Used to debug users reporting not receiving notifications.
+      const pushId = await getOneSignalUserIdOrError()
+      logger.info(
+        'Onesignal',
+        'NotificationWillShowInForeground',
+        `${pushId} received notification while app is open: ${event.getNotification().body}`
+      )
+    } catch (error) {
+      logger.error('Onesignal', 'NotificationWillShowInForeground', 'Error:', error)
+    }
+
     // Complete with undefined means don't show OS notifications while app is in foreground
     event.complete()
   })
 
   OneSignal.setNotificationOpenedHandler((event: OpenedEvent) => {
-    logger.info(
+    logger.debug(
       'Onesignal',
       'setNotificationOpenedHandler',
       `Notification opened: ${event.notification}`
