7d50603dde8647875b7215099d1eff3108fa6773
Charles Bachmeier <charlie@genie.xyz>
2023-01-11 11:38:08 -0800

    toggle bag if flag enabled

--- src/nft/components/profile/list/ListPage.tsx
+++ src/nft/components/profile/list/ListPage.tsx
@@ -85,8 +85,8 @@ const FloatingConfirmationBar = styled(Row)`
   background: ${({ theme }) => theme.backgroundSurface};
   position: fixed;
   bottom: 32px;
-  margin: 0px 60px;
-  width: calc(100vw - 120px);
+  margin: 0px 156px;
+  width: calc(100vw - 312px);
   z-index: ${Z_INDEX.under_dropdown};
 `
 
