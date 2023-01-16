c455eea38bf2392f9a524a68448bffe0127360a4
Nguyễn Hoài Danh <33005392+nguyenhoaidanh@users.noreply.github.com>
2023-01-16 14:10:37 +0700

    fix: nonce issues Limit order (#1652)
    * fix nonce issues
    
    * speed up

--- src/components/swapv2/LimitOrder/ListOrder/index.tsx
+++ src/components/swapv2/LimitOrder/ListOrder/index.tsx
@@ -416,7 +416,11 @@ export default forwardRef<ListOrderHandle>(function ListLimitOrder(props, ref) {
       attemptingTxn: true,
     }))
 
-    const { encodedData } = await getEncodeData([order?.id].filter(Boolean) as number[], isCancelAll)
+    const [{ encodedData }, nonce] = await Promise.all([
+      getEncodeData([order?.id].filter(Boolean) as number[], isCancelAll),
+      isCancelAll ? limitOrderContract.nonce(account) : Promise.resolve(BigNumber.from(0)),
+    ])
+
     const response = await sendEVMTransaction(
       account,
       library,
@@ -427,11 +431,7 @@ export default forwardRef<ListOrderHandle>(function ListLimitOrder(props, ref) {
     const newOrders = isCancelAll ? orders.map(e => e.id) : order?.id ? [order?.id] : []
     setCancellingOrders({ orderIds: cancellingOrdersIds.concat(newOrders) })
 
-    if (response?.hash && account) {
-      let nonce: BigNumber = BigNumber.from(0)
-      if (isCancelAll) {
-        nonce = await limitOrderContract.nonce(account)
-      }
+    if (response?.hash) {
       insertCancellingOrder({
         maker: account,
         chainId: chainId.toString(),
