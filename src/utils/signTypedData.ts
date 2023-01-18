b2ceae68f92d6da9a88d20e50494edde191ce2b7
Zach Pomerantz <zzmp@uniswap.org>
2023-01-18 11:36:19 -0800

    chore: clean up comments/logs

--- src/utils/signTypedData.ts
+++ src/utils/signTypedData.ts
@@ -2,8 +2,9 @@ import { _TypedDataEncoder } from '@ethersproject/hash'
 import { JsonRpcSigner } from '@ethersproject/providers'
 
 /**
- * Overrides the _signTypedData method to add support for wallets without EIP-712 support (eg Zerion).
- * Adds a fallback to eth_sign.
+ * Overrides the _signTypedData method to add support for wallets without EIP-712 support (eg Zerion),
+ * by adding a fallback to eth_sign.
+ * @see https://github.com/ethers-io/ethers.js/blob/c80fcddf50a9023486e9f9acb1848aba4c19f7b6/packages/providers/src.ts/json-rpc-provider.ts#L334
  */
 JsonRpcSigner.prototype._signTypedData = async function signTypedDataWithFallbacks(this, domain, types, value) {
   // Populate any ENS names (in-place)
@@ -14,18 +15,14 @@ JsonRpcSigner.prototype._signTypedData = async function signTypedDataWithFallbac
   const address = (await this.getAddress()).toLowerCase()
   const payload = JSON.stringify(_TypedDataEncoder.getPayload(populated.domain, types, populated.value))
 
-  return this.provider
-    .send('eth_signTypedData_v4', [address, payload])
-    .catch((e) => {
-      if ('message' in e && e.message.match(/not found/i)) {
-        console.warn('eth_signTypedData_v4 failed, falling back to eth_sign:', e)
-        const hash = _TypedDataEncoder.hash(populated.domain, types, populated.value)
-        return this.provider.send('eth_sign', [address, hash])
-      }
-      throw e
-    })
-    .catch((e) => {
+  return this.provider.send('eth_signTypedData_v4', [address, payload]).catch((e) => {
+    if (!('message' in e && e.message.match(/not found/i))) throw e
+
+    console.warn('eth_signTypedData_v4 failed, falling back to eth_sign:', e)
+    const hash = _TypedDataEncoder.hash(populated.domain, types, populated.value)
+    return this.provider.send('eth_sign', [address, hash]).catch((e) => {
       console.warn('eth_sign failed:', e)
       throw e
     })
+  })
 }
