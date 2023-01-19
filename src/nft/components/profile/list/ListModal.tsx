a59f15f0b3fadea6b8be352dedf0a1db1d4c18b9
Charles Bachmeier <charlie@genie.xyz>
2023-01-19 11:45:21 -0800

    init modal title

--- src/nft/components/profile/list/ListModal.tsx
+++ src/nft/components/profile/list/ListModal.tsx
@@ -1,7 +1,10 @@
-import ListingModal from 'nft/components/bag/profile/ListingModal'
+import { Trans } from '@lingui/macro'
+import Row from 'components/Row'
 import { Portal } from 'nft/components/common/Portal'
 import { Overlay } from 'nft/components/modals/Overlay'
+import { X } from 'react-feather'
 import styled from 'styled-components/macro'
+import { ThemedText } from 'theme'
 import { Z_INDEX } from 'theme/zIndex'
 
 const ListModalWrapper = styled.div`
@@ -13,16 +16,31 @@ const ListModalWrapper = styled.div`
   z-index: ${Z_INDEX.modalOverTooltip};
   background: ${({ theme }) => theme.backgroundSurface};
   border-radius: 20px;
-  border: ${({ theme }) => `1px solid ${theme.backgroundOutline}`};
+  border: 1px solid ${({ theme }) => theme.backgroundOutline};
   box-shadow: ${({ theme }) => theme.deepShadow};
-  padding: 0px 12px 4px;
+  padding: 20px 24px 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+const TitleRow = styled(Row)`
+  justify-content: space-between;
+  margin-bottom: 8px;
 `
 
 export const ListModal = ({ overlayClick }: { overlayClick: () => void }) => {
   return (
     <Portal>
       <ListModalWrapper>
-        <ListingModal />
+        <TitleRow>
+          <ThemedText.HeadlineSmall lineHeight="28px">
+            <Trans>List NFTs</Trans>
+          </ThemedText.HeadlineSmall>
+          <X size={24} cursor="pointer" onClick={overlayClick} />
+        </TitleRow>
+        <span>Approvals</span>
+        <span>Listings</span>
       </ListModalWrapper>
       <Overlay onClick={overlayClick} />
     </Portal>
