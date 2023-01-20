08b46c5ffc574e41ea124641eca13bf8ace32ae9
Charles Bachmeier <charlie@genie.xyz>
2023-01-19 22:27:45 -0800

    added left border to section

--- src/nft/components/profile/list/ListModal.tsx
+++ src/nft/components/profile/list/ListModal.tsx
@@ -1,12 +1,17 @@
-import { Trans } from '@lingui/macro'
+import { Plural, Trans } from '@lingui/macro'
+import { Trace } from '@uniswap/analytics'
+import { InterfaceModalName } from '@uniswap/analytics-events'
+import Column from 'components/Column'
 import Row from 'components/Row'
 import { Portal } from 'nft/components/common/Portal'
 import { ChevronUpIcon, ListingModalWindowActive, ListingModalWindowClosed } from 'nft/components/icons'
 import { Overlay } from 'nft/components/modals/Overlay'
+import { useNFTList } from 'nft/hooks'
 import { useReducer } from 'react'
 import { X } from 'react-feather'
 import styled, { useTheme } from 'styled-components/macro'
 import { ThemedText } from 'theme'
+import { colors } from 'theme/colors'
 import { TRANSITION_DURATIONS } from 'theme/styles'
 import { Z_INDEX } from 'theme/zIndex'
 
@@ -49,6 +54,14 @@ const SectionArrow = styled(ChevronUpIcon)<{ active: boolean }>`
   transform: rotate(${({ active }) => (active ? 0 : 180)}deg);
 `
 
+const SectionBody = styled(Column)`
+  height: 280px; //TODO make dynamic
+  border-left: 1.5px solid ${colors.gray650};
+  margin: 4px 0px 0px 7px;
+  padding-left: 20px;
+  padding-top: 4px;
+`
+
 enum Section {
   APPROVE,
   SIGN,
@@ -56,46 +69,61 @@ enum Section {
 
 export const ListModal = ({ overlayClick }: { overlayClick: () => void }) => {
   const theme = useTheme()
+  const listings = useNFTList((state) => state.listings)
+  const collectionsRequiringApproval = useNFTList((state) => state.collectionsRequiringApproval)
   const [openSection, toggleOpenSection] = useReducer(
     (s) => (s === Section.APPROVE ? Section.SIGN : Section.APPROVE),
     Section.APPROVE
   )
   return (
     <Portal>
-      <ListModalWrapper>
-        <TitleRow>
-          <ThemedText.HeadlineSmall lineHeight="28px">
-            <Trans>List NFTs</Trans>
-          </ThemedText.HeadlineSmall>
-          <X size={24} cursor="pointer" onClick={overlayClick} />
-        </TitleRow>
-        <SectionHeader>
-          <Row>
-            {openSection === Section.APPROVE ? <ListingModalWindowActive /> : <ListingModalWindowClosed />}
-            <SectionTitle active={openSection === Section.APPROVE} marginLeft="12px">
-              <Trans>Approve X Collections</Trans>
-            </SectionTitle>
-          </Row>
-          <SectionArrow
-            active={openSection === Section.APPROVE}
-            secondaryColor={openSection === Section.APPROVE ? theme.textPrimary : theme.textSecondary}
-            onClick={toggleOpenSection}
-          />
-        </SectionHeader>
-        <SectionHeader>
-          <Row>
-            {openSection === Section.SIGN ? <ListingModalWindowActive /> : <ListingModalWindowClosed />}
-            <SectionTitle active={openSection === Section.SIGN} marginLeft="12px">
-              <Trans>Sign X Listings</Trans>
-            </SectionTitle>
-          </Row>
-          <SectionArrow
-            active={openSection === Section.SIGN}
-            secondaryColor={openSection === Section.SIGN ? theme.textPrimary : theme.textSecondary}
-            onClick={toggleOpenSection}
-          />
-        </SectionHeader>
-      </ListModalWrapper>
+      <Trace modal={InterfaceModalName.NFT_LISTING}>
+        <ListModalWrapper>
+          <TitleRow>
+            <ThemedText.HeadlineSmall lineHeight="28px">
+              <Trans>List NFTs</Trans>
+            </ThemedText.HeadlineSmall>
+            <X size={24} cursor="pointer" onClick={overlayClick} />
+          </TitleRow>
+          <Column>
+            <SectionHeader>
+              <Row>
+                {openSection === Section.APPROVE ? <ListingModalWindowActive /> : <ListingModalWindowClosed />}
+                <SectionTitle active={openSection === Section.APPROVE} marginLeft="12px">
+                  <Trans>Approve</Trans>&nbsp;{collectionsRequiringApproval.length}&nbsp;
+                  <Plural value={collectionsRequiringApproval.length} _1="Collection" other="Collections" />
+                </SectionTitle>
+              </Row>
+              <SectionArrow
+                active={openSection === Section.APPROVE}
+                secondaryColor={openSection === Section.APPROVE ? theme.textPrimary : theme.textSecondary}
+                onClick={toggleOpenSection}
+              />
+            </SectionHeader>
+            {openSection === Section.APPROVE && (
+              <SectionBody>
+                <ThemedText.Caption lineHeight="16px" color="textSecondary">
+                  <Trans>Why is a transaction required?</Trans>
+                </ThemedText.Caption>
+              </SectionBody>
+            )}
+          </Column>
+          <SectionHeader>
+            <Row>
+              {openSection === Section.SIGN ? <ListingModalWindowActive /> : <ListingModalWindowClosed />}
+              <SectionTitle active={openSection === Section.SIGN} marginLeft="12px">
+                <Trans>Sign</Trans> &nbsp;{listings.length}&nbsp;{' '}
+                <Plural value={listings.length} _1="Listing" other="Listings" />
+              </SectionTitle>
+            </Row>
+            <SectionArrow
+              active={openSection === Section.SIGN}
+              secondaryColor={openSection === Section.SIGN ? theme.textPrimary : theme.textSecondary}
+              onClick={toggleOpenSection}
+            />
+          </SectionHeader>
+        </ListModalWrapper>
+      </Trace>
       <Overlay onClick={overlayClick} />
     </Portal>
   )
