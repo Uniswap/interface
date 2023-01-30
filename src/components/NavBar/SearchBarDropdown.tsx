fd6aeff100e17035640349ded8ea63dbbea40d0d
cartcrom <cartergcromer@gmail.com>
2023-01-30 13:31:47 -0500

    fix: removed fetch policy overrides

--- src/components/NavBar/SearchBarDropdown.tsx
+++ src/components/NavBar/SearchBarDropdown.tsx
@@ -146,8 +146,6 @@ export const SearchBarDropdown = ({
   const { chainId } = useWeb3React()
   const { data: trendingTokenData, loading: trendingTokensAreLoading } = useTrendingTokensQuery({
     variables: { chain: chainIdToBackendName(chainId) },
-    fetchPolicy: 'network-only', // Used for first execution
-    nextFetchPolicy: 'cache-first', // Used for subsequent executions
   })
 
   const trendingTokenResults = useMemo(
