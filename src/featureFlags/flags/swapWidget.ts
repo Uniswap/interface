a54d29e576b1c09b536015bc24d4060fc681b738
Eddie Dugan <eddie.dugan@uniswap.org>
2023-02-01 15:23:23 -0800

    feat: add new flag to modal

--- src/featureFlags/flags/swapWidget.ts
+++ src/featureFlags/flags/swapWidget.ts
@@ -1,9 +1,11 @@
 import { BaseVariant, FeatureFlag, useBaseFlag } from '../index'
 
-function useSwapWidgetFlag(): BaseVariant {
+export function useSwapWidgetFlag(): BaseVariant {
   return useBaseFlag(FeatureFlag.swapWidget, BaseVariant.Control)
 }
 
 export function useSwapWidgetEnabled(): boolean {
   return useSwapWidgetFlag() === BaseVariant.Enabled
 }
+
+export { BaseVariant as SwapWidgetVariant }
