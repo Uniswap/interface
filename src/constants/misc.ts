ff9c04ec0c061b6db74d70975aa8dc49e3f78f48
Justin Domingue <judo@uniswap.org>
2023-02-02 11:25:19 -0500

    perf: increase polling on dev builds to test out performance assumptions (#3011)

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -1,11 +1,17 @@
+import { ONE_MINUTE_MS } from 'src/utils/time'
+import { isDevBuild } from 'src/utils/version'
+
 export const DEFAULT_SLIPPAGE_TOLERANCE = 5
-export const DEFAULT_DEADLINE_S = 60 * 30 // 30 minutes
+export const DEFAULT_DEADLINE_S = 30 * ONE_MINUTE_MS
+
+// temporarily increase polling on dev builds to test out performance assumptions
+const scalingFactor = isDevBuild() ? 5 : 1
 
 // Polling interval (in milliseconds) for RTK-Query
 export enum PollingInterval {
-  Slow = 5 * 60 * 1000, // 5 minutes
-  Normal = 60 * 1000, // 1 minute
-  Fast = 12 * 1000, // 12 seconds, block times for mainnet
+  Slow = scalingFactor * 5 * ONE_MINUTE_MS * 1000,
+  Normal = scalingFactor * ONE_MINUTE_MS * 1000,
+  Fast = scalingFactor * 12 * 1000, // block times for mainnet
   LightningMcQueen = 3 * 1000, // 3 seconds, approx block times for polygon
 }
 
