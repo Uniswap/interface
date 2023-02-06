4f8185c62737f44cac3a4baa6d5ffc499e97edaf
Justin Domingue <judo@uniswap.org>
2023-02-06 16:34:33 -0500

    fix: increase polling interval value on dev (#3028)

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -1,14 +1,14 @@
 import { ONE_MINUTE_MS, ONE_SECOND_MS } from 'src/utils/time'
 import { isDevBuild } from 'src/utils/version'
 
-// temporarily increase polling on dev builds to test out performance assumptions
-const scalingFactor = isDevBuild() ? 0.2 : 1
+// temporarily slow down polling on dev builds to test out performance assumptions
+const scalingFactor = isDevBuild() ? 5 : 1
 
 // Polling interval (in milliseconds) for RTK-Query
 export enum PollingInterval {
-  Slow = Math.round(scalingFactor * 5 * ONE_MINUTE_MS),
-  Normal = Math.round(scalingFactor * ONE_MINUTE_MS),
-  Fast = Math.round(scalingFactor * 12 * ONE_SECOND_MS), // block times for mainnet
+  Slow = scalingFactor * 5 * ONE_MINUTE_MS,
+  Normal = scalingFactor * ONE_MINUTE_MS,
+  Fast = scalingFactor * 12 * ONE_SECOND_MS, // block times for mainnet
   LightningMcQueen = 3 * ONE_SECOND_MS, // 3 seconds, approx block times for polygon
 }
 
