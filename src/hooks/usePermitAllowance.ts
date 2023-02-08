d2529c5cca0d98d72bad11c649b7800804838be4
Zach Pomerantz <zzmp@uniswap.org>
2023-02-07 18:24:15 -0800

    test: https://github.com/Uniswap/conedison/pull/9

--- src/hooks/usePermitAllowance.ts
+++ src/hooks/usePermitAllowance.ts
@@ -1,6 +1,9 @@
-import { signTypedData } from '@uniswap/conedison/provider'
+import type { TypedDataDomain, TypedDataField } from '@ethersproject/abstract-signer'
+import { _TypedDataEncoder } from '@ethersproject/hash'
+import type { ExternalProvider, JsonRpcProvider, JsonRpcSigner, Web3Provider } from '@ethersproject/providers'
 import { AllowanceTransfer, MaxAllowanceTransferAmount, PERMIT2_ADDRESS, PermitSingle } from '@uniswap/permit2-sdk'
 import { CurrencyAmount, Token } from '@uniswap/sdk-core'
+import WalletConnectProvider from '@walletconnect/ethereum-provider'
 import { useWeb3React } from '@web3-react/core'
 import PERMIT2_ABI from 'abis/permit2.json'
 import { Permit2 } from 'abis/types'
@@ -85,4 +88,67 @@ export function useUpdatePermitAllowance(
       throw new Error(`${symbol} permit allowance failed: ${e instanceof Error ? e.message : e}`)
     }
   }, [account, chainId, nonce, onPermitSignature, provider, spender, token])
+}
+
+function isWeb3Provider(provider: JsonRpcProvider): provider is Web3Provider {
+  return 'provider' in provider
+}
+
+function isWalletConnectProvider(provider: ExternalProvider): provider is WalletConnectProvider {
+  return (provider as WalletConnectProvider).isWalletConnect
+}
+
+type PeerMeta = NonNullable<WalletConnectProvider['connector']['peerMeta']>
+
+function getPeerMeta(provider: JsonRpcProvider): PeerMeta | undefined {
+  if (isWeb3Provider(provider) && isWalletConnectProvider(provider.provider)) {
+    return provider.provider.connector.peerMeta ?? undefined
+  } else {
+    return
+  }
+}
+
+function supportsV4(provider: JsonRpcProvider): boolean {
+  const name = getPeerMeta(provider)?.name
+  if (name && ['SafePal Wallet'].includes(name)) {
+    return false
+  }
+
+  return true
+}
+
+/**
+ * Signs TypedData with EIP-712, if available, or else by falling back to eth_sign.
+ * Calls eth_signTypedData_v4, or eth_signTypedData for wallets with incomplete EIP-712 support.
+ *
+ * @see https://github.com/ethers-io/ethers.js/blob/c80fcddf50a9023486e9f9acb1848aba4c19f7b6/packages/providers/src.ts/json-rpc-provider.ts#L334
+ */
+async function signTypedData(
+  signer: JsonRpcSigner,
+  domain: TypedDataDomain,
+  types: Record<string, TypedDataField[]>,
+  // Use Record<string, any> for the value to match the JsonRpcSigner._signTypedData signature.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  value: Record<string, any>
+) {
+  // Populate any ENS names (in-place)
+  const populated = await _TypedDataEncoder.resolveNames(domain, types, value, (name: string) => {
+    return signer.provider.resolveName(name) as Promise<string>
+  })
+
+  const method = supportsV4(signer.provider) ? 'eth_signTypedData_v4' : 'eth_signTypedData'
+  const address = (await signer.getAddress()).toLowerCase()
+  const message = JSON.stringify(_TypedDataEncoder.getPayload(populated.domain, types, populated.value))
+
+  try {
+    return await signer.provider.send(method, [address, message])
+  } catch (error) {
+    // If eth_signTypedData is unimplemented, fall back to eth_sign.
+    if (typeof error.message === 'string' && error.message.match(/not (found|implemented)/i)) {
+      console.warn('signTypedData: wallet does not implement EIP-712, falling back to eth_sign', error.message)
+      const hash = _TypedDataEncoder.hash(populated.domain, types, populated.value)
+      return await signer.provider.send('eth_sign', [address, hash])
+    }
+    throw error
+  }
 }
