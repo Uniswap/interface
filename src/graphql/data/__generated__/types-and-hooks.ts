4203a108964796e8e572189db347108b7cf1e1ef
cartcrom <cartergcromer@gmail.com>
2023-02-10 13:22:13 -0500

    fix: update apollo types

--- src/graphql/data/__generated__/types-and-hooks.ts
+++ src/graphql/data/__generated__/types-and-hooks.ts
@@ -1094,64 +1094,6 @@ export const RecentlySearchedAssetsDocument = gql`
 }
     `;
 
-export const RecentlySearchedAssetsDocument = gql`
-    query RecentlySearchedAssets($collectionAddresses: [String!]!, $contracts: [ContractInput!]!) {
-  nftCollections(filter: {addresses: $collectionAddresses}) {
-    edges {
-      node {
-        collectionId
-        image {
-          url
-        }
-        isVerified
-        name
-        numAssets
-        nftContracts {
-          address
-        }
-        markets(currencies: ETH) {
-          floorPrice {
-            currency
-            value
-          }
-        }
-      }
-    }
-  }
-  tokens(contracts: $contracts) {
-    id
-    decimals
-    name
-    chain
-    standard
-    address
-    symbol
-    market(currency: USD) {
-      id
-      price {
-        id
-        value
-        currency
-      }
-      pricePercentChange(duration: DAY) {
-        id
-        value
-      }
-      volume24H: volume(duration: DAY) {
-        id
-        value
-        currency
-      }
-    }
-    project {
-      id
-      logoUrl
-      safetyLevel
-    }
-  }
-}
-    `;
-
 /**
  * __useRecentlySearchedAssetsQuery__
  *
