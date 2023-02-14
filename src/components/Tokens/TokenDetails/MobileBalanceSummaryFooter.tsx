94c214d83c7a99a6d505e11024ddd79f99c36162
Eddie Dugan <eddie.dugan@uniswap.org>
2023-02-14 14:04:37 -0800

    feat: use amplitude user ID

--- src/components/Tokens/TokenDetails/MobileBalanceSummaryFooter.tsx
+++ src/components/Tokens/TokenDetails/MobileBalanceSummaryFooter.tsx
@@ -87,7 +87,7 @@ export default function MobileBalanceSummaryFooter({ token }: { token: Currency 
   const formattedBalance = formatCurrencyAmount(balance, NumberType.TokenNonTx)
   const formattedUsdValue = formatCurrencyAmount(useStablecoinValue(balance), NumberType.FiatTokenStats)
   const chain = CHAIN_ID_TO_BACKEND_NAME[token.chainId].toLowerCase()
-  const enabled = useGate('web_first_feature_flag')
+  const enabled = useGate('web_dummy_gate_amplitude_id')
 
   return (
     <Wrapper>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,13 @@
-import { initializeAnalytics, OriginApplication, sendAnalyticsEvent, Trace, user } from '@uniswap/analytics'
+import {
+  getDeviceId,
+  getSessionId,
+  getUserId,
+  initializeAnalytics,
+  OriginApplication,
+  sendAnalyticsEvent,
+  Trace,
+  user,
+} from '@uniswap/analytics'
 import { CustomUserProperties, getBrowser, InterfacePageName, SharedEventName } from '@uniswap/analytics-events'
 import Loader from 'components/Loader'
 import { MenuDropdown } from 'components/NavBar/MenuDropdown'
@@ -9,11 +18,12 @@ import { Box } from 'nft/components/Box'
 import { lazy, Suspense, useEffect, useState } from 'react'
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import { useIsDarkMode } from 'state/user/hooks'
+import { StatsigProvider } from 'statsig-react'
 import styled from 'styled-components/macro'
 import { SpinnerSVG } from 'theme/components'
 import { flexRowNoWrap } from 'theme/styles'
 import { Z_INDEX } from 'theme/zIndex'
-import { isProductionEnv } from 'utils/env'
+import { getEnvName, isProductionEnv } from 'utils/env'
 import { getCLS, getFCP, getFID, getLCP, Metric } from 'web-vitals'
 
 import { useAnalyticsReporter } from '../components/analytics'
@@ -186,123 +196,136 @@ export default function App() {
 
   return (
     <ErrorBoundary>
-      <DarkModeQueryParamReader />
-      <ApeModeQueryParamReader />
-      <Trace page={currentPage}>
-        <HeaderWrapper transparent={isHeaderTransparent}>
-          <NavBar />
-        </HeaderWrapper>
-        <BodyWrapper>
-          <Popups />
-          <Polling />
-          <TopLevelModals />
-          <Suspense fallback={<Loader />}>
-            {isLoaded ? (
-              <Routes>
-                <Route path="/" element={<Landing />} />
+      <StatsigProvider
+        user={{
+          userID: getUserId(),
+          customIDs: { device_id: getDeviceId() ?? '', session_id: String(getSessionId() ?? -1) },
+        }}
+        // TODO: replace with proxy and cycle key
+        sdkKey="client-1rY92WZGidd2hgW4x1lsZ7afqm1Qfr3sJfH3A5b8eJa"
+        waitForInitialization={true}
+        options={{
+          environment: { tier: getEnvName() },
+        }}
+      >
+        <DarkModeQueryParamReader />
+        <ApeModeQueryParamReader />
+        <Trace page={currentPage}>
+          <HeaderWrapper transparent={isHeaderTransparent}>
+            <NavBar />
+          </HeaderWrapper>
+          <BodyWrapper>
+            <Popups />
+            <Polling />
+            <TopLevelModals />
+            <Suspense fallback={<Loader />}>
+              {isLoaded ? (
+                <Routes>
+                  <Route path="/" element={<Landing />} />
 
-                <Route path="tokens" element={<Tokens />}>
-                  <Route path=":chainName" />
-                </Route>
-                <Route path="tokens/:chainName/:tokenAddress" element={<TokenDetails />} />
-                <Route
-                  path="vote/*"
-                  element={
-                    <Suspense fallback={<LazyLoadSpinner />}>
-                      <Vote />
-                    </Suspense>
-                  }
-                />
-                <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} />
+                  <Route path="tokens" element={<Tokens />}>
+                    <Route path=":chainName" />
+                  </Route>
+                  <Route path="tokens/:chainName/:tokenAddress" element={<TokenDetails />} />
+                  <Route
+                    path="vote/*"
+                    element={
+                      <Suspense fallback={<LazyLoadSpinner />}>
+                        <Vote />
+                      </Suspense>
+                    }
+                  />
+                  <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} />
 
-                <Route path="send" element={<RedirectPathToSwapOnly />} />
-                <Route path="swap" element={<Swap />} />
+                  <Route path="send" element={<RedirectPathToSwapOnly />} />
+                  <Route path="swap" element={<Swap />} />
 
-                <Route path="pool/v2/find" element={<PoolFinder />} />
-                <Route path="pool/v2" element={<PoolV2 />} />
-                <Route path="pool" element={<Pool />} />
-                <Route path="pool/:tokenId" element={<PositionPage />} />
+                  <Route path="pool/v2/find" element={<PoolFinder />} />
+                  <Route path="pool/v2" element={<PoolV2 />} />
+                  <Route path="pool" element={<Pool />} />
+                  <Route path="pool/:tokenId" element={<PositionPage />} />
 
-                <Route path="add/v2" element={<RedirectDuplicateTokenIdsV2 />}>
-                  <Route path=":currencyIdA" />
-                  <Route path=":currencyIdA/:currencyIdB" />
-                </Route>
-                <Route path="add" element={<RedirectDuplicateTokenIds />}>
-                  {/* this is workaround since react-router-dom v6 doesn't support optional parameters any more */}
-                  <Route path=":currencyIdA" />
-                  <Route path=":currencyIdA/:currencyIdB" />
-                  <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
-                </Route>
+                  <Route path="add/v2" element={<RedirectDuplicateTokenIdsV2 />}>
+                    <Route path=":currencyIdA" />
+                    <Route path=":currencyIdA/:currencyIdB" />
+                  </Route>
+                  <Route path="add" element={<RedirectDuplicateTokenIds />}>
+                    {/* this is workaround since react-router-dom v6 doesn't support optional parameters any more */}
+                    <Route path=":currencyIdA" />
+                    <Route path=":currencyIdA/:currencyIdB" />
+                    <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
+                  </Route>
 
-                <Route path="increase" element={<AddLiquidity />}>
-                  <Route path=":currencyIdA" />
-                  <Route path=":currencyIdA/:currencyIdB" />
-                  <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
-                  <Route path=":currencyIdA/:currencyIdB/:feeAmount/:tokenId" />
-                </Route>
+                  <Route path="increase" element={<AddLiquidity />}>
+                    <Route path=":currencyIdA" />
+                    <Route path=":currencyIdA/:currencyIdB" />
+                    <Route path=":currencyIdA/:currencyIdB/:feeAmount" />
+                    <Route path=":currencyIdA/:currencyIdB/:feeAmount/:tokenId" />
+                  </Route>
 
-                <Route path="remove/v2/:currencyIdA/:currencyIdB" element={<RemoveLiquidity />} />
-                <Route path="remove/:tokenId" element={<RemoveLiquidityV3 />} />
+                  <Route path="remove/v2/:currencyIdA/:currencyIdB" element={<RemoveLiquidity />} />
+                  <Route path="remove/:tokenId" element={<RemoveLiquidityV3 />} />
 
-                <Route path="migrate/v2" element={<MigrateV2 />} />
-                <Route path="migrate/v2/:address" element={<MigrateV2Pair />} />
+                  <Route path="migrate/v2" element={<MigrateV2 />} />
+                  <Route path="migrate/v2/:address" element={<MigrateV2Pair />} />
 
-                <Route
-                  path="/nfts"
-                  element={
-                    <Suspense fallback={null}>
-                      <NftExplore />
-                    </Suspense>
-                  }
-                />
-                <Route
-                  path="/nfts/asset/:contractAddress/:tokenId"
-                  element={
-                    <Suspense fallback={null}>
-                      <Asset />
-                    </Suspense>
-                  }
-                />
-                <Route
-                  path="/nfts/profile"
-                  element={
-                    <Suspense fallback={null}>
-                      <Profile />
-                    </Suspense>
-                  }
-                />
-                <Route
-                  path="/nfts/collection/:contractAddress"
-                  element={
-                    <Suspense fallback={null}>
-                      <Collection />
-                    </Suspense>
-                  }
-                />
-                <Route
-                  path="/nfts/collection/:contractAddress/activity"
-                  element={
-                    <Suspense fallback={null}>
-                      <Collection />
-                    </Suspense>
-                  }
-                />
+                  <Route
+                    path="/nfts"
+                    element={
+                      <Suspense fallback={null}>
+                        <NftExplore />
+                      </Suspense>
+                    }
+                  />
+                  <Route
+                    path="/nfts/asset/:contractAddress/:tokenId"
+                    element={
+                      <Suspense fallback={null}>
+                        <Asset />
+                      </Suspense>
+                    }
+                  />
+                  <Route
+                    path="/nfts/profile"
+                    element={
+                      <Suspense fallback={null}>
+                        <Profile />
+                      </Suspense>
+                    }
+                  />
+                  <Route
+                    path="/nfts/collection/:contractAddress"
+                    element={
+                      <Suspense fallback={null}>
+                        <Collection />
+                      </Suspense>
+                    }
+                  />
+                  <Route
+                    path="/nfts/collection/:contractAddress/activity"
+                    element={
+                      <Suspense fallback={null}>
+                        <Collection />
+                      </Suspense>
+                    }
+                  />
 
-                <Route path="*" element={<Navigate to="/not-found" replace />} />
-                <Route path="/not-found" element={<NotFound />} />
-              </Routes>
-            ) : (
-              <Loader />
-            )}
-          </Suspense>
-        </BodyWrapper>
-        <MobileBottomBar>
-          <PageTabs />
-          <Box marginY="4">
-            <MenuDropdown />
-          </Box>
-        </MobileBottomBar>
-      </Trace>
+                  <Route path="*" element={<Navigate to="/not-found" replace />} />
+                  <Route path="/not-found" element={<NotFound />} />
+                </Routes>
+              ) : (
+                <Loader />
+              )}
+            </Suspense>
+          </BodyWrapper>
+          <MobileBottomBar>
+            <PageTabs />
+            <Box marginY="4">
+              <MenuDropdown />
+            </Box>
+          </MobileBottomBar>
+        </Trace>
+      </StatsigProvider>
     </ErrorBoundary>
   )
 }
