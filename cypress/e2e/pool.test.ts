1e29496ed11681e775e929181cbd08f8ef206292
Eddie Dugan <eddie.dugan@uniswap.org>
2023-02-15 09:58:53 -0800

    fix: use localStorage to dismiss FoR announcement in test

--- cypress/e2e/pool.test.ts
+++ cypress/e2e/pool.test.ts
@@ -1,10 +1,11 @@
-import { getTestSelector } from '../utils'
-
 describe('Pool', () => {
-  beforeEach(() => cy.visit('/pool'))
+  beforeEach(() => {
+    cy.visit('/pool').then(() => {
+      window.localStorage.setItem('FiatOnrampAnnouncement-dismissed', 'true')
+    })
+  })
 
   it('add liquidity links to /add/ETH', () => {
-    cy.get(getTestSelector('FiatOnrampAnnouncement-close')).first().click()
     cy.get('#join-pool-button').click()
     cy.url().should('contain', '/add/ETH')
   })
