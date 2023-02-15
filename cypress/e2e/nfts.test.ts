fa06e407ec77adb34f0cf3593d97d711a8768481
Eddie Dugan <eddie.dugan@uniswap.org>
2023-02-15 11:57:32 -0800

    fix: wait for page to load

--- cypress/e2e/nfts.test.ts
+++ cypress/e2e/nfts.test.ts
@@ -5,11 +5,8 @@ const BONSAI_COLLECTION_ADDRESS = '0xec9c519d49856fd2f8133a0741b4dbe002ce211b'
 
 describe('Testing nfts', () => {
   beforeEach(() => {
-    cy.visit('/')
-    cy.get('body').then((body) => {
-      if (body.find(getTestSelector('FiatOnrampAnnouncement-close')).length > 0) {
-        cy.get(getTestSelector('FiatOnrampAnnouncement-close')).click()
-      }
+    cy.visit('/').then(() => {
+      cy.get(getTestSelector('FiatOnrampAnnouncement-close')).first().click()
     })
   })
 
