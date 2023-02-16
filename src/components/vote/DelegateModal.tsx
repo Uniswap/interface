dd32f8bcdab0804f282258fa4bc2ca16da2ed025
gab <gabriele.rigo@gmail.com>
2023-02-16 08:43:09 +0100

    fix staking modal

--- src/components/vote/DelegateModal.tsx
+++ src/components/vote/DelegateModal.tsx
@@ -1,19 +1,26 @@
 import { isAddress } from '@ethersproject/address'
 import { Trans } from '@lingui/macro'
+//import { Currency } from '@uniswap/sdk-core'
 import { useWeb3React } from '@web3-react/core'
-import { ReactNode, useState } from 'react'
+//import tryParseCurrencyAmount from 'lib/utils/tryParseCurrencyAmount'
+import { ReactNode, /*useCallback,*/ useState } from 'react'
 import { X } from 'react-feather'
 import styled from 'styled-components/macro'
 import { formatCurrencyAmount } from 'utils/formatCurrencyAmount'
 
+import { GRG_TRANSFER_PROXY_ADDRESSES } from '../../constants/addresses'
+//import { isSupportedChain } from '../../constants/chains'
 import { GRG } from '../../constants/tokens'
+import { ApprovalState, useApproveCallback } from '../../hooks/useApproveCallback'
 import useENS from '../../hooks/useENS'
 import { useTokenBalance } from '../../state/connection/hooks'
-import { useDelegateCallback } from '../../state/governance/hooks'
+import { useDelegateCallback, usePoolIdByAddress } from '../../state/governance/hooks'
 import { ThemedText } from '../../theme'
 import AddressInputPanel from '../AddressInputPanel'
-import { ButtonPrimary } from '../Button'
+import { ButtonConfirmed, ButtonPrimary } from '../Button'
+//import { ButtonError } from '../Button'
 import { AutoColumn } from '../Column'
+//import CurrencyInputPanel from '../CurrencyInputPanel'
 import Modal from '../Modal'
 import { LoadingView, SubmittedView } from '../ModalViews'
 import { RowBetween } from '../Row'
@@ -51,13 +58,27 @@ export default function DelegateModal({ isOpen, onDismiss, title }: VoteModalPro
     setTyped(val)
   }
 
+  // state for updating input amount
+  //const [typedValue, setTypedValue] = useState('')
+  // wrapped onUserInput to clear signatures
+  /*const onUserInput = useCallback((typedValue: string) => {
+    setTypedValue(typedValue)
+  }, [])
+*/
   // monitor for self delegation or input for third part delegate
   // default is self delegation
-  const activeDelegate = usingDelegate ? typed : account
+  const activeDelegate = typed ?? account //usingDelegate ? typed : account
   const { address: parsedAddress } = useENS(activeDelegate)
 
   // get the number of votes available to delegate
   const grgBalance = useTokenBalance(account ?? undefined, chainId ? GRG[chainId] : undefined)
+  const rpoolId = usePoolIdByAddress(parsedAddress ?? undefined)
+
+  const stakeData = {
+    amount: grgBalance?.quotient.toString(),
+    pool: parsedAddress,
+    poolId: rpoolId,
+  }
 
   const delegateCallback = useDelegateCallback()
 
@@ -76,10 +97,10 @@ export default function DelegateModal({ isOpen, onDismiss, title }: VoteModalPro
     setAttempting(true)
 
     // if callback not returned properly ignore
-    if (!delegateCallback) return
+    if (!delegateCallback || !grgBalance || !stakeData) return
 
     // try delegation and store hash
-    const hash = await delegateCallback(parsedAddress ?? undefined)?.catch((error) => {
+    const hash = await delegateCallback(stakeData ?? undefined)?.catch((error) => {
       setAttempting(false)
       console.log(error)
     })
@@ -88,6 +109,24 @@ export default function DelegateModal({ isOpen, onDismiss, title }: VoteModalPro
       setHash(hash)
     }
   }
+
+  // usingDelegate equals isRbPool
+  const [approval, approveCallback] = useApproveCallback(
+    grgBalance ?? undefined,
+    chainId ? GRG_TRANSFER_PROXY_ADDRESSES[chainId] : undefined,
+    usingDelegate
+  )
+
+  async function onAttemptToApprove() {
+    // TODO: check dep requirements
+    if (!approval || !approveCallback) return
+    //if (!provider) throw new Error('missing dependencies')
+    if (!grgBalance) throw new Error('missing GRG amount')
+
+    await approveCallback()
+  }
+
+  //const [currencyValue] = useState<Currency>(GRG[chainId ?? 1])
 
   return (
     <Modal isOpen={isOpen} onDismiss={wrappedOnDismiss} maxHeight={90}>
@@ -115,8 +154,38 @@ export default function DelegateModal({ isOpen, onDismiss, title }: VoteModalPro
             <ThemedText.DeprecatedBody>
               <Trans>Your voting power will unlock at the beginning of the next Rigoblock epoch.</Trans>
             </ThemedText.DeprecatedBody>
-            {usingDelegate && <AddressInputPanel value={typed} onChange={handleRecipientType} />}
-            <ButtonPrimary disabled={!isAddress(parsedAddress ?? '')} onClick={onDelegate}>
+            {/* TODO: following results in 'scrolloverlay' console error, remember to allow staking amounts smaller than balance */}
+            {/*
+            {grgBalance && (
+              <CurrencyInputPanel
+                value={typedValue}
+                currency={currencyValue}
+                onUserInput={onUserInput}
+                showMaxButton={true}
+                showCommonBases={false}
+                showCurrencyAmount={true}
+                hideBalance={false}
+                label=""
+                renderBalance={(amount) => <Trans>Available to deposit: {formatCurrencyAmount(grgBalance, 4)}</Trans>}
+                id="stake-grg-token"
+              />
+            )}
+            */}
+            {!usingDelegate && (
+              <ButtonConfirmed
+                mr="0.5rem"
+                onClick={onAttemptToApprove}
+                confirmed={approval === ApprovalState.APPROVED}
+                disabled={approval !== ApprovalState.NOT_APPROVED}
+              >
+                <Trans>Approve Staking</Trans>
+              </ButtonConfirmed>
+            )}
+            <AddressInputPanel value={typed} onChange={handleRecipientType} />
+            <ButtonPrimary
+              disabled={!isAddress(parsedAddress ?? '') || approval !== ApprovalState.APPROVED}
+              onClick={onDelegate}
+            >
               <ThemedText.DeprecatedMediumHeader color="white">
                 {usingDelegate ? <Trans>Stake From Pool</Trans> : <Trans>Stake For Yourself</Trans>}
               </ThemedText.DeprecatedMediumHeader>
