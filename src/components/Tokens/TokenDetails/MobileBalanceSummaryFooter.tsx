c9d3dc36b8b4dffe04b630cba0eb55f61a75be75
eddie <66155195+just-toby@users.noreply.github.com>
2023-02-17 17:42:57 -0800

    feat: introduce statsig and expose dummy feature gate (#5974)
    * feat: introduce statsig and expose dummy feature gate
    
    * feat: use the flag for different copy
    
    * feat: use amplitude user ID
    
    * fix: lock file
    
    * feat: upgrade analytics package
    
    * fix: update snapshots
    
    * fix: unit test snapshot
    
    * fix: fix tests and value check
    
    * feat: switch user ID to device ID
    
    * fix: move statsig wrapper
    
    * fix: move statsig wrapper
    
    * fix: e2e tests
    
    * fix: e2e tests
    
    * fix: env var
    
    * fix: env var
    
    * fix: try testEnv
    
    * fix: undo last commit
    
    * fix: e2e tests
    
    * fix: typo
    
    * fix: dont wait for init
    
    * wip
    
    * fix: undo wip change

--- src/components/Tokens/TokenDetails/MobileBalanceSummaryFooter.tsx
+++ src/components/Tokens/TokenDetails/MobileBalanceSummaryFooter.tsx
@@ -3,9 +3,11 @@ import { formatCurrencyAmount, NumberType } from '@uniswap/conedison/format'
 import { Currency } from '@uniswap/sdk-core'
 import { useWeb3React } from '@web3-react/core'
 import { NATIVE_CHAIN_ID } from 'constants/tokens'
+import { FeatureGate } from 'featureFlags/flags/featureFlags'
 import { CHAIN_ID_TO_BACKEND_NAME } from 'graphql/data/util'
 import { useStablecoinValue } from 'hooks/useStablecoinPrice'
 import useCurrencyBalance from 'lib/hooks/useCurrencyBalance'
+import { useGate } from 'statsig-react'
 import styled from 'styled-components/macro'
 import { StyledInternalLink } from 'theme'
 
@@ -87,6 +89,7 @@ export default function MobileBalanceSummaryFooter({ token }: { token: Currency 
   const formattedBalance = formatCurrencyAmount(balance, NumberType.TokenNonTx)
   const formattedUsdValue = formatCurrencyAmount(useStablecoinValue(balance), NumberType.FiatTokenStats)
   const chain = CHAIN_ID_TO_BACKEND_NAME[token.chainId].toLowerCase()
+  const { value: isDummyGateFlagEnabled } = useGate(FeatureGate.DUMMY)
 
   return (
     <Wrapper>
@@ -102,7 +105,7 @@ export default function MobileBalanceSummaryFooter({ token }: { token: Currency 
         </BalanceInfo>
       )}
       <SwapButton to={`/swap?chainName=${chain}&outputCurrency=${token.isNative ? NATIVE_CHAIN_ID : token.address}`}>
-        <Trans>Swap</Trans>
+        <Trans>{isDummyGateFlagEnabled ? 'Go to Swap' : 'Swap'}</Trans>
       </SwapButton>
     </Wrapper>
   )
