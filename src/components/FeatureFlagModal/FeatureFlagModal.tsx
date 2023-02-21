454c4f6620bd23695dc9e4429a6dac52dd75ec70
Charles Bachmeier <charlie@genie.xyz>
2023-02-21 15:13:46 -0800

    remove listv2 feature flag

--- src/components/FeatureFlagModal/FeatureFlagModal.tsx
+++ src/components/FeatureFlagModal/FeatureFlagModal.tsx
@@ -1,6 +1,5 @@
 import { BaseVariant, FeatureFlag, featureFlagSettings, useUpdateFlag } from 'featureFlags'
 import { GqlRoutingVariant, useGqlRoutingFlag } from 'featureFlags/flags/gqlRouting'
-import { NftListV2Variant, useNftListV2Flag } from 'featureFlags/flags/nftListV2'
 import { PayWithAnyTokenVariant, usePayWithAnyTokenFlag } from 'featureFlags/flags/payWithAnyToken'
 import { Permit2Variant, usePermit2Flag } from 'featureFlags/flags/permit2'
 import { SwapWidgetVariant, useSwapWidgetFlag } from 'featureFlags/flags/swapWidget'
@@ -211,12 +210,6 @@ export default function FeatureFlagModal() {
         value={usePermit2Flag()}
         featureFlag={FeatureFlag.permit2}
         label="Permit 2 / Universal Router"
-      />
-      <FeatureFlagOption
-        variant={NftListV2Variant}
-        value={useNftListV2Flag()}
-        featureFlag={FeatureFlag.nftListV2}
-        label="NFT Listing Page v2"
       />
       <FeatureFlagOption
         variant={PayWithAnyTokenVariant}
