e28eae7400ad2fbb4b85aa229e6ea7b77013cf47
Daniel James <danielcolinjames@gmail.com>
2023-02-22 17:59:49 +0000

    chore: Make loading shimmer optional on Text component (#3157)
    * Make loading shimmer optional on Text component
    
    * Make inner text placeholder a component
    
    * Make loading prop boolean | "no-shimmer"
    
    * Fix shimmer logic

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -14,7 +14,7 @@ export type TextProps = ComponentProps<typeof ThemedText> & {
   maxFontSizeMultiplier?: number
   animated?: boolean
   allowFontScaling?: boolean
-  loading?: boolean
+  loading?: boolean | 'no-shimmer'
   loadingPlaceholderText?: string
 }
 
@@ -23,30 +23,40 @@ export type TextProps = ComponentProps<typeof ThemedText> & {
 const ThemedText = createText<Theme>()
 const ThemedAnimatedText = createText<Theme>(Animated.Text)
 
-const TextLoaderWrapper = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
+const TextPlaceholder = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
   return (
-    <Shimmer>
-      <Box alignItems="center" flexDirection="row">
-        <Box alignItems="center" flexDirection="row" position="relative">
-          <HiddenFromScreenReaders>{children}</HiddenFromScreenReaders>
-          <Box
-            bg="background3"
-            borderRadius="rounded4"
-            bottom="5%"
-            left={0}
-            position="absolute"
-            right={0}
-            top="5%"
-          />
-        </Box>
+    <Box alignItems="center" flexDirection="row">
+      <Box alignItems="center" flexDirection="row" position="relative">
+        <HiddenFromScreenReaders>{children}</HiddenFromScreenReaders>
+        <Box
+          bg="background3"
+          borderRadius="rounded4"
+          bottom="5%"
+          left={0}
+          position="absolute"
+          right={0}
+          top="5%"
+        />
       </Box>
-    </Shimmer>
+    </Box>
   )
+}
+
+const TextLoaderWrapper = ({
+  children,
+  loadingShimmer,
+}: { loadingShimmer?: boolean } & PropsWithChildren<unknown>): JSX.Element => {
+  const inner = <TextPlaceholder>{children}</TextPlaceholder>
+  if (loadingShimmer) {
+    return <Shimmer>{inner}</Shimmer>
+  }
+
+  return inner
 }
 
 /**
  * Use this component instead of the default React Native <Text> component anywhere text shows up throughout the app, so we can use the design system values for colors and sizes, and make sure all text looks and behaves the same way
- * @param loading Whether the text inside the component is still loading or not. Set this to true if whatever content goes inside the <Text> component is coming from a variable that might still be loading. This prop is optional and defaults to false.
+ * @param loading Whether the text inside the component is still loading or not. Set this to true if whatever content goes inside the <Text> component is coming from a variable that might still be loading. This prop is optional and defaults to false. This prop can also be set to "no-shimmer" to enable a loading state without the shimmer effect.
  * @param loadingPlaceholderText - The text that the loader's size will be derived from. Pick something that's close to the same length as the final text is expected to be, e.g. if it's a ticker symbol, "XXX" might be a good placeholder text. This prop is optional and defaults to "$00.00".
  */
 export const Text = ({
@@ -74,7 +84,7 @@ export const Text = ({
 
   if (loading) {
     return (
-      <TextLoaderWrapper>
+      <TextLoaderWrapper loadingShimmer={loading !== 'no-shimmer'}>
         <ThemedText
           allowFontScaling={enableFontScaling}
           color="none"
