d514a8fe32753cc8c3b6a80cfc1a924c7b165d8d
Nguyen Van Viet <vietnv1304@gmail.com>
2023-02-22 11:13:03 +0700

    fix: KEP-791 add limit 500 pools instead of default (100) for elastic (#1713)

--- src/apollo/queries/promm.ts
+++ src/apollo/queries/promm.ts
@@ -6,7 +6,7 @@ export const PROMM_POOLS_BULK = (block: number | undefined, pools: string[]) => 
   const queryString =
     `
     query pools {
-      pools(where: {id_in: ${poolString.toLowerCase()}},` +
+      pools(first: 500, where: {id_in: ${poolString.toLowerCase()}},` +
     (block ? `block: {number: ${block}} ,` : ``) +
     ` orderBy: totalValueLockedUSD, orderDirection: desc, subgraphError: allow) {
         id
