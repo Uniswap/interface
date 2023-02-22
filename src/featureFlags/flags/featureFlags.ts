3cc231916eb4aa2e8c9d3d058461d0072ec0247f
Charles Bachmeier <charlie@genie.xyz>
2023-02-22 08:45:10 -0800

    missed one

--- src/featureFlags/flags/featureFlags.ts
+++ src/featureFlags/flags/featureFlags.ts
@@ -1,7 +1,6 @@
 export enum FeatureFlag {
   traceJsonRpc = 'traceJsonRpc',
   permit2 = 'permit2',
-  nftListV2 = 'nftListV2',
   payWithAnyToken = 'payWithAnyToken',
   swapWidget = 'swapWidget',
   gqlRouting = 'gqlRouting',
