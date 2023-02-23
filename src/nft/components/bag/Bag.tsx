1348eb332263f6ca09aef921c232ea7d086beaa8
Jack Short <john.short.tj@gmail.com>
2023-02-22 20:43:35 -0500

    fix: token selector correct zIndex on mobile for pwat (#6017)

--- src/nft/components/bag/Bag.tsx
+++ src/nft/components/bag/Bag.tsx
@@ -65,7 +65,7 @@ const BagContainer = styled.div<{ raiseZIndex: boolean; isProfilePage: boolean }
   border-radius: 16px;
   box-shadow: ${({ theme }) => theme.shallowShadow};
   z-index: ${({ raiseZIndex, isProfilePage }) =>
-    raiseZIndex ? (isProfilePage ? Z_INDEX.modalOverTooltip : Z_INDEX.modalBackdrop + 2) : 3};
+    raiseZIndex ? (isProfilePage ? Z_INDEX.modalOverTooltip : Z_INDEX.modalBackdrop - 1) : 3};
 
   @media only screen and (max-width: ${({ theme }) => `${theme.breakpoint.sm}px`}) {
     right: 0px;
