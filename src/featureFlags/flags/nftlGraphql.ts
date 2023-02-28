e84cefa73b74d255737ae98d0a0aef89fa7ff2ac
Charles Bachmeier <charlie@genie.xyz>
2023-02-28 12:33:45 -0800

    remove comment

--- src/featureFlags/flags/nftlGraphql.ts
+++ src/featureFlags/flags/nftlGraphql.ts
@@ -4,9 +4,4 @@ export function useNftGraphqlFlag(): BaseVariant {
   return useBaseFlag(FeatureFlag.nftGraphql)
 }
 
-// TODO Enable when flag is ready to be used
-// export function useNftGraphqlEnabled(): boolean {
-//   return useNftGraphqlFlag() === BaseVariant.Enabled
-// }
-
 export { BaseVariant as NftGraphqlVariant }
