9c7da4305bba14a036fc6f50ecc3c43dc86f77b1
Vignesh Mohankumar <me@vig.xyz>
2023-03-03 16:20:33 -0500

    rm

--- src/featureFlags/flags/permit2.ts
+++ src/featureFlags/flags/permit2.ts
@@ -1,11 +1,3 @@
-import { BaseVariant } from '../index'
-
-export function usePermit2Flag(): BaseVariant {
-  return BaseVariant.Enabled
-}
-
 export function usePermit2Enabled(): boolean {
   return true
 }
-
-export { BaseVariant as Permit2Variant }
