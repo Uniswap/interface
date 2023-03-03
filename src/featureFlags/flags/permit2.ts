ca55e1eecf99f07bd5dfa31d77a7b0592954aeda
Vignesh Mohankumar <me@vig.xyz>
2023-03-03 16:16:18 -0500

    refactor: always enable permit2

--- src/featureFlags/flags/permit2.ts
+++ src/featureFlags/flags/permit2.ts
@@ -1,6 +1,3 @@
-import { UNIVERSAL_ROUTER_ADDRESS } from '@uniswap/universal-router-sdk'
-import { useWeb3React } from '@web3-react/core'
-
 import { BaseVariant, FeatureFlag, useBaseFlag } from '../index'
 
 export function usePermit2Flag(): BaseVariant {
@@ -8,17 +5,7 @@ export function usePermit2Flag(): BaseVariant {
 }
 
 export function usePermit2Enabled(): boolean {
-  const flagEnabled = usePermit2Flag() === BaseVariant.Enabled
-  const { chainId } = useWeb3React()
-  try {
-    // Detect if the Universal Router is not yet deployed to chainId.
-    // This is necessary so that we can fallback correctly on chains without a Universal Router deployment.
-    // It will be removed once Universal Router is deployed on all supported chains.
-    chainId && UNIVERSAL_ROUTER_ADDRESS(chainId)
-    return flagEnabled
-  } catch {
-    return false
-  }
+  return true
 }
 
 export { BaseVariant as Permit2Variant }
