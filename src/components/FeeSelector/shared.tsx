e4006476297ca87a0ffc9d3cd3be67c895587128
Vignesh Mohankumar <me@vig.xyz>
2023-03-03 14:48:46 -0500

    combine

--- src/components/FeeSelector/shared.tsx
+++ src/components/FeeSelector/shared.tsx
@@ -1,7 +1,6 @@
 import { Trans } from '@lingui/macro'
 import { FeeAmount } from '@uniswap/v3-sdk'
-import { SupportedChainId } from 'constants/chains'
-import { ALL_SUPPORTED_CHAIN_IDS } from 'constants/chains'
+import { ALL_SUPPORTED_CHAIN_IDS, SupportedChainId } from 'constants/chains'
 import type { ReactNode } from 'react'
 
 export const FEE_AMOUNT_DETAIL: Record<
