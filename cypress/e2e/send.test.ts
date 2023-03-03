cfbeba4ead136599e5b26d277e9d2a5d4dea9cff
Vignesh Mohankumar <me@vig.xyz>
2023-03-03 18:22:59 -0500

    fix

--- cypress/e2e/send.test.ts
+++ cypress/e2e/send.test.ts
@@ -5,7 +5,8 @@ describe('Send', () => {
   })
 
   it('should redirect with url params', () => {
-    cy.visit('/send?outputCurrency=ETH&recipient=bob.argent.xyz')
-    cy.url().should('contain', '/swap?outputCurrency=ETH&recipient=bob.argent.xyz')
+    cy.visit('/send', { qs: { outputCurrency: 'ETH', recipient: 'bob.argent.xyz' } })
+    cy.url().should('contain', 'outputCurrency=ETH')
+    cy.url().should('contain', 'recipient=bob.argent.xyz')
   })
 })
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -36,7 +36,8 @@ Cypress.Commands.overwrite(
     cy.intercept('/service-worker.js', options?.serviceWorker ? undefined : { statusCode: 404 }).then(() => {
       original({
         ...options,
-        url: (url.startsWith('/') && url.length > 2 && !url.startsWith('/#') ? `/#${url}` : url) + '?chain=goerli',
+        url: url.startsWith('/') && url.length > 2 && !url.startsWith('/#') ? `/#${url}` : url,
+        qs: { chain: 'goerli' },
         onBeforeLoad(win) {
           options?.onBeforeLoad?.(win)
           win.localStorage.clear()
