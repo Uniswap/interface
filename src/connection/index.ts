8dcb492982b6909230336969d985f08905d64c1f
Vignesh Mohankumar <me@vig.xyz>
2023-03-06 15:39:53 -0500

    revert

--- src/connection/index.ts
+++ src/connection/index.ts
@@ -66,6 +66,7 @@ const [web3WalletConnect, web3WalletConnectHooks] = initializeConnector<WalletCo
     actions,
     options: {
       rpc: RPC_URLS_WITHOUT_FALLBACKS,
+      qrcode: true,
     },
     onError,
   })
