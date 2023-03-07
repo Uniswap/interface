1eccd5c6826d462a6bcec8131104b5f6ece9ece3
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-03-07 12:17:14 -0600

    fix: use old gas margin calculation

--- src/utils/calculateGasMargin.ts
+++ src/utils/calculateGasMargin.ts
@@ -1,10 +1,9 @@
 import { BigNumber } from '@ethersproject/bignumber'
-import { TX_GAS_MARGIN } from 'constants/misc'
 
 /**
  * Returns the gas value plus a margin for unexpected or variable gas costs
  * @param value the gas value to pad
  */
 export function calculateGasMargin(value: BigNumber): BigNumber {
-  return value.add(value.mul(Math.floor(TX_GAS_MARGIN * 100)).div(100))
+  return value.mul(120).div(100)
 }
