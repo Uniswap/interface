333c3a289a49c93ee9e101e5e41c46874a4b0862
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2023-03-10 16:58:59 -0500

    fix: check if supported chainId before calling universal-router-sdk (#6126)

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -307,7 +307,7 @@ export default function Swap({ className }: { className?: string }) {
       (parsedAmounts[Field.INPUT]?.currency.isToken
         ? (parsedAmounts[Field.INPUT] as CurrencyAmount<Token>)
         : undefined),
-    chainId ? UNIVERSAL_ROUTER_ADDRESS(chainId) : undefined
+    isSupportedChain(chainId) ? UNIVERSAL_ROUTER_ADDRESS(chainId) : undefined
   )
   const isApprovalLoading = allowance.state === AllowanceState.REQUIRED && allowance.isApprovalLoading
   const [isAllowancePending, setIsAllowancePending] = useState(false)
