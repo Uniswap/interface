40667f1822954ebeb30e4cb7def40a69405b3467
Vignesh Mohankumar <me@vig.xyz>
2023-03-10 14:05:03 -0500

    try a fix

--- src/hooks/useSyncChainQuery.ts
+++ src/hooks/useSyncChainQuery.ts
@@ -5,7 +5,6 @@ import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 import useParsedQueryString from './useParsedQueryString'
-import usePrevious from './usePrevious'
 import useSelectChain from './useSelectChain'
 
 function getChainIdFromName(name: string) {
@@ -25,38 +24,26 @@ export default function useSyncChainQuery() {
   const { chainId, isActive } = useWeb3React()
   const parsedQs = useParsedQueryString()
 
-  const urlChainId = getParsedChainId(parsedQs)
-  const previousUrlChainId = usePrevious(urlChainId)
-
   const selectChain = useSelectChain()
 
-  // Can't use `usePrevious` because `chainId` can be undefined while activating.
-  const [previousChainId, setPreviousChainId] = useState<number | undefined>(undefined)
-  const [nextChainId, setNextChainId] = useState<number | undefined>(undefined)
-
-  useEffect(() => {
-    if (chainId && chainId !== previousChainId) {
-      setPreviousChainId(chainId)
-    }
-  }, [chainId, previousChainId])
+  const urlChainId = getParsedChainId(parsedQs)
 
   const [searchParams, setSearchParams] = useSearchParams()
 
+  const [nextChainId, setNextChainId] = useState<number | undefined>(undefined)
+
   useEffect(() => {
-    const chainQueryManuallyUpdated = urlChainId && urlChainId !== previousUrlChainId
-    if (chainQueryManuallyUpdated) {
+    if (chainId !== urlChainId && urlChainId) {
       setNextChainId(urlChainId)
     }
-  }, [previousUrlChainId, urlChainId])
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return useEffect(() => {
     if (nextChainId && isActive) {
-      // If the query param changed, and the chain didn't change, then activate the new chain
       selectChain(nextChainId).then(() => {
         searchParams.delete('chain')
         setSearchParams(searchParams)
-        setNextChainId(undefined)
       })
     }
-  }, [nextChainId, urlChainId, selectChain, searchParams, setSearchParams, isActive])
+  }, [nextChainId, isActive, searchParams, selectChain, setSearchParams])
 }
