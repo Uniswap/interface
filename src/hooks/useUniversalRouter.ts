46cb4778737931ca5439e1140d8ac052fbf0bbb9
Zach Pomerantz <zzmp@uniswap.org>
2023-03-10 01:43:14 -0800

    feat: trace swap.send

--- src/hooks/useUniversalRouter.ts
+++ src/hooks/useUniversalRouter.ts
@@ -9,10 +9,11 @@ import { SwapRouter, UNIVERSAL_ROUTER_ADDRESS } from '@uniswap/universal-router-
 import { FeeOptions, toHex } from '@uniswap/v3-sdk'
 import { useWeb3React } from '@web3-react/core'
 import { formatSwapSignedAnalyticsEventProperties } from 'lib/utils/analytics'
+import { trace } from 'logging/trace'
 import { useCallback } from 'react'
 import { calculateGasMargin } from 'utils/calculateGasMargin'
 import isZero from 'utils/isZero'
-import { swapErrorToUserReadableMessage } from 'utils/swapErrorToUserReadableMessage'
+import { parseSwapError, swapErrorToMessage } from 'utils/swapErrorToUserReadableMessage'
 
 import { PermitSignature } from './usePermitAllowance'
 
@@ -32,65 +33,86 @@ export function useUniversalRouterSwapCallback(
 ) {
   const { account, chainId, provider } = useWeb3React()
 
-  return useCallback(async (): Promise<TransactionResponse> => {
-    try {
-      if (!account) throw new Error('missing account')
-      if (!chainId) throw new Error('missing chainId')
-      if (!provider) throw new Error('missing provider')
-      if (!trade) throw new Error('missing trade')
+  return useCallback(
+    async (): Promise<TransactionResponse> =>
+      trace(
+        'swap.send',
+        async ({ transaction, traceChild }) => {
+          try {
+            if (!account) throw new Error('missing account')
+            if (!chainId) throw new Error('missing chainId')
+            if (!provider) throw new Error('missing provider')
+            if (!trade) throw new Error('missing trade')
 
-      const { calldata: data, value } = SwapRouter.swapERC20CallParameters(trade, {
-        slippageTolerance: options.slippageTolerance,
-        deadlineOrPreviousBlockhash: options.deadline?.toString(),
-        inputTokenPermit: options.permit,
-        fee: options.feeOptions,
-      })
-      const tx = {
-        from: account,
-        to: UNIVERSAL_ROUTER_ADDRESS(chainId),
-        data,
-        // TODO: universal-router-sdk returns a non-hexlified value.
-        ...(value && !isZero(value) ? { value: toHex(value) } : {}),
-      }
+            const { calldata: data, value } = SwapRouter.swapERC20CallParameters(trade, {
+              slippageTolerance: options.slippageTolerance,
+              deadlineOrPreviousBlockhash: options.deadline?.toString(),
+              inputTokenPermit: options.permit,
+              fee: options.feeOptions,
+            })
+            const tx = {
+              from: account,
+              to: UNIVERSAL_ROUTER_ADDRESS(chainId),
+              data,
+              // TODO: universal-router-sdk returns a non-hexlified value.
+              ...(value && !isZero(value) ? { value: toHex(value) } : {}),
+            }
 
-      let gasEstimate: BigNumber
-      try {
-        gasEstimate = await provider.estimateGas(tx)
-      } catch (gasError) {
-        console.warn(gasError)
-        throw new Error('Your swap is expected to fail')
-      }
-      const gasLimit = calculateGasMargin(gasEstimate)
-      const response = await provider
-        .getSigner()
-        .sendTransaction({ ...tx, gasLimit })
-        .then((response) => {
-          sendAnalyticsEvent(
-            SwapEventName.SWAP_SIGNED,
-            formatSwapSignedAnalyticsEventProperties({ trade, fiatValues, txHash: response.hash })
-          )
-          if (tx.data !== response.data) {
-            sendAnalyticsEvent(SwapEventName.SWAP_MODIFIED_IN_WALLET, { txHash: response.hash })
-            throw new InvalidSwapError(
-              t`Your swap was modified through your wallet. If this was a mistake, please cancel immediately or risk losing your funds.`
-            )
+            const gasEstimate = await traceChild('gasEstimate', async ({ transaction: gasEstimateTransaction }) => {
+              try {
+                return await provider.estimateGas(tx)
+              } catch (gasError) {
+                console.warn(gasError)
+                gasEstimateTransaction.setStatus('unknown_error')
+                transaction.setStatus('gas_estimate_failure')
+                throw new Error('Your swap is expected to fail')
+              } finally {
+                gasEstimateTransaction.finish()
+              }
+            })
+            const gasLimit = calculateGasMargin(gasEstimate)
+
+            const response = await provider
+              .getSigner()
+              .sendTransaction({ ...tx, gasLimit })
+              .then((response) => {
+                sendAnalyticsEvent(
+                  SwapEventName.SWAP_SIGNED,
+                  formatSwapSignedAnalyticsEventProperties({ trade, fiatValues, txHash: response.hash })
+                )
+                if (tx.data !== response.data) {
+                  transaction.setStatus('modified_in_wallet')
+                  sendAnalyticsEvent(SwapEventName.SWAP_MODIFIED_IN_WALLET, { txHash: response.hash })
+                  throw new InvalidSwapError(
+                    t`Your swap was modified through your wallet. If this was a mistake, please cancel immediately or risk losing your funds.`
+                  )
+                }
+                return response
+              })
+            return response
+          } catch (error: unknown) {
+            if (error instanceof InvalidSwapError) throw error
+            const swapError = parseSwapError(error)
+            const message = swapErrorToMessage(swapError, error)
+            if (!transaction.status) {
+              transaction.setStatus(status)
+            }
+            transaction.setData('error', message)
+            throw new Error(message)
           }
-          return response
-        })
-      return response
-    } catch (swapError: unknown) {
-      if (swapError instanceof InvalidSwapError) throw swapError
-      throw new Error(swapErrorToUserReadableMessage(swapError))
-    }
-  }, [
-    account,
-    chainId,
-    fiatValues,
-    options.deadline,
-    options.feeOptions,
-    options.permit,
-    options.slippageTolerance,
-    provider,
-    trade,
-  ])
+        },
+        { tags: { widget: false } }
+      ),
+    [
+      account,
+      chainId,
+      fiatValues,
+      options.deadline,
+      options.feeOptions,
+      options.permit,
+      options.slippageTolerance,
+      provider,
+      trade,
+    ]
+  )
 }
--- src/utils/swapErrorToUserReadableMessage.tsx
+++ src/utils/swapErrorToUserReadableMessage.tsx
@@ -1,29 +1,48 @@
-// eslint-disable-next-line no-restricted-imports
-import { t } from '@lingui/macro'
 /**
  * This is hacking out the revert reason from the ethers provider thrown error however it can.
- * This object seems to be undocumented by ethers.
- * @param error an error from the ethers provider
+ * This error object seems to be undocumented by ethers.
  */
-export function swapErrorToUserReadableMessage(error: any): string {
+// eslint-disable-next-line no-restricted-imports
+import { t } from '@lingui/macro'
+
+enum SwapError {
+  TRANSACTION_REJECTED = 'transaction_rejected',
+  EXPIRED = 'expired',
+  INSUFFICIENT_OUTPUT_AMOUNT = 'insufficient_output_amount',
+  EXCESSIVE_INPUT_AMOUNT = 'exceesive_input_amount',
+  TRANSFER_FROM_FAILED = 'transfer_from_failed',
+  TRANSFER_FAILED = 'transfer_failed',
+  K = 'k',
+  TOO_LITTLE_RECEIVED = 'too_little_received',
+  TOO_MUCH_REQUESTED = 'too_much_requested',
+  STF = 'stf',
+  TF = 'tf',
+  UNKNOWN = 'unknown_error',
+}
+
+function getReason(error: any): string | undefined {
   let reason: string | undefined
 
   if (error.code) {
     switch (error.code) {
       case 4001:
-        return t`Transaction rejected`
+        return 'user rejected'
     }
   }
-
-  console.warn('Swap error:', error)
 
   while (error) {
     reason = error.reason ?? error.message ?? reason
     error = error.error ?? error.data?.originalError
   }
 
-  // The 4001 error code doesn't capture the case where users reject a transaction for all wallets,
-  // so we need to parse the reason for these special cases:
+  if (reason?.indexOf('execution reverted: ') === 0) reason = reason.substr('execution reverted: '.length)
+
+  return reason
+}
+
+export function parseSwapError(error: any): SwapError {
+  const reason = getReason(error)
+
   if (
     // ethers v5.7.0 wrapped error
     error?.code === 'ACTION_REJECTED' ||
@@ -38,34 +57,67 @@ export function swapErrorToUserReadableMessage(error: any): string {
     // For Fireblocks
     reason?.match(/user rejected/i)
   ) {
-    return t`Transaction rejected`
+    return SwapError.TRANSACTION_REJECTED
   }
-
-  if (reason?.indexOf('execution reverted: ') === 0) reason = reason.substr('execution reverted: '.length)
 
   switch (reason) {
     case 'UniswapV2Router: EXPIRED':
-      return t`The transaction could not be sent because the deadline has passed. Please check that your transaction deadline is not too low.`
+      return SwapError.EXPIRED
     case 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT':
+      return SwapError.INSUFFICIENT_OUTPUT_AMOUNT
     case 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT':
-      return t`This transaction will not succeed either due to price movement or fee on transfer. Try increasing your slippage tolerance.`
+      return SwapError.EXCESSIVE_INPUT_AMOUNT
     case 'TransferHelper: TRANSFER_FROM_FAILED':
-      return t`The input token cannot be transferred. There may be an issue with the input token.`
+      return SwapError.TRANSFER_FROM_FAILED
     case 'UniswapV2: TRANSFER_FAILED':
-      return t`The output token cannot be transferred. There may be an issue with the output token.`
+      return SwapError.TRANSFER_FAILED
     case 'UniswapV2: K':
-      return t`The Uniswap invariant x*y=k was not satisfied by the swap. This usually means one of the tokens you are swapping incorporates custom behavior on transfer.`
+      return SwapError.K
     case 'Too little received':
+      return SwapError.TOO_LITTLE_RECEIVED
     case 'Too much requested':
+      return SwapError.TOO_MUCH_REQUESTED
     case 'STF':
-      return t`This transaction will not succeed due to price movement. Try increasing your slippage tolerance. Note: fee on transfer and rebase tokens are incompatible with Uniswap V3.`
+      return SwapError.STF
     case 'TF':
-      return t`The output token cannot be transferred. There may be an issue with the output token. Note: fee on transfer and rebase tokens are incompatible with Uniswap V3.`
+      return SwapError.TF
     default:
+      return SwapError.UNKNOWN
+  }
+}
+
+export function swapErrorToMessage(parsedError: SwapError, error: any): string {
+  if (parsedError !== SwapError.TRANSACTION_REJECTED) {
+    console.warn('Swap error:', error)
+  }
+
+  switch (parsedError) {
+    case SwapError.TRANSACTION_REJECTED:
+      return t`Transaction rejected`
+    case SwapError.EXPIRED:
+      return t`The transaction could not be sent because the deadline has passed. Please check that your transaction deadline is not too low.`
+    case SwapError.INSUFFICIENT_OUTPUT_AMOUNT:
+    case SwapError.EXCESSIVE_INPUT_AMOUNT:
+      return t`This transaction will not succeed either due to price movement or fee on transfer. Try increasing your slippage tolerance.`
+    case SwapError.TRANSFER_FROM_FAILED:
+      return t`The input token cannot be transferred. There may be an issue with the input token.`
+    case SwapError.TRANSFER_FAILED:
+      return t`The output token cannot be transferred. There may be an issue with the output token.`
+    case SwapError.K:
+      return t`The Uniswap invariant x*y=k was not satisfied by the swap. This usually means one of the tokens you are swapping incorporates custom behavior on transfer.`
+    case SwapError.TOO_LITTLE_RECEIVED:
+    case SwapError.TOO_MUCH_REQUESTED:
+    case SwapError.STF:
+      return t`This transaction will not succeed due to price movement. Try increasing your slippage tolerance. Note: fee on transfer and rebase tokens are incompatible with Uniswap V3.`
+    case SwapError.TF:
+      return t`The output token cannot be transferred. There may be an issue with the output token. Note: fee on transfer and rebase tokens are incompatible with Uniswap V3.`
+    case SwapError.UNKNOWN: {
+      const reason = getReason(error)
       if (reason?.indexOf('undefined is not an object') !== -1) {
         return t`An error occurred when trying to execute this swap. You may need to increase your slippage tolerance. If that does not work, there may be an incompatibility with the token you are trading. Note: fee on transfer and rebase tokens are incompatible with Uniswap V3.`
       }
       return t`${reason ? reason : 'Unknown error'}. Try increasing your slippage tolerance.
 Note: fee-on-transfer and rebase tokens are incompatible with Uniswap V3.`
+    }
   }
 }
