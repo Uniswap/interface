b43ec31e1850a140a387534fceeb63c776fb9821
Tina Zheng <tina.s.zheng+github@gmail.com>
2023-03-11 01:42:23 -0500

    remove console log

--- src/utils/computeFiatValuePriceImpact.tsx
+++ src/utils/computeFiatValuePriceImpact.tsx
@@ -7,7 +7,6 @@ export function computeFiatValuePriceImpact(
   if (!fiatValueOutput || !fiatValueInput) return undefined
   if (fiatValueInput === 0) return undefined
 
-  console.log('fiatValueOutput', fiatValueOutput, fiatValueInput)
   const ratio = 1 - fiatValueOutput / fiatValueInput
   const numerator = Math.floor(ratio * 10000)
   return new Percent(numerator, 10000)
