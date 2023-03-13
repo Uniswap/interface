5dee5c1f1d06859ba6f071132ca5f890cc0d485f
Vignesh Mohankumar <me@vig.xyz>
2023-03-13 12:25:27 -0400

    eslint

--- src/pages/Pool/PositionPage.tsx
+++ src/pages/Pool/PositionPage.tsx
@@ -18,7 +18,6 @@ import CurrencyLogo from 'components/Logo/CurrencyLogo'
 import { RowBetween, RowFixed } from 'components/Row'
 import { Dots } from 'components/swap/styleds'
 import Toggle from 'components/Toggle'
-import { MouseoverTooltip } from 'components/Tooltip'
 import TransactionConfirmationModal, { ConfirmationModalContent } from 'components/TransactionConfirmationModal'
 import { useToken } from 'hooks/Tokens'
 import { useV3NFTPositionManagerContract } from 'hooks/useContract'
@@ -76,7 +75,7 @@ const BadgeText = styled.div`
 // responsive text
 // disable the warning because we don't use the end prop, we just want to filter it out
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Label = styled(({ end, ...props }) => <ThemedText.DeprecatedLabel {...props} />) <{ end?: boolean }>`
+const Label = styled(({ end, ...props }) => <ThemedText.DeprecatedLabel {...props} />)<{ end?: boolean }>`
   display: flex;
   font-size: 16px;
   justify-content: ${({ end }) => (end ? 'flex-end' : 'flex-start')};
@@ -390,10 +389,10 @@ export function PositionPage() {
   const ratio = useMemo(() => {
     return priceLower && pool && priceUpper
       ? getRatio(
-        inverted ? priceUpper.invert() : priceLower,
-        pool.token0Price,
-        inverted ? priceLower.invert() : priceUpper
-      )
+          inverted ? priceUpper.invert() : priceLower,
+          pool.token0Price,
+          inverted ? priceLower.invert() : priceUpper
+        )
       : undefined
   }, [inverted, pool, priceLower, priceUpper])
 
@@ -561,11 +560,11 @@ export function PositionPage() {
 
   const showCollectAsWeth = Boolean(
     ownsNFT &&
-    (feeValue0?.greaterThan(0) || feeValue1?.greaterThan(0)) &&
-    currency0 &&
-    currency1 &&
-    (currency0.isNative || currency1.isNative) &&
-    !collectMigrationHash
+      (feeValue0?.greaterThan(0) || feeValue1?.greaterThan(0)) &&
+      currency0 &&
+      currency1 &&
+      (currency0.isNative || currency1.isNative) &&
+      !collectMigrationHash
   )
 
   return loading || poolState === PoolState.LOADING || !feeAmount ? (
@@ -772,7 +771,7 @@ export function PositionPage() {
                           )}
                         </AutoColumn>
                         {ownsNFT &&
-                          (feeValue0?.greaterThan(0) || feeValue1?.greaterThan(0) || !!collectMigrationHash) ? (
+                        (feeValue0?.greaterThan(0) || feeValue1?.greaterThan(0) || !!collectMigrationHash) ? (
                           <ResponsiveButtonConfirmed
                             disabled={collecting || !!collectMigrationHash}
                             confirmed={!!collectMigrationHash && !isCollectPending}
