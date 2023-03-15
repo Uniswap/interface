93766f9e32c40bc71817c69110609a55c0417270
Zach Pomerantz <zzmp@uniswap.org>
2023-03-15 15:55:32 -0700

    docs: even more docs

--- src/tracing/trace.ts
+++ src/tracing/trace.ts
@@ -15,10 +15,21 @@ interface TraceMetadata {
 // These methods are provided as an abstraction so that users will not interact with Sentry directly.
 // This avoids tightly coupling Sentry to our instrumentation outside of this file, in case we swap services.
 interface TraceCallbackOptions {
+  /**
+   * Traces the callback as a child of the active trace.
+   * @param name - The name of the child. (On Sentry, this will appear as the "op".)
+   * @param callback - The callback to trace. The child trace will run for the duration of the callback.
+   * @param metadata - Any data or tags to include in the child trace.
+   */
   traceChild<T>(name: string, callback: TraceCallback<T>, metadata?: TraceMetadata): Promise<T>
   setTraceData(key: string, value: unknown): void
   setTraceTag<K extends keyof TraceTags>(key: K, value: TraceTags[K]): void
+  /**
+   * Sets the status of a trace. If unset, the status will be set to 'ok' (or 'internal_error' if the callback throws).
+   * @param status - If a number is passed, the corresponding http status will be used.
+   */
   setTraceStatus(status: number | SpanStatusType): void
+  /** Sets the error data of a trace. If unset and the callback throws, the thrown error will be set. */
   setTraceError(error: unknown): void
 }
 type TraceCallback<T> = (options: TraceCallbackOptions) => Promise<T>
@@ -66,7 +77,12 @@ function traceSpan(span?: Span) {
   }
 }
 
-/** Traces the callback, adding any metadata to the trace. */
+/**
+ * Traces the callback, adding any metadata to the trace.
+ * @param name - The name of your trace.
+ * @param callback - The callback to trace. The trace will run for the duration of the callback.
+ * @param metadata - Any data or tags to include in the trace.
+ */
 export async function trace<T>(name: string, callback: TraceCallback<T>, metadata?: TraceMetadata): Promise<T> {
   const transaction = Sentry.startTransaction({ name, data: metadata?.data, tags: metadata?.tags })
   return traceSpan(transaction)(callback)
