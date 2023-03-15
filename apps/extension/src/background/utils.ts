6bbcf895618f785a5e742945eb59ac60580522e4
Thomas Thachil <github@thomasthachil.com>
2023-03-15 16:08:34 -0400

    feat: added background service worker code

---
+++ apps/extension/src/background/utils.ts
@@ -0,0 +1,75 @@
+function getKeyMaterial(password: string) {
+  return crypto.subtle.importKey(
+    'raw',
+    new TextEncoder().encode(password),
+    'PBKDF2',
+    false,
+    ['deriveBits', 'deriveKey']
+  )
+}
+
+function getKey(keyMaterial: CryptoKey, salt: Uint8Array) {
+  // TODO: This should use Argon2 like ToB recommended for the mobile app
+  // https://github.com/Uniswap/mobile/blob/main/ios/EncryptionHelper.swift
+  return crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt,
+      iterations: 100000,
+      hash: 'SHA-256',
+    },
+    keyMaterial,
+    { name: 'AES-GCM', length: 256 },
+    true,
+    ['encrypt', 'decrypt']
+  )
+}
+
+export async function encryptPassword(
+  password: string,
+  iv: Uint8Array,
+  salt: Uint8Array,
+  mnemonic: string
+) {
+  const keyMaterial = await getKeyMaterial(password)
+  const key = await getKey(keyMaterial, salt)
+
+  const encodedMnemonic = new TextEncoder().encode(mnemonic)
+  const encryptedPassword = await crypto.subtle.encrypt(
+    {
+      name: 'AES-GCM',
+      iv,
+    },
+    key,
+    encodedMnemonic
+  )
+
+  return new Uint8Array(encryptedPassword)
+}
+
+export async function decryptPassword(
+  passwordAttempt: string,
+  cipherText: ArrayBuffer,
+  iv: Uint8Array,
+  salt: Uint8Array
+) {
+  const keyMaterial = await getKeyMaterial(passwordAttempt)
+  const key = await getKey(keyMaterial, salt)
+
+  try {
+    // if this is successful, the password is correct. Otherwise it will throw an error
+    const result = await crypto.subtle.decrypt(
+      {
+        name: 'AES-GCM',
+        iv,
+      },
+      key,
+      cipherText
+    )
+    console.log('password is correct', new TextDecoder().decode(result))
+    return result
+  } catch (e) {
+    console.log('password is incorrect')
+    return undefined
+  }
+}
--- apps/extension/src/types/index.ts
+++ apps/extension/src/types/index.ts
@@ -3,8 +3,12 @@ export enum PortName {
     Store = 'store',
 }
 
+/** Message Passing */
 
-
+export type Message = {
+    type: MessageType;
+    data: any;
+};
 
 export enum MessageType {
     GetLocalStorage = "getLocalStorage",
@@ -22,3 +26,51 @@ export enum MessageType {
     SignMessageResponse = "SignMessageResponse",
     ValidatePassword = "ValidatePassword"
 }
+
+
+/** Notifications */
+export enum NotificationType {
+    swapSubmitting = 'swapSubmitting',
+    swapInProgress = 'swapInProgress',
+    swapComplete = 'swapComplete',
+    swapFailed = 'swapFailed',
+    swapCancelled = 'swapCancelled',
+    approvalPending = 'approvalPending',
+    approvalComplete = 'approvalComplete',
+    approvalFailed = 'approvalFailed',
+}
+
+export interface BaseNotification {
+    type: NotificationType
+}
+
+export interface SwapNotification extends BaseNotification {
+    swapInput: string
+    swapOutput: string
+    transactionHash?: string
+}
+
+export interface ApprovalNotification extends BaseNotification {
+    transactionHash: string
+}
+
+export interface SwapSubmittingNotification extends SwapNotification {
+    type: NotificationType.swapSubmitting
+}
+export interface SwapInProgressNotification extends SwapNotification {
+    type: NotificationType.swapInProgress
+}
+
+export interface SwapCompleteNotification extends SwapNotification {
+    type: NotificationType.swapComplete
+}
+
+export interface SwapFailedNotification extends SwapNotification {
+    type: NotificationType.swapFailed
+}
+
+export interface SwapCancelledNotification extends SwapNotification {
+    type: NotificationType.swapCancelled
+}
+
+export type Notification = SwapCompleteNotification | SwapFailedNotification | SwapCancelledNotification | SwapSubmittingNotification | SwapInProgressNotification
