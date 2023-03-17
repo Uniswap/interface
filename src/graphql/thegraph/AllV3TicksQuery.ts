03a2cbe80953e0d002d07972da1b575a6ed865bf
Vignesh Mohankumar <me@vig.xyz>
2023-03-17 11:08:12 -0400

    Revert "fix: skip when address undefined (#6172)"
    This reverts commit 801958d0ae93e5a0b96b568fac22a9098f95690a.

--- src/graphql/thegraph/AllV3TicksQuery.ts
+++ src/graphql/thegraph/AllV3TicksQuery.ts
@@ -1,9 +1,12 @@
+import { useQuery } from '@apollo/client'
 import gql from 'graphql-tag'
+import { useMemo } from 'react'
 
 import { AllV3TicksQuery } from './__generated__/types-and-hooks'
+import { apolloClient } from './apollo'
 
-gql`
-  query AllV3Ticks($poolAddress: String, $skip: Int!) {
+const query = gql`
+  query AllV3Ticks($poolAddress: String!, $skip: Int!) {
     ticks(first: 1000, skip: $skip, where: { poolAddress: $poolAddress }, orderBy: tickIdx) {
       tick: tickIdx
       liquidityNet
@@ -15,3 +18,27 @@ gql`
 
 export type Ticks = AllV3TicksQuery['ticks']
 export type TickData = Ticks[number]
+
+export default function useAllV3TicksQuery(poolAddress: string | undefined, skip: number, interval: number) {
+  const {
+    data,
+    loading: isLoading,
+    error,
+  } = useQuery<Record<'ticks', Ticks>>(query, {
+    variables: {
+      poolAddress: poolAddress?.toLowerCase(),
+      skip,
+    },
+    pollInterval: interval,
+    client: apolloClient,
+  })
+
+  return useMemo(
+    () => ({
+      error,
+      isLoading,
+      data,
+    }),
+    [data, error, isLoading]
+  )
+}
--- src/hooks/usePoolTickData.ts
+++ src/hooks/usePoolTickData.ts
@@ -3,8 +3,7 @@ import { FeeAmount, nearestUsableTick, Pool, TICK_SPACINGS, tickToPrice } from '
 import { useWeb3React } from '@web3-react/core'
 import { SupportedChainId } from 'constants/chains'
 import { ZERO_ADDRESS } from 'constants/misc'
-import { useAllV3TicksQuery } from 'graphql/thegraph/__generated__/types-and-hooks'
-import { TickData, Ticks } from 'graphql/thegraph/AllV3TicksQuery'
+import useAllV3TicksQuery, { TickData, Ticks } from 'graphql/thegraph/AllV3TicksQuery'
 import JSBI from 'jsbi'
 import { useSingleContractMultipleData } from 'lib/hooks/multicall'
 import ms from 'ms.macro'
@@ -156,11 +155,7 @@ function useTicksFromSubgraph(
         )
       : undefined
 
-  return useAllV3TicksQuery({
-    variables: { poolAddress: poolAddress?.toLowerCase(), skip },
-    skip: !poolAddress,
-    pollInterval: ms`30s`,
-  })
+  return useAllV3TicksQuery(poolAddress, skip, ms`30s`)
 }
 
 const MAX_THE_GRAPH_TICK_FETCH_VALUE = 1000
@@ -180,11 +175,12 @@ function useAllV3Ticks(
 
   const [skipNumber, setSkipNumber] = useState(0)
   const [subgraphTickData, setSubgraphTickData] = useState<Ticks>([])
-  const {
-    data,
-    error,
-    loading: isLoading,
-  } = useTicksFromSubgraph(useSubgraph ? currencyA : undefined, currencyB, feeAmount, skipNumber)
+  const { data, error, isLoading } = useTicksFromSubgraph(
+    useSubgraph ? currencyA : undefined,
+    currencyB,
+    feeAmount,
+    skipNumber
+  )
 
   useEffect(() => {
     if (data?.ticks.length) {
