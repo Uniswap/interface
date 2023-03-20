6fdf226e7f782c5dc3d3e81d64fb75df19557800
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-03-20 11:11:25 -0500

    fix: improve fetch-schema logging and platform support

--- scripts/fetch-schema.js
+++ scripts/fetch-schema.js
@@ -1,22 +1,26 @@
 /* eslint-env node */
 
 require('dotenv').config({ path: '.env.production' })
-
-const { exec } = require('child_process')
+const child_process = require('child_process')
+const fs = require('fs/promises')
+const { promisify } = require('util')
 const dataConfig = require('../graphql.config')
 const thegraphConfig = require('../graphql_thegraph.config')
 
-function fetchSchema(url, outputFile) {
-  exec(
-    `get-graphql-schema --h Origin=https://app.uniswap.org ${url} | tee ${outputFile}.temp`,
-    (error, stdout, stderr) => {
-      if (error || stderr) {
-        console.log(`Failed to fetch schema from ${url}`)
-      } else if (stdout) {
-        exec(`mv ${outputFile}.temp ${outputFile}`)
-      }
+const exec = promisify(child_process.exec)
+
+async function fetchSchema(url, outputFile) {
+  try {
+    const {stderr, stdout} = await exec(`npx get-graphql-schema --h Origin=https://app.uniswap.org ${url}`)
+    if (stderr) {
+      throw new Error(stderr)
+    } else {
+      fs.writeFile(outputFile, stdout)
     }
-  )
+  } catch (err) {
+    console.error(err)
+    console.error(`Failed to fetch schema from ${url}`)
+  }
 }
 
 fetchSchema(process.env.THE_GRAPH_SCHEMA_ENDPOINT, thegraphConfig.schema)
