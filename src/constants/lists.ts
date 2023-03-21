d18d007fe97b26eaca84dde37da3cfc7ef67e67c
alex <alex@taiko.xyz>
2023-03-21 23:08:30 +0800

    change link

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,5 +3,5 @@ export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'https://gist.githubusercontent.com/d1onys1us/8f8824daed0882b1094296f824fae53c/raw/0f90c917660e4cd77019bff8e9b7fe918f796d54/taiko_tokens.json'
+  'https://gist.githubusercontent.com/d1onys1us/8f8824daed0882b1094296f824fae53c/raw/15204c4a4927a7f48a78c195323ce0d573d6b7c0/taiko_tokens.json'
 ]
