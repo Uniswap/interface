c558facd3b284a30ac34de65cf6176ce03912d26
alex <alex@taiko.xyz>
2023-03-22 09:27:05 +0800

    feat: change the default tokens list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,7 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
-
-export const DEFAULT_LIST_OF_LISTS: string[] = [
-  DEFAULT_TOKEN_LIST_URL,
+export const DEFAULT_TOKEN_LIST_URL =
   'https://gist.githubusercontent.com/d1onys1us/8f8824daed0882b1094296f824fae53c/raw/15204c4a4927a7f48a78c195323ce0d573d6b7c0/taiko_tokens.json'
-]
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
