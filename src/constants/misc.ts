fe9cc87076cb58b421108b7b6b417f36de40dbb4
Justin Domingue <judo@uniswap.org>
2023-03-23 16:24:33 -0400

    chore: remove polling interval scaling factor (#3324)

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -1,14 +1,10 @@
 import { ONE_MINUTE_MS, ONE_SECOND_MS } from 'src/utils/time'
-import { isDevBuild } from 'src/utils/version'
-
-// temporarily slow down polling on dev builds to test out performance assumptions
-const scalingFactor = isDevBuild() ? 5 : 1
 
 // Polling interval (in milliseconds) for RTK-Query
 export enum PollingInterval {
-  Slow = scalingFactor * 5 * ONE_MINUTE_MS,
-  Normal = scalingFactor * ONE_MINUTE_MS,
-  Fast = scalingFactor * 12 * ONE_SECOND_MS, // block times for mainnet
+  Slow = 5 * ONE_MINUTE_MS,
+  Normal = ONE_MINUTE_MS,
+  Fast = 12 * ONE_SECOND_MS, // block times for mainnet
   LightningMcQueen = 3 * ONE_SECOND_MS, // 3 seconds, approx block times for polygon
 }
 
