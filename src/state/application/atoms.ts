acbaf09d01a4e27096b469caa0ff0ebd248508eb
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-03-27 12:21:37 -0500

    add comments explaining nft disable atom usage and suggesting future work

--- src/state/application/atoms.ts
+++ src/state/application/atoms.ts
@@ -1,5 +1,9 @@
 import { atomWithStorage, createJSONStorage } from 'jotai/utils'
 
+// note: consider a generic sessionStorage abstraction if this pattern becomes common.
+// i.e., for future promo dismissals like the tax serivice discounts or FoR launch notif
 const storage = createJSONStorage(() => sessionStorage)
 
+// some dapp browsers need to be able to disable the NFT portion of the app in order to pass Apple's app store review
+// this atom persists the inclusion of the `disableNFTs=boolean` query parameter via the webview's session storage
 export const shouldDisableNFTRoutesAtom = atomWithStorage('shouldDisableNFTRoutes', false, storage)
