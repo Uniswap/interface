7a4a8fea48dce5eed33c3db2b23b6ae829c087b0
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-03-27 14:25:23 -0500

    update comment

--- src/state/application/atoms.ts
+++ src/state/application/atoms.ts
@@ -1,9 +1,14 @@
 import { atomWithStorage, createJSONStorage } from 'jotai/utils'
 
-// note: consider a generic sessionStorage abstraction if this pattern becomes common.
-// i.e., for future promo dismissals like the tax serivice discounts or FoR launch notif
+/*
+  note: 
+  We should consider a generic sessionStorage abstraction if this pattern becomes common. (i.e., for future promo dismissals like the tax service discounts or FoR launch notification)
+  This would be something similar to the current feature flag implementation, but utilizing session instead
+
+  motivation:
+  some dapp browsers need to be able to disable the NFT portion of the app in order to pass Apple's app store review
+  this atom persists the inclusion of the `disableNFTs=boolean` query parameter via the webview's session storage
+*/
 const storage = createJSONStorage(() => sessionStorage)
 
-// some dapp browsers need to be able to disable the NFT portion of the app in order to pass Apple's app store review
-// this atom persists the inclusion of the `disableNFTs=boolean` query parameter via the webview's session storage
 export const shouldDisableNFTRoutesAtom = atomWithStorage('shouldDisableNFTRoutes', false, storage)
