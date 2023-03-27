300bee66aab3b51f4d2bdc844332cb71b2b1e26f
Spencer Yen <spencersyen@gmail.com>
2023-03-27 11:17:04 -0400

    chore: Remove Sentry log for notification received while app is open (#3345)

--- src/features/notifications/Onesignal.ts
+++ src/features/notifications/Onesignal.ts
@@ -1,27 +1,13 @@
 import { Linking } from 'react-native'
 import OneSignal, { NotificationReceivedEvent, OpenedEvent } from 'react-native-onesignal'
 import { config } from 'src/config'
-import { getOneSignalUserIdOrError } from 'src/features/firebase/utils'
 import { logger } from 'src/utils/logger'
 
 export const initOneSignal = (): void => {
   OneSignal.setLogLevel(6, 0)
   OneSignal.setAppId(config.onesignalAppId)
 
-  OneSignal.setNotificationWillShowInForegroundHandler(async (event: NotificationReceivedEvent) => {
-    try {
-      // Log to Sentry when a push notification is received while app is open.
-      // Used to debug users reporting not receiving notifications.
-      const pushId = await getOneSignalUserIdOrError()
-      logger.info(
-        'Onesignal',
-        'NotificationWillShowInForeground',
-        `${pushId} received notification while app is open: ${event.getNotification().body}`
-      )
-    } catch (error) {
-      logger.error('Onesignal', 'NotificationWillShowInForeground', 'Error:', error)
-    }
-
+  OneSignal.setNotificationWillShowInForegroundHandler((event: NotificationReceivedEvent) => {
     // Complete with undefined means don't show OS notifications while app is in foreground
     event.complete()
   })
