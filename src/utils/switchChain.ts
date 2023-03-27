e48e5cfaddcda2784874f819e1015864fe9f65cf
gab <gabriele.rigo@gmail.com>
2023-03-27 16:54:48 +0200

    add bnb to switch chain

--- src/utils/switchChain.ts
+++ src/utils/switchChain.ts
@@ -11,6 +11,7 @@ function getRpcUrl(chainId: SupportedChainId): string {
     case SupportedChainId.ROPSTEN:
     case SupportedChainId.KOVAN:
     case SupportedChainId.GOERLI:
+    case SupportedChainId.BNB:
       return RPC_URLS[chainId][0]
     // Attempting to add a chain using an infura URL will not work, as the URL will be unreachable from the MetaMask background page.
     // MetaMask allows switching to any publicly reachable URL, but for novel chains, it will display a warning if it is not on the "Safe" list.
