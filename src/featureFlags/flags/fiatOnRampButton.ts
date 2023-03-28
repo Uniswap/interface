cc3d5a78284fea5be1fadc2b91a62e9f141e58fe
Lynn Yu <lynn.yu@uniswap.org>
2023-03-28 17:57:26 -0400

    unused vars

--- src/featureFlags/flags/fiatOnRampButton.ts
+++ src/featureFlags/flags/fiatOnRampButton.ts
@@ -1,11 +1,9 @@
 import { BaseVariant, FeatureFlag, useBaseFlag } from '../index'
 
-export function useFiatOnRampButtonFlag(): BaseVariant {
+function useFiatOnRampButtonFlag(): BaseVariant {
   return useBaseFlag(FeatureFlag.fiatOnRampButtonOnSwap)
 }
 
 export function useFiatOnRampButtonEnabled(): boolean {
   return useFiatOnRampButtonFlag() === BaseVariant.Enabled
 }
-
-export { BaseVariant as FiatOnRampButtonVariant }
