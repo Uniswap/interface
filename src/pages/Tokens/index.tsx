6c59e88b06fec55f84500cc90a7a32b2c241ecb8
TBB <=>
2023-03-29 20:40:26 +0400

    testing change

--- src/pages/Tokens/index.tsx
+++ src/pages/Tokens/index.tsx
@@ -83,7 +83,7 @@ const Tokens = () => {
             placement="bottom"
           >
             <ThemedText.LargeHeader>
-              <Trans>Top tokens on Uniswap</Trans>
+              <Trans>Top tokens on Definitely Not Uniswap</Trans>
             </ThemedText.LargeHeader>
           </MouseoverTooltip>
         </TitleContainer>
