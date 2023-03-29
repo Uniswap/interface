d7542881d928ee4ac12e9a8fe10bea733495a323
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-03-29 14:04:06 -0500

    use NEVER_RELOAD

--- src/hooks/useTokenContractsConstant.ts
+++ src/hooks/useTokenContractsConstant.ts
@@ -1,14 +1,12 @@
 import { Interface } from '@ethersproject/abi'
 import ERC20ABI from 'abis/erc20.json'
 import { Erc20Interface } from 'abis/types/Erc20'
-import { useMultipleContractSingleData } from 'lib/hooks/multicall'
+import { NEVER_RELOAD, useMultipleContractSingleData } from 'lib/hooks/multicall'
 import { useMemo } from 'react'
 
 const ERC20Interface = new Interface(ERC20ABI) as Erc20Interface
 
 export function useTokenContractsConstant(tokens: string[], field: 'name' | 'symbol' | 'decimals' | 'totalSupply') {
   const memoizedTokens = useMemo(() => tokens, [tokens])
-  return useMultipleContractSingleData(memoizedTokens, ERC20Interface, field, undefined, {
-    blocksPerFetch: 10_000,
-  })
+  return useMultipleContractSingleData(memoizedTokens, ERC20Interface, field, undefined, NEVER_RELOAD)
 }
