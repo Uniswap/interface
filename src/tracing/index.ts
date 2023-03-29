7c37b9d00e565be2628091dc3a6fb7dbce917b39
Zach Pomerantz <zzmp@uniswap.org>
2023-03-29 11:05:13 -0700

    build: upgrade sentry (#6264)
    * build: upgrade sentry
    
    * chore: comment nits

--- src/tracing/index.ts
+++ src/tracing/index.ts
@@ -17,10 +17,13 @@ const AMPLITUDE_DUMMY_KEY = '00000000000000000000000000000000'
 export const STATSIG_DUMMY_KEY = 'client-0000000000000000000000000000000000000000000'
 
 Sentry.init({
+  // General configuration:
   dsn: process.env.REACT_APP_SENTRY_DSN,
   release: process.env.REACT_APP_GIT_COMMIT_HASH,
   environment: getEnvName(),
+  // Exception reporting configuration:
   enabled: isSentryEnabled(),
+  // Performance tracing configuration:
   tracesSampleRate: Number(process.env.REACT_APP_SENTRY_TRACES_SAMPLE_RATE ?? 0),
   integrations: [
     new BrowserTracing({
@@ -28,13 +31,6 @@ Sentry.init({
       startTransactionOnPageLoad: true,
     }),
   ],
-  /**
-   * TODO(INFRA-143)
-   * According to Sentry, this shouldn't be necessary, as they default to `3` when not set.
-   * Unfortunately, that doesn't work right now, so we workaround it by explicitly setting
-   * the `normalizeDepth` to `10`. This should be removed once the issue is fixed.
-   */
-  normalizeDepth: 10,
 })
 
 initializeAnalytics(AMPLITUDE_DUMMY_KEY, OriginApplication.INTERFACE, {
