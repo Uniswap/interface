d7e247c4ae458e718619f0e7e9e1fe80085449f6
Eddie Dugan <eddie.dugan@uniswap.org>
2023-03-29 10:39:46 -0700

    fix: add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,23 @@ require('@uniswap/eslint-config/load')
 
 module.exports = {
   extends: '@uniswap/eslint-config/react',
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        'no-restricted-imports': [
+          'error',
+          {
+            paths: [
+              {
+                name: 'zustand',
+                importNames: ['default'],
+                message: 'Default import from zustand is depracated. Use named import instead.',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
 }
