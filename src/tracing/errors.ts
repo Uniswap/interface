3901c4cd00d1e0705e973c856c6a084b43f6de65
Zach Pomerantz <zzmp@uniswap.org>
2023-04-03 13:53:29 -0700

    docs: type filterKnownErrors to beforeSend

--- src/tracing/errors.ts
+++ src/tracing/errors.ts
@@ -1,11 +1,15 @@
-import { ErrorEvent, EventHint } from '@sentry/types'
+import { ClientOptions, ErrorEvent, EventHint } from '@sentry/types'
 
 /** Identifies ethers request errors (as thrown by {@type import(@ethersproject/web).fetchJson}). */
 function isEthersRequestError(error: Error): error is Error & { requestBody: string } {
   return 'requestBody' in error && typeof (error as unknown as Record<'requestBody', unknown>).requestBody === 'string'
 }
 
-export function filterKnownErrors(event: ErrorEvent, hint: EventHint): ErrorEvent | null {
+/**
+ * Filters known (ignorable) errors out before sending them to Sentry.
+ * Intended as a {@link ClientOptions.beforeSend} callback. Returning null filters the error from Sentry.
+ */
+export const filterKnownErrors: Required<ClientOptions>['beforeSend'] = (event: ErrorEvent, hint: EventHint) => {
   const error = hint.originalException
   if (error instanceof Error) {
     // ethers aggressively polls for block number, and it sometimes fails (whether spuriously or through rate-limiting).
