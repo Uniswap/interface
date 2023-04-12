9f7a3757d477217eb396ed3173b17e162d8835bc
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-04-12 10:51:12 -0500

    tinaszheng pr feedback

--- src/hooks/useTokenContractsConstant.ts
+++ src/hooks/useTokenContractsConstant.ts
@@ -2,11 +2,9 @@ import { Interface } from '@ethersproject/abi'
 import ERC20ABI from 'abis/erc20.json'
 import { Erc20Interface } from 'abis/types/Erc20'
 import { NEVER_RELOAD, useMultipleContractSingleData } from 'lib/hooks/multicall'
-import { useMemo } from 'react'
 
 const ERC20Interface = new Interface(ERC20ABI) as Erc20Interface
 
 export function useTokenContractsConstant(tokens: string[], field: 'name' | 'symbol' | 'decimals' | 'totalSupply') {
-  const memoizedTokens = useMemo(() => tokens, [tokens])
-  return useMultipleContractSingleData(memoizedTokens, ERC20Interface, field, undefined, NEVER_RELOAD)
+  return useMultipleContractSingleData(tokens, ERC20Interface, field, undefined, NEVER_RELOAD)
 }
