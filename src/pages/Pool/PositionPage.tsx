084799ee78ff73849b300f4a55c17d5f33242076
Vignesh Mohankumar <me@vig.xyz>
2023-04-12 15:42:28 -0400

    fix

--- src/pages/Pool/PositionPage.tsx
+++ src/pages/Pool/PositionPage.tsx
@@ -346,21 +346,23 @@ const useInverter = ({
   }
 }
 
-const PositionPageUnsupportedContent = () => (
-  <PageWrapper>
-    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-      <ThemedText.HeadlineLarge style={{ marginBottom: '8px' }}>
-        <Trans>Position unavailable</Trans>
-      </ThemedText.HeadlineLarge>
-      <ThemedText.BodyPrimary style={{ marginBottom: '32px' }}>
-        <Trans>To view a position, you must be connected to the network it belongs to.</Trans>
-      </ThemedText.BodyPrimary>
-      <PositionPageButtonPrimary as={Link} to="/pools" width="fit-content">
-        <Trans>Back to Pools</Trans>
-      </PositionPageButtonPrimary>
-    </div>
-  </PageWrapper>
-)
+function PositionPageUnsupportedContent() {
+  return (
+    <PageWrapper>
+      <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+        <ThemedText.HeadlineLarge style={{ marginBottom: '8px' }}>
+          <Trans>Position unavailable</Trans>
+        </ThemedText.HeadlineLarge>
+        <ThemedText.BodyPrimary style={{ marginBottom: '32px' }}>
+          <Trans>To view a position, you must be connected to the network it belongs to.</Trans>
+        </ThemedText.BodyPrimary>
+        <PositionPageButtonPrimary as={Link} to="/pools" width="fit-content">
+          <Trans>Back to Pools</Trans>
+        </PositionPageButtonPrimary>
+      </div>
+    </PageWrapper>
+  )
+}
 
 export default function PositionPage() {
   const { chainId } = useWeb3React()
