34926778b943b15061092b72807d41e9e96a2fcf
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-04-12 11:08:27 -0500

    add comment explainer to useFilterPossiblyMaliciousPositions

--- src/hooks/useFilterPossiblyMaliciousPositions.ts
+++ src/hooks/useFilterPossiblyMaliciousPositions.ts
@@ -11,7 +11,17 @@ function getUniqueAddressesFromPositions(positions: PositionDetails[]): string[]
     new Set(positions.reduce<string[]>((acc, position) => acc.concat(position.token0, position.token1), []))
   )
 }
-
+/*
+ * This function is an attempt to filter out an observed phishing attack from LP list UIs.
+ * Attackers would airdrop valueless LP positions with urls in the symbol to render phishing sites into users' LP position list view.
+ *
+ * Our approach to filtering these out without naively prohibiting all valid URL symbols is to:
+ * 1. allow any pair if both tokens are in the supported list
+ * 2. allow one url if one token is in the supported list
+ * 3. allow no urls if neither token is in the supported list
+ *
+ * The hope is that this approach removes the cheapest version of the attack without punishing non-malicious url symbols
+ */
 export function useFilterPossiblyMaliciousPositions(positions: PositionDetails[]): PositionDetails[] {
   const activeTokensList = useDefaultActiveTokens()
 
