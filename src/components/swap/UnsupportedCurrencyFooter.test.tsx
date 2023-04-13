d7788aeec7874749d828e88b16192a87d9e9a075
Lynn Yu <lynn.yu@uniswap.org>
2023-04-13 15:06:31 -0400

    undo, it wasnt working

--- src/components/swap/UnsupportedCurrencyFooter.test.tsx
+++ src/components/swap/UnsupportedCurrencyFooter.test.tsx
@@ -50,10 +50,10 @@ describe('UnsupportedCurrencyFooter.tsx', () => {
     ).toBeNull()
   })
 
-  it('works as expected when one unsupported token exists', () => {
+  it('works as expected when one unsupported token exists', async () => {
     mockUseUnsupportedTokens.mockImplementation(() => ({ [unsupportedTokenAddress]: unsupportedToken }))
     mockGetExplorerLink.mockImplementation(() => unsupportedTokenExplorerLink)
-    render(<UnsupportedCurrencyFooter show={true} currencies={[unsupportedToken]} />)
+    const rendered = render(<UnsupportedCurrencyFooter show={true} currencies={[unsupportedToken]} />)
     fireEvent.click(screen.getByTestId('read-more-button'))
     expect(screen.getByText('Unsupported Assets')).toBeInTheDocument()
     expect(
@@ -66,13 +66,6 @@ describe('UnsupportedCurrencyFooter.tsx', () => {
       'href',
       unsupportedTokenExplorerLink
     )
-  })
-
-  it('works as expected when one unsupported token exists and modal is closed', async () => {
-    mockUseUnsupportedTokens.mockImplementation(() => ({ [unsupportedTokenAddress]: unsupportedToken }))
-    mockGetExplorerLink.mockImplementation(() => unsupportedTokenExplorerLink)
-    const rendered = render(<UnsupportedCurrencyFooter show={true} currencies={[unsupportedToken]} />)
-    fireEvent.click(screen.getByTestId('read-more-button'))
     fireEvent.click(screen.getByTestId('close-icon'))
     await waitForElementToBeRemoved(screen.getByTestId('unsupported-token-card'))
     expect(rendered.queryByText('Unsupported Assets')).toBeNull()
