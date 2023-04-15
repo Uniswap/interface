7e67a5f869a2a063a666483da973c2e31a0862fe
AlanOG <83054641+AlanPel@users.noreply.github.com>
2023-04-15 10:44:20 +0200

    Update ProtocolBanner.tsx

--- src/components/About/ProtocolBanner.tsx
+++ src/components/About/ProtocolBanner.tsx
@@ -95,7 +95,7 @@ const ProtocolBanner = () => {
         </DescriptionText>
       </TextContainer>
       <BannerButtonContainer>
-        <BannerButton width="200px" as="a" href="https://uniswap.org" rel="noopener noreferrer" target="_blank">
+        <BannerButton width="200px" as="a" href="https://twitter.com/theeosjourney" rel="noopener noreferrer" target="_blank">
           Learn more
         </BannerButton>
       </BannerButtonContainer>
