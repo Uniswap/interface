5f177059fb29b5c612ef6e6744022b5904ec65d2
Spencer Yen <spencersyen@gmail.com>
2023-04-18 09:35:17 -0700

    chore: Change Network Fee loader to spinner  (#3411)

--- src/components/Network/NetworkFee.tsx
+++ src/components/Network/NetworkFee.tsx
@@ -4,6 +4,7 @@ import { useAppTheme } from 'src/app/hooks'
 import InfoCircleSVG from 'src/assets/icons/info-circle.svg'
 import { TouchableArea } from 'src/components/buttons/TouchableArea'
 import { Box, Flex } from 'src/components/layout'
+import { SpinningLoader } from 'src/components/loading/SpinningLoader'
 import { InlineNetworkPill } from 'src/components/Network/NetworkPill'
 import { Text } from 'src/components/Text'
 import { ChainId } from 'src/constants/chains'
@@ -29,7 +30,6 @@ export function NetworkFee({
     <>
       <Text
         color={gasFallbackUsed && gasFeeUSD ? 'accentWarning' : 'textPrimary'}
-        loading={!gasFeeUSD}
         variant="subheadSmall">
         {formatUSDPrice(gasFeeUSD, NumberType.FiatGasPrice)}
       </Text>
@@ -48,28 +48,24 @@ export function NetworkFee({
   return (
     <Flex row alignItems="center" justifyContent="space-between" px="spacing12" py="spacing12">
       <Text variant="subheadSmall">{t('Network fee')}</Text>
-      <Flex row gap="none" justifyContent="flex-end">
-        <Flex row alignItems="center" gap="spacing8">
-          {gasFeeUSD ? (
-            <Flex row alignItems="center" gap="spacing8">
-              <InlineNetworkPill chainId={chainId} />
-              <Text variant="subheadSmall">•</Text>
-            </Flex>
-          ) : null}
-          {gasFallbackUsed && onShowGasWarning ? (
-            <TouchableArea
-              alignItems="center"
-              flexDirection="row"
-              justifyContent="space-between"
-              onPress={onShowGasWarning}>
-              {feeSectionContent}
-            </TouchableArea>
-          ) : (
-            <Flex row alignItems="center" justifyContent="space-between">
-              {feeSectionContent}
-            </Flex>
-          )}
-        </Flex>
+      <Flex row alignItems="center" gap="spacing8">
+        <InlineNetworkPill chainId={chainId} />
+        <Text variant="subheadSmall">•</Text>
+        {!gasFeeUSD ? (
+          <SpinningLoader size={theme.iconSizes.icon20} />
+        ) : gasFallbackUsed && onShowGasWarning ? (
+          <TouchableArea
+            alignItems="center"
+            flexDirection="row"
+            justifyContent="space-between"
+            onPress={onShowGasWarning}>
+            {feeSectionContent}
+          </TouchableArea>
+        ) : (
+          <Flex row alignItems="center" justifyContent="space-between">
+            {feeSectionContent}
+          </Flex>
+        )}
       </Flex>
     </Flex>
   )
