880c7f3b3270ed552fcf5a408c4e921e13a44b1c
Lynn Yu <lynn.yu@uniswap.org>
2023-04-19 10:48:52 -0400

    checkpoint

--- src/components/swap/AdvancedSwapDetails.test.tsx
+++ src/components/swap/AdvancedSwapDetails.test.tsx
@@ -1,30 +1,7 @@
-import { CurrencyAmount, Percent, Token, TradeType } from '@uniswap/sdk-core'
-import { FeeAmount, Pool, Route as V3Route } from '@uniswap/v3-sdk'
-import JSBI from 'jsbi'
-import { InterfaceTrade } from 'state/routing/types'
-import { render } from 'test-utils'
+import { testAllowedSlippage, testTradeExactInput } from 'test-utils/constants'
+import { render } from 'test-utils/render'
 
 import { AdvancedSwapDetails } from './AdvancedSwapDetails'
-
-const token1 = new Token(1, '0x0000000000000000000000000000000000000001', 18)
-const token2 = new Token(1, '0x0000000000000000000000000000000000000002', 18)
-
-const pool12 = new Pool(token1, token2, FeeAmount.HIGH, '2437312313659959819381354528', '10272714736694327408', -69633)
-
-const currencyAmount = (token: Token, amount: number) => CurrencyAmount.fromRawAmount(token, JSBI.BigInt(amount))
-
-const trade = new InterfaceTrade({
-  v3Routes: [
-    {
-      routev3: new V3Route([pool12], token1, token2),
-      inputAmount: currencyAmount(token1, 1000),
-      outputAmount: currencyAmount(token2, 1000),
-    },
-  ],
-  v2Routes: [],
-  tradeType: TradeType.EXACT_INPUT,
-})
-const allowedSlippage = new Percent(2, 100)
 
 jest.mock('@web3-react/core', () => {
   const web3React = jest.requireActual('@web3-react/core')
@@ -40,7 +17,9 @@ jest.mock('@web3-react/core', () => {
 
 describe('AdvancedSwapDetails.tsx', () => {
   it('matches base snapshot', () => {
-    const { asFragment } = render(<AdvancedSwapDetails trade={trade} allowedSlippage={allowedSlippage} />)
+    const { asFragment } = render(
+      <AdvancedSwapDetails trade={testTradeExactInput} allowedSlippage={testAllowedSlippage} />
+    )
     expect(asFragment()).toMatchSnapshot()
   })
 })
