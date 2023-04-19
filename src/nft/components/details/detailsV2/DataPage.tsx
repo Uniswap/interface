bd6e47075cd0dc8e48ab8ef1cded9578499f9b3e
Charles Bachmeier <charlie@genie.xyz>
2023-04-19 10:11:20 -0700

    mobile wrap

--- src/nft/components/details/detailsV2/DataPage.tsx
+++ src/nft/components/details/detailsV2/DataPage.tsx
@@ -1,6 +1,7 @@
 import Column from 'components/Column'
 import Row from 'components/Row'
 import styled from 'styled-components/macro'
+import { BREAKPOINTS } from 'theme'
 
 import { DataPageDescription } from './DataPageDescription'
 import { DataPageHeader } from './DataPageHeader'
@@ -8,10 +9,19 @@ import { DataPageTable } from './DataPageTable'
 import { DataPageTraits } from './DataPageTraits'
 
 const DataPageContainer = styled(Column)`
-  padding: 24px 120px 45px;
+  padding: 24px 120px;
   height: 100vh;
   width: 100%;
   gap: 36px;
+`
+
+const ContentContainer = styled(Row)`
+  gap: 24px;
+  padding-bottom: 45px;
+
+  @media screen and (max-width: ${BREAKPOINTS.lg}px) {
+    flex-wrap: wrap;
+  }
 `
 
 const LeftColumn = styled(Column)`
@@ -25,13 +35,13 @@ export const DataPage = () => {
   return (
     <DataPageContainer>
       <DataPageHeader />
-      <Row gap="24px">
+      <ContentContainer>
         <LeftColumn>
           <DataPageTraits />
           <DataPageDescription />
         </LeftColumn>
         <DataPageTable />
-      </Row>
+      </ContentContainer>
     </DataPageContainer>
   )
 }
