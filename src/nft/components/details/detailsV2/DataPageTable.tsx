f8f91c996d3aa3c7ab0145c4e2fbe118730ed9bc
Charles Bachmeier <charlie@genie.xyz>
2023-04-19 10:05:39 -0700

    add table tabs

--- src/nft/components/details/detailsV2/DataPageTable.tsx
+++ src/nft/components/details/detailsV2/DataPageTable.tsx
@@ -1,7 +1,12 @@
 import Column from 'components/Column'
 import Row from 'components/Row'
+import { useState } from 'react'
 import styled from 'styled-components/macro'
+import { ThemedText } from 'theme'
 
+import { ActivityTableContent } from './ActivityTableContent'
+import { ListingsTableContent } from './ListingsTableContent'
+import { OffersTableContent } from './OffersTableContent'
 import { containerStyles } from './shared'
 
 const TableContainer = styled(Column)`
@@ -13,11 +18,59 @@ const TableContainer = styled(Column)`
   ${containerStyles}
 `
 
+const TabsRow = styled(Row)`
+  gap: 32px;
+  margin-bottom: 24px;
+  width: 100;
+  padding: 0px 20px;
+`
+
+const Tab = styled(ThemedText.SubHeader)<{ isActive: boolean }>`
+  color: ${({ theme, isActive }) => (isActive ? theme.textPrimary : theme.textTertiary)};
+  line-height: 24px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: ${({ theme }) => theme.opacity.hover};
+  }
+`
+
+enum TableTabs {
+  Activity,
+  Offers,
+  Listings,
+}
+
 export const DataPageTable = () => {
+  const [activeTab, setActiveTab] = useState(TableTabs.Activity)
+
+  let activeTabContent
+  switch (activeTab) {
+    case TableTabs.Activity:
+      activeTabContent = <ActivityTableContent />
+      break
+    case TableTabs.Offers:
+      activeTabContent = <OffersTableContent />
+      break
+    case TableTabs.Listings:
+      activeTabContent = <ListingsTableContent />
+      break
+  }
+
   return (
     <TableContainer>
-      Table
-      <Row></Row>
+      <TabsRow>
+        <Tab isActive={activeTab === TableTabs.Activity} onClick={() => setActiveTab(TableTabs.Activity)}>
+          Activity
+        </Tab>
+        <Tab isActive={activeTab === TableTabs.Offers} onClick={() => setActiveTab(TableTabs.Offers)}>
+          Offers
+        </Tab>
+        <Tab isActive={activeTab === TableTabs.Listings} onClick={() => setActiveTab(TableTabs.Listings)}>
+          Listings
+        </Tab>
+      </TabsRow>
+      {activeTabContent}
     </TableContainer>
   )
 }
