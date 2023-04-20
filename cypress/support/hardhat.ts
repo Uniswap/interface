043311ae20b80d53ee64bc11138a4eb3188a7461
Zach Pomerantz <zzmp@uniswap.org>
2023-04-20 11:27:21 -0700

    fix: massage eth_sendTransaction

--- cypress/support/hardhat.ts
+++ cypress/support/hardhat.ts
@@ -1,22 +1,23 @@
 import { Eip1193Bridge } from '@ethersproject/experimental/lib/eip1193-bridge'
+import { JsonRpcProvider } from '@ethersproject/providers'
 import { Wallet } from '@ethersproject/wallet'
 import { HardhatUtils, Network } from 'cypress-hardhat/lib/browser'
 
 export class HardhatProvider extends Eip1193Bridge {
-  utils: HardhatUtils
-  chainId: string
-  signer: Wallet
-  provider: typeof this.utils.provider
+  readonly utils: HardhatUtils
+  readonly chainId: string
+  readonly wallet: Wallet
+
+  isMetaMask = true
 
   constructor(network: Network) {
     const utils = new HardhatUtils(network)
-    const signer = new Wallet(utils.account.privateKey, utils.provider)
-    super(signer, utils.provider)
+    const wallet = new Wallet(utils.account.privateKey, utils.provider)
+    super(wallet, utils.provider)
 
     this.utils = utils
     this.chainId = `0x${network.chainId.toString(16)}`
-    this.signer = signer
-    this.provider = utils.provider
+    this.wallet = wallet
   }
 
   async sendAsync(...args: any[]) {
@@ -24,13 +25,13 @@ export class HardhatProvider extends Eip1193Bridge {
   }
 
   async send(...args: any[]) {
-    console.debug('send', ...args)
+    console.debug('hardhat:send', ...args)
 
     // Parse callback form.
     const isCallbackForm = typeof args[0] === 'object' && typeof args[1] === 'function'
     let callback = <T>(error: Error | null, result?: { result: T }) => {
       if (error) throw error
-      return result
+      return result?.result
     }
     let method
     let params
@@ -43,19 +44,37 @@ export class HardhatProvider extends Eip1193Bridge {
       params = args[1]
     }
 
-    switch (method) {
-      case 'eth_requestAccounts':
-      case 'eth_accounts':
-        return callback(null, { result: [this.signer.address] })
-      case 'eth_chainId':
-        return callback(null, { result: this.chainId })
-    }
-
+    let result
     try {
-      const result = await super.send(method, params)
-      console.debug('receive', method, result)
+      switch (method) {
+        case 'eth_requestAccounts':
+        case 'eth_accounts':
+          result = [this.wallet.address]
+          break
+        case 'eth_chainId':
+          result = this.chainId
+          break
+        case 'eth_sendTransaction': {
+          // Eip1193Bridge doesn't support .gas and .from directly, so we massage it to satisfy ethers' expectations.
+          // See https://github.com/ethers-io/ethers.js/issues/1683.
+          params[0].gasLimit = params[0].gas
+          delete params[0].gas
+          delete params[0].from
+
+          const req = JsonRpcProvider.hexlifyTransaction(params[0])
+          req.gasLimit = req.gas
+          delete req.gas
+
+          result = (await this.signer.sendTransaction(req)).hash
+          break
+        }
+        default:
+          result = await super.send(method, params)
+      }
+      console.debug('hardhat:receive', method, result)
       return callback(null, { result })
     } catch (error) {
+      console.debug('hardhat:error', method, error)
       return callback(error as Error)
     }
   }
