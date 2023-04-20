70961b3a55e7be55cd730a5fa140b23eb6058133
Vignesh Mohankumar <me@vig.xyz>
2023-04-20 16:37:36 -0400

    cast earlier

--- src/tracing/errors.ts
+++ src/tracing/errors.ts
@@ -47,9 +47,10 @@ export const filterKnownErrors: Required<ClientOptions>['beforeSend'] = (event: 
      */
     if (error.message.match(/Loading chunk \d+ failed\. \(error: .+\.chunk\.js\)/)) {
       const asset = error.message.match(/https?:\/\/.+?\.chunk\.js/)?.[0]
-      const resource = [...(performance?.getEntriesByType('resource') ?? [])].find(({ name }) => name === asset)
+      const entries = [...(performance?.getEntriesByType('resource') as any[])]
+      const resource = entries?.find(({ name }) => name === asset)
       // `responseStatus` is not on the `ResponseStatus` type, because it's only supported on some browsers at the moment.
-      const status = (resource as any)?.responseStatus
+      const status = resource?.responseStatus
       // If the status if 499, then we ignore. If there's no status, for now we'll ignore all chunk loading errors also.
       if (!status || status === 499) {
         return null
