2ed70e7d033cf828b355317d0bed63f336520d6c
Vignesh Mohankumar <me@vig.xyz>
2023-04-20 11:04:24 -0400

    more tests

--- src/tracing/errors.test.ts
+++ src/tracing/errors.test.ts
@@ -37,78 +37,139 @@ describe('filterKnownErrors', () => {
       jest.restoreAllMocks()
     })
 
-    it('filters out 499 error coded chunk error', () => {
-      const mockedEntries = [
-        {
-          name: 'https://app.uniswap.org/static/js/20.d55382e0.chunk.js',
-          entryType: 'resource',
-          startTime: 253.79999999701977,
-          duration: 3.0999999940395355,
-          initiatorType: 'link',
-          nextHopProtocol: 'h2',
-          renderBlockingStatus: 'non-blocking',
-          workerStart: 254,
-          redirectStart: 0,
-          redirectEnd: 0,
-          fetchStart: 254.19999998807907,
-          domainLookupStart: 254.19999998807907,
-          domainLookupEnd: 254.19999998807907,
-          connectStart: 254.19999998807907,
-          secureConnectionStart: 254.19999998807907,
-          connectEnd: 254.19999998807907,
-          requestStart: 255.09999999403954,
-          responseStart: 256,
-          responseEnd: 256.8999999910593,
-          transferSize: 0,
-          encodedBodySize: 227688,
-          decodedBodySize: 227688,
-          responseStatus: 499,
-          serverTiming: [],
-        },
-      ]
-      getEntriesByTypeSpy = jest.spyOn(performance, 'getEntriesByType')
-      getEntriesByTypeSpy.mockImplementation(() => mockedEntries)
+    it('filters 499 error coded chunk error', () => {
+      const mockGetEntriesByType = jest.fn(() => {
+        return [
+          {
+            name: 'https://app.uniswap.org/static/js/20.d55382e0.chunk.js',
+            entryType: 'resource',
+            startTime: 253.79999999701977,
+            duration: 3.0999999940395355,
+            initiatorType: 'link',
+            nextHopProtocol: 'h2',
+            renderBlockingStatus: 'non-blocking',
+            workerStart: 254,
+            redirectStart: 0,
+            redirectEnd: 0,
+            fetchStart: 254.19999998807907,
+            domainLookupStart: 254.19999998807907,
+            domainLookupEnd: 254.19999998807907,
+            connectStart: 254.19999998807907,
+            secureConnectionStart: 254.19999998807907,
+            connectEnd: 254.19999998807907,
+            requestStart: 255.09999999403954,
+            responseStart: 256,
+            responseEnd: 256.8999999910593,
+            transferSize: 0,
+            encodedBodySize: 227688,
+            decodedBodySize: 227688,
+            responseStatus: 499,
+            serverTiming: [],
+          },
+        ]
+      })
+      window.performance.getEntriesByType = mockGetEntriesByType
       const originalException = new SyntaxError(
         'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
       )
-      expect(filterKnownErrors(ERROR, { originalException })).toBe(null)
+      expect(filterKnownErrors(ERROR, { originalException })).toBeNull()
     })
 
     it('keeps 200 error coded chunk error', () => {
-      const mockedEntries = [
-        {
-          name: 'https://app.uniswap.org/static/js/20.d55382e0.chunk.js',
-          entryType: 'resource',
-          startTime: 253.79999999701977,
-          duration: 3.0999999940395355,
-          initiatorType: 'link',
-          nextHopProtocol: 'h2',
-          renderBlockingStatus: 'non-blocking',
-          workerStart: 254,
-          redirectStart: 0,
-          redirectEnd: 0,
-          fetchStart: 254.19999998807907,
-          domainLookupStart: 254.19999998807907,
-          domainLookupEnd: 254.19999998807907,
-          connectStart: 254.19999998807907,
-          secureConnectionStart: 254.19999998807907,
-          connectEnd: 254.19999998807907,
-          requestStart: 255.09999999403954,
-          responseStart: 256,
-          responseEnd: 256.8999999910593,
-          transferSize: 0,
-          encodedBodySize: 227688,
-          decodedBodySize: 227688,
-          responseStatus: 200,
-          serverTiming: [],
-        },
-      ]
-      getEntriesByTypeSpy = jest.spyOn(performance, 'getEntriesByType')
-      getEntriesByTypeSpy.mockImplementation(() => mockedEntries)
+      const mockGetEntriesByType = jest.fn(() => {
+        return [
+          {
+            name: 'https://app.uniswap.org/static/js/20.d55382e0.chunk.js',
+            entryType: 'resource',
+            startTime: 253.79999999701977,
+            duration: 3.0999999940395355,
+            initiatorType: 'link',
+            nextHopProtocol: 'h2',
+            renderBlockingStatus: 'non-blocking',
+            workerStart: 254,
+            redirectStart: 0,
+            redirectEnd: 0,
+            fetchStart: 254.19999998807907,
+            domainLookupStart: 254.19999998807907,
+            domainLookupEnd: 254.19999998807907,
+            connectStart: 254.19999998807907,
+            secureConnectionStart: 254.19999998807907,
+            connectEnd: 254.19999998807907,
+            requestStart: 255.09999999403954,
+            responseStart: 256,
+            responseEnd: 256.8999999910593,
+            transferSize: 0,
+            encodedBodySize: 227688,
+            decodedBodySize: 227688,
+            responseStatus: 200,
+            serverTiming: [],
+          },
+        ]
+      })
+      window.performance.getEntriesByType = mockGetEntriesByType
       const originalException = new SyntaxError(
         'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
       )
-      expect(filterKnownErrors(ERROR, { originalException })).toBeDefined()
+      expect(filterKnownErrors(ERROR, { originalException })).not.toBeNull()
+    })
+
+    it('filters out error when resource is missing', () => {
+      const mockGetEntriesByType = jest.fn(() => {
+        return []
+      })
+      window.performance.getEntriesByType = mockGetEntriesByType
+      const originalException = new SyntaxError(
+        'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
+      )
+      expect(filterKnownErrors(ERROR, { originalException })).toBeNull()
+    })
+
+    it('filters out error when performance is undefined', () => {
+      const mockGetEntriesByType = jest.fn(() => {
+        return []
+      })
+      window.performance.getEntriesByType = mockGetEntriesByType
+      const originalException = new SyntaxError(
+        'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
+      )
+      expect(filterKnownErrors(ERROR, { originalException })).toBeNull()
+    })
+
+    it('filters out error when responseStatus is undefined', () => {
+      const mockGetEntriesByType = jest.fn(() => {
+        return [
+          {
+            name: 'https://app.uniswap.org/static/js/20.d55382e0.chunk.js',
+            entryType: 'resource',
+            startTime: 253.79999999701977,
+            duration: 3.0999999940395355,
+            initiatorType: 'link',
+            nextHopProtocol: 'h2',
+            renderBlockingStatus: 'non-blocking',
+            workerStart: 254,
+            redirectStart: 0,
+            redirectEnd: 0,
+            fetchStart: 254.19999998807907,
+            domainLookupStart: 254.19999998807907,
+            domainLookupEnd: 254.19999998807907,
+            connectStart: 254.19999998807907,
+            secureConnectionStart: 254.19999998807907,
+            connectEnd: 254.19999998807907,
+            requestStart: 255.09999999403954,
+            responseStart: 256,
+            responseEnd: 256.8999999910593,
+            transferSize: 0,
+            encodedBodySize: 227688,
+            decodedBodySize: 227688,
+            serverTiming: [],
+          },
+        ]
+      })
+      window.performance.getEntriesByType = mockGetEntriesByType
+      const originalException = new SyntaxError(
+        'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
+      )
+      expect(filterKnownErrors(ERROR, { originalException })).toBeNull()
     })
   })
 })
