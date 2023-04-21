e486d73d1b20b2e270198bf4dbeab92676599e74
Zach Pomerantz <zzmp@uniswap.org>
2023-04-21 15:01:53 -0700

    test: prevent async debounce updates

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,9 @@
 import '@testing-library/jest-dom' // jest custom assertions
+import { useWeb3React } from '@web3-react/core'
 import 'jest-styled-components' // adds style diffs to snapshot tests
 
 import { Readable } from 'stream'
+import { mocked } from 'test-utils/mocked'
 import { TextDecoder, TextEncoder } from 'util'
 
 if (typeof global.TextEncoder === 'undefined') {
@@ -19,3 +21,15 @@ global.matchMedia =
       removeEventListener: jest.fn(),
     }
   }
+
+jest.mock('react-popper', () => {
+  const { usePopper } = jest.requireActual('react-popper')
+  return {
+    usePopper: function () {
+      const popper = usePopper(arguments)
+      // Prevent popper from asynchronously debouncing updates during tests.
+      popper.update = popper.forceUpdate
+      return popper
+    }
+  }
+})
