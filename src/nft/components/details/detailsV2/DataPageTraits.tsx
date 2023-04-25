54b818e8a3cb6efd58f5e5cbc51a8bf99ed2818b
Charlie <charles@bachmeier.io>
2023-04-25 11:27:37 -0700

    add traits header row

--- src/nft/components/details/detailsV2/DataPageTraits.tsx
+++ src/nft/components/details/detailsV2/DataPageTraits.tsx
@@ -1,16 +1,47 @@
 import { Trans } from '@lingui/macro'
-import { GenieAsset } from 'nft/types'
+import Column from 'components/Column'
+import Row from 'components/Row'
+import { GenieAsset, Trait } from 'nft/types'
 import { useMemo } from 'react'
 import styled from 'styled-components/macro'
+import { ThemedText } from 'theme'
 
 import { Tab, TabbedComponent } from './TabbedComponent'
 
-const TraitsContentContainer = styled.div`
-  height: 492px;
+const TraitsContentContainer = styled(Column)`
+  padding-right: 12px;
 `
 
-const TraitsContent = () => {
-  return <TraitsContentContainer>Traits Content</TraitsContentContainer>
+const TraitsHeader = styled(ThemedText.SubHeaderSmall)<{ $flex?: number; alignRight?: boolean }>`
+  display: flex;
+  line-height: 20px;
+  color: ${({ theme }) => theme.textSecondary};
+  flex: ${({ $flex }) => $flex ?? 1};
+  ${({ alignRight }) => alignRight && 'justify-content: flex-end'};
+`
+
+const TraitsContent = ({ traits }: { traits?: Trait[] }) => {
+  return (
+    <TraitsContentContainer>
+      <Row>
+        <TraitsHeader $flex={3}>
+          <Trans>Trait</Trans>
+        </TraitsHeader>{' '}
+        <TraitsHeader $flex={1.5}>
+          <Trans>Floor price</Trans>
+        </TraitsHeader>{' '}
+        <TraitsHeader>
+          <Trans>Quantity</Trans>
+        </TraitsHeader>{' '}
+        <TraitsHeader alignRight={true}>
+          <Trans>Rarity</Trans>
+        </TraitsHeader>
+      </Row>
+      {traits?.map((trait) => (
+        <Row key={trait.trait_type + ':' + trait.trait_value}>{trait.trait_type + ':' + trait.trait_value} </Row>
+      ))}
+    </TraitsContentContainer>
+  )
 }
 
 enum TraitTabsKeys {
@@ -26,12 +57,12 @@ export const DataPageTraits = ({ asset }: { asset: GenieAsset }) => {
           {
             title: <Trans>Traits</Trans>,
             key: TraitTabsKeys.Traits,
-            content: <TraitsContent />,
+            content: <TraitsContent traits={asset.traits} />,
             count: asset.traits?.length,
           },
         ],
       ]),
-    [asset.traits?.length]
+    [asset.traits]
   )
   return <TabbedComponent tabs={TraitTabs} />
 }
