5cacab87b06ff8c26fd22a1cc6b3d2589782e321
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-04-26 14:11:15 -0500

    Update hardhat.config.js
    Co-authored-by: Zach Pomerantz <zzmp@uniswap.org>

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,6 +21,7 @@ module.exports = {
       accounts: {
         count: 1,
       },
+      // Disable auto-mining so that e2e tests can explicitly test pending states.
       mining: {
         auto: false,
         interval: 0,
