e4c4e90c94b8868f5d08d12e761e0aa6ab4ec982
Eddie Dugan <eddie.dugan@uniswap.org>
2023-04-26 11:23:44 -0700

    fix: remove unused test util

--- cypress/utils/index.ts
+++ cypress/utils/index.ts
@@ -1,7 +1,3 @@
 export const getTestSelector = (selectorId: string) => `[data-testid=${selectorId}]`
 
 export const getTestSelectorStartsWith = (selectorId: string) => `[data-testid^=${selectorId}]`
-
-// TODO: Remove this comment if you use the util.
-// eslint-disable-next-line import/no-unused-modules
-export const getClassContainsSelector = (selectorId: string) => `[class*=${selectorId}]`
