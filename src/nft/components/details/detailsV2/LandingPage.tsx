658733e268eae62e410b3c8e1b8c94e3697d8838
Jack Short <john.short.tj@gmail.com>
2023-04-27 12:52:43 -0400

    responding to comments

--- src/nft/components/details/detailsV2/LandingPage.tsx
+++ src/nft/components/details/detailsV2/LandingPage.tsx
@@ -1,4 +1,4 @@
-import Column from 'components/Column'
+import Column, { ColumnCenter } from 'components/Column'
 import Row from 'components/Row'
 import { VerifiedIcon } from 'nft/components/icons'
 import { CollectionInfoForAsset, GenieAsset } from 'nft/types'
@@ -7,10 +7,10 @@ import { BREAKPOINTS, ThemedText } from 'theme'
 
 const MAX_WIDTH = 560
 
-const Container = styled.div`
+const LandingPageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: ${({ theme }) => `calc(100vh - ${theme.navHeight}px)`};
+  min-height: ${({ theme }) => `calc(100vh - ${theme.navHeight}px - ${theme.mobileBottomBarHeight}px)`};
   align-items: center;
   padding: 22px 20px 0px;
   gap: 26px;
@@ -18,6 +18,10 @@ const Container = styled.div`
   @media screen and (min-width: ${BREAKPOINTS.sm}px) {
     gap: 64px;
     padding-top: 28px;
+  }
+
+  @media screen and (min-width: ${BREAKPOINTS.md}px) {
+    min-height: ${({ theme }) => `calc(100vh - ${theme.navHeight}px )`};
   }
 
   @media screen and (min-width: ${BREAKPOINTS.xl}px) {
@@ -28,9 +32,7 @@ const Container = styled.div`
   }
 `
 
-const InfoContainer = styled(Column)`
-  width: 100%;
-  align-items: center;
+const InfoContainer = styled(ColumnCenter)`
   gap: 40px;
 
   @media screen and (min-width: ${BREAKPOINTS.sm}px) {
@@ -49,7 +51,7 @@ const MediaContainer = styled.div`
 
   @media screen and (min-width: ${BREAKPOINTS.sm}px) {
     width: ${MAX_WIDTH}px;
-    width: ${MAX_WIDTH}px;
+    height: ${MAX_WIDTH}px;
   }
 `
 
@@ -82,19 +84,19 @@ interface LandingPageProps {
 
 export const LandingPage = ({ asset, collection }: LandingPageProps) => {
   return (
-    <Container>
+    <LandingPageContainer>
       <MediaContainer>
         <StyledMedia src={asset.imageUrl} />
       </MediaContainer>
       <InfoContainer>
         <InfoDetailsContainer>
           <Row justify="center" gap="4px" align="center">
-            <StyledSubheaderText>{asset.name ?? `${asset.collectionName} #${asset.tokenId}`}</StyledSubheaderText>
+            <StyledSubheaderText>{collection.collectionName}</StyledSubheaderText>
             {collection.isVerified && <VerifiedIcon width="16px" height="16px" />}
           </Row>
-          <StyledHeadlineText>{collection.collectionName}</StyledHeadlineText>
+          <StyledHeadlineText>{asset.name ?? `${asset.collectionName} #${asset.tokenId}`}</StyledHeadlineText>
         </InfoDetailsContainer>
       </InfoContainer>
-    </Container>
+    </LandingPageContainer>
   )
 }
