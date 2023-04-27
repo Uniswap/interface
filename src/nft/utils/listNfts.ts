b92b2866262b7c2df479ee966ba3e0910e6d2f95
Jack Short <john.short.tj@gmail.com>
2023-04-27 16:51:53 -0400

    chore: updating listing marketplace icons (#6458)
    * chore: updating listing marketplace icons
    
    * removing svgs from public
    
    * responding to comments

--- src/nft/utils/listNfts.ts
+++ src/nft/utils/listNfts.ts
@@ -35,12 +35,10 @@ export const ListingMarkets: ListingMarket[] = [
   {
     name: 'X2Y2',
     fee: 0.5,
-    icon: '/nft/svgs/marketplaces/x2y2.svg',
   },
   {
     name: 'OpenSea',
     fee: 0,
-    icon: '/nft/svgs/marketplaces/opensea.svg',
   },
 ]
 
