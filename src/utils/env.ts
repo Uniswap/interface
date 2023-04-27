09af0f62d47da67d6959077e288482a91a40a508
lavalamp <lavalamp-@users.noreply.github.com>
2023-04-27 16:04:57 -0600

    Restrict sentry reporting to staging environment

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -25,6 +25,7 @@ function isAppUniswapStagingOrg({ hostname }: { hostname: string }): boolean {
 
 export function isSentryEnabled(): boolean {
   // Disable in e2e test environments
+  if (isStagingEnv() && !isAppUniswapStagingOrg(window.location)) return false
   if (isProductionEnv() && !isAppUniswapOrg(window.location)) return false
   return process.env.REACT_APP_SENTRY_ENABLED === 'true'
 }
