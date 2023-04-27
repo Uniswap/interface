fa8c4f51f467154b8fa41034add22436156e055c
cartcrom <cartergcromer@gmail.com>
2023-04-27 11:47:51 -0400

    test: add snapshot tests for different Option states

---
+++ src/components/WalletModal/Option.test.tsx
@@ -0,0 +1,58 @@
+import { Connector } from '@web3-react/types'
+import UNIWALLET_ICON from 'assets/images/uniwallet.png'
+import { Connection, ConnectionType } from 'connection'
+import { useActivationState } from 'connection/activate'
+import { render, renderHook } from 'test-utils/render'
+
+import Option from './Option'
+
+const mockConnection1: Connection = {
+  getName: () => 'Mock Connection',
+  connector: {
+    activate: jest.fn(),
+    deactivate: jest.fn(),
+  } as unknown as Connector,
+  getIcon: () => UNIWALLET_ICON,
+  type: ConnectionType.UNIWALLET,
+} as unknown as Connection
+
+const mockConnection2: Connection = {
+  getName: () => 'Mock Connection',
+  connector: {
+    activate: jest.fn(),
+    deactivate: jest.fn(),
+  } as unknown as Connector,
+  getIcon: () => UNIWALLET_ICON,
+  type: ConnectionType.INJECTED,
+} as unknown as Connection
+
+describe('Wallet Option', () => {
+  it('renders default state', () => {
+    const component = render(<Option connection={mockConnection1} />)
+    const option = component.getByTestId('wallet-modal-option')
+
+    expect(option).toMatchSnapshot()
+  })
+
+  it('renders pending state when selected', () => {
+    const result = renderHook(useActivationState).result
+    result.current.tryActivation(mockConnection1, jest.fn())
+
+    const component = render(<Option connection={mockConnection1} />)
+    const option = component.getByTestId('wallet-modal-option')
+    option.click()
+
+    expect(option).toMatchSnapshot()
+  })
+
+  it('renders disabled state when another connection is pending', () => {
+    const result = renderHook(useActivationState).result
+    result.current.tryActivation(mockConnection2, jest.fn())
+
+    const component = render(<Option connection={mockConnection1} />)
+    const option = component.getByTestId('wallet-modal-option')
+    option.click()
+
+    expect(option).toMatchSnapshot()
+  })
+})
