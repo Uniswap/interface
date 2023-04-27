a8f51236c5c829512898c1e85708c96f6c18e804
Spencer Yen <spencersyen@gmail.com>
2023-04-27 10:44:24 -0700

    chore(mobile): Update autoslippage tolerance range to [0.1%, 5%] (#3473)

--- apps/mobile/src/constants/transactions.ts
+++ apps/mobile/src/constants/transactions.ts
@@ -1,4 +1,5 @@
 export const TRANSACTION_CANCELLATION_GAS_FACTOR = 1.2 // Increase gas price offer by this factor when cancellation txs
+// Slippage tolerances are percentages (ex. 5 = 5% slippage tolerance)
 export const DEFAULT_SLIPPAGE_TOLERANCE = 5
-export const MIN_AUTO_SLIPPAGE_TOLERANCE = 0.5
-export const MAX_AUTO_SLIPPAGE_TOLERANCE = 25
+export const MIN_AUTO_SLIPPAGE_TOLERANCE = 0.1
+export const MAX_AUTO_SLIPPAGE_TOLERANCE = 5
