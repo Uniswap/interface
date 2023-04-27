2ad66155940fc2c0b9c640706d7dc088811a5750
Jack Short <john.short.tj@gmail.com>
2023-04-27 12:05:54 -0400

    chore: moving nft test fixtures to their own file

--- src/nft/components/collection/ActivityCells.test.tsx
+++ src/nft/components/collection/ActivityCells.test.tsx
@@ -1,4 +1,4 @@
-import { TEST_NFT_ACTIVITY_EVENT } from 'test-utils/constants'
+import { TEST_NFT_ACTIVITY_EVENT } from 'test-utils/nftFixtures'
 import { render, screen } from 'test-utils/render'
 
 import { BuyCell } from './ActivityCells'
--- src/nft/components/collection/CollectionAsset.test.tsx
+++ src/nft/components/collection/CollectionAsset.test.tsx
@@ -1,5 +1,5 @@
 import { UniformAspectRatios } from 'nft/types'
-import { TEST_NFT_ASSET } from 'test-utils/constants'
+import { TEST_NFT_ASSET } from 'test-utils/nftFixtures'
 import { render } from 'test-utils/render'
 
 import { CollectionAsset } from './CollectionAsset'
