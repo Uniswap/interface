cbe51331d28ef1add5fc06ca66eae0d7c803908e
Zach Pomerantz <zzmp@uniswap.org>
2023-04-27 16:09:38 -0700

    test: update size-tests to reflect single entry

--- scripts/test-size.js
+++ scripts/test-size.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-undef */
-const assert = require('assert')
 const chalk = require('chalk')
 const fs = require('fs')
 const gzipSize = require('gzip-size').sync
@@ -15,42 +14,23 @@ try {
   process.exit(1)
 }
 
-// The last recorded size for these assets, as reported by `yarn build`.
-const LAST_SIZE_MAIN_KB = 435
-
-// This is the async-loaded js, called <number>.<hash>.js, with a matching css file.
-const LAST_SIZE_ENTRY_KB = 1442
-
+// The last recorded size for main.<hash>.js, as reported by `yarn build`.
+const SIZE_MAIN_KB = 1790
 const SIZE_TOLERANCE_KB = 10
 
-const jsEntrypoints = entrypoints.filter((entrypoint) => entrypoint.endsWith('js'))
-assert(jsEntrypoints.length === 3)
+const [mainJs] = entrypoints.filter((entrypoint) => entrypoint.endsWith('js'))
 
-let fail = false
-console.log('File sizes after gzip:\n')
-jsEntrypoints.forEach((entrypoint) => {
-  const name = entrypoint.match(/\/([\w\d-]*)\./)[1]
-  const size = gzipSize(fs.readFileSync(path.join(buildDir, entrypoint))) / 1024
+console.log('File size after gzip:\n')
+const size = gzipSize(fs.readFileSync(path.join(buildDir, mainJs))) / 1024
 
-  let maxSize = LAST_SIZE_ENTRY_KB + SIZE_TOLERANCE_KB
-  if (name === 'runtime-main') {
-    return
-  } else if (name === 'main') {
-    maxSize = LAST_SIZE_MAIN_KB + SIZE_TOLERANCE_KB
-  }
+let maxSize = SIZE_MAIN_KB + SIZE_TOLERANCE_KB
 
-  const report = `\t${size.toFixed(2).padEnd(8)}kB\t${chalk.dim(
-    `max: ${maxSize.toFixed().padEnd(4)} kB`
-  )}\t${entrypoint}`
-  if (maxSize > size) {
-    console.log(chalk.green(report))
-  } else {
-    console.log(chalk.red(report), '\tdid you import an unnecessary dependency?')
-    fail = true
-  }
-})
-if (fail) {
-  console.log(chalk.yellow('\nOne or more of your files has grown too large.'))
+const report = `\t${size.toFixed(2).padEnd(8)}kB\t${chalk.dim(`max: ${maxSize.toFixed().padEnd(4)} kB`)}\t${mainJs}`
+if (maxSize > size) {
+  console.log(chalk.green(report))
+} else {
+  console.log(chalk.red(report), '\tdid you import an unnecessary dependency?')
+  console.log(chalk.yellow('\nThe main.<hash>.js file has grown too large.'))
   console.log(chalk.yellow('Reduce the file size or update the size limit (in scripts/test-size.js)'))
   process.exit(1)
 }
