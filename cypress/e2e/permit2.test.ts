06268188bcd43418adccf41ea32b88c9bf464d66
cartcrom <cartergcromer@gmail.com>
2023-05-02 21:13:08 -0400

    refactor: reuse common permit util vars to reduce # of lines

--- cypress/e2e/permit2.test.ts
+++ cypress/e2e/permit2.test.ts
@@ -7,25 +7,24 @@ import { DAI, USDC_MAINNET } from '../../src/constants/tokens'
 
 const THIRTY_DAYS_MS = 2592000000
 
+const TEST_BALANCE_INCREMENT = 0.01
+
+let UNIVERSAL_ROUTER: string
+let owner: string
+
 // Shorthand util for approving DAI, permit2 spend, or both
 const approve = async (hardhat: Utils, type: 'token' | 'permit2' | 'both'): Promise<void> => {
   const promises: Array<Promise<void>> = []
 
   if (type === 'token' || type === 'both') {
-    promises.push(hardhat.approval.setTokenAllowanceForPermit2({ owner: hardhat.wallet.address, token: DAI }))
+    promises.push(hardhat.approval.setTokenAllowanceForPermit2({ owner, token: DAI }))
   }
   if (type === 'permit2' || type === 'both') {
+    const expiration = Math.floor((Date.now() + THIRTY_DAYS_MS) / 1000)
     promises.push(
       hardhat.approval.setPermit2Allowance(
-        {
-          owner: hardhat.wallet.address,
-          token: DAI,
-          spender: UNIVERSAL_ROUTER,
-        },
-        {
-          amount: MaxUint160,
-          expiration: Math.floor((Date.now() + THIRTY_DAYS_MS) / 1000),
-        }
+        { owner, token: DAI, spender: UNIVERSAL_ROUTER },
+        { amount: MaxUint160, expiration }
       )
     )
   }
@@ -35,29 +34,20 @@ const approve = async (hardhat: Utils, type: 'token' | 'permit2' | 'both'): Prom
 // Shorthand util for asserting DAI approval on-chain
 const expectTokenMaxApproval = (hardhat: Utils) => {
   // check token approval
-  cy.then(() =>
-    hardhat.approval.getTokenAllowanceForPermit2({
-      owner: hardhat.wallet.address,
-      token: DAI,
-    })
-  ).should('deep.equal', MaxUint256)
+  cy.then(() => hardhat.approval.getTokenAllowanceForPermit2({ owner, token: DAI })).should('deep.equal', MaxUint256)
 }
 
 // Shorthand util for asserting permit2 approval for DAI spend on-chain
 const expectPermit2MaxApproval = (hardhat: Utils, approvalTime: number) => {
-  cy.then(() =>
-    hardhat.approval.getPermit2Allowance({
-      owner: hardhat.wallet.address,
-      token: DAI,
-      spender: UNIVERSAL_ROUTER,
-    })
-  ).then((allowance) => {
-    cy.then(() => MaxUint160.eq(allowance.amount)).should('eq', true)
-    const expected = Math.floor((approvalTime + THIRTY_DAYS_MS) / 1000)
+  cy.then(() => hardhat.approval.getPermit2Allowance({ owner, token: DAI, spender: UNIVERSAL_ROUTER })).then(
+    (allowance) => {
+      cy.then(() => MaxUint160.eq(allowance.amount)).should('eq', true)
+      const expected = Math.floor((approvalTime + THIRTY_DAYS_MS) / 1000)
 
-    // Asserts that the on-chain expiration is in 30 days, with a tolerance of 20 seconds
-    cy.then(() => Math.abs(allowance.expiration - expected)).should('be.lessThan', 20)
-  })
+      // Asserts that the on-chain expiration is in 30 days, with a tolerance of 20 seconds
+      cy.then(() => Math.abs(allowance.expiration - expected)).should('be.lessThan', 20)
+    }
+  )
 }
 
 // Shorthand util setting up a DAI for USDC swap
@@ -77,13 +67,13 @@ const swap = () => {
   cy.contains('Swapped').should('exist')
 }
 
-const TEST_BALANCE_INCREMENT = 0.01
-let UNIVERSAL_ROUTER: string
-
 // TODO: Update tests to differentiate between permit2 vs token approval button text once UI is updated to indicate approval step
 describe('Permit2', () => {
   before(() => {
-    cy.hardhat().then((hardhat) => (UNIVERSAL_ROUTER = UNIVERSAL_ROUTER_ADDRESS(hardhat.network.chainId)))
+    cy.hardhat().then((hardhat) => {
+      UNIVERSAL_ROUTER = UNIVERSAL_ROUTER_ADDRESS(hardhat.network.chainId)
+      owner = hardhat.wallet.address
+    })
   })
 
   beforeEach(() => {
@@ -124,7 +114,7 @@ describe('Permit2', () => {
     })
   })
 
-  it.only('can swap after handling user rejection of approvals and signatures', () => {
+  it('can swap after handling user rejection of approvals and signatures', () => {
     const USER_REJECTION = { code: 4001 }
     cy.hardhat().then((hardhat) => {
       const tokenApprovalstub = cy.stub(hardhat.wallet, 'sendTransaction')
@@ -197,17 +187,10 @@ describe('Permit2', () => {
   })
 
   it('prompts signature when existing permit approval is expired', () => {
+    const expiredAllowance = { amount: MaxUint160, expiration: Math.floor((Date.now() - 1) / 1000) }
     cy.hardhat().then((hardhat) => {
       cy.then(() => approve(hardhat, 'token'))
-        .then(() =>
-          hardhat.approval.setPermit2Allowance(
-            {
-              owner: hardhat.wallet.address,
-              token: DAI,
-            },
-            { amount: MaxUint160, expiration: Math.floor((Date.now() - 1) / 1000) }
-          )
-        )
+        .then(() => hardhat.approval.setPermit2Allowance({ owner, token: DAI }, expiredAllowance))
         .then(() => {
           const approvalTime = Date.now()
           cy.get('[data-testid="swap-approve-button"]').click()
@@ -224,17 +207,10 @@ describe('Permit2', () => {
   })
 
   it('prompts signature when existing permit approval amount is too low', () => {
+    const smallAllowance = { amount: 1, expiration: Math.floor((Date.now() + THIRTY_DAYS_MS) / 1000) }
     cy.hardhat().then((hardhat) => {
       cy.then(() => approve(hardhat, 'token'))
-        .then(() =>
-          hardhat.approval.setPermit2Allowance(
-            {
-              owner: hardhat.wallet.address,
-              token: DAI,
-            },
-            { amount: 1, expiration: Math.floor((Date.now() + THIRTY_DAYS_MS) / 1000) }
-          )
-        )
+        .then(() => hardhat.approval.setPermit2Allowance({ owner, token: DAI }, smallAllowance))
         .then(() => {
           const approvalTime = Date.now()
           cy.get('[data-testid="swap-approve-button"]').click()
@@ -253,15 +229,7 @@ describe('Permit2', () => {
   it('prompts token approval when existing approval amount is too low', () => {
     cy.hardhat().then((hardhat) => {
       cy.then(() => approve(hardhat, 'permit2'))
-        .then(() =>
-          hardhat.approval.setTokenAllowanceForPermit2(
-            {
-              owner: hardhat.wallet.address,
-              token: DAI,
-            },
-            BigNumber.from(1)
-          )
-        )
+        .then(() => hardhat.approval.setTokenAllowanceForPermit2({ owner, token: DAI }, BigNumber.from(1)))
         .then(() => {
           cy.get('[data-testid="swap-approve-button"]').click()
 
