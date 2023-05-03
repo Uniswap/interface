2716b05ed8360d063a3cd906f581223be629a5a9
Eddie Dugan <eddie.dugan@uniswap.org>
2023-05-03 13:50:35 -0400

    fix: add comments to test

--- cypress/e2e/buy-crypto-modal.test.ts
+++ cypress/e2e/buy-crypto-modal.test.ts
@@ -4,33 +4,25 @@ describe('Buy Crypto Modal', () => {
   it('should open and close', () => {
     cy.visit('/')
 
-    cy.get(getTestSelector('buy-fiat-button'))
-      .click()
-      .then(() => {
-        cy.get(getTestSelector('fiat-onramp-modal')).should('be.visible')
-      })
+    // Open the fiat onramp modal
+    cy.get(getTestSelector('buy-fiat-button')).click()
+    cy.get(getTestSelector('fiat-onramp-modal')).should('be.visible')
 
-    cy.get('body')
-      .click(0, 100)
-      .then(() => {
-        cy.get(getTestSelector('fiat-onramp-modal')).should('not.exist')
-      })
+    // Click on a location that should be outside the modal, which should close it
+    cy.get('body').click(0, 100)
+    cy.get(getTestSelector('fiat-onramp-modal')).should('not.exist')
   })
 
   it('should open and close, mobile viewport', () => {
     cy.viewport('iphone-6')
     cy.visit('/')
 
-    cy.get(getTestSelector('buy-fiat-button'))
-      .click()
-      .then(() => {
-        cy.get(getTestSelector('fiat-onramp-modal')).should('be.visible')
-      })
+    // Open the fiat onramp modal
+    cy.get(getTestSelector('buy-fiat-button')).click()
+    cy.get(getTestSelector('fiat-onramp-modal')).should('be.visible')
 
-    cy.get('body')
-      .click(10, 10)
-      .then(() => {
-        cy.get(getTestSelector('fiat-onramp-modal')).should('not.exist')
-      })
+    // Click on a location that should be outside the modal, which should close it
+    cy.get('body').click(10, 10)
+    cy.get(getTestSelector('fiat-onramp-modal')).should('not.exist')
   })
 })
