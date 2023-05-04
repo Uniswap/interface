4a1ee0e7598e133b8f999ecf5009a2505a2a2f3b
cartcrom <cartergcromer@gmail.com>
2023-05-04 15:50:30 -0400

    refactor: remove approval util and update scoping

--- cypress/e2e/permit2.test.ts
+++ cypress/e2e/permit2.test.ts
@@ -4,19 +4,6 @@ import { DAI, USDC_MAINNET } from '../../src/constants/tokens'
 
 const APPROVE_BUTTON = '[data-testid="swap-approve-button"]'
 
-// The same tokens & swap-amount combination is used for all permit2 tests.
-const INPUT_TOKEN = DAI
-const OUTPUT_TOKEN = USDC_MAINNET
-const TEST_BALANCE_INCREMENT = 0.01
-
-const setupSwap = () => {
-  // Uses input/output in search params for faster test setup.
-  cy.visit(`/swap/?inputCurrency=${INPUT_TOKEN.address}&outputCurrency=${OUTPUT_TOKEN.address}`, {
-    ethereum: 'hardhat',
-  })
-  cy.get('#swap-currency-input .token-amount-input').clear().type(TEST_BALANCE_INCREMENT.toString())
-}
-
 /** Initiates a swap and confirms its success. */
 const swaps = () => {
   // Ensures that the swap button is enabled, since it can be temporarily disabled following approval.
@@ -33,33 +20,25 @@ const swaps = () => {
 
 // TODO: Update tests to differentiate between permit2 vs token approval button text once UI is updated to indicate approval step.
 describe('Permit2', () => {
-  let owner: string
-  before(() => cy.hardhat().then((hardhat) => (owner = hardhat.wallet.address)))
+  // The same tokens & swap-amount combination is used for all permit2 tests.
+  const INPUT_TOKEN = DAI
+  const OUTPUT_TOKEN = USDC_MAINNET
+  const TEST_BALANCE_INCREMENT = 0.01
 
-  beforeEach(setupSwap)
-
-  /** Sets a max approval for input token, permit2 spend, or both. */
-  const setApproval = (type: 'token' | 'permit2' | 'both') => {
-    return cy.hardhat().then(async (hardhat) => {
-      const promises: Array<Promise<void>> = []
-      if (type === 'token' || type === 'both') {
-        promises.push(hardhat.approval.setTokenAllowanceForPermit2({ owner, token: INPUT_TOKEN }))
-      }
-      if (type === 'permit2' || type === 'both') {
-        promises.push(hardhat.approval.setPermit2Allowance({ owner, token: INPUT_TOKEN }))
-      }
-
-      await Promise.all(promises)
-      return hardhat
+  beforeEach(() => {
+    // Uses input/output in search params for faster test setup.
+    cy.visit(`/swap/?inputCurrency=${INPUT_TOKEN.address}&outputCurrency=${OUTPUT_TOKEN.address}`, {
+      ethereum: 'hardhat',
     })
-  }
+    cy.get('#swap-currency-input .token-amount-input').clear().type(TEST_BALANCE_INCREMENT.toString())
+  })
 
   /** Asserts permit2 has a max approval for spend of the input token on-chain. */
   const expectTokenAllowanceForPermit2ToBeMax = () => {
     // check token approval
     return cy
       .hardhat()
-      .then((hardhat) => hardhat.approval.getTokenAllowanceForPermit2({ owner, token: INPUT_TOKEN }))
+      .then(({ approval, wallet }) => approval.getTokenAllowanceForPermit2({ owner: wallet, token: INPUT_TOKEN }))
       .should('deep.equal', MaxUint256)
   }
 
@@ -67,7 +46,7 @@ describe('Permit2', () => {
   const expectPermit2AllowanceForUniversalRouterToBeMax = (approvalTime: number) => {
     return cy
       .hardhat()
-      .then((hardhat) => hardhat.approval.getPermit2Allowance({ owner, token: INPUT_TOKEN }))
+      .then((hardhat) => hardhat.approval.getPermit2Allowance({ owner: hardhat.wallet, token: INPUT_TOKEN }))
       .then((allowance) => {
         cy.then(() => MaxUint160.eq(allowance.amount)).should('eq', true)
         const expected = Math.floor((approvalTime + 2_592_000_000) / 1000)
@@ -78,7 +57,12 @@ describe('Permit2', () => {
   }
 
   it('does not prompt approval when user has already approved token and permit2', () => {
-    setApproval('both').then(swaps)
+    cy.hardhat()
+      .then(({ approval, wallet }) => {
+        approval.setTokenAllowanceForPermit2({ owner: wallet, token: INPUT_TOKEN })
+        approval.setPermit2Allowance({ owner: wallet, token: INPUT_TOKEN })
+      })
+      .then(swaps)
   })
 
   it('can swap after completing full permit2 approval process', () => {
@@ -138,35 +122,42 @@ describe('Permit2', () => {
   })
 
   it('can swap with existing token approval and missing permit approval', () => {
-    setApproval('token').then(() => {
-      cy.get(APPROVE_BUTTON).click()
-      const approvalTime = Date.now()
+    cy.hardhat()
+      .then(({ approval, wallet }) => approval.setTokenAllowanceForPermit2({ owner: wallet, token: INPUT_TOKEN }))
+      .then(() => {
+        cy.get(APPROVE_BUTTON).click()
+        const approvalTime = Date.now()
 
-      swaps()
+        swaps()
 
-      expectPermit2AllowanceForUniversalRouterToBeMax(approvalTime)
-    })
+        expectPermit2AllowanceForUniversalRouterToBeMax(approvalTime)
+      })
   })
 
   it('can swap with existing permit approval and missing token approval', () => {
-    setApproval('permit2').then(() => {
-      cy.get(APPROVE_BUTTON).click()
-      cy.get(APPROVE_BUTTON).should('have.text', 'Approval pending')
+    cy.hardhat()
+      .then(({ approval, wallet }) => approval.setPermit2Allowance({ owner: wallet, token: INPUT_TOKEN }))
+      .then(() => {
+        cy.get(APPROVE_BUTTON).click()
+        cy.get(APPROVE_BUTTON).should('have.text', 'Approval pending')
 
-      // There should be a successful Approved notification.
-      cy.contains('Approved').should('exist')
+        // There should be a successful Approved notification.
+        cy.contains('Approved').should('exist')
 
-      swaps()
+        swaps()
 
-      expectTokenAllowanceForPermit2ToBeMax()
-    })
+        expectTokenAllowanceForPermit2ToBeMax()
+      })
   })
 
   it('prompts signature when existing permit approval is expired', () => {
     const expiredAllowance = { expiration: Math.floor((Date.now() - 1) / 1000) }
 
-    setApproval('token')
-      .then((hardhat) => hardhat.approval.setPermit2Allowance({ owner, token: INPUT_TOKEN }, expiredAllowance))
+    cy.hardhat()
+      .then(({ approval, wallet }) => {
+        approval.setTokenAllowanceForPermit2({ owner: wallet, token: INPUT_TOKEN })
+        approval.setPermit2Allowance({ owner: wallet, token: INPUT_TOKEN }, expiredAllowance)
+      })
       .then(() => {
         cy.get(APPROVE_BUTTON).click()
         const approvalTime = Date.now()
@@ -180,8 +171,11 @@ describe('Permit2', () => {
   it('prompts signature when existing permit approval amount is too low', () => {
     const smallAllowance = { amount: 1 }
 
-    setApproval('token')
-      .then((hardhat) => hardhat.approval.setPermit2Allowance({ owner, token: INPUT_TOKEN }, smallAllowance))
+    cy.hardhat()
+      .then(({ approval, wallet }) => {
+        approval.setTokenAllowanceForPermit2({ owner: wallet, token: INPUT_TOKEN })
+        approval.setPermit2Allowance({ owner: wallet, token: INPUT_TOKEN }, smallAllowance)
+      })
       .then(() => {
         cy.get(APPROVE_BUTTON).click()
         const approvalTime = Date.now()
@@ -193,8 +187,11 @@ describe('Permit2', () => {
   })
 
   it('prompts token approval when existing approval amount is too low', () => {
-    setApproval('permit2')
-      .then((hardhat) => hardhat.approval.setTokenAllowanceForPermit2({ owner, token: INPUT_TOKEN }, 1))
+    cy.hardhat()
+      .then(({ approval, wallet }) => {
+        approval.setPermit2Allowance({ owner: wallet, token: INPUT_TOKEN })
+        approval.setTokenAllowanceForPermit2({ owner: wallet, token: INPUT_TOKEN }, 1)
+      })
       .then(() => {
         cy.get(APPROVE_BUTTON).click()
 
