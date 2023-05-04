19655da70e1feaf38908e2b4f3a60dd71eca737e
Vignesh Mohankumar <me@vig.xyz>
2023-05-04 18:07:52 -0400

    casting

--- src/utils/matchMedia.test.ts
+++ src/utils/matchMedia.test.ts
@@ -3,15 +3,12 @@ import { addListener, removeListener } from './matchMedia'
 describe('addListener', () => {
   test('adds event listener to media query', () => {
     const mediaQuery = {
-      matches: true,
       addEventListener: jest.fn(),
       addListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      removeListener: jest.fn(),
     }
 
     const listener = jest.fn()
-    addListener(mediaQuery as MediaQueryList, listener)
+    addListener(mediaQuery as unknown as MediaQueryList, listener)
 
     expect(mediaQuery.addEventListener).toHaveBeenCalledWith('change', listener)
     expect(mediaQuery.addListener).not.toHaveBeenCalled()
@@ -19,17 +16,14 @@ describe('addListener', () => {
 
   test('falls back to addListener for older browsers', () => {
     const mediaQuery = {
-      matches: true,
       addEventListener: jest.fn(() => {
         throw new Error('not supported')
       }),
       addListener: jest.fn(),
-      removeEventListener: jest.fn(),
-      removeListener: jest.fn(),
     }
 
     const listener = jest.fn()
-    addListener(mediaQuery as MediaQueryList, listener)
+    addListener(mediaQuery as unknown as MediaQueryList, listener)
 
     expect(mediaQuery.addEventListener).toHaveBeenCalledWith('change', listener)
     expect(mediaQuery.addListener).toHaveBeenCalledWith(listener)
@@ -39,15 +33,12 @@ describe('addListener', () => {
 describe('removeListener', () => {
   test('removes event listener from media query', () => {
     const mediaQuery = {
-      matches: true,
-      addEventListener: jest.fn(),
-      addListener: jest.fn(),
       removeEventListener: jest.fn(),
       removeListener: jest.fn(),
     }
 
     const listener = jest.fn()
-    removeListener(mediaQuery as MediaQueryList, listener)
+    removeListener(mediaQuery as unknown as MediaQueryList, listener)
 
     expect(mediaQuery.removeEventListener).toHaveBeenCalledWith('change', listener)
     expect(mediaQuery.removeListener).not.toHaveBeenCalled()
@@ -55,9 +46,6 @@ describe('removeListener', () => {
 
   test('falls back to removeListener for older browsers', () => {
     const mediaQuery = {
-      matches: true,
-      addEventListener: jest.fn(),
-      addListener: jest.fn(),
       removeEventListener: jest.fn(() => {
         throw new Error('not supported')
       }),
@@ -65,7 +53,7 @@ describe('removeListener', () => {
     }
 
     const listener = jest.fn()
-    removeListener(mediaQuery as MediaQueryList, listener)
+    removeListener(mediaQuery as unknown as MediaQueryList, listener)
 
     expect(mediaQuery.removeEventListener).toHaveBeenCalledWith('change', listener)
     expect(mediaQuery.removeListener).toHaveBeenCalledWith(listener)
