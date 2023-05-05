dd76c29524a7fc9960ab6713673bb1c4e41645a4
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-05-05 13:59:57 -0500

    add e2e test

--- src/components/AccountDrawer/MiniPortfolio/Activity/useClearConflictingTransactions.ts
+++ src/components/AccountDrawer/MiniPortfolio/Activity/useClearConflictingTransactions.ts
@@ -3,17 +3,19 @@ import { useEffect } from 'react'
 import { useTransactionRemover } from 'state/transactions/hooks'
 
 import { ActivityMap } from './types'
+import { useWeb3React } from '@web3-react/core'
 
 // this hook removes local transactions that have nonces that are duplicates of remote transactions
 export function useClearConflictingTransactions(data: TransactionListQuery | undefined, localMap: ActivityMap) {
   const removeLocalTransaction = useTransactionRemover()
-
+  const { account } = useWeb3React()
   useEffect(() => {
     const remoteActivity = data?.portfolios?.[0]?.assetActivities
-    if (!remoteActivity) return
+    if (!remoteActivity || !account) return
     const duplicateNonceActions = []
     const localActivityList = Object.values(localMap)
-    const remoteNonces = new Set(remoteActivity.map((remoteAction) => remoteAction.transaction.nonce))
+    const ownedRemoteNoncesList = remoteActivity.filter(remoteActivity => remoteActivity.transaction.from === account).map((remoteActivity) => remoteActivity.transaction.nonce)
+    const remoteNonces = new Set(ownedRemoteNoncesList)
     for (const localAction of localActivityList) {
       // nonce is ? guarded because users may have transactions in localstorage without the data
       if (localAction?.nonce && remoteNonces.has(localAction.nonce)) {
