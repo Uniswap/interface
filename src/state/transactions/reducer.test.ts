9fc59e9ec31169371da5fd23fd08222d9a07e951
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-05-08 14:04:39 -0500

    use supported chain id for reducer tests

--- src/state/transactions/reducer.test.ts
+++ src/state/transactions/reducer.test.ts
@@ -29,7 +29,7 @@ describe('transaction reducer', () => {
         },
       })
       store.dispatch(updateVersion())
-      expect(store.getState()[1]['abc']).toBeUndefined()
+      expect(store.getState()[SupportedChainId.MAINNET]['abc']).toBeUndefined()
     })
     it('keeps old format transactions that do have info', () => {
       store = createStore(reducer, {
@@ -41,7 +41,7 @@ describe('transaction reducer', () => {
         },
       })
       store.dispatch(updateVersion())
-      expect(store.getState()[1]['abc']).toBeTruthy()
+      expect(store.getState()[SupportedChainId.MAINNET]['abc']).toBeTruthy()
     })
   })
 
@@ -102,7 +102,7 @@ describe('transaction reducer', () => {
         addTransaction({
           hash: '0x0',
           chainId: SupportedChainId.MAINNET,
-          nonce: 1,
+          nonce: 2,
           info: { type: TransactionType.APPROVAL, spender: '0x0', tokenAddress: '0x0' },
           from: '0x0',
         })
@@ -124,7 +124,7 @@ describe('transaction reducer', () => {
           },
         })
       )
-      const tx = store.getState()[4]?.['0x0']
+      const tx = store.getState()[SupportedChainId.MAINNET]?.['0x0']
       expect(tx?.confirmedTime).toBeGreaterThanOrEqual(beforeTime)
       expect(tx?.receipt).toEqual({
         status: 1,
@@ -155,7 +155,7 @@ describe('transaction reducer', () => {
         addTransaction({
           hash: '0x0',
           chainId: SupportedChainId.MAINNET,
-          nonce: 1,
+          nonce: 3,
           info: { type: TransactionType.APPROVAL, spender: '0x0', tokenAddress: '0x0' },
           from: '0x0',
         })
@@ -167,7 +167,7 @@ describe('transaction reducer', () => {
           blockNumber: 1,
         })
       )
-      const tx = store.getState()[4]?.['0x0']
+      const tx = store.getState()[SupportedChainId.MAINNET]?.['0x0']
       expect(tx?.lastCheckedBlockNumber).toEqual(1)
     })
     it('never decreases', () => {
@@ -175,7 +175,7 @@ describe('transaction reducer', () => {
         addTransaction({
           hash: '0x0',
           chainId: SupportedChainId.MAINNET,
-          nonce: 900,
+          nonce: 4,
           info: { type: TransactionType.APPROVAL, spender: '0x0', tokenAddress: '0x0' },
           from: '0x0',
         })
@@ -194,7 +194,7 @@ describe('transaction reducer', () => {
           blockNumber: 1,
         })
       )
-      const tx = store.getState()[4]?.['0x0']
+      const tx = store.getState()[SupportedChainId.MAINNET]?.['0x0']
       expect(tx?.lastCheckedBlockNumber).toEqual(3)
     })
   })
@@ -203,31 +203,37 @@ describe('transaction reducer', () => {
     it('removes all transactions for the chain', () => {
       store.dispatch(
         addTransaction({
-          chainId: 1,
+          chainId: SupportedChainId.MAINNET,
           hash: '0x0',
-          nonce: 1,
+          nonce: 5,
           info: { type: TransactionType.APPROVAL, spender: 'abc', tokenAddress: 'def' },
           from: 'abc',
         })
       )
       store.dispatch(
         addTransaction({
-          chainId: SupportedChainId.MAINNET,
-          nonce: 2,
+          chainId: SupportedChainId.OPTIMISM,
+          nonce: 6,
           hash: '0x1',
           info: { type: TransactionType.APPROVAL, spender: 'abc', tokenAddress: 'def' },
           from: 'abc',
         })
       )
       expect(Object.keys(store.getState())).toHaveLength(2)
-      expect(Object.keys(store.getState())).toEqual([String(1), String(4)])
-      expect(Object.keys(store.getState()[1] ?? {})).toEqual(['0x0'])
-      expect(Object.keys(store.getState()[4] ?? {})).toEqual(['0x1'])
-      store.dispatch(clearAllTransactions({ chainId: 1 }))
+      expect(Object.keys(store.getState())).toEqual([
+        String(SupportedChainId.MAINNET),
+        String(SupportedChainId.OPTIMISM),
+      ])
+      expect(Object.keys(store.getState()[SupportedChainId.MAINNET] ?? {})).toEqual(['0x0'])
+      expect(Object.keys(store.getState()[SupportedChainId.OPTIMISM] ?? {})).toEqual(['0x1'])
+      store.dispatch(clearAllTransactions({ chainId: SupportedChainId.MAINNET }))
       expect(Object.keys(store.getState())).toHaveLength(2)
-      expect(Object.keys(store.getState())).toEqual([String(1), String(4)])
-      expect(Object.keys(store.getState()[1] ?? {})).toEqual([])
-      expect(Object.keys(store.getState()[4] ?? {})).toEqual(['0x1'])
+      expect(Object.keys(store.getState())).toEqual([
+        String(SupportedChainId.MAINNET),
+        String(SupportedChainId.OPTIMISM),
+      ])
+      expect(Object.keys(store.getState()[SupportedChainId.MAINNET] ?? {})).toEqual([])
+      expect(Object.keys(store.getState()[SupportedChainId.OPTIMISM] ?? {})).toEqual(['0x1'])
     })
   })
 })
