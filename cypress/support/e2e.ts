f0d8f8b23b61b3560218d633e25275f770427bf3
Zach Pomerantz <zzmp@uniswap.org>
2023-05-08 15:04:02 -0700

    test(e2e): mock uniswap token lists from ipfs (#6511)
    * test(e2e): mock uniswap token lists from ipfs
    
    * test(e2e): correctly intercept

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -9,6 +9,7 @@ import '@cypress/code-coverage/support'
 import 'cypress-hardhat/lib/browser'
 
 import { Eip1193Bridge } from '@ethersproject/experimental/lib/eip1193-bridge'
+import TokenListJSON from '@uniswap/default-token-list'
 import assert from 'assert'
 
 import { FeatureFlag } from '../../src/featureFlags/flags/featureFlags'
@@ -115,6 +116,10 @@ beforeEach(() => {
         })
       )
     })
+    // Mock our own token list responses to avoid the latency of IPFS.
+    .intercept('https://gateway.ipfs.io/ipns/tokens.uniswap.org', TokenListJSON)
+    .intercept('https://gateway.ipfs.io/ipns/extendedtokens.uniswap.org', { statusCode: 201, body: { tokens: [] } })
+    .intercept('https://gateway.ipfs.io/ipns/unsupportedtokens.uniswap.org', { statusCode: 201, body: { tokens: [] } })
     // Reset hardhat between tests to ensure isolation.
     // This resets the fork, as well as options like automine.
     .hardhat()
