c984a07b9036956210b5dc1809c57fd273ef0316
Vignesh Mohankumar <me@vig.xyz>
2023-05-08 20:34:58 -0400

    refactor: return onDemandEntries instead of mediaURLs

--- src/serviceWorker/utils.ts
+++ src/serviceWorker/utils.ts
@@ -7,27 +7,25 @@ declare const self: ServiceWorkerGlobalScope
 export function isDevelopment() {
   return Boolean(
     self.location.hostname === 'localhost' ||
-      // [::1] is the IPv6 localhost address
-      self.location.hostname === '[::1]' ||
-      // 127.0.0.0/8 are considered localhost for IPv4
-      self.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/) ||
-      // vercel previews
-      self.location.hostname.endsWith('.vercel.app')
+    // [::1] is the IPv6 localhost address
+    self.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4
+    self.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/) ||
+    // vercel previews
+    self.location.hostname.endsWith('.vercel.app')
   )
 }
 
-type GroupedEntries = { mediaURLs: string[]; precacheEntries: PrecacheEntry[] }
+type GroupedEntries = { onDemandEntries: (string | PrecacheEntry)[]; precacheEntries: PrecacheEntry[] }
 export function groupEntries(entries: (string | PrecacheEntry)[]): GroupedEntries {
   return entries.reduce<GroupedEntries>(
-    ({ mediaURLs, precacheEntries }, entry) => {
-      if (typeof entry === 'string') {
-        return { precacheEntries, mediaURLs: [...mediaURLs, entry] }
-      } else if (entry.url.includes('/media/')) {
-        return { precacheEntries, mediaURLs: [...mediaURLs, entry.url] }
+    ({ onDemandEntries, precacheEntries }, entry) => {
+      if (typeof entry === 'string' || entry.url.includes('/media/')) {
+        return { precacheEntries, onDemandEntries: [...onDemandEntries, entry] }
       } else {
-        return { precacheEntries: [...precacheEntries, entry], mediaURLs }
+        return { precacheEntries: [...precacheEntries, entry], onDemandEntries }
       }
     },
-    { mediaURLs: [], precacheEntries: [] }
+    { onDemandEntries: [], precacheEntries: [] }
   )
 }
