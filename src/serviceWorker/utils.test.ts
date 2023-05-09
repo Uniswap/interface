ca82deb6feb4ac7e2edb81c2dbddb72ea63a882e
Vignesh Mohankumar <me@vig.xyz>
2023-05-08 20:43:37 -0400

    fix test

--- src/serviceWorker/utils.test.ts
+++ src/serviceWorker/utils.test.ts
@@ -12,7 +12,7 @@ describe('groupEntries', () => {
     const result = groupEntries(resources)
 
     expect(result).toEqual({
-      onDemandEntries: ['./static/whitepaper.pdf', { url: './static/js/main.js', revision: 'abc123' }],
+      onDemandEntries: ['./static/whitepaper.pdf', { url: './static/media/image.jpg', revision: 'ghi789' }],
       precacheEntries: [
         { url: './static/js/main.js', revision: 'abc123' },
         { url: './static/css/styles.css', revision: 'def456' },
