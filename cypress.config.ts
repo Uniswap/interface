5bf33ab0043c0a0b0ac6e18825a0b4002a616992
Vignesh Mohankumar <me@vig.xyz>
2023-05-10 10:53:53 -0400

    revert(e2e): waitForAnimations (#6525)
    Co-authored-by: Zach Pomerantz <zzmp@uniswap.org>

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,6 @@ import { setupHardhatEvents } from 'cypress-hardhat'
 export default defineConfig({
   projectId: 'yp82ef',
   videoUploadOnPasses: false,
-  waitForAnimations: false,
   defaultCommandTimeout: 24000, // 2x average block time
   chromeWebSecurity: false,
   retries: { runMode: 2 },
