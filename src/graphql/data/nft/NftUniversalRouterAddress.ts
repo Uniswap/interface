01aa3291b39640c8c1712c26c7fa784a58bfe78e
Jack Short <john.short.tj@gmail.com>
2023-05-10 12:45:58 -0400

    chore: bubbling up pwat spender from route query (#6481)
    * chore: bubbling up pwat spender from route query
    
    * refactoring
    
    * adding loading state
    
    * moving to loading allowance
    
    * responding to comments
    
    * linting

---
+++ src/graphql/data/nft/NftUniversalRouterAddress.ts
@@ -0,0 +1,23 @@
+import gql from 'graphql-tag'
+
+import { useNftUniversalRouterAddressQuery } from '../__generated__/types-and-hooks'
+
+gql`
+  query NftUniversalRouterAddress($chain: Chain = ETHEREUM) {
+    nftRoute(chain: $chain, senderAddress: "", nftTrades: []) {
+      toAddress
+    }
+  }
+`
+
+export function useNftUniversalRouterAddress() {
+  const { data, loading } = useNftUniversalRouterAddressQuery({
+    // no cache because a different version of nftRoute query is going to be called around the same time
+    fetchPolicy: 'no-cache',
+  })
+
+  return {
+    universalRouterAddress: data?.nftRoute?.toAddress,
+    universalRouterAddressIsLoading: loading,
+  }
+}
