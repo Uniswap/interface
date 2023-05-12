c71a15d968697288983e7d6af8f1aa081ee0b7a2
Charles Bachmeier <charlie@genie.xyz>
2023-05-12 10:19:24 -0400

    add buy and offer buttons, thumbnail, text

--- src/nft/components/details/detailsV2/DataPageHeader.tsx
+++ src/nft/components/details/detailsV2/DataPageHeader.tsx
@@ -1,20 +1,64 @@
+import { Trans } from '@lingui/macro'
+import { ButtonPrimary } from 'components/Button'
+import Column from 'components/Column'
+import Row from 'components/Row'
+import { HandHoldingDollarIcon, VerifiedIcon } from 'nft/components/icons'
+import { GenieAsset } from 'nft/types'
+import { formatEth } from 'nft/utils'
 import styled from 'styled-components/macro'
-import { BREAKPOINTS } from 'theme'
+import { ThemedText } from 'theme'
 
-import { containerStyles } from './shared'
-
-const HeaderContainer = styled.div`
+const AssetImage = styled.img`
+  width: 96px;
   height: 96px;
-  flex-shrink: 0;
-  padding-left: 0px;
-
-  ${containerStyles}
-
-  @media screen and (max-width: ${BREAKPOINTS.sm}px) {
-    display: none;
-  }
+  border-radius: 20px;
 `
 
-export const DataPageHeader = () => {
-  return <HeaderContainer>Header</HeaderContainer>
+const AssetText = styled(Column)`
+  gap: 4px;
+  flex-grow: 1;
+`
+
+const BuyButton = styled(ButtonPrimary)`
+  display: flex;
+  flex-direction: row;
+  padding: 16px 24px;
+  gap: 8px;
+  line-height: 24px;
+`
+
+const Price = styled.div`
+  color: ${({ theme }) => theme.textSecondary};
+`
+
+const MakeOfferButton = styled(ButtonPrimary)`
+  border-radius: 12px;
+  width: min-content;
+  flex-shrink: 0;
+`
+
+export const DataPageHeader = ({ asset }: { asset: GenieAsset }) => {
+  return (
+    <Row gap="24px">
+      <AssetImage src={asset.imageUrl} />
+      <AssetText>
+        <Row gap="4px">
+          <ThemedText.SubHeaderSmall>{asset.collectionName}</ThemedText.SubHeaderSmall>
+          <VerifiedIcon width="16px" height="16px" />
+        </Row>
+        <ThemedText.HeadlineMedium>
+          {asset.name ?? `${asset.collectionName} #${asset.tokenId}`}
+        </ThemedText.HeadlineMedium>
+      </AssetText>
+      <Row justifySelf="flex-end" width="min-content" gap="12px">
+        <BuyButton>
+          <Trans>Buy</Trans>
+          <Price>{formatEth(2.22)}</Price>
+        </BuyButton>
+        <MakeOfferButton>
+          <HandHoldingDollarIcon />
+        </MakeOfferButton>
+      </Row>
+    </Row>
+  )
 }
