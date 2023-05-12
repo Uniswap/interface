e942c77d5165cbbb8016fd4684558634ff2d761f
Charles Bachmeier <charlie@genie.xyz>
2023-05-12 09:14:03 -0400

    hide header on mobile

--- src/nft/components/details/detailsV2/DataPageHeader.tsx
+++ src/nft/components/details/detailsV2/DataPageHeader.tsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components/macro'
+import { BREAKPOINTS } from 'theme'
 
 import { containerStyles } from './shared'
 
 const HeaderContainer = styled.div`
   height: 96px;
   flex-shrink: 0;
+  padding-left: 0px;
 
   ${containerStyles}
 
-  padding-left: 0px;
+  @media screen and (max-width: ${BREAKPOINTS.sm}px) {
+    display: none;
+  }
 `
 
 export const DataPageHeader = () => {
