bf039afe21fda07e43af4e16606e732e8c3c9ab0
Vignesh Mohankumar <me@vig.xyz>
2023-05-15 12:42:41 -0400

    refactor: imports shared polyfills in setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom' // jest custom assertions
 import 'jest-styled-components' // adds style diffs to snapshot tests
+import 'polyfills'
 
-import { ResizeObserver } from '@juggle/resize-observer'
 import type { createPopper } from '@popperjs/core'
 import { useWeb3React } from '@web3-react/core'
 import failOnConsole from 'jest-fail-on-console'
@@ -31,8 +31,6 @@ global.matchMedia =
       removeEventListener: jest.fn(),
     }
   })
-
-global.ResizeObserver = ResizeObserver
 
 jest.mock('@popperjs/core', () => {
   const core = jest.requireActual('@popperjs/core')
