31e66bfb4d60fba7d4f885723e9eca169d5c6431
Vignesh Mohankumar <me@vig.xyz>
2023-05-15 19:01:20 -0400

    try again

--- rules/enforce-retry-on-import.js
+++ rules/enforce-retry-on-import.js
@@ -11,13 +11,18 @@ module.exports = {
   create(context) {
     return {
       ImportExpression(node) {
-        const parent = node.parent
-        if (parent.type !== 'CallExpression' || parent.callee.name !== 'retry') {
-          context.report({
-            node,
-            message: 'Dynamic import should be wrapped in a retry() call',
-          })
+        let currentNode = node
+        while (currentNode) {
+          if (currentNode.type === 'CallExpression' && currentNode.callee.name === 'retry') {
+            return // Found a retry() call, so no problem
+          }
+          currentNode = currentNode.parent
         }
+        // If we got here, no retry() call was found
+        context.report({
+          node,
+          message: 'Dynamic import should be wrapped in a retry() call',
+        })
       },
     }
   },
