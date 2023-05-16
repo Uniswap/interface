7ea415fca28992f428ac658d11cc3f17364c919b
Charles Bachmeier <charlie@genie.xyz>
2023-05-16 15:11:31 -0700

    add new snapshot test

--- src/nft/components/details/detailsV2/DataPage.test.tsx
+++ src/nft/components/details/detailsV2/DataPage.test.tsx
@@ -3,7 +3,12 @@ import { render } from 'test-utils/render'
 
 import { DataPage } from './DataPage'
 
-it('placeholder containers load', () => {
+it('data page loads with header showing', () => {
   const { asFragment } = render(<DataPage asset={TEST_NFT_ASSET} showDataHeader={true} />)
+  expect(asFragment()).toMatchSnapshot()
+})
+
+it('data page loads without header showing', () => {
+  const { asFragment } = render(<DataPage asset={TEST_NFT_ASSET} showDataHeader={false} />)
   expect(asFragment()).toMatchSnapshot()
 })
