bb7a7994b71d53b086d4785fdd804e4b7eca5644
Vignesh Mohankumar <me@vig.xyz>
2023-05-16 14:23:30 -0400

    test fixes

--- src/utils/retry.test.ts
+++ src/utils/retry.test.ts
@@ -1,16 +1,9 @@
 import { retry } from './retry'
 
 describe('retry function', () => {
-  jest.useRealTimers()
-  let mockFn: jest.Mock
-
-  beforeEach(() => {
-    mockFn = jest.fn()
-  })
-
   it('should resolve when function is successful', async () => {
     const expectedResult = 'Success'
-    mockFn.mockResolvedValue(expectedResult)
+    const mockFn = jest.fn().mockResolvedValue(expectedResult)
     const result = await retry(mockFn)
     expect(result).toEqual(expectedResult)
     expect(mockFn).toHaveBeenCalledTimes(1)
@@ -18,14 +11,14 @@ describe('retry function', () => {
 
   it('should retry the specified number of times before rejecting', async () => {
     const error = new Error('Failure')
-    mockFn.mockRejectedValue(error)
+    const mockFn = jest.fn().mockResolvedValue(error)
     await expect(retry(mockFn, 3, 1)).rejects.toEqual(error)
     expect(mockFn).toHaveBeenCalledTimes(3)
   })
 
   it('should resolve when function is successful on the second attempt', async () => {
     const expectedResult = 'Success'
-    mockFn.mockRejectedValueOnce(new Error('Failure')).mockResolvedValue(expectedResult)
+    const mockFn = jest.fn().mockRejectedValueOnce(new Error('Failure')).mockResolvedValue(expectedResult)
     const result = await retry(mockFn, 3, 1)
     expect(result).toEqual(expectedResult)
     expect(mockFn).toHaveBeenCalledTimes(2)
