14ac6f3a75416db67f9debb013d52618f311bd51
Mike <grabbou@gmail.com>
2023-05-16 22:59:07 +0400

    chore: update tes tname

--- src/utils/formatPriceImpact.test.ts
+++ src/utils/formatPriceImpact.test.ts
@@ -2,10 +2,12 @@ import { Percent } from "@uniswap/sdk-core"
 import formatPriceImpact from "./formatPriceImpact"
 
 describe('formatPriceImpact', () => {
-  it('formats positive price impact', () => {
+  it('formats price impact', () => {
     expect(formatPriceImpact(new Percent(5, 10_000))).toEqual('-0.05%')
   })
-  it('formats negative price impact', () => {
+  // While there's theoretically no such thing as "positive price impact", this can show up 
+  // due to a bug in routing-api, so it's still tested for
+  it('formats price impact when given a negative value', () => {
     expect(formatPriceImpact(new Percent(-5, 10_000))).toEqual('0.05%')
   })
 })
