e769760edc273da581b3664852edd4d8d0f67147
Vignesh Mohankumar <me@vig.xyz>
2023-05-16 10:25:13 -0400

    real timers but really short intervals

--- src/utils/retry.test.ts
+++ src/utils/retry.test.ts
@@ -1,48 +1,36 @@
 import { retry } from './retry'
 
 describe('retry function', () => {
+  jest.useRealTimers()
   let mockFn: jest.Mock
 
   beforeEach(() => {
-    jest.useFakeTimers()
     mockFn = jest.fn()
-  })
-
-  afterEach(() => {
-    jest.useRealTimers()
   })
 
   it('should resolve when function is successful', async () => {
     const expectedResult = 'Success'
-    mockFn.mockImplementation(() => Promise.resolve(expectedResult))
+    mockFn.mockResolvedValue(expectedResult)
     const retryFn = retry(mockFn)
-    const result = retryFn()
-    jest.runOnlyPendingTimers()
-    await expect(result).resolves.toEqual(expectedResult)
+    const result = await retryFn()
+    expect(result).toEqual(expectedResult)
     expect(mockFn).toHaveBeenCalledTimes(1)
   })
 
   it('should retry the specified number of times before rejecting', async () => {
     const error = new Error('Failure')
-    mockFn.mockImplementation(() => Promise.reject(error))
-    const retryFn = retry(mockFn, 2)
-    const result = retryFn()
-    jest.runOnlyPendingTimers()
-    await expect(result).rejects.toEqual(error)
+    mockFn.mockRejectedValue(error)
+    const retryFn = retry(mockFn, 3, 1)
+    await expect(retryFn()).rejects.toEqual(error)
     expect(mockFn).toHaveBeenCalledTimes(3)
   })
 
-  it('should respect the delay between retries', () => {
-    mockFn.mockImplementation(() => Promise.reject(new Error('Failure')))
-    const retryFn = retry(mockFn, 2, 2000)
-    retryFn()
-    jest.advanceTimersByTime(1999)
-    expect(mockFn).toHaveBeenCalledTimes(1)
-    jest.advanceTimersByTime(1)
+  it('should resolve when function is successful on the second attempt', async () => {
+    const expectedResult = 'Success'
+    mockFn.mockRejectedValueOnce(new Error('Failure')).mockResolvedValue(expectedResult)
+    const retryFn = retry(mockFn, 3, 1)
+    const result = await retryFn()
+    expect(result).toEqual(expectedResult)
     expect(mockFn).toHaveBeenCalledTimes(2)
-    jest.advanceTimersByTime(3999)
-    expect(mockFn).toHaveBeenCalledTimes(2)
-    jest.advanceTimersByTime(1)
-    expect(mockFn).toHaveBeenCalledTimes(3)
   })
 })
