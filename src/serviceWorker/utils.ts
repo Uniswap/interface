146c5f29cfe24e0b1f2b07ec8bc2942df78c0ff2
Zach Pomerantz <zzmp@uniswap.org>
2023-05-17 12:10:28 -0700

    feat: only pre-cache the document (#6580)
    * test(e2e): de-flake service-worker
    
    * feat: rm stale cache storage
    
    * fix: put not del
    
    * fix: staging and test
    
    * test: include staging
    
    * fix: log
    
    * test: rm console.log
    
    * fix: unregister before
    
    * test: deflake by restoring state afterwards

--- src/serviceWorker/utils.ts
+++ src/serviceWorker/utils.ts
@@ -17,15 +17,28 @@ export function isDevelopment() {
 }
 
 type GroupedEntries = { onDemandEntries: (string | PrecacheEntry)[]; precacheEntries: PrecacheEntry[] }
+
+/**
+ * Splits entries into on-demand and precachable entries.
+ * Effectively, splits out index.html as the only precachable entry.
+ */
 export function groupEntries(entries: (string | PrecacheEntry)[]): GroupedEntries {
   return entries.reduce<GroupedEntries>(
     ({ onDemandEntries, precacheEntries }, entry) => {
       if (typeof entry === 'string' || entry.url.includes('/media/')) {
         return { precacheEntries, onDemandEntries: [...onDemandEntries, entry] }
-      } else {
+      } else if (entry.revision) {
+        // index.html should be the only non-media entry with a revision, as code chunks have a hashed URL.
         return { precacheEntries: [...precacheEntries, entry], onDemandEntries }
+      } else {
+        return { precacheEntries, onDemandEntries: [...onDemandEntries, entry] }
       }
     },
     { onDemandEntries: [], precacheEntries: [] }
   )
+}
+
+export async function deleteUnusedCaches(caches: CacheStorage, { usedCaches }: { usedCaches: string[] }) {
+  const cacheKeys = await caches.keys()
+  cacheKeys.filter((key) => !usedCaches.includes(key)).forEach((key) => caches.delete(key))
 }
