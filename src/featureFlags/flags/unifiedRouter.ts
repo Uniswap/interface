0c5c437d5d3ddac0fc42d7a1e7e63b8cd40353ac
Tina Zheng <tina.s.zheng+github@gmail.com>
2023-05-18 13:21:42 -0400

    remove eslint ignore since the function is now being used

--- src/featureFlags/flags/unifiedRouter.ts
+++ src/featureFlags/flags/unifiedRouter.ts
@@ -4,7 +4,6 @@ export function useUnifiedRoutingAPIFlag(): BaseVariant {
   return useBaseFlag(FeatureFlag.uraEnabled)
 }
 
-// eslint-disable-next-line import/no-unused-modules
 export function useUnifiedRoutingAPIEnabled(): boolean {
   return useUnifiedRoutingAPIFlag() === BaseVariant.Enabled
 }
