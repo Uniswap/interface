a2db3e2719993c8b57b2550d7231d66739a366f5
Zach Pomerantz <zzmp@uniswap.org>
2023-05-19 06:32:42 -0700

    test(e2e): configure Cypress to post PR status comments (#6591)

--- cypress/e2e/swap/swap.test.ts
+++ cypress/e2e/swap/swap.test.ts
@@ -7,7 +7,7 @@ const UNI_MAINNET = UNI[SupportedChainId.MAINNET]
 
 describe('Swap', () => {
   describe('Swap on main page', () => {
-    before(() => {
+    beforeEach(() => {
       cy.visit('/swap', { ethereum: 'hardhat' })
     })
 
@@ -41,55 +41,53 @@ describe('Swap', () => {
     it('should render an error when a transaction fails due to a passed deadline', () => {
       const DEADLINE_MINUTES = 1
       const TEN_MINUTES_MS = 1000 * 60 * DEADLINE_MINUTES * 10
-      cy.visit('/swap', { ethereum: 'hardhat' })
-        .hardhat({ automine: false })
-        .then((hardhat) => {
-          cy.then(() => hardhat.getBalance(hardhat.wallet.address, USDC_MAINNET))
-            .then((balance) => Number(balance.toFixed(1)))
-            .then((initialBalance) => {
-              // Input swap info.
-              cy.get('#swap-currency-output .open-currency-select-button').click()
-              cy.contains('USDC').click()
-              cy.get('#swap-currency-output .token-amount-input').clear().type('1')
-              cy.get('#swap-currency-input .token-amount-input').should('not.equal', '')
+      cy.hardhat({ automine: false }).then((hardhat) => {
+        cy.then(() => hardhat.getBalance(hardhat.wallet.address, USDC_MAINNET))
+          .then((balance) => Number(balance.toFixed(1)))
+          .then((initialBalance) => {
+            // Input swap info.
+            cy.get('#swap-currency-output .open-currency-select-button').click()
+            cy.contains('USDC').click()
+            cy.get('#swap-currency-output .token-amount-input').clear().type('1')
+            cy.get('#swap-currency-input .token-amount-input').should('not.equal', '')
 
-              // Set deadline to minimum. (1 minute)
-              cy.get(getTestSelector('open-settings-dialog-button')).click()
-              cy.get(getTestSelector('transaction-deadline-settings')).click()
-              cy.get(getTestSelector('deadline-input')).clear().type(DEADLINE_MINUTES.toString())
-              cy.get('body').click('topRight')
-              cy.get(getTestSelector('deadline-input')).should('not.exist')
+            // Set deadline to minimum. (1 minute)
+            cy.get(getTestSelector('open-settings-dialog-button')).click()
+            cy.get(getTestSelector('transaction-deadline-settings')).click()
+            cy.get(getTestSelector('deadline-input')).clear().type(DEADLINE_MINUTES.toString())
+            cy.get('body').click('topRight')
+            cy.get(getTestSelector('deadline-input')).should('not.exist')
 
-              cy.get('#swap-button').click()
-              cy.get('#confirm-swap-or-send').click()
+            cy.get('#swap-button').click()
+            cy.get('#confirm-swap-or-send').click()
 
-              // Dismiss the modal that appears when a transaction is broadcast to the network.
-              cy.get(getTestSelector('dismiss-tx-confirmation')).click()
+            // Dismiss the modal that appears when a transaction is broadcast to the network.
+            cy.get(getTestSelector('dismiss-tx-confirmation')).click()
 
-              // The UI should show the transaction as pending.
-              cy.contains('1 Pending').should('exist')
+            // The UI should show the transaction as pending.
+            cy.contains('1 Pending').should('exist')
 
-              // Mine a block past the deadline.
-              cy.then(() => hardhat.mine(1, TEN_MINUTES_MS)).then(() => {
-                // The UI should no longer show the transaction as pending.
-                cy.contains('1 Pending').should('not.exist')
+            // Mine a block past the deadline.
+            cy.then(() => hardhat.mine(1, TEN_MINUTES_MS)).then(() => {
+              // The UI should no longer show the transaction as pending.
+              cy.contains('1 Pending').should('not.exist')
 
-                // Check that the user is informed of the failure
-                cy.contains('Swap failed').should('exist')
+              // Check that the user is informed of the failure
+              cy.contains('Swap failed').should('exist')
 
-                // Check that the balance is unchanged in the UI
-                cy.get('#swap-currency-output [data-testid="balance-text"]').should(
-                  'have.text',
-                  `Balance: ${initialBalance}`
-                )
+              // Check that the balance is unchanged in the UI
+              cy.get('#swap-currency-output [data-testid="balance-text"]').should(
+                'have.text',
+                `Balance: ${initialBalance}`
+              )
 
-                // Check that the balance is unchanged on chain
-                cy.then(() => hardhat.getBalance(hardhat.wallet.address, USDC_MAINNET))
-                  .then((balance) => Number(balance.toFixed(1)))
-                  .should('eq', initialBalance)
-              })
+              // Check that the balance is unchanged on chain
+              cy.then(() => hardhat.getBalance(hardhat.wallet.address, USDC_MAINNET))
+                .then((balance) => Number(balance.toFixed(1)))
+                .should('eq', initialBalance)
             })
-        })
+          })
+      })
     })
 
     it('should default inputs from URL params ', () => {
@@ -107,7 +105,6 @@ describe('Swap', () => {
     })
 
     it('ETH to wETH is same value (wrapped swaps have no price impact)', () => {
-      cy.visit('/swap')
       cy.get(`#swap-currency-output .open-currency-select-button`).click()
       cy.contains('WETH').click()
       cy.get('#swap-currency-input .token-amount-input').clear().type('0.01')
@@ -115,7 +112,6 @@ describe('Swap', () => {
     })
 
     it('Opens and closes the settings menu', () => {
-      cy.visit('/swap')
       cy.contains('Settings').should('not.exist')
       cy.get(getTestSelector('open-settings-dialog-button')).click()
       cy.contains('Max slippage').should('exist')
@@ -135,7 +131,6 @@ describe('Swap', () => {
     })
 
     it('can swap ETH for USDC', () => {
-      cy.visit('/swap', { ethereum: 'hardhat' })
       const TOKEN_ADDRESS = USDC_MAINNET.address
       const BALANCE_INCREMENT = 1
       cy.hardhat().then((hardhat) => {
