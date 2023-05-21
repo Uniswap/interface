fbc55db9374b0e5da59f8a890bb46735080e4a5d
Vignesh Mohankumar <me@vig.xyz>
2023-05-21 17:25:58 -0400

    chore: remove chunkResponseStatus tag (#6586)
    * chore: remove chunkResponseStatus tag
    
    * lint

--- src/tracing/errors.test.ts
+++ src/tracing/errors.test.ts
@@ -1,4 +1,4 @@
-import { ErrorEvent, Event } from '@sentry/types'
+import { ErrorEvent } from '@sentry/types'
 
 import { beforeSend } from './errors'
 
@@ -42,58 +42,6 @@ describe('beforeSend', () => {
 
       beforeSend(event, {})
       expect(event.request?.url).toBe('https://app.uniswap.org/example')
-    })
-  })
-
-  describe('chunkResponseStatus', () => {
-    afterEach(() => {
-      jest.restoreAllMocks()
-    })
-
-    it('handles when matching JS file not found', () => {
-      jest.spyOn(window.performance, 'getEntriesByType').mockReturnValue([])
-      const originalException = new Error(
-        'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
-      )
-      expect((beforeSend(ERROR, { originalException }) as Event).tags).toBeUndefined()
-    })
-
-    it('handles when matching CSS file not found', () => {
-      jest.spyOn(window.performance, 'getEntriesByType').mockReturnValue([])
-      const originalException = new Error(
-        'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
-      )
-      expect((beforeSend(ERROR, { originalException }) as Event).tags).toBeUndefined()
-    })
-
-    it('handles when performance is undefined', () => {
-      window.performance = undefined as any
-      const originalException = new Error('Loading CSS chunk 12 failed. (./static/css/12.d5b3cfe3.chunk.css)')
-      expect((beforeSend(ERROR, { originalException }) as Event).tags).toBeUndefined()
-    })
-
-    it('adds status for a matching JS file', () => {
-      jest.spyOn(window.performance, 'getEntriesByType').mockReturnValue([
-        {
-          name: 'https://app.uniswap.org/static/js/20.d55382e0.chunk.js',
-          responseStatus: 499,
-        } as PerformanceEntry,
-      ])
-      const originalException = new Error(
-        'Loading chunk 20 failed. (error: https://app.uniswap.org/static/js/20.d55382e0.chunk.js)'
-      )
-      expect((beforeSend(ERROR, { originalException }) as Event).tags?.chunkResponseStatus).toBe(499)
-    })
-
-    it('adds status for a matching CSS file', () => {
-      jest.spyOn(window.performance, 'getEntriesByType').mockReturnValue([
-        {
-          name: 'https://app.uniswap.org/static/css/12.d5b3cfe3.chunk.css',
-          responseStatus: 200,
-        } as PerformanceEntry,
-      ])
-      const originalException = new Error('Loading CSS chunk 12 failed. (./static/css/12.d5b3cfe3.chunk.css)')
-      expect((beforeSend(ERROR, { originalException }) as Event).tags?.chunkResponseStatus).toBe(200)
     })
   })
 
