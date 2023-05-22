3289fcce5e6f7ed8a23b97cb203acc10624946dc
Charles Bachmeier <charlie@genie.xyz>
2023-05-22 13:17:52 -0700

    use yarn and sanitize output

--- scripts/fetch-schema.js
+++ scripts/fetch-schema.js
@@ -10,12 +10,14 @@ const thegraphConfig = require('../graphql.thegraph.config')
 const exec = promisify(child_process.exec)
 
 function fetchSchema(url, outputFile) {
-  exec(`get-graphql-schema --h Origin=https://app.uniswap.org ${url}`)
+  exec(`yarn get-graphql-schema --h Origin=https://app.uniswap.org ${url}`)
     .then(({ stderr, stdout }) => {
       if (stderr) {
         throw new Error(stderr)
       } else {
-        fs.writeFile(outputFile, stdout)
+        // yarn adds the command that was run to the top of stdout, so this line removes the first line so only the fetched schema is parsed
+        const outputSchema = stdout.substring(stdout.indexOf('\n') + 1)
+        fs.writeFile(outputFile, outputSchema)
       }
     })
     .catch((err) => {
