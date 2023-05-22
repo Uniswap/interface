6c01efd68c58e477a1b534306cca2259269bbbd4
Mike <grabbou@gmail.com>
2023-05-22 21:57:07 +0400

    chore: add animation test

--- src/components/AnimatedDropdown/index.test.tsx
+++ src/components/AnimatedDropdown/index.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from 'test-utils/render'
+import { render, screen, waitFor } from 'test-utils/render'
 
 import AnimatedDropdown from './index'
 
 describe('AnimatedDropdown', () => {
-  it('does not render children when closed', async () => {
+  it('does not render children when closed', () => {
     render(<AnimatedDropdown open={false}>Body</AnimatedDropdown>)
     expect(screen.getByText('Body')).not.toBeVisible()
   })
@@ -11,5 +11,20 @@ describe('AnimatedDropdown', () => {
   it('renders children when open', () => {
     render(<AnimatedDropdown open={true}>Body</AnimatedDropdown>)
     expect(screen.getByText('Body')).toBeVisible()
+  })
+
+  it('animates when open changes', async () => {
+    const { rerender } = render(<AnimatedDropdown open={false}>Body</AnimatedDropdown>)
+
+    const body = screen.getByText('Body')
+
+    expect(body).not.toBeVisible()
+
+    rerender(<AnimatedDropdown open={true}>Body</AnimatedDropdown>)
+    expect(body).not.toBeVisible()
+
+    await waitFor(() => {
+      expect(body).toBeVisible()
+    })
   })
 })
