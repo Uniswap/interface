881fd78f9907c3fd17a5cdd1c2d972d1e617431a
Jack Short <john.short.tj@gmail.com>
2023-05-23 18:58:11 -0400

    added all the tests

--- src/nft/components/bag/BagFooter.test.tsx
+++ src/nft/components/bag/BagFooter.test.tsx
@@ -5,11 +5,14 @@ import { useWeb3React } from '@web3-react/core'
 import { nativeOnChain } from 'constants/tokens'
 import { useNftUniversalRouterAddress } from 'graphql/data/nft/NftUniversalRouterAddress'
 import { useCurrency } from 'hooks/Tokens'
+import { AllowanceState } from 'hooks/usePermit2Allowance'
 import { useTokenBalance } from 'lib/hooks/useCurrencyBalance'
 import { useBag, useWalletBalance } from 'nft/hooks'
 import { useBagTotalEthPrice } from 'nft/hooks/useBagTotalEthPrice'
 import useDerivedPayWithAnyTokenSwapInfo from 'nft/hooks/useDerivedPayWithAnyTokenSwapInfo'
 import usePayWithAnyTokenSwap from 'nft/hooks/usePayWithAnyTokenSwap'
+import usePermit2Approval from 'nft/hooks/usePermit2Approval'
+import { usePriceImpact } from 'nft/hooks/usePriceImpact'
 import { useTokenInput } from 'nft/hooks/useTokenInput'
 import { BagStatus } from 'nft/types'
 import { TradeState } from 'state/routing/types'
@@ -28,6 +31,8 @@ jest.mock('hooks/Tokens')
 jest.mock('nft/hooks/usePayWithAnyTokenSwap')
 jest.mock('nft/hooks/useDerivedPayWithAnyTokenSwapInfo')
 jest.mock('graphql/data/nft/NftUniversalRouterAddress')
+jest.mock('nft/hooks/usePermit2Approval')
+jest.mock('nft/hooks/usePriceImpact')
 
 const renderBagFooter = () => {
   render(<BagFooter setModalIsOpen={() => undefined} eventProperties={{}} />)
@@ -58,10 +63,21 @@ describe('BagFooter.tsx', () => {
       provider: undefined,
     })
 
+    mocked(usePermit2Approval).mockReturnValue({
+      allowance: {
+        state: AllowanceState.ALLOWED,
+        permitSignature: undefined,
+      },
+      isApprovalLoading: false,
+      isAllowancePending: false,
+      updateAllowance: () => Promise.resolve(),
+    })
     mocked(useNftUniversalRouterAddress).mockReturnValue({
       universalRouterAddress: undefined,
       universalRouterAddressIsLoading: false,
     })
+
+    mocked(usePriceImpact).mockReturnValue(undefined)
 
     mocked(useDerivedPayWithAnyTokenSwapInfo).mockReturnValue({
       state: TradeState.INVALID,
@@ -301,6 +317,99 @@ describe('BagFooter.tsx', () => {
     const buyButton = getBuyButton()
 
     expect(buyButton.textContent).toBe('Pay')
+    expect(buyButton).not.toBeDisabled()
+  })
+
+  it('loading allowance', () => {
+    mocked(usePermit2Approval).mockReturnValue({
+      allowance: {
+        state: AllowanceState.LOADING,
+      },
+      isAllowancePending: false,
+      isApprovalLoading: false,
+      updateAllowance: () => Promise.resolve(),
+    })
+
+    renderBagFooter()
+    const buyButton = getBuyButton()
+
+    expect(buyButton.textContent).toBe('Loading Allowance')
+    expect(buyButton).toBeDisabled()
+  })
+
+  it('approval is loading', () => {
+    mocked(usePermit2Approval).mockReturnValue({
+      allowance: {
+        state: AllowanceState.REQUIRED,
+        isApprovalLoading: false,
+        token: TEST_TOKEN_1,
+        approveAndPermit: () => Promise.resolve(),
+      },
+      isAllowancePending: false,
+      isApprovalLoading: true,
+      updateAllowance: () => Promise.resolve(),
+    })
+
+    renderBagFooter()
+    const buyButton = getBuyButton()
+
+    expect(buyButton.textContent).toBe('Approval pending')
+    expect(buyButton).toBeDisabled()
+  })
+
+  it('allowance to be confirmed in wallet', () => {
+    mocked(usePermit2Approval).mockReturnValue({
+      allowance: {
+        state: AllowanceState.REQUIRED,
+        isApprovalLoading: false,
+        token: TEST_TOKEN_1,
+        approveAndPermit: () => Promise.resolve(),
+      },
+      isAllowancePending: true,
+      isApprovalLoading: false,
+      updateAllowance: () => Promise.resolve(),
+    })
+
+    renderBagFooter()
+    const buyButton = getBuyButton()
+
+    expect(buyButton.textContent).toBe('Approve in your wallet')
+    expect(buyButton).toBeDisabled()
+  })
+
+  it('approve', () => {
+    mocked(usePermit2Approval).mockReturnValue({
+      allowance: {
+        state: AllowanceState.REQUIRED,
+        isApprovalLoading: false,
+        token: TEST_TOKEN_1,
+        approveAndPermit: () => Promise.resolve(),
+      },
+      isAllowancePending: false,
+      isApprovalLoading: false,
+      updateAllowance: () => Promise.resolve(),
+    })
+
+    renderBagFooter()
+    const buyButton = getBuyButton()
+
+    expect(buyButton.textContent).toBe('Approve')
+    expect(buyButton).not.toBeDisabled()
+  })
+
+  it('price impact high', () => {
+    mocked(usePriceImpact).mockReturnValue({
+      priceImpactSeverity: {
+        type: 'error',
+        color: 'accentCritical',
+      },
+      displayPercentage: () => '5%',
+    })
+
+    renderBagFooter()
+    const buyButton = getBuyButton()
+
+    expect(buyButton.textContent).toBe('Pay Anyway')
     expect(buyButton).not.toBeDisabled()
   })
 })
