d23b6e5da66bb3c12aeb61f39e04d00aae28238a
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-05-25 10:36:57 -0500

    fix: remove app advert on mobile safari (#6630)
    Co-authored-by: Jordan Frankfurt <jordan@CORN-Jordan-949.frankfurt>

--- src/utils/userAgent.ts
+++ src/utils/userAgent.ts
@@ -2,8 +2,11 @@ import { UAParser } from 'ua-parser-js'
 
 const parser = new UAParser(window.navigator.userAgent)
 const { type } = parser.getDevice()
+const { name } = parser.getBrowser()
 
 export const isMobile = type === 'mobile' || type === 'tablet'
 const platform = parser.getOS().name
 export const isIOS = platform === 'iOS'
 export const isNonIOSPhone = !isIOS && type === 'mobile'
+
+export const isMobileSafari = isMobile && isIOS && name?.toLowerCase().includes('safari')
