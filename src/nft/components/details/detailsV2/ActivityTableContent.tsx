c22f5a4c68c4c12ef8a51fdb7fe06fc04feb0ad6
Charles Bachmeier <charlie@genie.xyz>
2023-05-26 12:00:21 -0700

    no more abs positioning

--- src/nft/components/details/detailsV2/ActivityTableContent.tsx
+++ src/nft/components/details/detailsV2/ActivityTableContent.tsx
@@ -1,33 +1,22 @@
 import { HistoryDuration } from 'graphql/data/__generated__/types-and-hooks'
+import { Row } from 'nft/components/Flex'
 import { useState } from 'react'
 import styled from 'styled-components/macro'
-import { BREAKPOINTS } from 'theme'
 
 import { SupportedTimePeriodsType, TimePeriodSwitcher } from './TimePeriodSwitcher'
 
-const TableContentContainer = styled.div`
+const TableContentContainer = styled(Row)`
   height: 568px;
-  position: relative;
-`
-
-const SwitcherWrapper = styled.div`
-  position: absolute;
-  right: 0;
-  top: -36px;
-
-  @media screen and (max-width: ${BREAKPOINTS.sm}px) {
-    top: 0px;
-  }
+  justify-content: space-between;
+  align-items: flex-start;
 `
 
 export const ActivityTableContent = () => {
   const [timePeriod, setTimePeriod] = useState<SupportedTimePeriodsType>(HistoryDuration.Week)
   return (
     <TableContentContainer>
-      <SwitcherWrapper>
-        <TimePeriodSwitcher activeTimePeriod={timePeriod} setTimePeriod={setTimePeriod} />
-      </SwitcherWrapper>
-      Activity Content
+      <span>Activity Content</span>
+      <TimePeriodSwitcher activeTimePeriod={timePeriod} setTimePeriod={setTimePeriod} />
     </TableContentContainer>
   )
 }
