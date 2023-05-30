2897e3239d6936faf589102c8615591e71a26f1c
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-05-30 10:33:49 -0700

    fix: fix polygon bridge to send directly to POS bridge

--- src/constants/chainInfo.ts
+++ src/constants/chainInfo.ts
@@ -148,7 +148,7 @@ const CHAIN_INFO: ChainInfoMap = {
   [SupportedChainId.POLYGON]: {
     networkType: NetworkType.L1,
     blockWaitMsBeforeWarning: ms`10m`,
-    bridge: 'https://wallet.polygon.technology',
+    bridge: 'https://wallet.polygon.technology/polygon/bridge',
     docs: 'https://polygon.io/',
     explorer: 'https://polygonscan.com/',
     infoLink: 'https://info.uniswap.org/#/polygon/',
