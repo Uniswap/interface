0ce80625d485c21a74f2488486182430b2162bf4
Eddie Dugan <eddie.dugan@uniswap.org>
2023-06-05 12:56:42 -0700

    feat: log swap errors to sentry

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,4 +1,5 @@
 import { Trans } from '@lingui/macro'
+import * as Sentry from '@sentry/react'
 import { sendAnalyticsEvent, Trace, TraceEvent, useTrace } from '@uniswap/analytics'
 import {
   BrowserEvent,
@@ -440,6 +441,12 @@ export function Swap({
         })
       })
       .catch((error) => {
+        if (!didUserReject(error)) {
+          Sentry.withScope((scope) => {
+            scope.setExtra('swapState', { trade: JSON.stringify(tradeToConfirm), txHash })
+            Sentry.captureException(error)
+          })
+        }
         setSwapState((currentState) => ({
           ...currentState,
           swapError: error,
@@ -454,6 +461,8 @@ export function Swap({
     account,
     trade?.inputAmount?.currency?.symbol,
     trade?.outputAmount?.currency?.symbol,
+    tradeToConfirm,
+    txHash,
   ])
 
   // errors
