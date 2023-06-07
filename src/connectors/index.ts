16a9b7ca4b839445ec4ad4e1adf52e917dd295c2
Francisco <jscriptcoder@gmail.com>
2023-06-07 14:07:10 +0200

    network can be changed directly in metamask

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -23,7 +23,7 @@ export const network = new NetworkConnector({
 
 let networkLibrary: Web3Provider | undefined
 export function getNetworkLibrary(): Web3Provider {
-  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
+  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any, 'any'))
 }
 
 export const injected = new InjectedConnector({
