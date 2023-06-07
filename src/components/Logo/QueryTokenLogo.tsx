1e882862ff5849407576b3494fe251065399981f
brenno <brennoaraujoqueiroz@gmail.com>
2023-06-06 21:32:21 -0300

    chore: refactor QueryTokenLogo component to receive new Type Token

--- src/components/Logo/QueryTokenLogo.tsx
+++ src/components/Logo/QueryTokenLogo.tsx
@@ -1,31 +1,49 @@
+import { SupportedChainId } from '@pollum-io/widgets'
 import { NATIVE_CHAIN_ID } from 'constants/tokens'
 import { TokenStandard } from 'graphql/data/__generated__/types-and-hooks'
 import { SearchToken } from 'graphql/data/SearchTokens'
 import { TokenQueryData } from 'graphql/data/Token'
 import { TopToken } from 'graphql/data/TopTokens'
 import { CHAIN_NAME_TO_CHAIN_ID } from 'graphql/data/util'
+import { Token } from 'graphql/tokens/NewTopTokens'
 
 import AssetLogo, { AssetLogoBaseProps } from './AssetLogo'
 
+function getChainId(token?: TopToken | TokenQueryData | SearchToken | Token): SupportedChainId | undefined {
+  if (token && 'chain' in token) {
+    return token?.chain ? CHAIN_NAME_TO_CHAIN_ID[token?.chain] : undefined
+  } else {
+    return SupportedChainId.ROLLUX
+  }
+}
+
+function isNativeToken(token?: TopToken | TokenQueryData | SearchToken | Token): boolean {
+  if (token && 'address' in token) {
+    return !token?.address || token?.standard === TokenStandard.Native || token?.address === NATIVE_CHAIN_ID
+  } else {
+    // TODO check if token rollux is native token
+    return false
+  }
+}
 export default function QueryTokenLogo(
   props: AssetLogoBaseProps & {
-    token?: TopToken | TokenQueryData | SearchToken
+    token?: TopToken | TokenQueryData | SearchToken | Token
   }
 ) {
-  const chainId = props.token?.chain ? CHAIN_NAME_TO_CHAIN_ID[props.token?.chain] : undefined
+  const { token } = props
+  const chainId = getChainId(token)
+  const isNative = isNativeToken(token)
+
+  const address = token && 'address' in token ? token.address : token && 'id' in token ? token.id : undefined
+  const backupImg = token && 'project' in token && token?.project ? token.project.logoUrl : undefined
 
   return (
     <AssetLogo
-      isNative={
-        // TODO(cartcrom): simplify this check after backend fixes token standard on assetActivities tokens
-        !props.token?.address ||
-        props.token?.standard === TokenStandard.Native ||
-        props.token?.address === NATIVE_CHAIN_ID
-      }
+      isNative={isNative}
       chainId={chainId}
-      address={props.token?.address}
-      symbol={props.token?.symbol}
-      backupImg={props.token?.project?.logoUrl}
+      address={address}
+      symbol={token?.symbol}
+      backupImg={backupImg}
       {...props}
     />
   )
