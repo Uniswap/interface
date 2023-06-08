9bb98ff1efc5458b80b83b271f5870174a9c49a4
Eddie Dugan <eddie.dugan@uniswap.org>
2023-06-07 18:14:55 -0700

    fix: move test

--- cypress/e2e/permit2.test.ts
+++ cypress/e2e/permit2.test.ts
@@ -90,6 +90,28 @@ describe('Permit2', () => {
       cy.contains('Success')
       cy.get(getTestSelector('popups')).contains('Swapped')
     })
+
+    it('retains modal state when the window loses focus', () => {
+      initiateSwap()
+
+      cy.window().trigger('blur')
+
+      // Verify token approval
+      cy.contains('Enable spending DAI on Uniswap')
+      cy.wait('@eth_sendRawTransaction')
+      cy.hardhat().then((hardhat) => hardhat.mine())
+      cy.get(getTestSelector('popups')).contains('Approved')
+      expectTokenAllowanceForPermit2ToBeMax()
+
+      // Verify permit2 approval
+      cy.contains('Allow DAI to be used for swapping')
+      cy.wait('@eth_signTypedData_v4')
+      cy.wait('@eth_sendRawTransaction')
+      cy.hardhat().then((hardhat) => hardhat.mine())
+      cy.contains('Success')
+      cy.get(getTestSelector('popups')).contains('Swapped')
+      expectPermit2AllowanceForUniversalRouterToBeMax()
+    })
   })
 
   it('swaps when user has already approved token and permit2', () => {
@@ -104,28 +126,6 @@ describe('Permit2', () => {
     // Verify transaction
     cy.contains('Success')
     cy.get(getTestSelector('popups')).contains('Swapped')
-  })
-
-  it('retains modal state when the window loses focus', () => {
-    initiateSwap()
-
-    cy.window().trigger('blur')
-
-    // Verify token approval
-    cy.contains('Enable spending DAI on Uniswap')
-    cy.wait('@eth_sendRawTransaction')
-    cy.hardhat().then((hardhat) => hardhat.mine())
-    cy.get(getTestSelector('popups')).contains('Approved')
-    expectTokenAllowanceForPermit2ToBeMax()
-
-    // Verify permit2 approval
-    cy.contains('Allow DAI to be used for swapping')
-    cy.wait('@eth_signTypedData_v4')
-    cy.wait('@eth_sendRawTransaction')
-    cy.hardhat().then((hardhat) => hardhat.mine())
-    cy.contains('Success')
-    cy.get(getTestSelector('popups')).contains('Swapped')
-    expectPermit2AllowanceForUniversalRouterToBeMax()
   })
 
   it('swaps after handling user rejection of both approval and signature', () => {
