93ad114c805fe5edb033676644422a83f9772349
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-06-09 17:43:21 -0400

    fix: create tests

--- src/hooks/useDisableScrolling.ts
+++ src/hooks/useDisableScrolling.ts
@@ -1,12 +1,12 @@
 import { useEffect } from 'react'
 import { isMobile } from 'utils/userAgent'
 
-export default function useDisableScrolling(open: boolean | undefined | null) {
+export default function useDisableScrolling(disable: boolean | undefined | null) {
   useEffect(() => {
-    if (open && isMobile) {
+    if (disable && isMobile) {
       document.body.style.overflow = 'hidden'
     } else if (isMobile) {
       document.body.style.overflow = 'auto'
     }
-  }, [open])
+  }, [disable])
 }
