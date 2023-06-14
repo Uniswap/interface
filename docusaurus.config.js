f3f3ac00372e616effcbdd5463bc76e1e061800d
gab <gabriele.rigo@gmail.com>
2023-06-14 16:33:51 +0200

    fix docusaurus setup file

---
+++ docusaurus.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Rigoblock'.
+  url: 'https://app.rigoblock.com',
+  baseUrl: '/',
+  plugins: [
+    ['docusaurus-node-polyfills', { excludeAliases: ['console']}],
+  ],
+}
