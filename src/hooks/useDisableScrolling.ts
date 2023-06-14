62730838c6370c42fdfb37bac3e4937fb230f10a
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-06-14 10:54:54 -0400

    fix: simplify useeffect and add JSDoc

--- src/hooks/useDisableScrolling.ts
+++ src/hooks/useDisableScrolling.ts
@@ -1,12 +1,10 @@
 import { useEffect } from 'react'
 import { isMobile } from 'utils/userAgent'
 
+/** Disables scrolling of the main body on mobile when `true` is passed. Generally used for modals. */
 export default function useDisableScrolling(disable: boolean | undefined | null) {
   useEffect(() => {
-    if (disable && isMobile) {
-      document.body.style.overflow = 'hidden'
-    } else if (isMobile) {
-      document.body.style.overflow = 'auto'
-    }
+    if (!isMobile) return
+    document.body.style.overflow = disable ? 'hidden' : 'auto'
   }, [disable])
 }
