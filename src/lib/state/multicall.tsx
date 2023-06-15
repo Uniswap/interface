e9931a113f33c8f46d55f200328cc46a8479f80a
Charles Bachmeier <charlie@genie.xyz>
2023-06-15 13:15:38 -0700

    update avax blocksPerFetch

--- src/lib/state/multicall.tsx
+++ src/lib/state/multicall.tsx
@@ -9,14 +9,19 @@ const multicall = createMulticall()
 
 export default multicall
 
-// TODO(AVAX): How can I check this?
+/**
+ *
+ * @param chainId
+ * @returns The approximate number of blocks written to the corresponding chainId per Ethereum mainnet epoch.
+ */
 function getBlocksPerFetchForChainId(chainId: number | undefined): number {
   switch (chainId) {
+    case ChainId.AVALANCHE:
+      return 30
     case ChainId.ARBITRUM_ONE:
     case ChainId.OPTIMISM:
       return 15
     case ChainId.BNB:
-    case ChainId.AVALANCHE:
     case ChainId.CELO:
     case ChainId.CELO_ALFAJORES:
       return 5
