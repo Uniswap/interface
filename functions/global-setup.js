503cc40e2fcc4db3eb6335d0dea4e502f39ebefe
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-06-16 19:27:13 -0400

    fix: uninstall port ready

--- functions/global-setup.js
+++ functions/global-setup.js
@@ -1,12 +1,10 @@
 const { setup: setupDevServer } = require('jest-dev-server')
-const portReady = require('port-ready')
 
 module.exports = async function globalSetup() {
   globalThis.servers = await setupDevServer({
     command: `NODE_OPTIONS=--dns-result-order=ipv4first npx wrangler pages dev --proxy=3001 --port=3000 -- yarn start`,
-    launchTimeout: 50000,
+    launchTimeout: 60000,
     port: 3000,
   })
-  //await portReady(3000)
-  await new Promise((r) => setTimeout(r, 30000))
+  await new Promise((r) => setTimeout(r, 60000))
 }
