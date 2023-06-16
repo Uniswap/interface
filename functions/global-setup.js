aff3259940a2afa7eb18d8dbcd94ab86f008a92b
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-06-16 19:59:06 -0400

    fix: local tests work

--- functions/global-setup.js
+++ functions/global-setup.js
@@ -3,9 +3,7 @@ const portReady = require('port-ready')
 
 module.exports = async function globalSetup() {
   globalThis.servers = await setupDevServer({
-    command: `NODE_OPTIONS=--dns-result-order=ipv4first npx wrangler pages dev --proxy=3001 --port=3000 -- yarn start`,
-    launchTimeout: 60000,
-    port: 3000,
+    command: `yarn start:wrangler`,
   })
-  await portReady({ port: 3000 })
+  await new Promise((r) => setTimeout(r, 30000))
 }
