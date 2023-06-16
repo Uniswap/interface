c057853c95880fa6b9fecff4ec7f72234adeb82a
gab <gabriele.rigo@gmail.com>
2023-06-16 09:49:25 +0200

    feat: upgrade to mendable search 0.0.111
    - upgraded dep
    - removed unnecessary deps
    - removed docusaurus setup file

--- docusaurus.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  title: 'Rigoblock'.
-  url: 'https://app.rigoblock.com',
-  baseUrl: '/',
-  plugins: [
-    ['docusaurus-node-polyfills', { excludeAliases: ['console']}],
-  ],
-}
