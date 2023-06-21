eeb097cbacc983ba8c0208ed4eaf830dff5eb01a
gab <gabriele.rigo@gmail.com>
2023-06-21 16:53:50 +0200

    feat: display user votes in vote page
    - display user votes if positive in vote landing page
    - display loader conditional to components loading, display votes button if not loading
    - removed unused delegatee and related deps
    - removed commented code from vote landing page

--- src/pages/Vote/Landing.tsx
+++ src/pages/Vote/Landing.tsx
@@ -1,35 +1,28 @@
 import { Trans } from '@lingui/macro'
 import { Trace } from '@uniswap/analytics'
 import { InterfacePageName } from '@uniswap/analytics-events'
-//import { CurrencyAmount, Token } from '@uniswap/sdk-core'
-import { useWeb3React } from '@web3-react/core'
 import { ButtonPrimary } from 'components/Button'
 import { AutoColumn } from 'components/Column'
 import { CardBGImage, CardNoise, CardSection, DataCard } from 'components/earn/styled'
-//import FormattedCurrencyAmount from 'components/FormattedCurrencyAmount'
+import FormattedCurrencyAmount from 'components/FormattedCurrencyAmount'
 import Loader from 'components/Icons/LoadingSpinner'
-import { AutoRow, RowBetween, RowFixed } from 'components/Row'
+import { AutoRow, RowBetween } from 'components/Row'
 import { SwitchLocaleLink } from 'components/SwitchLocaleLink'
 import Toggle from 'components/Toggle'
 import DelegateModal from 'components/vote/DelegateModal'
 import ProposalEmptyState from 'components/vote/ProposalEmptyState'
-//import JSBI from 'jsbi'
+import JSBI from 'jsbi'
 import { darken } from 'polished'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'rebass/styled-components'
 import { useModalIsOpen, useToggleDelegateModal } from 'state/application/hooks'
 import { ApplicationModal } from 'state/application/reducer'
-//import { useTokenBalance } from 'state/connection/hooks'
 import { ProposalData, ProposalState } from 'state/governance/hooks'
-import { useAllProposalData, useUserDelegatee /*, useUserVotes*/ } from 'state/governance/hooks'
+import { useAllProposalData, useUserVotes } from 'state/governance/hooks'
 import styled, { useTheme } from 'styled-components/macro'
 import { ExternalLink, ThemedText } from 'theme'
-import { shortenAddress } from 'utils'
-import { ExplorerDataType, getExplorerLink } from 'utils/getExplorerLink'
 
-import { ZERO_ADDRESS } from '../../constants/misc'
-//import { GRG } from '../../constants/tokens'
 import { ProposalStatus } from './styled'
 
 const PageWrapper = styled(AutoColumn)`
@@ -97,31 +90,8 @@ const WrapSmall = styled(RowBetween)`
   `};
 `
 
-const TextButton = styled(ThemedText.DeprecatedMain)`
-  color: ${({ theme }) => theme.accentAction};
-  :hover {
-    cursor: pointer;
-    text-decoration: underline;
-  }
-`
-
-const AddressButton = styled.div`
-  padding: 2px 4px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: ${({ theme }) => theme.accentAction};
-`
-
-const StyledExternalLink = styled(ExternalLink)`
-  color: ${({ theme }) => theme.textPrimary};
-`
-
 export default function Landing() {
   const theme = useTheme()
-  const { account } = useWeb3React()
-
   const [hideCancelled, setHideCancelled] = useState(true)
 
   // toggle for showing delegation modal
@@ -132,19 +102,10 @@ export default function Landing() {
   const { data: allProposals, loading: loadingProposals } = useAllProposalData()
 
   // user data
-  //const { loading: loadingAvailableVotes, votes: availableVotes } = useUserVotes()
-  //const grgBalance: CurrencyAmount<Token> | undefined = useTokenBalance(
-  //  account ?? undefined,
-  //  chainId ? GRG[chainId] : undefined
-  //)
-  // TODO: check if should remove this RPC call
-  const userDelegatee: string | undefined = useUserDelegatee()
+  const { loading: loadingAvailableVotes, votes: availableVotes } = useUserVotes()
 
   // show delegation option if they have have a balance, but have not delegated
-  //const showUnlockVoting = Boolean(
-  //  grgBalance && JSBI.notEqual(grgBalance.quotient, JSBI.BigInt(0)) && userDelegatee === ZERO_ADDRESS
-  //)
-  const showUnlockVoting = true
+  const showUnlockVoting = availableVotes && Boolean(JSBI.equal(availableVotes.quotient, JSBI.BigInt(0)))
   return (
     <>
       <Trace page={InterfacePageName.VOTE_PAGE} shouldLogImpression>
@@ -197,47 +158,26 @@ export default function Landing() {
                 <Trans>Proposals</Trans>
               </ThemedText.DeprecatedMediumHeader>
               <AutoRow gap="6px" justify="flex-end">
-                {loadingProposals ? <Loader /> : null}
-                {showUnlockVoting && (
+                {loadingProposals || loadingAvailableVotes ? (
+                  <Loader />
+                ) : (
                   <ButtonPrimary
                     style={{ width: 'fit-content', height: '40px' }}
                     padding="8px"
                     $borderRadius="8px"
                     onClick={toggleDelegateModal}
                   >
-                    <Trans>Unlock Votes</Trans>
-                  </ButtonPrimary>
-                )}
-                {/*
-                  {loadingProposals || loadingAvailableVotes ? <Loader /> : null}
-                  {showUnlockVoting ? (
-                    <ButtonPrimary
-                      style={{ width: 'fit-content' }}
-                      padding="8px"
-                      $borderRadius="8px"
-                      onClick={toggleDelegateModal}
-                    >
+                    {showUnlockVoting ? (
                       <Trans>Unlock Votes</Trans>
-                    </ButtonPrimary>
-                  ) : availableVotes && JSBI.notEqual(JSBI.BigInt(0), availableVotes?.quotient) ? (
-                    <ThemedText.DeprecatedBody fontWeight={500} mr="6px">
+                    ) : availableVotes ? (
                       <Trans>
                         <FormattedCurrencyAmount currencyAmount={availableVotes} /> Votes
                       </Trans>
-                    </ThemedText.DeprecatedBody>
-                  ) : grgBalance &&
-                    userDelegatee &&
-                    userDelegatee !== ZERO_ADDRESS &&
-                    JSBI.notEqual(JSBI.BigInt(0), grgBalance?.quotient) ? (
-                    <ThemedText.DeprecatedBody fontWeight={500} mr="6px">
-                      <Trans>
-                        <FormattedCurrencyAmount currencyAmount={grgBalance} /> Votes
-                      </Trans>
-                    </ThemedText.DeprecatedBody>
-                  ) : (
-                    ''
-                  )}
-                */}
+                    ) : (
+                      ''
+                    )}
+                  </ButtonPrimary>
+                )}
                 <ButtonPrimary
                   as={Link}
                   to="/create-proposal"
@@ -248,31 +188,6 @@ export default function Landing() {
                 </ButtonPrimary>
               </AutoRow>
             </WrapSmall>
-            {!showUnlockVoting && (
-              <RowBetween>
-                <div />
-                {userDelegatee && userDelegatee !== ZERO_ADDRESS ? (
-                  <RowFixed>
-                    <ThemedText.DeprecatedBody fontWeight={500} mr="4px">
-                      <Trans>Delegated to:</Trans>
-                    </ThemedText.DeprecatedBody>
-                    <AddressButton>
-                      <StyledExternalLink
-                        href={getExplorerLink(1, userDelegatee, ExplorerDataType.ADDRESS)}
-                        style={{ margin: '0 4px' }}
-                      >
-                        {userDelegatee === account ? <Trans>Self</Trans> : shortenAddress(userDelegatee)}
-                      </StyledExternalLink>
-                      <TextButton onClick={toggleDelegateModal} style={{ marginLeft: '4px' }}>
-                        <Trans>(edit)</Trans>
-                      </TextButton>
-                    </AddressButton>
-                  </RowFixed>
-                ) : (
-                  ''
-                )}
-              </RowBetween>
-            )}
 
             {allProposals?.length === 0 && <ProposalEmptyState />}
 
