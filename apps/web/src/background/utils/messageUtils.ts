9273921c03ba7917264c4676f6b7525193566e8b
Thomas Thachil <thomasthachil@users.noreply.github.com>
2023-06-23 15:49:35 -0400

    fix(ext): fix no connection errors (#3917)
    Co-authored-by: Thomas Thachil <>

--- apps/web/src/background/utils/messageUtils.ts
+++ apps/web/src/background/utils/messageUtils.ts
@@ -4,10 +4,14 @@ import {
   TransactionRejectedResponse,
 } from 'src/background/features/dappRequests/dappRequestTypes'
 
-export function sendMessageToActiveTab(message: Message): void {
+export function sendMessageToActiveTab(message: Message, onError?: () => void): void {
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
     if (tab?.id) {
-      chrome.tabs.sendMessage<Message>(tab.id, message)
+      chrome.tabs.sendMessage<Message>(tab.id, message).catch(() => {
+        onError?.()
+        // If no listener is listening to the message, the promise will be rejected,
+        // so we need to catch it unless there is an explicit error handler
+      })
     }
   })
 }
