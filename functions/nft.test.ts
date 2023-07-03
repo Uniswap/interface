4a07f6ef6003732c420d45c25df21264b42127b2
Brendan Wong <bjwpokemon@gmail.com>
2023-07-03 13:47:59 -0400

    tests

--- functions/nft.test.ts
+++ functions/nft.test.ts
@@ -10,28 +10,29 @@ beforeAll(async () => {
   await waitPort(params)
 }, 60000)
 
+const assets = [
+  {
+    address: '0xed5af388653567af2f388e6224dc7c4b3241c544',
+    assetId: '2550',
+    collectionName: 'Azuki',
+    image: 'https://cdn.center.app/1/0xED5AF388653567Af2F388E6224dC7C4b3241C544/2550/d268b7f60a56306ced68b9762709ceaff4f1ee939f3150e7363fae300a59da12.png'
+  },
+  {
+    address: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
+    assetId: '3735',
+    collectionName: 'Bored Ape Yacht Club',
+    image: 'https://cdn.center.app/v2/1/697f69bb495aaa24c66638cae921977354f0b8274fc2e2814e455f355e67f01d/88c2ac6b73288e41051d3fd58ff3cef1f4908403f05f4a7d2a8435d003758529.png'
+  },
+  {
+    address: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
+    assetId: '3947',
+    collectionName: 'CryptoPunk',
+    image: 'https://cdn.center.app/1/0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB/3947/62319d784e7a816d190aa184ffe58550d6ed8eb2e117b218e2ac02f126538ee6.png'
+  }
+]
+
 test('should inject metadata for valid nfts', async () => {
-  const nfts = [
-    {
-      address: '0xed5af388653567af2f388e6224dc7c4b3241c544',
-      assetId: '2550',
-      collectionName: 'Azuki',
-      image: 'https://cdn.center.app/1/0xED5AF388653567Af2F388E6224dC7C4b3241C544/2550/d268b7f60a56306ced68b9762709ceaff4f1ee939f3150e7363fae300a59da12.png'
-    },
-    {
-      address: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
-      assetId: '3735',
-      collectionName: 'Bored Ape Yacht Club',
-      image: 'https://cdn.center.app/v2/1/697f69bb495aaa24c66638cae921977354f0b8274fc2e2814e455f355e67f01d/88c2ac6b73288e41051d3fd58ff3cef1f4908403f05f4a7d2a8435d003758529.png'
-    },
-    {
-      address: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
-      assetId: '3947',
-      collectionName: 'CryptoPunk',
-      image: 'https://cdn.center.app/1/0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB/3947/62319d784e7a816d190aa184ffe58550d6ed8eb2e117b218e2ac02f126538ee6.png'
-    }
-  ]
-  for (const nft of nfts) {
+  for (const nft of assets) {
     const url = 'http://127.0.0.1:3000/nfts/asset/' + nft.address + '/' + nft.assetId
     const req = new Request(url)
     const res = await fetch(req)
@@ -82,5 +83,30 @@ test('should not inject metadata for invalid calls', async () => {
     const res = await fetch(req)
     const body = await res.text()
     expect(body).toEqual(baseBody)
+  }
+})
+
+test('api should return a valid response', async () => {
+  for (const nft of assets) {
+    const url = 'http://127.0.0.1:3000/nfts/asset/' + nft.address + '/' + nft.assetId
+    const req = new Request(url)
+    const res = await fetch(req)
+    expect(res.status).toEqual(200)
+  }
+})
+
+test('api should not return a valid response', async () => {
+  const invalidUrls = [
+    '0xed5af388653567af2f388e6224dc7c4b3241c545',
+    '0xed5af388653567af2f388e6224dc7c4b3241c545/',
+    '0xed5af388653567af2f388e6224dc7c4b3241c545/0',
+    '0xed5af388653567af2f388e6224dc7c4b3241c544/100000',
+    '0xed5af388653567af2f388e6224dc7c4b3241c544',
+  ]
+  for(const urls of invalidUrls) {
+    const url = 'http://127.0.0.1:3000/api/image/nfts/asset/' + urls
+    const req = new Request(url)
+    const res = await fetch(req)
+    expect([404, 500]).toContain(res.status)
   }
 })
--- functions/token.test.ts
+++ functions/token.test.ts
@@ -10,39 +10,40 @@ beforeAll(async () => {
   await waitPort(params)
 }, 60000)
 
+const tokens = [
+  {
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    network: 'ethereum',
+    symbol: 'USDC',
+    image: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png'
+  },
+  {
+    address: 'NATIVE',
+    network: 'ethereum',
+    symbol: 'ETH',
+    image: 'https://token-icons.s3.amazonaws.com/eth.png'
+  },
+  {
+    address: 'NATIVE',
+    network: 'polygon',
+    symbol: 'MATIC',
+    image: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png'
+  },
+  {
+    address: '0x1f52145666c862ed3e2f1da213d479e61b2892af',
+    network: 'arbitrum',
+    symbol: 'FUC',
+    image: 'https://assets.coingecko.com/coins/images/30081/large/fuc.png?1683016112'
+  },
+  {
+    address: '0x6982508145454ce325ddbe47a25d4ec3d2311933',
+    network: 'ethereum',
+    symbol: 'PEPE',
+    image: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x6982508145454Ce325dDbE47a25d4ec3d2311933/logo.png'
+  }
+]
+
 test('should inject metadata for valid tokens', async () => {
-  const tokens = [
-    {
-      address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-      network: 'ethereum',
-      symbol: 'USDC',
-      image: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png'
-    },
-    {
-      address: 'NATIVE',
-      network: 'ethereum',
-      symbol: 'ETH',
-      image: 'https://token-icons.s3.amazonaws.com/eth.png'
-    },
-    {
-      address: 'NATIVE',
-      network: 'polygon',
-      symbol: 'MATIC',
-      image: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png'
-    },
-    {
-      address: '0x1f52145666c862ed3e2f1da213d479e61b2892af',
-      network: 'arbitrum',
-      symbol: 'FUC',
-      image: 'https://assets.coingecko.com/coins/images/30081/large/fuc.png?1683016112'
-    },
-    {
-      address: '0x6982508145454ce325ddbe47a25d4ec3d2311933',
-      network: 'ethereum',
-      symbol: 'PEPE',
-      image: 'https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x6982508145454Ce325dDbE47a25d4ec3d2311933/logo.png'
-    }
-  ]
   for (const token of tokens) {
     const url = 'http://127.0.0.1:3000/tokens/' + token.network + '/' + token.address
     const req = new Request(url)
@@ -94,5 +95,20 @@ test('should not inject metadata for invalid calls', async () => {
     const res = await fetch(req)
     const body = await res.text()
     expect(body).toEqual(baseBody)
+  }
+})
+
+test('api should not return a valid response', async () => {
+  const invalidUrls = [
+    'ethereum/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb49',
+    'ethereum/0x0',
+    'potato/?potato=1',
+    'tomato/NATIVE',
+  ]
+  for(const urls of invalidUrls) {
+    const url = 'http://127.0.0.1:3000/api/image/tokens/' + urls
+    const req = new Request(url)
+    const res = await fetch(req)
+    expect([404, 500]).toContain(res.status)
   }
 })
