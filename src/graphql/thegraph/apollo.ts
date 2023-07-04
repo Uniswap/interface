aefc28c2ffa09c5662b31392bb2ce1cdcd7ec75b
thomga <gabriel.thom02@gmail.com>
2023-07-04 15:58:18 -0300

    chore: fix apollo client middleware issue

--- src/graphql/thegraph/apollo.ts
+++ src/graphql/thegraph/apollo.ts
@@ -1,34 +1,53 @@
-import { ApolloClient, ApolloLink, concat, HttpLink, InMemoryCache } from '@apollo/client'
-import { SupportedChainId } from 'constants/chains'
+// import { ApolloClient, ApolloLink, concat, HttpLink, InMemoryCache } from '@apollo/client'
+// import { SupportedChainId } from 'constants/chains'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 import store from '../../state/index'
 
-const CHAIN_SUBGRAPH_URL: Record<number, string> = {
-  [SupportedChainId.ROLLUX]: 'https://rollux.graph.pegasys.fi/subgraphs/name/pollum-io/pegasys-v3',
-}
+// const CHAIN_SUBGRAPH_URL: Record<number, string> = {
+//   [SupportedChainId.ROLLUX]: 'https://rollux.graph.pegasys.fi/subgraphs/name/pollum-io/pegasys-v3',
+// }
 
-const httpLink = new HttpLink({ uri: CHAIN_SUBGRAPH_URL[SupportedChainId.ROLLUX] })
+// const httpLink = new HttpLink({ uri: CHAIN_SUBGRAPH_URL[SupportedChainId.ROLLUX] })
 
-// This middleware will allow us to dynamically update the uri for the requests based off chainId
-// For more information: https://www.apollographql.com/docs/react/networking/advanced-http-networking/
-const authMiddleware = new ApolloLink((operation, forward) => {
-  // add the authorization to the headers
-  const chainId = store.getState().application.chainId
+// // This middleware will allow us to dynamically update the uri for the requests based off chainId
+// // For more information: https://www.apollographql.com/docs/react/networking/advanced-http-networking/
+// const authMiddleware = new ApolloLink((operation, forward) => {
+//   // add the authorization to the headers
+//   const chainId = store.getState().application.chainId
 
-  operation.setContext(() => ({
-    uri:
-      chainId && CHAIN_SUBGRAPH_URL[chainId]
-        ? CHAIN_SUBGRAPH_URL[chainId]
-        : CHAIN_SUBGRAPH_URL[SupportedChainId.ROLLUX],
-  }))
+//   operation.setContext(() => ({
+//     uri:
+//       chainId && CHAIN_SUBGRAPH_URL[chainId]
+//         ? CHAIN_SUBGRAPH_URL[chainId]
+//         : CHAIN_SUBGRAPH_URL[SupportedChainId.ROLLUX],
+//   }))
 
-  return forward(operation)
-})
+//   return forward(operation)
+// })
+
+// export const apolloClient = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   link: concat(authMiddleware, httpLink),
+// })
+
 
 export const apolloClient = new ApolloClient({
+  uri: 'https://rollux.graph.pegasys.fi/subgraphs/name/pollum-io/pegasys-v3',
   cache: new InMemoryCache(),
-  link: concat(authMiddleware, httpLink),
+  queryDeduplication: true,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+  },
 })
+
+
 
 export const blockClient = new ApolloClient({
   uri: 'https://rollux.graph.pegasys.fi/subgraphs/name/pollum-io/syscoin-blocks',
