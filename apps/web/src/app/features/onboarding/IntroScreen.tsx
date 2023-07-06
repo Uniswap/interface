f53017223d4dfdb6514c57d7fc3bf60afa006ab8
Daniel James <danielcolinjames@gmail.com>
2023-07-06 12:14:00 -0400

    chore(ext): design updates on intro screen (#3967)

--- apps/web/src/app/features/onboarding/IntroScreen.tsx
+++ apps/web/src/app/features/onboarding/IntroScreen.tsx
@@ -4,46 +4,12 @@ import {
   ImportOnboardingRoutes,
   OnboardingRoutes,
 } from 'src/app/navigation/constants'
-import { Circle, Icons, Image, Stack, Text, XStack, YStack } from 'ui/src'
+import { Image, Stack, Text, XStack, YStack } from 'ui/src'
 import { UNISWAP_LOGO } from 'ui/src/assets'
 import { Button } from 'ui/src/components/button/Button'
-import { Flex } from 'ui/src/components/layout/Flex'
-import { opacify } from 'ui/src/theme/color/utils'
 import { iconSizes } from 'ui/src/theme/iconSizes'
 
-function InfoRow({
-  primaryText,
-  secondaryText,
-  backgroundColor,
-  icon,
-}: {
-  primaryText: string
-  secondaryText: string
-  backgroundColor: string
-  icon: JSX.Element
-}): JSX.Element {
-  return (
-    <XStack alignItems="center" gap="$spacing12">
-      <Stack alignItems="center" justifyContent="center">
-        <Circle backgroundColor={backgroundColor} opacity={0.2} size={40} />
-        <Flex
-          alignItems="center"
-          height={40}
-          justifyContent="center"
-          position="absolute"
-          width={40}>
-          {icon}
-        </Flex>
-      </Stack>
-      <YStack>
-        <Text variant="subheadLarge">{primaryText}</Text>
-        <Text color="$textTertiary" variant="bodySmall">
-          {secondaryText}
-        </Text>
-      </YStack>
-    </XStack>
-  )
-}
+const INTRO_SCREEN_WIDTH = 320
 
 export function IntroScreen(): JSX.Element {
   const navigate = useNavigate()
@@ -54,8 +20,8 @@ export function IntroScreen(): JSX.Element {
       gap={100}
       justifyContent="center"
       marginBottom="$spacing60"
-      width="100%">
-      <YStack gap="$spacing12" maxWidth={320}>
+      minWidth={INTRO_SCREEN_WIDTH}>
+      <YStack gap="$spacing12" width="100%">
         <Stack alignItems="center" justifyContent="center" paddingBottom="$spacing8">
           <Stack
             alignItems="center"
@@ -72,17 +38,14 @@ export function IntroScreen(): JSX.Element {
             />
           </Stack>
         </Stack>
-        <Text marginBottom="$spacing48" textAlign="center" variant="headlineMedium">
-          Get started with Uniswap Wallet
-        </Text>
-        <Button
-          flexGrow={1}
-          theme="secondary"
-          onPress={(): void =>
-            navigate(`${OnboardingRoutes.Import}/${ImportOnboardingRoutes.Password}`)
-          }>
-          I already have a wallet
-        </Button>
+        <Stack marginBottom="$spacing48">
+          <Text textAlign="center" variant="headlineMedium">
+            Welcome to
+          </Text>
+          <Text color="$magentaVibrant" textAlign="center" variant="headlineMedium">
+            Uniswap Wallet
+          </Text>
+        </Stack>
         <Button
           flexGrow={1}
           theme="primary"
@@ -91,26 +54,14 @@ export function IntroScreen(): JSX.Element {
           }>
           Create a new wallet
         </Button>
-      </YStack>
-      <YStack alignItems="flex-start" gap="$spacing24" justifyContent="center">
-        <InfoRow
-          backgroundColor={opacify(10, '$green200')}
-          icon={<Icons.Bolt size="$icon.20" />}
-          primaryText="Supercharge your swaps"
-          secondaryText="1-click checkout"
-        />
-        <InfoRow
-          backgroundColor="$pink200"
-          icon={<Icons.Multichain size="$icon.20" />}
-          primaryText="Multichain experience"
-          secondaryText="Say goodbye to switching chains"
-        />
-        <InfoRow
-          backgroundColor="$blue200"
-          icon={<Icons.Book size="$icon.20" />}
-          primaryText="Human readable transactions"
-          secondaryText="Blockchain activity you can understand"
-        />
+        <Button
+          flexGrow={1}
+          theme="secondary"
+          onPress={(): void =>
+            navigate(`${OnboardingRoutes.Import}/${ImportOnboardingRoutes.Password}`)
+          }>
+          I already have a wallet
+        </Button>
       </YStack>
     </XStack>
   )
