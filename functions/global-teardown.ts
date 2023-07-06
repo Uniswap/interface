c29604e3148bbd1cdc676dd4fdd8e9c4a3882e4f
Brendan Wong <bjwpokemon@gmail.com>
2023-07-06 14:27:32 -0400

    changing typing of globalThis servers

--- functions/global-teardown.ts
+++ functions/global-teardown.ts
@@ -1,5 +1,5 @@
 import { teardown } from 'jest-dev-server'
 
 module.exports = async function globalTeardown() {
-  await teardown(globalThis.servers)
+  await teardown(await globalThis.servers)
 }
--- functions/global.d.ts
+++ functions/global.d.ts
@@ -1,6 +1,6 @@
-import { SpawndChildProcess } from 'spawnd'
+import { setup } from 'jest-dev-server'
 
 declare global {
   // eslint-disable-next-line no-var
-  var servers: SpawndChildProcess[]
+  var servers: ReturnType<typeof setup>
 }
