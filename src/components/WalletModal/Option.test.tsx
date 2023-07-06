9e732db31809a3a500e1f41ef5cae8d01a583f78
Vignesh Mohankumar <me@vig.xyz>
2023-07-06 14:17:47 -0400

    fix test

--- src/components/WalletModal/Option.test.tsx
+++ src/components/WalletModal/Option.test.tsx
@@ -23,7 +23,7 @@ const mockConnection1: Connection = {
     deactivate: jest.fn(),
   } as unknown as Connector,
   getIcon: () => UNIWALLET_ICON,
-  type: ConnectionType.UNISWAP_WALLET,
+  type: ConnectionType.UNISWAP_WALLET_V2,
 } as unknown as Connection
 
 const mockConnection2: Connection = {
