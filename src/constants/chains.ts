5c0a9ebbd501a601912dc8f55d8c04a614da6d4b
Charlie B <charles@bachmeier.io>
2023-07-06 11:17:41 -0700

    remove extra uses of mumbai

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -52,8 +52,6 @@ export const TESTNET_CHAIN_IDS = [
   ChainId.POLYGON_MUMBAI,
   ChainId.ARBITRUM_GOERLI,
   ChainId.OPTIMISM_GOERLI,
-  ChainId.POLYGON_MUMBAI,
-  ChainId.POLYGON_MUMBAI,
   ChainId.CELO_ALFAJORES,
 ] as const
 
@@ -71,7 +69,7 @@ export const L1_CHAIN_IDS = [
   ChainId.BNB,
 ] as const
 
-export type SupportedL1ChainId = (typeof L1_CHAIN_IDS)[number]
+export type SupportedL1ChainId = typeof L1_CHAIN_IDS[number]
 
 /**
  * Controls some L2 specific behavior, e.g. slippage tolerance, special UI behavior.
@@ -84,7 +82,7 @@ export const L2_CHAIN_IDS = [
   ChainId.OPTIMISM_GOERLI,
 ] as const
 
-export type SupportedL2ChainId = (typeof L2_CHAIN_IDS)[number]
+export type SupportedL2ChainId = typeof L2_CHAIN_IDS[number]
 
 export function isPolygonChain(chainId: number): chainId is ChainId.POLYGON | ChainId.POLYGON_MUMBAI {
   return chainId === ChainId.POLYGON || chainId === ChainId.POLYGON_MUMBAI
