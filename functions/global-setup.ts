abebcb3d9740960e0c0f4af823e15a2d0bd08c84
Brendan Wong <bjwpokemon@gmail.com>
2023-07-07 10:47:38 -0400

    final touches

--- functions/global-setup.ts
+++ functions/global-setup.ts
@@ -2,7 +2,7 @@ import { setup } from 'jest-dev-server'
 
 module.exports = async function globalSetup() {
   globalThis.servers = await setup({
-    command: `yarn start:wrangler`,
+    command: `yarn start:cloud`,
     port: 3000,
     launchTimeout: 50000,
   })
