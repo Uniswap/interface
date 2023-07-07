3158484dec9d4c3af8e0372c1a46d4ea79ede53f
Charlie B <charles@bachmeier.io>
2023-07-06 18:41:24 -0700

    readonly

--- src/components/FeeSelector/shared.tsx
+++ src/components/FeeSelector/shared.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react'
 
 export const FEE_AMOUNT_DETAIL: Record<
   FeeAmount,
-  { label: string; description: ReactNode; supportedChains: ChainId[] }
+  { label: string; description: ReactNode; supportedChains: readonly ChainId[] }
 > = {
   [FeeAmount.LOWEST]: {
     label: '0.01',
@@ -25,16 +25,16 @@ export const FEE_AMOUNT_DETAIL: Record<
   [FeeAmount.LOW]: {
     label: '0.05',
     description: <Trans>Best for stable pairs.</Trans>,
-    supportedChains: [...SUPPORTED_CHAINS],
+    supportedChains: SUPPORTED_CHAINS,
   },
   [FeeAmount.MEDIUM]: {
     label: '0.3',
     description: <Trans>Best for most pairs.</Trans>,
-    supportedChains: [...SUPPORTED_CHAINS],
+    supportedChains: SUPPORTED_CHAINS,
   },
   [FeeAmount.HIGH]: {
     label: '1',
     description: <Trans>Best for exotic pairs.</Trans>,
-    supportedChains: [...SUPPORTED_CHAINS],
+    supportedChains: SUPPORTED_CHAINS,
   },
 }
