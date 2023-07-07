34cc64f2bda98bf298ad0439459028eb646b7f3e
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-07-07 10:34:39 -0400

    Update functions/global.d.ts
    Co-authored-by: Zach Pomerantz <zzmp@uniswap.org>

--- functions/global.d.ts
+++ functions/global.d.ts
@@ -2,5 +2,5 @@ import { setup } from 'jest-dev-server'
 
 declare global {
   // eslint-disable-next-line no-var
-  var servers: ReturnType<typeof setup>
+  var servers: Awaited<ReturnType<typeof setup>>
 }
