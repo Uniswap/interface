3b4e09bf4c699c90b0d9ea55ae56d9b51a56ff55
Brendan Wong <bjwpokemon@gmail.com>
2023-07-10 13:56:47 -0400

    remove shadow and gap on mobile

--- src/components/NavBar/NavDropdown.css.ts
+++ src/components/NavBar/NavDropdown.css.ts
@@ -11,9 +11,7 @@ const baseNavDropdown = style([
     paddingBottom: '8',
     paddingTop: '8',
   }),
-  {
-    boxShadow: '0px 4px 12px 0px #00000026',
-  },
+  {},
 ])
 
 export const NavDropdown = style([
@@ -22,7 +20,7 @@ export const NavDropdown = style([
     position: 'absolute',
     borderRadius: '12',
   }),
-  {},
+  { boxShadow: '0px 4px 12px 0px #00000026' },
 ])
 
 export const mobileNavDropdown = style([
@@ -32,7 +30,7 @@ export const mobileNavDropdown = style([
     borderTopRightRadius: '12',
     borderTopLeftRadius: '12',
     top: 'unset',
-    bottom: '56',
+    bottom: '50',
     left: '0',
     right: '0',
     width: 'full',
