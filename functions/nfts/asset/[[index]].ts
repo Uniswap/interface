1e7376bd1505b2b234e1af2b3cc23097210a5d02
Brendan Wong <bjwpokemon@gmail.com>
2023-07-13 14:03:26 -0400

    feat: parallelize cache?

--- functions/nfts/asset/[[index]].ts
+++ functions/nfts/asset/[[index]].ts
@@ -6,22 +6,22 @@ export const onRequest: PagesFunction = async ({ params, request, next }) => {
   const { index } = params
   const collectionAddress = String(index[0])
   const tokenId = String(index[1])
+  const cache = caches.default
   const assetPromise = getAsset(collectionAddress, tokenId, request.url)
   const resPromise = next()
+  const cachePromise = cache.match(request.url)
   try {
-    const cache = caches.default
-    const response = await cache.match(request.url)
-    if (response) {
-      const data = JSON.parse(await response.text())
-      return new HTMLRewriter().on('head', new MetaTagInjector(data)).transform(await resPromise)
+    const [graphData, cacheResponse, res] = await Promise.all([assetPromise, cachePromise, resPromise])
+    if (cacheResponse) {
+      const cacheData = JSON.parse(await cacheResponse.text())
+      return new HTMLRewriter().on('head', new MetaTagInjector(cacheData)).transform(res)
     } else {
-      const [data, res] = await Promise.all([assetPromise, resPromise])
-      if (!data) {
+      if (!graphData) {
         return resPromise
       }
-      const response = new Response(JSON.stringify(data))
+      const response = new Response(JSON.stringify(graphData))
       await cache.put(request.url, response)
-      return new HTMLRewriter().on('head', new MetaTagInjector(data)).transform(res)
+      return new HTMLRewriter().on('head', new MetaTagInjector(graphData)).transform(res)
     }
   } catch (e) {
     return resPromise
--- functions/tokens/[[index]].ts
+++ functions/tokens/[[index]].ts
@@ -10,22 +10,22 @@ export const onRequest: PagesFunction = async ({ params, request, next }) => {
     tokenAddress !== 'undefined' && tokenAddress === 'NATIVE'
       ? '0x0000000000000000000000000000000000000000'
       : tokenAddress
+  const cache = caches.default
   const tokenPromise = getToken(networkName, tokenAddress, request.url)
   const resPromise = next()
+  const cachePromise = cache.match(request.url)
   try {
-    const cache = caches.default
-    const response = await cache.match(request.url)
-    if (response) {
-      const data = JSON.parse(await response.text())
-      return new HTMLRewriter().on('head', new MetaTagInjector(data)).transform(await resPromise)
+    const [graphData, cacheResponse, res] = await Promise.all([tokenPromise, cachePromise, resPromise])
+    if (cacheResponse) {
+      const cacheData = JSON.parse(await cacheResponse.text())
+      return new HTMLRewriter().on('head', new MetaTagInjector(cacheData)).transform(res)
     } else {
-      const [data, res] = await Promise.all([tokenPromise, resPromise])
-      if (!data) {
+      if (!graphData) {
         return resPromise
       }
-      const response = new Response(JSON.stringify(data))
+      const response = new Response(JSON.stringify(graphData))
       await cache.put(request.url, response)
-      return new HTMLRewriter().on('head', new MetaTagInjector(data)).transform(res)
+      return new HTMLRewriter().on('head', new MetaTagInjector(graphData)).transform(res)
     }
   } catch (e) {
     return resPromise
