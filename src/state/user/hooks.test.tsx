6650479aeded0993224e407aa0c8e364dc1875a3
Zach Pomerantz <zzmp@uniswap.org>
2023-07-14 14:23:38 -0700

    test: useUserDisabledUniswapX

--- src/state/user/hooks.test.tsx
+++ src/state/user/hooks.test.tsx
@@ -5,7 +5,13 @@ import store from 'state'
 import { RouterPreference } from 'state/routing/slice'
 import { renderHook } from 'test-utils/render'
 
-import { deserializeToken, serializeToken, useRouterPreference, useUserSlippageTolerance } from './hooks'
+import {
+  deserializeToken,
+  serializeToken,
+  useRouterPreference,
+  useUserDisabledUniswapX,
+  useUserSlippageTolerance,
+} from './hooks'
 import { updateUserSlippageTolerance } from './reducer'
 import { SlippageTolerance } from './types'
 
@@ -73,5 +79,14 @@ describe('useRouterPreference', () => {
       },
     } = renderHook(() => useRouterPreference())
     expect(routerPreference).toBe(RouterPreference.API)
+  })
+})
+
+describe('useUserDisabledUniswapX', () => {
+  it('returns `false` by default', () => {
+    const {
+      result: { current: disabledUniswapX },
+    } = renderHook(() => useUserDisabledUniswapX())
+    expect(disabledUniswapX).toBe(false)
   })
 })
