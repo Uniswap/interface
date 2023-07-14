4fbe4872598fed821b2d6e3c26550a82ee47d179
Gary Ye <garysye@gmail.com>
2023-07-14 20:59:51 +0200

    fix(mob): Updates support link to route to support home page (#4075)

--- apps/mobile/src/constants/urls.ts
+++ apps/mobile/src/constants/urls.ts
@@ -5,7 +5,7 @@ export const APP_STORE_LINK = 'https://apps.apple.com/us/app/uniswap-wallet/id64
 export const APP_FEEDBACK_LINK =
   'https://docs.google.com/forms/d/e/1FAIpQLSepzL5aMuSfRhSgw0zDw_gVmc2aeVevfrb1UbOwn6WGJ--46w/viewform'
 
-export const GET_HELP_LINK = `${uniswapUrls.helpUrl}/hc/en-us/categories/11301970439565-Uniswap-Wallet`
+export const GET_HELP_LINK = `${uniswapUrls.helpUrl}`
 
 export const RECOVERY_PHRASE_HELP_URL = `${uniswapUrls.helpUrl}/hc/en-us/articles/11380692567949-How-to-import-my-recovery-phrase-`
 
