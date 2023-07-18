aec4c5a44a4738c2e34389feda24d043f6dc9a18
Charles Bachmeier <charles@bachmeier.io>
2023-07-18 15:05:39 -0700

    fix: Update Dependencies to Re-Enable Avalanche and BNB Routing (#6973)
    * compiles
    
    * base not supported
    
    * latest uniswapx-sdk version
    
    * correct provider
    
    * updated cypress-hardhat

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -109,6 +109,8 @@ export const FALLBACK_URLS = {
     'https://endpoints.omniatech.io/v1/avax/mainnet/public',
     'https://ava-mainnet.public.blastapi.io/ext/bc/C/rpc',
   ],
+  [ChainId.BASE]: [],
+  [ChainId.BASE_GOERLI]: [],
 }
 
 /**
@@ -141,4 +143,6 @@ export const RPC_URLS = {
   [ChainId.CELO_ALFAJORES]: FALLBACK_URLS[ChainId.CELO_ALFAJORES],
   [ChainId.BNB]: [QUICKNODE_RPC_URL, ...FALLBACK_URLS[ChainId.BNB]],
   [ChainId.AVALANCHE]: [`https://avalanche-mainnet.infura.io/v3/${INFURA_KEY}`, ...FALLBACK_URLS[ChainId.AVALANCHE]],
+  [ChainId.BASE]: [],
+  [ChainId.BASE_GOERLI]: [],
 }
