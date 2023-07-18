dbd02496401aab65e599aba796ff2276e7bb1130
brenno <brennoaraujoqueiroz@gmail.com>
2023-07-18 15:58:31 -0300

    fix: lingui error

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -3,12 +3,12 @@ export const SUPPORTED_LOCALES = [
   'en-US',
   'es-ES',
 ]
-export type SupportedLocale = (typeof SUPPORTED_LOCALES)[number] | 'pseudo'
+
+export type SupportedLocale = (typeof SUPPORTED_LOCALES)[number]
 
 export const DEFAULT_LOCALE: SupportedLocale = 'en-US'
 
 export const LOCALE_LABEL: { [locale in SupportedLocale]: string } = {
   'en-US': 'English',
   'es-ES': 'Español',
-  pseudo: 'ƥƨèúδô',
 }
