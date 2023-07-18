2a6866590cca78dcee84b993f311039097ca24dd
Charlie B <charles@bachmeier.io>
2023-07-18 16:26:28 -0700

    add avax safe fallbacks

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -103,11 +103,7 @@ export const FALLBACK_URLS = {
   [ChainId.AVALANCHE]: [
     // "Safe" URLs
     'https://api.avax.network/ext/bc/C/rpc',
-    'https://rpc.ankr.com/avalanche',
-    'https://avalanche.blockpi.network/v1/rpc/public',
     'https://avalanche-c-chain.publicnode.com',
-    'https://endpoints.omniatech.io/v1/avax/mainnet/public',
-    'https://ava-mainnet.public.blastapi.io/ext/bc/C/rpc',
   ],
   [ChainId.BASE]: [
     'https://developer-access-mainnet.base.org',
