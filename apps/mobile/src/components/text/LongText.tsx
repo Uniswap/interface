4e589424e1e86bd0d76f4d8d5e52ed7dbf23b28a
Adrian Kant <adrian.kant@uniswap.org>
2023-07-24 17:05:27 -0400

    fix(mobile): iOS long text rendering (#4121)

--- apps/mobile/src/components/text/LongText.tsx
+++ apps/mobile/src/components/text/LongText.tsx
@@ -105,7 +105,7 @@ export function LongText(props: LongTextProps): JSX.Element {
             variant={variant}
             onTextLayout={onTextLayout}
             {...rest}>
-            text
+            {text}
           </Text>
         )}
       </Box>
