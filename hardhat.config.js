796dcbbce2e4c13e4ee9620d715ce441281ddd50
Charlie B <charles@bachmeier.io>
2023-07-24 14:33:19 -0700

    update ur sdk

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,6 @@ import { UNIVERSAL_ROUTER_CREATION_BLOCK } from '@uniswap/universal-router-sdk'
 /* eslint-env node */
 require('dotenv').config()
 
-// Block selection is arbitrary, as e2e tests will build up their own state.
-// The only requirement is that all infrastructure under test (eg Permit2 contracts) are already deployed.
-// TODO(WEB-2187): Make more dynamic to avoid manually updating
-// const BLOCK_NUMBER = 17693163
-// const POLYGON_BLOCK_NUMBER = 43600000
-
 const forkingConfig = {
   httpHeaders: {
     Origin: 'localhost:3000', // infura allowlists requests by origin
