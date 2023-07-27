8ed6481f1603ab453f1eae1d06d95bf504ca5bae
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-07-27 13:34:26 -0400

    fix: display ens name/avatar on other chains (#6981)
    * maintain ens info on other chains
    
    * update ens hook
    
    * feedback!
    
    * initial test
    
    * test progress
    
    * e2e test?
    
    * final feedback
    
    * fix(lint): rm unused test var
    
    * fix final bugs
    
    * last lint issue
    
    * fix: update catches
    
    * fix: define catch handlers
    
    * fix: use error in catch
    
    * empty commit
    
    ---------
    
    Co-authored-by: Zach Pomerantz <zzmp@uniswap.org>

--- cypress/e2e/mini-portfolio/accounts.test.ts
+++ cypress/e2e/mini-portfolio/accounts.test.ts
@@ -77,4 +77,36 @@ describe('Mini Portfolio account drawer', () => {
         })
     })
   })
+
+  it('fetches ENS name', () => {
+    cy.hardhat().then(() => {
+      const haydenAccount = '0x50EC05ADe8280758E2077fcBC08D878D4aef79C3'
+      const haydenENS = 'hayden.eth'
+
+      // Opens the account drawer
+      cy.get(getTestSelector('web3-status-connected')).click()
+
+      // Simulate wallet changing to Hayden's account
+      cy.window().then((win) => win.ethereum.emit('accountsChanged', [haydenAccount]))
+
+      // Hayden's ENS name should be shown
+      cy.contains(haydenENS).should('exist')
+
+      // Close account drawer
+      cy.get(getTestSelector('close-account-drawer')).click()
+
+      // Switch chain to Polygon
+      cy.get(getTestSelector('chain-selector')).eq(1).click()
+      cy.contains('Polygon').click()
+
+      //Reopen account drawer
+      cy.get(getTestSelector('web3-status-connected')).click()
+
+      // Simulate wallet changing to Hayden's account
+      cy.window().then((win) => win.ethereum.emit('accountsChanged', [haydenAccount]))
+
+      // Hayden's ENS name should be shown
+      cy.contains(haydenENS).should('exist')
+    })
+  })
 })
