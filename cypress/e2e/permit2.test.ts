e791c15e378e19d8adc5eb7e780adece9671e773
Zach Pomerantz <zzmp@uniswap.org>
2023-07-27 14:54:27 -0700

    fix: update mainnet block

--- cypress/e2e/permit2.test.ts
+++ cypress/e2e/permit2.test.ts
@@ -29,7 +29,7 @@ describe('Permit2', () => {
     cy.hardhat()
       .then(({ approval, wallet }) => approval.getTokenAllowanceForPermit2({ owner: wallet, token: inputToken }))
       .then((allowance) => {
-        Cypress.log({ name: `Token allowace: ${allowance.toString()}` })
+        Cypress.log({ name: `Token allowance: ${allowance.toString()}` })
         cy.wrap(allowance).should('deep.equal', MaxUint256)
       })
   }
@@ -39,7 +39,7 @@ describe('Permit2', () => {
     cy.hardhat()
       .then(({ approval, wallet }) => approval.getPermit2Allowance({ owner: wallet, token: inputToken }))
       .then((allowance) => {
-        Cypress.log({ name: `Permit2 allowace: ${allowance.amount.toString()}` })
+        Cypress.log({ name: `Permit2 allowance: ${allowance.amount.toString()}` })
         cy.wrap(allowance.amount).should('deep.equal', MaxUint160)
         // Asserts that the on-chain expiration is in 30 days, within a tolerance of 40 seconds.
         const THIRTY_DAYS_SECONDS = 2_592_000
