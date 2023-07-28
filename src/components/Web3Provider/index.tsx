ef3467913f828bcfbd57daccf786c5deb710639d
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-07-28 16:45:58 -0400

    fix: clearing input on connect wallet (#6928)
    * fix: clearing input on connect wallet
    
    * update e2e tests

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -10,7 +10,7 @@ import { TraceJsonRpcVariant, useTraceJsonRpcFlag } from 'featureFlags/flags/tra
 import useEagerlyConnect from 'hooks/useEagerlyConnect'
 import useOrderedConnections from 'hooks/useOrderedConnections'
 import usePrevious from 'hooks/usePrevious'
-import { ReactNode, useEffect, useMemo, useState } from 'react'
+import { ReactNode, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useConnectedWallets } from 'state/wallets/hooks'
 import { getCurrentPageFromLocation } from 'utils/urlRoutes'
@@ -20,16 +20,8 @@ export default function Web3Provider({ children }: { children: ReactNode }) {
   const connections = useOrderedConnections()
   const connectors: [Connector, Web3ReactHooks][] = connections.map(({ hooks, connector }) => [connector, hooks])
 
-  // Force a re-render when our connection state changes.
-  const [index, setIndex] = useState(0)
-  useEffect(() => setIndex((index) => index + 1), [connections])
-  const key = useMemo(
-    () => connections.map((connection) => connection.getName()).join('-') + index,
-    [connections, index]
-  )
-
   return (
-    <Web3ReactProvider connectors={connectors} key={key}>
+    <Web3ReactProvider connectors={connectors}>
       <Updater />
       {children}
     </Web3ReactProvider>
