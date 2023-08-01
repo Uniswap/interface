34468c79ae299e4a9234e20f1dbe61867b67fd05
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-08-01 13:41:43 +0200

    fix(mob): Fix local images not being loaded on Android (#4177)

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -4,6 +4,8 @@
  *
  * @format
  */
+const { getMetroAndroidAssetsResolutionFix } = require('react-native-monorepo-tools')
+const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix()
 
 process.env.TAMAGUI_TARGET = 'native'
 
@@ -33,6 +35,12 @@ module.exports = (async () => {
         },
       }),
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      publicPath: androidAssetsResolutionFix.publicPath,
+    },
+    server: {
+      enhanceMiddleware: (middleware) => {
+        return androidAssetsResolutionFix.applyMiddleware(middleware)
+      },
     },
     watchFolders,
   }
