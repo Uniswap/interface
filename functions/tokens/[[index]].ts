b2d53897d32d8841a2ff8fe43f7662fef9635329
Brendan Wong <bjwpokemon@gmail.com>
2023-08-02 18:08:20 -0400

    fix: failing token cloud function tests

--- functions/tokens/[[index]].ts
+++ functions/tokens/[[index]].ts
@@ -2,17 +2,28 @@
 import { MetaTagInjector } from '../components/metaTagInjector'
 import getToken from '../utils/getToken'
 
-const convertTokenAddress = (tokenAddress: string) => {
-  return tokenAddress && tokenAddress === 'NATIVE' ? '0x0000000000000000000000000000000000000000' : tokenAddress
+const convertTokenAddress = (tokenAddress: string, networkName: string) => {
+  if (tokenAddress === 'NATIVE') {
+    switch (networkName) {
+      case 'CELO':
+        return '0x471EcE3750Da237f93B8E339c536989b8978a438'
+      case 'POLYGON':
+        return '0x0000000000000000000000000000000000001010'
+      default:
+        return undefined
+    }
+  }
+  return tokenAddress
 }
 
 export const onRequest: PagesFunction = async ({ params, request, next }) => {
   const { index } = params
   const networkName = index[0]?.toString().toUpperCase()
-  const tokenAddress = convertTokenAddress(index[1]?.toString())
-  if (!tokenAddress) {
+  const tokenString = index[1]?.toString()
+  if (!tokenString) {
     return next()
   }
+  const tokenAddress = convertTokenAddress(tokenString, networkName)
   const tokenPromise = getToken(networkName, tokenAddress, request.url)
   const resPromise = next()
   try {
