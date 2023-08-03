7e358ffe5fc292c90a53e388c879baaf0f8931dd
Jack Short <john.short.tj@gmail.com>
2023-08-03 17:44:49 -0400

    req reply

--- cypress/e2e/swap/errors.test.ts
+++ cypress/e2e/swap/errors.test.ts
@@ -120,7 +120,12 @@ describe('Swap errors', () => {
   it('insufficient liquidity', () => {
     cy.visit(`/swap?inputCurrency=${USDC_MAINNET.address}&outputCurrency=${DAI.address}`)
     // The API response is too variable so stubbing a 404.
-    cy.intercept('/v2/quote', { statusCode: 404, fixture: 'insufficientLiquidity.json' })
+    cy.intercept('/v2/quote', (req) => {
+      req.reply({
+        statusCode: 404,
+        fixture: 'insufficientLiquidity.json',
+      })
+    })
     cy.get('#swap-currency-output .token-amount-input').type('100000000000000').should('have.value', '100000000000000') // 100 trillion
     cy.contains('Insufficient liquidity for this trade.')
     cy.get('#swap-button').should('not.exist')
