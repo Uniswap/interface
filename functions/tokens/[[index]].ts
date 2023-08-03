cd43e0beaa8c4631ffc66ea5ff5c142a7b98a7cd
Brendan Wong <35351983+LunrEclipse@users.noreply.github.com>
2023-08-03 12:30:50 -0400

    fix: failing token cloud function tests (#7074)
    * fix: failing token cloud function tests
    
    * utilize enum from chains

--- functions/tokens/[[index]].ts
+++ functions/tokens/[[index]].ts
@@ -1,18 +1,30 @@
 /* eslint-disable import/no-unused-modules */
+import { Chain } from '../../src/graphql/data/__generated__/types-and-hooks'
 import { MetaTagInjector } from '../components/metaTagInjector'
 import getToken from '../utils/getToken'
 
-const convertTokenAddress = (tokenAddress: string) => {
-  return tokenAddress && tokenAddress === 'NATIVE' ? '0x0000000000000000000000000000000000000000' : tokenAddress
+const convertTokenAddress = (tokenAddress: string, networkName: string) => {
+  if (tokenAddress === 'NATIVE') {
+    switch (networkName) {
+      case Chain.Celo:
+        return '0x471EcE3750Da237f93B8E339c536989b8978a438'
+      case Chain.Polygon:
+        return '0x0000000000000000000000000000000000001010'
+      default:
+        return undefined
+    }
+  }
+  return tokenAddress
 }
 
 export const onRequest: PagesFunction = async ({ params, request, next }) => {
   const { index } = params
   const networkName = index[0]?.toString().toUpperCase()
-  const tokenAddress = convertTokenAddress(index[1]?.toString())
-  if (!tokenAddress) {
+  const tokenString = index[1]?.toString()
+  if (!tokenString) {
     return next()
   }
+  const tokenAddress = convertTokenAddress(tokenString, networkName)
   const tokenPromise = getToken(networkName, tokenAddress, request.url)
   const resPromise = next()
   try {
