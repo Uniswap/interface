89cff3afb815f5e5db005347f20812b83e047057
Jack Short <john.short.tj@gmail.com>
2023-08-04 13:50:58 -0400

    adding back timeout

--- cypress/e2e/swap/errors.test.ts
+++ cypress/e2e/swap/errors.test.ts
@@ -118,14 +118,10 @@ describe('Swap errors', () => {
   })
 
   it('insufficient liquidity', () => {
-    cy.intercept('POST', 'https://api.uniswap.org/v2/quote', {
-      statusCode: 404,
-      fixture: 'insufficientLiquidity.json',
-    })
     cy.visit(`/swap?inputCurrency=${USDC_MAINNET.address}&outputCurrency=${DAI.address}`)
-    // The API response is too variable so stubbing a 404.
     cy.get('#swap-currency-output .token-amount-input').type('100000000000000').should('have.value', '100000000000000') // 100 trillion
-    cy.contains('Insufficient liquidity for this trade.')
+    // The API takes a long and variable amount of time to return a 404, so setting the timeout to 60 seconds.
+    cy.contains('Insufficient liquidity for this trade.', { timeout: 60000 })
     cy.get('#swap-button').should('not.exist')
   })
 })
