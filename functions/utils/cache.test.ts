88249535abf1f69c290acb73bf8a2d9e078089ef
Brendan Wong <bjwpokemon@gmail.com>
2023-08-04 13:35:52 -0400

    final final final feedback!

--- functions/utils/cache.test.ts
+++ functions/utils/cache.test.ts
@@ -1,6 +1,6 @@
 import CacheMock from 'browser-cache-mock'
 
-//import { mocked } from '../../src/test-utils/mocked'
+import { mocked } from '../../src/test-utils/mocked'
 import Cache from './cache'
 
 const cacheMock = new CacheMock()
@@ -21,17 +21,24 @@ beforeAll(() => {
 
 test('Should put cache properly', async () => {
   jest.spyOn(cacheMock, 'put')
-  const response = await Cache.match('https://example.com')
-  expect(response).toBeUndefined()
+  jest.spyOn(cacheMock, 'match')
   await Cache.put(data, 'https://example.com')
   expect(cacheMock.put).toHaveBeenCalledWith('https://example.com', expect.anything())
+  const call = mocked(cacheMock.put).mock.calls[0]
+  const response = JSON.parse(await (call[1] as Response).clone().text())
+  expect(response).toStrictEqual(data)
 
-  const response2 = await Cache.match('https://example.com')
-  expect(response2).toStrictEqual(data)
+  await expect(Cache.match('https://example.com')).resolves.toStrictEqual(data)
 })
 
 test('Should match cache properly', async () => {
   jest.spyOn(cacheMock, 'match').mockResolvedValueOnce(new Response(JSON.stringify(data)))
   const response = await Cache.match('https://example.com')
   expect(response).toStrictEqual(data)
+})
+
+test('Should return undefined if not all data is present', async () => {
+  jest.spyOn(cacheMock, 'match').mockResolvedValueOnce(new Response(JSON.stringify({ ...data, title: undefined })))
+  const response = await Cache.match('https://example.com')
+  expect(response).toBeUndefined()
 })
--- functions/utils/cache.ts
+++ functions/utils/cache.ts
@@ -6,11 +6,11 @@ interface Data {
 
 class Cache {
   async match(request: string): Promise<Data | undefined> {
-    const cache = await caches.open('function-cache')
+    const cache = await caches.open('functions-cache')
     const response = await cache.match(request)
     if (!response) return undefined
     const data: Data = JSON.parse(await response.text())
-    if (!data.title || !data.image || !data.url) throw new Error('Invalid data')
+    if (!data.title || !data.image || !data.url) return undefined
     return data
   }
 
@@ -18,7 +18,7 @@ class Cache {
     // Set max age to 1 week
     const response = new Response(JSON.stringify(data))
     response.headers.set('Cache-Control', 'max-age=604800')
-    const cache = await caches.open('function-cache')
+    const cache = await caches.open('functions-cache')
     await cache.put(request, response)
   }
 }
