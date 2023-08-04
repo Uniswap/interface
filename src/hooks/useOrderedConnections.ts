b7663857221629445671521c76b8340806d6e330
eddie <66155195+just-toby@users.noreply.github.com>
2023-08-04 15:07:12 -0700

    fix: revert #6928 (#7089)

--- src/hooks/useOrderedConnections.ts
+++ src/hooks/useOrderedConnections.ts
@@ -1,6 +1,7 @@
 import { getConnection } from 'connection'
 import { ConnectionType } from 'connection/types'
 import { useMemo } from 'react'
+import { useAppSelector } from 'state/hooks'
 
 const SELECTABLE_WALLETS = [
   ConnectionType.UNISWAP_WALLET_V2,
@@ -10,17 +11,23 @@ const SELECTABLE_WALLETS = [
 ]
 
 export default function useOrderedConnections() {
+  const selectedWallet = useAppSelector((state) => state.user.selectedWallet)
+
   return useMemo(() => {
     const orderedConnectionTypes: ConnectionType[] = []
 
     // Always attempt to use to Gnosis Safe first, as we can't know if we're in a SafeContext.
     orderedConnectionTypes.push(ConnectionType.GNOSIS_SAFE)
 
-    orderedConnectionTypes.push(...SELECTABLE_WALLETS)
+    // Add the `selectedWallet` to the top so it's prioritized, then add the other selectable wallets.
+    if (selectedWallet) {
+      orderedConnectionTypes.push(selectedWallet)
+    }
+    orderedConnectionTypes.push(...SELECTABLE_WALLETS.filter((wallet) => wallet !== selectedWallet))
 
     // Add network connection last as it should be the fallback.
     orderedConnectionTypes.push(ConnectionType.NETWORK)
 
     return orderedConnectionTypes.map((connectionType) => getConnection(connectionType))
-  }, [])
+  }, [selectedWallet])
 }
