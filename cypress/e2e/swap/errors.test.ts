b8575310994e5a6759327ddc16a7b468f761040e
Jack Short <john.short.tj@gmail.com>
2023-08-07 14:43:20 -0400

    in describe block

--- cypress/e2e/swap/errors.test.ts
+++ cypress/e2e/swap/errors.test.ts
@@ -116,12 +116,17 @@ describe('Swap errors', () => {
       getBalance(DAI).should('be.closeTo', initialBalance + 200, 1)
     })
   })
+})
 
-  it('insufficient liquidity', () => {
+describe('Swap errors (no liquidity)', () => {
+  beforeEach(() => {
     cy.intercept('POST', 'https://api.uniswap.org/v2/quote', {
       statusCode: 404,
       fixture: 'insufficientLiquidity.json',
     })
+  })
+
+  it('insufficient liquidity', () => {
     cy.visit(`/swap?inputCurrency=${USDC_MAINNET.address}&outputCurrency=${DAI.address}`)
     // The API response is too variable so stubbing a 404.
     cy.get('#swap-currency-output .token-amount-input').type('100000000000000').should('have.value', '100000000000000') // 100 trillion
