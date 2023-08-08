0341ffd0a340a85533595a7b496676cd25796824
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-08-08 10:20:47 -0500

    fix: add back correct fellback setter

--- src/state/routing/slice.ts
+++ src/state/routing/slice.ts
@@ -128,8 +128,9 @@ export const routingApi = createApi({
         )
       },
       async queryFn(args, _api, _extraOptions, fetch) {
-        const fellBack = false
+        let fellBack = false
         if (shouldUseAPIRouter(args)) {
+          fellBack = true
           try {
             const {
               tokenInAddress,
