9fcccf67a1df7bb4438decbc7f778fc456946584
Jack Short <john.short.tj@gmail.com>
2023-08-09 12:43:45 -0400

    removing eslint

--- src/featureFlags/flags/currencyConversion.ts
+++ src/featureFlags/flags/currencyConversion.ts
@@ -1,6 +1,5 @@
 import { BaseVariant, FeatureFlag, useBaseFlag } from '../index'
 
-// eslint-disable-next-line import/no-unused-modules
 export function useCurrencyConversionFlag(): BaseVariant {
   return useBaseFlag(FeatureFlag.currencyConversion)
 }
