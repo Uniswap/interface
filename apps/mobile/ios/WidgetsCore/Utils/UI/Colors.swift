054e255134bf71df5fa78c632d8bc0931b0fc8f0
Eric Huang <91709373+erichuang27@users.noreply.github.com>
2023-08-10 10:40:48 -0400

    feat(MOB): hardcode colors for widget and graph (#4253)

--- apps/mobile/ios/WidgetsCore/Utils/UI/Colors.swift
+++ apps/mobile/ios/WidgetsCore/Utils/UI/Colors.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 import UIImageColors
+import OSLog
 
 public extension Color {
   static let widgetGrey = Color(red: 0.96, green: 0.96, blue: 0.99).opacity(0.5)
@@ -23,7 +24,6 @@ public extension Color {
   static let PEPE = Color(red: 0.24, green: 0.68, blue: 0.08)
   static let UNI = Color(red: 0.90, green: 0.21, blue: 0.55)
   static let UNIBOT = Color(red: 0.29, green: 0.05, blue: 0.31)
-  static let USDT = Color(red: 0.31, green: 0.7, blue: 0.59)
   static let USDC = Color(red: 0, green: 0.4, blue: 0.85)
   static let HEX = Color(red: 0.97, green: 0.25, blue: 0.55)
   static let MONG = Color(red: 0.64, green: 0.34, blue: 1)
@@ -38,7 +38,14 @@ public extension Color {
   static let AIDOGE = Color(red: 0.16, green: 0.63, blue: 0.94)
   static let SIMPSON = Color(red: 0.91, green: 0.57, blue: 0)
   static let RENQ = Color(red: 0.18, green: 0.52, blue: 1)
+  static let LUSD = Color(red: 1, green: 0.48, blue: 0.31)
+  static let OX = Color(red: 0.16, green: 0.35, blue: 0.85)
+  static let ANGLE = Color(red: 1, green: 0.33, blue: 0.33)
   static let APE = Color(red: 0.01, green: 0.29, blue: 0.84)
+  static let GUSD = Color(red: 0, green: 0.64, blue: 0.74)
+  static let OGN = Color(red: 0.01, green: 0.29, blue: 0.84)
+  static let GALA = Color.black
+  static let RPL = Color(red: 1, green: 0.48, blue: 0.31)
   
   static let magentaVibrant = Color(red: 0.99, green: 0.45, blue: 1.00)
   static let backgroundGray = Color.gray
@@ -103,12 +110,12 @@ public struct ColorExtraction {
     // BUSD
     "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x4Fabb145d64652a948d72533023f6E7A623C7C53/logo.png":
       UIColor(Color.BUSD),
-    // AI-X
-    "https://s2.coinmarketcap.com/static/img/coins/64x64/26984.png":
-      UIColor(Color.X),
-    // ETH
-    "https://token-icons.s3.amazonaws.com/eth.png":
-      UIColor(Color.ETH),
+      // AI-X
+      "https://s2.coinmarketcap.com/static/img/coins/64x64/26984.png":
+        UIColor(Color.X),
+      // ETH
+      "https://token-icons.s3.amazonaws.com/eth.png":
+        UIColor(Color.ETH),
     // HARRYPOTTERSHIBAINUBITCOIN
     "https://assets.coingecko.com/coins/images/30323/large/hpos10i_logo_casino_night-dexview.png?1684117567":
       UIColor(Color.HARRYPOTTERBITCOIN),
@@ -124,9 +131,6 @@ public struct ColorExtraction {
     // UNIBOT v1
     "https://assets.coingecko.com/coins/images/30462/small/logonoline_%281%29.png?1687510315":
       UIColor(Color.UNIBOT),
-    // USDT
-    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png":
-      UIColor(Color.USDT),
     // USDC
     "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png":
       UIColor(Color.USDC),
@@ -139,6 +143,48 @@ public struct ColorExtraction {
     // ARB
     "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1/logo.png":
       UIColor(Color.ARB),
+    // Quant
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x4a220E6096B25EADb88358cb44068A3248254675/logo.png":
+      UIColor(Color.QNT),
+    // Xen
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8/logo.png":
+      UIColor(Color.XEN),
+    // PSYOP
+    "https://s2.coinmarketcap.com/static/img/coins/64x64/25422.png":
+      UIColor(Color.PSYOP),
+    // MATIC
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png":
+      UIColor(Color.MATIC),
+    // TURBO
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xA35923162C49cF95e6BF26623385eb431ad920D3/logo.png":
+      UIColor(Color.TURBO),
+    // AIDOGE
+    "https://assets.coingecko.com/coins/images/29852/large/photo_2023-04-18_14-25-28.jpg?1681799160":
+      UIColor(Color.AIDOGE),
+    // SIMPSON
+    "https://assets.coingecko.com/coins/images/30243/large/1111.png?1683692033":
+      UIColor(Color.SIMPSON),
+    // LUSD
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2/logo.png":
+      UIColor(Color.LUSD),
+    // OX
+    "https://assets.coingecko.com/coins/images/30604/large/Logo2.png?1685522119":
+      UIColor(Color.OX),
+    // ANGLE
+    "https://assets.coingecko.com/coins/images/19060/large/ANGLE_Token-light.png?1666774221":
+      UIColor(Color.ANGLE),
+    // GUSD
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd/logo.png":
+      UIColor(Color.GUSD),
+    // OGN
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x8207c1FfC5B6804F6024322CcF34F29c3541Ae26/logo.png":
+      UIColor(Color.OGN),
+    // GALA
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0x15D4c048F83bd7e37d49eA4C83a07267Ec4203dA/logo.png":
+      UIColor(Color.GALA),
+    // RPL
+    "https://raw.githubusercontent.com/Uniswap/assets/master/blockchains/ethereum/assets/0xD33526068D116cE69F19A9ee46F0bd304F21A51f/logo.png":
+      UIColor(Color.RPL)
     
   ]
   
