435d7f7dedad8835c99f425ea9052e74b48fce14
brenno <brennoaraujoqueiroz@gmail.com>
2023-08-11 20:24:33 -0300

    feat: customSwitch component

--- src/components/CustomSwitch/CustomSwitch.tsx
+++ src/components/CustomSwitch/CustomSwitch.tsx
@@ -1,43 +1,61 @@
-import { Box } from 'rebass'
+import { Trans } from '@lingui/macro'
+import Row from 'components/Row'
+import { useAtom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
+import ms from 'ms.macro'
+import { useCallback } from 'react'
+import { Segment, SegmentedControl } from 'theme/components/SegmentedControl'
 
 interface SwitchItems {
-  text: string
+  text: number
   onClick: () => void
   condition: boolean
 }
 
 interface CustomSwitchProps {
-  width: number | string
-  height: number
+  width?: number | string
+  height?: string
   items: SwitchItems[]
   isLarge?: boolean
+  disabled?: boolean
 }
 
-export default function CustomSwitch({ width, height, items, isLarge }: CustomSwitchProps) {
+enum SwitchMode {
+  active,
+  ended,
+}
+
+const themeModeAtom = atomWithStorage<SwitchMode>('switch-farm', SwitchMode.active)
+const THEME_UPDATE_DELAY = ms`0.1s`
+
+export default function CustomSwitch({ disabled, items }: CustomSwitchProps) {
+  const [mode, setMode] = useAtom(themeModeAtom)
+
+  const switchMode = useCallback(
+    (mode: SwitchMode) => {
+      const item = items.find((item) => item.text === mode)
+      !disabled &&
+        item &&
+        setTimeout(() => {
+          setMode(mode)
+          item.onClick()
+        }, THEME_UPDATE_DELAY)
+    },
+    [disabled, items, setMode]
+  )
+
   return (
-    <Box display="flex" width={width} height={height}>
-      {items.map((item, index) => {
-        const returnBorderRadius = (ind: number) => {
-          return index === ind ? 8 : 0 // this makes border radius for individual switch item
-        }
-        return (
-          <Box
-            key={index}
-            className={`switchItem${item.condition ? ' activeSwitchItem' : ''}
-        `}
-            style={{
-              // makes left border radius for the first switch item and right border radius for the last switch item
-              borderTopLeftRadius: returnBorderRadius(0),
-              borderBottomLeftRadius: returnBorderRadius(0),
-              borderTopRightRadius: returnBorderRadius(items.length - 1),
-              borderBottomRightRadius: returnBorderRadius(items.length - 1),
-            }}
-            onClick={item.onClick}
-          >
-            <p className={isLarge ? '' : 'small'}>{item.text}</p>
-          </Box>
-        )
-      })}
-    </Box>
+    <Row align="center">
+      <Row flexGrow={1} justify="flex-end" width="100%">
+        <SegmentedControl selected={mode} onSelect={switchMode}>
+          <Segment value={SwitchMode.active} testId="theme-auto">
+            <Trans>Active</Trans>
+          </Segment>
+          <Segment value={SwitchMode.ended} testId="theme-lightmode">
+            <Trans>Ended</Trans>
+          </Segment>
+        </SegmentedControl>
+      </Row>
+    </Row>
   )
 }
