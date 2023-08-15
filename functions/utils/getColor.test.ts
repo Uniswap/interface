e3106f4a1742719914b634b3566bd81313bb7ff0
Brendan Wong <bjwpokemon@gmail.com>
2023-08-15 10:31:15 -0400

    Update getColor.test.ts

--- functions/utils/getColor.test.ts
+++ functions/utils/getColor.test.ts
@@ -22,7 +22,7 @@ test('should return the average color of a white PNG image', async () => {
 test('should return the average color of a white PNG image with whiteness dimmed', async () => {
   const image = 'https://www.cac.cornell.edu/wiki/images/4/44/White_square.png'
   const color = await getColor(image, true)
-  expect(color).toEqual([235, 235, 235])
+  expect(color).toEqual([226, 226, 226])
 })
 
 test('should return the average color of a black JPG image', async () => {
