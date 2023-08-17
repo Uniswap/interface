e6aa428e8aed999fdc9b23acce8276a2312ec509
Ivan Sorokin <ivan@sorokin.io>
2023-08-17 21:35:44 +0200

    fix(MOB): No tokens in token selector for empty wallets (#4327)

--- apps/mobile/src/components/TokenSelector/hooks.tsx
+++ apps/mobile/src/components/TokenSelector/hooks.tsx
@@ -197,7 +197,7 @@ export function usePortfolioTokenOptions(
   })
 
   const portfolioBalances = useMemo(
-    () => (shownTokens ? sortPortfolioBalances(shownTokens) : undefined),
+    () => (shownTokens ? sortPortfolioBalances(shownTokens) : []),
     [shownTokens]
   )
 
