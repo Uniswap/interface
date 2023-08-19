caa9822d02d54f348b95bed2d5b27037aec750b2
patrickroyme <patrickroyme@gmail.com>
2023-08-19 10:24:23 +0300

    Initial changes.

--- functions/[[index]].ts
+++ functions/[[index]].ts
@@ -4,10 +4,10 @@ import { MetaTagInjector } from './components/metaTagInjector'
 export const onRequest: PagesFunction = async ({ request, next }) => {
   const imageUri = new URL(request.url).origin + '/images/1200x630_Rich_Link_Preview_Image.png'
   const data = {
-    title: 'Uniswap Interface',
+    title: 'Kinetix Interface',
     image: imageUri,
     url: request.url,
-    description: 'Swap or provide liquidity on the Uniswap Protocol',
+    description: 'Swap or provide liquidity on the Kinetix Protocol',
   }
   const res = next()
   try {
