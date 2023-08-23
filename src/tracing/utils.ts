4aad0e531642ed38c12de2ba78f526f01d91ac11
eddie <66155195+just-toby@users.noreply.github.com>
2023-08-23 09:28:12 -0700

    fix: null check on elapsedTime for some browsers

--- src/tracing/utils.ts
+++ src/tracing/utils.ts
@@ -2,7 +2,8 @@
  * Returns the time elapsed between page load and now.
  * @param markName the identifier for the performance mark to be created and measured.
  */
-export function calculateElapsedTimeWithPerformanceMark(markName: string): number {
+export function calculateElapsedTimeWithPerformanceMark(markName: string): number | undefined {
   const elapsedTime = performance.mark(markName)
+  if (!elapsedTime) return undefined
   return elapsedTime.startTime
 }
