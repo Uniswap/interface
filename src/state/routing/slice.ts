d4845f054ff7b3010bad4b239394066b7e37ce46
eddie <66155195+just-toby@users.noreply.github.com>
2023-08-30 09:46:45 -0700

    fix: remove useUniswapX param

--- src/state/routing/slice.ts
+++ src/state/routing/slice.ts
@@ -138,7 +138,6 @@ export const routingApi = createApi({
               tokenOut: tokenOutAddress,
               amount,
               type,
-              useUniswapX: true,
               configs: getRoutingAPIConfig(args),
             }
 
