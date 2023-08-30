6ac43797d2604818540ce33683a74a4939ffd593
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-08-30 00:22:01 -0500

    add mainnet quicknode example, use old staticJsonRpc extension

--- src/constants/providers.ts
+++ src/constants/providers.ts
@@ -1,162 +1,68 @@
-import { StaticJsonRpcProvider } from '@ethersproject/providers'
-import { ChainId, SupportedChainsType } from '@uniswap/sdk-core'
+import { ChainId } from '@uniswap/sdk-core'
 
 import AppRpcProvider from './appProvider'
-import { CHAIN_IDS_TO_NAMES } from './chains'
+import { SupportedInterfaceChain } from './chains'
+import { AppStaticJsonRpcProvider } from './extendedStaticJsonRpcProvider'
 import { RPC_URLS } from './networks'
 
 /**
  * These are the only JsonRpcProviders used directly by the interface.
  */
-export const RPC_PROVIDERS: { [key in SupportedChainsType]: AppRpcProvider } = {
+export const RPC_PROVIDERS: { [key in SupportedInterfaceChain]: AppRpcProvider } = {
   [ChainId.MAINNET]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.MAINNET][0], {
-      chainId: ChainId.MAINNET,
-      name: CHAIN_IDS_TO_NAMES[ChainId.MAINNET],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.MAINNET][1], {
-      chainId: ChainId.MAINNET,
-      name: CHAIN_IDS_TO_NAMES[ChainId.MAINNET],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.MAINNET, RPC_URLS[ChainId.MAINNET][0]),
+    new AppStaticJsonRpcProvider(ChainId.MAINNET, RPC_URLS[ChainId.MAINNET][1]),
   ]),
   [ChainId.GOERLI]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.GOERLI][0], {
-      chainId: ChainId.GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.GOERLI],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.GOERLI][1], {
-      chainId: ChainId.GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.GOERLI],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.GOERLI, RPC_URLS[ChainId.GOERLI][0]),
+    new AppStaticJsonRpcProvider(ChainId.GOERLI, RPC_URLS[ChainId.GOERLI][1]),
   ]),
   [ChainId.SEPOLIA]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.SEPOLIA][0], {
-      chainId: ChainId.SEPOLIA,
-      name: CHAIN_IDS_TO_NAMES[ChainId.SEPOLIA],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.SEPOLIA][1], {
-      chainId: ChainId.SEPOLIA,
-      name: CHAIN_IDS_TO_NAMES[ChainId.SEPOLIA],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.SEPOLIA, RPC_URLS[ChainId.SEPOLIA][0]),
+    new AppStaticJsonRpcProvider(ChainId.SEPOLIA, RPC_URLS[ChainId.SEPOLIA][1]),
   ]),
   [ChainId.OPTIMISM]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.OPTIMISM][0], {
-      chainId: ChainId.OPTIMISM,
-      name: CHAIN_IDS_TO_NAMES[ChainId.OPTIMISM],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.OPTIMISM][1], {
-      chainId: ChainId.OPTIMISM,
-      name: CHAIN_IDS_TO_NAMES[ChainId.OPTIMISM],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.OPTIMISM, RPC_URLS[ChainId.OPTIMISM][0]),
+    new AppStaticJsonRpcProvider(ChainId.OPTIMISM, RPC_URLS[ChainId.OPTIMISM][1]),
   ]),
   [ChainId.OPTIMISM_GOERLI]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.OPTIMISM_GOERLI][0], {
-      chainId: ChainId.OPTIMISM_GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.OPTIMISM_GOERLI],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.OPTIMISM_GOERLI][1], {
-      chainId: ChainId.OPTIMISM_GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.OPTIMISM_GOERLI],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.OPTIMISM_GOERLI, RPC_URLS[ChainId.OPTIMISM_GOERLI][0]),
+    new AppStaticJsonRpcProvider(ChainId.OPTIMISM_GOERLI, RPC_URLS[ChainId.OPTIMISM_GOERLI][1]),
   ]),
   [ChainId.ARBITRUM_ONE]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.ARBITRUM_ONE][0], {
-      chainId: ChainId.ARBITRUM_ONE,
-      name: CHAIN_IDS_TO_NAMES[ChainId.ARBITRUM_ONE],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.ARBITRUM_ONE][1], {
-      chainId: ChainId.ARBITRUM_ONE,
-      name: CHAIN_IDS_TO_NAMES[ChainId.ARBITRUM_ONE],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.ARBITRUM_ONE, RPC_URLS[ChainId.ARBITRUM_ONE][0]),
+    new AppStaticJsonRpcProvider(ChainId.ARBITRUM_ONE, RPC_URLS[ChainId.ARBITRUM_ONE][1]),
   ]),
   [ChainId.ARBITRUM_GOERLI]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.ARBITRUM_GOERLI][0], {
-      chainId: ChainId.ARBITRUM_GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.ARBITRUM_GOERLI],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.ARBITRUM_GOERLI][1], {
-      chainId: ChainId.ARBITRUM_GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.ARBITRUM_GOERLI],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.ARBITRUM_GOERLI, RPC_URLS[ChainId.ARBITRUM_GOERLI][0]),
+    new AppStaticJsonRpcProvider(ChainId.ARBITRUM_GOERLI, RPC_URLS[ChainId.ARBITRUM_GOERLI][1]),
   ]),
   [ChainId.POLYGON]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.POLYGON][0], {
-      chainId: ChainId.POLYGON,
-      name: CHAIN_IDS_TO_NAMES[ChainId.POLYGON],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.POLYGON][1], {
-      chainId: ChainId.POLYGON,
-      name: CHAIN_IDS_TO_NAMES[ChainId.POLYGON],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.POLYGON, RPC_URLS[ChainId.POLYGON][0]),
+    new AppStaticJsonRpcProvider(ChainId.POLYGON, RPC_URLS[ChainId.POLYGON][1]),
   ]),
   [ChainId.POLYGON_MUMBAI]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.POLYGON_MUMBAI][0], {
-      chainId: ChainId.POLYGON_MUMBAI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.POLYGON_MUMBAI],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.POLYGON_MUMBAI][1], {
-      chainId: ChainId.POLYGON_MUMBAI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.POLYGON_MUMBAI],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.POLYGON_MUMBAI, RPC_URLS[ChainId.POLYGON_MUMBAI][0]),
+    new AppStaticJsonRpcProvider(ChainId.POLYGON_MUMBAI, RPC_URLS[ChainId.POLYGON_MUMBAI][1]),
   ]),
   [ChainId.CELO]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.CELO][0], {
-      chainId: ChainId.CELO,
-      name: CHAIN_IDS_TO_NAMES[ChainId.CELO],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.CELO][1], {
-      chainId: ChainId.CELO,
-      name: CHAIN_IDS_TO_NAMES[ChainId.CELO],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.CELO, RPC_URLS[ChainId.CELO][0]),
+    new AppStaticJsonRpcProvider(ChainId.CELO, RPC_URLS[ChainId.CELO][1]),
   ]),
   [ChainId.CELO_ALFAJORES]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.CELO_ALFAJORES][0], {
-      chainId: ChainId.CELO_ALFAJORES,
-      name: CHAIN_IDS_TO_NAMES[ChainId.CELO_ALFAJORES],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.CELO_ALFAJORES][1], {
-      chainId: ChainId.CELO_ALFAJORES,
-      name: CHAIN_IDS_TO_NAMES[ChainId.CELO_ALFAJORES],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.CELO_ALFAJORES, RPC_URLS[ChainId.CELO_ALFAJORES][0]),
+    new AppStaticJsonRpcProvider(ChainId.CELO_ALFAJORES, RPC_URLS[ChainId.CELO_ALFAJORES][1]),
   ]),
   [ChainId.BNB]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.BNB][0], {
-      chainId: ChainId.BNB,
-      name: CHAIN_IDS_TO_NAMES[ChainId.BNB],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.BNB][1], {
-      chainId: ChainId.BNB,
-      name: CHAIN_IDS_TO_NAMES[ChainId.BNB],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.BNB, RPC_URLS[ChainId.BNB][0]),
+    new AppStaticJsonRpcProvider(ChainId.BNB, RPC_URLS[ChainId.BNB][1]),
   ]),
   [ChainId.AVALANCHE]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.AVALANCHE][0], {
-      chainId: ChainId.AVALANCHE,
-      name: CHAIN_IDS_TO_NAMES[ChainId.AVALANCHE],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.AVALANCHE][1], {
-      chainId: ChainId.AVALANCHE,
-      name: CHAIN_IDS_TO_NAMES[ChainId.AVALANCHE],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.AVALANCHE, RPC_URLS[ChainId.AVALANCHE][0]),
+    new AppStaticJsonRpcProvider(ChainId.AVALANCHE, RPC_URLS[ChainId.AVALANCHE][1]),
   ]),
   [ChainId.BASE]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.BASE][0], {
-      chainId: ChainId.BASE,
-      name: CHAIN_IDS_TO_NAMES[ChainId.BASE],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.BASE][1], {
-      chainId: ChainId.BASE,
-      name: CHAIN_IDS_TO_NAMES[ChainId.BASE],
-    }),
-  ]),
-  [ChainId.BASE_GOERLI]: new AppRpcProvider([
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.BASE_GOERLI][0], {
-      chainId: ChainId.BASE_GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.BASE_GOERLI],
-    }),
-    new StaticJsonRpcProvider(RPC_URLS[ChainId.BASE_GOERLI][1], {
-      chainId: ChainId.BASE_GOERLI,
-      name: CHAIN_IDS_TO_NAMES[ChainId.BASE_GOERLI],
-    }),
+    new AppStaticJsonRpcProvider(ChainId.BASE, RPC_URLS[ChainId.BASE][0]),
+    new AppStaticJsonRpcProvider(ChainId.BASE, RPC_URLS[ChainId.BASE][1]),
   ]),
 }
