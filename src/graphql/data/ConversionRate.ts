7a18441e5e26efce2d4ab82fcaafd863eea49002
Jack Short <john.short.tj@gmail.com>
2023-08-31 14:09:17 -0400

    5m cache policy

--- src/graphql/data/ConversionRate.ts
+++ src/graphql/data/ConversionRate.ts
@@ -1,11 +1,25 @@
+import { SupportedLocalCurrency } from 'constants/localCurrencies'
 import gql from 'graphql-tag'
+import ms from 'ms'
+import { getFetchPolicyForKey } from 'utils/getFetchPolicyForKey'
+
+import { useConvertQuery } from './__generated__/types-and-hooks'
 
 gql`
-  query Convert {
-    convert(fromAmount: { currency: USD, value: 1.0 }, toCurrency: JPY) {
+  query Convert($toCurrency: Currency!) {
+    convert(fromAmount: { currency: USD, value: 1.0 }, toCurrency: $toCurrency) {
       id
       value
       currency
     }
   }
 `
+
+export function useLocalCurrencyConversionRate(localCurrency: SupportedLocalCurrency): number | undefined {
+  const { data } = useConvertQuery({
+    variables: { toCurrency: localCurrency },
+    fetchPolicy: getFetchPolicyForKey(`convert-${localCurrency}`, ms('5m')),
+  })
+
+  return data?.convert?.value
+}
