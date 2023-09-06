e18c1554d86b1074f1dbb40a4fd7401ee820e0be
cartcrom <cartergcromer@gmail.com>
2023-09-06 14:14:22 -0400

    fix: gate FOT UI in swap component

--- src/state/routing/types.ts
+++ src/state/routing/types.ts
@@ -46,7 +46,6 @@ export interface GetQuoteArgs {
   uniswapXEthOutputEnabled: boolean
   uniswapXExactOutputEnabled: boolean
   userDisabledUniswapX: boolean
-  fotAdjustmentsEnabled: boolean
   inputTax: Percent
   outputTax: Percent
 }
