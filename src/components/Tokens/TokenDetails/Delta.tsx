29a6b01bac3c46e6736dd3f05abfe4996ee9e403
cartcrom <cartergcromer@gmail.com>
2023-09-08 13:19:55 -0400

    fix: svg prop + snapshots failing unit tests

--- src/components/Tokens/TokenDetails/Delta.tsx
+++ src/components/Tokens/TokenDetails/Delta.tsx
@@ -2,11 +2,11 @@ import { ArrowChangeDown } from 'components/Icons/ArrowChangeDown'
 import { ArrowChangeUp } from 'components/Icons/ArrowChangeUp'
 import styled from 'styled-components'
 
-const StyledUpArrow = styled(ArrowChangeUp)<{ noColor?: boolean }>`
-  color: ${({ theme, noColor }) => (noColor ? theme.neutral2 : theme.success)};
+const StyledUpArrow = styled(ArrowChangeUp)<{ $noColor?: boolean }>`
+  color: ${({ theme, $noColor }) => ($noColor ? theme.neutral2 : theme.success)};
 `
-const StyledDownArrow = styled(ArrowChangeDown)<{ noColor?: boolean }>`
-  color: ${({ theme, noColor }) => (noColor ? theme.neutral2 : theme.critical)};
+const StyledDownArrow = styled(ArrowChangeDown)<{ $noColor?: boolean }>`
+  color: ${({ theme, $noColor }) => ($noColor ? theme.neutral2 : theme.critical)};
 `
 
 export function calculateDelta(start: number, current: number) {
@@ -35,9 +35,9 @@ export function DeltaArrow({ delta, noColor = false, size = 16 }: DeltaArrowProp
   if (!isValidDelta(delta)) return null
 
   return Math.sign(delta) < 0 ? (
-    <StyledDownArrow width={size} height={size} key="arrow-down" aria-label="down" noColor={noColor} />
+    <StyledDownArrow width={size} height={size} key="arrow-down" aria-label="down" $noColor={noColor} />
   ) : (
-    <StyledUpArrow width={size} height={size} key="arrow-up" aria-label="up" noColor={noColor} />
+    <StyledUpArrow width={size} height={size} key="arrow-up" aria-label="up" $noColor={noColor} />
   )
 }
 
