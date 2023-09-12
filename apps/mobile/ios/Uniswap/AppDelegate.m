2e8b0a03638012584be38d85b539443bb0741209
Ivan Sorokin <ivan@sorokin.io>
2023-09-12 18:38:41 +0200

    fix(MOB): flipper network module (#4486)

--- apps/mobile/ios/Uniswap/AppDelegate.m
+++ apps/mobile/ios/Uniswap/AppDelegate.m
@@ -6,29 +6,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <ReactNativePerformance/ReactNativePerformance.h>
-
+#import <React/RCTAppSetupUtils.h>
 #import "RNSplashScreen.h"
-
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#import <FlipperPerformancePlugin.h>
-
-static void InitializeFlipper(UIApplication *application) {
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client addPlugin:[FlipperPerformancePlugin new]];
-  [client start];
-}
-#endif
 
 @implementation AppDelegate
 
@@ -36,10 +15,6 @@ static void InitializeFlipper(UIApplication *application) {
 {
   // must be first line in startup routine
   [ReactNativePerformance onAppStarted];
-
-#ifdef FB_SONARKIT_ENABLED
-  InitializeFlipper(application);
-#endif
 
  [FIRApp configure];
 
