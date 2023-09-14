ec44dac4cdfbd26eebc5bcc1e97f16e68112c8d5
eddie <66155195+just-toby@users.noreply.github.com>
2023-09-14 14:19:45 -0700

    fix: use one wrapper

--- src/pages/AddLiquidityV2/index.tsx
+++ src/pages/AddLiquidityV2/index.tsx
@@ -225,44 +225,46 @@ export default function AddLiquidity() {
   }
 
   const modalHeader = () => {
-    return noLiquidity ? (
+    return (
       <AddLiquidityHeaderContainer>
-        <LightCard mt="20px" $borderRadius="20px">
-          <AutoRow justify="space-between">
-            <Text fontSize="24px" fontWeight={535} lineHeight="42px" marginRight={10}>
-              {currencies[Field.CURRENCY_A]?.symbol + '/' + currencies[Field.CURRENCY_B]?.symbol}
-            </Text>
-            <DoubleCurrencyLogo
-              currency0={currencies[Field.CURRENCY_A]}
-              currency1={currencies[Field.CURRENCY_B]}
-              size={30}
-            />
-          </AutoRow>
-        </LightCard>
-      </AddLiquidityHeaderContainer>
-    ) : (
-      <AddLiquidityHeaderContainer>
-        <RowFlat style={{ marginTop: '20px' }}>
-          <Text fontSize="48px" fontWeight={535} lineHeight="42px" marginRight={10}>
-            {liquidityMinted?.toSignificant(6)}
-          </Text>
-          <DoubleCurrencyLogo
-            currency0={currencies[Field.CURRENCY_A]}
-            currency1={currencies[Field.CURRENCY_B]}
-            size={30}
-          />
-        </RowFlat>
-        <Row>
-          <Text fontSize="24px">
-            {currencies[Field.CURRENCY_A]?.symbol + '/' + currencies[Field.CURRENCY_B]?.symbol + ' Pool Tokens'}
-          </Text>
-        </Row>
-        <ThemedText.DeprecatedItalic fontSize={12} textAlign="left" padding="8px 0 0 0 ">
-          <Trans>
-            Output is estimated. If the price changes by more than {allowedSlippage.toSignificant(4)}% your transaction
-            will revert.
-          </Trans>
-        </ThemedText.DeprecatedItalic>
+        {noLiquidity ? (
+          <LightCard mt="20px" $borderRadius="20px">
+            <AutoRow justify="space-between">
+              <Text fontSize="24px" fontWeight={535} lineHeight="42px" marginRight={10}>
+                {currencies[Field.CURRENCY_A]?.symbol + '/' + currencies[Field.CURRENCY_B]?.symbol}
+              </Text>
+              <DoubleCurrencyLogo
+                currency0={currencies[Field.CURRENCY_A]}
+                currency1={currencies[Field.CURRENCY_B]}
+                size={30}
+              />
+            </AutoRow>
+          </LightCard>
+        ) : (
+          <>
+            <RowFlat style={{ marginTop: '20px' }}>
+              <Text fontSize="48px" fontWeight={535} lineHeight="42px" marginRight={10}>
+                {liquidityMinted?.toSignificant(6)}
+              </Text>
+              <DoubleCurrencyLogo
+                currency0={currencies[Field.CURRENCY_A]}
+                currency1={currencies[Field.CURRENCY_B]}
+                size={30}
+              />
+            </RowFlat>
+            <Row>
+              <Text fontSize="24px">
+                {currencies[Field.CURRENCY_A]?.symbol + '/' + currencies[Field.CURRENCY_B]?.symbol + ' Pool Tokens'}
+              </Text>
+            </Row>
+            <ThemedText.DeprecatedItalic fontSize={12} textAlign="left" padding="8px 0 0 0 ">
+              <Trans>
+                Output is estimated. If the price changes by more than {allowedSlippage.toSignificant(4)}% your
+                transaction will revert.
+              </Trans>
+            </ThemedText.DeprecatedItalic>
+          </>
+        )}
       </AddLiquidityHeaderContainer>
     )
   }
