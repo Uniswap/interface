13a9d28b31f74e0aea78cfa6136b8e694edaff06
cartcrom <cartergcromer@gmail.com>
2023-09-14 10:41:38 -0400

    refactor: relocate pricechart test file

--- src/components/Charts/PriceChart/index.test.tsx
+++ src/components/Charts/PriceChart/index.test.tsx
@@ -1,7 +1,7 @@
 import { TimePeriod } from 'graphql/data/util'
 import { render, screen } from 'test-utils/render'
 
-import { PriceChart } from './PriceChart'
+import { PriceChart } from '.'
 
 jest.mock('components/Charts/AnimatedInLineChart', () => ({
   __esModule: true,
