598873fbf0d4ef9fd97a6f17811f9a1d00688b83
Zach Pomerantz <zzmp@uniswap.org>
2023-09-18 12:12:37 -0700

    feat: connectionReady

--- src/connection/eagerlyConnect.ts
+++ src/connection/eagerlyConnect.ts
@@ -3,6 +3,13 @@ import { Connector } from '@web3-react/types'
 import { getConnection, gnosisSafeConnection, networkConnection } from './index'
 import { ConnectionType, selectedWalletKey, toConnectionType } from './types'
 
+/**
+ * This may be thrown (as a promise) by components to suspend rendering until a wallet connection is attempted.
+ * @example
+ *     if (connectionReady instanceof Promise) throw connectionReady
+ */
+export let connectionReady: Promise<void> | true
+
 async function connect(connector: Connector, type: ConnectionType) {
   performance.mark(`web3:connect:${type}:start`)
   try {
@@ -26,11 +33,15 @@ const selectedWallet = toConnectionType(localStorage.getItem(selectedWalletKey) 
 if (selectedWallet) {
   const selectedConnection = getConnection(selectedWallet)
   if (selectedConnection) {
-    connect(selectedConnection.connector, selectedWallet).then((connected) => {
-      if (!connected) {
-        // only clear the persisted wallet type if it failed to connect.
-        localStorage.removeItem(selectedWalletKey)
-      }
-    })
+    connectionReady = connect(selectedConnection.connector, selectedWallet)
+      .then((connected) => {
+        if (!connected) {
+          // only clear the persisted wallet type if it failed to connect.
+          localStorage.removeItem(selectedWalletKey)
+        }
+      })
+      .finally(() => {
+        connectionReady = true
+      })
   }
 }
