71ccf087aef6a982a18f8082b22b15a045c27f66
Thomas Thachil <thomasthachil@users.noreply.github.com>
2023-09-19 13:15:32 -0400

    chore(mob): clean up ethers-rs for kotlin (#4351)

--- apps/mobile/android/app/src/main/java/com/uniswap/RnEthersRs.kt
+++ apps/mobile/android/app/src/main/java/com/uniswap/RnEthersRs.kt
@@ -1,8 +1,15 @@
 package com.uniswap
+
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
+import com.uniswap.EthersRs.generateMnemonic
+import com.uniswap.EthersRs.privateKeyFromMnemonic
+import com.uniswap.EthersRs.signHashWithWallet
+import com.uniswap.EthersRs.signMessageWithWallet
+import com.uniswap.EthersRs.signTxWithWallet
+import com.uniswap.EthersRs.walletFromPrivateKey
 
 class RnEthersRs(applicationContext: Context) {
 
@@ -45,9 +52,7 @@ class RnEthersRs(applicationContext: Context) {
     val mnemonic = generateMnemonic()
     val mnemonicStr = mnemonic.mnemonic
     val addressStr = mnemonic.address
-    val res = storeNewMnemonic(mnemonicStr, addressStr)
-    mnemonicFree(mnemonic)
-    return res
+    return storeNewMnemonic(mnemonicStr, addressStr)
   }
 
   /**
@@ -92,9 +97,7 @@ class RnEthersRs(applicationContext: Context) {
    */
   fun generateAddressForMnemonic(mnemonic: String, derivationIndex: Int): String {
     val privateKey = privateKeyFromMnemonic(mnemonic, derivationIndex)
-    val address = privateKey.address
-    privateKeyFree(privateKey)
-    return address
+    return privateKey.address
   }
 
   /**
@@ -109,7 +112,6 @@ class RnEthersRs(applicationContext: Context) {
     val xprv = privateKey.privateKey
     val address = privateKey.address
     storeNewPrivateKey(address, xprv)
-    privateKeyFree(privateKey)
     return address
   }
 
@@ -186,121 +188,5 @@ class RnEthersRs(applicationContext: Context) {
     private const val MNEMONIC_PREFIX = ".mnemonic."
     private const val PRIVATE_KEY_PREFIX = ".privateKey."
     private const val ENTIRE_MNEMONIC_PREFIX = PREFIX + MNEMONIC_PREFIX
-
-    /**
-     * Generates a mnemonic and its associated address.
-     * @return A CMnemonicAndAddress object containing the generated mnemonic and its associated address.
-     */
-    private external fun generateMnemonic(): CMnemonicAndAddress
-
-
-    /**
-     * Frees the memory allocated for the mnemonic.
-     * @param mnemonic The mnemonic to be freed.
-     */
-    private external fun mnemonicFree(mnemonic: CMnemonicAndAddress)
-
-    /**
-     * Generates a private key from a given mnemonic.
-     * @param mnemonic The mnemonic to generate the private key from.
-     * @param index The index of the private key to generate.
-     * @return A CPrivateKey object containing the generated private key.
-     */
-    private external fun privateKeyFromMnemonic(mnemonic: String?, index: Int): CPrivateKey
-
-    /**
-     * Frees the memory allocated for the private key.
-     * @param privateKey The private key to be freed.
-     */
-    private external fun privateKeyFree(privateKey: CPrivateKey)
-
-    /**
-     * Creates a wallet from a given private key.
-     * @param privateKey The private key to create the wallet from.
-     * @return A long representing the pointer to the created wallet.
-     */
-    private external fun walletFromPrivateKey(privateKey: String?): Long
-
-    /**
-     * Frees the memory allocated for the wallet.
-     * @param walletPtr The pointer to the wallet to be freed.
-     */
-    private external fun walletFree(walletPtr: Long)
-
-    /**
-     * Signs a transaction with a wallet.
-     * @param localWallet The wallet to sign the transaction with.
-     * @param txHash The transaction hash to sign.
-     * @param chainId The id of the blockchain network.
-     * @return A signed transaction hash.
-     */
-    private external fun signTxWithWallet(
-      localWallet: Long,
-      txHash: String,
-      chainId: Long
-    ): String
-
-    /**
-     * Signs a message with a wallet.
-     * @param localWallet The wallet to sign the message with.
-     * @param message The message to sign.
-     * @return The signed message.
-     */
-    private external fun signMessageWithWallet(
-      localWallet: Long,
-      message: String
-    ): String
-
-    /**
-     * Signs a hash with a wallet.
-     * @param localWallet The wallet to sign the hash with.
-     * @param hash The hash to sign.
-     * @param chainId The id of the blockchain network.
-     * @return The signed hash.
-     */
-    private external fun signHashWithWallet(
-      localWallet: Long,
-      hash: String,
-      chainId: Long
-    ): String
-
-  /**
-   * Frees the memory allocated for the string.
-   * @param string The string to be freed.
-   */
-    private external fun stringFree(string: String)
   }
 }
-
-/**
- * Represents a private key and its associated address.
- * @property privateKey The private key.
- * @property address The address associated with the private key.
- * @property handle This is a pointer to a Rust CPrivateKey struct.
- */
-class CPrivateKey(
-  var privateKey: String,
-  var address: String,
-  var handle: Long
-)
-
-/**
- * Represents a mnemonic and its associated address.
- * @property mnemonic The mnemonic phrase.
- * @property address The address associated with the mnemonic.
- * @property handle This is a pointer to a Rust CMnemonicAndAddress struct.
- */
-class CMnemonicAndAddress(
-  var mnemonic: String,
-  var address: String,
-  var handle: Long
-)
-
-
-/**
- * Represents a signed transaction.
- * @property signature The signature of the transaction.
- */
-internal class CSignedTransaction(
-  var signature: String
-)
