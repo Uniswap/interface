61030306bc2838a42d9c1431fb6b17d6593e990e
eddie <66155195+just-toby@users.noreply.github.com>
2023-09-19 14:13:56 -0700

    fix: unit test

--- src/components/Web3Provider/index.test.tsx
+++ src/components/Web3Provider/index.test.tsx
@@ -14,6 +14,7 @@ import { mocked } from 'test-utils/mocked'
 import Web3Provider from '.'
 
 jest.mock('analytics', () => ({
+  useTrace: jest.fn(),
   sendAnalyticsEvent: jest.fn(),
   user: { set: jest.fn(), postInsert: jest.fn() },
 }))
