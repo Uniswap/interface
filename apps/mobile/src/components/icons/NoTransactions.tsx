3086c71e508ec191a694f8d81b989d4666e0e394
Daniel James <danielcolinjames@gmail.com>
2023-09-20 11:37:41 -0400

    fix(MOB): Wrong colors on no activity asset (#4549)

--- apps/mobile/src/components/icons/NoTransactions.tsx
+++ apps/mobile/src/components/icons/NoTransactions.tsx
@@ -1,18 +1,18 @@
 import React, { memo } from 'react'
 import OverlayIcon from 'src/components/icons/OverlayIcon'
-import { Flex } from 'ui/src'
+import { Flex, useSporeColors } from 'ui/src'
 import NoTransactionFgIcon from 'ui/src/assets/icons/empty-state-coin.svg'
 import NoTransactionBgIcon from 'ui/src/assets/icons/empty-state-transaction.svg'
-import { theme } from 'ui/src/theme/restyle'
 
 export const NoTransactions = memo(function _NoTransactions() {
+  const colors = useSporeColors()
   return (
     <Flex mb="$spacing24">
       <OverlayIcon
         bottom={-23}
-        icon={<NoTransactionBgIcon color={theme.colors.neutral3} />}
+        icon={<NoTransactionBgIcon color={colors.neutral3.val} />}
         left={5}
-        overlay={<NoTransactionFgIcon color={theme.colors.neutral3} fill={theme.colors.accent1} />}
+        overlay={<NoTransactionFgIcon color={colors.neutral3.val} />}
       />
     </Flex>
   )
