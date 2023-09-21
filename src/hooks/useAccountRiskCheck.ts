6a884bedcdf0980f07c1c9514f39d5f92d821f2f
Kristie Huang <kristie.huang@uniswap.org>
2023-09-21 12:59:26 -0400

    clean code + write tests

--- src/hooks/useAccountRiskCheck.ts
+++ src/hooks/useAccountRiskCheck.ts
@@ -8,14 +8,13 @@ export default function useAccountRiskCheck(account: string | null | undefined) 
 
   useEffect(() => {
     if (account) {
-      // check local browser cache
       const riskCheckLocalStorageKey = `risk-check-${account}`
       const now = Date.now()
       try {
+        // Check local browser cache
         const storedTime = localStorage.getItem(riskCheckLocalStorageKey)
         const checkExpirationTime = storedTime ? parseInt(storedTime) : now - 1
         if (checkExpirationTime < Date.now()) {
-          // query API
           const headers = new Headers({
             'Content-Type': 'application/json',
           })
@@ -26,14 +25,12 @@ export default function useAccountRiskCheck(account: string | null | undefined) 
           })
             .then((res) => res.json())
             .then((data) => {
-              console.log('SUCCESSFUL DATA', data)
-              // if blocked, dispatch Blocked modal
               if (data.block) {
                 dispatch(setOpenModal(ApplicationModal.BLOCKED_ACCOUNT))
               }
             })
-            .catch((err) => {
-              console.log('NOT SUCCESSFUL', err)
+            .catch((e) => {
+              console.error(e)
               dispatch(setOpenModal(null))
             })
         }
@@ -44,35 +41,3 @@ export default function useAccountRiskCheck(account: string | null | undefined) 
     }
   }, [account, dispatch])
 }
-
-// export default function useAccountRiskCheck(account: string | null | undefined) {
-//   const dispatch = useAppDispatch()
-
-//   useEffect(() => {
-//     if (account) {
-//       const riskCheckLocalStorageKey = `risk-check-${account}`
-//       const now = Date.now()
-//       try {
-//         const storedTime = localStorage.getItem(riskCheckLocalStorageKey)
-//         const checkExpirationTime = storedTime ? parseInt(storedTime) : now - 1
-//         if (checkExpirationTime < Date.now()) {
-//           const headers = new Headers({ 'Content-Type': 'application/json' })
-//           fetch('https://screening-worker.uniswap.workers.dev', {
-//             method: 'POST',
-//             headers,
-//             body: JSON.stringify({ address: account }),
-//           })
-//             .then((res) => res.json())
-//             .then((data) => {
-//               if (data.block) {
-//                 dispatch(setOpenModal(ApplicationModal.BLOCKED_ACCOUNT))
-//               }
-//             })
-//             .catch(() => dispatch(setOpenModal(null)))
-//         }
-//       } finally {
-//         localStorage.setItem(riskCheckLocalStorageKey, (now + ms(`1d`)).toString())
-//       }
-//     }
-//   }, [account, dispatch])
-// }
