2b55b65066d9cef3bff1b8024d67e9ee7611eedc
Nate Wienert <natewienert@gmail.com>
2023-09-25 09:00:13 -1000

    fix(mob): fix flickering of wrong theme on background of app due to RN bug (#4606)

--- packages/wallet/src/features/appearance/hooks.tsx
+++ packages/wallet/src/features/appearance/hooks.tsx
@@ -1,4 +1,9 @@
-import { useColorScheme } from 'react-native'
+import { useEffect, useState } from 'react'
+import {
+  AppState,
+  ColorSchemeName,
+  useColorScheme as useColorSchemeReactNative,
+} from 'react-native'
 import { useAppSelector } from 'wallet/src/state'
 import { AppearanceSettingType } from './slice'
 
@@ -7,9 +12,34 @@ export function useCurrentAppearanceSetting(): AppearanceSettingType {
   return selectedAppearanceSettings
 }
 
+// fixes flashing of wrong color on background of app
+// see this for context:
+// https://github.com/bluesky-social/social-app/pull/1417
+// waiting on this merge from react-native to not need anymore:
+// https://github.com/facebook/react-native/pull/39439
+function useColorScheme(): ColorSchemeName {
+  const colorSchemeFromRN = useColorSchemeReactNative()
+  const [nativeColorScheme, setNativeColorScheme] = useState<ColorSchemeName>(colorSchemeFromRN)
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (state) => {
+      const isActive = state === 'active'
+      if (!isActive) return
+      setNativeColorScheme(colorSchemeFromRN)
+    })
+
+    return () => {
+      subscription.remove()
+    }
+  }, [colorSchemeFromRN])
+
+  return nativeColorScheme
+}
+
 export function useSelectedColorScheme(): 'light' | 'dark' {
   const currentAppearanceSetting = useCurrentAppearanceSetting()
   const isDarkMode = useColorScheme() === 'dark'
+
   if (currentAppearanceSetting !== AppearanceSettingType.System) {
     return currentAppearanceSetting === AppearanceSettingType.Dark ? 'dark' : 'light'
   }
