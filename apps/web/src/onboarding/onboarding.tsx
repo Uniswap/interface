ecd019fb41aefa95218fdbf1bec15569376c9acc
Christine Legge <christine.legge@uniswap.org>
2023-09-26 10:06:46 -0400

    fix(ext): fix unsafe-eval error in the onboarding flow (#4612)

--- apps/web/src/onboarding/onboarding.tsx
+++ apps/web/src/onboarding/onboarding.tsx
@@ -1,8 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../../../../index.d.ts" />
 
+import { lazy } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from 'src/app/OnboardingApp'
+;(globalThis as any).regeneratorRuntime = undefined // eslint-disable-line @typescript-eslint/no-explicit-any
+// The globalThis.regeneratorRuntime = undefined addresses a potentially unsafe-eval problem
+// see https://github.com/facebook/regenerator/issues/378#issuecomment-802628326
+
+const App = lazy(() => import('src/app/OnboardingApp'))
 
 const container = document.getElementById('onboarding-root')
 if (container) {
