6bf6a8a687dd40b50a6f8959846dcbb8c0173c2e
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-09-26 18:01:00 +0200

    fix(mob): Seed phrase words not shown (#4619)

--- apps/mobile/android/app/src/main/java/com/uniswap/extensions/ScrollFadeExtensions.kt
+++ apps/mobile/android/app/src/main/java/com/uniswap/extensions/ScrollFadeExtensions.kt
@@ -1,5 +1,7 @@
 package com.uniswap.extensions
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.compose.foundation.ScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,42 +15,52 @@ import androidx.compose.ui.unit.dp
 import com.uniswap.theme.UniswapTheme
 import java.lang.Float.min
 
+@SuppressLint("ComposableModifierFactory")
 @Composable
 fun Modifier.fadingEdges(
   scrollState: ScrollState,
   topEdgeHeight: Dp = 0.dp,
   bottomEdgeHeight: Dp = UniswapTheme.spacing.spacing48,
-): Modifier = this.then(
-  Modifier
-    // adding layer fixes issue with blending gradient and content
-    .graphicsLayer { alpha = 0.99F }
-    .drawWithContent {
-      drawContent()
+): Modifier = this.then(Modifier
+  // adding layer fixes issue with blending gradient and content
+  .graphicsLayer { alpha = 0.99F }
+  .drawWithContent {
+    drawContent()
 
-      val topColors = listOf(Color.Transparent, Color.Black)
-      val topStartY = scrollState.value.toFloat()
-      val topGradientHeight = min(topEdgeHeight.toPx(), topStartY)
+    val topColors = listOf(Color.Transparent, Color.Black)
+    val topStartY = scrollState.value.toFloat()
+    val topGradientHeight = min(topEdgeHeight.toPx(), topStartY)
+    val topGradientBrush = Brush.verticalGradient(
+      colors = topColors, startY = topStartY, endY = topStartY + topGradientHeight
+    )
+
+    val bottomColors = listOf(Color.Black, Color.Transparent)
+    val bottomEndY = size.height - scrollState.maxValue + scrollState.value
+    val bottomGradientHeight =
+      min(bottomEdgeHeight.toPx(), scrollState.maxValue.toFloat() - scrollState.value)
+    val bottomGradientBrush = Brush.verticalGradient(
+      colors = bottomColors, startY = bottomEndY - bottomGradientHeight, endY = bottomEndY
+    )
+
+    // Render gradient with blend mode on Android Q and above
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
       drawRect(
-        brush = Brush.verticalGradient(
-          colors = topColors,
-          startY = topStartY,
-          endY = topStartY + topGradientHeight
-        ),
-        blendMode = BlendMode.DstIn
+        brush = topGradientBrush, blendMode = BlendMode.DstIn
       )
-
-      val bottomColors = listOf(Color.Black, Color.Transparent)
-      val bottomEndY = size.height - scrollState.maxValue + scrollState.value
-      val bottomGradientHeight = min(bottomEdgeHeight.toPx(), scrollState.maxValue.toFloat() - scrollState.value)
       if (bottomGradientHeight != 0f) {
         drawRect(
-          brush = Brush.verticalGradient(
-            colors = bottomColors,
-            startY = bottomEndY - bottomGradientHeight,
-            endY = bottomEndY
-          ),
-          blendMode = BlendMode.DstIn
+          brush = bottomGradientBrush, blendMode = BlendMode.DstIn
+        )
+      }
+    // Otherwise, render gradient without blend mode if the blend mode is not supported
+    } else {
+      drawRect(
+        brush = topGradientBrush,
+      )
+      if (bottomGradientHeight != 0f) {
+        drawRect(
+          brush = bottomGradientBrush
         )
       }
     }
-)
+  })
