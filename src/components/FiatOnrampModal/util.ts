d6d71e8e1b418be02740c57b2db567eafffe471a
eddie <66155195+just-toby@users.noreply.github.com>
2023-09-27 13:37:54 -0700

    fix: case insensitive address comparison

--- src/components/FiatOnrampModal/util.ts
+++ src/components/FiatOnrampModal/util.ts
@@ -2,6 +2,7 @@ import { ChainId, WETH9 } from '@uniswap/sdk-core'
 import { MATIC, USDC_ARBITRUM, USDC_MAINNET, USDC_OPTIMISM, USDC_POLYGON, USDT, WBTC } from 'constants/tokens'
 import { Chain } from 'graphql/data/__generated__/types-and-hooks'
 import { validateUrlChainParam } from 'graphql/data/util'
+import { isSameAddress } from 'utils/addresses'
 
 export const MOONPAY_SUPPORTED_CURRENCY_CODES = [
   'eth',
@@ -32,34 +33,34 @@ export function getDefaultCurrencyCode(
   if (address === 'NATIVE' && chain === Chain.Optimism) {
     return 'eth_optimism'
   }
-  if (address === '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619' && chain === Chain.Polygon) {
+  if (isSameAddress(address, '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619') && chain === Chain.Polygon) {
     return 'eth_polygon'
   }
-  if (address === WETH9[ChainId.MAINNET]?.address && chain === Chain.Ethereum) {
+  if (isSameAddress(address, WETH9[ChainId.MAINNET]?.address) && chain === Chain.Ethereum) {
     return 'weth'
   }
-  if (address === WBTC.address && chain === Chain.Ethereum) {
+  if (isSameAddress(address, WBTC.address) && chain === Chain.Ethereum) {
     return 'wbtc'
   }
-  if (address === 'NATIVE' && chain === Chain.Polygon) {
+  if (isSameAddress(address, 'NATIVE') && chain === Chain.Polygon) {
     return 'matic_polygon'
   }
-  if (address === MATIC.address && chain === Chain.Ethereum) {
+  if (isSameAddress(address, MATIC.address) && chain === Chain.Ethereum) {
     return 'polygon'
   }
-  if (address === USDC_MAINNET.address && chain === Chain.Ethereum) {
+  if (isSameAddress(address, USDC_MAINNET.address) && chain === Chain.Ethereum) {
     return 'usdc'
   }
-  if (address === USDC_ARBITRUM.address && chain === Chain.Arbitrum) {
+  if (isSameAddress(address, USDC_ARBITRUM.address) && chain === Chain.Arbitrum) {
     return 'usdc_arbitrum'
   }
-  if (address === USDC_OPTIMISM.address && chain === Chain.Optimism) {
+  if (isSameAddress(address, USDC_OPTIMISM.address) && chain === Chain.Optimism) {
     return 'usdc_optimism'
   }
-  if (address === USDC_POLYGON.address && chain === Chain.Polygon) {
+  if (isSameAddress(address, USDC_POLYGON.address) && chain === Chain.Polygon) {
     return 'usdc_polygon'
   }
-  if (address === USDT.address && chain === Chain.Ethereum) {
+  if (isSameAddress(address, USDT.address) && chain === Chain.Ethereum) {
     return 'usdt'
   }
   return 'eth'
