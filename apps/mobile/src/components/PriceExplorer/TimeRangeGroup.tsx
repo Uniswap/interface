33e197b3d7039f88b9727c22074c328e6055b502
Daniel James <danielcolinjames@gmail.com>
2023-09-27 11:58:57 -0400

    fix(MOB): fix error from using .get() to access color in useAnimatedStyle (#4640)

--- apps/mobile/src/components/PriceExplorer/TimeRangeGroup.tsx
+++ apps/mobile/src/components/PriceExplorer/TimeRangeGroup.tsx
@@ -32,7 +32,7 @@ export function TimeRangeLabel({ index, label, selectedIndex, transition }: Prop
   const style = useAnimatedStyle(() => {
     const selected = index === selectedIndex.value
 
-    if (!selected) return { color: colors.neutral2.get() }
+    if (!selected) return { color: colors.neutral2.val }
 
     const color = interpolateColor(
       transition.value,
