5aa595e41d34a2b1876101a0f48464208af52ee0
eddie <66155195+just-toby@users.noreply.github.com>
2023-09-28 15:08:41 -0700

    fix: comment

--- src/components/FiatOnrampModal/utils.ts
+++ src/components/FiatOnrampModal/utils.ts
@@ -59,6 +59,10 @@ export function getDefaultCurrencyCode(
   return 'eth'
 }
 
+/**
+ * You should use useParams() from react-router-dom instead of this function if possible.
+ * This function is only used in the case where we need to parse the path outside the scope of the router.
+ */
 export function parsePathParts(pathname: string): { network?: string; tokenAddress?: string } {
   const pathParts = pathname.split('/')
   // Matches the /tokens/<network>/<tokenAddress> path.
