b6bdbcf587f9bbda07db18de036e2dcf3d65d075
eddie <66155195+just-toby@users.noreply.github.com>
2023-10-03 14:37:27 -0700

    test: mock http requests in jest (#7394)
    * test: try no coverage for unit tests
    
    * test: onlychanged
    
    * test: try parallel
    
    * fix: no http in unit tests
    
    * fix: deduplicate
    
    * fix: nock only
    
    * fix: maxworkers 2
    
    * fix: remove nock
    
    * fix: restore nock
    
    * fix: comment
    
    * fix: try again with jest-offline
    
    * fix: remove jest-offline
    
    * test: try 2 again
    
    * fix: 100%

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -7,6 +7,7 @@ import 'polyfills'
 import type { createPopper } from '@popperjs/core'
 import { useWeb3React } from '@web3-react/core'
 import failOnConsole from 'jest-fail-on-console'
+import { disableNetConnect, restore as restoreNetConnect } from 'nock'
 import { Readable } from 'stream'
 import { toBeVisible } from 'test-utils/matchers'
 import { mocked } from 'test-utils/mocked'
@@ -116,6 +117,15 @@ beforeEach(() => {
 
   // Mock useWeb3React to return a chainId of 1 by default.
   mocked(useWeb3React).mockReturnValue({ chainId: 1 } as ReturnType<typeof useWeb3React>)
+
+  // Disable network connections by default.
+  disableNetConnect()
+})
+
+afterEach(() => {
+  // Without this, nock causes a memory leak and the tests will fail on CI.
+  // https://github.com/nock/nock/issues/1817
+  restoreNetConnect()
 })
 
 /**
