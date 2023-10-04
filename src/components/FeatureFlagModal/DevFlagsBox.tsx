329330bb89365d4b4fdb2a89d6101e29e127cab6
Kristie Huang <kristiehuang01@gmail.com>
2023-10-04 13:58:15 -0400

    pr changes

--- src/components/FeatureFlagModal/DevFlagsBox.tsx
+++ src/components/FeatureFlagModal/DevFlagsBox.tsx
@@ -1,7 +1,7 @@
-import { BaseVariant, FeatureFlag, featureFlagSettings } from 'featureFlags'
+import { BaseVariant, featureFlagSettings } from 'featureFlags'
 import { useAtomValue } from 'jotai/utils'
 import { useMemo, useState } from 'react'
-import { GateResult, useGate } from 'statsig-react'
+import { useGate } from 'statsig-react'
 import styled from 'styled-components'
 import { ThemedText } from 'theme/components'
 import { isDevelopmentEnv, isStagingEnv } from 'utils/env'
@@ -16,73 +16,49 @@ const Box = styled.div`
   z-index: 1000;
   width: 300px;
 `
+const TopBar = styled.div`
+  margin-bottom: 8px;
+  display: flex;
+  justify-content: space-between;
+`
+const Gate = ({ flagSetting }: { flagSetting: [string, string] }) => {
+  const [flagName, setting] = flagSetting
+  const gateResult = useGate(flagName)
+  if (gateResult) {
+    const { value: statsigValue } = gateResult
+    const settingValue = setting === BaseVariant.Enabled
+    if (statsigValue !== settingValue) {
+      return (
+        <ThemedText.LabelSmall key={flagName}>
+          {flagName}: {setting}
+        </ThemedText.LabelSmall>
+      )
+    }
+  }
+  return null
+}
 
 export default function DevFlagsBox() {
-  const shouldShow = isDevelopmentEnv() || isStagingEnv()
-  const featureFlags = Object.entries(useAtomValue(featureFlagSettings))
-
-  const statsigValues: { [key: string]: GateResult } = {
-    [FeatureFlag.traceJsonRpc]: useGate(FeatureFlag.traceJsonRpc),
-    [FeatureFlag.debounceSwapQuote]: useGate(FeatureFlag.debounceSwapQuote),
-    [FeatureFlag.fallbackProvider]: useGate(FeatureFlag.fallbackProvider),
-    [FeatureFlag.uniswapXSyntheticQuote]: useGate(FeatureFlag.uniswapXSyntheticQuote),
-    [FeatureFlag.uniswapXEthOutputEnabled]: useGate(FeatureFlag.uniswapXEthOutputEnabled),
-    [FeatureFlag.uniswapXExactOutputEnabled]: useGate(FeatureFlag.uniswapXExactOutputEnabled),
-    [FeatureFlag.multichainUX]: useGate(FeatureFlag.multichainUX),
-    [FeatureFlag.currencyConversion]: useGate(FeatureFlag.currencyConversion),
-    [FeatureFlag.fotAdjustedmentsEnabled]: useGate(FeatureFlag.fotAdjustedmentsEnabled),
-    [FeatureFlag.infoExplore]: useGate(FeatureFlag.infoExplore),
-    [FeatureFlag.infoTDP]: useGate(FeatureFlag.infoTDP),
-    [FeatureFlag.infoPoolPage]: useGate(FeatureFlag.infoPoolPage),
-    [FeatureFlag.infoLiveViews]: useGate(FeatureFlag.infoLiveViews),
-    [FeatureFlag.uniswapXDefaultEnabled]: useGate(FeatureFlag.uniswapXDefaultEnabled),
-    [FeatureFlag.quickRouteMainnet]: useGate(FeatureFlag.quickRouteMainnet),
-  }
-
-  const overrides = useMemo(() => {
-    const overrides = []
-    for (const [flag, setting] of featureFlags) {
-      const gateResult = statsigValues[flag]
-      if (gateResult) {
-        const { value: statsigValue } = gateResult
-        const settingValue = setting === BaseVariant.Enabled
-        if (statsigValue !== settingValue) {
-          overrides.push([flag, setting])
-        }
-      }
-    }
-    return overrides
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [featureFlags])
+  const featureFlagsAtom = useAtomValue(featureFlagSettings)
+  const featureFlags = useMemo(() => Object.entries(featureFlagsAtom), [featureFlagsAtom])
 
   const [isCollapsed, setIsCollapsed] = useState(false)
   const toggleCollapse = () => setIsCollapsed(!isCollapsed)
 
-  if (shouldShow) {
-    return (
-      <Box>
-        <div
-          style={{
-            marginBottom: '8px',
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-          onClick={toggleCollapse}
-        >
-          <ThemedText.SubHeader>Feature flag setting overrides</ThemedText.SubHeader>
-          {isCollapsed ? '😿☝️' : '😺👇'}
-        </div>
-        {!isCollapsed &&
-          overrides.length &&
-          overrides.map(([flag, setting]) => (
-            <ThemedText.LabelSmall key={flag}>
-              {flag}: {setting}
-            </ThemedText.LabelSmall>
-          ))}
-        {overrides.length === 0 && <ThemedText.LabelSmall>No overrides</ThemedText.LabelSmall>}
-      </Box>
-    )
-  } else {
-    return null
-  }
+  const overrides = featureFlags.map((flag) => Gate({ flagSetting: flag }))
+  const hasNoOverrides = useMemo(() => overrides.every((g) => g === null), [overrides])
+
+  return (
+    <Box>
+      <TopBar onClick={toggleCollapse}>
+        <ThemedText.SubHeader>
+          {isStagingEnv() && 'Staging build overrides'}
+          {isDevelopmentEnv() && 'Development build overrides'}
+        </ThemedText.SubHeader>
+        {isCollapsed ? '😿☝️' : '😺👇'}
+      </TopBar>
+      {!isCollapsed && overrides}
+      {!isCollapsed && hasNoOverrides && <ThemedText.LabelSmall>No overrides</ThemedText.LabelSmall>}
+    </Box>
+  )
 }
