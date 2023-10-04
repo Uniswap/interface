0b675545ff1a5c29dcbcea503eee8792ccacdab1
Charlie B <charles@bachmeier.io>
2023-10-04 15:19:22 -0700

    use token I created

--- cypress/e2e/token-details.test.ts
+++ cypress/e2e/token-details.test.ts
@@ -49,7 +49,7 @@ describe('Token details', () => {
 
   it('token with warning and low trading volume should have all information populated', () => {
     // Shiba predator token, low trading volume and also has warning modal
-    cy.visit('/tokens/ethereum/0xbea0000029ad1c77d3d5d23ba2d8893db9d1efab')
+    cy.visit('/tokens/ethereum/0x1efbb78c8b917f67986bce54ce575069c0143681')
 
     // Should have missing price chart when price unavailable (expected for this token)
     if (cy.get('[data-cy="chart-header"]').contains('Price unavailable')) {
@@ -67,16 +67,16 @@ describe('Token details', () => {
     cy.get('[data-cy="resources-container"]').within(() => {
       cy.contains('Etherscan')
         .should('have.attr', 'href')
-        .and('include', 'etherscan.io/address/0xbea0000029ad1c77d3d5d23ba2d8893db9d1efab')
+        .and('include', 'etherscan.io/address/0x1efbb78c8b917f67986bce54ce575069c0143681')
       cy.contains('More analytics')
         .should('have.attr', 'href')
-        .and('include', 'info.uniswap.org/#/tokens/0xbea0000029ad1c77d3d5d23ba2d8893db9d1efab')
+        .and('include', 'info.uniswap.org/#/tokens/0x1efbb78c8b917f67986bce54ce575069c0143681')
       cy.contains('Website').should('have.attr', 'href').and('include', 'qom')
       cy.contains('Twitter').should('have.attr', 'href').and('include', 'twitter.com/ShibaPredator1')
     })
 
     // Contract address should be displayed
-    cy.contains('0xbea0000029ad1c77d3d5d23ba2d8893db9d1efab').should('exist')
+    cy.contains('0x1efbb78c8b917f67986bce54ce575069c0143681').should('exist')
 
     // Warning label should show if relevant ([spec](https://www.notion.so/3f7fce6f93694be08a94a6984d50298e))
     cy.get('[data-cy="token-safety-message"]')
