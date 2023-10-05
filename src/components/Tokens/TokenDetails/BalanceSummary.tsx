3e88ccfaef31229cbb017e565fbd33c88b4204c1
Kristie Huang <kristiehuang01@gmail.com>
2023-10-05 17:50:17 -0400

    remove balance summary entirely from tdp explore

--- src/components/Tokens/TokenDetails/BalanceSummary.tsx
+++ src/components/Tokens/TokenDetails/BalanceSummary.tsx
@@ -2,24 +2,21 @@ import { Trans } from '@lingui/macro'
 import { ChainId, Currency } from '@uniswap/sdk-core'
 import { useWeb3React } from '@web3-react/core'
 import CurrencyLogo from 'components/Logo/CurrencyLogo'
-import Row from 'components/Row'
 import { getChainInfo } from 'constants/chainInfo'
 import { asSupportedChain } from 'constants/chains'
-import { useInfoTDPEnabled } from 'featureFlags/flags/infoTDP'
 import { useStablecoinValue } from 'hooks/useStablecoinPrice'
 import useCurrencyBalance from 'lib/hooks/useCurrencyBalance'
 import styled, { useTheme } from 'styled-components'
 import { ThemedText } from 'theme/components'
 import { NumberType, useFormatter } from 'utils/formatNumbers'
 
-const BalancesCard = styled.div<{ isInfoTDPEnabled: boolean }>`
+const BalancesCard = styled.div`
   border-radius: 16px;
-  ${({ isInfoTDPEnabled, theme }) => `color: ${isInfoTDPEnabled ? theme.neutral2 : theme.neutral1};`};
+  color: ${({ theme }) => theme.neutral1};
   display: none;
   height: fit-content;
   padding: 16px;
   width: 100%;
-  ${({ isInfoTDPEnabled, theme }) => isInfoTDPEnabled && `background-color: ${theme.surface2};`};
 
   // 768 hardcoded to match NFT-redesign navbar breakpoints
   // src/nft/css/sprinkles.css.ts
@@ -57,14 +54,6 @@ const BalanceAmountsContainer = styled.div`
   align-items: center;
 `
 
-const PerformanceContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 12px;
-`
-
 const StyledNetworkLabel = styled.div`
   color: ${({ color }) => color};
   font-size: 12px;
@@ -85,52 +74,32 @@ export default function BalanceSummary({ token }: { token: Currency }) {
     amount: useStablecoinValue(balance),
     type: NumberType.FiatTokenStats,
   })
-  const twentyFourHourReturn = (
-    <Row>
-      <ThemedText.LabelSmall>$ dolla</ThemedText.LabelSmall>
-      <ThemedText.LabelSmall>$ dolla</ThemedText.LabelSmall>
-    </Row>
-  )
-
-  const isInfoTDPEnabled = useInfoTDPEnabled()
 
   if (!account || !balance) {
     return null
   }
   return (
-    <BalancesCard isInfoTDPEnabled={isInfoTDPEnabled}>
+    <BalancesCard>
       <BalanceSection>
-        <ThemedText.SubHeaderSmall>
-          {/* // question: hmm it looks like Trans does handle variables? */}
-          {isInfoTDPEnabled ? <Trans>Your {token.symbol}</Trans> : <Trans>Your balance on {label}</Trans>}
+        <ThemedText.SubHeaderSmall color={theme.neutral1}>
+          <Trans>Your balance on {label}</Trans>
         </ThemedText.SubHeaderSmall>
         <BalanceRow>
-          <CurrencyLogo currency={token} size={isInfoTDPEnabled ? '24px' : '2rem'} hideL2Icon={false} />
+          <CurrencyLogo currency={token} size="2rem" hideL2Icon={false} />
           <BalanceContainer>
             <BalanceAmountsContainer>
               <BalanceItem>
                 <ThemedText.SubHeader>
-                  {formattedBalance} {!isInfoTDPEnabled && token.symbol}
+                  {formattedBalance} {token.symbol}
                 </ThemedText.SubHeader>
               </BalanceItem>
               <BalanceItem>
-                {/* figma says this text should be 18px */}
-                {isInfoTDPEnabled ? (
-                  <ThemedText.BodySecondary>{formattedUsdValue}</ThemedText.BodySecondary>
-                ) : (
-                  <ThemedText.BodyPrimary>{formattedUsdValue}</ThemedText.BodyPrimary>
-                )}
+                <ThemedText.BodyPrimary>{formattedUsdValue}</ThemedText.BodyPrimary>
               </BalanceItem>
             </BalanceAmountsContainer>
-            {!isInfoTDPEnabled && <StyledNetworkLabel color={color}>{label}</StyledNetworkLabel>}
+            <StyledNetworkLabel color={color}>{label}</StyledNetworkLabel>
           </BalanceContainer>
         </BalanceRow>
-        {isInfoTDPEnabled && (
-          <PerformanceContainer>
-            <ThemedText.BodySmall>24H return</ThemedText.BodySmall>
-            <ThemedText.LabelSmall>{twentyFourHourReturn}</ThemedText.LabelSmall>
-          </PerformanceContainer>
-        )}
       </BalanceSection>
     </BalancesCard>
   )
