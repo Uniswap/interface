6e4746a7fe5d52ec30c38af40fe69289c54a4291
Jack Short <john.short.tj@gmail.com>
2023-10-06 11:00:07 -0700

    feat: uk disclaimer banner (#7428)
    * feat: uk disclaimer banner
    
    * bad merge with sitemap
    
    * button
    
    * cypress test
    
    * intercept ordering
    
    * comments
    
    * sitemap was committed idk why
    
    * font weights
    
    * moving uk disclaimer
    
    * removing trash

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,6 +4,7 @@ import { getDeviceId, sendAnalyticsEvent, sendInitializationEvent, Trace, user }
 import ErrorBoundary from 'components/ErrorBoundary'
 import Loader from 'components/Icons/LoadingSpinner'
 import NavBar, { PageTabs } from 'components/NavBar'
+import { UK_BANNER_HEIGHT, UK_BANNER_HEIGHT_MD, UK_BANNER_HEIGHT_SM, UkBanner } from 'components/NavBar/UkBanner'
 import { useFeatureFlagsIsLoaded } from 'featureFlags'
 import { useUniswapXDefaultEnabled } from 'featureFlags/flags/uniswapXDefault'
 import { useAtom } from 'jotai'
@@ -11,6 +12,8 @@ import { useBag } from 'nft/hooks/useBag'
 import { lazy, Suspense, useEffect, useLayoutEffect, useMemo, useState } from 'react'
 import { Navigate, Route, Routes, useLocation, useSearchParams } from 'react-router-dom'
 import { shouldDisableNFTRoutesAtom } from 'state/application/atoms'
+import { useAppSelector } from 'state/hooks'
+import { AppState } from 'state/reducer'
 import { RouterPreference } from 'state/routing/types'
 import { useRouterPreference, useUserOptedOutOfUniswapX } from 'state/user/hooks'
 import { StatsigProvider, StatsigUser } from 'statsig-react'
@@ -60,15 +63,23 @@ const MobileBottomBar = styled.div`
   }
 `
 
-const HeaderWrapper = styled.div<{ transparent?: boolean }>`
+const HeaderWrapper = styled.div<{ transparent?: boolean; bannerIsVisible?: boolean; scrollY: number }>`
   ${flexRowNoWrap};
   background-color: ${({ theme, transparent }) => !transparent && theme.surface1};
   border-bottom: ${({ theme, transparent }) => !transparent && `1px solid ${theme.surface3}`};
   width: 100%;
   justify-content: space-between;
   position: fixed;
-  top: 0;
+  top: ${({ bannerIsVisible }) => (bannerIsVisible ? Math.max(UK_BANNER_HEIGHT - scrollY, 0) : 0)}px;
   z-index: ${Z_INDEX.dropdown};
+
+  @media only screen and (max-width: ${({ theme }) => `${theme.breakpoint.md}px`}) {
+    top: ${({ bannerIsVisible }) => (bannerIsVisible ? Math.max(UK_BANNER_HEIGHT_MD - scrollY, 0) : 0)}px;
+  }
+
+  @media only screen and (max-width: ${({ theme }) => `${theme.breakpoint.sm}px`}) {
+    top: ${({ bannerIsVisible }) => (bannerIsVisible ? Math.max(UK_BANNER_HEIGHT_SM - scrollY, 0) : 0)}px;
+  }
 `
 
 export default function App() {
@@ -80,14 +91,18 @@ export default function App() {
   const currentPage = getCurrentPageFromLocation(pathname)
   const isDarkMode = useIsDarkMode()
   const [routerPreference] = useRouterPreference()
-  const [scrolledState, setScrolledState] = useState(false)
+  const [scrollY, setScrollY] = useState(0)
+  const scrolledState = scrollY > 0
   const isUniswapXDefaultEnabled = useUniswapXDefaultEnabled()
   const userOptedOutOfUniswapX = useUserOptedOutOfUniswapX()
   const routerConfig = useRouterConfig()
 
+  const originCountry = useAppSelector((state: AppState) => state.user.originCountry)
+  const renderUkBannner = Boolean(originCountry) && originCountry === 'GB'
+
   useEffect(() => {
     window.scrollTo(0, 0)
-    setScrolledState(false)
+    setScrollY(0)
   }, [pathname])
 
   const [searchParams] = useSearchParams()
@@ -148,7 +163,7 @@ export default function App() {
 
   useEffect(() => {
     const scrollListener = () => {
-      setScrolledState(window.scrollY > 0)
+      setScrollY(window.scrollY)
     }
     window.addEventListener('scroll', scrollListener)
     return () => window.removeEventListener('scroll', scrollListener)
@@ -198,7 +213,8 @@ export default function App() {
             api: process.env.REACT_APP_STATSIG_PROXY_URL,
           }}
         >
-          <HeaderWrapper transparent={isHeaderTransparent}>
+          {renderUkBannner && <UkBanner />}
+          <HeaderWrapper transparent={isHeaderTransparent} bannerIsVisible={renderUkBannner} scrollY={scrollY}>
             <NavBar blur={isHeaderTransparent} />
           </HeaderWrapper>
           <BodyWrapper>
