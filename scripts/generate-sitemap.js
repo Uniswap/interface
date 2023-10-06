4d0dcb28006d659d1080f44fd40733856b0fd7a0
eddie <66155195+just-toby@users.noreply.github.com>
2023-10-06 11:23:45 -0700

    fix: remove unnecessary header

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -35,8 +35,6 @@ fs.readFile('./public/sitemap.xml', 'utf8', async (err, data) => {
         headers: {
           'Content-Type': 'application/json',
           Origin: 'https://app.uniswap.org',
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.110 Safari/537.36',
         },
         body: JSON.stringify({ query: getQuery(chainName) }),
       })
