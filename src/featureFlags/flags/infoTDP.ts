48379c66ce90223d205617f9c00e97decad2c8e4
Kristie Huang <kristiehuang01@gmail.com>
2023-10-06 13:07:29 -0400

    feat: [info] remove balance summaries from TDP (#7430)

--- src/featureFlags/flags/infoTDP.ts
+++ src/featureFlags/flags/infoTDP.ts
@@ -3,3 +3,7 @@ import { BaseVariant, FeatureFlag, useBaseFlag } from '../index'
 export function useInfoTDPFlag(): BaseVariant {
   return useBaseFlag(FeatureFlag.infoTDP)
 }
+
+export function useInfoTDPEnabled(): boolean {
+  return useInfoTDPFlag() === BaseVariant.Enabled
+}
