45c3e1dc7861cdcae8562dbe9c7cef2313262d03
Kristie Huang <kristiehuang01@gmail.com>
2023-10-10 15:37:47 -0400

    feat: add dynamicconfig modal setting (#7395)
    * feat: add dynamicconfig feature flags setting
    
    * better typing
    
    * use diff atom for configs
    
    * fix
    
    * add config to devflagsbox
    
    * fix devbox intiailization
    
    * lint

--- src/featureFlags/dynamicConfig/quickRouteChains.ts
+++ src/featureFlags/dynamicConfig/quickRouteChains.ts
@@ -1,13 +1,19 @@
 import { ChainId } from '@uniswap/sdk-core'
+import { useFeatureFlagsContext } from 'featureFlags'
 
 import { DynamicConfigName, useDynamicConfig } from '.'
 
-// eslint-disable-next-line import/no-unused-modules
 export function useQuickRouteChains(): ChainId[] {
-  const config = useDynamicConfig(DynamicConfigName.quickRouteChains)
-  const chains = config.get('chains', [])
+  const statsigConfig = useDynamicConfig(DynamicConfigName.quickRouteChains)
+  const featureFlagsContext = useFeatureFlagsContext()
+
+  const remoteConfigChains = statsigConfig.get(DynamicConfigName.quickRouteChains, []) as ChainId[]
+  const localConfigChains =
+    featureFlagsContext.configs[DynamicConfigName.quickRouteChains]?.[DynamicConfigName.quickRouteChains]
+
+  const chains = Array.isArray(localConfigChains) ? localConfigChains : remoteConfigChains
   if (chains.every((c) => Object.values(ChainId).includes(c))) {
-    return chains as ChainId[]
+    return chains
   } else {
     console.error('feature flag config chains contain invalid ChainId')
     return []
