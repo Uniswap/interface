d9ea6f7283ae69f1197b7a380a5829149f25fca6
Nate Wienert <natewienert@gmail.com>
2023-10-11 08:53:39 -1000

    fix(mob): fix scheme not changing back on theme switch (#4796)

--- packages/wallet/src/features/appearance/hooks.tsx
+++ packages/wallet/src/features/appearance/hooks.tsx
@@ -1,39 +1,10 @@
-import { useEffect, useState } from 'react'
-import {
-  AppState,
-  ColorSchemeName,
-  useColorScheme as useColorSchemeReactNative,
-} from 'react-native'
+import { useColorScheme } from 'react-native'
 import { useAppSelector } from 'wallet/src/state'
 import { AppearanceSettingType } from './slice'
 
 export function useCurrentAppearanceSetting(): AppearanceSettingType {
   const { selectedAppearanceSettings } = useAppSelector((state) => state.appearanceSettings)
   return selectedAppearanceSettings
-}
-
-// fixes flashing of wrong color on background of app
-// see this for context:
-// https://github.com/bluesky-social/social-app/pull/1417
-// waiting on this merge from react-native to not need anymore:
-// https://github.com/facebook/react-native/pull/39439
-function useColorScheme(): ColorSchemeName {
-  const colorSchemeFromRN = useColorSchemeReactNative()
-  const [nativeColorScheme, setNativeColorScheme] = useState<ColorSchemeName>(colorSchemeFromRN)
-
-  useEffect(() => {
-    const subscription = AppState.addEventListener('change', (state) => {
-      const isActive = state === 'active'
-      if (!isActive) return
-      setNativeColorScheme(colorSchemeFromRN)
-    })
-
-    return () => {
-      subscription.remove()
-    }
-  }, [colorSchemeFromRN])
-
-  return nativeColorScheme
 }
 
 export function useSelectedColorScheme(): 'light' | 'dark' {
