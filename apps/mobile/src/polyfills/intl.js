464aec06bf0210f6e9a367e959468b6192dbe642
Gary Ye <garysye@gmail.com>
2023-10-14 03:26:05 +0800

    fix(mob): Updates formatjs to force numberformat polyfill for Android (#4821)

--- apps/mobile/src/polyfills/intl.js
+++ apps/mobile/src/polyfills/intl.js
@@ -1,24 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import { Platform } from 'react-native'
+import { IS_ANDROID } from 'src/constants/globals'
 
 // TODO: [MOB-247] remove polyfill once Hermes support it
 // https://github.com/facebook/hermes/issues/23
 
-if (Platform.OS === 'ios') {
-  // Polyfills required to use Intl with Hermes engine
-  require('@formatjs/intl-getcanonicallocales/polyfill').default
+// Polyfills required to use Intl with Hermes engine
+require('@formatjs/intl-getcanonicallocales/polyfill').default
 
-  require('@formatjs/intl-locale/polyfill').default
+require('@formatjs/intl-locale/polyfill').default
 
-  require('@formatjs/intl-pluralrules/polyfill').default
-  require('@formatjs/intl-pluralrules/locale-data/en').default
+require('@formatjs/intl-pluralrules/polyfill').default
+require('@formatjs/intl-pluralrules/locale-data/en').default
 
+if (IS_ANDROID) {
+  // Forces polyfill to replace Hermes NumberFormat due to issues with "compact" notation
+  // https://hermesengine.dev/docs/intl/#android-11
+  require('@formatjs/intl-numberformat/polyfill-force').default
+} else {
   require('@formatjs/intl-numberformat/polyfill').default
-  require('@formatjs/intl-numberformat/locale-data/en').default
-
-  require('@formatjs/intl-datetimeformat/polyfill').default
-  require('@formatjs/intl-datetimeformat/locale-data/en').default // locale-data for en
-
-  require('@formatjs/intl-relativetimeformat/polyfill').default
-  require('@formatjs/intl-relativetimeformat/locale-data/en').default
 }
+require('@formatjs/intl-numberformat/locale-data/en').default
+
+require('@formatjs/intl-datetimeformat/polyfill').default
+require('@formatjs/intl-datetimeformat/locale-data/en').default // locale-data for en
+
+require('@formatjs/intl-relativetimeformat/polyfill').default
+require('@formatjs/intl-relativetimeformat/locale-data/en').default
