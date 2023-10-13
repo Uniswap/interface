0fb2bf23e06c46008bf00d9cd46aded65042add1
Spencer <spencersyen@gmail.com>
2023-10-13 09:54:27 -0700

    fix(MOB-1678): Skip calling returnToPreviousDapp native module on Android (#4826)

--- apps/mobile/src/features/walletConnect/WalletConnect.ts
+++ apps/mobile/src/features/walletConnect/WalletConnect.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-import { NativeModules } from 'react-native'
+import { NativeModules, Platform } from 'react-native'
 
 const { RNWalletConnect } = NativeModules
 
 export const returnToPreviousApp = (): boolean => {
+  // TOOD(MOB-1680): Implement return to previous app for Android
+  if (Platform.OS === 'android') {
+    return false
+  }
   return RNWalletConnect.returnToPreviousApp()
 }
