fd137792ba9b7e70774e19a69360d49af39c5071
anassohail99 <anas.sohailabs1@gmail.com>
2023-10-19 17:19:34 +0500

    enabled tracing

--- src/tracing/trace.ts
+++ src/tracing/trace.ts
@@ -1,51 +1,88 @@
 import * as Sentry from '@sentry/react'
-import { BrowserTracing } from '@sentry/tracing'
-import { SharedEventName } from '@uniswap/analytics-events'
-import { initializeAnalytics, OriginApplication } from 'analytics'
-import store from 'state'
-import { setOriginCountry } from 'state/user/reducer'
-import { isDevelopmentEnv, isProductionEnv, isSentryEnabled } from 'utils/env'
-import { getEnvName } from 'utils/env'
-import { v4 as uuidv4 } from 'uuid'
+import { Span, SpanStatusType } from '@sentry/tracing'
 
-import { beforeSend } from './errors'
+// Modify this type if you want to add any Trace tags.
+type TraceTags = Record<string, never>
 
-// Dump some metadata into the window to allow client verification.
-window.GIT_COMMIT_HASH = process.env.REACT_APP_GIT_COMMIT_HASH
+interface TraceMetadata {
+  /** Arbitrary data stored on a trace. */
+  data?: Record<string, unknown>
+  /** Indexed (ie searchable) tags associated with a trace. */
+  tags?: Partial<TraceTags>
+}
 
-// This is used to identify the user in Sentry.
-const SENTRY_USER_ID_KEY = 'sentry-user-id'
+// These methods are provided as an abstraction so that users will not interact with Sentry directly.
+// This avoids tightly coupling Sentry to our instrumentation outside of this file, in case we swap services.
+interface TraceCallbackOptions {
+  /**
+   * Traces the callback as a child of the active trace.
+   * @param name - The name of the child. (On Sentry, this will appear as the "op".)
+   * @param callback - The callback to trace. The child trace will run for the duration of the callback.
+   * @param metadata - Any data or tags to include in the child trace.
+   */
+  traceChild<T>(name: string, callback: TraceCallback<T>, metadata?: TraceMetadata): Promise<T>
+  setTraceData(key: string, value: unknown): void
+  setTraceTag<K extends keyof TraceTags>(key: K, value: TraceTags[K]): void
+  /**
+   * Sets the status of a trace. If unset, the status will be set to 'ok' (or 'internal_error' if the callback throws).
+   * @param status - If a number is passed, the corresponding http status will be used.
+   */
+  setTraceStatus(status: number | SpanStatusType): void
+  /** Sets the error data of a trace. If unset and the callback throws, the thrown error will be set. */
+  setTraceError(error: unknown): void
+}
+type TraceCallback<T> = (options: TraceCallbackOptions) => Promise<T>
 
-// Actual KEYs are set by proxy servers.
-const AMPLITUDE_DUMMY_KEY = '00000000000000000000000000000000'
-export const STATSIG_DUMMY_KEY = 'client-0000000000000000000000000000000000000000000'
+/**
+ * Sets up TraceCallbackOptions for a Span (NB: Transaction extends Span).
+ * @returns a handler which will run a TraceCallback and propagate its result.
+ */
+function traceSpan(span?: Span) {
+  const traceChild = <T>(name: string, callback: TraceCallback<T>, metadata?: TraceMetadata) => {
+    const child = span?.startChild({ ...metadata, op: name })
+    return traceSpan(child)(callback)
+  }
+  const setTraceData = <K extends keyof TraceTags>(key: K, value: TraceTags[K]) => {
+    span?.setData(key, value)
+  }
+  const setTraceTag = (key: string, value: string | number | boolean) => {
+    span?.setTag(key, value)
+  }
+  const setTraceStatus = (status: number | SpanStatusType) => {
+    if (typeof status === 'number') {
+      span?.setHttpStatus(status)
+    } else {
+      span?.setStatus(status)
+    }
+  }
+  const setTraceError = (error: unknown) => {
+    span?.setData('error', error)
+  }
 
-// Sentry.init({
-//   dsn: process.env.REACT_APP_SENTRY_DSN,
-//   release: process.env.REACT_APP_GIT_COMMIT_HASH,
-//   environment: getEnvName(),
-//   enabled: isSentryEnabled(),
-//   tracesSampleRate: Number(process.env.REACT_APP_SENTRY_TRACES_SAMPLE_RATE ?? 0),
-//   integrations: [
-//     new BrowserTracing({
-//       startTransactionOnLocationChange: false,
-//       startTransactionOnPageLoad: true,
-//     }),
-//   ],
-//   beforeSend,
-// })
+  return async function boundTrace<T>(callback: TraceCallback<T>): Promise<T> {
+    try {
+      return await callback({ traceChild, setTraceData, setTraceTag, setTraceStatus, setTraceError })
+    } catch (error) {
+      // Do not overwrite any custom status or error data that was already set.
+      if (!span?.status) span?.setStatus('internal_error')
+      if (!span?.data.error) span?.setData('error', error)
 
-// let sentryUserId = localStorage.getItem(SENTRY_USER_ID_KEY)
-// if (!sentryUserId) {
-//   localStorage.setItem(SENTRY_USER_ID_KEY, (sentryUserId = uuidv4()))
-// }
-// Sentry.setUser({ id: sentryUserId })
+      throw error
+    } finally {
+      // If no status was reported, assume that it was 'ok'. Otherwise, it will default to 'unknown'.
+      if (!span?.status) span?.setStatus('ok')
+      span?.finish()
+    }
+  }
+}
 
-// initializeAnalytics(AMPLITUDE_DUMMY_KEY, OriginApplication.INTERFACE, {
-//   proxyUrl: process.env.REACT_APP_AMPLITUDE_PROXY_URL,
-//   defaultEventName: SharedEventName.PAGE_VIEWED,
-//   commitHash: process.env.REACT_APP_GIT_COMMIT_HASH,
-//   isProductionEnv: isProductionEnv(),
-//   debug: isDevelopmentEnv(),
-//   reportOriginCountry: (country: string) => store.dispatch(setOriginCountry(country)),
-// })
+/**
+ * Traces the callback, adding any metadata to the trace.
+ * @param name - The name of your trace.
+ * @param callback - The callback to trace. The trace will run for the duration of the callback.
+ * @param metadata - Any data or tags to include in the trace.
+ */
+export async function trace<T>(name: string, callback: TraceCallback<T>, metadata?: TraceMetadata): Promise<T> {
+  const transaction = Sentry.startTransaction({ name, data: metadata?.data, tags: metadata?.tags })
+  return traceSpan(transaction)(callback)
+}
