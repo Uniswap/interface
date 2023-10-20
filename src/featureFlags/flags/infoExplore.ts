b9873df5cac9b53be869fcd8410a17adf1c3ed06
Charlie B <charles@bachmeier.io>
2023-10-20 12:32:52 -0700

    fix regression

--- src/featureFlags/flags/infoExplore.ts
+++ src/featureFlags/flags/infoExplore.ts
@@ -1,7 +1,7 @@
 import { BaseVariant, FeatureFlag, useBaseFlag } from '../index'
 
 export function useInfoExploreFlag(): BaseVariant {
-  return useBaseFlag(FeatureFlag.infoExplore, BaseVariant.Enabled)
+  return useBaseFlag(FeatureFlag.infoExplore)
 }
 
 export function useInfoExplorePageEnabled(): boolean {
