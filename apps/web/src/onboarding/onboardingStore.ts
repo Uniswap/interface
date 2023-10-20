28ad012e261b2330022fabc54a093f1623f7e963
Daniel James <danielcolinjames@gmail.com>
2023-10-20 12:10:35 -0400

    fix(EXT-362): Add missing reducers to onboarding store (#4913)

--- apps/web/src/onboarding/onboardingStore.ts
+++ apps/web/src/onboarding/onboardingStore.ts
@@ -3,20 +3,15 @@ import { persistReducer, persistStore } from 'redux-persist'
 import { persistConfig } from 'src/background/reducer'
 import { monitoredSagaReducers, webRootSaga } from 'src/background/saga'
 import { loggerMiddleware } from 'src/background/utils/loggerMiddleware'
-import { appearanceSettingsReducer } from 'wallet/src/features/appearance/slice'
-import { walletReducer } from 'wallet/src/features/wallet/slice'
 import { createStore } from 'wallet/src/state'
+import { sharedReducers } from 'wallet/src/state/reducer'
 
 // Even though the onboarding flow and the main extension app have their own redux stores they are actually
 // going to persist to the same place. When the onboarding flow is complete then the main extension app will
 // read the persisted storage and load the onboarded wallets.
 const onboardingReducer = persistReducer(
   persistConfig,
-  combineReducers({
-    appearanceSettings: appearanceSettingsReducer,
-    wallet: walletReducer,
-    saga: monitoredSagaReducers,
-  })
+  combineReducers({ saga: monitoredSagaReducers, ...sharedReducers })
 )
 
 export const store = createStore({
