ff3ed31dd7dee8fe46ccb79a8b11e7b8f243f5c0
Jack Short <john.short.tj@gmail.com>
2023-10-31 13:45:26 -0400

    chore: cleaning up previous uk blocking functionality (#7489)

--- src/hooks/useIsNotOriginCountry.ts
+++
@@ -1,7 +0,0 @@
-import { useAppSelector } from 'state/hooks'
-import { AppState } from 'state/reducer'
-
-export function useIsNotOriginCountry(country: string) {
-  const originCountry = useAppSelector((state: AppState) => state.user.originCountry)
-  return Boolean(originCountry) && originCountry !== country
-}
