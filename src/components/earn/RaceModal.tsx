0d5501d881a2f8cacae0aff2856d39c33502cac8
gab <gabriele.rigo@gmail.com>
2023-11-04 18:39:48 +0100

    feat: return error in race modal
    - add error return message if pool has no active stake
    - added generic error if user denies signature

--- src/components/earn/RaceModal.tsx
+++ src/components/earn/RaceModal.tsx
@@ -51,6 +51,7 @@ export default function RaceModal({ isOpen, poolAddress, poolName, onDismiss, ti
 
   // monitor call to help UI loading state
   const [hash, setHash] = useState<string | undefined>()
+  const [errorReason, setErrorReason] = useState<string | undefined>()
   const [attempting, setAttempting] = useState(false)
 
   const transaction = useTransaction(hash)
@@ -60,6 +61,7 @@ export default function RaceModal({ isOpen, poolAddress, poolName, onDismiss, ti
   // wrapper to reset state on modal close
   function wrappedOnDismiss() {
     setHash(undefined)
+    setErrorReason(undefined)
     setAttempting(false)
     onDismiss()
   }
@@ -71,6 +73,7 @@ export default function RaceModal({ isOpen, poolAddress, poolName, onDismiss, ti
 
     // try credit reward and store hash
     const hash = await raceCallback(poolAddress)?.catch((error) => {
+      setErrorReason(error.reason)
       setAttempting(false)
       console.log(error)
     })
@@ -89,18 +92,33 @@ export default function RaceModal({ isOpen, poolAddress, poolName, onDismiss, ti
               <ThemedText.DeprecatedMediumHeader fontWeight={500}>{title}</ThemedText.DeprecatedMediumHeader>
               <StyledClosed stroke="black" onClick={wrappedOnDismiss} />
             </RowBetween>
-            <RowBetween>
-              <p>
-                Enroll <NameText>{poolName}</NameText> to compete for the network rewards. To race,{' '}
-                <EmphasisText>the pool requires actively staked GRG</EmphasisText>. This action only needs to be run
-                once per each epoch.
-              </p>
-            </RowBetween>
-            <ButtonPrimary disabled={false} onClick={onRace}>
-              <ThemedText.DeprecatedMediumHeader color="white">
-                <Trans>Race</Trans>{' '}
-              </ThemedText.DeprecatedMediumHeader>
-            </ButtonPrimary>
+            {!errorReason ? (
+              <>
+                <RowBetween>
+                  <p>
+                    Enroll <NameText>{poolName}</NameText> to compete for the network rewards. To race,{' '}
+                    <EmphasisText>the pool requires actively staked GRG</EmphasisText>. This action only needs to be run
+                    once per each epoch.
+                  </p>
+                </RowBetween>
+                <ButtonPrimary disabled={false} onClick={onRace}>
+                  <ThemedText.DeprecatedMediumHeader color="white">
+                    <Trans>Race</Trans>{' '}
+                  </ThemedText.DeprecatedMediumHeader>
+                </ButtonPrimary>
+              </>
+            ) : errorReason === 'execution reverted: POP_STAKING_POOL_BALANCES_NULL_ERROR' ? (
+              <RowBetween>
+                <p>
+                  <NameText>{poolName}</NameText> does not have an active GRG stake. If you are its pool operator,
+                  select your pool and click the <EmphasisText>Stake</EmphasisText> button at the bottom of the page,
+                  then select <EmphasisText>Stake from Pool.</EmphasisText> This will allow you to stake from the pool
+                  in 1 click.
+                </p>
+              </RowBetween>
+            ) : (
+              <p>User rejected transaction error</p>
+            )}
           </AutoColumn>
         </ContentWrapper>
       )}
