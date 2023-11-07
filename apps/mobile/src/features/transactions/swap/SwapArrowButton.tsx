e39ad3d81a31bcaf8a2510ecec350e922b61f9d9
Nate Wienert <natewienert@gmail.com>
2023-11-07 13:31:50 -1000

    chore(mob): remove unnecessary useMemo (#5062)

--- apps/mobile/src/features/transactions/swap/SwapArrowButton.tsx
+++ apps/mobile/src/features/transactions/swap/SwapArrowButton.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { Arrow } from 'src/components/icons/Arrow'
 import { Flex, TouchableArea, TouchableAreaProps, useSporeColors } from 'ui/src'
 import { iconSizes } from 'ui/src/theme'
@@ -11,28 +11,25 @@ type SwapArrowButtonProps = Pick<
 export function SwapArrowButton(props: SwapArrowButtonProps): JSX.Element {
   const colors = useSporeColors()
   const { testID, onPress, disabled, bg = '$surface2', size = iconSizes.icon20, ...rest } = props
-  return useMemo(
-    () => (
-      <TouchableArea
-        hapticFeedback
-        alignItems="center"
-        alignSelf="center"
-        bg={bg}
-        borderColor="$surface1"
-        borderRadius="$rounded16"
-        borderWidth={4}
-        disabled={disabled}
-        justifyContent="center"
-        p="$spacing8"
-        testID={testID}
-        onPress={onPress}
-        {...rest}>
-        {/* hack to add 2px more padding without adjusting design system values */}
-        <Flex centered p="$spacing2">
-          <Arrow color={colors.neutral2.get()} direction="s" size={size} />
-        </Flex>
-      </TouchableArea>
-    ),
-    [bg, disabled, onPress, testID, rest, colors.neutral2, size]
+  return (
+    <TouchableArea
+      hapticFeedback
+      alignItems="center"
+      alignSelf="center"
+      bg={bg}
+      borderColor="$surface1"
+      borderRadius="$rounded16"
+      borderWidth={4}
+      disabled={disabled}
+      justifyContent="center"
+      p="$spacing8"
+      testID={testID}
+      onPress={onPress}
+      {...rest}>
+      {/* hack to add 2px more padding without adjusting design system values */}
+      <Flex centered p="$spacing2">
+        <Arrow color={colors.neutral2.get()} direction="s" size={size} />
+      </Flex>
+    </TouchableArea>
   )
 }
