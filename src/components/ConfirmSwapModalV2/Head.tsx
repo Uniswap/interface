ba99db2c9704b9ef52b1ce716cf4d5ddd20013db
Gray Newfield <18626088+gnewfield@users.noreply.github.com>
2023-11-08 22:38:38 -0500

    add Head component

--- src/components/ConfirmSwapModalV2/Head.tsx
+++ src/components/ConfirmSwapModalV2/Head.tsx
@@ -4,6 +4,7 @@ import Badge from 'components/Badge'
 import { ChainLogo } from 'components/Logo/ChainLogo'
 import Row, { RowFixed } from 'components/Row'
 import { ConfirmModalState } from 'components/swap/ConfirmSwapModal'
+import { FadePresence } from 'components/swap/PendingModalContent/Logos'
 import { getChainInfo } from 'constants/chainInfo'
 import { X } from 'react-feather'
 import styled from 'styled-components'
@@ -20,12 +21,14 @@ function L2Badge({ confirmModalState }: { confirmModalState: ConfirmModalState }
   }
   const info = getChainInfo(chainId)
   return (
-    <StyledL2Badge>
-      <RowFixed data-testid="confirmation-modal-chain-icon" gap="sm">
-        <ChainLogo chainId={chainId} size={16} />
-        <ThemedText.SubHeaderSmall>{info.label}</ThemedText.SubHeaderSmall>
-      </RowFixed>
-    </StyledL2Badge>
+    <FadePresence>
+      <StyledL2Badge>
+        <RowFixed data-testid="confirmation-modal-chain-icon" gap="sm">
+          <ChainLogo chainId={chainId} size={16} />
+          <ThemedText.SubHeaderSmall>{info.label}</ThemedText.SubHeaderSmall>
+        </RowFixed>
+      </StyledL2Badge>
+    </FadePresence>
   )
 }
 
@@ -47,9 +50,13 @@ export function Head({
         <L2Badge confirmModalState={confirmModalState} />
       </Row>
       <Row justify="center">
-        <ThemedText.SubHeader textAlign="center">
-          {confirmModalState === ConfirmModalState.REVIEWING ? <Trans>Review swap</Trans> : undefined}
-        </ThemedText.SubHeader>
+        {confirmModalState === ConfirmModalState.REVIEWING && (
+          <FadePresence>
+            <ThemedText.SubHeader textAlign="center">
+              <Trans>Review swap</Trans>
+            </ThemedText.SubHeader>
+          </FadePresence>
+        )}
       </Row>
       <Row justify="right">
         <CloseIcon onClick={onDismiss} data-testid="confirmation-close-icon" />
