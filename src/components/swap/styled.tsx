cd45b47ba236e854147c7dbe0e66359399364e7f
eddie <66155195+just-toby@users.noreply.github.com>
2023-11-13 15:09:42 -0800

    fix: remove more unused styled components

--- src/components/swap/styled.tsx
+++ src/components/swap/styled.tsx
@@ -4,7 +4,6 @@ import { AlertTriangle } from 'react-feather'
 import styled, { css } from 'styled-components'
 import { Z_INDEX } from 'theme/zIndex'
 
-import { useIsDarkMode } from '../../theme/components/ThemeToggle'
 import { AutoColumn } from '../Column'
 
 export const PageWrapper = styled.div`
@@ -59,90 +58,6 @@ const SwapWrapperInner = styled.div`
   z-index: -1;
   padding: 8px;
   padding-top: 12px;
-`
-
-export const UniswapPopoverContainer = styled.div`
-  padding: 18px;
-  color: ${({ theme }) => theme.neutral1};
-  font-weight: 485;
-  font-size: 12px;
-  line-height: 16px;
-  word-break: break-word;
-  background: ${({ theme }) => theme.surface1};
-  border-radius: 20px;
-  border: 1px solid ${({ theme }) => theme.surface3};
-  box-shadow: 0 4px 8px 0 ${({ theme }) => transparentize(0.9, theme.shadow1)};
-  position: relative;
-  overflow: hidden;
-`
-
-const springDownKeyframes = `@keyframes spring-down {
-  0% { transform: translateY(-80px); }
-  25% { transform: translateY(4px); }
-  50% { transform: translateY(-1px); }
-  75% { transform: translateY(0px); }
-  100% { transform: translateY(0px); }
-}`
-
-const backUpKeyframes = `@keyframes back-up {
-  0% { transform: translateY(0px); }
-  100% { transform: translateY(-80px); }
-}`
-
-export const UniswapXShine = (props: any) => {
-  const isDarkMode = useIsDarkMode()
-  return <UniswapXShineInner {...props} style={{ opacity: isDarkMode ? 0.15 : 0.05, ...props.style }} />
-}
-
-const UniswapXShineInner = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: -1;
-  pointer-events: none;
-  background: linear-gradient(130deg, transparent 20%, ${({ theme }) => theme.accent1}, transparent 80%);
-  opacity: 0.15;
-`
-
-// overflow hidden to hide the SwapMustacheShadow
-export const SwapOptInSmallContainer = styled.div<{ visible: boolean }>`
-  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
-  overflow: hidden;
-  margin-top: -14px;
-  transform: translateY(${({ visible }) => (visible ? 0 : -80)}px);
-  transition: all ease 400ms;
-  animation: ${({ visible }) => (visible ? `spring-down 900ms ease forwards` : 'back-up 200ms ease forwards')};
-
-  ${springDownKeyframes}
-  ${backUpKeyframes}
-`
-
-export const SwapMustache = styled.main`
-  position: relative;
-  background: ${({ theme }) => theme.surface1};
-  border-radius: 16px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-  border: 1px solid ${({ theme }) => theme.surface3};
-  border-top-width: 0;
-  padding: 18px;
-  padding-top: calc(12px + 18px);
-  z-index: 0;
-  transition: transform 250ms ease;
-`
-
-export const SwapMustacheShadow = styled.main`
-  position: absolute;
-  top: 0;
-  left: 0;
-  border-radius: 16px;
-  height: 100%;
-  width: 100%;
-  transform: translateY(-100%);
-  box-shadow: 0 0 20px 20px ${({ theme }) => theme.surface2};
-  background: red;
 `
 
 export const ArrowWrapper = styled.div<{ clickable: boolean }>`
