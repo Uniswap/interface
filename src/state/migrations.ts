f6660bef0382566f3628407bfe24763e6929b32e
Jack Short <john.short.tj@gmail.com>
2023-11-14 14:41:39 -0500

    fix: flushing user locale redux when locale is German (#7586)
    * fix: set current redux version to 3 (#7578)
    
    * fix: set current redux version to 3
    
    * fix: tests
    
    * fix: redux migration to flush german locale (#7584)
    
    * fix: redux migration to flush german locale
    
    * lint
    
    * my linter was not workking
    
    ---------
    
    Co-authored-by: eddie <66155195+just-toby@users.noreply.github.com>

--- src/state/migrations.ts
+++ src/state/migrations.ts
@@ -4,6 +4,8 @@ import { MigrationConfig } from 'redux-persist/es/createMigrate'
 import { migration0 } from './migrations/0'
 import { migration1 } from './migrations/1'
 import { migration2 } from './migrations/2'
+import { migration3 } from './migrations/3'
+import { migration4 } from './migrations/4'
 import { legacyLocalStorageMigration } from './migrations/legacy'
 
 /**
@@ -19,6 +21,8 @@ export const migrations: MigrationManifest = {
   0: migration0,
   1: migration1,
   2: migration2,
+  3: migration3,
+  4: migration4,
 }
 
 // We use a custom migration function for the initial state, because redux-persist
