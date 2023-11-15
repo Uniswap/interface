2950487edff5f4945315635159a91eb2417ba3eb
Kristie Huang <kristiehuang01@gmail.com>
2023-11-15 18:35:18 -0500

    feat: [info] add tdp charts toggle, WIP

---
+++ src/components/Charts/utils.ts
@@ -0,0 +1,8 @@
+export enum ChartType {
+  PRICE = 'Price',
+  VOLUME = 'Volume',
+  TVL = 'TVL',
+  FEES_ACCRUED = 'Fees',
+}
+
+export const TDP_CHART_TYPES: ChartType[] = [ChartType.PRICE, ChartType.VOLUME, ChartType.TVL]
--- src/pages/TokenDetails/index.tsx
+++ src/pages/TokenDetails/index.tsx
@@ -1,16 +1,18 @@
+import { ChartType } from 'components/Charts/utils'
 import TokenDetails from 'components/Tokens/TokenDetails'
 import { TokenDetailsPageSkeleton } from 'components/Tokens/TokenDetails/Skeleton'
 import { NATIVE_CHAIN_ID } from 'constants/tokens'
 import { useTokenPriceQuery, useTokenQuery } from 'graphql/data/__generated__/types-and-hooks'
 import { TimePeriod, toHistoryDuration, validateUrlChainParam } from 'graphql/data/util'
 import useParsedQueryString from 'hooks/useParsedQueryString'
-import { useAtom } from 'jotai'
+import { atom, useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 import { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getNativeTokenDBAddress } from 'utils/nativeTokens'
 
 export const pageTimePeriodAtom = atomWithStorage<TimePeriod>('tokenDetailsTimePeriod', TimePeriod.DAY)
+export const pageChartTypeAtom = atom<ChartType>(ChartType.PRICE)
 
 export default function TokenDetailsPage() {
   const { tokenAddress, chainName } = useParams<{
@@ -20,6 +22,7 @@ export default function TokenDetailsPage() {
   const chain = validateUrlChainParam(chainName)
   const isNative = tokenAddress === NATIVE_CHAIN_ID
   const [timePeriod, setTimePeriod] = useAtom(pageTimePeriodAtom)
+  const [chartType, setChartType] = useAtom(pageChartTypeAtom) // todo something with this
   const [detailedTokenAddress, duration] = useMemo(
     // tokenAddress will always be defined in the path for for this page to render, but useParams will always
     // return optional arguments; nullish coalescing operator is present here to appease typechecker
@@ -65,6 +68,7 @@ export default function TokenDetailsPage() {
       tokenQuery={tokenQuery}
       tokenPriceQuery={currentPriceQuery}
       onChangeTimePeriod={setTimePeriod}
+      onChangeChartType={setChartType}
       inputTokenAddress={parsedInputTokenAddress}
     />
   )
