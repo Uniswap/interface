956449462dca5b2019db2c5a5504f2ec747e2417
Nate Wienert <natewienert@gmail.com>
2023-11-15 19:14:17 -1000

    fix(repo): fix vercel build ignore logic (#5187)

--- scripts/web-vercel-ignore.sh
+++ scripts/web-vercel-ignore.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-if [ "$VERCEL_GIT_COMMIT_REF" == "interface" ]; then
-  exit 0;
-else
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+# wtf vercel so successful exit (0) = cancel build
+#                    error exit (1) = run build
+# ????
+# https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "interface" ]] ; then
+  echo "✅ - Build can proceed"
   exit 1;
+else
+  echo "🛑 - Build cancelled"
+  exit 0;
 fi
