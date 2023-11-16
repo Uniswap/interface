447f9b3a20e00489fc177a13e659c54ce468b103
eddie <66155195+just-toby@users.noreply.github.com>
2023-11-16 13:36:57 -0800

    fix: remove /increase route (#7603)
    * fix: remove /increase route
    
    * fix: rename confusing components

--- apps/web2/src/pages/AddLiquidityV2/redirects.tsx
+++ apps/web2/src/pages/AddLiquidityV2/redirects.tsx
@@ -2,7 +2,7 @@ import { Navigate, useParams } from 'react-router-dom'
 
 import AddLiquidityV2 from './index'
 
-export default function RedirectDuplicateTokenIdsV2() {
+export default function AddLiquidityV2WithTokenRedirects() {
   const { currencyIdA, currencyIdB } = useParams<{ currencyIdA: string; currencyIdB: string }>()
 
   if (currencyIdA && currencyIdB && currencyIdA.toLowerCase() === currencyIdB.toLowerCase()) {
