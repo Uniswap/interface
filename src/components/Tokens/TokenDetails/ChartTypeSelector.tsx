e77c093219be0b4e3bbfdeed171edb7c3f7caad4
Kristie Huang <kristiehuang01@gmail.com>
2023-11-16 15:57:26 -0500

    add moving active pill

--- src/components/Tokens/TokenDetails/ChartTypeSelector.tsx
+++ src/components/Tokens/TokenDetails/ChartTypeSelector.tsx
@@ -1,5 +1,5 @@
 import { ChartType, TDP_CHART_TYPES } from 'components/Charts/utils'
-import { startTransition } from 'react'
+import { createRef, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const ChartTypeOptionsContainer = styled.div`
@@ -13,12 +13,18 @@ const ChartTypeOptionsContainer = styled.div`
   padding: 4px;
   width: fit-content;
 `
+const ActivePill = styled.div`
+  position: absolute;
+  height: 30px;
+  background-color: ${({ theme }) => theme.surface3};
+  border-radius: 15px;
+`
 const ChartTypeButton = styled.button<{ active: boolean }>`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme, active }) => (active ? theme.surface3 : 'transparent')};
+  background-color: transparent;
   font-weight: 535;
   font-size: 16px;
   padding: 8px 12px;
@@ -32,7 +38,11 @@ const ChartTypeButton = styled.button<{ active: boolean }>`
     ${({ active, theme }) => !active && `opacity: ${theme.opacity.hover};`}
   }
 `
-
+type ActivePillStyle = {
+  left?: string
+  width?: string
+  transition?: string
+}
 export default function ChartTypeSelector({
   currentChartType,
   onChartTypeChange,
@@ -40,17 +50,40 @@ export default function ChartTypeSelector({
   currentChartType: ChartType
   onChartTypeChange: (c: ChartType) => void
 }) {
+  const buttonRefs = TDP_CHART_TYPES.map(() => createRef<HTMLButtonElement>())
+  const [activePillStyle, setActivePillStyle] = useState<ActivePillStyle>({})
+
+  // Set initial active pill position on mount
+  useEffect(() => {
+    const rect = buttonRefs[0].current?.getBoundingClientRect()
+    setActivePillStyle({ left: `${rect?.left}px`, width: `${rect?.width}px` })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <ChartTypeOptionsContainer>
-      {TDP_CHART_TYPES.map((chart, i) => (
-        <ChartTypeButton
-          key={i}
-          active={currentChartType === chart}
-          onClick={() => startTransition(() => onChartTypeChange(chart))}
-        >
-          {chart}
-        </ChartTypeButton>
-      ))}
+      <ActivePill style={activePillStyle} />
+      {TDP_CHART_TYPES.map((chart, i) => {
+        const ref = buttonRefs[i]
+        return (
+          <ChartTypeButton
+            ref={ref}
+            key={i}
+            active={currentChartType === chart}
+            onClick={() => {
+              onChartTypeChange(chart)
+              const rect = ref.current?.getBoundingClientRect()
+              setActivePillStyle({
+                left: `${rect?.left}px`,
+                width: `${rect?.width}px`,
+                transition: 'left 0.3s ease, width 0.3s ease',
+              })
+            }}
+          >
+            {chart}
+          </ChartTypeButton>
+        )
+      })}
     </ChartTypeOptionsContainer>
   )
 }
