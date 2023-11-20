4a829efffa979fa88cb724b98a05862ad1a4b7e4
Daniel James <danielcolinjames@gmail.com>
2023-11-20 11:45:09 -0500

    fix(MOB): landing animation alignment (#5201)

--- apps/mobile/src/screens/Onboarding/LandingScreen.tsx
+++ apps/mobile/src/screens/Onboarding/LandingScreen.tsx
@@ -54,65 +54,67 @@ export function LandingScreen({ navigation }: Props): JSX.Element {
     // TODO(blocked by MOB-1082): delete bg prop
     // dark mode onboarding asset needs to be re-exported with #131313 (surface1) as background color
     <Screen bg={isDarkMode ? '$sporeBlack' : '$surface1'} edges={['bottom']}>
-      <Flex shrink height="100%" width="100%">
-        <LandingBackground />
-      </Flex>
-      <Flex grow height="auto">
-        <Flex
-          grow
-          $short={{ gap: '$spacing12' }}
-          gap="$spacing24"
-          justifyContent="flex-end"
-          mx="$spacing16">
-          <Trace logPress element={ElementName.CreateAccount}>
-            <Button
-              hapticFeedback
-              $short={{ size: 'medium' }}
-              size="large"
-              onPress={onPressCreateWallet}>
-              {t('Create a new wallet')}
-            </Button>
-          </Trace>
-          <Trace logPress element={ElementName.ImportAccount}>
-            <TouchableArea
-              hapticFeedback
-              alignItems="center"
-              hitSlop={16}
-              onLongPress={async (): Promise<void> => {
-                if (isDevBuild()) {
-                  await selectionAsync()
-                  dispatch(openModal({ name: ModalName.Experiments }))
-                }
-              }}
-              onPress={onPressImportWallet}>
-              <Text
-                $short={{ variant: 'buttonLabel2', fontSize: '$medium' }}
-                color="$accent1"
-                variant="buttonLabel1">
-                {t('Add an existing wallet')}
-              </Text>
-            </TouchableArea>
-          </Trace>
-          <Flex $short={{ py: '$none', mx: '$spacing12' }} mx="$spacing24" py="$spacing12">
-            <Text color="$neutral2" mx="$spacing4" textAlign="center" variant="buttonLabel4">
-              <Trans t={t}>
-                By continuing, I agree to the{' '}
+      <Flex fill gap="$spacing8">
+        <Flex shrink height="100%" width="100%">
+          <LandingBackground />
+        </Flex>
+        <Flex grow height="auto">
+          <Flex
+            grow
+            $short={{ gap: '$spacing12' }}
+            gap="$spacing24"
+            justifyContent="flex-end"
+            mx="$spacing16">
+            <Trace logPress element={ElementName.CreateAccount}>
+              <Button
+                hapticFeedback
+                $short={{ size: 'medium' }}
+                size="large"
+                onPress={onPressCreateWallet}>
+                {t('Create a new wallet')}
+              </Button>
+            </Trace>
+            <Trace logPress element={ElementName.ImportAccount}>
+              <TouchableArea
+                hapticFeedback
+                alignItems="center"
+                hitSlop={16}
+                onLongPress={async (): Promise<void> => {
+                  if (isDevBuild()) {
+                    await selectionAsync()
+                    dispatch(openModal({ name: ModalName.Experiments }))
+                  }
+                }}
+                onPress={onPressImportWallet}>
                 <Text
+                  $short={{ variant: 'buttonLabel2', fontSize: '$medium' }}
                   color="$accent1"
-                  variant="buttonLabel4"
-                  onPress={(): Promise<void> => openUri(uniswapUrls.termsOfServiceUrl)}>
-                  Terms of Service
-                </Text>{' '}
-                and consent to the{' '}
-                <Text
-                  color="$accent1"
-                  variant="buttonLabel4"
-                  onPress={(): Promise<void> => openUri(uniswapUrls.privacyPolicyUrl)}>
-                  Privacy Policy
+                  variant="buttonLabel1">
+                  {t('Add an existing wallet')}
                 </Text>
-                .
-              </Trans>
-            </Text>
+              </TouchableArea>
+            </Trace>
+            <Flex $short={{ py: '$none', mx: '$spacing12' }} mx="$spacing24" py="$spacing12">
+              <Text color="$neutral2" mx="$spacing4" textAlign="center" variant="buttonLabel4">
+                <Trans t={t}>
+                  By continuing, I agree to the{' '}
+                  <Text
+                    color="$accent1"
+                    variant="buttonLabel4"
+                    onPress={(): Promise<void> => openUri(uniswapUrls.termsOfServiceUrl)}>
+                    Terms of Service
+                  </Text>{' '}
+                  and consent to the{' '}
+                  <Text
+                    color="$accent1"
+                    variant="buttonLabel4"
+                    onPress={(): Promise<void> => openUri(uniswapUrls.privacyPolicyUrl)}>
+                    Privacy Policy
+                  </Text>
+                  .
+                </Trans>
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
