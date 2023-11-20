7be698991d55375075610f1b2efc83fbddcf4fc1
Kristie Huang <kristiehuang01@gmail.com>
2023-11-20 12:32:49 -0500

    hotfix: fix: android banner DownloadButton onClick should not propagate up #7611

--- src/components/Banner/AndroidAnnouncementBanner/index.tsx
+++ src/components/Banner/AndroidAnnouncementBanner/index.tsx
@@ -51,7 +51,12 @@ export default function AndroidAnnouncementBanner() {
           <ThemedText.LabelMicro>
             <Trans>Available now - download from the Google Play Store today</Trans>
           </ThemedText.LabelMicro>
-          <DownloadButton onClick={onClick}>
+          <DownloadButton
+            onClick={(e) => {
+              e.stopPropagation()
+              onClick()
+            }}
+          >
             <Trans>Download now</Trans>
           </DownloadButton>
         </TextContainer>
