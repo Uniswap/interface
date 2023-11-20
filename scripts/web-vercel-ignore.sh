b34e4a7371a1308b11ffcfeddb9084adeda7cd3c
natew <natewienert@gmail.com>
2023-11-20 08:30:32 -1000

    cleanup vercel ignore script

--- scripts/web-vercel-ignore.sh
+++ scripts/web-vercel-ignore.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# This script controls whether the web app builds for Vercel
+
+# It has a Very Strange API surface
+# https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel
+# successful exit (0) = cancel build
+# error exit (1) = run build
+
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-# wtf vercel so successful exit (0) = cancel build
-#                    error exit (1) = run build
-# ????
-# https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel
+# For now we're running web builds always:
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "interface" ]] ; then
-  echo "✅ - Build can proceed"
-  exit 1;
-else
-  echo "🛑 - Build cancelled"
-  exit 0;
-fi
+echo "✅ - Build can proceed"
+exit 1;
+
+# Example of ignoring based on a branch:
+
+# if [[ "$VERCEL_GIT_COMMIT_REF" == "interface" ]] ; then
+#   echo "✅ - Build can proceed"
+#   exit 1;
+# else
+#   echo "🛑 - Build cancelled"
+#   exit 0;
+# fi
