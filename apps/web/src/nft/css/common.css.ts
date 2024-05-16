242dc37fd140b055ce9683915a37aee0d361b993
Gabriele Rigo <gabriele.rigo@gmail.com>
2024-05-16 17:30:21 +0200

    fix search bar background color

--- apps/web/src/nft/css/common.css.ts
+++ apps/web/src/nft/css/common.css.ts
@@ -28,7 +28,7 @@ const magicalGradient = style({
       content: '',
       position: 'absolute',
       inset: '-1px',
-      background: 'linear-gradient(45deg, #FC72FF80 0%, #FC72FF80 100.13%) border-box',
+      background: 'linear-gradient(45deg, #feb23980 0%, #feb23980 100.13%) border-box',
       borderColor: 'transparent',
       WebkitMask: 'linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);',
       WebkitMaskComposite: 'xor;',
