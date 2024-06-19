aee261ae8c275c999f0ce4c295384bb44e12c96e
DevZi1la <developeranon85@gmail.com>
2024-06-19 15:03:07 +0200

    fixes build errors

--- apps/web/craco.config.cjs
+++ apps/web/craco.config.cjs
@@ -1,25 +1,25 @@
 /* eslint-env node */
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin')
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
-const { execSync } = require('child_process')
-const { readFileSync } = require('fs')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
-const { IgnorePlugin, ProvidePlugin } = require('webpack')
-const { RetryChunkLoadPlugin } = require('webpack-retry-chunk-load-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const { execSync } = require("child_process");
+const { readFileSync } = require("fs");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
+const { IgnorePlugin, ProvidePlugin } = require("webpack");
+const { RetryChunkLoadPlugin } = require("webpack-retry-chunk-load-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-const commitHash = execSync('git rev-parse HEAD').toString().trim()
-const isProduction = process.env.NODE_ENV === 'production'
+const commitHash = execSync("git rev-parse HEAD").toString().trim();
+const isProduction = process.env.NODE_ENV === "production";
 
-process.env.REACT_APP_GIT_COMMIT_HASH = commitHash
+process.env.REACT_APP_GIT_COMMIT_HASH = commitHash;
 
 // Static analyses (linting and type check) are only necessary as part of local development.
-const enableStaticAnalysis = !process.env.CI && !isProduction
+const enableStaticAnalysis = !process.env.CI && !isProduction;
 
 // Our .swcrc wasn't being picked up in the monorepo, so we load it directly.
-const swcrc = JSON.parse(readFileSync('./.swcrc', 'utf-8'))
+const swcrc = JSON.parse(readFileSync("./.swcrc", "utf-8"));
 
 // Add all node modules that have to be compiled
 const compileNodeModules = [
@@ -27,13 +27,13 @@ const compileNodeModules = [
   // in the library to code that doesn't use JSX syntax. This file extension is not automatically
   // recognized as extension for files containing JSX, so we have to manually add them to
   // the build proess (to the appropriate loader) and don't exclude them with other node_modules
-  'expo-clipboard',
-  'expo-linear-gradient',
-]
+  "expo-clipboard",
+  "expo-linear-gradient",
+];
 
 function getCacheDirectory(cacheName) {
   // Include the trailing slash to denote that this is a directory.
-  return `${path.join(__dirname, 'node_modules/.cache/', cacheName)}/`
+  return `${path.join(__dirname, "node_modules/.cache/", cacheName)}/`;
 }
 
 module.exports = {
@@ -42,14 +42,14 @@ module.exports = {
     pluginOptions(eslintConfig) {
       return Object.assign(eslintConfig, {
         cache: true,
-        cacheLocation: getCacheDirectory('eslint'),
-        ignorePath: '.gitignore',
+        cacheLocation: getCacheDirectory("eslint"),
+        ignorePath: ".gitignore",
         // Use our own eslint/plugins/config, as overrides interfere with caching.
         // This ensures that `yarn start` and `yarn lint` share one cache.
-        eslintPath: require.resolve('eslint'),
+        eslintPath: require.resolve("eslint"),
         resolvePluginsRelativeTo: null,
         baseConfig: null,
-      })
+      });
     },
   },
   typescript: {
@@ -61,26 +61,31 @@ module.exports = {
         globals: {
           __DEV__: true,
         },
-        cacheDirectory: getCacheDirectory('jest'),
+        cacheDirectory: getCacheDirectory("jest"),
         transform: {
-          ...Object.entries(jestConfig.transform).reduce((transform, [key, value]) => {
-            if (value.match(/babel/)) return transform
-            return { ...transform, [key]: value }
-          }, {}),
+          ...Object.entries(jestConfig.transform).reduce(
+            (transform, [key, value]) => {
+              if (value.match(/babel/)) return transform;
+              return { ...transform, [key]: value };
+            },
+            {}
+          ),
           // Transform vanilla-extract using its own transformer.
           // See https://sandroroth.com/blog/vanilla-extract-cra#jest-transform.
-          '\\.css\\.ts$': '@vanilla-extract/jest-transform',
-          '\\.(t|j)sx?$': ['@swc/jest', swcrc],
+          "\\.css\\.ts$": "@vanilla-extract/jest-transform",
+          "\\.(t|j)sx?$": ["@swc/jest", swcrc],
         },
         // Use d3-arrays's build directly, as jest does not support its exports.
-        transformIgnorePatterns: ['d3-array'],
+        transformIgnorePatterns: ["d3-array"],
         moduleNameMapper: {
-          'd3-array': 'd3-array/dist/d3-array.min.js',
-          '^react-native$': 'react-native-web',
-          'react-native-gesture-handler': require.resolve('react-native-gesture-handler'),
+          "d3-array": "d3-array/dist/d3-array.min.js",
+          "^react-native$": "react-native-web",
+          "react-native-gesture-handler": require.resolve(
+            "react-native-gesture-handler"
+          ),
         },
-        setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-      })
+        setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+      });
     },
   },
   webpack: {
@@ -88,7 +93,7 @@ module.exports = {
       // Webpack 5 does not polyfill node globals, so we do so for those necessary:
       new ProvidePlugin({
         // - react-markdown requires process.cwd
-        process: 'process/browser.js',
+        process: "process/browser.js",
       }),
       new VanillaExtractPlugin(),
       new RetryChunkLoadPlugin({
@@ -107,9 +112,9 @@ module.exports = {
         // do bundle analysis
         webpackConfig.plugins.push(
           new BundleAnalyzerPlugin({
-            analyzerMode: 'json',
+            analyzerMode: "json",
           })
-        )
+        );
       }
 
       // Configure webpack plugins:
@@ -118,101 +123,121 @@ module.exports = {
           // CSS ordering is mitigated through scoping / naming conventions, so we can ignore order warnings.
           // See https://webpack.js.org/plugins/mini-css-extract-plugin/#remove-order-warnings.
           if (plugin instanceof MiniCssExtractPlugin) {
-            plugin.options.ignoreOrder = true
+            plugin.options.ignoreOrder = true;
           }
 
           // Disable TypeScript's config overwrite, as it interferes with incremental build caching.
           // This ensures that `yarn start` and `yarn typecheck` share one cache.
-          if (plugin.constructor.name == 'ForkTsCheckerWebpackPlugin') {
-            delete plugin.options.typescript.configOverwrite
-            plugin.options.typescript.build = true
+          if (plugin.constructor.name == "ForkTsCheckerWebpackPlugin") {
+            delete plugin.options.typescript.configOverwrite;
+            plugin.options.typescript.build = true;
           }
 
-          return plugin
+          return plugin;
         })
         .filter((plugin) => {
           // Case sensitive paths are already enforced by TypeScript.
           // See https://www.typescriptlang.org/tsconfig#forceConsistentCasingInFileNames.
-          if (plugin instanceof CaseSensitivePathsPlugin) return false
+          if (plugin instanceof CaseSensitivePathsPlugin) return false;
 
           // IgnorePlugin is used to tree-shake moment locales, but we do not use moment in this project.
-          if (plugin instanceof IgnorePlugin) return false
+          if (plugin instanceof IgnorePlugin) return false;
 
-          return true
-        })
+          return true;
+        });
 
       // Configure webpack resolution:
       webpackConfig.resolve = Object.assign(webpackConfig.resolve, {
         alias: {
           ...webpackConfig.resolve.alias,
-          '@web3-react/core': path.resolve(__dirname, 'src/connection/web3reactShim.ts'),
-          crypto: require.resolve('expo-crypto'),
-          'react-native-gesture-handler$': require.resolve('react-native-gesture-handler'),
-          'react-native-svg$': require.resolve('@tamagui/react-native-svg'),
-          'react-native$': 'react-native-web',
+          "@web3-react/core": path.resolve(
+            __dirname,
+            "src/connection/web3reactShim.ts"
+          ),
+          crypto: require.resolve("expo-crypto"),
+          "react-native-gesture-handler$": require.resolve(
+            "react-native-gesture-handler"
+          ),
+          "react-native-svg$": require.resolve("@tamagui/react-native-svg"),
+          "react-native$": "react-native-web",
         },
         plugins: webpackConfig.resolve.plugins.map((plugin) => {
           // Allow vanilla-extract in production builds.
           // This is necessary because create-react-app guards against external imports.
           // See https://sandroroth.com/blog/vanilla-extract-cra#production-build.
           if (plugin instanceof ModuleScopePlugin) {
-            plugin.allowedPaths.push(path.join(__dirname, '..', '..', 'node_modules/@vanilla-extract/webpack-plugin'))
+            plugin.allowedPaths.push(
+              path.join(
+                __dirname,
+                "..",
+                "..",
+                "node_modules/@vanilla-extract/webpack-plugin"
+              )
+            );
           }
 
-          return plugin
+          return plugin;
         }),
         // Webpack 5 does not resolve node modules, so we do so for those necessary:
         fallback: {
           // - react-markdown requires path
-          path: require.resolve('path-browserify'),
+          path: require.resolve("path-browserify"),
+          assert: false,
+          http: false,
+          https: false,
+          stream: false,
+          fs: require.resolve("browserify-fs"),
         },
-      })
+      });
 
       // Retain source maps for node_modules packages:
       webpackConfig.module.rules[0] = {
         ...webpackConfig.module.rules[0],
         exclude: /node_modules/,
-      }
+      };
 
       // Configure webpack transpilation (create-react-app specifies transpilation rules in a oneOf):
-      webpackConfig.module.rules[1].oneOf = webpackConfig.module.rules[1].oneOf.map((rule) => {
-        if (rule.loader && rule.loader.match(/babel-loader/)) {
-          rule.loader = 'swc-loader'
-          rule.options = swcrc
+      webpackConfig.module.rules[1].oneOf =
+        webpackConfig.module.rules[1].oneOf.map((rule) => {
+          if (rule.loader && rule.loader.match(/babel-loader/)) {
+            rule.loader = "swc-loader";
+            rule.options = swcrc;
 
-          rule.include = (inPath) => {
-            // if not a node_module (except for the ones we want to compile)
-            // we parse with SWC (so other packages in monorepo are importable)
-            if (inPath.match(/node_modules/)) {
-              return compileNodeModules.some((nodeModule) => inPath.includes(nodeModule))
-            }
-            return true
+            rule.include = (inPath) => {
+              // if not a node_module (except for the ones we want to compile)
+              // we parse with SWC (so other packages in monorepo are importable)
+              if (inPath.match(/node_modules/)) {
+                return compileNodeModules.some((nodeModule) =>
+                  inPath.includes(nodeModule)
+                );
+              }
+              return true;
+            };
           }
-        }
-        return rule
-      })
+          return rule;
+        });
 
       // since wallet package uses react-native-dotenv and that needs a babel plugin
       // adding this before the swc loader
       webpackConfig.module.rules[1].oneOf.unshift({
-        loader: 'babel-loader',
+        loader: "babel-loader",
         include: (path) => /uniswap\/src.*\.(js|ts)x?$/.test(path),
         options: {
-          presets: ['module:@react-native/babel-preset'],
+          presets: ["module:@react-native/babel-preset"],
           plugins: [
             [
-              'module:react-native-dotenv',
+              "module:react-native-dotenv",
               {
                 // ideally use envName here to add a mobile namespace but this doesn't work when sharing with dotenv-webpack
-                moduleName: 'react-native-dotenv',
-                path: '../../.env.defaults', // must use this path so this file can be shared with web since dotenv-webpack is less flexible
+                moduleName: "react-native-dotenv",
+                path: "../../.env.defaults", // must use this path so this file can be shared with web since dotenv-webpack is less flexible
                 safe: true,
                 allowUndefined: false,
               },
             ],
           ],
         },
-      })
+      });
 
       // TODO(WEB-3632): Tamagui linear gradient isn't fully-specified, temporary
       webpackConfig.module.rules.unshift({
@@ -220,18 +245,18 @@ module.exports = {
         resolve: {
           fullySpecified: false,
         },
-      })
+      });
 
       // Run terser compression on node_modules before tree-shaking, so that tree-shaking is more effective.
       // This works by eliminating dead code, so that webpack can identify unused imports and tree-shake them;
       // it is only necessary for node_modules - it is done through linting for our own source code -
       // see https://medium.com/engineering-housing/dead-code-elimination-and-tree-shaking-at-housing-part-1-307a94b30f23#7e03:
       webpackConfig.module.rules.push({
-        enforce: 'post',
+        enforce: "post",
         test: /node_modules.*\.(js)$/,
-        loader: path.join(__dirname, 'scripts/terser-loader.js'),
+        loader: path.join(__dirname, "scripts/terser-loader.js"),
         options: { compress: true, mangle: false },
-      })
+      });
 
       // Configure webpack optimization:
       webpackConfig.optimization = Object.assign(
@@ -239,15 +264,17 @@ module.exports = {
         isProduction
           ? {
               // Optimize over all chunks, instead of async chunks (the default), so that initial chunks are also included.
-              splitChunks: { chunks: 'all' },
+              splitChunks: { chunks: "all" },
             }
           : {}
-      )
+      );
 
       // Configure webpack resolution. webpackConfig.cache is unused with swc-loader, but the resolver can still cache:
-      webpackConfig.resolve = Object.assign(webpackConfig.resolve, { unsafeCache: true })
+      webpackConfig.resolve = Object.assign(webpackConfig.resolve, {
+        unsafeCache: true,
+      });
 
-      return webpackConfig
+      return webpackConfig;
     },
   },
-}
+};
--- apps/web/src/i18n.tsx
+++ apps/web/src/i18n.tsx
@@ -1,55 +1,58 @@
-import { dynamicActivate } from 'i18n/dynamicActivate'
-import { initialLocale } from 'i18n/initialLocale'
-import i18n from 'i18next'
-import resourcesToBackend from 'i18next-resources-to-backend'
-import { initReactI18next } from 'react-i18next'
-import { logger } from 'utilities/src/logger/logger'
-import enUsLocale from './i18n/locales/source/en-US.json'
+import { dynamicActivate } from "i18n/dynamicActivate";
+import { initialLocale } from "i18n/initialLocale";
+import i18n from "i18next";
+import resourcesToBackend from "i18next-resources-to-backend";
+import { initReactI18next } from "react-i18next";
+import { logger } from "utilities/src/logger/logger";
+import enUsLocale from "./i18n/locales/source/en-US.json";
 
-export { t } from 'i18next'
-export { Plural } from './i18n/Plural'
-export { Trans } from './i18n/Trans'
+export { t } from "i18next";
+export { Plural } from "./i18n/Plural";
+export { Trans } from "./i18n/Trans";
 
 i18n
   .use(initReactI18next)
   .use(
     resourcesToBackend((language: string) => {
       // not sure why but it tries to load es THEN es-ES, for any language, but we just want the second
-      if (!language.includes('-')) {
-        return
+      if (!language.includes("-")) {
+        return;
       }
-      if (language === 'en-US') {
-        return enUsLocale
-      }
-      return import(`./i18n/locales/translations/${language}.json`)
+      // if (language === 'en-US') {
+      return enUsLocale;
+      // }
+      // return import(`./i18n/locales/translations/${language}.json`)
     })
   )
-  .on('failedLoading', (language, namespace, msg) => {
-    logger.error(new Error(`Error loading language ${language} ${namespace}: ${msg}`), {
-      tags: {
-        file: 'i18n',
-        function: 'onFailedLoading',
-      },
-    })
-  })
+  .on("failedLoading", (language, namespace, msg) => {
+    logger.error(
+      new Error(`Error loading language ${language} ${namespace}: ${msg}`),
+      {
+        tags: {
+          file: "i18n",
+          function: "onFailedLoading",
+        },
+      }
+    );
+  });
 
 i18n
   .init({
     returnEmptyString: false,
     keySeparator: false,
-    lng: 'en-US',
-    fallbackLng: 'en-US',
+    lng: "en-US",
+    fallbackLng: "en-US",
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
   })
-  .catch(() => undefined)
+  .catch(() => undefined);
 
 // add default english ns right away
-i18n.addResourceBundle('en-US', 'translations', {
-  'en-US': {
+i18n.addResourceBundle("en-US", "translations", {
+  "en-US": {
     translation: enUsLocale,
   },
-})
+});
 
-dynamicActivate(initialLocale)
+dynamicActivate(initialLocale);
