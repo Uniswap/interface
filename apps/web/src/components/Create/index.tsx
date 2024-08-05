1e686dd3c5bb7f9acb8122e0778ccbfb5081bd21
joyfulmagician <joyfulmagician@gmail.com>
2024-08-04 21:29:43 -0400

    validation of ERC20 address

--- apps/web/src/components/Create/index.tsx
+++ apps/web/src/components/Create/index.tsx
@@ -1,14 +1,13 @@
-
-import styled from 'styled-components'
-import { Trans } from 'i18n'
-import { AutoColumn } from 'components/Column'
-import { ThemedText } from 'theme/components'
+import styled from 'styled-components';
+import { Trans } from 'i18n';
+import { AutoColumn } from 'components/Column';
+import { ThemedText } from 'theme/components';
 import { ButtonLight } from "components/Button";
 import { useAccountDrawer } from "components/AccountDrawer/MiniPortfolio/hooks";
-import { useAccount } from 'hooks/useAccount'
+import { useAccount } from 'hooks/useAccount';
 import NetworkTypeMenu from "./NetworkTypeMenu";
 import DatePickerValue from './DatePickerValue';
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import TimePickerValue from './TimePickerValue';
 import { useEffect, useState } from 'react';
@@ -28,7 +27,7 @@ const HeaderText = styled(ThemedText.DeprecatedLabel)`
   ${({ theme }) => theme.deprecated_mediaWidth.deprecated_upToMedium`
     font-size: 16px;
   `};
-`
+`;
 
 const ResponsiveColumn = styled(AutoColumn)`
   grid-template-columns: 1fr;
@@ -40,14 +39,14 @@ const ResponsiveColumn = styled(AutoColumn)`
     gap: 8px;
   `};
   justify-content: space-between;
-`
+`;
 
 const ValueInput = styled.input`
   background-size: 20px 20px;
   background-position: 12px center;
   background-color: ${({ theme }) => theme.surface1};
   border-radius: 20px;
-  border: 1px solid ${({ theme }) => 'gray'}; // Set a consistent border color
+  border: 1px solid ${({ theme }) => 'gray'};
   height: 100%;
   width: 400px;
   font-size: 18px;
@@ -60,7 +59,7 @@ const ValueInput = styled.input`
   :focus {
     outline: none;
     background-color: ${({ theme }) => theme.surface1};
-    border-color: ${({ theme }) => theme.accent1}; // Keep the same border color on focus
+    border-color: ${({ theme }) => theme.accent1};
     color: ${({ theme }) => theme.neutral1};
   }
 
@@ -72,22 +71,23 @@ const ValueInput = styled.input`
     appearance: none;
     cursor: pointer;
   }
-`
+`;
 
 const CustomP = styled.p`
 margin: 0px;
 font-size: 16px;
 display: block;
-`
+`;
+
 const CustomDiv = styled.div`
 margin: 0px;
 gap: 12px;
 display:flex;
 align-items: center;
-`
+`;
 
 export default function Create() {
-    const account = useAccount()
+    const account = useAccount();
     const accountDrawer = useAccountDrawer();
     const today = dayjs().utc().format("YYYY-MM-DD");
     const tomorrow = dayjs().add(1, 'day').utc().format("YYYY-MM-DD");
@@ -101,18 +101,33 @@ export default function Create() {
     const [startDateFormat, setStartDateFormat] = useState("");
     const [endDateFormat, setEndDateFormat] = useState("");
 
-
     useEffect(() => {
         const formattedStartDate = dayjs(startDate).format("MMMM D, YYYY");
         const formattedEndDate = dayjs(endDate).format("MMMM D, YYYY");
         setStartDateFormat(formattedStartDate);
         setEndDateFormat(formattedEndDate);
-    }, [startDate, endDate])
+    }, [startDate, endDate]);
 
-    useEffect(() => {
-        console.log("startTime", startTime);
-        console.log("endTime", endTime);
-    }, [startTime, endTime])
+    const [rewardTokenAddress, setRewardTokenAddress] = useState('');
+    const [borderColor, setBorderColor] = useState('gray');
+
+    const isValidEthereumAddress = (address: string): boolean => {
+        return /^0x[a-fA-F0-9]{40}$/.test(address);
+    };
+
+    const handleRewardTokenAddressChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const value = event.target.value;
+        setRewardTokenAddress(value);
+
+        // Validate the address
+        if (isValidEthereumAddress(value)) {
+            setBorderColor('gray');
+        } else if (value === "") {
+            setBorderColor('gray');
+        } else {
+            setBorderColor('red');
+        }
+    };
 
     return (
         <>
@@ -132,7 +147,12 @@ export default function Create() {
                 <ThemedText.DeprecatedBody style={{ alignItems: 'center', display: 'flex', fontWeight: 485, fontSize: 16 }}>
                     <Trans i18nKey="common.create.incentives.set.token.reward.description" />
                 </ThemedText.DeprecatedBody>
-                <ValueInput placeholder='Reward token address' />
+                <ValueInput
+                    placeholder='Reward token address'
+                    value={rewardTokenAddress}
+                    onChange={handleRewardTokenAddressChange}
+                    style={{ borderColor }} // Apply dynamic border color
+                />
                 <CustomP><Trans i18nKey="common.create.incentives.set.token.reward.explaination" /></CustomP>
             </ResponsiveColumn>
             <ResponsiveColumn>
@@ -172,7 +192,6 @@ export default function Create() {
                     <TimePickerValue labelName="End TIme" onTimeChange={setEndTime} />
                     <CustomP>Ends on {endDateFormat} at {endTime}</CustomP>
                 </CustomDiv>
-
             </ResponsiveColumn>
             <ResponsiveColumn>
                 <HeaderText>
@@ -209,8 +228,6 @@ export default function Create() {
                     </ButtonLight>
                 )
             }
-
         </>
-
-    )
+    );
 }
