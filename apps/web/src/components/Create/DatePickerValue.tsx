7dcbab4f9ab9fc7586ea194545317fab3f958f0e
DevZi1la <developeranon85@gmail.com>
2024-08-19 15:20:08 +0200

    changes template colors to be Taraswap

--- apps/web/src/components/Create/DatePickerValue.tsx
+++ apps/web/src/components/Create/DatePickerValue.tsx
@@ -1,19 +1,23 @@
-import * as React from 'react';
+import * as React from "react";
 import { styled } from "@mui/system";
-import dayjs, { Dayjs } from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { useEffect } from 'react';
+import dayjs, { Dayjs } from "dayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useEffect } from "react";
 
 interface DateValueProps {
-  date: string
-  labelName: string
+  date: string;
+  labelName: string;
   onDateChange: (newDate: string) => void;
 }
 
-export default function DatePickerValue({ date, labelName, onDateChange }: DateValueProps) {
+export default function DatePickerValue({
+  date,
+  labelName,
+  onDateChange,
+}: DateValueProps) {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs(date));
 
   useEffect(() => {
@@ -24,37 +28,37 @@ export default function DatePickerValue({ date, labelName, onDateChange }: DateV
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DatePicker', 'DatePicker']}>
+      <DemoContainer components={["DatePicker", "DatePicker"]}>
         <DatePicker
           label={labelName}
           value={value}
           onChange={(newValue) => setValue(newValue)}
           sx={{
-            '& .MuiOutlinedInput-root': {
-              borderRadius: '20px',
-              '& fieldset': {
-                borderColor: 'gray',
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "20px",
+              "& fieldset": {
+                borderColor: "gray",
               },
-              '&:hover fieldset': {
-                borderColor: 'gray',
+              "&:hover fieldset": {
+                borderColor: "gray",
               },
-              '&.Mui-focused fieldset': {
-                borderColor: '#FC72FF',
-                borderWidth: '1px',
+              "&.Mui-focused fieldset": {
+                borderColor: "#15AC5B",
+                borderWidth: "1px",
               },
             },
-            '& .MuiInputBase-root': {
-              fontSize: '18px',
+            "& .MuiInputBase-root": {
+              fontSize: "18px",
               fontWeight: 485,
-              color: '#7D7D7D',
+              color: "#7D7D7D",
             },
-            '& .MuiInputLabel-root': {
-              fontSize: '18px',
+            "& .MuiInputLabel-root": {
+              fontSize: "18px",
               fontWeight: 485,
-              color: '#7D7D7D',
+              color: "#7D7D7D",
             },
-            '& .MuiSvgIcon-root': {
-              color: '#7D7D7D',
+            "& .MuiSvgIcon-root": {
+              color: "#7D7D7D",
             },
             // '& .MuiInputBase-root': {
             //   borderRadius: '12px', // This might also be needed to ensure the input part has the same border radius
