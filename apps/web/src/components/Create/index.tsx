ac5bc9442d52a2ce3ce8eabe75a2ef4c658d035e
sprintowl <florin@sprintowl.com>
2024-09-06 17:29:41 +0300

    Fixed Create page form

--- apps/web/src/components/Create/index.tsx
+++ apps/web/src/components/Create/index.tsx
@@ -14,11 +14,10 @@ import { useEffect, useState } from "react";
 import { Flex } from "ui/src";
 import { BigNumber, ethers } from "ethers";
 
-import { Token } from "@taraswap/sdk-core";
 import { useChainId } from "wagmi";
-import { useTokenAllowance } from "hooks/useTokenAllowance";
 import { STAKER_ADDRESS, useV3StakerContract } from "hooks/useV3StakerContract";
 import { useTokenContract } from "hooks/useContract";
+import { useNavigate } from "react-router-dom";
 
 dayjs.extend(utc);
 
@@ -137,6 +136,7 @@ function InputField({
 
 export default function Create() {
   const account = useAccount();
+  const navigate = useNavigate();
   const accountDrawer = useAccountDrawer();
   const today = dayjs().utc().format("YYYY-MM-DD");
   const tomorrow = dayjs().add(1, "day").utc().format("YYYY-MM-DD");
@@ -157,15 +157,29 @@ export default function Create() {
     dayjs(tomorrow).format("MMMM D, YYYY")
   );
 
-  const [rewardTokenAddress, setRewardTokenAddress] = useState("");
-  const [tokenSymbol, setTokenSymbol] = useState("");
-  const [tokenDecimals, setTokenDecimals] = useState(18);
-  const [rewardsAmount, setRewardsAmount] = useState("");
-  const [vestingPeriod, setVestingPeriod] = useState("");
-  const [poolAddress, setPoolAddress] = useState("");
-  const [refundeeAddress, setRefundeeAddress] = useState("");
-  const [tokenAllowance, setTokenAllowance] = useState(BigNumber.from(0));
-  const [approvalComplete, setApprovalComplete] = useState(false);
+  const [shouldFetchAllowance, setShouldFetchAllowance] = useState(1);
+
+  const [formData, setFormData] = useState({
+    rewardTokenAddress: "",
+    tokenSymbol: "",
+    tokenDecimals: 18,
+    rewardsAmount: "",
+    vestingPeriod: "",
+    poolAddress: "",
+    refundeeAddress: "",
+    approvalComplete: false,
+    tokenAllowance: ethers.BigNumber.from(0),
+  });
+
+  const [isAproving, setIsAproving] = useState(false);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const updateFormData = (
+    field: string,
+    value: string | number | boolean | BigNumber
+  ) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+  };
 
   const [errorMessages, setErrorMessages] = useState({
     rewardToken: "",
@@ -176,14 +190,14 @@ export default function Create() {
     date: "",
   });
 
-  const tokenContract = useTokenContract(rewardTokenAddress);
+  const tokenContract = useTokenContract(formData.rewardTokenAddress);
 
   const approveToken = async (amount: BigNumber) => {
     if (tokenContract) {
       try {
         const tx = await tokenContract.approve(STAKER_ADDRESS, amount);
         await tx.wait();
-        setApprovalComplete(true);
+        updateFormData("approvalComplete", true);
       } catch (error) {
         console.error(error);
         alert("Failed to approve token: " + error.message);
@@ -193,39 +207,42 @@ export default function Create() {
 
   useEffect(() => {
     if (tokenContract) {
-      tokenContract.symbol().then(setTokenSymbol);
+      Promise.all([tokenContract.symbol(), tokenContract.decimals()])
+        .then(([symbol, decimals]) => {
+          updateFormData("tokenSymbol", symbol);
+          updateFormData("tokenDecimals", decimals);
+        })
+        .catch(console.error);
     }
   }, [tokenContract]);
 
   useEffect(() => {
-    if (tokenContract) {
-      tokenContract.decimals().then(setTokenDecimals);
+    const requiredAmount = ethers.utils.parseUnits(
+      formData.rewardsAmount || "0",
+      formData.tokenDecimals
+    );
+    if (formData.tokenAllowance.gte(requiredAmount)) {
+      updateFormData("approvalComplete", true);
     }
-  }, [tokenContract]);
-
-  useEffect(() => {
-    if (
-      tokenAllowance.gte(
-        ethers.utils.parseUnits(rewardsAmount || "0", tokenDecimals)
-      )
-    ) {
-      setApprovalComplete(true);
-    }
-  }, [tokenAllowance, rewardsAmount, tokenDecimals]);
+  }, [formData.tokenAllowance, formData.rewardsAmount, formData.tokenDecimals]);
 
   useEffect(() => {
     const fetchAllowance = async () => {
       if (tokenContract && account.address) {
-        const allowance = await tokenContract.allowance(
-          account.address,
-          STAKER_ADDRESS
-        );
-        setTokenAllowance(allowance);
-        console.log("tokenAllowance", tokenAllowance);
+        try {
+          const allowance = await tokenContract.allowance(
+            account.address,
+            STAKER_ADDRESS
+          );
+          updateFormData("tokenAllowance", allowance);
+          console.log("tokenAllowance", allowance);
+        } catch (error) {
+          console.error("Error fetching token allowance:", error);
+        }
       }
     };
     fetchAllowance();
-  }, [tokenContract, account.address, chainId, approvalComplete]);
+  }, [tokenContract, account.address, chainId, shouldFetchAllowance]);
 
   useEffect(() => {
     setFormattedStartDate(dayjs(startDate).format("MMMM D, YYYY"));
@@ -245,33 +262,37 @@ export default function Create() {
     }));
   };
 
-  type SetterFunction = React.Dispatch<React.SetStateAction<string>>;
+  type InputChangeHandler = (
+    field: keyof typeof formData,
+    value: string | number | boolean | BigNumber
+  ) => void;
   type ValidatorFunction = (value: string) => boolean;
 
-  const handleInputChange = (
-    setter: SetterFunction,
+  const handleInputChange: InputChangeHandler = (field, value) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+  };
+
+  const handleGenericInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: keyof typeof formData,
     validator: ValidatorFunction,
-    errorKey: keyof typeof errorMessages,
-    event: React.ChangeEvent<HTMLInputElement>
+    errorKey: keyof typeof errorMessages
   ) => {
     const value = event.target.value;
-    setter(value);
+    handleInputChange(field, value);
 
     setErrorMessages((prev) => {
       if (value === "") {
         return {
           ...prev,
-          [errorKey]: `The ${errorKey.replace(
-            /[A-Z]/g,
-            (match) => " " + match.toLowerCase()
-          )} is required.`,
+          [errorKey]: `The ${field} is required.`,
         };
       } else {
         return {
           ...prev,
           [errorKey]: validator(value)
             ? ""
-            : `common.create.incentives.set.token.notValid.${errorKey}`,
+            : `common.create.incentives.set.token.notValid.${field}`,
         };
       }
     });
@@ -280,75 +301,80 @@ export default function Create() {
   const handleRewardTokenAddressChange = (
     e: React.ChangeEvent<HTMLInputElement>
   ) =>
-    handleInputChange(
-      setRewardTokenAddress,
+    handleGenericInputChange(
+      e,
+      "rewardTokenAddress",
       isValidEthereumAddress,
-      "rewardToken",
-      e
+      "rewardToken"
     );
 
   const handlePoolAddressChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    handleInputChange(setPoolAddress, isValidEthereumAddress, "poolAddress", e);
+    handleGenericInputChange(
+      e,
+      "poolAddress",
+      isValidEthereumAddress,
+      "poolAddress"
+    );
 
   const handleRefundeeAddressChange = (
     e: React.ChangeEvent<HTMLInputElement>
   ) =>
-    handleInputChange(
-      setRefundeeAddress,
-      isValidEthereumAddress,
+    handleGenericInputChange(
+      e,
       "refundeeAddress",
-      e
+      isValidEthereumAddress,
+      "refundeeAddress"
     );
 
   const handleRewardsAmountChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    handleInputChange(
-      setRewardsAmount,
-      (val) => /^\d*\.?\d*$/.test(val),
+    handleGenericInputChange(
+      e,
       "rewardsAmount",
-      e
+      (value) => /^\d*\.?\d*$/.test(value),
+      "rewardsAmount"
     );
 
   const handleVestingPeriodChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    handleInputChange(
-      setVestingPeriod,
-      (val) => /^\d*\.?\d*$/.test(val),
+    handleGenericInputChange(
+      e,
       "vestingPeriod",
-      e
+      (value) => /^\d+$/.test(value),
+      "vestingPeriod"
     );
 
   const createIncentive = async () => {
-    if (!rewardTokenAddress)
+    if (!formData.rewardTokenAddress)
       setErrorMessages((prev) => ({
         ...prev,
         rewardToken: "The reward token address is required.",
       }));
-    if (!rewardsAmount)
+    if (!formData.rewardsAmount)
       setErrorMessages((prev) => ({
         ...prev,
         rewardsAmount: "The reward amount is required.",
       }));
-    if (!poolAddress)
+    if (!formData.poolAddress)
       setErrorMessages((prev) => ({
         ...prev,
         poolAddress: "The pool address is required.",
       }));
-    if (!vestingPeriod)
+    if (!formData.vestingPeriod)
       setErrorMessages((prev) => ({
         ...prev,
         vestingPeriod: "The vesting period is required.",
       }));
-    if (!refundeeAddress)
+    if (!formData.refundeeAddress)
       setErrorMessages((prev) => ({
         ...prev,
         refundeeAddress: "The refundee address is required.",
       }));
 
     if (
-      rewardTokenAddress &&
-      rewardsAmount &&
-      poolAddress &&
-      vestingPeriod &&
-      refundeeAddress
+      formData.rewardTokenAddress &&
+      formData.rewardsAmount &&
+      formData.poolAddress &&
+      formData.vestingPeriod &&
+      formData.refundeeAddress
     ) {
       try {
         const startedTime = Math.floor(
@@ -357,32 +383,69 @@ export default function Create() {
         const endedTime = Math.floor(
           new Date(`${endDate} ${endTime}`).getTime() / 1000
         );
-        const reward = ethers.utils.parseUnits(rewardsAmount, tokenDecimals);
+        const reward = ethers.utils.parseUnits(
+          formData.rewardsAmount,
+          formData.tokenDecimals
+        );
         const incentiveKey = {
-          rewardToken: rewardTokenAddress,
-          pool: poolAddress,
+          rewardToken: formData.rewardTokenAddress,
+          pool: formData.poolAddress,
           startTime: startedTime,
           endTime: endedTime,
-          vestingPeriod: vestingPeriod,
-          refundee: refundeeAddress,
+          vestingPeriod: formData.vestingPeriod,
+          refundee: formData.refundeeAddress,
         };
 
         if (v3StakerContract) {
+          setIsCreating(true);
           const tx = await v3StakerContract.createIncentive(
             incentiveKey,
             reward
           );
           await tx.wait();
 
-          alert("Incentive created successfully!");
+          setIsCreating(false);
+          // Reset form data
+          setFormData({
+            rewardTokenAddress: "",
+            tokenSymbol: "",
+            tokenDecimals: 18,
+            rewardsAmount: "",
+            vestingPeriod: "",
+            poolAddress: "",
+            refundeeAddress: "",
+            approvalComplete: false,
+            tokenAllowance: ethers.BigNumber.from(0),
+          })
+          navigate({
+            pathname: "/farms",
+          });
         } else {
           alert("Contract is not available.");
         }
       } catch (error) {
         console.error(error);
         alert("Failed to create incentive: " + error.message);
+        setIsCreating(false);
       }
     }
+  };
+
+  const approve = async () => {
+    setIsAproving(true);
+    approveToken(
+      ethers.utils.parseUnits(
+        formData.rewardsAmount || "0",
+        formData.tokenDecimals
+      )
+    )
+      .then(() => {
+        setShouldFetchAllowance((prev) => prev + 1);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => {
+        setIsAproving(false);
+      });
   };
 
   return (
@@ -428,7 +491,7 @@ export default function Create() {
         </ThemedText.DeprecatedBody>
         <InputField
           placeholder="Reward token address"
-          value={rewardTokenAddress}
+          value={formData.rewardTokenAddress}
           onChange={handleRewardTokenAddressChange}
           error={!!errorMessages.rewardToken}
           errorMessage={errorMessages.rewardToken}
@@ -451,7 +514,7 @@ export default function Create() {
         </ThemedText.DeprecatedBody>
         <InputField
           placeholder="Rewards amount"
-          value={rewardsAmount}
+          value={formData.rewardsAmount}
           onChange={handleRewardsAmountChange}
           error={!!errorMessages.rewardsAmount}
           errorMessage={errorMessages.rewardsAmount}
@@ -474,7 +537,7 @@ export default function Create() {
         </ThemedText.DeprecatedBody>
         <InputField
           placeholder="Pool Address"
-          value={poolAddress}
+          value={formData.poolAddress}
           onChange={handlePoolAddressChange}
           error={!!errorMessages.poolAddress}
           errorMessage={errorMessages.poolAddress}
@@ -560,7 +623,7 @@ export default function Create() {
         </ThemedText.DeprecatedBody>
         <InputField
           placeholder="Vesting period in days"
-          value={vestingPeriod}
+          value={formData.vestingPeriod}
           onChange={handleVestingPeriodChange}
           error={!!errorMessages.vestingPeriod}
           errorMessage={errorMessages.vestingPeriod}
@@ -583,7 +646,7 @@ export default function Create() {
         </ThemedText.DeprecatedBody>
         <InputField
           placeholder="Refundee address"
-          value={refundeeAddress}
+          value={formData.refundeeAddress}
           onChange={handleRefundeeAddressChange}
           error={!!errorMessages.refundeeAddress}
           errorMessage={errorMessages.refundeeAddress}
@@ -600,9 +663,12 @@ export default function Create() {
         >
           <Trans i18nKey="common.connectWallet.button" />
         </ButtonLight>
-      ) : tokenAllowance &&
-        tokenAllowance.gte(
-          ethers.utils.parseUnits(rewardsAmount || "0", tokenDecimals)
+      ) : formData.tokenAllowance &&
+        formData.tokenAllowance.gte(
+          ethers.utils.parseUnits(
+            formData.rewardsAmount || "0",
+            formData.tokenDecimals
+          )
         ) ? (
         <ButtonLight
           fontWeight={535}
@@ -610,9 +676,36 @@ export default function Create() {
           marginTop={2}
           marginBottom={3}
           onClick={createIncentive}
-          disabled={!approvalComplete || !rewardTokenAddress}
+          disabled={
+            !formData.approvalComplete ||
+            !formData.rewardTokenAddress ||
+            isCreating
+          }
         >
-          <Trans i18nKey="common.incentives.create.button" />
+          {isCreating ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+              style={{ marginRight: "8px" }}
+            >
+              <path
+                fill="currentColor"
+                d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  dur="0.75s"
+                  repeatCount="indefinite"
+                  type="rotate"
+                  values="0 12 12;360 12 12"
+                />
+              </path>
+            </svg>
+          ) : (
+            <Trans i18nKey="common.incentives.create.button" />
+          )}
         </ButtonLight>
       ) : (
         <ButtonLight
@@ -620,16 +713,36 @@ export default function Create() {
           $borderRadius="16px"
           marginTop={2}
           marginBottom={3}
-          onClick={() =>
-            approveToken(
-              ethers.utils.parseUnits(rewardsAmount || "0", tokenDecimals)
-            )
-          }
+          onClick={approve}
+          disabled={isAproving}
         >
-          <Trans
-            i18nKey="common.incentives.approve.button"
-            values={{ tokenSymbol }}
-          />
+          {isAproving ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+              style={{ marginRight: "8px" }}
+            >
+              <path
+                fill="currentColor"
+                d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  dur="0.75s"
+                  repeatCount="indefinite"
+                  type="rotate"
+                  values="0 12 12;360 12 12"
+                />
+              </path>
+            </svg>
+          ) : (
+            <Trans
+              i18nKey="common.incentives.approve.button"
+              values={{ tokenSymbol: formData.tokenSymbol }}
+            />
+          )}
         </ButtonLight>
       )}
     </Flex>
