0f1e0ca0bc1609fe7c666aa62b383bcc828bb017
DevZi1la <developeranon85@gmail.com>
2024-10-10 12:42:49 +0200

    removes uniswap calls

--- apps/web/src/hooks/useTransactionGasFee.ts
+++ apps/web/src/hooks/useTransactionGasFee.ts
@@ -1,70 +1,73 @@
-import { TransactionRequest } from '@ethersproject/abstract-provider'
-import { useCallback, useMemo } from 'react'
-import { useAsyncData } from 'utilities/src/react/hooks'
+import { TransactionRequest } from "@ethersproject/abstract-provider";
+import { useCallback, useMemo } from "react";
+import { useAsyncData } from "utilities/src/react/hooks";
 
 enum FeeType {
-  Legacy = 'legacy',
-  Eip1559 = 'eip1559',
+  Legacy = "legacy",
+  Eip1559 = "eip1559",
 }
 
 interface GasFeeResponseBase {
-  type: FeeType
-  gasLimit: string
+  type: FeeType;
+  gasLimit: string;
   gasFee: {
-    normal: string
-    fast: string
-    urgent: string
-  }
+    normal: string;
+    fast: string;
+    urgent: string;
+  };
 }
 
 interface GasFeeResponseEip1559 extends GasFeeResponseBase {
-  type: FeeType.Eip1559
+  type: FeeType.Eip1559;
   maxFeePerGas: {
-    normal: string
-    fast: string
-    urgent: string
-  }
+    normal: string;
+    fast: string;
+    urgent: string;
+  };
   maxPriorityFeePerGas: {
-    normal: string
-    fast: string
-    urgent: string
-  }
+    normal: string;
+    fast: string;
+    urgent: string;
+  };
 }
 
 interface GasFeeResponseLegacy extends GasFeeResponseBase {
-  type: FeeType.Legacy
+  type: FeeType.Legacy;
   gasPrice: {
-    normal: string
-    fast: string
-    urgent: string
-  }
+    normal: string;
+    fast: string;
+    urgent: string;
+  };
 }
 
 type TransactionLegacyFeeParams = {
-  gasPrice: string
-  gasLimit: string
-}
+  gasPrice: string;
+  gasLimit: string;
+};
 
 type TransactionEip1559FeeParams = {
-  maxFeePerGas: string
-  maxPriorityFeePerGas: string
-  gasLimit: string
-}
+  maxFeePerGas: string;
+  maxPriorityFeePerGas: string;
+  gasLimit: string;
+};
 
 export interface GasFeeResult {
-  value?: string
-  isLoading: boolean
-  params?: TransactionLegacyFeeParams | TransactionEip1559FeeParams
+  value?: string;
+  isLoading: boolean;
+  params?: TransactionLegacyFeeParams | TransactionEip1559FeeParams;
 }
 
 // GasFeeResponse is the type that comes directly from the Gas Service API
-type GasFeeResponseError = { errorCode: string; detail?: string; id?: string }
-type GasFeeResponse = GasFeeResponseEip1559 | GasFeeResponseLegacy | GasFeeResponseError
+type GasFeeResponseError = { errorCode: string; detail?: string; id?: string };
+type GasFeeResponse =
+  | GasFeeResponseEip1559
+  | GasFeeResponseLegacy
+  | GasFeeResponseError;
 
 export enum GasSpeed {
-  Normal = 'normal',
-  Fast = 'fast',
-  Urgent = 'urgent',
+  Normal = "normal",
+  Fast = "fast",
+  Urgent = "urgent",
 }
 
 export function useTransactionGasFee(
@@ -72,58 +75,63 @@ export function useTransactionGasFee(
   speed: GasSpeed = GasSpeed.Urgent,
   skip: boolean = !tx
 ): GasFeeResult {
-  const gasFeeFetcher = useGasFeeQuery(tx, skip)
-  const { data, isLoading } = useAsyncData(gasFeeFetcher)
+  // const gasFeeFetcher = useGasFeeQuery(tx, skip);
+  // const { data, isLoading } = useAsyncData(gasFeeFetcher);
 
   return useMemo(() => {
-    if (!data) {
-      return { isLoading }
-    }
+    // if (!data) {
+    //   return { isLoading };
+    // }
 
-    const params =
-      data.type === FeeType.Eip1559
-        ? {
-            maxPriorityFeePerGas: data.maxPriorityFeePerGas[speed],
-            maxFeePerGas: data.maxFeePerGas[speed],
-            gasLimit: data.gasLimit,
-          }
-        : {
-            gasPrice: data.gasPrice[speed],
-            gasLimit: data.gasLimit,
-          }
+    // const params =
+    //   data.type === FeeType.Eip1559
+    //     ? {
+    //         maxPriorityFeePerGas: data.maxPriorityFeePerGas[speed],
+    //         maxFeePerGas: data.maxFeePerGas[speed],
+    //         gasLimit: data.gasLimit,
+    //       }
+    //     : {
+    //         gasPrice: "1",
+    //         gasLimit: "15000000",
+    //       };
 
     return {
-      value: data.gasFee[speed],
-      isLoading,
-      params,
-    }
-  }, [data, isLoading, speed])
+      value: "1",
+      isLoading: false,
+      params: {
+        gasPrice: "1",
+        gasLimit: "15000000",
+      },
+    };
+  }, [speed]);
 }
 
-const UNISWAP_API_URL = process.env.REACT_APP_UNISWAP_BASE_API_URL
+const UNISWAP_API_URL = process.env.REACT_APP_UNISWAP_BASE_API_URL;
 
-const isErrorResponse = (res: Response, gasFee: GasFeeResponse): gasFee is GasFeeResponseError =>
-  res.status < 200 || res.status > 202
+const isErrorResponse = (
+  res: Response,
+  gasFee: GasFeeResponse
+): gasFee is GasFeeResponseError => res.status < 200 || res.status > 202;
 
-function useGasFeeQuery(tx?: TransactionRequest, skip: boolean = !tx) {
-  const gasFeeFetcher = useCallback(async () => {
-    if (skip) {
-      return
-    }
+// function useGasFeeQuery(tx?: TransactionRequest, skip: boolean = !tx) {
+//   const gasFeeFetcher = useCallback(async () => {
+//     if (skip) {
+//       return;
+//     }
 
-    const res = await fetch(`${UNISWAP_API_URL}/v1/gas-fee`, {
-      method: 'POST',
-      body: JSON.stringify(tx),
-    })
+//     const res = await fetch(`${UNISWAP_API_URL}/v1/gas-fee`, {
+//       method: "POST",
+//       body: JSON.stringify(tx),
+//     });
 
-    const body = (await res.json()) as GasFeeResponse
+//     const body = (await res.json()) as GasFeeResponse;
 
-    if (isErrorResponse(res, body)) {
-      return
-    }
+//     if (isErrorResponse(res, body)) {
+//       return;
+//     }
 
-    return body
-  }, [skip, tx])
+//     return body;
+//   }, [skip, tx]);
 
-  return gasFeeFetcher
-}
+//   return gasFeeFetcher;
+// }
--- packages/wallet/src/constants/tokens.ts
+++ packages/wallet/src/constants/tokens.ts
@@ -1,134 +1,162 @@
 // Copied from https://github.com/Uniswap/interface/blob/main/src/constants/tokens.ts
-import { Token } from '@taraswap/sdk-core'
-import { ChainId } from 'uniswap/src/types/chains'
-import { UNI_ADDRESS } from 'wallet/src/constants/addresses'
-import { CHAIN_INFO } from 'wallet/src/constants/chains'
+import { Token } from "@taraswap/sdk-core";
+import { ChainId } from "uniswap/src/types/chains";
+import { UNI_ADDRESS } from "wallet/src/constants/addresses";
+import { CHAIN_INFO } from "wallet/src/constants/chains";
+
+export const LARA_TARAXA = new Token(
+  ChainId.Taraxa,
+  "0xE6A69cD4FF127ad8E53C21a593F7BaC4c608945e",
+  18,
+  "LARA",
+  "Lara"
+);
+
+export const USDT_TARAXA = new Token(
+  ChainId.Taraxa,
+  "0x5a51fadEF313f503239D1FE0b25DE2486B022c02",
+  6,
+  "TUSDT",
+  "Test Tether"
+);
 
 export const DAI = new Token(
   ChainId.Mainnet,
-  '0x6b175474e89094c44da98b954eedeac495271d0f',
+  "0x6b175474e89094c44da98b954eedeac495271d0f",
   18,
-  'DAI',
-  'Dai Stablecoin'
-)
+  "DAI",
+  "Dai Stablecoin"
+);
 export const DAI_ARBITRUM_ONE = new Token(
   ChainId.ArbitrumOne,
-  '0xda10009cbd5d07dd0cecc66161fc93d7c9000da1',
+  "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1",
   18,
-  'DAI',
-  'Dai stable coin'
-)
+  "DAI",
+  "Dai stable coin"
+);
 export const USDC = new Token(
   ChainId.Mainnet,
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
   6,
-  'USDC',
-  'USD//C'
-)
+  "USDC",
+  "USD//C"
+);
 export const USDC_ARBITRUM = new Token(
   ChainId.ArbitrumOne,
-  '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+  "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
   6,
-  'USDC',
-  'USD//C'
-)
+  "USDC",
+  "USD//C"
+);
 export const USDBC_BASE = new Token(
   ChainId.Base,
-  '0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca',
+  "0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca",
   6,
-  'USDbC',
-  'USD Base Coin'
-)
+  "USDbC",
+  "USD Base Coin"
+);
 export const USDT_BNB = new Token(
   ChainId.Bnb,
-  '0x55d398326f99059ff775485246999027b3197955',
+  "0x55d398326f99059ff775485246999027b3197955",
   18,
-  'USDT',
-  'TetherUSD'
-)
+  "USDT",
+  "TetherUSD"
+);
 export const USDC_OPTIMISM = new Token(
   ChainId.Optimism,
-  '0x7f5c764cbc14f9669b88837ca1490cca17c31607',
+  "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
   6,
-  'USDC',
-  'USD//C'
-)
+  "USDC",
+  "USD//C"
+);
 export const USDC_POLYGON = new Token(
   ChainId.Polygon,
-  '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+  "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
   6,
-  'USDC',
-  'USD//C'
-)
+  "USDC",
+  "USD//C"
+);
 
 export const USDC_GOERLI = new Token(
   ChainId.Polygon,
-  '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
+  "0x07865c6e87b9f70255377e024ace6630c1eaa37f",
   6,
-  'USDC',
-  'USD//C'
-)
+  "USDC",
+  "USD//C"
+);
 
 export const USDT = new Token(
   ChainId.Mainnet,
-  '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  "0xdac17f958d2ee523a2206206994597c13d831ec7",
   6,
-  'USDT',
-  'Tether USD'
-)
+  "USDT",
+  "Tether USD"
+);
 
 export const USDB = new Token(
   ChainId.Blast,
-  '0x4300000000000000000000000000000000000003',
+  "0x4300000000000000000000000000000000000003",
   18,
-  'USDB',
-  'USDB'
-)
+  "USDB",
+  "USDB"
+);
 
 export const CUSD = new Token(
   ChainId.Celo,
-  '0x765de816845861e75a25fca122bb6898b8b1282a',
+  "0x765de816845861e75a25fca122bb6898b8b1282a",
   18,
-  'CUSD',
-  'CUSD'
-)
+  "CUSD",
+  "CUSD"
+);
 
 export const USDC_AVALANCHE = new Token(
   ChainId.Avalanche,
-  '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
+  "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
   6,
-  'USDC',
-  'USDC'
-)
+  "USDC",
+  "USDC"
+);
 
 export const USDzC = new Token(
   ChainId.Zora,
-  '0xCccCCccc7021b32EBb4e8C08314bD62F7c653EC4',
+  "0xCccCCccc7021b32EBb4e8C08314bD62F7c653EC4",
   6,
-  'USDzC',
-  'USD Coin'
-)
+  "USDzC",
+  "USD Coin"
+);
 
 export const WBTC = new Token(
   ChainId.Mainnet,
-  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+  "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
   8,
-  'WBTC',
-  'Wrapped BTC'
-)
+  "WBTC",
+  "Wrapped BTC"
+);
 
 export const UNI = {
-  [ChainId.Mainnet]: new Token(ChainId.Mainnet, UNI_ADDRESS[ChainId.Mainnet], 18, 'UNI', 'Uniswap'),
-  [ChainId.Goerli]: new Token(ChainId.Goerli, UNI_ADDRESS[ChainId.Goerli], 18, 'UNI', 'Uniswap'),
-}
+  [ChainId.Mainnet]: new Token(
+    ChainId.Mainnet,
+    UNI_ADDRESS[ChainId.Mainnet],
+    18,
+    "UNI",
+    "Uniswap"
+  ),
+  [ChainId.Goerli]: new Token(
+    ChainId.Goerli,
+    UNI_ADDRESS[ChainId.Goerli],
+    18,
+    "UNI",
+    "Uniswap"
+  ),
+};
 
 export function wrappedNativeCurrency(chainId: ChainId): Token {
-  const wrappedCurrencyInfo = CHAIN_INFO[chainId].wrappedNativeCurrency
+  const wrappedCurrencyInfo = CHAIN_INFO[chainId].wrappedNativeCurrency;
   return new Token(
     chainId,
     wrappedCurrencyInfo.address,
     wrappedCurrencyInfo.decimals,
     wrappedCurrencyInfo.symbol,
     wrappedCurrencyInfo.name
-  )
+  );
 }
