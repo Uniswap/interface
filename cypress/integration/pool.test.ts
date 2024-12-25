9f1bbadc689a27e318660cd03dca270c92a9c917
alagunoff <lagunov.artamon@gmail.com>
2024-12-25 13:35:16 +0300

    chore: remove some dependencies

--- cypress/integration/pool.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Pool', () => {
-  beforeEach(() => cy.visit('/pool'))
-  it('add liquidity links to /add/ETH', () => {
-    cy.get('#join-pool-button').click()
-    cy.url().should('contain', '/add/ETH')
-  })
-})
