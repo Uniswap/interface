1bf6943ad6271cd4e30ccec72476e27b360734fe
Uniswap Labs Service Account <hello-happy-puppy@users.noreply.github.com>
2025-02-26 19:11:17 +0000

    ci(release): publish latest release

--- packages/uniswap/src/features/transactions/swap/hooks/usePollingIntervalByChain.ts
+++ packages/uniswap/src/features/transactions/swap/hooks/usePollingIntervalByChain.ts
@@ -21,9 +21,19 @@ export function usePollingIntervalByChain(chainId?: UniverseChainId): number {
     AVERAGE_L2_BLOCK_TIME_MS,
   )
 
+  const monadTestnetPollingIntervalMs = useDynamicConfigValue(
+    DynamicConfigs.Swap,
+    SwapConfigKey.MonadTestnetPollingIntervalMs,
+    AVERAGE_L2_BLOCK_TIME_MS,
+  )
+
   // TODO(WEB-6132): remove this flag once short term experiment is complete
   const enableTwoSecondInterval = useFeatureFlag(FeatureFlags.TwoSecondSwapQuotePollingInterval)
   const l2PollingInterval = enableTwoSecondInterval ? 2 * ONE_SECOND_MS : averageL2BlockTimeMs
 
+  // Remove this dynamic config once Monad RPC latency issues are resolved
+  if (chainId === UniverseChainId.MonadTestnet) {
+    return monadTestnetPollingIntervalMs
+  }
   return isMainnetChainId(chainId) ? averageL1BlockTimeMs : l2PollingInterval
 }
