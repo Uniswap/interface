751d8a8bf608b882e9df2b176a39674b1dc84015
zumairaka <zumairaka@gmail.com>
2025-05-01 13:28:20 +0530

    Fix: compile-ajv-validator's directory issue

--- apps/web/scripts/compile-ajv-validators.js
+++ apps/web/scripts/compile-ajv-validators.js
@@ -6,15 +6,19 @@ const Ajv = require('ajv')
 const standaloneCode = require('ajv/dist/standalone').default
 const addFormats = require('ajv-formats')
 const schema = require('@uniswap/token-lists/dist/tokenlist.schema.json')
+const outputDir = path.join(__dirname, '../src/utils/__generated__')
+
+// ensure the directory exists
+fs.mkdirSync(outputDir, { recursive: true })
 
 const tokenListAjv = new Ajv({ code: { source: true, esm: true } })
 addFormats(tokenListAjv)
 const validateTokenList = tokenListAjv.compile(schema)
 let tokenListModuleCode = standaloneCode(tokenListAjv, validateTokenList)
-fs.writeFileSync(path.join(__dirname, '../src/utils/__generated__/validateTokenList.js'), tokenListModuleCode)
+fs.writeFileSync(path.join(outputDir, 'validateTokenList.js'), tokenListModuleCode)
 
 const tokensAjv = new Ajv({ code: { source: true, esm: true } })
 addFormats(tokensAjv)
 const validateTokens = tokensAjv.compile({ ...schema, required: ['tokens'] })
 let tokensModuleCode = standaloneCode(tokensAjv, validateTokens)
-fs.writeFileSync(path.join(__dirname, '../src/utils/__generated__/validateTokens.js'), tokensModuleCode)
+fs.writeFileSync(path.join(outputDir, 'validateTokens.js'), tokensModuleCode)
