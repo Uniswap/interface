c01f697c363ab2703fc4b92e02cc8d1b380dd900
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-09-02 17:08:50 +0200

    Revert "Merge pull request #5 from JuiceSwapxyz/fix/github-actions-workflow"
    This reverts commit 73cf6f4b2606fb75527b7cafd0a4fca742b970aa, reversing
    changes made to bd625f049c4b4ea380d340d34f9c9df578264fb3.

--- apps/web/src/utils/validateTokenList.ts
+++ apps/web/src/utils/validateTokenList.ts
@@ -17,10 +17,10 @@ async function validate(schema: ValidationSchema, data: unknown): Promise<unknow
   let validatorImport
   switch (schema) {
     case ValidationSchema.LIST:
-      validatorImport = await import('utils/__generated__/validateTokenList.js')
+      validatorImport = await import('utils/__generated__/validateTokenList')
       break
     case ValidationSchema.TOKENS:
-      validatorImport = await import('utils/__generated__/validateTokens.js')
+      validatorImport = await import('utils/__generated__/validateTokens')
       break
     default:
       throw new Error('No validation function specified for token list schema')
